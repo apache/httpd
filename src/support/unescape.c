babeec61586be389f2846cba0533321e5eedfe26
brian <brian@unknown>
1996-05-28 00:35:17 +0000

    Submitted by:	Brian Behlendorf Obtained from:  "ken@spry.com"?
    Removed hole through which carriage returns could be passed to shell scripts.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@76517 13f79535-47bb-0310-9956-ffa450edef68

--- src/support/unescape.c
+++ src/support/unescape.c
@@ -52,7 +52,7 @@ void escape_shell_cmd(char *cmd) {
 
     l=strlen(cmd);
     for(x=0;cmd[x];x++) {
-        if(ind("&;`'|*?-~<>^()[]{}$\\",cmd[x]) != -1){
+        if(ind("&;`'|*?-~<>^()[]{}$\\\n",cmd[x]) != -1){
             for(y=l+1;y>x;y--)
                 cmd[y] = cmd[y-1];
             l++; /* length has been increased */
