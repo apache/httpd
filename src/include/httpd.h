93e09620c9c2f0a6389ffadea97ea44efcca6c01
Mark J. Cox <mjc@apache.org>
1996-06-05 15:23:05 +0000

    Doh!
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@76537 13f79535-47bb-0310-9956-ffa450edef68

--- src/include/httpd.h
+++ src/include/httpd.h
@@ -230,7 +230,7 @@
  * Example: "Apache/1.1b3 MrWidget/0.1-alpha" 
  */
 
-#define SERVER_VERSION "Apache/1.1b3-dev" /* SEE COMMENTS ABOOE */
+#define SERVER_VERSION "Apache/1.1b3-dev" /* SEE COMMENTS ABOVE */
 
 #define SERVER_PROTOCOL "HTTP/1.0"
 #define SERVER_SUPPORT "http://www.apache.org/"
