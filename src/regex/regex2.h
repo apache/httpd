f43232fc2dfde75ccc80a635f6fdf1c7b977cd01
Ben Laurie <ben@apache.org>
1996-07-24 13:52:27 +0000

    Fix compiler warnings.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@76656 13f79535-47bb-0310-9956-ffa450edef68

--- src/regex/regex2.h
+++ src/regex/regex2.h
@@ -61,11 +61,11 @@ typedef long sopno;
 #define	OLPAREN	(13<<OPSHIFT)	/* (		fwd to )		*/
 #define	ORPAREN	(14<<OPSHIFT)	/* )		back to (		*/
 #define	OCH_	(15<<OPSHIFT)	/* begin choice	fwd to OOR2		*/
-#define	OOR1	(16<<OPSHIFT)	/* | pt. 1	back to OOR1 or OCH_	*/
-#define	OOR2	(17<<OPSHIFT)	/* | pt. 2	fwd to OOR2 or O_CH	*/
-#define	O_CH	(18<<OPSHIFT)	/* end choice	back to OOR1		*/
-#define	OBOW	(19<<OPSHIFT)	/* begin word	-			*/
-#define	OEOW	(20<<OPSHIFT)	/* end word	-			*/
+#define	OOR1	(16u<<OPSHIFT)	/* | pt. 1	back to OOR1 or OCH_	*/
+#define	OOR2	(17u<<OPSHIFT)	/* | pt. 2	fwd to OOR2 or O_CH	*/
+#define	O_CH	(18u<<OPSHIFT)	/* end choice	back to OOR1		*/
+#define	OBOW	(19u<<OPSHIFT)	/* begin word	-			*/
+#define	OEOW	(20u<<OPSHIFT)	/* end word	-			*/
 
 /*
  * Structure for [] character-set representation.  Character sets are
