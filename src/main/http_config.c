90b7f95ec372b67f101b981f4b2b01f7a126087f
Alexei Kosut <akosut@apache.org>
1996-08-10 22:12:18 +0000

    Cause Apache to die gracefully (instad of a likely seg fault) if a module compiled under an older API version attempts to load.
    Reviewed by: sameer@c2.net, rasmus@madhaus.utcs.utoronto.ca
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@76723 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/http_config.c
+++ src/main/http_config.c
@@ -362,6 +362,18 @@ void add_module (module *m)
      * teased out...
      */
 
+    /* At some point, we may want to offer back-compatibility for
+     * loading modules that are for older versions of Apache. For now,
+     * though, we don't.
+     */
+
+    if (m->version != MODULE_MAGIC_NUMBER) {
+	fprintf(stderr, "httpd: module \"%s\" is not compatible with this "
+		        "version of Apache.\n", m->name);
+	fprintf(stderr, "Please contact the author for the correct version.\n");
+	exit(1);
+    }
+
     m->next = top_module;
     top_module = m;
     m->module_index = num_modules++;
