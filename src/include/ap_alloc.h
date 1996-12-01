e12bdb748508f5cd9700a992eccaf664d15430d5
Randy Terbush <randy@apache.org>
1996-12-01 20:32:15 +0000

    Copyright updates for /src
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@77128 13f79535-47bb-0310-9956-ffa450edef68

--- src/include/ap_alloc.h
+++ src/include/ap_alloc.h
@@ -1,6 +1,6 @@
 
 /* ====================================================================
- * Copyright (c) 1995 The Apache Group.  All rights reserved.
+ * Copyright (c) 1995,1996 The Apache Group.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
