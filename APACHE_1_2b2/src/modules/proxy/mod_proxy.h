55fb7a44750e0b4a9b23b63358bce67f7a8aec2e
(no author) <(no author)@unknown>
1996-12-10 01:15:10 +0000

    This commit was manufactured by cvs2svn to create tag 'APACHE_1_2b2'.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/tags/1.3@77249 13f79535-47bb-0310-9956-ffa450edef68

---
+++ APACHE_1_2b2/src/modules/proxy/mod_proxy.h
@@ -0,0 +1,276 @@
+/* ====================================================================
+ * Copyright (c) 1996 The Apache Group.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this
+ *    software must display the following acknowledgment:
+ *    "This product includes software developed by the Apache Group
+ *    for use in the Apache HTTP server project (http://www.apache.org/)."
+ *
+ * 4. The names "Apache Server" and "Apache Group" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission.
+ *
+ * 5. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the Apache Group
+ *    for use in the Apache HTTP server project (http://www.apache.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE APACHE GROUP ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE APACHE GROUP OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Group and was originally based
+ * on public domain software written at the National Center for
+ * Supercomputing Applications, University of Illinois, Urbana-Champaign.
+ * For more information on the Apache Group and the Apache HTTP server
+ * project, please see <http://www.apache.org/>.
+ *
+ */
+
+/*
+ * Main include file for the Apache proxy
+ */
+
+/*
+
+Note that the Explain() stuff is not yet complete.
+Also note numerous FIXMEs and CHECKMEs which should be eliminated.
+
+If TESTING is set, then garbage collection doesn't delete ... probably a good
+idea when hacking.
+
+This code is still experimental!
+
+Things to do:
+
+1. Make it garbage collect in the background, not while someone is waiting for
+a response!
+
+2. Check the logic thoroughly.
+
+3. Empty directories are only removed the next time round (but this does avoid
+two passes). Consider doing them the first time round.
+
+Ben Laurie <ben@algroup.co.uk> 30 Mar 96
+
+More things to do:
+
+0. Code cleanup
+
+1. add 230 response output and PASV mode for ftp now that it works
+
+2. Add gopher & WAIS
+
+3. Various other fixups to insure no NULL strings parsed, etc.
+
+4. NoProxy directive for excluding sites to proxy
+ 
+5. Imply NoCache * if cache directory is not configured, to enable proxy
+   without cache (and avoid SIGSEGV)
+ 
+6. Use protocol handler struct a la Apache module handlers (Dirk van Gulik)
+ 
+7. Use a cache expiry database for more efficient GC (Jeremy Wohl)
+
+8. Handle multiple IPs for doconnect()
+
+9. Bulletproof GC against SIGALRM
+
+10. Make HTTPS and SNEWS ports configurable from a list
+
+Chuck Murcko <chuck@telebase.com> 1 Oct 96
+
+*/
+
+#define TESTING	0
+#undef EXPLAIN
+
+#include "httpd.h"
+#include "http_config.h"
+#include "http_protocol.h"
+  
+#include "explain.h"
+
+DEF_Explain
+
+
+extern module proxy_module;
+
+
+/* for proxy_canonenc() */
+enum enctype { enc_path, enc_search, enc_user, enc_fpath, enc_parm }; 
+ 
+#define HDR_APP (0)	/* append header, for proxy_add_header() */
+#define HDR_REP (1)	/* replace header, for proxy_add_header() */
+
+/* number of characters in the hash */
+#define HASH_LEN (22*2)
+
+#define	SEC_ONE_DAY		86400	/* one day, in seconds */
+#define	SEC_ONE_HR		3600	/* one hour, in seconds */
+
+#define	DEFAULT_FTP_DATA_PORT	20
+#define	DEFAULT_FTP_PORT	21
+#define	DEFAULT_GOPHER_PORT	70
+#define	DEFAULT_NNTP_PORT	119
+#define	DEFAULT_WAIS_PORT	210
+#define	DEFAULT_HTTPS_PORT	443
+#define	DEFAULT_SNEWS_PORT	563
+#define	DEFAULT_PROSPERO_PORT	1525	/* WARNING: conflict w/Oracle */
+
+/* Some WWW schemes and their default ports; this is basically /etc/services */
+struct proxy_services
+{
+    const char *scheme;
+    int port;
+};
+
+/* static information about a remote proxy */
+struct proxy_remote
+{
+    const char *scheme;    /* the schemes handled by this proxy, or '*' */
+    const char *protocol;  /* the scheme used to talk to this proxy */
+    const char *hostname;  /* the hostname of this proxy */
+    int port;              /* the port for this proxy */
+};
+
+struct proxy_alias {
+    char *real;
+    char *fake;
+};
+
+struct nocache_entry {
+    char *name;
+};
+
+#define DEFAULT_CACHE_SPACE 5
+#define DEFAULT_CACHE_MAXEXPIRE SEC_ONE_DAY
+#define DEFAULT_CACHE_EXPIRE    SEC_ONE_HR
+#define DEFAULT_CACHE_LMFACTOR (0.1)
+
+/* static information about the local cache */
+struct cache_conf
+{
+    const char *root;   /* the location of the cache directory */
+    int space;          /* Maximum cache size (in 1024 bytes) */
+    int maxexpire;      /* Maximum time to keep cached files in secs */
+    int defaultexpire;  /* default time to keep cached file in secs */
+    double lmfactor;    /* factor for estimating expires date */
+    int gcinterval;     /* garbage collection interval, in seconds */
+    int dirlevels;	/* Number of levels of subdirectories */
+    int dirlength;	/* Length of subdirectory names */
+};
+
+typedef struct
+{
+    struct cache_conf cache;  /* cache configuration */
+    array_header *proxies;
+    array_header *aliases;
+    array_header *nocaches;
+    int req;                 /* true if proxy requests are enabled */
+} proxy_server_conf;
+
+struct hdr_entry
+{
+    char *field;
+    char *value;
+};
+
+/* caching information about a request */
+struct cache_req
+{
+    request_rec *req;  /* the request */
+    char *url;         /* the URL requested */
+    char *filename;    /* name of the cache file, or NULL if no cache */
+    char *tempfile;    /* name of the temporary file, of NULL if not caching */
+    time_t ims;        /* if-modified-since date of request; -1 if no header */
+    BUFF *fp;          /* the cache file descriptor if the file is cached
+                          and may be returned, or NULL if the file is
+                          not cached (or must be reloaded) */
+    time_t expire;      /* calculated expire date of cached entity */
+    time_t lmod;        /* last-modified date of cached entity */
+    time_t date;        /* the date the cached file was last touched */
+    int version;        /* update count of the file */
+    unsigned int len;   /* content length */
+    char *protocol;     /* Protocol, and major/minor number, e.g. HTTP/1.1 */
+    int status;         /* the status of the cached file */
+    char *resp_line;    /* the whole status like (protocol, code + message) */
+    array_header *hdrs; /* the HTTP headers of the file */
+};
+      
+/* Function prototypes */
+
+/* proxy_cache.c */
+
+void proxy_cache_tidy(struct cache_req *c);
+int proxy_cache_check(request_rec *r, char *url, struct cache_conf *conf,
+    struct cache_req **cr);
+int proxy_cache_update(struct cache_req *c, array_header *resp_hdrs,
+    const char *protocol, int nocache);
+
+/* proxy_connect.c */
+
+int proxy_connect_handler(request_rec *r, struct cache_req *c, char *url);
+
+/* proxy_ftp.c */
+
+int proxy_ftp_canon(request_rec *r, char *url);
+int proxy_ftp_handler(request_rec *r, struct cache_req *c, char *url);
+
+/* proxy_http.c */
+
+int proxy_http_canon(request_rec *r, char *url, const char *scheme,
+    int def_port);
+int proxy_http_handler(request_rec *r, struct cache_req *c, char *url,
+    const char *proxyhost, int proxyport);
+
+/* proxy_util.c */
+
+int proxy_hex2c(const char *x);
+void proxy_c2hex(int ch, char *x);
+char *proxy_canonenc(pool *p, const char *x, int len, enum enctype t,
+    int isenc);
+char *proxy_canon_netloc(pool *pool, char **const urlp, char **userp,
+    char **passwordp, char **hostp, int *port);
+char *proxy_date_canon(pool *p, char *x);
+array_header *proxy_read_headers(pool *pool, char *buffer, int size, BUFF *f);
+long int proxy_send_fb(BUFF *f, request_rec *r, BUFF *f2, struct cache_req *c);
+struct hdr_entry *proxy_get_header(array_header *hdrs_arr, const char *name);
+struct hdr_entry *proxy_add_header(array_header *hdrs_arr, char *field,
+    char *value, int rep);
+void proxy_del_header(array_header *hdrs_arr, const char *field);
+void proxy_send_headers(BUFF *fp, const char *respline, array_header *hdrs_arr);
+int proxy_liststr(const char *list, const char *val);
+void proxy_hash(const char *it, char *val,int ndepth,int nlength);
+int proxy_hex2sec(const char *x);
+void proxy_sec2hex(int t, char *y);
+void proxy_log_uerror(const char *routine, const char *file, const char *err,
+    server_rec *s);
+BUFF *proxy_cache_error(struct cache_req *r);
+int proxyerror(request_rec *r, const char *message);
+const char *proxy_host2addr(const char *host, struct in_addr *addr);
+int proxy_doconnect(int sock, struct sockaddr_in *addr, request_rec *r);
+
---
+++ APACHE_1_2b2/src/support/suexec.c
@@ -0,0 +1,401 @@
+/* ====================================================================
+ * Copyright (c) 1995,1996 The Apache Group.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this
+ *    software must display the following acknowledgment:
+ *    "This product includes software developed by the Apache Group
+ *    for use in the Apache HTTP server project (http://www.apache.org/)."
+ *
+ * 4. The names "Apache Server" and "Apache Group" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission.
+ *
+ * 5. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the Apache Group
+ *    for use in the Apache HTTP server project (http://www.apache.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE APACHE GROUP ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE APACHE GROUP OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Group and was originally based
+ * on public domain software written at the National Center for
+ * Supercomputing Applications, University of Illinois, Urbana-Champaign.
+ * For more information on the Apache Group and the Apache HTTP server
+ * project, please see <http://www.apache.org/>.
+ *
+ */
+
+/*
+ * suexec.c -- "Wrapper" support program for suEXEC behaviour for Apache
+ *
+ ***********************************************************************
+ *
+ * NOTE! : DO NOT edit this code!!!  Unless you know what you are doing,
+ *         editing this code might open up your system in unexpected 
+ *         ways to would-be crackers.  Every precaution has been taken 
+ *         to make this code as safe as possible; alter it at your own
+ *         risk.
+ *
+ ***********************************************************************
+ *
+ * Codebase originally from Majordomo(v1.93) release.
+ * Heavy modifications by:
+ *    Jason A. Dour (jad@bcc.louisville.edu)
+ *    Randy Terbush (randy@zyzzyva.com)
+ *
+ * Version 0.1.0 - Jason A. Dour
+ *    First beta.  Removed HAVE_RLIMIT and related rlimit code
+ * now that the server handles the funcitonality.  Moved user-
+ * defined code to suexec.h.  Added "DON'T EDIT" warning in code.
+ * No more "security by obscurity"...comments added at each step.
+ *
+ * Version 0.0.3 - Jason A. Dour
+ *    Third alpha.  Added NNAME and NGID directives to fix
+ * portability problem -- various systems have different
+ * values for nobody and nogroup.  Yuck.  Submitted to the
+ * Apache development group for consideration.
+ *
+ * Version 0.0.2 - Jason A. Dour
+ *    Second alpha.  Cleaned up code and comments.  Fixed some
+ * test-case bugs.  Cleaned up and locked down the groups access
+ * list.  Fixed setgid behaviour.  Added more paranoia checks.
+ * Added ~userid support.  Cleaned up exit codes.
+ *
+ * Version 0.0.1 - Randy Terbush
+ *    First assigned version. Heavily modified to act as generic
+ * SUID wrapper for Apache.  Submitted to the Apache development
+ * group for consideration.
+ *
+ * Version Primordial Ooze - Jason A. Dour
+ *    First version.   Heavily modified from MDomo source.  Acted
+ * only for ~userdir requests out of the ~userdir/cgi-bin dir.  Not
+ * extremely useful...but it worked.
+ */
+
+
+#include "suexec.h"
+
+#include <sys/param.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <strings.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+#include <sys/stat.h>
+
+
+static FILE *log;
+
+static void
+err_output (const char *fmt, va_list ap)
+{
+    time_t timevar;
+    struct tm *lt;
+
+    if (!log)
+	if ((log = fopen (LOG_EXEC, "a")) == NULL)
+	{
+	    fprintf (stderr, "failed to open log file\n");
+	    perror ("fopen");
+	}
+
+    time (&timevar);
+    lt = localtime (&timevar);
+    
+    fprintf (log, "[%.2d:%.2d:%.2d %.2d-%.2d-%.2d]: ", lt->tm_hour, lt->tm_min,
+	     lt->tm_sec, lt->tm_mday, lt->tm_mon, lt->tm_year);
+    
+    vfprintf (log, fmt, ap);
+
+    fflush (log);
+    return;
+}
+
+void
+log_err (const char *fmt, ...)
+{
+#ifdef LOG_EXEC
+    va_list     ap;
+
+    va_start (ap, fmt);
+    err_output (fmt, ap);
+    va_end(ap);
+#endif /* LOG_EXEC */
+    return;
+}
+
+int
+main(int argc, char *argv[], char **env)
+{
+    int doclen;             /* length of the docroot     */
+    int userdir = 0;        /* ~userdir flag             */
+    uid_t uid;              /* user information          */
+    char *target_uname;     /* target user name          */
+    char *target_gname;     /* target group name         */
+    char *prog;             /* name of this program      */
+    char *cmd;              /* command to be executed    */
+    char *cwd;              /* current working directory */
+    char *buf = NULL;       /* temporary buffer          */
+    struct passwd *pw;      /* password entry holder     */
+    struct group *gr;       /* group entry holder        */
+    struct stat dir_info;   /* directory info holder     */
+    struct stat prg_info;   /* program info holder       */
+
+    
+
+    /*
+     * If there are a proper number of arguments, set
+     * all of them to variables.  Otherwise, error out.
+     */
+    prog = argv[0];
+    if (argc < 4)
+    {
+	log_err ("too few arguments\n");
+	exit(101);
+    }
+    target_uname = argv[1];
+    target_gname = argv[2];
+    cmd = argv[3];
+
+    /*
+     * Check existence/validity of the UID of the user
+     * running this program.  Error out if invalid.
+     */
+    uid = getuid();
+    if ((pw = getpwuid (uid)) == NULL)
+    {
+	log_err ("invalid uid: (%ld)\n", uid);
+	exit (102);
+    }
+    
+    /*
+     * Check to see if the user running this program
+     * is the user allowed to do so as defined in
+     * suexec.h.  If not the allowed user, error out.
+     */
+    if (strcmp (HTTPD_USER, pw->pw_name))
+    {
+	log_err ("user mismatch (%s)\n", pw->pw_name);
+	exit (103);
+    }
+    
+    /*
+     * Check for a '/' in the command to be executed,
+     * to protect against attacks.  If a '/' is
+     * found, error out.  Naughty naughty crackers.
+     */
+    if (strchr (cmd, '/') != (char) NULL )
+    {
+	log_err ("invalid command (%s)\n", cmd);
+	exit (104);
+    }
+
+    /*
+     * Check to see if this is a ~userdir request.  If
+     * so, set the flag, and remove the '~' from the
+     * target username.
+     */
+    if (!strncmp( "~", target_uname, 1))
+    {
+	target_uname++;
+	userdir = 1;
+    }
+
+    /*
+     * Get the current working directory, as well as
+     * the proper document root (dependant upon whether
+     * or not it is a ~userdir request.  Error out if
+     * we cannot get either one, or if the command is
+     * not in the docroot.
+     */
+    if ((cwd = getcwd (buf, MAXPATHLEN)) == NULL)
+    {
+        log_err ("cannot get current working directory\n");
+        exit (105);
+    }
+    if (userdir)
+    {
+	doclen = strlen (pw->pw_dir);
+	if (strncmp (cwd, pw->pw_dir, doclen))
+        {   
+	    log_err ("command not in docroot (%s/%s)\n", cwd, cmd);
+	    exit (106);
+	}
+    } else {
+	doclen = strlen (DOC_ROOT);
+	if (strncmp (cwd, DOC_ROOT, doclen))
+        {
+	    log_err ("command not in docroot (%s/%s)\n", cwd, cmd);
+	    exit (106);
+	}
+    }
+
+    /*
+     * Stat the cwd and verify it is a directory, or error out.
+     */
+    if ( (lstat (cwd, &dir_info))     ||
+         !(S_ISDIR(dir_info.st_mode)) )
+    {
+	log_err ("cannot stat directory: (%s)\n", cwd);
+	exit (107);
+    }
+
+    /*
+     * Error out if cwd is writable by others.
+     */
+    if ((dir_info.st_mode & S_IWOTH) || (dir_info.st_mode & S_IWGRP))
+    {
+	log_err ("directory is writable by others: (%s)\n", cwd);
+	exit (108);
+    }
+
+    /*
+     * Error out if we cannot stat the program.
+     */
+    if ((lstat (cmd, &prg_info)) || (S_ISLNK(prg_info.st_mode)))
+    {
+	log_err ("cannot stat program: (%s)\n", cmd);
+	exit (109);
+    }
+
+    /*
+     * Error out if the program is writable by others.
+     */
+    if ((prg_info.st_mode & S_IWOTH) || (prg_info.st_mode & S_IWGRP))
+    {
+	log_err ("file is writable by others: (%s/%s)\n", cwd, cmd);
+	exit (110);
+    }
+
+    /*
+     * Error out if the file is setuid or setgid.
+     */
+    if ((prg_info.st_mode & S_ISUID) || (prg_info.st_mode & S_ISGID))
+    {
+	log_err ("file is either setuid or setgid: (%s/%s)\n",cwd,cmd);
+	exit (111);
+    }
+
+    /*
+     * Error out if the target username is invalid.
+     */
+    if ( (pw = getpwnam (target_uname)) == NULL )
+    {
+	log_err ("invalid target user name: (%s)\n", target_uname);
+	exit (112);
+    }
+
+    /*
+     * Error out if the target group name is invalid.
+     */
+    if ( (gr = getgrnam (target_gname)) == NULL )
+    {
+	log_err ("invalid target group name: (%s)\n", target_gname);
+	exit (113);
+    }
+
+    /*
+     * Error out if the target name/group is different from
+     * the name/group of the cwd or the program.
+     */
+    if ( (pw->pw_uid != dir_info.st_uid) ||
+	 (gr->gr_gid != dir_info.st_gid) ||
+	 (pw->pw_uid != prg_info.st_uid) ||
+	 (gr->gr_gid != prg_info.st_gid) )
+    {
+	log_err ("target uid/gid (%ld/%ld) mismatch with directory (%ld/%ld) or program (%ld/%ld)\n",
+		 pw->pw_uid, gr->gr_gid,
+		 dir_info.st_uid, dir_info.st_gid,
+		 prg_info.st_uid, prg_info.st_gid);
+	exit (114);
+    }
+
+    /*
+     * Error out if attempt is made to execute as root.  Tsk tsk.
+     */
+    if (pw->pw_uid == 0)
+    {
+	log_err ("cannot run as uid 0 (%s)\n", cmd);
+	exit (115);
+    }
+
+    /*
+     * Error out if attempt is made to execute as root group.  Tsk tsk.
+     */
+    if (gr->gr_gid == 0)
+    {
+	log_err ("cannot run as gid 0 (%s)\n", cmd);
+	exit (116);
+    }
+
+    /*
+     * Log the transaction here to be sure we have an open log 
+     * before we setuid().
+     */
+    log_err ("uid: (%s) gid: (%s) %s\n", target_uname, target_gname, cmd);
+
+    /*
+     * Initialize the group access list for the target user,
+     * and setgid() to the target group. If unsuccessful, error out.
+     */
+    if ( (initgroups (NNAME,NGID) != 0) ||
+         (setgid (gr->gr_gid)      != 0) )
+    {
+        log_err ("failed to initialize groups or setgid (%ld: %s/%s)\n", gr->gr_gid, cwd, cmd);
+        exit (117);
+    }
+
+    /*
+     * setuid() to the target user.  Error out on fail.
+     */
+    if ((setuid (pw->pw_uid)) != 0)
+    {
+	log_err ("failed to setuid (%ld: %s/%s)\n", pw->pw_uid, cwd, cmd);
+	exit (118);
+    }
+
+    /*
+     * Execute the command, replacing our image with its own.
+     */
+    execve (cmd, &argv[3], env);
+
+    /*
+     * (I can't help myself...sorry.)
+     *
+     * Uh oh.  Still here.  Where's the kaboom?  There was supposed to be an
+     * EARTH-shattering kaboom!
+     *
+     * Oh well, log the failure and error out.
+     */
+    log_err ("exec failed (%s)\n", cmd);
+    exit(255);
+}
