a643635ea9e9b17ed9b50a2aa0da60292f3fe823
Roy T. Fielding <fielding@apache.org>
1997-02-21 00:22:29 +0000

    We do not want TRACE to bypass a proxy request, since the proxy is supposed to pass it on following the rules in RFC 2068.  The code is currently failing to respond correctly to full-URIs if there is no proxy module compiled-in, but that's a different problem.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@77662 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/http_request.c
+++ src/main/http_request.c
@@ -870,17 +870,17 @@ void process_request_internal (request_rec *r)
 	return;
     }
 
-    /* We don't want TRACE to run through the normal handler set,
-     * we handle it specially.
-     */
-    if (r->method_number == M_TRACE) {
-	send_http_trace (r);
-	finalize_request_protocol (r);
-	return;
-    }
-
     if (!r->proxyreq)
     {
+        /* We don't want TRACE to run through the normal handler set,
+         * we handle it specially.
+         */
+        if (r->method_number == M_TRACE) {
+            send_http_trace(r);
+            finalize_request_protocol(r);
+            return;
+        }
+
 	access_status = unescape_url(r->uri);
 	if (access_status)
 	{
