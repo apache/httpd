8263c4d4d222fb292c3df0513febba9009de5900
Ben Laurie <ben@apache.org>
1997-02-21 17:12:49 +0000

    Don't note -ve fds for cleanup. Reviewed by: Randy, Chuck, Marc
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@77666 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/buff.c
+++ src/main/buff.c
@@ -149,8 +149,9 @@ bpushfd(BUFF *fb, int fd_in, int fd_out)
 {
     fb->fd = fd_out;
     fb->fd_in = fd_in;
-    note_cleanups_for_fd(fb->pool,fb->fd);
-    if(fb->fd != fb->fd_in)
+    if(fb->fd >= 0)
+	note_cleanups_for_fd(fb->pool,fb->fd);
+    if(fb->fd != fb->fd_in && fb->fd_in >= 0)
 	note_cleanups_for_fd(fb->pool,fb->fd_in);
 }
 
