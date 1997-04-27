d8b60c42eca28e34ac35246cc06e6965c7162e63
dgaudet <dgaudet@unknown>
1997-04-27 07:52:19 +0000

    Clean up Linux configuration by detecting 2.x linux and enabling HAVE_SHMGET, and HAVE_SYS_RESOURCE_H.  Don't touch Linux 1.x systems for fear of various compatibility problems.  Ditch that silly hack LINUX_TWEAK that I wish I hadn't introduced.
    Reviewed by:	Paul S., Roy
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@78039 13f79535-47bb-0310-9956-ffa450edef68

--- src/include/ap_config.h
+++ src/include/ap_config.h
@@ -253,20 +253,12 @@ typedef int pid_t;
 #define NO_USE_SIGACTION
 
 #elif defined(LINUX)
-#ifndef LINUX_TWEAK
-/* the old "stable" way ... it's unfortunate that by doing this we'll incur
- * bug reports from linux users that run multiple Apaches without setting
- * a different ScoreBoardFile for each of them.
- */
-/* #define HAVE_SHMGET */
-#define USE_FCNTL_SERIALIZED_ACCEPT
-#else
-/* this stuff works under 2.0 on i386, but we've had bug reports of it
- * causing compile-time problems on 1.x systems, and on 2.x alphas.
- */
+#if LINUX > 1
 #define HAVE_SHMGET
-/* #define USE_FCNTL_SERIALIZED_ACCEPT */
+#define HAVE_SYS_RESOURCE_H
+typedef int rlim_t;
 #endif
+#define USE_FCNTL_SERIALIZED_ACCEPT
 #undef HAVE_GMTOFF
 #undef NO_KILLPG
 #undef NO_SETSID
