d183a06939bd9b087a7ba8dc005ceaf671287a9a
Jim Jagielski <jim@apache.org>
1997-06-16 18:42:47 +0000

    Make clear when we exit and why
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@78319 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/alloc.c
+++ src/main/alloc.c
@@ -1187,7 +1187,7 @@ int spawn_child_err (pool *p, int (*func)(void *), void *data,
     signal (SIGCHLD, SIG_DFL);	/* Was that it? */
     
     func (data);
-    exit (1);			/* Should never get here... */
+    exit (1);		/* Should only get here if the exec in func() failed */
   }
 
   /* Parent process */
