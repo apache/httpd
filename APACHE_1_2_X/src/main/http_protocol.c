3a476382670183a6a591d9d25811c6fb594bb9bf
dgaudet <dgaudet@unknown>
1997-07-01 01:37:09 +0000

    Added begun_read_body to request_rec so that subreqs and internal redirects won't try to read the request body twice.
    Submitted by: Roy Fielding
    Reviewed by:  Alexei Kosut, Dean Gaudet
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3@78483 13f79535-47bb-0310-9956-ffa450edef68

--- APACHE_1_2_X/src/main/http_protocol.c
+++ APACHE_1_2_X/src/main/http_protocol.c
@@ -868,6 +868,7 @@ void set_sub_req_protocol (request_rec *rnew, const request_rec *r)
     
     rnew->read_length = r->read_length;
     rnew->read_body   = REQUEST_NO_BODY;
+    rnew->begun_read_body = r->begun_read_body;
     
     rnew->main = (request_rec *)r;
 }
@@ -1345,7 +1346,7 @@ int setup_client_block (request_rec *r, int read_policy)
 
 int should_client_block (request_rec *r)
 {
-    if (is_HTTP_ERROR(r->status))
+    if (r->begun_read_body || is_HTTP_ERROR(r->status))
         return 0;
 
     if (!r->read_chunked && (r->remaining <= 0))
@@ -1396,6 +1397,8 @@ long get_client_block (request_rec *r, char *buffer, int bufsiz)
     int c;
     long len_read, len_to_read;
     long chunk_start = 0;
+
+    r->begun_read_body = 1;
 
     if (!r->read_chunked) {                 /* Content-length read */
         len_to_read = (r->remaining > bufsiz) ? bufsiz : r->remaining;
