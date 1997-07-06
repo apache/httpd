cf8350f4bb200ab27f99338f7b34537041b9bb17
Marc Slemko <marc@apache.org>
1997-07-06 02:35:15 +0000

    Bump version back to 1.2.2-dev. PR:
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3@78538 13f79535-47bb-0310-9956-ffa450edef68

--- APACHE_1_2_X/src/include/httpd.h
+++ APACHE_1_2_X/src/include/httpd.h
@@ -256,7 +256,7 @@
  * Example: "Apache/1.1.0 MrWidget/0.1-alpha" 
  */
 
-#define SERVER_BASEVERSION "Apache/1.2.1" /* SEE COMMENTS ABOVE */
+#define SERVER_BASEVERSION "Apache/1.2.2-dev" /* SEE COMMENTS ABOVE */
 #ifdef SERVER_SUBVERSION
 #define SERVER_VERSION	SERVER_BASEVERSION " " SERVER_SUBVERSION
 #else
