575750d8b0e87ab82cbd8520dc2faa6632087230
dgaudet <dgaudet@unknown>
1997-07-21 00:34:05 +0000

    I messed up applying the 1.2.x version of the MSIE patch to the 1.3 tree.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@78710 13f79535-47bb-0310-9956-ffa450edef68

--- src/mod_browser.c
+++ src/mod_browser.c
@@ -185,6 +185,6 @@ module MODULE_VAR_EXPORT browser_module = {
    NULL,			/* type_checker */
    NULL,			/* fixups */
    NULL,			/* logger */
-   NULL				/* header parser */
+   NULL,			/* header parser */
    NULL				/* child_init */
 };
