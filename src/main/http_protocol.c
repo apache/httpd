afba79660e8c622854825af0d160c0034c1a8b9a
dgaudet <dgaudet@unknown>
1997-08-03 21:35:58 +0000

    Reverse this change for now, waiting for Roy's comments on another alternative.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@78885 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/http_protocol.c
+++ src/main/http_protocol.c
@@ -350,9 +350,10 @@ API_EXPORT(int) set_last_modified(request_rec *r, time_t mtime)
 {
     char *etag, weak_etag[MAX_STRING_LEN];
     char *if_match, *if_modified_since, *if_unmodified, *if_nonematch;
-    time_t now;
+    time_t now = time(NULL);
 
-    now = r->request_time;
+    if (now < 0)
+	now = r->request_time;
 
     table_set(r->headers_out, "Last-Modified",
               gm_timestr_822(r->pool, (mtime > now) ? now : mtime));
