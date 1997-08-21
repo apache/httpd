d5442675314e762c720c7fec52ab776e301a35e5
dgaudet <dgaudet@unknown>
1997-08-21 18:12:23 +0000

    Add "TestCompile func" to test if a certain func exists in the already supplied libraries.  Move dbm/db specific code from Configure into the appropriate module file.  Disable the printing of module commands ... they were like total spam when the rest of this patch is taken into account.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@79007 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/standard/mod_auth_dbm.c
+++ src/modules/standard/mod_auth_dbm.c
@@ -79,20 +79,22 @@
  * MODULE-DEFINITION-START
  * Name: dbm_auth_module
  * ConfigStart
-    case "$PLAT" in
-	*-linux*)
-	    # many systems don't have -ldbm
-	    DBM_LIB=""
-	    if ./helpers/TestCompile lib dbm; then
-		DBM_LIB="-ldbm"
-	    elif ./helpers/TestCompile lib ndbm; then
-		DBM_LIB="-lndbm"
-	    fi
-	    ;;
-    esac
-    LIBS="$LIBS $DBM_LIB"
-    if [ "X$DBM_LIB" != "X" ]; then
-	echo " + using $DBM_LIB for mod_auth_dbm"
+    if ! ./helpers/TestCompile func dbm_open; then
+	case "$PLAT" in
+	    *-linux*)
+		# many systems don't have -ldbm
+		DBM_LIB=""
+		if ./helpers/TestCompile lib dbm; then
+		    DBM_LIB="-ldbm"
+		elif ./helpers/TestCompile lib ndbm; then
+		    DBM_LIB="-lndbm"
+		fi
+		;;
+	esac
+	LIBS="$LIBS $DBM_LIB"
+	if [ "X$DBM_LIB" != "X" ]; then
+	    echo " + using $DBM_LIB for mod_auth_dbm"
+	fi
     fi
  * ConfigEnd
  * MODULE-DEFINITION-END
