056bd7c09e82eb08798670f8c58143ef91713c99
Randy Terbush <randy@apache.org>
1997-09-10 02:35:37 +0000

    Attempt to make syslog support more portable as suggested by Alexei.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@79171 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/http_log.c
+++ src/main/http_log.c
@@ -75,26 +75,60 @@ static TRANS facilities[] = {
 #ifdef LOG_AUTHPRIV
     {"authpriv",LOG_AUTHPRIV},
 #endif
+#ifdef LOG_CRON
     {"cron", 	LOG_CRON},
+#endif
+#ifdef LOG_DAEMON
     {"daemon",	LOG_DAEMON},
+#endif
 #ifdef LOG_FTP
     {"ftp",	LOG_FTP},
 #endif
+#ifdef LOG_KERN
     {"kern",	LOG_KERN},
+#endif
+#ifdef LOG_LPR
     {"lpr",	LOG_LPR},
+#endif
+#ifdef LOG_MAIL
     {"mail",	LOG_MAIL},
+#endif
+#ifdef LOG_NEWS
     {"news",	LOG_NEWS},
+#endif
+#ifdef LOG_SYSLOG
     {"syslog",	LOG_SYSLOG},
+#endif
+#ifdef LOG_USER
     {"user",	LOG_USER},
+#endif
+#ifdef LOG_UUCP
     {"uucp",	LOG_UUCP},
+#endif
+#ifdef LOG_LOCAL0
     {"local0",	LOG_LOCAL0},
+#endif
+#ifdef LOG_LOCAL1
     {"local1",	LOG_LOCAL1},
+#endif
+#ifdef LOG_LOCAL2
     {"local2",	LOG_LOCAL2},
+#endif
+#ifdef LOG_LOCAL3
     {"local3",	LOG_LOCAL3},
+#endif
+#ifdef LOG_LOCAL4
     {"local4",	LOG_LOCAL4},
+#endif
+#ifdef LOG_LOCAL5
     {"local5",	LOG_LOCAL5},
+#endif
+#ifdef LOG_LOCAL6
     {"local6",	LOG_LOCAL6},
+#endif
+#ifdef LOG_LOCAL7
     {"local7",	LOG_LOCAL7},
+#endif
     {NULL,		-1},
 };
 #endif
