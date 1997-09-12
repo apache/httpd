ec172b69b019115be0aedc2a88003fd83a0d5775
pcs <pcs@unknown>
1997-09-12 07:28:50 +0000

    Add capability to OS abstract on Unix systems, with either functions, inlines or macros. All code in os.c, os.h and os-inline.c is available to all Apache source files. Only GCC is known to support inline at this stage.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@79186 13f79535-47bb-0310-9956-ffa450edef68

--- src/os/unix/os.c
+++ src/os/unix/os.c
@@ -1,1 +1,6 @@
-char AP_OPERATING_SYSTEM[] = "UNIX";
+/*
+ * This file will include OS specific functions which are not inlineable.
+ * Any inlineable functions should be defined in os-inline.c instead.
+ */
+
+#include "os.h"
