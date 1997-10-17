cca37b4b4a553d334f486924521c7cb5cdd46aeb
Jim Jagielski <jim@apache.org>
1997-10-17 13:28:45 +0000

    Move back down to 1.3b2 for tagging
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@79397 13f79535-47bb-0310-9956-ffa450edef68

--- src/include/httpd.h
+++ src/include/httpd.h
@@ -342,7 +342,7 @@
  * Example: "Apache/1.1.0 MrWidget/0.1-alpha" 
  */
 
-#define SERVER_BASEVERSION "Apache/1.3b3-dev"	/* SEE COMMENTS ABOVE */
+#define SERVER_BASEVERSION "Apache/1.3b2"	/* SEE COMMENTS ABOVE */
 #ifdef SERVER_SUBVERSION
 #define SERVER_VERSION	SERVER_BASEVERSION " " SERVER_SUBVERSION
 #else
