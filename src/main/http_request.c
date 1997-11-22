eb502deb6c7c5a3fd53adb501ad443749b645f93
dgaudet <dgaudet@unknown>
1997-11-22 22:23:41 +0000

    Comment was a little bit inaccurate.  Also explicitly mention 2068 S9, S14.23 in the error message so that maybe we can avoid more and more people complaining about us rejecting HTTP/1.1 requests without a Host: header.
    PR:		1454
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@79639 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/http_request.c
+++ src/main/http_request.c
@@ -988,10 +988,12 @@ void process_request_internal(request_rec *r)
         /*
          * Client sent us a HTTP/1.1 or later request without telling us the
          * hostname, either with a full URL or a Host: header. We therefore
-         * need to (as per the 1.1 spec) send an error
+         * need to (as per the 1.1 spec) send an error.  As a special case,
+	 * HTTP/1.1 mentions twice (S9, S14.23) that a request MUST contain
+	 * a Host: header, and the server MUST respond with 400 if it doesn't.
          */
         aplog_error(APLOG_MARK, APLOG_NOERRNO|APLOG_ERR, r->server,
-               "client sent HTTP/1.1 request without hostname: %s", r->uri);
+               "client sent HTTP/1.1 request without hostname (see RFC2068 section 9, and 14.23): %s", r->uri);
         die(BAD_REQUEST, r);
         return;
     }
