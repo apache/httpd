aebd0723cf3c9e93bf86624ebf10c1fb59b98610
Ken Coar <coar@apache.org>
1998-01-07 16:47:15 +0000

    Update the copyright year of all the various HEAD files to 	include 1998.
    Reviewed by:	Jim Jagielski, Martin Kraemer
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@79843 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/md5c.c
+++ src/main/md5c.c
@@ -32,7 +32,7 @@
  */
 
 /* ====================================================================
- * Copyright (c) 1996,1997 The Apache Group.  All rights reserved.
+ * Copyright (c) 1996-1998 The Apache Group.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
