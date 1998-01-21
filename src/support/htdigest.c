a8986035e58a185a524a529e2aa1a1cffae4d82e
dgaudet <dgaudet@unknown>
1998-01-21 22:05:45 +0000

    Tweaks to use "copy" instead of "cp" under win32 and os2.
    PR:		1482
    Submitted by:	Brian Havard
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@79935 13f79535-47bb-0310-9956-ffa450edef68

--- src/support/htdigest.c
+++ src/support/htdigest.c
@@ -200,7 +200,11 @@ int main(int argc, char *argv[])
     }
     fclose(f);
     fclose(tfp);
+#if defined(__EMX__) || defined(WIN32)
+    sprintf(command, "copy \"%s\" \"%s\"", tn, argv[1]);
+#else
     sprintf(command, "cp %s %s", tn, argv[1]);
+#endif
     system(command);
     unlink(tn);
     exit(0);
