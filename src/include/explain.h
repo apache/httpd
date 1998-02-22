e2c43c5cb5d813190cd53156f4a5e0090316cd35
Martin Kraemer <martin@apache.org>
1998-02-22 21:17:05 +0000

    Rewrite explain.h to use aplog_error(...APLOG_DEBUG...); remove explain.c
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@80289 13f79535-47bb-0310-9956-ffa450edef68

--- src/include/explain.h
+++ src/include/explain.h
@@ -8,17 +8,28 @@
 #define Explain5(f,a1,a2,a3,a4,a5)
 #define Explain6(f,a1,a2,a3,a4,a5,a6)
 #else
-#define DEF_Explain	static const char *__ExplainFile=__FILE__;
-void _Explain(const char *szFile, int nLine, const char *szFmt,...)
-		__attribute__((format(printf,3,4)));
-#define Explain0(f)	_Explain(__ExplainFile,__LINE__,f)
-#define Explain1(f,a1)	_Explain(__ExplainFile,__LINE__,f,a1)
-#define Explain2(f,a1,a2)	_Explain(__ExplainFile,__LINE__,f,a1,a2)
-#define Explain3(f,a1,a2,a3)	_Explain(__ExplainFile,__LINE__,f,a1,a2,a3)
-#define Explain4(f,a1,a2,a3,a4)	_Explain(__ExplainFile,__LINE__,f,a1,a2,a3,a4)
-#define Explain5(f,a1,a2,a3,a4,a5)	\
-			_Explain(__ExplainFile,__LINE__,f,a1,a2,a3,a4,a5)
-#define Explain6(f,a1,a2,a3,a4,a5,a6)	\
-			_Explain(__ExplainFile,__LINE__,f,a1,a2,a3,a4,a5,a6)
+#include "http_log.h"
+#define DEF_Explain
+#define Explain0(f) \
+        aplog_error(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, NULL, \
+                    f)
+#define Explain1(f,a1) \
+        aplog_error(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, NULL, \
+                    f,a1)
+#define Explain2(f,a1,a2) \
+        aplog_error(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, NULL, \
+                    f,a1,a2)
+#define Explain3(f,a1,a2,a3) \
+        aplog_error(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, NULL, \
+                    f,a1,a2,a3)
+#define Explain4(f,a1,a2,a3,a4) \
+        aplog_error(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, NULL, \
+                    f,a1,a2,a3,a4)
+#define Explain5(f,a1,a2,a3,a4,a5)  \
+        aplog_error(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, NULL, \
+                    f,a1,a2,a3,a4,a5)
+#define Explain6(f,a1,a2,a3,a4,a5,a6)   \
+        aplog_error(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, NULL, \
+                    f,a1,a2,a3,a4,a5,a6)
 
 #endif
