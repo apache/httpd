42456d1119395001775c2b227c8d842a42955921
dgaudet <dgaudet@unknown>
1998-03-25 18:14:55 +0000

    Fix Martin's previous checkin... it failed the mod_test_util_uri.c testsuite...  Is there some other bug I'm not seeing Martin?  There was some bogus code previously (namely the test uptr->port == port... 'cause it was always true... thanks for spotting that).
    The test_util_uri thing doesn't test bogus ports, or any other bogosities.
    That would be a welcome addition.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@80663 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/util_uri.c
+++ src/main/util_uri.c
@@ -520,7 +520,7 @@ deal_with_host:
 	if (uri != s) {
 	    port = strtol(uptr->port_str, &endstr, 10);
 	    uptr->port = port;
-	    if (endstr == uri) {
+	    if (*endstr == '\0') {
 		goto deal_with_path;
 	    }
 	    /* Invalid characters after ':' found */
