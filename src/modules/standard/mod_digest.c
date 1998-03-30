e62ec858f32f11099baa4f44a05a87752dd5a1de
Ralf S. Engelschall <rse@apache.org>
1998-03-30 08:06:50 +0000

    remove comment: relict from mod_auth
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@80747 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/standard/mod_digest.c
+++ src/modules/standard/mod_digest.c
@@ -297,7 +297,6 @@ static int authenticate_digest_user(request_rec *r)
 	note_digest_auth_failure(r);
 	return AUTH_REQUIRED;
     }
-    /* anyone know where the prototype for crypt is? */
     if (strcmp(response->digest, find_digest(r, response, a1))) {
 	aplog_error(APLOG_MARK, APLOG_NOERRNO|APLOG_ERR, r->server,
 		    "user %s: password mismatch: %s", c->user, r->uri);
