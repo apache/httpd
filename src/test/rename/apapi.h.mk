cb98af976363c2029fe6347f46b845cabaf036d9
Ralf S. Engelschall <rse@apache.org>
1998-04-03 06:20:16 +0000

    Move the global symbol renaming stuff from apache-core (where it was located wrong) to apache-1.3/src/test/rename/ (where it is not located a lot better, but a little bit better). After the renaming (if done) these stuff gets removed, of course.
    Brian: We should really create a, say apache-misc, repository for those
           (temporary) stuff.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@80789 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/rename/apapi.h.mk
@@ -0,0 +1,59 @@
+:
+eval 'exec perl -S $0 ${1+"$@"}'
+    if $running_under_some_shell;
+##
+##  apapi.h.mk -- 
+##
+
+#   configuration
+$id       = "../src/ID";
+$renamecf = "./rename.cf";
+$header   = "./apapi.h";
+
+sub func_prototype {
+    my ($func) = @_;
+
+    $rc = join('', `lid -f $id --key=token --result=grep $func`);
+    if (($r, $f) = ($rc =~ m|\.c:\d+:\s*API_EXPORT\((.+?)\)(.+?\))|s)) {
+        $pt = "$r$f";
+        $pt =~ s|\n.+?:\d+:||sg;
+        return "extern $1$2;";
+    }
+    return '';
+}
+
+open(CFG, "<$renamecf") || die;
+open(HDR, ">$header") || die;
+print HDR <<'EOT';
+/*
+**  apapi.h -- Apache API prototypes
+*/
+
+#ifndef APAPI_H
+#define APAPI_H
+
+EOT
+while ($l = <CFG>) {
+    $l =~ s|\n$||;
+    next if ($l =~ m|^\s*#.*| or $l =~ m|^\s*$|);
+    if (($s1, $old, $s2, $new, $s3) = ($l =~ m|^(\s*)(\S+)(\s+)(\S+)(\s*)$|)) {
+        if ($new =~ m|^API_|) {
+            $new =~ s|^API_|ap_|;
+            $pt = &func_prototype($new);
+			if ($pt eq '') {
+				print STDERR "Sorry, prototype for `$new' cannot be found\n";
+				print HDR "/* prototype for `$new' cannot be found */\n";
+			}
+			else {
+                print HDR "$pt\n";
+			}
+        }
+    }
+}
+print HDR <<'EOT';
+
+#endif /* APAPI_H */
+EOT
+close(CFG);
+close(HDR);
+
