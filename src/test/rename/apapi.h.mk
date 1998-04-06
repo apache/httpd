af34eb6db79cd7a584f52725fa736f331b7729b2
Ralf S. Engelschall <rse@apache.org>
1998-04-06 10:49:49 +0000

    Ok, here it comes the last part: The completely generated apapi.h header file containing prototypes for all public API functions (ap_xxx)
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@80821 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/rename/apapi.h.mk
+++ src/test/rename/apapi.h.mk
@@ -14,11 +14,31 @@ sub func_prototype {
     my ($func) = @_;
 
     $rc = join('', `lid -f $id --key=token --result=grep $func`);
-    if (($r, $f) = ($rc =~ m|\.c:\d+:\s*API_EXPORT\((.+?)\)(.+?\))|s)) {
-        $pt = "$r$f";
-        $pt =~ s|\n.+?:\d+:||sg;
-        return "extern $1$2;";
-    }
+    if (   $rc =~ m|^(.+?\.h):\d+:\s*API_EXPORT(?:_NONSTD)?\(.+?\)\s*$func\s*\(|m
+        or $rc =~ m|^(.+?\.h):\d+:\s*(?:extern\s*)?API_VAR_EXPORT\s*.+?$func.+?;|m) {
+		$file = $1;
+		open(FP, "<$file") || die;
+		$contents = join('', <FP>);
+		close(FP);
+        if (($r, $f) = ($contents =~ m|\n\s*API_EXPORT(?:_NONSTD)?\(([^)]+)\)\s+($func\s*\(.+?\);)|s)) {
+            $r =~ s|\s*\n\s*| |sg;
+            $r =~ s|\s+| |sg;
+			$r =~ s|extern\s+||g;
+            $f =~ s|\s*\n\s*| |sg;
+            $f =~ s|\s+| |sg;
+            $pt = sprintf("%-20s %s", $r, $f);
+            return $pt;
+        }
+        if (($r, $f) = ($contents =~ m|\n\s*(?:extern\s*)?API_VAR_EXPORT\s*([^;]*?)($func[^;]*;)|s)) {
+            $r =~ s|\s*\n\s*| |sg;
+            $r =~ s|\s+| |sg;
+			$r =~ s|extern\s+||g;
+            $f =~ s|\s*\n\s*| |sg;
+            $f =~ s|\s+| |sg;
+            $pt = sprintf("%-20s %s", $r, $f);
+            return $pt;
+        }
+	}
     return '';
 }
 
@@ -26,9 +46,10 @@ open(CFG, "<$renamecf") || die;
 open(HDR, ">$header") || die;
 print HDR <<'EOT';
 /*
-**  apapi.h -- Apache API prototypes
+**  apapi.h -- Apache 1.3 API prototypes
 **
-**  THIS IS NOT COMPLETE BECAUSE IT NEEDS MANUAL FIXUP!
+**  THIS IS A COMPLETE LIST OF PROTOTYPES
+**  FOR ALL PUBLIC API FUNCTIONS
 */
 
 #ifndef APAPI_H
@@ -41,16 +62,13 @@ while ($l = <CFG>) {
     if (($s1, $old, $s2, $new, $s3) = ($l =~ m|^(\s*)(\S+)(\s+)(\S+)(\s*)$|)) {
         if ($new =~ m|^API_|) {
             $new =~ s|^API_|ap_|;
+			$pt = &func_prototype($new);
             if ($pt eq '') {
                 print STDERR "Sorry, prototype for `$new' cannot be determined automatically\n";
                 print HDR "/* prototype for `$new' cannot be automatically determined */\n";
             }
-            elsif ($pt eq 'extern ;') {
-                print STDERR "Sorry, prototype for `$new' cannot be determined automatically\n";
-                print HDR "/* prototype for `$new' cannot be automatically determined */\n";
-            }
             else {
-                print HDR "$pt\n";
+                print HDR "extern $pt\n";
             }
         }
     }
