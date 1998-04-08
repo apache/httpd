3a6c1a30e3474d29c763049a313f8aa8634ff0cc
Ralf S. Engelschall <rse@apache.org>
1998-04-08 06:40:24 +0000

    A little fix for the error messages from RewriteBase directive.
    Submitted by: Todd Eigenschink <eigenstr@mixi.net>
    Reviewed by: Ralf S. Engelschall
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@80836 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/standard/mod_rewrite.c
+++ src/modules/standard/mod_rewrite.c
@@ -520,10 +520,10 @@ static const char *cmd_rewritebase(cmd_parms *cmd, rewrite_perdir_conf *dconf,
 {
     if (cmd->path == NULL || dconf == NULL)
         return "RewriteBase: only valid in per-directory config files";
-    if (a1[0] != '/')
-        return "RewriteBase: argument is not a valid URL";
     if (a1[0] == '\0')
         return "RewriteBase: empty URL not allowed";
+    if (a1[0] != '/')
+        return "RewriteBase: argument is not a valid URL";
 
     dconf->baseurl = a1;
 
