1708caf5c9a6a1bcaa80c27884723dadd9dbcd7f
Martin Kraemer <martin@apache.org>
1998-06-09 21:02:13 +0000

    Fix typos from zb era
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@81524 13f79535-47bb-0310-9956-ffa450edef68

--- src/support/ab.c
+++ src/support/ab.c
@@ -231,7 +231,7 @@ static void output_results(void)
     printf("Document Path:          %s\n", path);
     printf("Document Length:        %d bytes\n", doclen);
     printf("\n");
-    printf("Concurency Level:       %d\n", concurrency);
+    printf("Concurrency Level:      %d\n", concurrency);
     printf("Time taken for tests:   %d.%03d seconds\n",
            timetaken / 1000, timetaken % 1000);
     printf("Complete requests:      %d\n", done);
@@ -241,12 +241,12 @@ static void output_results(void)
                err_conn, err_length, err_except);
     if (keepalive)
         printf("Keep-Alive requests:    %d\n", doneka);
-    printf("Total transfered:       %d bytes\n", totalread);
-    printf("HTML transfered:        %d bytes\n", totalbread);
+    printf("Total transferred:      %d bytes\n", totalread);
+    printf("HTML transferred:       %d bytes\n", totalbread);
 
     /* avoid divide by zero */
     if (timetaken) {
-        printf("Requests per seconds:   %.2f\n", 1000 * (float) (done) / timetaken);
+        printf("Requests per second:    %.2f\n", 1000 * (float) (done) / timetaken);
         printf("Transfer rate:          %.2f kb/s\n",
                (float) (totalread) / timetaken);
     }
