d8422b771014acc7a6da25b2b64add55ede044d0
Ralf S. Engelschall <rse@apache.org>
1998-07-06 11:54:30 +0000

    Add Dynamic Shared Object (DSO) support for SCO5 (OpenServer 5.0.x).
    Submitted by: Ronald Record <rr@sco.com>
    Reviewed by: Ralf S. Engelschall
    PR: 2533
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@81659 13f79535-47bb-0310-9956-ffa450edef68

--- src/os/unix/os.h
+++ src/os/unix/os.h
@@ -94,7 +94,8 @@ extern int ap_os_is_path_absolute(const char *f);
 #if defined(LINUX) || defined(__FreeBSD__) ||\
     defined(__OpenBSD__) || defined(__NetBSD__) || \
     defined(SOLARIS2) || defined(__bsdi__) || \
-    defined(IRIX) || defined(SVR4) || defined(OSF1)
+    defined(IRIX) || defined(SVR4) || defined(OSF1) ||\
+    defined(SCO5)
 #define HAVE_DLFCN_H 1
 #endif
 
