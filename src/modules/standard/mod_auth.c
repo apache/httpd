29f67794d617f6e7e8315e2e84e85c68b365b05a
Marc Slemko <marc@apache.org>
1998-07-10 06:33:24 +0000

    Add a warning if a valid user that enters a proper password fails to get access to a directory because they aren't in the list of those allowed access.
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@81698 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/standard/mod_auth.c
+++ src/modules/standard/mod_auth.c
@@ -292,6 +292,12 @@ static int check_user_access(request_rec *r)
     if (!(sec->auth_authoritative))
 	return DECLINED;
 
+    ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_ERR, r->server,
+	"access to %s failed for %s, reason: user %s not allowed access",
+	r->uri,
+	ap_get_remote_host(r->connection, r->per_dir_config, REMOTE_NAME),
+	user);
+	
     ap_note_basic_auth_failure(r);
     return AUTH_REQUIRED;
 }
