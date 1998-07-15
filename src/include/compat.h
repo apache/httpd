fa6a48cc33819d4539997cc48e6fb1fb2cd37888
Marc Slemko <marc@apache.org>
1998-07-15 03:27:03 +0000

    Fix up compat.h and conf.h legacy support.
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@81741 13f79535-47bb-0310-9956-ffa450edef68

--- src/include/compat.h
+++ src/include/compat.h
@@ -2,9 +2,8 @@
  *  compat.h -- backward compatibility header for ap_compat.h
  */
 
-#if __GNUC__
-#warning "This header becomes obsolete, use ap_compat.h instead"
+#ifdef __GNUC__
+#warning "This header is obsolete, use ap_compat.h instead"
 #endif
 
 #include "ap_compat.h"
-
