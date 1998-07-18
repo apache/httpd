d9655ee042da8cd6705aeedc38d0de804e34a9ee
Marc Slemko <marc@apache.org>
1998-07-18 23:35:43 +0000

    And bump to 1.3.2-dev after roll.
    PR:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@81773 13f79535-47bb-0310-9956-ffa450edef68

--- src/os/win32/registry.c
+++ src/os/win32/registry.c
@@ -28,7 +28,7 @@
 
 #define VENDOR   "Apache Group"
 #define SOFTWARE "Apache"
-#define VERSION  "1.3.1"
+#define VERSION  "1.3.2 dev"
 
 #define REGKEY "SOFTWARE\\" VENDOR "\\" SOFTWARE "\\" VERSION
 
