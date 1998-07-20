a8cfe1c9789e5ca732dac600cf047e51df72cfa1
dgaudet <dgaudet@unknown>
1998-07-20 16:33:57 +0000

    use sub_req_lookup_file for fsize and flastmod.
    PR:		2355
    Submitted by:	Manoj Kasichainula <manojk@raleigh.ibm.com>
    Reviewed by:	Brian, Dean
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@81788 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/standard/mod_include.c
+++ src/modules/standard/mod_include.c
@@ -1004,22 +1004,39 @@ static int find_file(request_rec *r, const char *directive, const char *tag,
                      char *tag_val, struct stat *finfo, const char *error)
 {
     char *to_send;
+    request_rec *rr;
+    int ret=0;
 
     if (!strcmp(tag, "file")) {
         ap_getparents(tag_val);    /* get rid of any nasties */
-        to_send = ap_make_full_path(r->pool, "./", tag_val);
-        if (stat(to_send, finfo) == -1) {
-            ap_log_error(APLOG_MARK, APLOG_ERR, r->server,
-                        "unable to get information about \"%s\" "
-                        "in parsed file %s",
-                        to_send, r->filename);
-            ap_rputs(error, r);
-            return -1;
+        
+        rr = ap_sub_req_lookup_file(tag_val, r);
+
+        if (rr->status == HTTP_OK && rr->finfo.st_mode != 0) {
+            to_send = rr->filename;
+            if ((ret = stat(to_send, finfo)) == -1) {
+                ap_log_error(APLOG_MARK, APLOG_ERR, r->server,
+                            "unable to get information about \"%s\" "
+                            "in parsed file %s",
+                            to_send, r->filename);
+                ap_rputs(error, r);
+            }
         }
-        return 0;
+        else {
+            ret = -1;
+            ap_log_error(APLOG_MARK, APLOG_ERR, r->server,
+                        "unable to lookup information about \"%s\" "
+                        "in parsed file %s",
+                        tag_val, r->filename);
+            ap_rputs(error, r);
+        }
+        
+        ap_destroy_sub_req(rr);
+        
+        return ret;
     }
     else if (!strcmp(tag, "virtual")) {
-        request_rec *rr = ap_sub_req_lookup_uri(tag_val, r);
+        rr = ap_sub_req_lookup_uri(tag_val, r);
 
         if (rr->status == HTTP_OK && rr->finfo.st_mode != 0) {
             memcpy((char *) finfo, (const char *) &rr->finfo,
