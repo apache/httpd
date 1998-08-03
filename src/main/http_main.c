c2266e6b8974382a1835fdf582df2a18f27f1832
Ralf S. Engelschall <rse@apache.org>
1998-08-03 07:34:03 +0000

    Fix suEXEC start message: Has to be of `notice' level to really get printed together with the standard startup message because the `notice' level is handled special inside ap_log_error() for startup messages.
    PR: 2761, 2761, 2765
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@81844 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/http_main.c
+++ src/main/http_main.c
@@ -4139,12 +4139,12 @@ static void standalone_main(int argc, char **argv)
 	ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_NOTICE, server_conf,
 		    "%s configured -- resuming normal operations",
 		    ap_get_server_version());
-	ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_INFO, server_conf,
-		    "Server built: %s", ap_get_server_built());
 	if (ap_suexec_enabled) {
-	    ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_INFO, server_conf,
+	    ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_NOTICE, server_conf,
 		         "suEXEC mechanism enabled (wrapper: %s)", SUEXEC_BIN);
 	}
+	ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_INFO, server_conf,
+		    "Server built: %s", ap_get_server_built());
 	restart_pending = shutdown_pending = 0;
 
 	while (!restart_pending && !shutdown_pending) {
