e2f3ffa769cb84ac82a1694d2c562ffaeff5e1a6
Martin Kraemer <martin@apache.org>
1998-08-12 10:23:46 +0000

    Add missing space
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@81921 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/standard/mod_status.c
+++ src/modules/standard/mod_status.c
@@ -692,7 +692,7 @@ static int status_handler(request_rec *r)
 
     } else {
 
-    ap_rputs("<hr>To obtain a full report with current status information", r);
+    ap_rputs("<hr>To obtain a full report with current status information ", r);
     ap_rputs("you need to use the <code>ExtendedStatus On</code> directive. \n", r);
 
     }
