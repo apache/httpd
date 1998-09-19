cc7abc3ab564cfefbfad1b2ab54074ac9f9edb94
Ralf S. Engelschall <rse@apache.org>
1998-09-19 12:12:36 +0000

    Add support for Berkeley-DB/2.x (in addition to Berkeley-DB/1.x) to mod_auth_db to both be friendly to users who wants to use this version and to avoid problems under platforms where only version 2.x is present.
    Submitted by: Dan Jacobowitz <drow@false.org>
    Enhanced by: Ralf S. Engelschall
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@82093 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/standard/mod_auth_db.c
+++ src/modules/standard/mod_auth_db.c
@@ -97,6 +97,10 @@
 #include "http_protocol.h"
 #include <db.h>
 
+#if defined(DB_VERSION_MAJOR) && (DB_VERSION_MAJOR == 2)
+#define DB2
+#endif
+
 typedef struct {
 
     char *auth_dbpwfile;
@@ -154,19 +158,31 @@ static char *get_db_pw(request_rec *r, char *user, const char *auth_dbpwfile)
     q.data = user;
     q.size = strlen(q.data);
 
+#ifdef DB2
+    if (db_open(auth_dbpwfile, DB_HASH, O_RDONLY, 0664, NULL, NULL,  &f) != 0) {
+#else
     if (!(f = dbopen(auth_dbpwfile, O_RDONLY, 0664, DB_HASH, NULL))) {
+#endif
 	ap_log_rerror(APLOG_MARK, APLOG_ERR, r,
 		    "could not open db auth file: %s", auth_dbpwfile);
 	return NULL;
     }
 
+#ifdef DB2
+    if (!((f->get) (f, NULL, &q, &d, 0))) {
+#else
     if (!((f->get) (f, &q, &d, 0))) {
+#endif
 	pw = ap_palloc(r->pool, d.size + 1);
 	strncpy(pw, d.data, d.size);
 	pw[d.size] = '\0';	/* Terminate the string */
     }
 
+#ifdef DB2
+    (f->close) (f, 0);
+#else
     (f->close) (f);
+#endif
     return pw;
 }
 
