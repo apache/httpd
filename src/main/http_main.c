7f10d5a73976fb9b4dd51b027c46781485d89944
pcs <pcs@unknown>
1998-09-21 10:16:11 +0000

    Remove PID file on windows as well as Unix
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@82113 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/http_main.c
+++ src/main/http_main.c
@@ -5581,6 +5581,17 @@ die_now:
     }
     service_set_status(SERVICE_STOPPED);
 
+    /* cleanup pid file on normal shutdown */
+    {
+	const char *pidfile = NULL;
+	pidfile = ap_server_root_relative (pparent, ap_pid_fname);
+	if ( pidfile != NULL && unlink(pidfile) == 0)
+	    ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_INFO,
+			 server_conf,
+			 "httpd: removed PID file %s (pid=%ld)",
+			 pidfile, (long)getpid());
+    }
+
     if (pparent) {
 	ap_destroy_pool(pparent);
     }
