6abef4e96fb6501d49d7ff8cd982b9efa0ac548d
Roy T. Fielding <fielding@apache.org>
1998-09-25 22:49:38 +0000

    Ooops. Friday it is.  Undo last unintended commit.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@82163 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/http_config.c
+++ src/main/http_config.c
@@ -1229,10 +1229,7 @@ int ap_parse_htaccess(void **result, request_rec *r, int override,
 	filename = ap_make_full_path(r->pool, d, w);
 	f = ap_pcfg_openfile(r->pool, filename);
     }
-    if (!access_name[0]) {
-	dc = NULL;
-    }
-    else if (f) {
+    if (f) {
 	dc = ap_create_per_dir_config(r->pool);
 
 	parms.config_file = f;
