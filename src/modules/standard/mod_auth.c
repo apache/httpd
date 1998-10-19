d4af9d18e4d732a0ab001218b9b99ddd255c185a
Marc Slemko <marc@apache.org>
1998-10-19 05:43:34 +0000

    Log an error if we encounter a malformed "require" directive in mod_auth if we know that we know that no other module can deal with it.
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@82277 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/standard/mod_auth.c
+++ src/modules/standard/mod_auth.c
@@ -283,6 +283,16 @@ static int check_user_access(request_rec *r)
 		if (ap_table_get(grpstatus, w))
 		    return OK;
 	    }
+	} else if (sec->auth_authoritative) {
+	    /* if we aren't authoritative, any require directive could be
+	     * valid even if we don't grok it.  However, if we are 
+	     * authoritative, we can warn the user they did something wrong.
+	     * That something could be a missing "AuthAuthoritative off", but
+	     * more likely is a typo in the require directive.
+	     */
+	    ap_log_rerror(APLOG_MARK, APLOG_NOERRNO|APLOG_ERR, r,
+		"access to %s failed, reason: unknown require directive:"
+		"\"%s\"", r->uri, reqs[x].requirement);
 	}
     }
 
