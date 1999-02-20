7968742520ad484d0767475e5e6886f740929ebc
Jim Jagielski <jim@apache.org>
1999-02-20 17:57:53 +0000

    Add Brian Havard's os/2 patch for removing ap_assert() which caused ap_os_canonical_filename() under os/2 to die with a bad filename
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@82830 13f79535-47bb-0310-9956-ffa450edef68

--- src/os/os2/util_os2.c
+++ src/os/os2/util_os2.c
@@ -1,4 +1,5 @@
 #define INCL_DOSFILEMGR
+#define INCL_DOSERRORS
 #include <os2.h>
 #include "httpd.h"
 #include "http_log.h"
@@ -19,7 +20,16 @@ API_EXPORT(char *)ap_os_canonical_filename(pool *pPool, const char *szFile)
         buf[--len] = 0;
       
     rc = DosQueryPathInfo(buf, FIL_QUERYFULLNAME, buf2, HUGE_STRING_LEN);
-    ap_assert(rc == 0);
+
+    if (rc) {
+        if ( rc != ERROR_INVALID_NAME ) {
+            ap_log_error(APLOG_MARK, APLOG_ERR|APLOG_NOERRNO, NULL, "OS/2 error %d for file %s", rc, szFile);
+            return ap_pstrdup(pPool, "");
+        } else {
+            return ap_pstrdup(pPool, szFile);
+        }
+    }
+
     strlwr(buf2);
     
 /* Switch backslashes to forward */
