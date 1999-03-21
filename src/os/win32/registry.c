6476ead92f953ec50b501dbe8184dd0aedb85fbf
Lars Eilebrecht <lars@apache.org>
1999-03-21 05:07:25 +0000

    make source ready for the next round...
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@82967 13f79535-47bb-0310-9956-ffa450edef68

--- src/os/win32/registry.c
+++ src/os/win32/registry.c
@@ -28,7 +28,7 @@
 
 #define VENDOR   "Apache Group"
 #define SOFTWARE "Apache"
-#define VERSION  "1.3.5"
+#define VERSION  "1.3.6 dev"
 
 #define REGKEY "SOFTWARE\\" VENDOR "\\" SOFTWARE "\\" VERSION
 
