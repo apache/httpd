8831e532f9a3d89333025ed70a12da144bf214d0
Lars Eilebrecht <lars@apache.org>
1999-03-21 04:33:44 +0000

    roll 1.3.5
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@82965 13f79535-47bb-0310-9956-ffa450edef68

--- src/os/win32/registry.c
+++ src/os/win32/registry.c
@@ -28,7 +28,7 @@
 
 #define VENDOR   "Apache Group"
 #define SOFTWARE "Apache"
-#define VERSION  "1.3.5 dev"
+#define VERSION  "1.3.5"
 
 #define REGKEY "SOFTWARE\\" VENDOR "\\" SOFTWARE "\\" VERSION
 
