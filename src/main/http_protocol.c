d84bc201988020eeb54cb9ec1ff09588a0c63118
Ken Coar <coar@apache.org>
1999-05-26 18:28:06 +0000

    Make sure that redirect processing looks for the "Location" 	field in both headers_out and err_headers_out.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@83247 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/http_protocol.c
+++ src/main/http_protocol.c
@@ -2308,6 +2308,14 @@ void ap_send_error_response(request_rec *r, int recursive_error)
     char *custom_response;
     const char *location = ap_table_get(r->headers_out, "Location");
 
+    /*
+     * It's possible that the Location field might be in r->err_headers_out
+     * instead of r->headers_out; use the latter if possible, else the
+     * former.
+     */
+    if (location == NULL) {
+	location = ap_table_get(r->err_headers_out, "Location");
+    }
     /* We need to special-case the handling of 204 and 304 responses,
      * since they have specific HTTP requirements and do not include a
      * message body.  Note that being assbackwards here is not an option.
@@ -2359,9 +2367,10 @@ void ap_send_error_response(request_rec *r, int recursive_error)
         r->err_headers_out = tmp;
         ap_clear_table(r->err_headers_out);
 
-        if (location && *location
-            && (ap_is_HTTP_REDIRECT(status) || status == HTTP_CREATED))
+        if ((location != NULL) && *location
+            && (ap_is_HTTP_REDIRECT(status) || status == HTTP_CREATED)) {
             ap_table_setn(r->headers_out, "Location", location);
+	}
 
         r->content_language = NULL;
         r->content_languages = NULL;
