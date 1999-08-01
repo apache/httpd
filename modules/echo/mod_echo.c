d205a3fe22ef54dbcca8f67c46c0b4d1d37b6b25
Ben Laurie <ben@apache.org>
1999-08-01 14:12:33 +0000

    Another hook.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@83547 13f79535-47bb-0310-9956-ffa450edef68

--- modules/echo/mod_echo.c
+++ modules/echo/mod_echo.c
@@ -64,7 +64,6 @@ static void register_hooks()
 API_VAR_EXPORT module echo_module = {
     STANDARD20_MODULE_STUFF,
     NULL,			/* post_config */
-    NULL,			/* open_logs */
     NULL, 			/* child_init */
     NULL,			/* create per-directory config structure */
     NULL,			/* merge per-directory config structures */
