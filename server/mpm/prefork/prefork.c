2746674d1129820e570c25e4a172be6200c14afc
Ben Laurie <ben@apache.org>
1999-08-01 14:12:33 +0000

    Another hook.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@83547 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/prefork/prefork.c
+++ server/mpm/prefork/prefork.c
@@ -3085,7 +3085,6 @@ LISTEN_COMMANDS
 module MODULE_VAR_EXPORT mpm_prefork_module = {
     STANDARD20_MODULE_STUFF,
     NULL,			/* post_config */
-    NULL,			/* open_logs */
     NULL, 			/* child_init */
     NULL,			/* create per-directory config structure */
     NULL,			/* merge per-directory config structures */
