71eeffa93b3b580dce9ac06db3f44f2a90b936e5
dgaudet <dgaudet@unknown>
1999-08-06 01:37:58 +0000

    CIDR addresses such as a.b.c.d/24 where d != 0 weren't handled properly... mask off the "host" bits of the address.
    PR:		4770
    Submitted by:	"Paul J. Reder" <rederpj@raleigh.ibm.com>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@83589 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/standard/mod_access.c
+++ src/modules/standard/mod_access.c
@@ -202,7 +202,7 @@ static const char *allow_cmd(cmd_parms *cmd, void *dv, char *from, char *where)
 	    mask = htonl(mask);
 	}
 	a->x.ip.mask = mask;
-
+        a->x.ip.net  = (a->x.ip.net & mask);   /* pjr - This fixes PR 4770 */
     }
     else if (ap_isdigit(*where) && is_ip(where)) {
 	/* legacy syntax for ip addrs: a.b.c. ==> a.b.c.0/24 for example */
