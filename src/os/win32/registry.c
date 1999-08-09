a248368ae4ff8ecbedb804cc173ed077330b683d
Ken Coar <coar@apache.org>
1999-08-09 17:22:10 +0000

    Rolling 1.3.8..
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@83626 13f79535-47bb-0310-9956-ffa450edef68

--- src/os/win32/registry.c
+++ src/os/win32/registry.c
@@ -38,7 +38,7 @@
 
 #define VENDOR   "Apache Group"
 #define SOFTWARE "Apache"
-#define VERSION  "1.3.8 dev"
+#define VERSION  "1.3.8"
 
 #define REGKEY "SOFTWARE\\" VENDOR "\\" SOFTWARE "\\" VERSION
 
