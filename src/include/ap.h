c1061c6cbf256534640e91ce7149a19f20d15459
Martin Kraemer <martin@apache.org>
1999-10-11 23:04:13 +0000

    typo
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@83972 13f79535-47bb-0310-9956-ffa450edef68

--- src/include/ap.h
+++ src/include/ap.h
@@ -180,7 +180,7 @@ API_EXPORT(int) ap_base64decode(char * plain_dst, const char *coded_src);
 API_EXPORT(int) ap_base64decode_binary(unsigned char * plain_dst, const char *coded_src);
 
 /* Password validation, as used in AuthType Basic which is able to cope
- * (based on the prexix) with the SHA1, Apache's internal MD5 and (depending
+ * (based on the prefix) with the SHA1, Apache's internal MD5 and (depending
  * on your platform either plain or crypt(3) passwords.
  */
 API_EXPORT(char *) ap_validate_password(const char *passwd, const char *hash);
