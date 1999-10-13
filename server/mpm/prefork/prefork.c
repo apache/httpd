72fd2854576a72265f4b5290c54da1195b071767
Manoj Kasichainula <manoj@apache.org>
1999-10-13 18:16:50 +0000

    My connection status patch broke all the MPMs that didn't have connection status support yet. For now, throw in stub functions so that the server compiles.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@83985 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/prefork/prefork.c
+++ server/mpm/prefork/prefork.c
@@ -2968,6 +2968,18 @@ API_EXPORT(void) ap_thread_mutex_destroy(ap_thread_mutex *mtx)
     free(mtx);
 }
 
+/* Stub functions until this MPM supports the connection status API */
+
+API_EXPORT(void) ap_update_connection_status(long conn_id, const char *key, \
+                                             const char *value)
+{
+    /* NOP */
+}
+
+API_EXPORT(void) ap_reset_connection_status(long conn_id)
+{
+    /* NOP */
+}
 
 static const command_rec prefork_cmds[] = {
 UNIX_DAEMON_COMMANDS
