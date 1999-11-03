f8f25e2d3be9e58044b9e7b892e9ea2b8c544a88
Brian Havard <bjh@apache.org>
1999-11-03 12:47:23 +0000

    OS/2: Don't return APR_EOF from ap_read(). EOF is indicated by a APR_SUCCESS status with nbytes set to 0.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@84097 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http/http_protocol.c
+++ modules/http/http_protocol.c
@@ -2111,7 +2111,7 @@ API_EXPORT(long) ap_send_fb_length(BUFF *fb, request_rec *r, long length)
     while (!ap_is_aborted(r->connection)) {
         rv = ap_bread(fb, buf, sizeof(buf), &n);
         if (n == 0) {
-            if (rv == APR_SUCCESS || rv == APR_EOF) {    /* eof */
+            if (rv == APR_SUCCESS) {    /* eof */
                 (void) ap_rflush(r);
                 break;
             }
