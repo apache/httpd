ebbbacc26d683a9b7a7320b54724cc03249f9758
Martin Kraemer <martin@apache.org>
1999-11-24 17:12:25 +0000

    Adapt the more generic interface introduced by IBM's TPF port
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@84152 13f79535-47bb-0310-9956-ffa450edef68

--- src/os/bs2000/ebcdic.h
+++ src/os/bs2000/ebcdic.h
@@ -1,8 +1,12 @@
+#ifndef AP_EBCDIC_H
+#define AP_EBCDIC_H  "$Id: ebcdic.h,v 1.5 1999/11/24 17:12:25 martin Exp $"
+
 #include <sys/types.h>
 
 extern const unsigned char os_toascii[256];
 extern const unsigned char os_toebcdic[256];
-void ebcdic2ascii(unsigned char *dest, const unsigned char *srce, size_t count);
-void ebcdic2ascii_strictly(unsigned char *dest, const unsigned char *srce, size_t count);
-void ascii2ebcdic(unsigned char *dest, const unsigned char *srce, size_t count);
+void ebcdic2ascii(void *dest, const void *srce, size_t count);
+void ebcdic2ascii_strictly(void *dest, const void *srce, size_t count);
+void ascii2ebcdic(void *dest, const void *srce, size_t count);
 
+#endif /*AP_EBCDIC_H*/
