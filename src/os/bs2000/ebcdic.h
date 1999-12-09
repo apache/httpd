b6898ea62cdceba9a23e7814088bc4effc9c9c02
Martin Kraemer <martin@apache.org>
1999-12-09 16:55:54 +0000

    Take advantage of the new clean CRLF implementation. The *ebcdic*_strictly() kludge is now no longer required.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@84259 13f79535-47bb-0310-9956-ffa450edef68

--- src/os/bs2000/ebcdic.h
+++ src/os/bs2000/ebcdic.h
@@ -1,12 +1,16 @@
 #ifndef AP_EBCDIC_H
-#define AP_EBCDIC_H  "$Id: ebcdic.h,v 1.5 1999/11/24 17:12:25 martin Exp $"
+#define AP_EBCDIC_H  "$Id: ebcdic.h,v 1.6 1999/12/09 16:55:54 martin Exp $"
 
 #include <sys/types.h>
 
 extern const unsigned char os_toascii[256];
 extern const unsigned char os_toebcdic[256];
-void ebcdic2ascii(void *dest, const void *srce, size_t count);
-void ebcdic2ascii_strictly(void *dest, const void *srce, size_t count);
-void ascii2ebcdic(void *dest, const void *srce, size_t count);
+void *ebcdic2ascii(void *dest, const void *srce, size_t count);
+void *ascii2ebcdic(void *dest, const void *srce, size_t count);
+
+/* Provide backward compatibility until all EBCDIC platforms
+ * have switched to using ebcdic2ascii() only: 
+ */
+#define ebcdic2ascii_strictly(_to,_from,_len) ebcdic2ascii(_to,_from,_len)
 
 #endif /*AP_EBCDIC_H*/
