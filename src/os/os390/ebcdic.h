23be943d44aa2eb7ea0faecb0d6ca89be31f5853
Martin Kraemer <martin@apache.org>
1999-12-10 12:28:26 +0000

    Under the assumption that the recent CRLF change makes the *_strictly kludge no longer necessary, I removed that part of ebcdic.[ch]. This is untested by me, so please tell me if it breaks anything for you and I'll revert it.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@84273 13f79535-47bb-0310-9956-ffa450edef68

--- src/os/os390/ebcdic.h
+++ src/os/os390/ebcdic.h
@@ -3,6 +3,9 @@
 extern const unsigned char os_toascii[256];
 extern const unsigned char os_toebcdic[256];
 void ebcdic2ascii(void *dest, const void *srce, size_t count);
-void ebcdic2ascii_strictly(unsigned char *dest, const unsigned char *srce, size_t count);
 void ascii2ebcdic(void *dest, const void *srce, size_t count);
 
+/* Provide backward compatibility until all EBCDIC platforms
+ * have switched to using ebcdic2ascii() only: 
+ */
+#define ebcdic2ascii_strictly(_to,_from,_len) ebcdic2ascii(_to,_from,_len)
