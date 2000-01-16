312bc880295618023df307c811e6dceaea7a7028
Sascha Schumann <sascha@apache.org>
2000-01-16 17:18:13 +0000

    Remove dependency on automake's aclocal. We continue to use acinclude.m4, because aclocal.m4 needs to contain libtool's m4 macros.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@84471 13f79535-47bb-0310-9956-ffa450edef68

--- build/build.mk
+++ build/build.mk
@@ -21,7 +21,7 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 ##############################################################################
-# $Id: build.mk,v 1.2 2000/01/12 16:55:58 sascha Exp $ 
+# $Id: build.mk,v 1.3 2000/01/16 17:18:13 sascha Exp $ 
 #
 # Makefile to generate build tools
 #
@@ -33,6 +33,7 @@ all: $(STAMP) generated_lists
 
 generated_lists:
 	@echo config_m4_files = `find . -name config.m4` > $@
+	@n=`which libtool`; echo libtool_prefix = `dirname $$n`/.. >> $@
 
 $(STAMP): build/buildcheck.sh
 	@build/buildcheck.sh && touch $(STAMP)
