57f10e9ea39eac6efa0a9988a05a31079f0e63bb
Ryan Bloom <rbb@apache.org>
2000-05-19 22:39:40 +0000

    Remove a warning about ap_base64encode not being defined anywhere.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@85261 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -117,6 +117,7 @@
 #include "apr_file_io.h"
 #include "apr_time.h"
 #include "apr_getopt.h"
+#include "ap_base64.h"
 #ifdef NOT_ASCII
 #include "apr_xlate.h"
 #endif
@@ -839,14 +840,14 @@ static void test(void)
 static void copyright(void)
 {
     if (!use_html) {
-        printf("This is ApacheBench, Version %s\n", VERSION " <$Revision: 1.15 $> apache-2.0");
+        printf("This is ApacheBench, Version %s\n", VERSION " <$Revision: 1.16 $> apache-2.0");
         printf("Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/\n");
         printf("Copyright (c) 1998-2000 The Apache Software Foundation, http://www.apache.org/\n");
         printf("\n");
     }
     else {
         printf("<p>\n");
-        printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", VERSION, "$Revision: 1.15 $");
+        printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", VERSION, "$Revision: 1.16 $");
         printf(" Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/<br>\n");
         printf(" Copyright (c) 1998-2000 The Apache Software Foundation, http://www.apache.org/<br>\n");
         printf("</p>\n<p>\n");
