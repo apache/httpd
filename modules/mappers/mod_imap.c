ba4d673c47ab2a762f2fa18c83aa20cb5b1b05c2
Ben Laurie <ben@apache.org>
2000-06-20 20:52:44 +0000

    Fix warnings.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@85643 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_imap.c
+++ modules/mappers/mod_imap.c
@@ -288,14 +288,14 @@ static double get_x_coord(const char *args)
 static double get_y_coord(const char *args)
 {
     char *endptr;               /* we want it non-null */
-    char *start_of_y = NULL;
+    const char *start_of_y = NULL;
     double y_coord = -1;        /* -1 is returned on error */
 
     if (args == NULL) {
         return (-1);            /* in case we aren't passed anything */
     }
 
-    start_of_y = strchr(args, ',');     /* the comma */
+    start_of_y = ap_strchr_c(args, ',');     /* the comma */
 
     if (start_of_y) {
 
@@ -411,7 +411,7 @@ static char *imap_url(request_rec *r, const char *base, const char *value)
     }
 
     /* must be a relative URL to be combined with base */
-    if (strchr(base, '/') == NULL && (!strncmp(value, "../", 3)
+    if (ap_strchr_c(base, '/') == NULL && (!strncmp(value, "../", 3)
         || !strcmp(value, ".."))) {
         ap_log_rerror(APLOG_MARK, APLOG_NOERRNO|APLOG_ERR, 0, r,
                     "invalid base directive in map file: %s", r->uri);
