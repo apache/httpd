b9ce8c3ebcfae4e7f5b36389915e4cdc8f3fd01d
William A. Rowe Jr <wrowe@apache.org>
2000-06-28 04:52:39 +0000

    Hook functions aren't translated (and when they are, they are _NONSTD)...   but you don't need to export a function you will pass by ref to a   register hook function.
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@85715 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http/http_core.c
+++ modules/http/http_core.c
@@ -2609,7 +2609,7 @@ AP_INIT_TAKE12("RLimitNPROC", no_set_limit, NULL,
  * Core handlers for various phases of server operation...
  */
 
-API_EXPORT(int) ap_core_translate(request_rec *r)
+int ap_core_translate(request_rec *r)
 {
     void *sconf = r->server->module_config;
     core_server_config *conf = ap_get_module_config(sconf, &core_module);
