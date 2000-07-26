63449e55c491d49fa375c0de1d7ba47e2b4d3882
Ryan Bloom <rbb@apache.org>
2000-07-26 05:25:58 +0000

    Remove empty header file.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@85889 13f79535-47bb-0310-9956-ffa450edef68

--- src/include/conf.h
+++
@@ -1,9 +0,0 @@
-/*
- *  conf.h -- backward compatibility header for ap_config.h
- */
-
-#ifdef __GNUC__
-#warning "This header is obsolete, use ap_config.h instead"
-#endif
-
-#include "ap_config.h"
