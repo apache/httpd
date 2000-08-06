a7dce91e0499274e20a7c359254c75d9d6a41443
Ryan Bloom <rbb@apache.org>
2000-08-06 04:23:39 +0000

    Document util_cfgtree using ScanDoc
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@86006 13f79535-47bb-0310-9956-ffa450edef68

--- include/util_cfgtree.h
+++ include/util_cfgtree.h
@@ -55,22 +55,55 @@
 #ifndef AP_CONFTREE_H
 #define AP_CONFTREE_H
 
-typedef struct ap_directive_t {
+/**
+ * @package Config Tree Package
+ */
+
+typedef struct ap_directive_t ap_directive_t;
+
+/**
+ * Structure used to build the config tree.  The config tree only stores
+ * the directives that will be active in the running server.  Directives
+ * that contain other directions, such as <Directory ...> cause a sub-level
+ * to be created, where the included directives are stored.  The closing
+ * directive (</Directory>) is not stored in the tree.
+ */
+struct ap_directive_t {
+    /** The current directive */
     const char *directive;
+    /** The arguments for the current directive, stored as a space 
+     *  separated list */
     const char *args;
+    /** The next directive node in the tree
+     *  @defvar ap_directive_t *next */
     struct ap_directive_t *next;
+    /** The first child node of this directive 
+     *  @defvar ap_directive_t *first_child */
     struct ap_directive_t *first_child;
+    /** The parent node of this directive 
+     *  @defvar ap_directive_t *parent */
     struct ap_directive_t *parent;
 
-    void *data;		/* directive's module can store add'l data here */
+    /** directive's module can store add'l data here */
+    void *data;
 
     /* ### these may go away in the future, but are needed for now */
+    /** The name of the file this directive was found in */
     const char *filename;
+    /** The line number the directive was on */
     int line_num;
+};
 
-} ap_directive_t;
-
+/**
+ * Add a node to the configuration tree.
+ * @param parent The current parent node.  If the added node is a first_child,
+                 then this is changed to the current node
+ * @param current The current node
+ * @param toadd The node to add to the tree
+ * @param child Is the node to add a child node
+ * @return the added node
+ */
 ap_directive_t *ap_add_node(ap_directive_t **parent, ap_directive_t *current, 
-                            ap_directive_t *todadd, int child);
+                            ap_directive_t *toadd, int child);
 
 #endif
