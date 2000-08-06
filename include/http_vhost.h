9b3af370df958eff4ef0c7dc48e642e1554b5229
Ryan Bloom <rbb@apache.org>
2000-08-06 00:48:14 +0000

    Update http_vhost.h to use ScanDoc
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@86001 13f79535-47bb-0310-9956-ffa450edef68

--- include/http_vhost.h
+++ include/http_vhost.h
@@ -59,29 +59,58 @@
 extern "C" {
 #endif
 
-/* called before any config is read */
+/**
+ * @package Virtual Host package
+ */
+
+/**
+ * called before any config is read
+ * @param p Pool to allocate out of
+ */
 void ap_init_vhost_config(apr_pool_t *p);
 
-/* called after the config has been read */
+/**
+ * called after the config has been read to compile the tables needed to do 
+ * the run-time vhost lookups
+ * @param p The pool to allocate out of
+ * @param main_server The start of the virtual host list
+ */
 void ap_fini_vhost_config(apr_pool_t *p, server_rec *main_server);
 
-/* handle addresses in <VirtualHost> statement */
+/**
+ * handle addresses in <VirtualHost> statement
+ * @param p The pool to allocate out of
+ * @param hostname The hostname in the VirtualHost statement
+ * @param s The list of Virtual Hosts.
+ */
 const char *ap_parse_vhost_addrs(apr_pool_t *p, const char *hostname, server_rec *s);
 
 /* handle NameVirtualHost directive */
 const char *ap_set_name_virtual_host (cmd_parms *cmd, void *dummy,
 				      const char *arg);
 
-/* given an ip address only, give our best guess as to what vhost it is */
+/**
+ * given an ip address only, give our best guess as to what vhost it is 
+ * @param conn The current connection
+ */
 void ap_update_vhost_given_ip(conn_rec *conn);
 
-/* The above is never enough, and this is always called after the headers
- * have been read.  It may change r->server.
+/**
+ * ap_update_vhost_given_ip is never enough, and this is always called after 
+ * the headers have been read.  It may change r->server.
+ * @param r The current request
  */
 void ap_update_vhost_from_headers(request_rec *r);
 
-/* return 1 if the host:port matches any of the aliases of r->server
+/**
+ * Match the host in the header with the hostname of the server for this
+ * request.
+ * @param r The current request
+ * @param host The hostname in the headers
+ * @param port The port from the headers
+ * @return return 1 if the host:port matches any of the aliases of r->server,
  * return 0 otherwise
+ * @deffunc int ap_matches_request_vhost(request_rec *r, const char *host, unsigned port)
  */
 API_EXPORT(int) ap_matches_request_vhost(request_rec *r, const char *host,
     unsigned port);
