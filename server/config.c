a0d4bf5d6f51f038ba07f4f98cb62774bd79d800
Ryan Bloom <rbb@apache.org>
2000-08-14 03:07:58 +0000

    Remove a bunch of warnings from the server when compiled using --with-maintainer-mode
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@86070 13f79535-47bb-0310-9956-ffa450edef68

--- server/config.c
+++ server/config.c
@@ -79,6 +79,7 @@
 #include "apr_file_io.h"
 #include "httpd.h"
 #include "http_config.h"
+#include "http_protocol.h"
 #include "http_core.h"
 #include "http_log.h"		/* for errors in parse_htaccess */
 #include "http_request.h"	/* for default_handler (see invoke_handler) */
