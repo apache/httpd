74f4b57ee34abef8633702f938655beebdb9ae1d
William A. Rowe Jr <wrowe@apache.org>
2000-09-21 13:06:14 +0000

    sdbm from mod_dav 1.0 added to accomodate Win32 and other DBM-less   platforms.  Required by mod_auth_dbm in the core distro for Win32.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@86271 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/lib/sdbm/sdbm_tune.h
@@ -0,0 +1,26 @@
+/*
+ * sdbm - ndbm work-alike hashed database library
+ * tuning and portability constructs [not nearly enough]
+ * author: oz@nexus.yorku.ca
+ */
+
+#define BYTESIZ		8
+
+/*
+ * important tuning parms (hah)
+ */
+
+#define SEEDUPS			/* always detect duplicates */
+#define BADMESS			/* generate a message for worst case:
+				   cannot make room after SPLTMAX splits */
+/*
+ * misc
+ */
+#ifdef DEBUG
+#define debug(x)	printf x
+#else
+#define debug(x)
+#endif
+
+int sdbm_fd_lock(int fd, int readonly);
+int sdbm_fd_unlock(int fd);
