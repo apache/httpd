1599a6257f986414ea16803447d7e0d5d372541c
Bill Stoddard <stoddard@apache.org>
2000-10-09 22:08:49 +0000

    Fix another bug in the send_the_file() read/write loop. A partial  send by apr_send would cause unsent data in the read buffer to  get clobbered. Complete making send_the_file handle partial  writes to the network.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@86485 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http/http_core.c
+++ modules/http/http_core.c
@@ -2548,6 +2548,8 @@ static apr_status_t writev_it_all(apr_socket_t *s, struct iovec *vec, int nvec,
     apr_ssize_t n = len;
     apr_ssize_t i = 0;
 
+    *nbytes = 0;
+
     /* XXX handle checking for non-blocking socket */
     while (bytes_written != len) {
         rv = apr_sendv(s, vec + i, nvec - i, &n);
@@ -2577,44 +2579,78 @@ static apr_status_t writev_it_all(apr_socket_t *s, struct iovec *vec, int nvec,
 
     return APR_SUCCESS;
 }
-/* XXX handle partial writes */
+/*
+ * send_the_file()
+ * Sends the contents of file fd along with header/trailer bytes, if any,
+ * to the network. send_the_file will return only when all the bytes have been
+ * sent (i.e., it handles partial writes) or on a network error condition.
+ */
 static apr_status_t send_the_file(conn_rec *c, apr_file_t *fd, 
                                   apr_hdtr_t *hdtr, apr_off_t offset, 
                                   apr_size_t length, apr_size_t *nbytes) 
 {
     apr_status_t rv = APR_SUCCESS;
-    apr_size_t n = length;
-    apr_int32_t sendlen = 0;
-    apr_int32_t togo;
+    apr_int32_t togo;         /* Remaining number of bytes in the file to send */
+    apr_int32_t sendlen;
+    apr_int32_t bytes_sent;
+    apr_int32_t i;
+    apr_off_t o;              /* Track the file offset for partial writes */
     char buffer[8192];
 
+    *nbytes = 0;
+
+    /* Send the headers 
+     * writev_it_all handles partial writes.
+     * XXX: optimization... if headers are less than MIN_WRITE_SIZE, copy 
+     * them into buffer
+     */
+    sendlen = 0;
     if ( hdtr && hdtr->numheaders > 0 ) {
-        rv = apr_sendv(c->client->bsock, hdtr->headers, hdtr->numheaders, 
-                       &sendlen);
+        for (i = 0; i < hdtr->numheaders; i++) {
+            sendlen += hdtr->headers[i].iov_len;
+        }
+        rv = writev_it_all(c->client->bsock, hdtr->headers, hdtr->numheaders,
+                           sendlen, &bytes_sent);
+        if (rv == APR_SUCCESS)
+            *nbytes += bytes_sent;     /* track total bytes sent */
     }
 
+    /* Seek the file to 'offset' */
     if (offset != 0 && rv == APR_SUCCESS) {
         rv = apr_seek(fd, APR_SET, &offset);
     }
 
-    togo = n;
-    n = sendlen;
-
-    while (rv == APR_SUCCESS && togo > 0) {
+    /* Send the file, making sure to handle partial writes */
+    togo = length;
+    while (rv == APR_SUCCESS && togo) {
         sendlen = togo > sizeof(buffer) ? sizeof(buffer) : togo;
+        o = 0;
         rv = apr_read(fd, buffer, &sendlen);
-
-        if (rv == APR_SUCCESS) {
-            togo -= sendlen;
-            rv = apr_send(c->client->bsock, buffer, &sendlen);
-            n += sendlen;
+        while (rv == APR_SUCCESS && sendlen) {
+            bytes_sent = sendlen;
+            rv = apr_send(c->client->bsock, &buffer[o], &bytes_sent);
+            if (rv == APR_SUCCESS) {
+                sendlen -= bytes_sent; /* sendlen != bytes_sent ==> partial write */
+                o += bytes_sent;       /* o is where we are in the buffer */
+                *nbytes += bytes_sent;
+                togo -= bytes_sent;    /* track how much of the file we've sent */
+            }
         }
     }
 
+    /* Send the trailers 
+     * XXX: optimization... if it will fit, send this on the last send in the 
+     * loop above
+     */
+    sendlen = 0;
     if ( rv == APR_SUCCESS && hdtr && hdtr->numtrailers > 0 ) {
-        rv = apr_sendv(c->client->bsock, hdtr->trailers, hdtr->numtrailers, 
-                       &sendlen);
-        n += sendlen;
+        for (i = 0; i < hdtr->numtrailers; i++) {
+            sendlen += hdtr->trailers[i].iov_len;
+        }
+        rv = writev_it_all(c->client->bsock, hdtr->trailers, hdtr->numtrailers,
+                           sendlen, &bytes_sent);
+        if (rv == APR_SUCCESS)
+            *nbytes += bytes_sent;
     }
 
     return rv;
