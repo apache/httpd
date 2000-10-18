641e079c763987cce19deeec0b946ae07ba9621a
William A. Rowe Jr <wrowe@apache.org>
2000-10-18 17:38:30 +0000

    Fix a broken thing
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@86645 13f79535-47bb-0310-9956-ffa450edef68

--- include/http_request.h
+++ include/http_request.h
@@ -129,8 +129,8 @@ AP_DECLARE(request_rec *) ap_sub_req_method_uri(const char *method,
  * @param bb The brigade to filter
  * @deffuc apr_status_t ap_sub_req_output_filter(ap_filter_t *f, ap_bucket_brigade *bb)
  */
-AP_CORE_DECLARE(apr_status_t) ap_sub_req_output_filter(ap_filter_t *f,
-                                                 ap_bucket_brigade *bb);
+AP_CORE_DECLARE_NONSTD(apr_status_t) ap_sub_req_output_filter(ap_filter_t *f,
+                                                        ap_bucket_brigade *bb);
 
 /**
  * Run the handler for the sub request
