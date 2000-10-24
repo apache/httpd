b76509428b5803e37cc085f171a74d1cde05b545
Jeff Trawick <trawick@apache.org>
2000-10-24 17:21:41 +0000

    Fix the signature of no_set_limit() to avoid a warning when AP_DEBUG is defined. (no_set_limit() is compiled only if we are missing one of the RLIMIT definitions.)
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@86736 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http/http_core.c
+++ modules/http/http_core.c
@@ -2515,7 +2515,7 @@ static const char *set_interpreter_source(cmd_parms *cmd, core_dir_config *d,
 #endif
 
 #if !defined (RLIMIT_CPU) || !(defined (RLIMIT_DATA) || defined (RLIMIT_VMEM) || defined(RLIMIT_AS)) || !defined (RLIMIT_NPROC)
-static const char *no_set_limit(cmd_parms *cmd, core_dir_config *conf,
+static const char *no_set_limit(cmd_parms *cmd, void *conf_,
                                 const char *arg, const char *arg2)
 {
     ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_ERR, 0, cmd->server,
