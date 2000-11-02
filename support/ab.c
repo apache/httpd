93bdf51d342d7b1caa6c8dcd1e0de6432266486e
Jeff Trawick <trawick@apache.org>
2000-11-02 02:12:34 +0000

    Get rid of some warnings on Solaris 8 by using APR's ctype macros: logresolve.c: In function `main': logresolve.c:327: warning: subscript has type `char' ab.c: In function `main': ab.c:1074: warning: subscript has type `char' ab.c:1087: warning: subscript has type `char'
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@86793 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -876,14 +876,14 @@ static void test(void)
 static void copyright(void)
 {
     if (!use_html) {
-        printf("This is ApacheBench, Version %s\n", AB_VERSION " <$Revision: 1.29 $> apache-2.0");
+        printf("This is ApacheBench, Version %s\n", AB_VERSION " <$Revision: 1.30 $> apache-2.0");
         printf("Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/\n");
         printf("Copyright (c) 1998-2000 The Apache Software Foundation, http://www.apache.org/\n");
         printf("\n");
     }
     else {
         printf("<p>\n");
-        printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AB_VERSION, "$Revision: 1.29 $");
+        printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AB_VERSION, "$Revision: 1.30 $");
         printf(" Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/<br>\n");
         printf(" Copyright (c) 1998-2000 The Apache Software Foundation, http://www.apache.org/<br>\n");
         printf("</p>\n<p>\n");
@@ -1071,7 +1071,7 @@ int main(int argc, char *const *argv)
             /* assume username passwd already to be in colon separated form. 
              * Ready to be uu-encoded.
              */
-            while(isspace(*optarg))
+            while(apr_isspace(*optarg))
                 optarg++;
             l=ap_base64encode(tmp, optarg, strlen(optarg));
             tmp[l]='\0';
@@ -1084,7 +1084,7 @@ int main(int argc, char *const *argv)
             /*
              * assume username passwd already to be in colon separated form.
              */
-            while(isspace(*optarg))
+            while(apr_isspace(*optarg))
                 optarg++;
             l=ap_base64encode(tmp, optarg, strlen(optarg));
             tmp[l]='\0';
