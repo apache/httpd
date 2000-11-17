0bb6b7f1d652b02a53817e98bf92f5eb219bb4a0
Jeff Trawick <trawick@apache.org>
2000-11-17 02:33:00 +0000

    Get exports.c symbols to compile even if the declaration is hidden in APR include file.
    Implement all exports.c symbols even if the package or function isn't
    available in the current configuration.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@86989 13f79535-47bb-0310-9956-ffa450edef68

--- build/buildexports.sh
+++ build/buildexports.sh
@@ -7,25 +7,21 @@ echo "/* This is an ugly hack that needs to be here, so that libtool will"
 echo " * link all of the APR functions into server regardless of whether"
 echo " * the base server uses them."
 echo " */"
-echo ""
 
-cd lib/apr/include
-for file in *.h
-do
-    echo "#include \"$file\""
-done
-cd ../../../
-echo ""
 echo ""
 
 while read LINE
 do
     if [ "x`echo $LINE | egrep  '^[:space:]*apr_'`" != "x" ]; then
         newline=`echo "$LINE" |\
+            sed -e 's%^\(.*\)%extern const void *\1\\(void\);%'`
+        echo $newline
+        newline=`echo "$LINE" |\
             sed -e 's%^\(.*\)%const void *ap_hack_\1 = \(const void *\)\1\;%'`
         echo $newline
     fi
 done
 
+echo ""
 echo "void *ap_ugly_hack;"
 exit 0
