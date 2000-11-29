42e9ec0e36c47637ea491c66c29abdcc781b7f19
Jeff Trawick <trawick@apache.org>
2000-11-29 13:02:11 +0000

    Get ab to compile cleanly after the apr_initopt() change.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@87127 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -893,14 +893,14 @@ static void test(void)
 static void copyright(void)
 {
     if (!use_html) {
-        printf("This is ApacheBench, Version %s\n", AB_VERSION " <$Revision: 1.37 $> apache-2.0");
+        printf("This is ApacheBench, Version %s\n", AB_VERSION " <$Revision: 1.38 $> apache-2.0");
         printf("Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/\n");
         printf("Copyright (c) 1998-2000 The Apache Software Foundation, http://www.apache.org/\n");
         printf("\n");
     }
     else {
         printf("<p>\n");
-        printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AB_VERSION, "$Revision: 1.37 $");
+        printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AB_VERSION, "$Revision: 1.38 $");
         printf(" Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/<br>\n");
         printf(" Copyright (c) 1998-2000 The Apache Software Foundation, http://www.apache.org/<br>\n");
         printf("</p>\n<p>\n");
@@ -908,7 +908,7 @@ static void copyright(void)
 }
 
 /* display usage information */
-static void usage(char *progname)
+static void usage(const char *progname)
 {
     fprintf(stderr, "Usage: %s [options] [http://]hostname[:port]/path\n", progname);
     fprintf(stderr, "Options are:\n");
@@ -1027,7 +1027,7 @@ static int open_postfile(const char *pfile)
 /* ------------------------------------------------------- */
 
 /* sort out command-line args and call test */
-int main(int argc, char *const *argv)
+int main(int argc, const char * const argv[])
 {
     int r, l;
     char tmp[1024];
