88525818f793b591222241e4d735442047a5747d
William A. Rowe Jr <wrowe@apache.org>
2000-12-05 21:02:20 +0000

    Fix some leftover dirtyness from the conversion.  All is building   over in Win32 land.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@87223 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -121,7 +121,7 @@
 #include "apr_getopt.h"
 #include "apr_general.h"
 #include "apr_lib.h"
-#include "ap_config_auto.h"
+#include "ap_config.h"
 
 #if APR_HAVE_STDIO_H
 #include <stdio.h>              /* for EOF */
@@ -895,14 +895,14 @@ static void test(void)
 static void copyright(void)
 {
     if (!use_html) {
-        printf("This is ApacheBench, Version %s\n", AB_VERSION " <$Revision: 1.41 $> apache-2.0");
+        printf("This is ApacheBench, Version %s\n", AB_VERSION " <$Revision: 1.42 $> apache-2.0");
         printf("Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/\n");
         printf("Copyright (c) 1998-2000 The Apache Software Foundation, http://www.apache.org/\n");
         printf("\n");
     }
     else {
         printf("<p>\n");
-        printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AB_VERSION, "$Revision: 1.41 $");
+        printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AB_VERSION, "$Revision: 1.42 $");
         printf(" Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/<br>\n");
         printf(" Copyright (c) 1998-2000 The Apache Software Foundation, http://www.apache.org/<br>\n");
         printf("</p>\n<p>\n");
