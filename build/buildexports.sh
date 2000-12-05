552eee5093681cda2beac285546d1b57993f5a65
Brian Havard <bjh@apache.org>
2000-12-05 14:36:31 +0000

    Speed up the generation of exports.c by using an awk script to process apr.exports instead of shell script.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@87208 13f79535-47bb-0310-9956-ffa450edef68

--- build/buildexports.sh
+++ build/buildexports.sh
@@ -17,27 +17,7 @@ done
 cd ../../../
 echo ""
 
-while read LINE
-do
-    if [ "x`echo $LINE | egrep  '^[:space:]*APR_'`" != "x" ]; then
-        ifline=`echo "$LINE" |\
-            sed -e 's%^\(.*\)%\#if \1%'`
-        echo $ifline
-    fi
-    if [ "x`echo $LINE | egrep  '^[:space:]*apr_'`" != "x" ]; then
-#        newline=`echo "$LINE" |\
-#            sed -e 's%^\(.*\)%extern const void *\1\\(void\);%'`
-#        echo $newline
-        newline=`echo "$LINE" |\
-            sed -e 's%^\(.*\)%const void *ap_hack_\1 = \(const void *\)\1\;%'`
-        echo $newline
-    fi
-    if [ "x`echo $LINE | egrep  '^[:space:]*\/APR_'`" != "x" ]; then
-        endline=`echo "$LINE" |\
-            sed -e 's%^\/\(.*\)%\#endif \/\*\1\*\/%'`
-        echo "$endline"
-    fi
-done
+awk -f build/buildexports.awk
 
 echo ""
 echo "void *ap_ugly_hack;"
