24b0693a21b5da667ff9926affee5d740cd8ef8e
Jeff Trawick <trawick@apache.org>
2000-12-16 11:45:13 +0000

    Display whether or not APR (and thus Apache) supports IPv6 in the httpd -V output.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@87368 13f79535-47bb-0310-9956-ffa450edef68

--- server/main.c
+++ server/main.c
@@ -120,6 +120,9 @@ static void show_compile_settings(void)
 #ifdef NO_LINGCLOSE
     printf(" -D NO_LINGCLOSE\n");
 #endif
+#if APR_HAVE_IPV6
+    printf(" -D APR_HAVE_IPV6\n");
+#endif
 #if APR_USE_FLOCK_SERIALIZE
     printf(" -D APR_USE_FLOCK_SERIALIZE\n");
 #endif
