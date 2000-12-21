4bf4d9dd56a559f27f2f861fdaec3eaac3a853ad
William A. Rowe Jr <wrowe@apache.org>
2000-12-21 20:29:26 +0000

    This program should run entirely without ap_config support from   Apache, given that it is apr'ized
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@87498 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -121,7 +121,6 @@
 #include "apr_getopt.h"
 #include "apr_general.h"
 #include "apr_lib.h"
-#include "ap_config.h"
 
 #if APR_HAVE_STDIO_H
 #include <stdio.h>              /* for EOF */
@@ -894,14 +893,14 @@ static void test(void)
 static void copyright(void)
 {
     if (!use_html) {
-        printf("This is ApacheBench, Version %s\n", AB_VERSION " <$Revision: 1.43 $> apache-2.0");
+        printf("This is ApacheBench, Version %s\n", AB_VERSION " <$Revision: 1.44 $> apache-2.0");
         printf("Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/\n");
         printf("Copyright (c) 1998-2000 The Apache Software Foundation, http://www.apache.org/\n");
         printf("\n");
     }
     else {
         printf("<p>\n");
-        printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AB_VERSION, "$Revision: 1.43 $");
+        printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AB_VERSION, "$Revision: 1.44 $");
         printf(" Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/<br>\n");
         printf(" Copyright (c) 1998-2000 The Apache Software Foundation, http://www.apache.org/<br>\n");
         printf("</p>\n<p>\n");
