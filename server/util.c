f9a1287c612ae0ef538428c5c9da7de4a21c5104
Ryan Bloom <rbb@apache.org>
2001-02-11 00:51:38 +0000

    Fix a potential segfault on all platforms.  David Reid fixed this for BEOS, but this could actually happen on any platform, so we should be safe and protect ourselves. Submitted by:	Cliff Woolley <cliffwoolley@yahoo.com> Reviewed by:	Ryan Bloom
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@88080 13f79535-47bb-0310-9956-ffa450edef68

--- server/util.c
+++ server/util.c
@@ -1788,11 +1788,7 @@ char *ap_get_local_host(apr_pool_t *a)
         if ((!(p = gethostbyname(str))) 
             || (!(server_hostname = find_fqdn(a, p)))) {
             /* Recovery - return the default servername by IP: */
-#ifndef BEOS
-            if (p->h_addr_list[0]) {
-#else
-            if (p) {
-#endif
+            if (p && p->h_addr_list[0]) {
                 apr_snprintf(str, sizeof(str), "%pA", p->h_addr_list[0]);
 	        server_hostname = apr_pstrdup(a, str);
                 /* We will drop through to report the IP-named server */
