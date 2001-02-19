1d73deb9f2311a08998048650fa418e3d2b2c890
(no author) <(no author)@unknown>
2001-02-19 13:30:22 +0000

    This commit was manufactured by cvs2svn to create tag 'APACHE_1_3_18'.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/tags/1.3@88235 13f79535-47bb-0310-9956-ffa450edef68

---
+++ APACHE_1_3_18/src/os/os390/ebcdic.h
@@ -0,0 +1,6 @@
+#include <sys/types.h>
+
+extern const unsigned char os_toascii[256];
+extern const unsigned char os_toebcdic[256];
+void ebcdic2ascii(void *dest, const void *srce, size_t count);
+void ascii2ebcdic(void *dest, const void *srce, size_t count);
