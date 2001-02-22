71e040e8fba16fd46478b0ba0bc2c405d5b6c533
Ryan Bloom <rbb@apache.org>
2001-02-22 23:52:51 +0000

    Use "Basic" authentication instead of "basic" in ab, as the spec says we should. PR:	7294 Submitted by:	Andre Breiler <andre.breiler@rd.bbc.co.uk>
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@88281 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -903,14 +903,14 @@ static void test(void)
 static void copyright(void)
 {
     if (!use_html) {
-        printf("This is ApacheBench, Version %s\n", AB_VERSION " <$Revision: 1.56 $> apache-2.0");
+        printf("This is ApacheBench, Version %s\n", AB_VERSION " <$Revision: 1.57 $> apache-2.0");
         printf("Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/\n");
         printf("Copyright (c) 1998-2001 The Apache Software Foundation, http://www.apache.org/\n");
         printf("\n");
     }
     else {
         printf("<p>\n");
-        printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AB_VERSION, "$Revision: 1.56 $");
+        printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AB_VERSION, "$Revision: 1.57 $");
         printf(" Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/<br>\n");
         printf(" Copyright (c) 1998-2001 The Apache Software Foundation, http://www.apache.org/<br>\n");
         printf("</p>\n<p>\n");
@@ -1128,7 +1128,7 @@ int main(int argc, const char * const argv[])
             l=apr_base64_encode(tmp, optarg, strlen(optarg));
             tmp[l]='\0';
  
-            strncat(auth, "Authorization: basic ", sizeof(auth));
+            strncat(auth, "Authorization: Basic ", sizeof(auth));
             strncat(auth, tmp, sizeof(auth));
             strncat(auth, "\r\n", sizeof(auth));
             break;
@@ -1141,7 +1141,7 @@ int main(int argc, const char * const argv[])
             l=apr_base64_encode(tmp, optarg, strlen(optarg));
             tmp[l]='\0';
  
-            strncat(auth, "Proxy-Authorization: basic ", sizeof(auth));
+            strncat(auth, "Proxy-Authorization: Basic ", sizeof(auth));
             strncat(auth, tmp, sizeof(auth));
             strncat(auth, "\r\n", sizeof(auth));
             break;
