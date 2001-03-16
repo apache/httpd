15e12b0c7f06227f10d0e6aadf4e14459bc50bb7
Chuck Murcko <chuck@apache.org>
2001-03-16 07:37:12 +0000

    Stops the forced downgrade of the connection to HTTP/1.0 for proxy requests. Submitted by:	Graham Leggett Reviewed by:	Chuck Murcko
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@88528 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http/http_protocol.c
+++ modules/http/http_protocol.c
@@ -770,12 +770,10 @@ static void basic_http_header_check(request_rec *r,
     if (!r->status_line)
         r->status_line = status_lines[ap_index_of_response(r->status)];
 
-    /* mod_proxy is only HTTP/1.0, so avoid sending HTTP/1.1 error response;
-     * kluge around broken browsers when indicated by force-response-1.0
+    /* kluge around broken browsers when indicated by force-response-1.0
      */
-    if (r->proxyreq
-        || (r->proto_num == HTTP_VERSION(1,0)
-            && apr_table_get(r->subprocess_env, "force-response-1.0"))) {
+    if (r->proto_num == HTTP_VERSION(1,0)
+            && apr_table_get(r->subprocess_env, "force-response-1.0")) {
 
         *protocol = "HTTP/1.0";
         r->connection->keepalive = -1;
