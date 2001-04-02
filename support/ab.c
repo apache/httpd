0e8fe2508d4ee4fc88e44d3f5fa2ce9213c095dd
Jeff Trawick <trawick@apache.org>
2001-04-02 18:49:44 +0000

    "void *res = signal(blah)" isn't cool 'cause void * is not compatible with a function pointer; but we don't need to save the old signal pointer anyway so drop the variable
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@88653 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -1278,14 +1278,14 @@ static void test(void)
 static void copyright(void)
 {
     if (!use_html) {
-	printf("This is ApacheBench, Version %s\n", AB_VERSION " <$Revision: 1.63 $> apache-2.0");
+	printf("This is ApacheBench, Version %s\n", AB_VERSION " <$Revision: 1.64 $> apache-2.0");
 	printf("Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/\n");
 	printf("Copyright (c) 1998-2001 The Apache Software Foundation, http://www.apache.org/\n");
 	printf("\n");
     }
     else {
 	printf("<p>\n");
-	printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AB_VERSION, "$Revision: 1.63 $");
+	printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AB_VERSION, "$Revision: 1.64 $");
 	printf(" Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/<br>\n");
 	printf(" Copyright (c) 1998-2001 The Apache Software Foundation, http://www.apache.org/<br>\n");
 	printf("</p>\n<p>\n");
@@ -1443,9 +1443,6 @@ int main(int argc, const char *const argv[])
     apr_getopt_t *opt;
     const char *optarg;
     char c;
-#ifdef SIGPIPE
-    void *res;
-#endif
 
     /* table defaults  */
     tablestring = "";
@@ -1652,7 +1649,7 @@ int main(int argc, const char *const argv[])
     }
 #endif
 #if SIGPIPE
-    res = signal(SIGPIPE, SIG_IGN);	/* Ignore writes to connections that
+    signal(SIGPIPE, SIG_IGN);	        /* Ignore writes to connections that
 					 * have been closed at the other end. */
 #endif
     copyright();
