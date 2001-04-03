e14a9d7ce435032d39c70e893742794645631d94
Jeff Trawick <trawick@apache.org>
2001-04-03 16:49:25 +0000

    Uhh, we really need the prototypes for standard string functions.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@88691 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -140,6 +140,8 @@
 #include <signal.h>
 #include "apr_lib.h"
 
+#define APR_WANT_STRFUNC
+#include "apr_want.h"
 #if APR_HAVE_STDIO_H
 #include <stdio.h>		/* for EOF */
 #endif
@@ -1275,14 +1277,14 @@ static void test(void)
 static void copyright(void)
 {
     if (!use_html) {
-	printf("This is ApacheBench, Version %s\n", AB_VERSION " <$Revision: 1.68 $> apache-2.0");
+	printf("This is ApacheBench, Version %s\n", AB_VERSION " <$Revision: 1.69 $> apache-2.0");
 	printf("Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/\n");
 	printf("Copyright (c) 1998-2001 The Apache Software Foundation, http://www.apache.org/\n");
 	printf("\n");
     }
     else {
 	printf("<p>\n");
-	printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AB_VERSION, "$Revision: 1.68 $");
+	printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AB_VERSION, "$Revision: 1.69 $");
 	printf(" Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/<br>\n");
 	printf(" Copyright (c) 1998-2001 The Apache Software Foundation, http://www.apache.org/<br>\n");
 	printf("</p>\n<p>\n");
