cbc715a914fa048cc1ebd79a0e3f60762a76ba53
Ben Laurie <ben@apache.org>
2001-05-19 15:54:53 +0000

    Cleanup.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@89163 13f79535-47bb-0310-9956-ffa450edef68

--- modules/experimental/mod_case_filter.c
+++ modules/experimental/mod_case_filter.c
@@ -95,7 +95,7 @@ static const command_rec CaseFilterCmds[] =
     { NULL }
     };
 
-static void CaseFilterRegisterHooks(void)
+static void CaseFilterRegisterHooks(apr_pool_t *p)
     {
     ap_hook_insert_filter(CaseFilterInsertFilter,NULL,NULL,APR_HOOK_MIDDLE);
     ap_register_output_filter(s_szCaseFilterName,CaseFilterOutFilter,
--- modules/experimental/mod_case_filter_in.c
+++ modules/experimental/mod_case_filter_in.c
@@ -1,4 +1,61 @@
-// Ben messing around some more...
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2000-2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+/*
+ * An example input filter - this converts input to upper case. Note that
+ * because of the moment it gets inserted it does NOT convert request headers.
+ */
 
 #include "httpd.h"
 #include "http_config.h"
@@ -95,29 +152,29 @@ static apr_status_t CaseFilterInFilter(ap_filter_t *f,
 }
 	    
 	
-static const char *CaseFilterEnable(cmd_parms *cmd, void *dummy, int arg)
-    {
+static const char *CaseFilterInEnable(cmd_parms *cmd, void *dummy, int arg)
+{
     CaseFilterInConfig *pConfig
       =ap_get_module_config(cmd->server->module_config,&case_filter_in_module);
     pConfig->bEnabled=arg;
 
     return NULL;
-    }
+}
 
 static const command_rec CaseFilterInCmds[] = 
-    {
-    AP_INIT_FLAG("CaseFilterIn", CaseFilterEnable, NULL, RSRC_CONF,
+{
+    AP_INIT_FLAG("CaseFilterIn", CaseFilterInEnable, NULL, RSRC_CONF,
                  "Run an input case filter on this host"),
     { NULL }
-    };
+};
 
 
 static void CaseFilterInRegisterHooks(apr_pool_t *p)
-    {
+{
     ap_hook_insert_filter(CaseFilterInInsertFilter,NULL,NULL,APR_HOOK_MIDDLE);
     ap_register_input_filter(s_szCaseFilterName,CaseFilterInFilter,
 			      AP_FTYPE_CONTENT);
-    }
+}
 
 module case_filter_in_module =
 {
