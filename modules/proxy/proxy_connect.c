8f5002997dc0a863975af665c7ca0a59c036e949
Victor J. Orlikowski <orlikowski@apache.org>
2001-05-23 06:43:46 +0000

    YAPJBUJ. (Yet Another Proxy Janitor Botched-Up Job). Need to be looking for APR_SUCCESS here, not HTTP_OK. Obtained from: Justin Erenkrantz
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@89213 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/proxy_connect.c
+++ modules/proxy/proxy_connect.c
@@ -155,7 +155,7 @@ int ap_proxy_connect_handler(request_rec *r, proxy_server_conf *conf,
      */
 
     /* we break the URL into host, port, uri */
-    if (HTTP_OK != apr_uri_parse_hostinfo_components(p, url, &uri)) {
+    if (APR_SUCCESS != apr_uri_parse_hostinfo_components(p, url, &uri)) {
 	return ap_proxyerror(r, HTTP_BAD_REQUEST,
 			     apr_pstrcat(p, "URI cannot be parsed: ", url, NULL));
     }
