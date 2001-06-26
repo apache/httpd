d3c97dfdac43ad7657cbb546e2d249d28086aaf8
Cliff Woolley <jwoolley@apache.org>
2001-06-26 04:07:43 +0000

    Fix spelling mistake.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@89414 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -492,7 +492,7 @@ static void output_results(void)
     if (timetaken) {
 	printf("Requests per second:    %.2f [#/sec] (mean)\n", done / timetaken);
 	printf("Time per request:       %.3f [ms] (mean)\n", concurrency * timetaken / done);
-	printf("Time per request:       %.3f [ms] (mean, across all concurent requests)\n",
+	printf("Time per request:       %.3f [ms] (mean, across all concurrent requests)\n",
 	       timetaken / done);
 	printf("Transfer rate:          %.2f [Kbytes/sec] received\n",
 	       totalread / 1024 / timetaken);
@@ -1289,14 +1289,14 @@ static void test(void)
 static void copyright(void)
 {
     if (!use_html) {
-	printf("This is ApacheBench, Version %s\n", AB_VERSION " <$Revision: 1.71 $> apache-2.0");
+	printf("This is ApacheBench, Version %s\n", AB_VERSION " <$Revision: 1.72 $> apache-2.0");
 	printf("Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/\n");
 	printf("Copyright (c) 1998-2001 The Apache Software Foundation, http://www.apache.org/\n");
 	printf("\n");
     }
     else {
 	printf("<p>\n");
-	printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AB_VERSION, "$Revision: 1.71 $");
+	printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AB_VERSION, "$Revision: 1.72 $");
 	printf(" Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/<br>\n");
 	printf(" Copyright (c) 1998-2001 The Apache Software Foundation, http://www.apache.org/<br>\n");
 	printf("</p>\n<p>\n");
