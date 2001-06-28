9713e64724034a7958af24584828b87c4b3df825
Victor J. Orlikowski <orlikowski@apache.org>
2001-06-28 17:49:54 +0000

    proxy_ftp.c
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@89474 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/proxy_ftp.c
+++ modules/proxy/proxy_ftp.c
@@ -1591,10 +1591,11 @@ int ap_proxy_ftp_handler(request_rec *r, proxy_server_conf *conf,
 	    ap_pass_brigade(r->output_filters, bb);
 	    apr_brigade_cleanup(bb);
 	}
+	ap_flush_conn(remote);
+	apr_socket_close(remote_sock);
 	apr_brigade_cleanup(bb);
 	ap_log_error(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, 0, r->server,
 		     "proxy: FTP: end body send");
-
     }
     else {
 
@@ -1637,7 +1638,13 @@ int ap_proxy_ftp_handler(request_rec *r, proxy_server_conf *conf,
     ap_pass_brigade(origin->output_filters, bb);
     ap_log_error(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, 0, r->server,
                  "proxy: FTP: QUIT");
-
+    /* responses: 221, 500 */
+    /* 221 Service closing control connection. */
+    /* 500 Syntax error, command unrecognized. */
+    i = ftp_getrc_msg(origin, cbb, buffer, sizeof(buffer));
+    ap_log_error(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, 0, r->server,
+                 "proxy: FTP: %d %s", i, buffer);
+    apr_socket_close(sock);
     apr_brigade_destroy(bb);
     return OK;
 }
