911344e711ffe632346a86bae7d0e00e4e5a0e8c
Victor J. Orlikowski <orlikowski@apache.org>
2001-06-28 07:04:18 +0000

    ftp_getrc_msg is blocking for no good reason after the receive loop. Disable it till we figure out what's going on. ;( At least it works now.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@89470 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/proxy_ftp.c
+++ modules/proxy/proxy_ftp.c
@@ -1640,10 +1640,13 @@ int ap_proxy_ftp_handler(request_rec *r, proxy_server_conf *conf,
     /* responses: 221, 500 */
     /*   221 Service closing control connection. */
     /*   500 Syntax error, command unrecognized. */
-    i = ftp_getrc_msg(origin, cbb, buffer, sizeof(buffer));
-    ap_log_error(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, 0, r->server,
-                 "proxy: FTP: %d %s", i, buffer);
-
+    /* FIXME
+     * Using ftp_getrc_msg is blocking here (in fact, anywhere after the
+     * while loop in section VI). Disabling for now, to get things working.
+     * i = ftp_getrc_msg(origin, cbb, buffer, sizeof(buffer));
+     * ap_log_error(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, 0, r->server,
+     *           "proxy: FTP: %d %s", i, buffer);
+     */
     apr_brigade_destroy(bb);
     return OK;
 }
