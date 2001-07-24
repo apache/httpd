9c114104ab0813500b79d30e4a03d51169e621be
Greg Ames <gregames@apache.org>
2001-07-24 22:55:29 +0000

    Reduce CPU consumption in conv_10 function, used to format "%d" by apr_*printf
    This includes two changes to APR:
      * new functions apr_itoa, apr_ltoa, and apr_off_t_toa
        that provide itoa-type functionality based on pools
      * Inline code in inet_ntop4 to replace sprintf for converting
        binary IP addresses into dotted-decimal format
    
    and two changes to Apache:
      * use the apr_itoa functions in setting the content length,
        in place of apr_psprintf
      * use the apr_itoa functions to replace frequent uses of
        'sprintf("%d",...)' in mod_log_config.
    
    Submitted by: Brian Pane
    Reviewed by:  Dean Gaudet, Greg Ames
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@89686 13f79535-47bb-0310-9956-ffa450edef68

--- server/protocol.c
+++ server/protocol.c
@@ -154,7 +154,7 @@ AP_DECLARE(void) ap_set_content_length(request_rec *r, apr_off_t clength)
 {
     r->clength = clength;
     apr_table_setn(r->headers_out, "Content-Length",
-                   apr_psprintf(r->pool, "%" APR_OFF_T_FMT, clength));
+		   apr_off_t_toa(r->pool, clength));
 }
 
 /*
