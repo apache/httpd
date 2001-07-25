e5a98e5c3d99b1301f42cab3946cd17916425786
Martin Kraemer <martin@apache.org>
2001-07-25 13:25:18 +0000

    Typo
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@89706 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/util.c
+++ src/main/util.c
@@ -469,7 +469,7 @@ API_EXPORT(void) ap_getparents(char *name)
 {
     int l, w;
 
-    /* Four paseses, as per RFC 1808 */
+    /* Four passes, as per RFC 1808 */
     /* a) remove ./ path segments */
 
     for (l = 0, w = 0; name[l] != '\0';) {
