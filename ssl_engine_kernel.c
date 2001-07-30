b8c297321e85516b245c7728499d9a9f15ed4bfa
William A. Rowe Jr <wrowe@apache.org>
2001-07-30 22:30:51 +0000

    More XHTML foo.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk/modules/ssl@89805 13f79535-47bb-0310-9956-ffa450edef68

--- ssl_engine_kernel.c
+++ ssl_engine_kernel.c
@@ -558,13 +558,13 @@ int ssl_hook_Handler(request_rec *r)
         port = ap_get_server_port(r);
         if (!ap_is_default_port(port, r))
             thisport = apr_psprintf(r->pool, ":%u", port);
-        thisurl = apr_psprintf(r->pool, "https://%s%s/",
-                               ap_get_server_name(r), thisport);
+        thisurl = ap_escape_html(r->pool, apr_psprintf(r->pool, "https://%s%s/",
+                                 ap_get_server_name(r), thisport));
 
         apr_table_setn(r->notes, "error-notes", apr_psprintf(r->pool,
-                       "Reason: You're speaking plain HTTP to an SSL-enabled server port.<BR>\n"
-                       "Instead use the HTTPS scheme to access this URL, please.<BR>\n"
-                       "<BLOCKQUOTE>Hint: <A HREF=\"%s\"><B>%s</B></A></BLOCKQUOTE>",
+                       "Reason: You're speaking plain HTTP to an SSL-enabled server port.<br />\n"
+                       "Instead use the HTTPS scheme to access this URL, please.<br />\n"
+                       "<blockquote>Hint: <a href=\"%s\"><b>%s</b></a></blockquote>",
                        thisurl, thisurl));
     }
 
