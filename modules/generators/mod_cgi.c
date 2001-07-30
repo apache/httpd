48b8f81380f5479c00530f281c504f159908ad6d
Bill Stoddard <stoddard@apache.org>
2001-07-30 15:11:40 +0000

    Win32: Get exec cgi tag working.  finfo.protection == 0 on Windows from the apr_stat() call in directory walk (because discovering the permissions on Windows is ridiculously expensive).  All we really need to know is whether the file exists.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@89788 13f79535-47bb-0310-9956-ffa450edef68

--- modules/generators/mod_cgi.c
+++ modules/generators/mod_cgi.c
@@ -815,7 +815,7 @@ static int include_cgi(char *s, request_rec *r, ap_filter_t *next,
     if ((rr->path_info && rr->path_info[0]) || rr->args) {
         return -1;
     }
-    if (rr->finfo.protection == 0) {
+    if (rr->finfo.filetype == 0) {
         return -1;
     }
 
