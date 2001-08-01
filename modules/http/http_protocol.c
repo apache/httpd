3db17f7926d8decc1daa9d3779cc90d3c44a56d7
William A. Rowe Jr <wrowe@apache.org>
2001-08-01 04:08:36 +0000

    Restore to 1.333 state (problematic, in that we could be facing >(size_t)   bytes, but better till I figure out the _right_ fix.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@89853 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http/http_protocol.c
+++ modules/http/http_protocol.c
@@ -1435,11 +1435,8 @@ AP_DECLARE(long) ap_get_client_block(request_rec *r, char *buffer, apr_size_t bu
 
     do {
         if (APR_BRIGADE_EMPTY(bb)) {
-            apr_size_t asmuch = bufsiz;
-            if (r->remaining < asmuch)
-                asmuch = (apr_size_t)r->remaining;
             if (ap_get_brigade(r->input_filters, bb, AP_MODE_BLOCKING,
-                               &asmuch) != APR_SUCCESS) {
+                               &r->remaining) != APR_SUCCESS) {
                 /* if we actually fail here, we want to just return and
                  * stop trying to read data from the client.
                  */
