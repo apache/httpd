b64a6e878e841ffda44931c8018e4c1a3a10b17b
Victor J. Orlikowski <orlikowski@apache.org>
2001-08-03 16:25:10 +0000

    Both the ftp and http proxies were erroneously sending a flush bucket after an eos bucket. This had been put in place to repair a bug, but was covering the symptoms, rather than providing the cure. Bug repaired, and kludge removed.
    Submitted by: barbee@veribox.net
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@89907 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/proxy_http.c
+++ modules/proxy/proxy_http.c
@@ -783,8 +783,6 @@ int ap_proxy_http_handler(request_rec *r, proxy_server_conf *conf,
 	/* read the body, pass it to the output filters */
 	while (ap_get_brigade(rp->input_filters, bb, AP_MODE_BLOCKING, &readbytes) == APR_SUCCESS) {
 	    if (APR_BUCKET_IS_EOS(APR_BRIGADE_LAST(bb))) {
-		e = apr_bucket_flush_create();
-		APR_BRIGADE_INSERT_TAIL(bb, e);
 		ap_pass_brigade(r->output_filters, bb);
 		break;
 	    }
