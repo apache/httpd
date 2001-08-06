2ca9a4c0e9130d9e59713ded3f1b432483584172
William A. Rowe Jr <wrowe@apache.org>
2001-08-06 19:10:12 +0000

    _THIS_ is why mod_dir wouldn't serve the results of mod_negotiation   with a query string
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@89948 13f79535-47bb-0310-9956-ffa450edef68

--- include/http_request.h
+++ include/http_request.h
@@ -207,7 +207,7 @@ AP_DECLARE(void) ap_internal_redirect_handler(const char *new_uri, request_rec *
  * @tip the sub_req's pool will be merged into r's pool, be very careful
  * not to destroy this subrequest, it will be destroyed with the main request!
  */
-AP_DECLARE(void) ap_internal_fast_redirect(request_rec *sub_req, request_rec *r))
+AP_DECLARE(void) ap_internal_fast_redirect(request_rec *sub_req, request_rec *r);
 
 /**
  * Can be used within any handler to determine if any authentication
