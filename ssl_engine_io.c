2a556b97b6dfe3289130dbc0ca455d3cfeb55f00
Doug MacEachern <dougm@apache.org>
2001-08-08 02:10:51 +0000

    drop a brace inside #if 0 so emacs can indent properly
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk/modules/ssl@90023 13f79535-47bb-0310-9956-ffa450edef68

--- ssl_engine_io.c
+++ ssl_engine_io.c
@@ -193,7 +193,6 @@ static apr_status_t churn_output(SSLFilterRec *pRec)
 	    }
             assert (n > 0); /* XXX => Check if required */
 #if 0 /* XXX */ 
-            }
             else if (n == 0)
                 done = 1;
             else
@@ -228,9 +227,9 @@ static apr_status_t churn (SSLFilterRec *pRec,
     ap_input_mode_t eMode = (eReadType == APR_BLOCK_READ) 
                             ? AP_MODE_BLOCKING : AP_MODE_NONBLOCKING;
 
-/* XXX : Errrr... bad way of doing things TBD */
-eReadType = APR_BLOCK_READ;
-eMode     = AP_MODE_BLOCKING;
+    /* XXX : Errrr... bad way of doing things TBD */
+    eReadType = APR_BLOCK_READ;
+    eMode     = AP_MODE_BLOCKING;
 
     if(APR_BRIGADE_EMPTY(pRec->pbbInput)) {
 	ap_get_brigade(pRec->pInputFilter->next,pRec->pbbInput,eMode,readbytes);
