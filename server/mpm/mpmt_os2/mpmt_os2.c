2fa4bab64a53df0a61b967de1c095cc1fda95495
Paul J. Reder <rederpj@apache.org>
2001-08-20 19:47:52 +0000

    The prefork and OS/2 MPMs are overwriting the pid file when a second copy of httpd is started and shuts down due to socket conflict. Moving the call to ap_log_pid solves the problem.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@90418 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/mpmt_os2/mpmt_os2.c
+++ server/mpm/mpmt_os2/mpmt_os2.c
@@ -193,13 +193,14 @@ int ap_mpm_run(apr_pool_t *_pconf, apr_pool_t *plog, server_rec *s )
         /* Parent process */
         char restart;
         is_parent_process = TRUE;
-        ap_log_pid(pconf, ap_pid_fname);
 
         if (ap_setup_listeners(ap_server_conf) < 1) {
             ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_ALERT, 0, s,
                          "no listening sockets available, shutting down");
             return 1;
         }
+
+        ap_log_pid(pconf, ap_pid_fname);
 
         restart = master_main();
         ++ap_my_generation;
