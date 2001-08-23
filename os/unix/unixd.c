0defb632876e8ee3f2225262545018921159b769
Joshua Slive <slive@apache.org>
2001-08-23 00:15:37 +0000

    This probably falls under the category: if you can't figure it out yourself, you shouldn't be doing it.  But we might as well remove obsolete stuff.
    PR: 4685
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@90527 13f79535-47bb-0310-9956-ffa450edef68

--- os/unix/unixd.c
+++ os/unix/unixd.c
@@ -203,9 +203,9 @@ AP_DECLARE(const char *) unixd_set_user(cmd_parms *cmd, void *dummy,
 		"\trunning as root.  There are known race conditions that\n"
 		"\twill allow any local user to read any file on the system.\n"
 		"\tIf you still desire to serve pages as root then\n"
-		"\tadd -DBIG_SECURITY_HOLE to the EXTRA_CFLAGS line in your\n"
-		"\tsrc/Configuration file and rebuild the server.  It is\n"
-		"\tstrongly suggested that you instead modify the User\n"
+		"\tadd -DBIG_SECURITY_HOLE to the CFLAGS env variable\n"
+		"\tand then rebuild the server.\n"
+		"\tIt is strongly suggested that you instead modify the User\n"
 		"\tdirective in your httpd.conf file to list a non-root\n"
 		"\tuser.\n";
     }
