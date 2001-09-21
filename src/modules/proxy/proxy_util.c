c53b3fa4101e44bd52ba530c7e15d1e36bec5918
Martin Kraemer <martin@apache.org>
2001-09-21 09:04:19 +0000

    Remove obsolete comment
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@91099 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/proxy/proxy_util.c
+++ src/modules/proxy/proxy_util.c
@@ -290,9 +290,6 @@ char *
 	return "Bad IP address in URL";
     }
 
-/*    if (strchr(host,'.') == NULL && domain != NULL)
-   host = pstrcat(p, host, domain, NULL);
- */
     *urlp = url;
     *hostp = host;
 
