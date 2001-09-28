bea7dd2faf7b760e8efa84d0c3f722c7a5ca314c
William A. Rowe Jr <wrowe@apache.org>
2001-09-28 04:35:59 +0000

    Remove the translate_name hack that caused Greg Ames some grief with   mod_rewrite.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@91168 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -2552,12 +2552,6 @@ AP_DECLARE_NONSTD(int) ap_core_translate(request_rec *r)
     void *sconf = r->server->module_config;
     core_server_config *conf = ap_get_module_config(sconf, &core_module);
   
-    /* XXX We have already been here, or another module did the work
-     * for us.  At this moment, we will enable only file subrequests.
-     */
-    if (r->main && r->filename)
-        return OK;
-
     /* XXX this seems too specific, this should probably become
      * some general-case test 
      */
