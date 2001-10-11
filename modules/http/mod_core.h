d2b3b0076da7a79d24b721935a9702c39603b657
Justin Erenkrantz <jerenkrantz@apache.org>
2001-10-11 19:44:51 +0000

    Remove deprecated ap_dechunk_filter from header.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@91434 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http/mod_core.h
+++ modules/http/mod_core.h
@@ -75,8 +75,6 @@ extern "C" {
  */
 apr_status_t ap_http_filter(ap_filter_t *f, apr_bucket_brigade *b,
                             ap_input_mode_t mode, apr_off_t *readbytes);
-apr_status_t ap_dechunk_filter(ap_filter_t *f, apr_bucket_brigade *b,
-                               ap_input_mode_t mode, apr_off_t *readbytes);
 
 char *ap_response_code_string(request_rec *r, int error_index);
 
