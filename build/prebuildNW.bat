3050920e9decefdd4615cc990e8c2d1673ca099b
Bradley Nicholes <bnicholes@apache.org>
2001-10-22 18:29:45 +0000

    Scripts for setting up the build environment and generating the import lists
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@91638 13f79535-47bb-0310-9956-ffa450edef68

---
+++ build/prebuildNW.bat
@@ -0,0 +1,24 @@
+@echo off
+rem # As part of the pre-build process, the utilities GenChars.NLM
+rem #  (Gen Test Chars) and DFTables.NLM (dftables) must be built, 
+rem #  copied to a NetWare server and run using the following commands:
+rem #
+rem # genchars >test_char.h
+rem # dftables >chartables.c
+rem #
+rem #  The files "sys:\test_chars.h" and "sys:\chartables.c" must be 
+rem #  copied to "httpd\os\netware" on the build machine.
+
+@echo Fixing up the APR headers
+copy ..\srclib\apr\include\apr.hnw ..\srclib\apr\include\apr.h
+
+@echo Fixing up the APR-Util headers
+copy ..\srclib\apr-util\include\apu.h.in ..\srclib\apr-util\include\apu.h
+
+@echo Fixing up the pcre headers
+copy ..\srclib\pcre\config.hw ..\srclib\pcre\config.h
+copy ..\srclib\pcre\pcre.hw ..\srclib\pcre\pcre.h
+
+@echo Generating the import lists...
+awk95 -f make_nw_export.awk ..\srclib\apr\include\*.h |sort > ..\os\netware\aprlib.imp
+awk95 -f make_nw_export.awk ..\srclib\apr-util\include\*.h |sort > ..\os\netware\aprutil.imp
