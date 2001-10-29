0f0c205e0f39d8c5c5faf2eecd58e3fe24188aaf
Aaron Bannert <aaron@apache.org>
2001-10-29 19:25:08 +0000

    Mixed tabs and spaces within many nexted {}-less if-else expressions can be very difficult to read. No logic was changed.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@91688 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_access.c
+++ modules/aaa/mod_access.c
@@ -294,37 +294,36 @@ static int find_allowdeny(request_rec *r, apr_array_header_t *a, int method)
 static int check_dir_access(request_rec *r)
 {
     int method = r->method_number;
-    access_dir_conf *a =
-    (access_dir_conf *)
-    ap_get_module_config(r->per_dir_config, &access_module);
     int ret = OK;
+    access_dir_conf *a = (access_dir_conf *)
+        ap_get_module_config(r->per_dir_config, &access_module);
 
     if (a->order[method] == ALLOW_THEN_DENY) {
-	ret = HTTP_FORBIDDEN;
-	if (find_allowdeny(r, a->allows, method))
-	    ret = OK;
-	if (find_allowdeny(r, a->denys, method))
-	    ret = HTTP_FORBIDDEN;
+        ret = HTTP_FORBIDDEN;
+        if (find_allowdeny(r, a->allows, method))
+            ret = OK;
+        if (find_allowdeny(r, a->denys, method))
+            ret = HTTP_FORBIDDEN;
     }
     else if (a->order[method] == DENY_THEN_ALLOW) {
-	if (find_allowdeny(r, a->denys, method))
-	    ret = HTTP_FORBIDDEN;
-	if (find_allowdeny(r, a->allows, method))
-	    ret = OK;
+        if (find_allowdeny(r, a->denys, method))
+            ret = HTTP_FORBIDDEN;
+        if (find_allowdeny(r, a->allows, method))
+            ret = OK;
     }
     else {
-	if (find_allowdeny(r, a->allows, method)
-	    && !find_allowdeny(r, a->denys, method))
-	    ret = OK;
-	else
-	    ret = HTTP_FORBIDDEN;
+        if (find_allowdeny(r, a->allows, method)
+            && !find_allowdeny(r, a->denys, method))
+            ret = OK;
+        else
+            ret = HTTP_FORBIDDEN;
     }
 
     if (ret == HTTP_FORBIDDEN
-	&& (ap_satisfies(r) != SATISFY_ANY || !ap_some_auth_required(r))) {
-	ap_log_rerror(APLOG_MARK, APLOG_NOERRNO|APLOG_ERR, 0, r,
-		  "client denied by server configuration: %s",
-		  r->filename);
+        && (ap_satisfies(r) != SATISFY_ANY || !ap_some_auth_required(r))) {
+        ap_log_rerror(APLOG_MARK, APLOG_NOERRNO|APLOG_ERR, 0, r,
+            "client denied by server configuration: %s",
+            r->filename);
     }
 
     return ret;
