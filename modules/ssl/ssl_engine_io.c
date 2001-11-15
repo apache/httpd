43def6bf623fb6a64cacac3f2ee9ca6ca79d1f96
Ryan Bloom <rbb@apache.org>
2001-11-15 20:55:13 +0000

    Fix the SSL filter logic.  The SSL filter is not a network filter, because it does not actually do the reading and writing to the network.  By moving that filter to in between CONNECTION and NETWORK filters, we ensure that SSL is always called before the core.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@91969 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_io.c
+++ modules/ssl/ssl_engine_io.c
@@ -578,8 +578,8 @@ void ssl_io_filter_init(conn_rec *c, SSL *ssl)
 
 void ssl_io_filter_register(apr_pool_t *p)
 {
-    ap_register_input_filter  (ssl_io_filter, ssl_io_filter_Input,  AP_FTYPE_NETWORK);
-    ap_register_output_filter (ssl_io_filter, ssl_io_filter_Output, AP_FTYPE_NETWORK);
+    ap_register_input_filter  (ssl_io_filter, ssl_io_filter_Input,  AP_FTYPE_CONNECTION + 5);
+    ap_register_output_filter (ssl_io_filter, ssl_io_filter_Output, AP_FTYPE_CONNECTION + 5);
     return;
 }
 
