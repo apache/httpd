1d3fe7a19bd963fc6855b995b2daef295cd7f468
Doug MacEachern <dougm@apache.org>
2001-11-29 05:08:47 +0000

    move srand() call from ssl_rand_choosenum() to ssl_init_Child()
    this was being called twice per-connection with 'SSLRandom connect builtin'
    configured (which is in the default config)
    
    this also gets rid of two time() syscalls per-connection
    
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@92232 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_rand.c
+++ modules/ssl/ssl_engine_rand.c
@@ -203,7 +203,6 @@ static int ssl_rand_choosenum(int l, int h)
     int i;
     char buf[50];
 
-    srand((unsigned int)time(NULL));
     apr_snprintf(buf, sizeof(buf), "%.0f",
                  (((double)(rand()%RAND_MAX)/RAND_MAX)*(h-l)));
     i = atoi(buf)+1;
