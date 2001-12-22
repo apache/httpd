6f1a199bdcdeb251c398c7f31a5da9dfd998cf5b
Jeff Trawick <trawick@apache.org>
2001-12-22 23:53:49 +0000

    fix a nasty bug which periodically led to segfaults trying to access the scoreboard
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@92579 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/experimental/perchild/perchild.c
+++ server/mpm/experimental/perchild/perchild.c
@@ -552,7 +552,7 @@ static void process_socket(apr_pool_t *p, apr_socket_t *sock, long conn_id)
         ap_sock_disable_nagle(sock);
     }
 
-    ap_create_sb_handle(&sbh, p, conn_id / HARD_SERVER_LIMIT, thread_num);
+    ap_create_sb_handle(&sbh, p, conn_id / HARD_THREAD_LIMIT, thread_num);
     current_conn = ap_run_create_connection(p, ap_server_conf, sock, conn_id, sbh);
     if (current_conn) {
         ap_process_connection(current_conn);
