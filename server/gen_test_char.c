16ffdd12cb0099f86b12735807cc1549bc2f1413
Ian Holsman <ianh@apache.org>
2001-12-26 22:00:01 +0000

    remove unneeded include
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@92612 13f79535-47bb-0310-9956-ffa450edef68

--- server/gen_test_char.c
+++ server/gen_test_char.c
@@ -64,7 +64,6 @@
 #endif
 
 /* we need some of the portability definitions... for strchr */
-#include "ap_config.h"
 #include "httpd.h"
 
 /* A bunch of functions in util.c scan strings looking for certain characters.
