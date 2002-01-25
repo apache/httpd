be950610131dd353fef7a0e8740e263fd060083a
Justin Erenkrantz <jerenkrantz@apache.org>
2002-01-25 02:38:52 +0000

    Add another status code to ignore in the error_log. (I'm really tempted to ditch this log line altogether.)
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@93021 13f79535-47bb-0310-9956-ffa450edef68

--- server/protocol.c
+++ server/protocol.c
@@ -566,10 +566,11 @@ static int read_request_line(request_rec *r)
                          &len, r, 0);
 
         if (rv != APR_SUCCESS) {
-            /* We'll get TIMEUP or EOF on keepalives, so those are common
-             * errors that we don't want to log.
+            /* We could get TIMEUP, EOF, or CONNRESET on socket timeouts,
+             * so those are common errors that we don't want to log.
              */
-            if (!APR_STATUS_IS_TIMEUP(rv) && !APR_STATUS_IS_EOF(rv)) {
+            if (!APR_STATUS_IS_TIMEUP(rv) && !APR_STATUS_IS_EOF(rv) &&
+                !APR_STATUS_IS_ECONNRESET(rv)) {
                 ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r, "read_request_line() failed");
             }
 	        r->request_time = apr_time_now();
