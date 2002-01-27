08002aaebbc8c5196b4d7f960bc017e6ef75bce2
Brian Pane <brianp@apache.org>
2002-01-27 01:54:54 +0000

    Added new functions ap_add_input_filter_handle() and ap_add_output_filter_handle()
    There are many places in the core modules where we do
    ap_add_input_filter() or ap_add_output_filter() on a
    hardcoded filter name (e.g., "CORE" or "BYTERANGE").
    This requires a string-to-filter mapping that wastes
    CPU time.  (Even though the string lookup uses a trie
    for speed, it still ranks as a big consumer of CPU time
    because of the large number of filters added per request.)
    
    The new ap_add_*_filter_handle() functions will allow
    us to skip the string-to-filter mapping in cases where
    the module adding the filter happens to have the
    ap_filter_rec_t* for the registered filter (e.g., because
    it's the same module that registered the filter in the
    first place).
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@93039 13f79535-47bb-0310-9956-ffa450edef68

--- include/util_filter.h
+++ include/util_filter.h
@@ -353,6 +353,19 @@ AP_DECLARE(ap_filter_t *) ap_add_input_filter(const char *name, void *ctx,
 					      request_rec *r, conn_rec *c);
 
 /**
+ * Variant of ap_add_input_filter() that accepts a registered filter handle
+ * (as returned by ap_register_input_filter()) rather than a filter name
+ *
+ * @param name The filter to add
+ * @param r The request to add this filter for (or NULL if it isn't associated with a request)
+ * @param c The connection to add the fillter for
+ */
+AP_DECLARE(ap_filter_t *) ap_add_input_filter_handle(ap_filter_rec_t *f,
+                                                     void *ctx,
+                                                     request_rec *r,
+                                                     conn_rec *c);
+
+/**
  * Add a filter to the current request.  Filters are added in a FIFO manner.
  * The first filter added will be the first filter called.
  * @param name The name of the filter to add
@@ -362,6 +375,19 @@ AP_DECLARE(ap_filter_t *) ap_add_input_filter(const char *name, void *ctx,
  */
 AP_DECLARE(ap_filter_t *) ap_add_output_filter(const char *name, void *ctx, 
 					       request_rec *r, conn_rec *c);
+
+/**
+ * Variant of ap_add_output_filter() that accepts a registered filter handle
+ * (as returned by ap_register_output_filter()) rather than a filter name
+ *
+ * @param name The filter to add
+ * @param r The request to add this filter for (or NULL if it isn't associated with a request)
+ * @param c The connection to add the fillter for
+ */
+AP_DECLARE(ap_filter_t *) ap_add_output_filter_handle(ap_filter_rec_t *f,
+                                                      void *ctx,
+                                                      request_rec *r,
+                                                      conn_rec *c);
 
 /**
  * Remove an input filter from either the request or connection stack
