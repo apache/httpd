d5c229c0fd08dc8cd1c444c7ac723fce1159b4a3
Bill Stoddard <stoddard@apache.org>
2002-01-27 12:52:08 +0000

    Remove the create_connection hook and put the client_socket back into the conn_rec. The create_connection_hook has a design flaw that prevents it from making decisions based on vhost information.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@93050 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/proxy_ftp.c
+++ modules/proxy/proxy_ftp.c
@@ -746,7 +746,7 @@ int ap_proxy_ftp_handler(request_rec *r, proxy_server_conf *conf,
     }
 
     /* the socket is now open, create a new connection */
-    origin = ap_run_create_connection(p, r->server, sock, r->connection->id, r->connection->sbh);
+    origin = ap_new_connection(p, r->server, sock, r->connection->id, r->connection->sbh);
     if (!origin) {
 	/* the peer reset the connection already; ap_new_connection() 
 	 * closed the socket */
@@ -1549,7 +1549,7 @@ int ap_proxy_ftp_handler(request_rec *r, proxy_server_conf *conf,
     }
 
     /* the transfer socket is now open, create a new connection */
-    remote = ap_run_create_connection(p, r->server, remote_sock, r->connection->id, r->connection->sbh);
+    remote = ap_new_connection(p, r->server, remote_sock, r->connection->id, r->connection->sbh);
     if (!remote) {
 	/* the peer reset the connection already; ap_new_connection() 
 	 * closed the socket */
