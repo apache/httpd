64ad9ca8997bfee7283b2b3e2a303f2832267dee
Ken Coar <coar@apache.org>
2002-01-31 20:29:41 +0000

    If the RedirectMatch target is an abs_path, make it an absoluteURI
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@93142 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/standard/mod_alias.c
+++ src/modules/standard/mod_alias.c
@@ -393,11 +393,20 @@ static int fixup_redir(request_rec *r)
 
     if ((ret = try_alias_list(r, dirconf->redirects, 1, &status)) != NULL) {
         if (ap_is_HTTP_REDIRECT(status)) {
+            if (ret[0] == '/') {
+                char *orig_target = ret;
+
+                ret = ap_construct_url(r->pool, ret, r);
+                ap_log_rerror(APLOG_MARK, APLOG_WARNING|APLOG_NOERRNO, r,
+                              "incomplete redirection target of '%s' for "
+                              "URI '%s' modified to '%s'",
+                              orig_target, r->uri, ret);
+            }
             if (!ap_is_url(ret)) {
                 status = HTTP_INTERNAL_SERVER_ERROR;
                 ap_log_rerror(APLOG_MARK, APLOG_ERR|APLOG_NOERRNO, r,
                               "cannot redirect '%s' to '%s'; "
-                              "target is not a valid absoluteURI",
+                              "target is not a valid absoluteURI or abs_path",
                               r->uri, ret);
             }
             else {
