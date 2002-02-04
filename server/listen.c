15721bb31231f409e2dfc4f893daa4dc41f29ad9
Ryan Bloom <rbb@apache.org>
2002-02-04 16:58:54 +0000

    This patch allows the prefork MPM to print messages to the console if it can't open a socket for some reason.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@93224 13f79535-47bb-0310-9956-ffa450edef68

--- server/listen.c
+++ server/listen.c
@@ -142,7 +142,7 @@ static apr_status_t make_sock(apr_pool_t *p, ap_listen_rec *server)
 #endif
 
     if ((stat = apr_bind(s, server->bind_addr)) != APR_SUCCESS) {
-        ap_log_perror(APLOG_MARK, APLOG_CRIT, stat, p,
+        ap_log_perror(APLOG_MARK, APLOG_STARTUP|APLOG_CRIT, stat, p,
                      "make_sock: could not bind to address %pI", 
                      server->bind_addr);
         apr_socket_close(s);
@@ -150,7 +150,7 @@ static apr_status_t make_sock(apr_pool_t *p, ap_listen_rec *server)
     }
 
     if ((stat = apr_listen(s, ap_listenbacklog)) != APR_SUCCESS) {
-        ap_log_perror(APLOG_MARK, APLOG_ERR, stat, p,
+        ap_log_perror(APLOG_MARK, APLOG_STARTUP|APLOG_ERR, stat, p,
             "make_sock: unable to listen for connections on address %pI", 
                      server->bind_addr);
         apr_socket_close(s);
--- server/main.c
+++ server/main.c
@@ -446,8 +446,11 @@ int main(int argc, const char * const argv[])
 	destroy_and_exit_process(process, 0);
     }
     apr_pool_clear(plog);
+    /* It is assumed that if you are going to fail the open_logs phase, then
+     * you will print out your own message that explains what has gone wrong.
+     * The server doesn't need to do that for you.
+     */
     if ( ap_run_open_logs(pconf, plog, ptemp, server_conf) != OK) {
-        ap_log_error(APLOG_MARK, APLOG_STARTUP |APLOG_ERR| APLOG_NOERRNO, 0, NULL, "Unable to open logs\n");
         destroy_and_exit_process(process, 1);
     }
     if ( ap_run_post_config(pconf, plog, ptemp, server_conf) != OK) {
