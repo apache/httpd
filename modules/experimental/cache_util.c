a771a33db8348101d269b3261367d837f86c0f72
Bill Stoddard <stoddard@apache.org>
2002-02-04 14:48:55 +0000

    Bringing up some code to our coding style guidelines
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@93221 13f79535-47bb-0310-9956-ffa450edef68

--- modules/experimental/cache_util.c
+++ modules/experimental/cache_util.c
@@ -272,11 +272,11 @@ static void cache_hash(const char *it, char *val, int ndepth, int nlength)
      * 5 x 3 bytes + 1 byte -> 5 * 4 characters + 2 characters
      */
     for (i = 0, k = 0; i < 15; i += 3) {
-    x = (digest[i] << 16) | (digest[i + 1] << 8) | digest[i + 2];
-    tmp[k++] = enc_table[x >> 18];
-    tmp[k++] = enc_table[(x >> 12) & 0x3f];
-    tmp[k++] = enc_table[(x >> 6) & 0x3f];
-    tmp[k++] = enc_table[x & 0x3f];
+        x = (digest[i] << 16) | (digest[i + 1] << 8) | digest[i + 2];
+        tmp[k++] = enc_table[x >> 18];
+        tmp[k++] = enc_table[(x >> 12) & 0x3f];
+        tmp[k++] = enc_table[(x >> 6) & 0x3f];
+        tmp[k++] = enc_table[x & 0x3f];
     }
 
     /* one byte left */
