a6da063e6edbcc1eba98c1418270c133286a19d5
Ken Coar <coar@apache.org>
2002-02-05 11:35:57 +0000

    Change the 'we turned /foo into http://host/foo' log message 	to DEBUG; no reason to clutter people's log files with this 	when it's supported behaviour.  Of course, they can find out 	about the performance impact by LogLeveling Debug. :-)
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@93251 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_alias.c
+++ modules/mappers/mod_alias.c
@@ -440,7 +440,7 @@ static int fixup_redir(request_rec *r)
                 char *orig_target = ret;
 
                 ret = ap_construct_url(r->pool, ret, r);
-                ap_log_rerror(APLOG_MARK, APLOG_WARNING|APLOG_NOERRNO, 0, r,
+                ap_log_rerror(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, 0, r,
                               "incomplete redirection target of '%s' for "
                               "URI '%s' modified to '%s'",
                               orig_target, r->uri, ret);
