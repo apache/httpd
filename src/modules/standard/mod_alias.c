2ce7a94202fdf67d45b5becabec263e6ec2a4607
Ken Coar <coar@apache.org>
2002-02-05 15:21:55 +0000

    Demote the 'turned /foo into http://host/foo' message from 	WARNING to DEBUG; it's a supported operation, so no need to 	fill normal log files with noise.  Setting LogLevel to Debug 	will show the activity, though.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@93255 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/standard/mod_alias.c
+++ src/modules/standard/mod_alias.c
@@ -397,7 +397,7 @@ static int fixup_redir(request_rec *r)
                 char *orig_target = ret;
 
                 ret = ap_construct_url(r->pool, ret, r);
-                ap_log_rerror(APLOG_MARK, APLOG_WARNING|APLOG_NOERRNO, r,
+                ap_log_rerror(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, r,
                               "incomplete redirection target of '%s' for "
                               "URI '%s' modified to '%s'",
                               orig_target, r->uri, ret);
