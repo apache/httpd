2eb8d382da841bf25583e641cdf7749ed30b2a4e
Joshua Slive <slive@apache.org>
2002-02-14 00:48:04 +0000

    Add the parts for the xml to html transformation system for the module docs.  Details will be at http://httpd.apache.org/docs-project/docsformat.html shortly.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@93398 13f79535-47bb-0310-9956-ffa450edef68

---
+++ docs/manual/style/build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if [ "$JAVA_HOME" = "" ] ; then
+  echo You must set JAVA_HOME to point at your Java Development Kit directory
+  exit 1
+fi
+
+# convert the existing path to unix
+if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
+   CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
+fi
+
+# Add in your .jar files first
+for i in ./lib/*.jar
+do
+    CLASSPATH=$CLASSPATH:"$i"
+done
+
+# convert the unix path to windows
+if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
+   CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
+fi
+
+BUILDFILE=build.xml
+
+${JAVA_HOME}/bin/java $ANT_OPTS -classpath "$CLASSPATH" \
+                org.apache.tools.ant.Main \
+                -Dant.home=$ANT_HOME \
+                -buildfile ${BUILDFILE} \
+                 "$@"
