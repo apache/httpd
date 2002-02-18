beccdc76d68cf750ea0d0e651d3a4ae92a85b22c
Greg Ames <gregames@apache.org>
2002-02-18 18:19:06 +0000

    one step closer to happiness on ebcdic boxes
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@93482 13f79535-47bb-0310-9956-ffa450edef68

--- support/htdbm.c
+++ support/htdbm.c
@@ -181,7 +181,7 @@ static apr_status_t htdbm_init(apr_pool_t **pool, htdbm_t **hdbm)
     (*hdbm)->pool = *pool;
 
 #if APR_CHARSET_EBCDIC
-    rv = apr_xlate_open(to_ascii, "ISO8859-1", APR_DEFAULT_CHARSET, (*hdbm)->pool);
+    rv = apr_xlate_open(&((*hdbm)->to_ascii), "ISO8859-1", APR_DEFAULT_CHARSET, (*hdbm)->pool);
     if (rv) {
         fprintf(stderr, "apr_xlate_open(to ASCII)->%d\n", rv);
         return APR_EGENERAL;
