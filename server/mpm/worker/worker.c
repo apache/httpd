7803813f295026a6f569658fd36da790b8dd6d95
Jeff Trawick <trawick@apache.org>
2002-03-05 21:01:24 +0000

    fix a typo in a log message...  the message claimed that apr_proc_mutex_lock() failed when actually it was apr_proc_mutex_unlock() which failed
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@93719 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/worker/worker.c
+++ server/mpm/worker/worker.c
@@ -695,7 +695,7 @@ static void *listener_thread(apr_thread_t *thd, void * dummy)
             if ((rv = SAFE_ACCEPT(apr_proc_mutex_unlock(accept_mutex)))
                 != APR_SUCCESS) {
                 ap_log_error(APLOG_MARK, APLOG_EMERG, rv, ap_server_conf,
-                             "apr_proc_mutex_lock failed. Attempting to "
+                             "apr_proc_mutex_unlock failed. Attempting to "
                              "shutdown process gracefully.");
                 signal_workers();
             }
