9f57186f6ef3a7f1009d8eae9a98cd0b889ba8d2
Jim Jagielski <jim@apache.org>
2002-03-06 13:03:15 +0000

    Fix a -Wall warning and keep things for Darwin(MacOS) in sync. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@93724 13f79535-47bb-0310-9956-ffa450edef68

--- src/include/hsregex.h
+++ src/include/hsregex.h
@@ -16,7 +16,8 @@ extern "C" {
 #endif
 #endif
 
-#if defined(MAC_OS) || defined(MAC_OS_X_SERVER)
+#undef ap_private_extern
+#if defined(MAC_OS) || defined(MAC_OS_X_SERVER) || (defined(DARWIN) && defined(__DYNAMIC__))
 #define ap_private_extern __private_extern__
 #else
 #define ap_private_extern
