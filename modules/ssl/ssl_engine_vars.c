5f33a419b474581b3c4f398d113fbb3178663af4
Doug MacEachern <dougm@apache.org>
2002-03-10 00:22:07 +0000

    don't allocate SSLConnRec unless ssl is enabled on this vhost. also provides a shorter shortcut for mod_ssl hooks to decline if ssl is not enabled.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@93823 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_vars.c
+++ modules/ssl/ssl_engine_vars.c
@@ -679,7 +679,7 @@ static const char *ssl_var_log_handler_x(request_rec *r, char *a)
     char *result;
 
     result = NULL;
-    if (sslconn->ssl != NULL)
+    if (sslconn && sslconn->ssl)
         result = ssl_var_lookup(r->pool, r->server, r->connection, r, a);
     if (result != NULL && result[0] == NUL)
         result = NULL;
