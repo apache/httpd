928aff8a025f6c85be22b47a110e09f55b9b8f8f
Jeff Trawick <trawick@apache.org>
2002-03-18 18:24:55 +0000

    Fix a hang condition with graceful restart and prefork MPM in the situation where MaxClients is very high but much fewer servers are actually started at the time of the restart.
    The way we notify an entire generation to die at once is
    changed so that we don't have to use the pod (and deal with
    the ease of filling the kernel pipe buffer).
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@93999 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm_common.c
+++ server/mpm_common.c
@@ -540,14 +540,21 @@ void ap_mpm_pod_killpg(ap_pod_t *pod, int num)
     int i;
     apr_status_t rv = APR_SUCCESS;
 
+    /* we don't write anything to the pod here...  we assume
+     * that the would-be reader of the pod has another way to
+     * see that it is time to die once we wake it up
+     *
+     * writing lots of things to the pod at once is very
+     * problematic... we can fill the kernel pipe buffer and
+     * be blocked until somebody consumes some bytes or
+     * we hit a timeout...  if we hit a timeout we can't just
+     * keep trying because maybe we'll never successfully
+     * write again...  but then maybe we'll leave would-be
+     * readers stranded (a number of them could be tied up for
+     * a while serving time-consuming requests)
+     */
     for (i = 0; i < num && rv == APR_SUCCESS; i++) {
-        rv = pod_signal_internal(pod);
-    }
-
-    if (rv == APR_SUCCESS) {
-        for (i = 0; i < num && rv == APR_SUCCESS; i++) {
-             rv = dummy_connection(pod);
-        }
+        rv = dummy_connection(pod);
     }
 }
 #endif /* #ifdef AP_MPM_USES_POD */
