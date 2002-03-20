115c57b05800abdc74eaba85da509cf84a7d3a49
William A. Rowe Jr <wrowe@apache.org>
2002-03-20 04:29:55 +0000

    When restarting [always graceful on Win32], we don't repeat pre_mpm   (Unix doesn't, we shouldn't either.)  [Ryan Bloom]
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@94032 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/winnt/mpm_winnt.c
+++ server/mpm/winnt/mpm_winnt.c
@@ -2382,7 +2382,12 @@ AP_DECLARE(int) ap_mpm_run(apr_pool_t *_pconf, apr_pool_t *plog, server_rec *s )
 {
     static int restart = 0;            /* Default is "not a restart" */
 
-    if ((parent_pid == my_pid) || one_process) {
+    /* ### If non-graceful restarts are ever introduced - we need to rerun 
+     * the pre_mpm hook on subsequent non-graceful restarts.  But Win32 
+     * has only graceful style restarts - and we need this hook to act 
+     * the same on Win32 as on Unix.
+     */
+    if (!restart && ((parent_pid == my_pid) || one_process)) {
         /* Set up the scoreboard. */
         if (ap_run_pre_mpm(pconf, SB_SHARED) != OK) {
             return 1;
