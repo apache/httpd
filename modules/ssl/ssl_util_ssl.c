57b30b4bdcc84ad03cea98bd9e30364d8a0cf431
Doug MacEachern <dougm@apache.org>
2002-03-27 01:28:20 +0000

    add modssl_session_get_time() function to give mod_ssl what it needs from SSL_SESSION_get_time() if using OpenSSL or sslc.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@94195 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_util_ssl.c
+++ modules/ssl/ssl_util_ssl.c
@@ -550,3 +550,15 @@ char *SSL_SESSION_id2sz(unsigned char *id, int idlen,
     return str;
 }
 
+/* sslc+OpenSSL compat */
+
+int modssl_session_get_time(SSL_SESSION *session)
+{
+#ifdef OPENSSL_VERSION_NUMBER
+    return SSL_SESSION_get_time(session);
+#else /* assume sslc */
+    CRYPTO_TIME_T ct;
+    SSL_SESSION_get_time(session, &ct);
+    return CRYPTO_time_to_int(&ct);
+#endif
+}
