965c3a8fea139cdf3541727fac1d5906d3897b0f
Brian Pane <brianp@apache.org>
2002-04-14 09:21:43 +0000

    fixed a typo
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@94643 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -648,7 +648,7 @@ static void output_results(void)
                            "       deviation apart. These results are NOT reliable.\n"); \
                 else if (d > sd ) \
                     printf("WARING: The median and mean for " what " are not within a normal deviation\n" \
-                           "        These results are propably not that reliable.\n"); \
+                           "        These results are probably not that reliable.\n"); \
             }
 	    SANE("the initial connection time", totalcon, meancon, sdcon);
 	    SANE("the processing time", totald, meand, sdd);
@@ -1302,14 +1302,14 @@ static void test(void)
 static void copyright(void)
 {
     if (!use_html) {
-	printf("This is ApacheBench, Version %s\n", AP_SERVER_BASEREVISION " <$Revision: 1.94 $> apache-2.0");
+	printf("This is ApacheBench, Version %s\n", AP_SERVER_BASEREVISION " <$Revision: 1.95 $> apache-2.0");
 	printf("Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/\n");
 	printf("Copyright (c) 1998-2002 The Apache Software Foundation, http://www.apache.org/\n");
 	printf("\n");
     }
     else {
 	printf("<p>\n");
-	printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AP_SERVER_BASEREVISION, "$Revision: 1.94 $");
+	printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AP_SERVER_BASEREVISION, "$Revision: 1.95 $");
 	printf(" Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/<br>\n");
 	printf(" Copyright (c) 1998-2002 The Apache Software Foundation, http://www.apache.org/<br>\n");
 	printf("</p>\n<p>\n");
