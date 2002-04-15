525a3aef80bbdeda2afc6e72114f8c5c70d45406
Brian Pane <brianp@apache.org>
2002-04-15 23:43:04 +0000

    Fix an uninitialized variable
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@94664 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/experimental/leader/leader.c
+++ server/mpm/experimental/leader/leader.c
@@ -947,7 +947,7 @@ static void * APR_THREAD_FUNC start_threads(apr_thread_t *thd, void *dummy)
 
     idle_worker_stack = worker_stack_create(pchild, ap_threads_per_child);
     if (idle_worker_stack == NULL) {
-        ap_log_error(APLOG_MARK, APLOG_ALERT, rv, ap_server_conf,
+        ap_log_error(APLOG_MARK, APLOG_ALERT|APLOG_NOERRNO, 0, ap_server_conf,
                      "worker_stack_create() failed");
         clean_child_exit(APEXIT_CHILDFATAL);
     }
