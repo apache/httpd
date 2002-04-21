2a644fbdb84e1aed37623d661229083770871d53
Cliff Woolley <jwoolley@apache.org>
2002-04-21 19:10:07 +0000

    Fix HTML 3.2 validation error: &O is not a valid entity.  Should be &amp;O
    Reported by: validator.w3.org
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@94740 13f79535-47bb-0310-9956-ffa450edef68

--- modules/generators/mod_autoindex.c
+++ modules/generators/mod_autoindex.c
@@ -1441,7 +1441,7 @@ static void emit_link(request_rec *r, const char *anchor, char column,
                       char curkey, char curdirection, 
                       const char *colargs, int nosort)
 {
-    char qvalue[9];
+    char qvalue[13];
     int reverse;
 
     if (!nosort) {
@@ -1451,10 +1451,14 @@ static void emit_link(request_rec *r, const char *anchor, char column,
         qvalue[2] = '=';
         qvalue[3] = column;
         qvalue[4] = '&';
-        qvalue[5] = 'O';
-        qvalue[6] = '=';
-        qvalue[7] = reverse ? D_DESCENDING : D_ASCENDING;
-        qvalue[8] = '\0';
+        qvalue[5] = 'a';
+        qvalue[6] = 'm';
+        qvalue[7] = 'p';
+        qvalue[8] = ';';
+        qvalue[9] = 'O';
+        qvalue[10] = '=';
+        qvalue[11] = reverse ? D_DESCENDING : D_ASCENDING;
+        qvalue[12] = '\0';
         ap_rvputs(r, "<a href=\"", qvalue, colargs ? colargs : "", 
                      "\">", anchor, "</a>", NULL);
     }
