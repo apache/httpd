c3595b4eaf9b4767c2ace922cc05e4d69ea79ff7
Jim Jagielski <jim@apache.org>
2002-04-22 12:36:18 +0000

    For supported versions of Darwin, place dynamically loaded Apache extensions' public symbols into the global symbol table. This allows dynamically loaded PHP extensions. NOTE: this works as long as multiple modules don't include overlapping symbols. Jim added the wrapper to check for the existance of the option (which doesn't exist in Rhapsody).
    PR:
    Obtained from:
    Submitted by:	Marko Karppinen <markonen@php.net>
    Reviewed by:	Jim Jagielski, Wilfredo Sanchez
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@94753 13f79535-47bb-0310-9956-ffa450edef68

--- src/os/unix/os.c
+++ src/os/unix/os.c
@@ -111,7 +111,11 @@ void *ap_os_dso_load(const char *path)
     if (NSCreateObjectFileImageFromFile(path, &image) !=
         NSObjectFileImageSuccess)
         return NULL;
+#if defined(NSLINKMODULE_OPTION_NONE)
+    handle = NSLinkModule(image, path, NSLINKMODULE_OPTION_NONE);
+#else
     handle = NSLinkModule(image, path, TRUE);
+#endif
     NSDestroyObjectFileImage(image);
     return handle;
 
