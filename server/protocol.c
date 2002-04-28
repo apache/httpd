f8ead96f43927a74e2424911f0e9ed6989cd3aae
Justin Erenkrantz <jerenkrantz@apache.org>
2002-04-28 06:24:53 +0000

    Simplify this little chunk of code since r is set to f->r earlier.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@94831 13f79535-47bb-0310-9956-ffa450edef68

--- server/protocol.c
+++ server/protocol.c
@@ -1145,8 +1145,7 @@ AP_CORE_DECLARE_NONSTD(apr_status_t) ap_content_length_filter(ap_filter_t *f,
      * We should be able to force connection close from this filter
      * when we see we are buffering too much.
      */
-    if ((r->proto_num >= HTTP_VERSION(1, 1))
-        || (!f->r->connection->keepalive)) {
+    if ((r->proto_num >= HTTP_VERSION(1, 1)) || (!r->connection->keepalive)) {
         partial_send_okay = 1;
     }
 
