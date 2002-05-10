bc900091983972e028191ee8268653ea5ce5c427
Brian Pane <brianp@apache.org>
2002-05-10 15:08:59 +0000

    Moved ap_setup_make_content_type() declaration to http_protocol.h
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@95031 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -3851,8 +3851,6 @@ static apr_status_t core_output_filter(ap_filter_t *f, apr_bucket_brigade *b)
     return APR_SUCCESS;
 }
 
-AP_DECLARE(void) ap_setup_make_content_type(apr_pool_t *pool);
-
 static int core_post_config(apr_pool_t *pconf, apr_pool_t *plog, apr_pool_t *ptemp, server_rec *s)
 {
     ap_set_version(pconf);
