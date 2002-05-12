f55b544f3825a688b67b1f8b5533b0d596d5b0b4
Brian Pane <brianp@apache.org>
2002-05-12 16:55:41 +0000

    Fixed missing null-termination of ap_make_content_type() patterns list
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@95047 13f79535-47bb-0310-9956-ffa450edef68

--- server/protocol.c
+++ server/protocol.c
@@ -129,6 +129,7 @@ AP_DECLARE(void) ap_setup_make_content_type(apr_pool_t *pool)
     for (i = 0; needcset[i]; i++) {
         needcset_patterns[i] = apr_strmatch_precompile(pool, needcset[i], 0);
     }
+    needcset_patterns[i] = NULL;
     charset_pattern = apr_strmatch_precompile(pool, "charset=", 0);
 }
 
