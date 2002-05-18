3d9c16b61ad8e0a032c642b2e3390963c74a1626
Cliff Woolley <jwoolley@apache.org>
2002-05-18 06:27:06 +0000

    There's really no reason for a -dev here.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@95168 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -109,12 +109,12 @@
    **        is now treated as fatal.
    **      Contributed by Aaron Bannert, April 24, 2002
    **
-   ** Version 2.0.36-dev2
+   ** Version 2.0.36-2
    **	  Internalized the version string - this string is part
    **     of the Agent: header and the result output.
    **
    **/
-#define AP_AB_BASEREVISION "2.0.36-dev2"		/* Note: this version
+#define AP_AB_BASEREVISION "2.0.36-2"		/* Note: this version
 			string should start with \d+[\d\.]* and be a valid
 			string for an HTTP Agent: header when prefixed with
 			'ApacheBench/'. 
@@ -1329,14 +1329,14 @@ static void test(void)
 static void copyright(void)
 {
     if (!use_html) {
-	printf("This is ApacheBench, Version %s\n", AP_AB_BASEREVISION " <$Revision: 1.100 $> apache-2.0");
+	printf("This is ApacheBench, Version %s\n", AP_AB_BASEREVISION " <$Revision: 1.101 $> apache-2.0");
 	printf("Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/\n");
 	printf("Copyright (c) 1998-2002 The Apache Software Foundation, http://www.apache.org/\n");
 	printf("\n");
     }
     else {
 	printf("<p>\n");
-	printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AP_AB_BASEREVISION, "$Revision: 1.100 $");
+	printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AP_AB_BASEREVISION, "$Revision: 1.101 $");
 	printf(" Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/<br>\n");
 	printf(" Copyright (c) 1998-2002 The Apache Software Foundation, http://www.apache.org/<br>\n");
 	printf("</p>\n<p>\n");
