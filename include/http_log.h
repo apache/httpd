7897b1d57451894e4c99da441462b368a7f3324f
Bill Stoddard <stoddard@apache.org>
2002-06-04 19:01:05 +0000

    Update comments for Stas Bekman
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@95506 13f79535-47bb-0310-9956-ffa450edef68

--- include/http_log.h
+++ include/http_log.h
@@ -107,8 +107,17 @@ extern "C" {
  */
 #define APLOG_NOERRNO		(APLOG_LEVELMASK + 1)
 
-/* Use APLOG_TOCLIENT to cause ap_log_rerror() to send the message
- * to the client in addition to recording it to the error log.
+/* Use APLOG_TOCLIENT on ap_log_rerror() to give content
+ * handlers the option of including the error text in the 
+ * ErrorDocument sent back to the client. Setting APLOG_TOCLIENT
+ * will cause the error text to be saved in the request_rec->notes 
+ * table, keyed to the string "error-notes", iff:
+ * - the severity level of the message is APLOG_WARNING or greater
+ * - there are no other "error-notes" set in request_rec->notes
+ * Once error-notes is set, it is up to the content handler to
+ * determine whether this text should be send back to the client.
+ * Note: Client generated text streams sent back to the  client MUST 
+ * be escaped to prevent CSS attacks.
  */
 #define APLOG_TOCLIENT          (APLOG_LEVELMASK + 2)
 
