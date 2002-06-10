4c133221e098462bf8a105dd08acb27bc3b17727
Jeff Trawick <trawick@apache.org>
2002-06-10 16:43:52 +0000

    Fix the display of the default name for the mime types config file.
    PR:           9729
    Submitted by: Matthew Brecknell <mbrecknell@orchestream.com>
    Reviewed by:  Jeff Trawick
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@95591 13f79535-47bb-0310-9956-ffa450edef68

--- server/main.c
+++ server/main.c
@@ -230,8 +230,8 @@ static void show_compile_settings(void)
     printf(" -D DEFAULT_ERRORLOG=\"" DEFAULT_ERRORLOG "\"\n");
 #endif
 
-#ifdef TYPES_CONFIG_FILE
-    printf(" -D TYPES_CONFIG_FILE=\"" TYPES_CONFIG_FILE "\"\n");
+#ifdef AP_TYPES_CONFIG_FILE
+    printf(" -D AP_TYPES_CONFIG_FILE=\"" AP_TYPES_CONFIG_FILE "\"\n");
 #endif
 
 #ifdef SERVER_CONFIG_FILE
