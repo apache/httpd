e347dc4ec0fd604fe42d1ffa35dbadaba0dbede2
Cliff Woolley <jwoolley@apache.org>
2002-06-24 07:17:36 +0000

    Include apr_lib.h for the definition of apr_toupper() to get rid of undefined symbol link failures (apr_toupper is a macro, at least on Unix).
    Reported by:  Trawick's regression build
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@95878 13f79535-47bb-0310-9956-ffa450edef68

--- modules/experimental/mod_case_filter.c
+++ modules/experimental/mod_case_filter.c
@@ -2,9 +2,10 @@
 
 #include "httpd.h"
 #include "http_config.h"
-#include "apr_general.h"
-#include "util_filter.h"
 #include "apr_buckets.h"
+#include "apr_general.h"
+#include "apr_lib.h"
+#include "util_filter.h"
 #include "http_request.h"
 
 #include <ctype.h>
