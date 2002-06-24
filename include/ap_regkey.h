14111fd4cad3589491fd86e7b82b32ba7d19531f
William A. Rowe Jr <wrowe@apache.org>
2002-06-24 04:50:41 +0000

    Introduce win32 registry handling through a very apr-ish interface.   Deals with apr utf-8 encoding of filesystem values, so we preserve   strings in any locality or nationalized flavor of WinNT, and avoid   the gunk for Win9x.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@95875 13f79535-47bb-0310-9956-ffa450edef68

---
+++ include/ap_regkey.h
@@ -0,0 +1,160 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * Portions of this software are based upon public domain software
+ * originally written at the National Center for Supercomputing Applications,
+ * University of Illinois, Urbana-Champaign.
+ */
+
+#ifndef AP_REGKEY_H
+#define AP_REGKEY_H
+
+#include "apr.h"
+#include "apr_pools.h"
+#include "ap_config.h"      /* Just for AP_DECLARE */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct ap_regkey_t ap_regkey_t;
+
+AP_DECLARE(const ap_regkey_t *) ap_regkey_const(int i);
+
+/**
+ * Constants for ap_regkey_open() only;
+ */
+#define AP_REGKEY_CLASSES_ROOT         ap_regkey_const(0)
+#define AP_REGKEY_CURRENT_CONFIG       ap_regkey_const(1)
+#define AP_REGKEY_CURRENT_USER         ap_regkey_const(2)
+#define AP_REGKEY_LOCAL_MACHINE        ap_regkey_const(3)
+#define AP_REGKEY_USERS                ap_regkey_const(4)
+#define AP_REGKEY_PERFORMANCE_DATA     ap_regkey_const(5)
+#define AP_REGKEY_DYN_DATA             ap_regkey_const(6)
+
+/**
+ * Open the specified win32 registry key.
+ * @param newkey The opened registry key
+ * @param parentkey The open registry key of the parent, or one of
+ * <PRE>
+ *           AP_REGKEY_CLASSES_ROOT
+ *           AP_REGKEY_CURRENT_CONFIG
+ *           AP_REGKEY_CURRENT_USER
+ *           AP_REGKEY_LOCAL_MACHINE
+ *           AP_REGKEY_USERS
+ *           AP_REGKEY_PERFORMANCE_DATA 
+ *           AP_REGKEY_DYN_DATA 
+ * </PRE>
+ * @param keyname The path of the key relative to the parent key
+ * @param flags Or'ed value of:
+ * <PRE>
+ *           APR_READ             open key for reading
+ *           APR_WRITE            open key for writing
+ *           APR_CREATE           create the key if it doesn't exist
+ *           APR_EXCL             return error if APR_CREATE and file exists
+ * </PRE>
+ * @param pool The pool to alloc newkey and temporary storage within.
+ * @ingroup apr_file_open
+ * @remark If perm is APR_OS_DEFAULT and the file is being created, appropriate 
+ *      default permissions will be used.  *arg1 must point to a valid file_t, 
+ *      or NULL (in which case it will be allocated)
+ */
+AP_DECLARE(apr_status_t) ap_regkey_open(ap_regkey_t **newkey, 
+                                        const ap_regkey_t *parentkey,
+                                        const char *keyname,
+                                        apr_int32_t flags, 
+                                        apr_pool_t *pool);
+
+/**
+ * Close the win32 registry key opened or created by ap_regkey_open().
+ * @param key The registry key to close
+ */
+AP_DECLARE(apr_status_t) ap_regkey_close(ap_regkey_t *key);
+
+/**
+ * Remove the given win32 registry key.
+ * @param parentkey The open registry key of the parent, or one of
+ * <PRE>
+ *           AP_REGKEY_CLASSES_ROOT
+ *           AP_REGKEY_CURRENT_CONFIG
+ *           AP_REGKEY_CURRENT_USER
+ *           AP_REGKEY_LOCAL_MACHINE
+ *           AP_REGKEY_USERS
+ *           AP_REGKEY_PERFORMANCE_DATA 
+ *           AP_REGKEY_DYN_DATA 
+ * </PRE>
+ * @param keyname The path of the key relative to the parent key
+ * @param pool The pool to alloc newkey and temporary storage within.
+ * @remark ap_regkey_remove() is not recursive, although it removes
+ * all values within the given keyname.
+ */
+AP_DECLARE(apr_status_t) ap_regkey_remove(const ap_regkey_t *parent, 
+                                          const char *keyname,
+                                          apr_pool_t *pool);
+
+/**
+ * Retrieve a win32 registry value string from an open key.
+ * @param result The string value retrieved 
+ * @param key The registry key to retrieve the value from
+ * @param valuename The named value to retrieve (pass "" for the default)
+ * @param pool The pool used to store the result.
+ */
+AP_DECLARE(apr_status_t) ap_regkey_value_get(char **result, 
+                                             ap_regkey_t *key, 
+                                             const char *valuename, 
+                                             apr_pool_t *pool);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* AP_REGKEY_H */
---
+++ os/win32/ap_regkey.c
@@ -0,0 +1,323 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * Portions of this software are based upon public domain software
+ * originally written at the National Center for Supercomputing Applications,
+ * University of Illinois, Urbana-Champaign.
+ */
+
+#ifdef WIN32
+
+#include "ap_regkey.h"
+#include "arch/win32/fileio.h"
+#include "arch/win32/misc.h"
+
+struct ap_regkey_t {
+    apr_pool_t *pool;
+    HKEY        hkey;
+};
+
+
+AP_DECLARE(const ap_regkey_t *) ap_regkey_const(int i)
+{
+    static struct ap_regkey_t ap_regkey_consts[7] = 
+    {
+        {NULL, HKEY_CLASSES_ROOT},
+        {NULL, HKEY_CURRENT_CONFIG},
+        {NULL, HKEY_CURRENT_USER},
+        {NULL, HKEY_LOCAL_MACHINE},
+        {NULL, HKEY_USERS},
+        {NULL, HKEY_PERFORMANCE_DATA},
+        {NULL, HKEY_DYN_DATA}
+    };
+    return ap_regkey_consts + i;
+}
+
+
+apr_status_t regkey_cleanup(void *key)
+{
+    ap_regkey_t *regkey = key;
+
+    if (regkey->hkey && regkey->hkey != INVALID_HANDLE_VALUE) {
+        RegCloseKey(regkey->hkey);
+        regkey->hkey = INVALID_HANDLE_VALUE;
+    }
+    return APR_SUCCESS;
+}
+
+
+AP_DECLARE(apr_status_t) ap_regkey_open(ap_regkey_t **newkey, 
+                                        const ap_regkey_t *parentkey,
+                                        const char *keyname,
+                                        apr_int32_t flags, 
+                                        apr_pool_t *pool)
+{
+    DWORD access = KEY_QUERY_VALUE;
+    DWORD exists;
+    HKEY hkey;
+    LONG rc;
+
+    if (flags & APR_READ)
+        access |= KEY_READ;
+    if (flags & APR_WRITE)
+        access |= KEY_WRITE; 
+
+#if APR_HAS_UNICODE_FS
+    IF_WIN_OS_IS_UNICODE 
+    {
+        apr_size_t keylen = strlen(keyname) + 1;
+        apr_size_t wkeylen = 256;
+        apr_wchar_t wkeyname[256];
+        apr_status_t rv = apr_conv_utf8_to_ucs2(keyname, &keylen, wkeyname, &wkeylen);
+        if (rv != APR_SUCCESS)
+            return rv;
+        else if (keylen)
+            return APR_ENAMETOOLONG;
+
+        if (flags & APR_CREATE)
+            rc = RegCreateKeyExW(parentkey->hkey, wkeyname, 0, NULL, 0, 
+                                 access, NULL, &hkey, &exists);
+        else
+            rc = RegOpenKeyExW(parentkey->hkey, wkeyname, 0, access, &hkey);
+    }
+#endif /* APR_HAS_UNICODE_FS */
+#if APR_HAS_ANSI_FS
+    ELSE_WIN_OS_IS_ANSI
+    {
+        if (flags & APR_CREATE)
+            rc = RegCreateKeyEx(parentkey->hkey, keyname, 0, NULL, 0, 
+                                access, NULL, &hkey, &exists);
+        else
+            rc = RegOpenKeyEx(parentkey->hkey, keyname, 0, access, &hkey);
+    }
+#endif
+    if (rc != ERROR_SUCCESS) {
+        return APR_FROM_OS_ERROR(rc);
+    }
+    if ((flags & APR_EXCL) && (exists == REG_OPENED_EXISTING_KEY)) {
+        RegCloseKey(hkey);
+        return APR_EEXIST;
+    }
+
+    *newkey = apr_palloc(pool, sizeof(**newkey));
+    (*newkey)->pool = pool;
+    (*newkey)->hkey = hkey;
+    apr_pool_cleanup_register((*newkey)->pool, (void *)(*newkey), 
+                              regkey_cleanup, apr_pool_cleanup_null);
+    return APR_SUCCESS;
+}
+
+
+AP_DECLARE(apr_status_t) ap_regkey_close(ap_regkey_t *regkey)
+{
+    apr_status_t stat;
+    if ((stat = regkey_cleanup(regkey)) == APR_SUCCESS) {
+        apr_pool_cleanup_kill(regkey->pool, regkey, regkey_cleanup);
+    }
+    return stat;
+}
+
+
+AP_DECLARE(apr_status_t) ap_regkey_remove(const ap_regkey_t *parent, 
+                                          const char *keyname,
+                                          apr_pool_t *pool)
+{
+    LONG rc;
+
+#if APR_HAS_UNICODE_FS
+    IF_WIN_OS_IS_UNICODE 
+    {
+        apr_size_t keylen = strlen(keyname) + 1;
+        apr_size_t wkeylen = 256;
+        apr_wchar_t wkeyname[256];
+        apr_status_t rv = apr_conv_utf8_to_ucs2(keyname, &keylen, wkeyname, &wkeylen);
+        if (rv != APR_SUCCESS)
+            return rv;
+        else if (keylen)
+            return APR_ENAMETOOLONG;
+        rc = RegDeleteKeyW(parent->hkey, wkeyname);
+    }
+#endif /* APR_HAS_UNICODE_FS */
+#if APR_HAS_ANSI_FS
+    ELSE_WIN_OS_IS_ANSI
+    {
+        /* We need to determine if subkeys exist on Win9x, to provide
+         * consistent behavior with NT, which returns access denied
+         * if subkeys exist when attempting to delete a key.
+         */
+        DWORD subkeys;
+        HKEY hkey;
+        rc = RegOpenKeyEx(parent->hkey, keyname, 0, KEY_READ, &hkey);
+        if (rc != ERROR_SUCCESS)
+            return APR_FROM_OS_ERROR(rc);
+        rc = RegQueryInfoKey(hkey, NULL, NULL, NULL, &subkeys, NULL, NULL,
+                             NULL, NULL, NULL, NULL, NULL);
+        RegCloseKey(hkey);
+        if (rc != ERROR_SUCCESS)
+            return APR_FROM_OS_ERROR(rc);
+        else if (subkeys)
+            return APR_FROM_OS_ERROR(ERROR_ACCESS_DENIED);
+        rc = RegDeleteKey(parent->hkey, keyname);
+    }
+#endif
+    if (rc != ERROR_SUCCESS) {
+        return APR_FROM_OS_ERROR(rc);
+    }
+    return APR_SUCCESS;
+}
+
+
+AP_DECLARE(apr_status_t) ap_regkey_value_get(char **result, 
+                                             ap_regkey_t *key, 
+                                             const char *valuename, 
+                                             apr_pool_t *pool)
+{
+    /* Retrieve a registry string value, and explode any envvars
+     * that the system has configured (e.g. %SystemRoot%/someapp.exe)
+     */
+    LONG rc;
+    DWORD type;
+    DWORD size = 0;
+    
+#if APR_HAS_UNICODE_FS
+    IF_WIN_OS_IS_UNICODE 
+    {
+        apr_size_t valuelen = strlen(valuename) + 1;
+        apr_size_t wvallen = 256;
+        apr_wchar_t wvalname[256];
+        apr_wchar_t *wvalue;
+        apr_status_t rv;
+        rv = apr_conv_utf8_to_ucs2(valuename, &valuelen, wvalname, &wvallen);
+        if (rv != APR_SUCCESS)
+            return rv;
+        else if (valuelen)
+            return APR_ENAMETOOLONG;
+        /* Read to NULL buffer to determine value size */
+        rc = RegQueryValueExW(key->hkey, wvalname, 0, &type, NULL, &size);
+        if (rc != ERROR_SUCCESS) {
+            return APR_FROM_OS_ERROR(rc);
+        }
+        if ((size < 4) || (type != REG_SZ && type != REG_EXPAND_SZ)) {
+            return APR_FROM_OS_ERROR(ERROR_INVALID_PARAMETER);
+        }
+
+        wvalue = apr_palloc(pool, size);
+        /* Read value based on size query above */
+        rc = RegQueryValueExW(key->hkey, wvalname, 0, &type, 
+                              (LPBYTE)wvalue, &size);
+        if (rc != ERROR_SUCCESS) {
+            return APR_FROM_OS_ERROR(rc);
+        }
+        if (type == REG_EXPAND_SZ) {
+            apr_wchar_t zbuf[1];
+            size = ExpandEnvironmentStringsW(wvalue, zbuf, 0);
+            if (size) {
+                apr_wchar_t *tmp = wvalue;
+                /* The size returned by ExpandEnvironmentStringsW is wchars */
+                wvalue = apr_palloc(pool, size * 2);
+                size = ExpandEnvironmentStringsW(tmp, wvalue, size);
+            }
+        }
+        else {
+            /* count wchars from RegQueryValueExW, rather than bytes */
+            size /= 2;
+        }
+        /* ###: deliberately overallocate all but the trailing null.
+         * We could precalculate the exact buffer here instead, the question
+         * is a matter of storage v.s. cpu cycles.
+         */
+        valuelen = (size - 1) * 3 + 1;
+        *result = apr_palloc(pool, valuelen);
+        rv = apr_conv_ucs2_to_utf8(wvalue, &size, *result, &valuelen);
+        if (rv != APR_SUCCESS)
+            return rv;
+        else if (size)
+            return APR_ENAMETOOLONG;
+    }
+#endif /* APR_HAS_UNICODE_FS */
+#if APR_HAS_ANSI_FS
+    ELSE_WIN_OS_IS_ANSI
+    {
+        /* Read to NULL buffer to determine value size */
+        rc = RegQueryValueEx(key->hkey, valuename, 0, &type, NULL, &size);
+        if (rc != ERROR_SUCCESS)
+            return APR_FROM_OS_ERROR(rc);
+
+        if ((size < 2) || (type != REG_SZ && type != REG_EXPAND_SZ)) {
+            return APR_FROM_OS_ERROR(ERROR_INVALID_PARAMETER);
+        }
+
+        *result = apr_palloc(pool, size);
+        /* Read value based on size query above */
+        rc = RegQueryValueEx(key->hkey, valuename, 0, &type, *result, &size);
+        if (rc != ERROR_SUCCESS)
+            return APR_FROM_OS_ERROR(rc);
+
+        if (type == REG_EXPAND_SZ) {
+            /* Advise ExpandEnvironmentStrings that we have a zero char
+             * buffer to force computation of the required length.
+             */
+            char zbuf[1];
+            size = ExpandEnvironmentStrings(*result, zbuf, 0);
+            if (size) {
+                char *tmp = *result;
+                *result = apr_palloc(pool, size);
+                size = ExpandEnvironmentStrings(tmp, *result, size);
+            }
+        }
+    }
+#endif
+    return APR_SUCCESS;
+}
+
+#endif /* defined WIN32 */
