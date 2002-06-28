e68ebfdc72e204833dd50cccdbf25fd115b4e597
William A. Rowe Jr <wrowe@apache.org>
2002-06-28 16:08:22 +0000

    Prevent us from getting stuck in a NULL provider hash.   Reported by: Sebastian Bergmann
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@95908 13f79535-47bb-0310-9956-ffa450edef68

--- modules/dav/main/providers.c
+++ modules/dav/main/providers.c
@@ -80,5 +80,10 @@ DAV_DECLARE(void) dav_register_provider(apr_pool_t *p, const char *name,
 
 const dav_provider * dav_lookup_provider(const char *name)
 {
+    /* Better watch out against no registered providers */
+    if (dav_repos_providers == NULL) {
+        return NULL;
+    }
+
     return apr_hash_get(dav_repos_providers, name, APR_HASH_KEY_STRING);
 }
