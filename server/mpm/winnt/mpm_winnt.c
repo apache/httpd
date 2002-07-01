d2fe0d14c591cf5ee41f31b4d4bc7420be14d270
William A. Rowe Jr <wrowe@apache.org>
2002-07-01 20:51:00 +0000

    The pitfalls of cut'n'paste
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@95935 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/winnt/mpm_winnt.c
+++ server/mpm/winnt/mpm_winnt.c
@@ -1638,13 +1638,14 @@ static int create_process(apr_pool_t *p, HANDLE *child_proc, HANDLE *child_exit_
     else {
         rv = apr_file_open_stderr(&child_err, ptemp);
     }
-    if (((rv) != APR_SUCCESS)
-            || ((rv = apr_procattr_child_out_set(attr, child_err, NULL))
-                    != APR_SUCCESS)) {
-        ap_log_error(APLOG_MARK, APLOG_CRIT, rv, ap_server_conf,
-                        "Parent: Unable to connect child stderr to log.\n");
-        apr_pool_destroy(ptemp);
-        return -1;
+    if (rv == APR_SUCCESS) {
+        if ((rv = apr_procattr_child_err_set(attr, child_err, NULL))
+                != APR_SUCCESS) {
+            ap_log_error(APLOG_MARK, APLOG_CRIT, rv, ap_server_conf,
+                            "Parent: Unable to connect child stderr.\n");
+            apr_pool_destroy(ptemp);
+            return -1;
+        }
     }
 
     /* Create the child_exit_event */
