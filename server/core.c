8c86d4bf8803e43e1e50c666398fd04599ce57b7
Brian Pane <brianp@apache.org>
2002-07-02 21:40:13 +0000

    Optimized the addition of the core output filter
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@95941 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -4061,7 +4061,7 @@ static int core_pre_connection(conn_rec *c, void *csd)
 
     ap_set_module_config(net->c->conn_config, &core_module, csd);
     ap_add_input_filter_handle(ap_core_input_filter_handle, net, NULL, net->c);
-    ap_add_output_filter("CORE", net, NULL, net->c);
+    ap_add_output_filter_handle(ap_core_output_filter_handle, net, NULL, net->c);
     return DONE;
 }
 
