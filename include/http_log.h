9cc51b2356fa3c51f8cdd2ba45f5cfd43e868f5a
William A. Rowe Jr <wrowe@apache.org>
2002-07-11 06:14:30 +0000

    Fix code comments
    Submitted by:	Stas Bekman <stas@stason.org>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@96007 13f79535-47bb-0310-9956-ffa450edef68

--- include/http_log.h
+++ include/http_log.h
@@ -111,12 +111,12 @@ extern "C" {
  * handlers the option of including the error text in the 
  * ErrorDocument sent back to the client. Setting APLOG_TOCLIENT
  * will cause the error text to be saved in the request_rec->notes 
- * table, keyed to the string "error-notes", iff:
+ * table, keyed to the string "error-notes", if and only if:
  * - the severity level of the message is APLOG_WARNING or greater
  * - there are no other "error-notes" set in request_rec->notes
  * Once error-notes is set, it is up to the content handler to
- * determine whether this text should be send back to the client.
- * Note: Client generated text streams sent back to the  client MUST 
+ * determine whether this text should be sent back to the client.
+ * Note: Client generated text streams sent back to the client MUST 
  * be escaped to prevent CSS attacks.
  */
 #define APLOG_TOCLIENT          (APLOG_LEVELMASK + 2)
