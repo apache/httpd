9e376949dbbbf64987c8e7212bd0280e41aca5ab
William A. Rowe Jr <wrowe@apache.org>
2002-07-15 08:18:50 +0000

    How isn't this built on Win32?  Thanks Sander.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@96060 13f79535-47bb-0310-9956-ffa450edef68

--- server/rfc1413.c
+++ server/rfc1413.c
@@ -147,8 +147,7 @@ static apr_status_t rfc1413_connect(apr_socket_t **newsock, conn_rec *conn,
         return rv;
     }
 
-    if ((rv = apr_socket_timeout_set(*newsock, APR_SO_TIMEOUT, 
-                                     apr_time_from_sec(ap_rfc1413_timeout)))
+    if ((rv = apr_socket_timeout_set(*newsock, apr_time_from_sec(ap_rfc1413_timeout)))
             != APR_SUCCESS) {
         ap_log_error(APLOG_MARK, APLOG_CRIT, rv, srv,
                      "rfc1413: error setting query socket timeout");
