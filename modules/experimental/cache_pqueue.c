210eb96dfc22163e22a5e9bd19425fee5f895ed5
Cliff Woolley <jwoolley@apache.org>
2002-08-14 00:46:50 +0000

    make this more gdb-friendly.  no functional change.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@96371 13f79535-47bb-0310-9956-ffa450edef68

--- modules/experimental/cache_pqueue.c
+++ modules/experimental/cache_pqueue.c
@@ -304,13 +304,21 @@ void cache_pq_print(cache_pqueue_t *q,
 
 static int cache_pq_subtree_is_valid(cache_pqueue_t *q, int pos)
 {
-    if ((left(pos) <q->size &&(q->pri(q->d[pos]) < q->pri(q->d[left(pos)]))) ||
-        (right(pos)<q->size &&(q->pri(q->d[pos]) < q->pri(q->d[right(pos)]))))
-    {
-        return 0;
+    if (left(pos) < q->size) {
+        /* has a left child */
+        if (q->pri(q->d[pos]) < q->pri(q->d[left(pos)]))
+            return 0;
+        if (!cache_pq_subtree_is_valid(q, left(pos)))
+            return 0;
     }
-    return ((left(pos)>=q->size ||(cache_pq_subtree_is_valid(q, left(pos)))) &&
-            (right(pos)>=q->size||(cache_pq_subtree_is_valid(q, right(pos)))));
+    if (right(pos) < q->size) {
+        /* has a right child */
+        if (q->pri(q->d[pos]) < q->pri(q->d[right(pos)]))
+            return 0;
+        if (!cache_pq_subtree_is_valid(q, right(pos)))
+            return 0;
+    }
+    return 1;
 }
 
 int cache_pq_is_valid(cache_pqueue_t *q)
