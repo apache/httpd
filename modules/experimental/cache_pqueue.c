767363e87b489cbbc12c89c76f2fc4d63e88b146
Cliff Woolley <jwoolley@apache.org>
2002-08-14 01:24:16 +0000

    on second thought, it actually *is* possible for this to bubble up rather than down, strange as that may seem.  this function still needs further testing, but this is better than what was there a minute ago.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@96372 13f79535-47bb-0310-9956-ffa450edef68

--- modules/experimental/cache_pqueue.c
+++ modules/experimental/cache_pqueue.c
@@ -223,7 +223,10 @@ apr_status_t cache_pq_remove(cache_pqueue_t *q, void *d)
 {
     apr_ssize_t posn = q->get(d);
     q->d[posn] = q->d[--q->size];
-    cache_pq_percolate_down(q, posn);
+    if (q->pri(q->d[posn]) > q->pri(d))
+        cache_pq_bubble_up(q, posn);
+    else
+        cache_pq_percolate_down(q, posn);
 
     return APR_SUCCESS;
 }
