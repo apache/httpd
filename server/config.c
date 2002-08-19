66a56483b0efb602c58bef7904e2186d78fe7fab
Ian Holsman <ianh@apache.org>
2002-08-19 20:40:08 +0000

    Make module loading error messages more informative Bug #11213 Obtained from: Ian Darwin <ian779@darwinsys.com>
    Reviewed by:   Ian Holsman
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@96449 13f79535-47bb-0310-9956-ffa450edef68

--- server/config.c
+++ server/config.c
@@ -451,7 +451,9 @@ AP_DECLARE(void) ap_add_module(module *m, apr_pool_t *p)
     if (m->version != MODULE_MAGIC_NUMBER_MAJOR) {
         ap_log_error(APLOG_MARK, APLOG_STARTUP, 0, NULL,
                      "%s: module \"%s\" is not compatible with this "
-                     "version of Apache.", ap_server_argv0, m->name);
+                     "version of Apache (found %d, need %d).",
+                     ap_server_argv0, m->name, m->version,
+                     MODULE_MAGIC_NUMBER_MAJOR);
         ap_log_error(APLOG_MARK, APLOG_STARTUP, 0, NULL,
                      "Please contact the vendor for the correct version.");
         exit(1);
