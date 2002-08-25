1ef12234c2086452df6d1601c4891ed9ae501883
Ian Holsman <ianh@apache.org>
2002-08-25 20:40:11 +0000

    make the output XHTML Submitted by : Erik Abele <erik@codefaktor.de>
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@96528 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/proxy_util.c
+++ modules/proxy/proxy_util.c
@@ -612,13 +612,13 @@ PROXY_DECLARE(int) ap_proxyerror(request_rec *r, int statuscode, const char *mes
     apr_table_setn(r->notes, "error-notes",
 	apr_pstrcat(r->pool, 
 		"The proxy server could not handle the request "
-		"<EM><A HREF=\"", ap_escape_uri(r->pool, r->uri),
+		"<em><a href=\"", ap_escape_uri(r->pool, r->uri),
 		"\">", ap_escape_html(r->pool, r->method),
 		"&nbsp;", 
-		ap_escape_html(r->pool, r->uri), "</A></EM>.<P>\n"
-		"Reason: <STRONG>",
+		ap_escape_html(r->pool, r->uri), "</a></em>.<p>\n"
+		"Reason: <strong>",
 		ap_escape_html(r->pool, message), 
-		"</STRONG>", NULL));
+		"</strong></p>", NULL));
 
     /* Allow "error-notes" string to be printed by ap_send_error_response() */
     apr_table_setn(r->notes, "verbose-error-to", apr_pstrdup(r->pool, "*"));
