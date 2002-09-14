47c8319d1f3d491469b3b575341508f1f2157ba1
Brian Pane <brianp@apache.org>
2002-09-14 04:51:55 +0000

    Remove the setting of the status line from the cache in mod_disk_cache, because this was causing 200s to be sent on responses that were really 304s Submitted by: Kris Verbeeck <Kris.Verbeeck@ubizen.com> Reviewed by:   Brian Pane
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@96803 13f79535-47bb-0310-9956-ffa450edef68

--- modules/experimental/mod_disk_cache.c
+++ modules/experimental/mod_disk_cache.c
@@ -484,16 +484,14 @@ static apr_status_t read_headers(cache_handle_t *h, request_rec *r)
 
     r->status = atoi(urlbuff);                           /* Save status line into request rec  */
 
-    rv = apr_file_gets(&urlbuff[0], urllen, dobj->hfd);               /* Read status line */
+    /* Read and ignore the status line (This request might result in a
+     * 304, so we don't necessarily want to retransmit a 200 from the cache.)
+     */
+    rv = apr_file_gets(&urlbuff[0], urllen, dobj->hfd);
     if (rv != APR_SUCCESS) {
         /* XXX log message */
 	return rv;
     }
-
-    if ((temp = strchr(&urlbuff[0], '\n')) != NULL)       /* trim off new line character */
-	*temp = '\0';              /* overlay it with the null terminator */
-
-    r->status_line = apr_pstrdup(r->pool, urlbuff);            /* Save status line into request rec  */
 
     h->req_hdrs = apr_table_make(r->pool, 20);
     
