e01dcce3774cecbadff9a2044441ef21e51bb2d3
William A. Rowe Jr <wrowe@apache.org>
2002-09-15 21:34:08 +0000

    We log this failure (with an identical message) in the restart loop code,   there's no reason not to note it in the preflight pass.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@96827 13f79535-47bb-0310-9956-ffa450edef68

--- server/main.c
+++ server/main.c
@@ -581,11 +581,9 @@ int main(int argc, const char * const argv[])
 
     apr_pool_clear(plog);
 
-    /* It is assumed that if you are going to fail the open_logs phase, then
-     * you will print out your own message that explains what has gone wrong.
-     * The server doesn't need to do that for you.
-     */
     if ( ap_run_open_logs(pconf, plog, ptemp, server_conf) != OK) {
+        ap_log_error(APLOG_MARK, APLOG_STARTUP |APLOG_ERR,
+                     0, NULL, "Unable to open logs\n");
         destroy_and_exit_process(process, 1);
     }
 
@@ -651,7 +649,7 @@ int main(int argc, const char * const argv[])
     apr_pool_lock(pconf, 0);
     destroy_and_exit_process(process, 0);
 
-    return 0; /* Supress compiler warning. */
+    return 0; /* Termination 'ok' */
 }
 
 /* force Expat to be linked into the server executable */
