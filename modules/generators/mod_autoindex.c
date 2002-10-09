c375c529b7bbed13f1a2d2e026e5e717e27ef515
Sander Striker <striker@apache.org>
2002-10-09 09:50:09 +0000

    One line fix to get HTMLTable to validate.
    PR: 13351
    Submitted by: Thom May <thommay@apache.org>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@97158 13f79535-47bb-0310-9956-ffa450edef68

--- modules/generators/mod_autoindex.c
+++ modules/generators/mod_autoindex.c
@@ -1523,7 +1523,7 @@ static void output_directories(struct ent **ar, int n,
         int cols = 1;
         ap_rputs("<table><tr>", r);
         if (!(autoindex_opts & SUPPRESS_ICON)) {
-            ap_rputs("<th title=\"Icon\">", r);
+            ap_rputs("<th>", r);
             if ((tp = find_default_icon(d, "^^BLANKICON^^"))) {
                 ap_rvputs(r, "<img src=\"", ap_escape_html(scratch, tp),
                              "\" alt=\"[ICO]\"", NULL);
