71cec63e048d51939f2aedf4b66d4b02e0a5e141
William A. Rowe Jr <wrowe@apache.org>
2002-10-14 00:12:02 +0000

    A couple of observations
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@97199 13f79535-47bb-0310-9956-ffa450edef68

--- modules/arch/netware/mod_nw_ssl.c
+++ modules/arch/netware/mod_nw_ssl.c
@@ -147,6 +147,9 @@ static unsigned long parse_addr(const char *w, unsigned short *ports)
     hep = gethostbyname(w);
 
     if ((!hep) || (hep->h_addrtype != AF_INET || !hep->h_addr_list[0])) {
+        /* XXX Should be echoing by r_errno the actual failure, no? 
+         * ap_log_error would be good here.
+         */
         fprintf(stderr, "Cannot resolve host name %s --- exiting!\n", w);
         exit(1);
     }
