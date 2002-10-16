f93a452c356775f5b65f9b94a1dda14c52985d26
Jeff Trawick <trawick@apache.org>
2002-10-16 18:03:44 +0000

    don't use deprecated function apr_sort_hooks()
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@97242 13f79535-47bb-0310-9956-ffa450edef68

--- server/main.c
+++ server/main.c
@@ -572,7 +572,7 @@ int main(int argc, const char * const argv[])
     ap_process_config_tree(server_conf, ap_conftree, process->pconf, ptemp);
     ap_fixup_virtual_hosts(pconf, server_conf);
     ap_fini_vhost_config(pconf, server_conf);
-    apr_sort_hooks();
+    apr_hook_sort_all();
     if (configtestonly) {
         ap_log_error(APLOG_MARK, APLOG_STARTUP, 0, NULL, "Syntax OK");
         destroy_and_exit_process(process, 0);
@@ -629,7 +629,7 @@ int main(int argc, const char * const argv[])
         ap_process_config_tree(server_conf, ap_conftree, process->pconf, ptemp);
         ap_fixup_virtual_hosts(pconf, server_conf);
         ap_fini_vhost_config(pconf, server_conf);
-        apr_sort_hooks();
+        apr_hook_sort_all();
         apr_pool_clear(plog);
         if (ap_run_open_logs(pconf, plog, ptemp, server_conf) != OK) {
             ap_log_error(APLOG_MARK, APLOG_STARTUP |APLOG_ERR,
