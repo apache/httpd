3387b1741739c4b940be8c4d2a46ddf981473e2d
Justin Erenkrantz <jerenkrantz@apache.org>
2002-11-01 18:01:27 +0000

    Use APR_STATUS_IS_EOF rather than a check against APR_EOF
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@97378 13f79535-47bb-0310-9956-ffa450edef68

--- server/util.c
+++ server/util.c
@@ -884,7 +884,7 @@ static void *cfg_getstr(void *buf, size_t bufsiz, void *param)
     apr_file_t *cfp = (apr_file_t *) param;
     apr_status_t rv;
     rv = apr_file_gets(buf, bufsiz, cfp);
-    if (rv == APR_SUCCESS || (rv == APR_EOF && strcmp(buf, "")))
+    if (rv == APR_SUCCESS || (APR_STATUS_IS_EOF(rv) && strcmp(buf, "")))
         return buf;
     return NULL;
 }
