7e015fe4a5cb9d42c5fadcc1357a9d3338971b7c
William A. Rowe Jr <wrowe@apache.org>
2002-11-23 17:57:26 +0000

    Just 2c and fix my old borked comment.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@97605 13f79535-47bb-0310-9956-ffa450edef68

--- modules/arch/netware/mod_nw_ssl.c
+++ modules/arch/netware/mod_nw_ssl.c
@@ -147,8 +147,8 @@ static unsigned long parse_addr(const char *w, unsigned short *ports)
     hep = gethostbyname(w);
 
     if ((!hep) || (hep->h_addrtype != AF_INET || !hep->h_addr_list[0])) {
-        /* XXX Should be echoing by r_errno the actual failure, no? 
-         * ap_log_error would be good here.
+        /* XXX Should be echoing by h_errno the actual failure, no? 
+         * ap_log_error would be good here.  Better yet - APRize.
          */
         fprintf(stderr, "Cannot resolve host name %s --- exiting!\n", w);
         exit(1);
