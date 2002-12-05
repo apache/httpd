8ac676ac7f01e3ce965df1e1b71d0e84c94ffb4c
Brian Pane <brianp@apache.org>
2002-12-05 03:49:45 +0000

    mod_proxy: Don't remove the Content-Length from responses
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@97777 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/proxy_http.c
+++ modules/proxy/proxy_http.c
@@ -907,16 +907,12 @@ apr_status_t ap_proxy_http_process_response(apr_pool_t * p, request_rec *r,
 
             /* We need to copy the output headers and treat them as input
              * headers as well.  BUT, we need to do this before we remove
-             * TE and C-L, so that they are preserved accordingly for
+             * TE, so that they are preserved accordingly for
              * ap_http_filter to know where to end.
              */
             rp->headers_in = apr_table_copy(r->pool, r->headers_out);
 
-            /* In order for ap_set_keepalive to work properly, we can NOT
-             * have any length information stored in the output headers.
-             */
             apr_table_unset(r->headers_out,"Transfer-Encoding");
-            apr_table_unset(r->headers_out,"Content-Length");
 
             ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, r->server,
                          "proxy: start body send");
