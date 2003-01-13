8803963b140825e7e38a495a122ae99c99ddf493
William A. Rowe Jr <wrowe@apache.org>
2003-01-13 17:10:42 +0000

    After introducing tests in the cmds, we lose the absolute authority   of the CRYPTO_malloc_init() which must happen the moment we load the   module and prior to *any* ssl library fn invocation.
      Moved the CRYPTO_malloc_init() into the ssl_register_hooks() function,
      the absolute first call made into any loaded module.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@98252 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_init.c
+++ modules/ssl/ssl_engine_init.c
@@ -114,7 +114,6 @@ static void ssl_init_SSLLibrary(server_rec *s)
     ap_log_error(APLOG_MARK, APLOG_INFO, 0, s,
                  "Init: Initializing %s library", SSL_LIBRARY_NAME);
 
-    CRYPTO_malloc_init();
     SSL_load_error_strings();
     SSL_library_init();
 }
