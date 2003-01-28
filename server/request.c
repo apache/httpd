53b508b387c45f1d1689546335ee673cfaf43083
André Malo <nd@apache.org>
2003-01-28 15:57:38 +0000

    Fix "Satisfy Any" logic. The access_checker has nothing to do with auth_type.
    Related PR: 9076
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@98516 13f79535-47bb-0310-9956-ffa450edef68

--- server/request.c
+++ server/request.c
@@ -251,13 +251,9 @@ AP_DECLARE(int) ap_process_request_internal(request_rec *r)
             break;
 
         case SATISFY_ANY:
-            if (((access_status = ap_run_access_checker(r)) != 0)
-                || !ap_auth_type(r)) {
+            if (((access_status = ap_run_access_checker(r)) != 0)) {
                 if (!ap_some_auth_required(r)) {
-                    return decl_die(access_status, ap_auth_type(r)
-                                  ? "check access"
-                                  : "perform authentication. AuthType not set!",
-                                  r);
+                    return decl_die(access_status, "check access", r);
                 }
 
                 if (((access_status = ap_run_check_user_id(r)) != 0)
