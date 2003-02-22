452c3280c3e3c88d8a99316857a745fd99456a97
André Malo <nd@apache.org>
2003-02-22 18:38:13 +0000

    Hook mod_proxy's fixup before mod_rewrite's fixup, so that by mod_rewrite proxied URLs will not be escaped accidentally by mod_proxy's fixup.
    PR: 16368
    Reviewed by: Justin Erenkrantz, Jeff Trawick, Jim Jagielski
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/APACHE_2_0_BRANCH@98763 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_rewrite.c
+++ modules/mappers/mod_rewrite.c
@@ -4357,6 +4357,11 @@ static const command_rec command_table[] = {
 
 static void register_hooks(apr_pool_t *p)
 {
+    /* fixup after mod_proxy, so that the proxied url will not
+     * escaped accidentally by mod_proxy's fixup.
+     */
+    static const char * const aszPre[]={ "mod_proxy.c", NULL };
+
     APR_REGISTER_OPTIONAL_FN(ap_register_rewrite_mapfunc);
 
     ap_hook_handler(handler_redirect, NULL, NULL, APR_HOOK_MIDDLE);
@@ -4364,7 +4369,7 @@ static void register_hooks(apr_pool_t *p)
     ap_hook_post_config(post_config, NULL, NULL, APR_HOOK_MIDDLE);
     ap_hook_child_init(init_child, NULL, NULL, APR_HOOK_MIDDLE);
 
-    ap_hook_fixups(hook_fixup, NULL, NULL, APR_HOOK_FIRST);
+    ap_hook_fixups(hook_fixup, aszPre, NULL, APR_HOOK_FIRST);
     ap_hook_translate_name(hook_uri2file, NULL, NULL, APR_HOOK_FIRST);
     ap_hook_type_checker(hook_mimetype, NULL, NULL, APR_HOOK_MIDDLE);
 }
