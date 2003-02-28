13ea325bd863f55138d4c0914816c75b7d6837e6
Jeff Trawick <trawick@apache.org>
2003-02-28 14:02:42 +0000

    fix some license text which was inadvertently lost
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@98843 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/winnt/child.c
+++ server/mpm/winnt/child.c
@@ -13,7 +13,7 @@
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or  materials provided with the
+ *    the documentation and/or other materials provided with the
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution,
