d07c669012ecf130c7d7066cc1a14c5db1feb1a9
William A. Rowe Jr <wrowe@apache.org>
2003-03-06 08:44:01 +0000

    DougM confirms Madhu's suspicions, this change was inadvertent.   Reverting to no longer skip the first cert in the chain.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/APACHE_2_0_BRANCH@98897 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_init.c
+++ modules/ssl/ssl_engine_init.c
@@ -654,7 +654,7 @@ static void ssl_init_ctx_cert_chain(server_rec *s,
                                     apr_pool_t *ptemp,
                                     modssl_ctx_t *mctx)
 {
-    BOOL skip_first = TRUE;
+    BOOL skip_first = FALSE;
     int i, n;
     const char *chain = mctx->cert_chain;
 
