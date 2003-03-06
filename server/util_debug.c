d4afb99c56a8f32f6e9c505fa5f3ce9d9b3c7f94
Sander Striker <striker@apache.org>
2003-03-06 17:56:05 +0000

    * include/http_config.h
      (ap_get_module_config, ap_set_module_config): Always declare.
    
    
    * include/httpd.h
    
      (ap_strchr, ap_strchr_c, ap_strrchr, ap_strrchr_c, ap_strstr, ap_strstr_c):
        Always declare.
    
    
    * server/util_debug.c
    
      (ap_get_module_config, ap_set_module_config, ap_strchr, ap_strchr_c,
       ap_strrchr, ap_strrchr_c, ap_strstr, ap_strstr_c):
        Always implement and export.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@98903 13f79535-47bb-0310-9956-ffa450edef68

--- server/util_debug.c
+++ server/util_debug.c
@@ -62,38 +62,85 @@
 #include "httpd.h"
 #include "http_config.h"
 
-#ifdef AP_DEBUG
-
-/* get rid of the macros we defined in httpd.h */
+/* Possibly get rid of the macros we defined in httpd.h */
+#if defined(strchr)
 #undef strchr
+#endif
+
+#if defined (strrchr)
 #undef strrchr
+#endif
+
+#if defined (strstr)
 #undef strstr
+#endif
+
+
+#if defined(ap_strchr)
+#undef ap_strchr
+AP_DECLARE(char *) ap_strchr(char *s, int c);
+#endif
 
 AP_DECLARE(char *) ap_strchr(char *s, int c)
 {
     return strchr(s,c);
 }
+
+#if defined(ap_strchr_c)
+#undef ap_strchr_c
+AP_DECLARE(const char *) ap_strchr_c(const char *s, int c);
+#endif
+
 AP_DECLARE(const char *) ap_strchr_c(const char *s, int c)
 {
     return strchr(s,c);
 }
+
+#if defined(ap_strrchr)
+#undef ap_strrchr
+AP_DECLARE(char *) ap_strrchr(char *s, int c);
+#endif
+
 AP_DECLARE(char *) ap_strrchr(char *s, int c)
 {
     return strrchr(s,c);
 }
+
+#if defined(ap_strrchr_c)
+#undef ap_strrchr_c
+AP_DECLARE(const char *) ap_strrchr_c(const char *s, int c);
+#endif
+
 AP_DECLARE(const char *) ap_strrchr_c(const char *s, int c)
 {
     return strrchr(s,c);
 }
+
+#if defined(ap_strstr)
+#undef ap_strstr
+AP_DECLARE(char *) ap_strstr(char *s, const char *c);
+#endif
+
 AP_DECLARE(char *) ap_strstr(char *s, const char *c)
 {
     return strstr(s,c);
 }
+
+#if defined(ap_strstr_c)
+#undef ap_strstr_c
+AP_DECLARE(const char *) ap_strstr_c(const char *s, const char *c);
+#endif
+
 AP_DECLARE(const char *) ap_strstr_c(const char *s, const char *c)
 {
     return strstr(s,c);
 }
 
+#if defined(ap_get_module_config)
+#undef ap_get_module_config
+AP_DECLARE(void *) ap_get_module_config(const ap_conf_vector_t *cv,
+                                        const module *m);
+#endif
 
 AP_DECLARE(void *) ap_get_module_config(const ap_conf_vector_t *cv,
                                         const module *m)
@@ -110,11 +157,14 @@ AP_DECLARE(void *) ap_get_module_config(const ap_conf_vector_t *cv,
  * @param val The module-specific data to set
  * @deffunc void ap_set_module_config(ap_conf_vector_t *cv, const module *m, void *val)
  */
+#if defined(ap_set_module_config)
+#undef ap_set_module_config
+AP_DECLARE(void) ap_set_module_config(ap_conf_vector_t *cv, const module *m,
+                                      void *val);
+#endif
+
 AP_DECLARE(void) ap_set_module_config(ap_conf_vector_t *cv, const module *m,
                                       void *val)
 {
     ((void **)cv)[m->module_index] = val;
 }
-
-
-#endif /* AP_DEBUG */
