cecbae314bd38105f5d66cfb45ada450dd17b533
Bradley Nicholes <bnicholes@apache.org>
2003-03-26 19:21:32 +0000

    Need to report a select() error vs a timeout.  Also avoid hogging the CPU on an error.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@99065 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/netware/mpm_netware.c
+++ server/mpm/netware/mpm_netware.c
@@ -418,8 +418,14 @@ void worker_main(void *arg)
             memcpy(&main_fds, &listenfds, sizeof(fd_set));
             srv = select(listenmaxfd + 1, &main_fds, NULL, NULL, &tv);
 
-            if (srv <= 0)
+            if (srv <= 0) {
+                if (srv < 0) {
+                    ap_log_error(APLOG_MARK, APLOG_NOTICE, apr_get_netos_error(), ap_server_conf,
+                        "select() failed on listen socket");
+                    apr_thread_yield();
+                }
                 continue;
+            }
 
             /* remember the last_lr we searched last time around so that
             we don't end up starving any particular listening socket */
