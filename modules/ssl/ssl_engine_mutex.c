e56645ae603230253af113dc586ef684109e9209
William A. Rowe Jr <wrowe@apache.org>
2003-03-31 07:24:34 +0000

    Must fix - Win32 can't deal with this code, and we can no longer fix     sysv mutexes with an APR_USE_ test... we have to look at the choice!
      Reviewed by:	Brian Pane, Justin Erenkrantz
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@99146 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_mutex.c
+++ modules/ssl/ssl_engine_mutex.c
@@ -88,16 +88,25 @@ int ssl_mutex_init(server_rec *s, apr_pool_t *p)
                          "Cannot create SSLMutex");
         return FALSE;
     }
+#if APR_HAS_FLOCK_SERIALIZE
     if (mc->szMutexFile && mc->ChownMutexFile == TRUE)
         chown(mc->szMutexFile, unixd_config.user_id, -1);
+#endif
 
+#if APR_HAS_SYSVSEM_SERIALIZE
 #if APR_USE_SYSVSEM_SERIALIZE
-    rv = unixd_set_global_mutex_perms(mc->pMutex);
-    if (rv != APR_SUCCESS) {
-        ap_log_error(APLOG_MARK, APLOG_ERR, rv, s,
-                     "Could not set permissions on ssl_mutex; check User "
-                     "and Group directives");
-        return FALSE;
+    if (mc->nMutexMech == APR_LOCK_DEFAULT || 
+        mc->nMutexMech == APR_LOCK_SYSVSEM) {
+#else
+    if (mc->nMutexMech == APR_LOCK_SYSVSEM) {
+#endif
+        rv = unixd_set_global_mutex_perms(mc->pMutex);
+        if (rv != APR_SUCCESS) {
+            ap_log_error(APLOG_MARK, APLOG_ERR, rv, s,
+                         "Could not set permissions on ssl_mutex; check User "
+                         "and Group directives");
+            return FALSE;
+        }
     }
 #endif
     return TRUE;
