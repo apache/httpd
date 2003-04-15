66a299ac6706d35a641201ab757cca91407374d3
Graham Leggett <minfrin@apache.org>
2003-04-15 16:07:45 +0000

    Fixed a segfault when multiple ProxyBlock directives were used. PR:		19023 Obtained from: Submitted by:	Sami Tikka <sami.tikka@f-secure.com> Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/APACHE_2_0_BRANCH@99364 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/proxy_util.c
+++ modules/proxy/proxy_util.c
@@ -1015,10 +1015,12 @@ PROXY_DECLARE(int) ap_proxy_checkproxyblock(request_rec *r, proxy_server_conf *c
                              apr_sockaddr_t *uri_addr)
 {
     int j;
+    apr_sockaddr_t * src_uri_addr = uri_addr;
     /* XXX FIXME: conf->noproxies->elts is part of an opaque structure */
     for (j = 0; j < conf->noproxies->nelts; j++) {
         struct noproxy_entry *npent = (struct noproxy_entry *) conf->noproxies->elts;
         struct apr_sockaddr_t *conf_addr = npent[j].addr;
+        uri_addr = src_uri_addr;
         ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, r->server,
                      "proxy: checking remote machine [%s] against [%s]", uri_addr->hostname, npent[j].name);
         if ((npent[j].name && ap_strstr_c(uri_addr->hostname, npent[j].name))
