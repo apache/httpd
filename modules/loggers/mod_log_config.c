95ebd5235bf92b9f867ae16891ac8339585318b0
Jeff Trawick <trawick@apache.org>
2003-04-19 03:06:30 +0000

    as Andre' pointed out, we don't need %{pid/tid}P since the user can do %P/%{tid}P or %{pid}P/%{tid}P
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@99449 13f79535-47bb-0310-9956-ffa450edef68

--- modules/loggers/mod_log_config.c
+++ modules/loggers/mod_log_config.c
@@ -616,25 +616,18 @@ static const char *log_server_name(request_rec *r, char *a)
 static const char *log_pid_tid(request_rec *r, char *a)
 {
     if (*a == '\0' || !strcmp(a, "pid")) {
-        return apr_psprintf(r->pool, "%ld", (long)getpid());
+        return apr_psprintf(r->pool, "%" APR_PID_T_FMT, getpid());
     }
-    else {
+    else if (!strcmp(a, "tid")) {
 #if APR_HAS_THREADS
         apr_os_thread_t tid = apr_os_thread_current();
 #else
         int tid = 0; /* APR will format "0" anyway but an arg is needed */
 #endif
-
-        if (!strcmp(a, "pid/tid")) {
-            return apr_psprintf(r->pool, "%ld/%pT", (long)getpid(), &tid);
-        }
-        else if (!strcmp(a, "tid")) {
-            return apr_psprintf(r->pool, "%pT", &tid);
-        }
-        else { /* bogus string */
-            return a;
-        }
+        return apr_psprintf(r->pool, "%pT", &tid);
     }
+    /* bogus format */
+    return a;
 }
 
 static const char *log_connection_status(request_rec *r, char *a)
