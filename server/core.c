e686a699c164cb84c226a392ccbbd7cb08b12656
Jeff Trawick <trawick@apache.org>
2003-05-13 14:22:45 +0000

    fix a bogus check for IPv6 support that led to an unnecessary string search on builds of Apache without IPv6 support
    Submitted by:	Bill Stoddard
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@99776 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -894,7 +894,7 @@ static const char *get_server_name_for_url(request_rec *r)
 {
     const char *plain_server_name = ap_get_server_name(r);
 
-#ifdef APR_HAVE_IPV6
+#if APR_HAVE_IPV6
     if (ap_strchr_c(plain_server_name, ':')) { /* IPv6 literal? */
         return apr_psprintf(r->pool, "[%s]", plain_server_name);
     }
