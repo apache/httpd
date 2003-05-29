8dd8543053a9f8949aed580d36dc67a7b7e78999
André Malo <nd@apache.org>
2003-05-29 23:04:32 +0000

    Rework of the recursion stopper - collapse recursion counters into one function
    Reviewed by: Justin Erenkrantz
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@100095 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -2661,22 +2661,26 @@ static const char *set_recursion_limit(cmd_parms *cmd, void *dummy,
     return NULL;
 }
 
-static void log_backtrace(const request_rec *top, const request_rec *r)
+static void log_backtrace(const request_rec *r)
 {
-    while (top) {
-        ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
-                      "redirected from r->uri = %s",
-                      top->uri ? top->uri : "(unexpectedly NULL)");
+    const request_rec *top = r;
+
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
+                  "r->uri = %s", r->uri ? r->uri : "(unexpectedly NULL)");
+
+    while (top && (top->prev || top->main)) {
+        if (top->prev) {
+            top = top->prev;
+            ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
+                          "redirected from r->uri = %s",
+                          top->uri ? top->uri : "(unexpectedly NULL)");
+        }
 
         if (!top->prev && top->main) {
             top = top->main;
-
             ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
                           "subrequested from r->uri = %s",
                           top->uri ? top->uri : "(unexpectedly NULL)");
-        }
-        else {
-            top = top->prev;
         }
     }
 }
@@ -2684,79 +2688,63 @@ static void log_backtrace(const request_rec *top, const request_rec *r)
 /*
  * check whether redirect limit is reached
  */
-AP_DECLARE(int) ap_is_redirect_limit_exceeded(const request_rec *r)
+AP_DECLARE(int) ap_is_recursion_limit_exceeded(const request_rec *r)
 {
     core_server_config *conf = ap_get_module_config(r->server->module_config,
                                                     &core_module);
     const request_rec *top = r;
-    int redirects = 0;
-    int limit = conf->redirect_limit
-                ? conf->redirect_limit
-                : AP_DEFAULT_MAX_INTERNAL_REDIRECTS;
+    int redirects = 0, subreqs = 0;
+    int rlimit = conf->redirect_limit
+                 ? conf->redirect_limit
+                 : AP_DEFAULT_MAX_INTERNAL_REDIRECTS;
+    int slimit = conf->subreq_limit
+                 ? conf->subreq_limit
+                 : AP_DEFAULT_MAX_SUBREQ_DEPTH;
 
-    while (top->prev) {
-        if (++redirects >= limit) {
-            /* uuh, too much. */
-            ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
-                          "Request exceeded the limit of %d internal redirects "
-                          "due to probable configuration error. Use "
-                          "'LimitInternalRecursion' to increase the limit if "
-                          "necessary. Use 'LogLevel debug' to get a "
-                          "backtrace.", limit);
 
-            /* post backtrace */
-            log_backtrace(r->prev, r);
+    while (top->prev || top->main) {
+        if (top->prev) {
+            if (++redirects >= rlimit) {
+                /* uuh, too much. */
+                ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+                              "Request exceeded the limit of %d internal "
+                              "redirects due to probable configuration error. "
+                              "Use 'LimitInternalRecursion' to increase the "
+                              "limit if necessary. Use 'LogLevel debug' to get "
+                              "a backtrace.", rlimit);
 
-            /* return failure */
-            return 1;
+                /* post backtrace */
+                log_backtrace(r);
+
+                /* return failure */
+                return 1;
+            }
+
+            top = top->prev;
         }
 
         if (!top->prev && top->main) {
+            if (++subreqs >= slimit) {
+                /* uuh, too much. */
+                ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+                              "Request exceeded the limit of %d subrequest "
+                              "nesting levels due to probable confguration "
+                              "error. Use 'LimitInternalRecursion' to increase "
+                              "the limit if necessary. Use 'LogLevel debug' to "
+                              "get a backtrace.", slimit);
+
+                /* post backtrace */
+                log_backtrace(r);
+
+                /* return failure */
+                return 1;
+            }
+
             top = top->main;
         }
-        else {
-            top = top->prev;
-        }
     }
 
-    /* number of redirects is ok */
-    return 0;
-}
-
-/*
- * check whether subrequest depth limit is reached
- */
-AP_DECLARE(int) ap_is_subreq_limit_exceeded(const request_rec *r)
-{
-    core_server_config *conf = ap_get_module_config(r->server->module_config,
-                                                    &core_module);
-    const request_rec *top = r;
-    int subreqs = 0;
-    int limit = conf->subreq_limit
-                ? conf->subreq_limit
-                : AP_DEFAULT_MAX_SUBREQ_DEPTH;
-
-    while (top->main) {
-        if (++subreqs >= limit) {
-            /* uuh, too much. */
-            ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
-                          "Request exceeded the limit of %d subrequest "
-                          "nesting levels due to probable confguration error. "
-                          "Use 'LimitInternalRecursion' to increase the limit "
-                          "if necessary. Use 'LogLevel debug' to get a "
-                          "backtrace.", limit);
-
-            /* post backtrace */
-            log_backtrace(r->main, r);
-
-            /* return failure */
-            return 1;
-        }
-
-        top = top->main;
-    }
-
-    /* number of subrequests is ok */
+    /* recursion state: ok */
     return 0;
 }
 
