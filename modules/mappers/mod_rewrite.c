dd0522a5e583eeccb926d8c9a4484e59e905e2e3
Jeff Trawick <trawick@apache.org>
2003-06-07 13:09:10 +0000

    mod_rewrite: Perform child initialization on the rewrite log lock. This fixes a log corruption issue when flock-based serialization is used (e.g., FreeBSD).
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@100187 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_rewrite.c
+++ modules/mappers/mod_rewrite.c
@@ -1072,6 +1072,12 @@ static void init_child(apr_pool_t *p, server_rec *s)
         }
     }
 
+    rv = apr_global_mutex_child_init(&rewrite_log_lock, NULL, p);
+    if (rv != APR_SUCCESS) {
+        ap_log_error(APLOG_MARK, APLOG_CRIT, rv, s,
+                     "mod_rewrite: could not init rewrite log lock in child");
+    }
+    
     /* create the lookup cache */
     cachep = init_cache(p);
 }
