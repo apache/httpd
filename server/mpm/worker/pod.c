f6f3d2804bbd2f2e81d13ef6003bac4127e533c6
Joe Orton <jorton@apache.org>
2003-07-10 20:11:15 +0000

    Minor cleanup:
    * pod.c (pod_signal_internal): apr_file_write() is defined to
    never return EINTR, so don't check for that case.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@100541 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/worker/pod.c
+++ server/mpm/worker/pod.c
@@ -127,9 +127,7 @@ static apr_status_t pod_signal_internal(ap_pod_t *pod, int graceful)
     char char_of_death = graceful ? GRACEFUL_CHAR : RESTART_CHAR;
     apr_size_t one = 1;
 
-    do {
-        rv = apr_file_write(pod->pod_out, &char_of_death, &one);
-    } while (APR_STATUS_IS_EINTR(rv));
+    rv = apr_file_write(pod->pod_out, &char_of_death, &one);
     if (rv != APR_SUCCESS) {
         ap_log_error(APLOG_MARK, APLOG_WARNING, rv, ap_server_conf,
                      "write pipe_of_death");
