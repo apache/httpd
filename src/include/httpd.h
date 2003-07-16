cfa0e52545c84b6c4ea6c92c7a1d05034182a587
Jim Jagielski <jim@apache.org>
2003-07-16 20:27:08 +0000

    And we are up to 1.3.29-dev
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@100662 13f79535-47bb-0310-9956-ffa450edef68

--- src/include/httpd.h
+++ src/include/httpd.h
@@ -428,7 +428,7 @@ extern "C" {
 
 #define SERVER_BASEVENDOR   "Apache Group"
 #define SERVER_BASEPRODUCT  "Apache"
-#define SERVER_BASEREVISION "1.3.28"
+#define SERVER_BASEREVISION "1.3.29-dev"
 #define SERVER_BASEVERSION  SERVER_BASEPRODUCT "/" SERVER_BASEREVISION
 
 #define SERVER_PRODUCT  SERVER_BASEPRODUCT
@@ -449,7 +449,7 @@ API_EXPORT(const char *) ap_get_server_built(void);
  * Always increases along the same track as the source branch.
  * For example, Apache 1.4.2 would be '10402100', 2.5b7 would be '20500007'.
  */
-#define APACHE_RELEASE 10328100
+#define APACHE_RELEASE 10329000
 
 #define SERVER_PROTOCOL "HTTP/1.1"
 #ifndef SERVER_SUPPORT
