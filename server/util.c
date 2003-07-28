cdddc76c84678cd855e07f366a359685c2b9ca94
Jeff Trawick <trawick@apache.org>
2003-07-28 20:10:42 +0000

    axe a special check for EOF+data from apr_file_gets()...  apr_file_gets() doesn't return that combination of conditions any more
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@100826 13f79535-47bb-0310-9956-ffa450edef68

--- server/util.c
+++ server/util.c
@@ -887,8 +887,9 @@ static void *cfg_getstr(void *buf, size_t bufsiz, void *param)
     apr_file_t *cfp = (apr_file_t *) param;
     apr_status_t rv;
     rv = apr_file_gets(buf, bufsiz, cfp);
-    if (rv == APR_SUCCESS || (APR_STATUS_IS_EOF(rv) && strcmp(buf, "")))
+    if (rv == APR_SUCCESS) {
         return buf;
+    }
     return NULL;
 }
 
