1518b867395de3cf3ce57c360675ce0cdc8b65e4
André Malo <nd@apache.org>
2003-07-28 15:32:27 +0000

    allow piped rewrite logs to be relative to serverroot
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@100818 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_rewrite.c
+++ modules/mappers/mod_rewrite.c
@@ -385,8 +385,6 @@ static int open_rewritelog(server_rec *s, apr_pool_t *p)
 {
     rewrite_server_conf *conf;
     const char *fname;
-    apr_status_t rc;
-    piped_log *pl;
 
     conf = ap_get_module_config(s->module_config, &rewrite_module);
 
@@ -399,15 +397,27 @@ static int open_rewritelog(server_rec *s, apr_pool_t *p)
     }
 
     if (*conf->rewritelogfile == '|') {
-        if ((pl = ap_open_piped_log(p, conf->rewritelogfile+1)) == NULL) {
+        piped_log *pl;
+
+        fname = ap_server_root_relative(p, conf->rewritelogfile+1);
+        if (!fname) {
+            ap_log_error(APLOG_MARK, APLOG_ERR, APR_EBADPATH, s,
+                         "mod_rewrite: Invalid RewriteLog "
+                         "path %s", conf->rewritelogfile+1);
+            return 0;
+        }
+
+        if ((pl = ap_open_piped_log(p, fname)) == NULL) {
             ap_log_error(APLOG_MARK, APLOG_ERR, 0, s,
                          "mod_rewrite: could not open reliable pipe "
-                         "to RewriteLog filter %s", conf->rewritelogfile+1);
+                         "to RewriteLog filter %s", fname);
             return 0;
         }
         conf->rewritelogfp = ap_piped_log_write_fd(pl);
     }
     else {
+        apr_status_t rc;
+
         fname = ap_server_root_relative(p, conf->rewritelogfile);
         if (!fname) {
             ap_log_error(APLOG_MARK, APLOG_ERR, APR_EBADPATH, s,
@@ -415,6 +425,7 @@ static int open_rewritelog(server_rec *s, apr_pool_t *p)
                          "path %s", conf->rewritelogfile);
             return 0;
         }
+
         if ((rc = apr_file_open(&conf->rewritelogfp, fname,
                                 REWRITELOG_FLAGS, REWRITELOG_MODE, p))
                 != APR_SUCCESS) {
