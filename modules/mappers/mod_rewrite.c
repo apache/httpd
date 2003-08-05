8e072c403b8854b1b8836d9fa1d4bc1f84beb709
Andre Malo <nd@apache.org>
2003-08-05 18:45:53 +0000

    There are different kinds of people:
    - the good      English speaking people
    - the bad       English speaking people
    - and the ugly  ones, who should not even try to do so
    
    I think, I'm one of the latter group.
    Thanks to Thom May :)
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@100907 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_rewrite.c
+++ modules/mappers/mod_rewrite.c
@@ -4662,7 +4662,7 @@ static int handler_redirect(request_rec *r)
 #ifdef REWRITELOG_DISABLED
 static const char *fake_rewritelog(cmd_parms *cmd, void *dummy, const char *a1)
 {
-    return "RewriteLog and RewriteLogLevel are not support by this build "
+    return "RewriteLog and RewriteLogLevel are not supported by this build "
            "of mod_rewrite because it was compiled using the "
            "-DREWRITELOG_DISABLED compiler option. You have to recompile "
            "mod_rewrite WITHOUT this option in order to use the rewrite log.";
