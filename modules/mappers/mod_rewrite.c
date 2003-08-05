1eae6eaec03b04763dab6e8f0dba4604eb5bc818
Andre Malo <nd@apache.org>
2003-08-05 17:44:02 +0000

    word smithing / grammar
    Submitted by: Jeff Trawick
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@100906 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_rewrite.c
+++ modules/mappers/mod_rewrite.c
@@ -4662,8 +4662,8 @@ static int handler_redirect(request_rec *r)
 #ifdef REWRITELOG_DISABLED
 static const char *fake_rewritelog(cmd_parms *cmd, void *dummy, const char *a1)
 {
-    return "RewriteLog and RewriteLogLevel are not support by this incarnation "
-           "of mod_rewrite, because it was compiled using the "
+    return "RewriteLog and RewriteLogLevel are not support by this build "
+           "of mod_rewrite because it was compiled using the "
            "-DREWRITELOG_DISABLED compiler option. You have to recompile "
            "mod_rewrite WITHOUT this option in order to use the rewrite log.";
 }
