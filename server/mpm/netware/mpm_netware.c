403aa59854bdba56567c33bf3a99ad9923dffeb6
Cliff Woolley <jwoolley@apache.org>
2003-09-05 19:08:56 +0000

    forward-port Brad's commit to the 2.0 branch that gets rid of the explicit bucket_alloc_destroy(), which is redundant
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@101164 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/netware/mpm_netware.c
+++ server/mpm/netware/mpm_netware.c
@@ -212,12 +212,10 @@ static int volatile wait_to_finish=1;
 ap_generation_t volatile ap_my_generation=0;
 
 /* a clean exit from a child with proper cleanup */
-static void clean_child_exit(int code, int worker_num, apr_pool_t *ptrans, 
-                             apr_bucket_alloc_t *bucket_alloc) __attribute__ ((noreturn));
-static void clean_child_exit(int code, int worker_num, apr_pool_t *ptrans, 
-                             apr_bucket_alloc_t *bucket_alloc)
+static void clean_child_exit(int code, int worker_num, apr_pool_t *ptrans)
+                             __attribute__ ((noreturn));
+static void clean_child_exit(int code, int worker_num, apr_pool_t *ptrans)
 {
-    apr_bucket_alloc_destroy(bucket_alloc);
     if (!shutdown_pending) {
         apr_pool_destroy(ptrans);
     }
@@ -402,7 +400,7 @@ void worker_main(void *arg)
         if ((ap_max_requests_per_child > 0
             && requests_this_child++ >= ap_max_requests_per_child)) {
             DBPRINT1 ("\n**Thread slot %d is shutting down", my_worker_num);
-            clean_child_exit(0, my_worker_num, ptrans, bucket_alloc);
+            clean_child_exit(0, my_worker_num, ptrans);
         }
 
         ap_update_child_status_from_indexes(0, my_worker_num, WORKER_READY, 
@@ -415,7 +413,7 @@ void worker_main(void *arg)
         for (;;) {
             if (shutdown_pending || restart_pending || (ap_scoreboard_image->servers[0][my_worker_num].status == WORKER_IDLE_KILL)) {
                 DBPRINT1 ("\nThread slot %d is shutting down\n", my_worker_num);
-             	 clean_child_exit(0, my_worker_num, ptrans, bucket_alloc);
+             	 clean_child_exit(0, my_worker_num, ptrans);
             }
 
             /* Check the listen queue on all sockets for requests */
@@ -524,13 +522,12 @@ void worker_main(void *arg)
                         */
                         ap_log_error(APLOG_MARK, APLOG_EMERG, stat, ap_server_conf,
                             "apr_socket_accept: giving up.");
-                        clean_child_exit(APEXIT_CHILDFATAL, my_worker_num, ptrans, 
-                                         bucket_alloc);
+                        clean_child_exit(APEXIT_CHILDFATAL, my_worker_num, ptrans);
                 }
                 else {
                         ap_log_error(APLOG_MARK, APLOG_ERR, stat, ap_server_conf,
                             "apr_socket_accept: (client socket)");
-                        clean_child_exit(1, my_worker_num, ptrans, bucket_alloc);
+                        clean_child_exit(1, my_worker_num, ptrans);
                 }
             }
         }
@@ -549,7 +546,7 @@ void worker_main(void *arg)
         }
         request_count++;
     }
-    clean_child_exit(0, my_worker_num, ptrans, bucket_alloc);
+    clean_child_exit(0, my_worker_num, ptrans);
 }
 
 
