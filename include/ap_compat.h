442fffd2ee532312be761fdcc66092899aba7cb9
Sander Striker <striker@apache.org>
2003-09-06 07:48:47 +0000

    apu_compat.h no longer exists...
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@101178 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_compat.h
+++ include/ap_compat.h
@@ -1,9 +1,6 @@
 #ifndef AP_COMPAT_H
 #define AP_COMPAT_H
 
-/* Drag in apu (and therefore apr) renamed symbols */
-#include "apu_compat.h"
-
 /* redefine 1.3.x symbols to the new symbol names */
 
 #define MODULE_VAR_EXPORT    AP_MODULE_DECLARE_DATA
