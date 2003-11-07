5d3e2ff374a092250f1f59e10eb79c5e4e05e480
Bradley Nicholes <bnicholes@apache.org>
2003-11-07 01:01:27 +0000

    Allow translation of generated output such as mod_autoindex using charsets other than EBCDIC.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@101708 13f79535-47bb-0310-9956-ffa450edef68

--- modules/experimental/mod_charset_lite.c
+++ modules/experimental/mod_charset_lite.c
@@ -286,7 +286,7 @@ static int find_code_page(request_rec *r)
  */
              
     if (strncasecmp(mime_type, "text/", 5) &&
-#if APR_CHARSET_EBCDIC
+#if APR_CHARSET_EBCDIC || AP_WANT_DIR_TRANSLATION
         /* On an EBCDIC machine, be willing to translate mod_autoindex-
          * generated output.  Otherwise, it doesn't look too cool.
          *
