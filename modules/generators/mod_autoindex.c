ca8eb973c6f52dde69f0cafabe89e84c0d0ebd0f
Andre Malo <nd@apache.org>
2003-11-23 17:20:55 +0000

    restore the space which broke all tests :-)
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@101853 13f79535-47bb-0310-9956-ffa450edef68

--- modules/generators/mod_autoindex.c
+++ modules/generators/mod_autoindex.c
@@ -206,7 +206,7 @@ static void emit_preamble(request_rec *r, int xhtml, const char *title)
         ap_rvputs(r, "  <link rel=\"stylesheet\" href=\"", d->style_sheet,
                 "\" type=\"text/css\"", xhtml ? "/>\n" : ">\n", NULL);
     }
-    ap_rvputs(r, "</head>\n <body>\n", NULL);
+    ap_rvputs(r, " </head>\n <body>\n", NULL);
 }
 
 static void push_item(apr_array_header_t *arr, char *type, const char *to,
