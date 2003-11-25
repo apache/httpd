c665770e7c19e876b4a1263645f9fee5330e9cea
Joe Orton <jorton@apache.org>
2003-11-25 13:16:10 +0000

    * modules/ssl/ssl_engine_vars.c (ssl_var_log_handler_x): Fix unused variable from previous commit.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@101881 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_vars.c
+++ modules/ssl/ssl_engine_vars.c
@@ -682,7 +682,6 @@ static const char *ssl_var_log_handler_c(request_rec *r, char *a)
  */
 static const char *ssl_var_log_handler_x(request_rec *r, char *a)
 {
-    SSLConnRec *sslconn = myConnConfig(r->connection);
     char *result;
 
     result = ssl_var_lookup(r->pool, r->server, r->connection, r, a);
