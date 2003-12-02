82574ba481bb98cee7e3dc88379b736fbee76433
Joe Orton <jorton@apache.org>
2003-12-02 14:38:41 +0000

    Backport from HEAD:
    * proxy_http.c (ap_proxy_http_process_response): Send a valid
    status-line even if the parsed status-line had no trailing spaces.
    Remove the warning for this case as triggers for valid status-lines
    too.
    
    PR: 23998
    Reviewed by: Jeff Trawick, Bill Stoddard
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/APACHE_2_0_BRANCH@101954 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/proxy_http.c
+++ modules/proxy/proxy_http.c
@@ -756,16 +756,18 @@ apr_status_t ap_proxy_http_process_response(apr_pool_t * p, request_rec *r,
             backasswards = 0;
 
             keepchar = buffer[12];
-            if (keepchar == '\0') {
-                ap_log_error(APLOG_MARK, APLOG_WARNING, 0,
-                             r->server, "proxy: bad HTTP/%d.%d status line "
-                             "returned by %s (%s)", major, minor, r->uri,
-                             r->method);
-            }
             buffer[12] = '\0';
             r->status = atoi(&buffer[9]);
 
-            buffer[12] = keepchar;
+            if (keepchar != '\0') {
+                buffer[12] = keepchar;
+            } else {
+                /* 2616 requires the space in Status-Line; the origin
+                 * server may have sent one but ap_rgetline_core will
+                 * have stripped it. */
+                buffer[12] = ' ';
+                buffer[13] = '\0';
+            }
             r->status_line = apr_pstrdup(p, &buffer[9]);
             
 
