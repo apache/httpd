de2c0ccf1f3b685b8b342c2522ae1cf37e39ce9e
Joe Orton <jorton@apache.org>
2003-12-02 14:23:25 +0000

    Backport from HEAD:
    * ssl_util.c (ssl_util_thread_cleanup): Clear the id_callback.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/APACHE_2_0_BRANCH@101953 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_util.c
+++ modules/ssl/ssl_util.c
@@ -454,6 +454,7 @@ static unsigned long ssl_util_thr_id(void)
 static apr_status_t ssl_util_thread_cleanup(void *data)
 {
     CRYPTO_set_locking_callback(NULL);
+    CRYPTO_set_id_callback(NULL);
 
     /* Let the registered mutex cleanups do their own thing 
      */
