8cb01d6980a35a9d56be042b6eacefb05a412a13
Jeff Trawick <trawick@apache.org>
2003-12-04 06:31:01 +0000

    Fix a problem with the display of empty variables ("SetEnv foo") in mod_include. PR:               24734 Submitted by:	  Markus Julen <mj zermatt.net> Reviewed by:	  trawick, nd, striker
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/APACHE_2_0_BRANCH@101979 13f79535-47bb-0310-9956-ffa450edef68

--- modules/metadata/mod_env.c
+++ modules/metadata/mod_env.c
@@ -158,7 +158,7 @@ static const char *add_env_module_vars_set(cmd_parms *cmd, void *sconf_,
     /* name is mandatory, value is optional.  no value means
      * set the variable to an empty string
      */
-    apr_table_setn(sconf->vars, name, value);
+    apr_table_setn(sconf->vars, name, value ? value : "");
 
     return NULL;
 }
