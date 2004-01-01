56339011019e7ca6e8f0b6b7c14e8f61d82e7378
Andre Malo <nd@apache.org>
2004-01-01 13:26:26 +0000

    update license to 2004.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@102135 13f79535-47bb-0310-9956-ffa450edef68

--- build/ltlib.mk
+++ build/ltlib.mk
@@ -1,7 +1,7 @@
 # ====================================================================
 # The Apache Software License, Version 1.1
 #
-# Copyright (c) 2000-2003 The Apache Software Foundation.  All rights
+# Copyright (c) 2000-2004 The Apache Software Foundation.  All rights
 # reserved.
 #
 # Redistribution and use in source and binary forms, with or without
