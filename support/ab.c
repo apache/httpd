7a06ef17b23e585655131676cbe523995074acdc
Joe Orton <jorton@apache.org>
2004-01-03 15:58:19 +0000

    * support/ab.c (write_request): Fix gcc warning at -O2 in USE_SSL build.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@102155 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -735,7 +735,7 @@ static void write_request(struct connection * c)
     do {
 	apr_time_t tnow = apr_time_now();
 	apr_size_t l = c->rwrite;
-	apr_status_t e;
+	apr_status_t e = APR_SUCCESS; /* prevent gcc warning */
 
 	/*
 	 * First time round ?
@@ -1825,14 +1825,14 @@ static void test(void)
 static void copyright(void)
 {
     if (!use_html) {
-	printf("This is ApacheBench, Version %s\n", AP_AB_BASEREVISION " <$Revision: 1.133 $> apache-2.0");
+	printf("This is ApacheBench, Version %s\n", AP_AB_BASEREVISION " <$Revision: 1.134 $> apache-2.0");
 	printf("Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/\n");
 	printf("Copyright (c) 1998-2002 The Apache Software Foundation, http://www.apache.org/\n");
 	printf("\n");
     }
     else {
 	printf("<p>\n");
-	printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AP_AB_BASEREVISION, "$Revision: 1.133 $");
+	printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AP_AB_BASEREVISION, "$Revision: 1.134 $");
 	printf(" Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/<br>\n");
 	printf(" Copyright (c) 1998-2002 The Apache Software Foundation, http://www.apache.org/<br>\n");
 	printf("</p>\n<p>\n");
