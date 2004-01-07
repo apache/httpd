bc5485cae8b3bc03478081a31c39de83df85b05e
Jeff Trawick <trawick@apache.org>
2004-01-07 18:55:50 +0000

    fix mispelling
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@102214 13f79535-47bb-0310-9956-ffa450edef68

--- src/support/ab.c
+++ src/support/ab.c
@@ -958,7 +958,7 @@ static void close_connection(struct connection * c)
 	/*
 	 * server has legitimately shut down an idle keep alive request
 	 */
-	good--;			/* connection never happend */
+	good--;			/* connection never happened */
     }
     else {
 	if (good == 1) {
@@ -1358,14 +1358,14 @@ static void test(void)
 static void copyright(void)
 {
     if (!use_html) {
-	printf("This is ApacheBench, Version %s\n", VERSION " <$Revision: 1.71 $> apache-1.3");
+	printf("This is ApacheBench, Version %s\n", VERSION " <$Revision: 1.72 $> apache-1.3");
 	printf("Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/\n");
 	printf("Copyright (c) 1998-2002 The Apache Software Foundation, http://www.apache.org/\n");
 	printf("\n");
     }
     else {
 	printf("<p>\n");
-	printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-1.3<br>\n", VERSION, "$Revision: 1.71 $");
+	printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-1.3<br>\n", VERSION, "$Revision: 1.72 $");
 	printf(" Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/<br>\n");
 	printf(" Copyright (c) 1998-2002 The Apache Software Foundation, http://www.apache.org/<br>\n");
 	printf("</p>\n<p>\n");
