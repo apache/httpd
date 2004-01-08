edb3c3f70932b8c5d7367aea7b3ccf8ec90d6b5c
Joe Orton <jorton@apache.org>
2004-01-08 13:08:57 +0000

    * modules/dav/main/mod_dav.c (dav_handler): Reject request if the Request-URI includes a fragment part, i.e. an unescaped #.
    PR: 21779
    Submitted by: Amit Athavale <amit_athavale@lycos.com>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@102224 13f79535-47bb-0310-9956-ffa450edef68

--- modules/dav/main/mod_dav.c
+++ modules/dav/main/mod_dav.c
@@ -4563,6 +4563,16 @@ static int dav_handler(request_rec *r)
     if (strcmp(r->handler, DAV_HANDLER_NAME) != 0)
         return DECLINED;
 
+    /* Reject requests with an unescaped hash character, as these may
+     * be more destructive than the user intended. */
+    if (r->parsed_uri.fragment != NULL) {
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+                     "buggy client used un-escaped hash in Request-URI");
+        return dav_error_response(r, HTTP_BAD_REQUEST, 
+                                  "The request was invalid: the URI included "
+                                  "an un-escaped hash character");
+    }
+
     /* ### do we need to do anything with r->proxyreq ?? */
 
     /*
