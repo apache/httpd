866b6af8f39d6ab71096a5bd1d51f726039d410a
André Malo <nd@apache.org>
2004-01-18 00:00:18 +0000

    in conservative C declarations still have to be the first in a function.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@102361 13f79535-47bb-0310-9956-ffa450edef68

--- modules/loggers/mod_log_config.c
+++ modules/loggers/mod_log_config.c
@@ -1219,10 +1219,6 @@ static apr_status_t flush_all_logs(void *data)
 
 static int init_config_log(apr_pool_t *pc, apr_pool_t *p, apr_pool_t *pt, server_rec *s)
 {
-    if (buffered_logs) {
-        all_buffered_logs = apr_array_make(p, 5, sizeof(buffered_log *));
-    }
-
     /* First, do "physical" server, which gets default log fd and format
      * for the virtual servers, if they don't override...
      */
@@ -1232,6 +1228,11 @@ static int init_config_log(apr_pool_t *pc, apr_pool_t *p, apr_pool_t *pt, server
 
     for (s = s->next; (res == OK) && s; s = s->next) {
         res = open_multi_logs(s, p);
+    }
+
+    /* Finally init buffered logs */
+    if (buffered_logs) {
+        all_buffered_logs = apr_array_make(p, 5, sizeof(buffered_log *));
     }
 
     return res;
