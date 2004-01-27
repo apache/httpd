0086f5d4d4cfb325752c7ad854ca347ff59b6433
Jeff Trawick <trawick@apache.org>
2004-01-27 15:19:58 +0000

    Unix MPMs: Stop dropping connections when the file descriptor is at least FD_SETSIZE.
    That isn't a problem for most users (since poll() is widespread).
    On old boxes where select() must be used by APR, APR needs to fail
    operations that don't work with such fds.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@102419 13f79535-47bb-0310-9956-ffa450edef68

--- os/unix/unixd.c
+++ os/unix/unixd.c
@@ -508,15 +508,6 @@ AP_DECLARE(apr_status_t) unixd_accept(void **accepted, ap_listen_rec *lr,
     if (status == APR_SUCCESS) { 
         *accepted = csd;
         apr_os_sock_get(&sockdes, csd);
-        if (sockdes >= FD_SETSIZE) {
-            ap_log_error(APLOG_MARK, APLOG_WARNING, 0, NULL,
-                         "new file descriptor %d is too large; you probably need "
-                         "to rebuild Apache with a larger FD_SETSIZE "
-                         "(currently %d)",
-                         sockdes, FD_SETSIZE);
-            apr_socket_close(csd);
-            return APR_EINTR;
-        } 
 #ifdef TPF
         if (sockdes == 0) {                  /* 0 is invalid socket for TPF */
             return APR_EINTR;
--- server/mpm/worker/worker.c
+++ server/mpm/worker/worker.c
@@ -557,16 +557,6 @@ static void process_socket(apr_pool_t *p, apr_socket_t *sock, int my_child_num,
     ap_create_sb_handle(&sbh, p, my_child_num, my_thread_num);
     apr_os_sock_get(&csd, sock);
 
-    if (csd >= FD_SETSIZE) {
-        ap_log_error(APLOG_MARK, APLOG_WARNING, 0, NULL,
-                     "new file descriptor %d is too large; you probably need "
-                     "to rebuild Apache with a larger FD_SETSIZE "
-                     "(currently %d)", 
-                     csd, FD_SETSIZE);
-        apr_socket_close(sock);
-        return;
-    }
-
     current_conn = ap_run_create_connection(p, ap_server_conf, sock,
                                             conn_id, sbh, bucket_alloc);
     if (current_conn) {
