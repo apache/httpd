8359de94b9481809acd492b45a2a3968fb13e3dc
Paul J. Reder <rederpj@apache.org>
2004-01-30 16:20:51 +0000

    *) core.c: If large file support is enabled, allow any file that is    greater than AP_MAX_SENDFILE to be split into multiple buckets.    This allows Apache to send files that are greater than 2gig.    Otherwise we run into 32/64 bit type mismatches in the file size.
    Submitted by: Brad Nicholes
    Reviewed by: nd, jerenkrantz, rederpj
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/APACHE_2_0_BRANCH@102467 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -3522,8 +3522,12 @@ static int default_handler(request_rec *r)
         }
 
         bb = apr_brigade_create(r->pool, c->bucket_alloc);
-#if APR_HAS_SENDFILE && APR_HAS_LARGE_FILES
+#if APR_HAS_LARGE_FILES
+#if APR_HAS_SENDFILE
         if ((d->enable_sendfile != ENABLE_SENDFILE_OFF) &&
+#else
+        if (
+#endif
             (r->finfo.size > AP_MAX_SENDFILE)) {
             /* APR_HAS_LARGE_FILES issue; must split into mutiple buckets,
              * no greater than MAX(apr_size_t), and more granular than that
