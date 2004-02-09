df5e8dd7dddfe68985b6e123b9f4392b072b6025
André Malo <nd@apache.org>
2004-02-09 20:31:03 +0000

    fix name of The Apache Software Foundation
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk/modules/ssl@102618 13f79535-47bb-0310-9956-ffa450edef68

--- ssl_engine_dh.c
+++ ssl_engine_dh.c
@@ -2,7 +2,7 @@
 =pod
 #endif
 
-/* Copyright 2001-2004 Apache Software Foundation
+/* Copyright 2001-2004 The Apache Software Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
