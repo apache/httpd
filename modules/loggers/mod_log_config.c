7122d6ea3a4ec060794310a39c1e42b7be3c6edd
Jeff Trawick <trawick@apache.org>
2004-02-26 20:00:55 +0000

    a kind fix for a compile error unfortunately broke an order dependency...  the buffered logs array needs to be initialized prior to opening the logs
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@102781 13f79535-47bb-0310-9956-ffa450edef68

--- modules/loggers/mod_log_config.c
+++ modules/loggers/mod_log_config.c
@@ -1176,20 +1176,22 @@ static apr_status_t flush_all_logs(void *data)
 
 static int init_config_log(apr_pool_t *pc, apr_pool_t *p, apr_pool_t *pt, server_rec *s)
 {
-    /* First, do "physical" server, which gets default log fd and format
+    int res;
+
+    /* First init the buffered logs array, which is needed when opening the logs. */
+    if (buffered_logs) {
+        all_buffered_logs = apr_array_make(p, 5, sizeof(buffered_log *));
+    }
+
+    /* Next, do "physical" server, which gets default log fd and format
      * for the virtual servers, if they don't override...
      */
-    int res = open_multi_logs(s, p);
+    res = open_multi_logs(s, p);
 
     /* Then, virtual servers */
 
     for (s = s->next; (res == OK) && s; s = s->next) {
         res = open_multi_logs(s, p);
-    }
-
-    /* Finally init buffered logs */
-    if (buffered_logs) {
-        all_buffered_logs = apr_array_make(p, 5, sizeof(buffered_log *));
     }
 
     return res;
