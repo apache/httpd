5a427cb84d73aa9c2c1b8ce78d634c38966fcd4f
Bill Stoddard <stoddard@apache.org>
2004-02-28 19:02:24 +0000

    Win32: Shutdown the server on critical child failures that will prevent the server from operating
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@102804 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/winnt/mpm_winnt.c
+++ server/mpm/winnt/mpm_winnt.c
@@ -944,8 +944,9 @@ static int master_main(server_rec *s, HANDLE shutdown_event, HANDLE restart_even
         if (   exitcode == APEXIT_CHILDFATAL 
             || exitcode == APEXIT_CHILDINIT
             || exitcode == APEXIT_INIT) {
-            ap_log_error(APLOG_MARK, APLOG_ERR, 0, ap_server_conf, 
+            ap_log_error(APLOG_MARK, APLOG_CRIT, 0, ap_server_conf, 
                          "Parent: child process exited with status %u -- Aborting.", exitcode);
+            shutdown_pending = 1;
         }
         else {
             int i;
