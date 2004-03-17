8915b61c79ac5a865ee19e8816511128ec0f1892
Madhusudan Mathihalli <madhum@apache.org>
2004-03-17 23:16:33 +0000

    Add Missing parenthesis !
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@103015 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -1786,14 +1786,14 @@ static void test(void)
 static void copyright(void)
 {
     if (!use_html) {
-	printf("This is ApacheBench, Version %s\n", AP_AB_BASEREVISION " <$Revision: 1.140 $> apache-2.0");
+	printf("This is ApacheBench, Version %s\n", AP_AB_BASEREVISION " <$Revision: 1.141 $> apache-2.0");
 	printf("Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/\n");
 	printf("Copyright (c) 1998-2002 The Apache Software Foundation, http://www.apache.org/\n");
 	printf("\n");
     }
     else {
 	printf("<p>\n");
-	printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AP_AB_BASEREVISION, "$Revision: 1.140 $");
+	printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AP_AB_BASEREVISION, "$Revision: 1.141 $");
 	printf(" Copyright (c) 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/<br>\n");
 	printf(" Copyright (c) 1998-2002 The Apache Software Foundation, http://www.apache.org/<br>\n");
 	printf("</p>\n<p>\n");
@@ -2162,7 +2162,7 @@ int main(int argc, const char * const argv[])
     if ((concurrency < 0) || (concurrency > MAX_CONCURRENCY)) {
 	fprintf(stderr, "%s: Invalid Concurrency [Range 0..%d]\n",
                 argv[0], MAX_CONCURRENCY);
-        usage(argv[0];
+        usage(argv[0]);
     }
 
     if ((heartbeatres) && (requests > 150)) {
