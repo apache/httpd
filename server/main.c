4e49e3ea3e9b3e40ee6ac6b9db39a0072a09d445
Jeff Trawick <trawick@apache.org>
2004-03-29 02:21:29 +0000

    Don't include newlines in strings passed to ap_log_error.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@103182 13f79535-47bb-0310-9956-ffa450edef68

--- server/main.c
+++ server/main.c
@@ -568,7 +568,7 @@ int main(int argc, const char * const argv[])
     server_conf = ap_read_config(process, ptemp, confname, &ap_conftree);
     if (ap_run_pre_config(pconf, plog, ptemp) != OK) {
         ap_log_error(APLOG_MARK, APLOG_STARTUP |APLOG_ERR, 0,
-                     NULL, "Pre-configuration failed\n");
+                     NULL, "Pre-configuration failed");
         destroy_and_exit_process(process, 1);
     }
 
@@ -633,7 +633,7 @@ int main(int argc, const char * const argv[])
         server_conf = ap_read_config(process, ptemp, confname, &ap_conftree);
         if (ap_run_pre_config(pconf, plog, ptemp) != OK) {
             ap_log_error(APLOG_MARK, APLOG_STARTUP |APLOG_ERR,
-                         0, NULL, "Pre-configuration failed\n");
+                         0, NULL, "Pre-configuration failed");
             destroy_and_exit_process(process, 1);
         }
 
