ac92e64b69ca9ac3b146b2c8d6bf5dc5fb843020
Jeff Trawick <trawick@apache.org>
2004-03-30 20:53:06 +0000

    backport this from 2.1-dev:
      Don't include newlines in strings passed to ap_log_error.
    
    Reviewed by: geoff, nd
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/APACHE_2_0_BRANCH@103220 13f79535-47bb-0310-9956-ffa450edef68

--- server/main.c
+++ server/main.c
@@ -531,7 +531,7 @@ int main(int argc, const char * const argv[])
     server_conf = ap_read_config(process, ptemp, confname, &ap_conftree);
     if (ap_run_pre_config(pconf, plog, ptemp) != OK) {
         ap_log_error(APLOG_MARK, APLOG_STARTUP |APLOG_ERR, 0,
-                     NULL, "Pre-configuration failed\n");
+                     NULL, "Pre-configuration failed");
         destroy_and_exit_process(process, 1);
     }
 
@@ -588,7 +588,7 @@ int main(int argc, const char * const argv[])
         server_conf = ap_read_config(process, ptemp, confname, &ap_conftree);
         if (ap_run_pre_config(pconf, plog, ptemp) != OK) {
             ap_log_error(APLOG_MARK, APLOG_STARTUP |APLOG_ERR,
-                         0, NULL, "Pre-configuration failed\n");
+                         0, NULL, "Pre-configuration failed");
             destroy_and_exit_process(process, 1);
         }
 
