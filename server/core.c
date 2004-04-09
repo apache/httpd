3f8667a2c8dbfc59a7c48f7c1e296c76599f9144
Geoffrey Young <geoff@apache.org>
2004-04-09 00:56:26 +0000

    Enable special ErrorDocument value 'default' which restores the canned server response for the scope of the directive.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@103310 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -271,9 +271,7 @@ static void *merge_core_dir_configs(apr_pool_t *a, void *basev, void *newv)
                sizeof(*conf->response_code_strings) * RESPONSE_CODES);
 
         for (i = 0; i < RESPONSE_CODES; ++i) {
-            if (new->response_code_strings[i] != NULL) {
-                conf->response_code_strings[i] = new->response_code_strings[i];
-            }
+            conf->response_code_strings[i] = new->response_code_strings[i];
         }
     }
     /* Otherwise we simply use the base->response_code_strings array
@@ -1178,13 +1176,21 @@ static const char *set_error_document(cmd_parms *cmd, void *conf_,
                             RESPONSE_CODES);
         }
 
-        /* hack. Prefix a " if it is a msg; as that is what
-         * http_protocol.c relies on to distinguish between
-         * a msg and a (local) path.
-         */
-        conf->response_code_strings[index_number] = (what == MSG) ?
-                apr_pstrcat(cmd->pool, "\"",msg,NULL) :
-                apr_pstrdup(cmd->pool, msg);
+        if (strcmp(msg, "default") == 0) {
+            /* special case: ErrorDocument 404 default restores the
+             * canned server error response
+             */
+            conf->response_code_strings[index_number] = NULL;
+        }
+        else {
+            /* hack. Prefix a " if it is a msg; as that is what
+             * http_protocol.c relies on to distinguish between
+             * a msg and a (local) path.
+             */
+            conf->response_code_strings[index_number] = (what == MSG) ?
+                    apr_pstrcat(cmd->pool, "\"",msg,NULL) :
+                    apr_pstrdup(cmd->pool, msg);
+        }
     }
 
     return NULL;
