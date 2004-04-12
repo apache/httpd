22758568a15969fd53c2ddea1f5b912f75472abe
Bradley Nicholes <bnicholes@apache.org>
2004-04-12 21:26:26 +0000

    Make sure that if auth_ldap is loaded, mod_ldap is also loaded
    Reviewed by: bnicholes, minfrin, trawick
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/APACHE_2_0_BRANCH@103372 13f79535-47bb-0310-9956-ffa450edef68

--- modules/experimental/mod_auth_ldap.c
+++ modules/experimental/mod_auth_ldap.c
@@ -953,6 +953,15 @@ static int auth_ldap_post_config(apr_pool_t *p, apr_pool_t *plog, apr_pool_t *pt
     }
     */
 
+    /* make sure that mod_ldap (util_ldap) is loaded */
+    if (ap_find_linked_module("util_ldap.c") == NULL) {
+        ap_log_error(APLOG_MARK, APLOG_ERR|APLOG_NOERRNO, 0, s,
+                     "Module mod_ldap missing. Mod_ldap (aka. util_ldap) "
+                     "must be loaded in order for mod_auth_ldap to function properly");
+        return HTTP_INTERNAL_SERVER_ERROR;
+
+    }
+
     if (!charset_confname) {
         return OK;
     }
