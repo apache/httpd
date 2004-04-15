acc5177528600c9b77d913a58fa029c461f0de83
Joe Orton <jorton@apache.org>
2004-04-15 20:36:45 +0000

    * server/vhost.c (remove_unused_name_vhosts): Remove redundant check; catch all port-based NameVirtualHosts without defined VirtualHosts, preventing segfaults later in some misconfigurations.
    PR: 27731
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@103400 13f79535-47bb-0310-9956-ffa450edef68

--- server/vhost.c
+++ server/vhost.c
@@ -500,12 +500,6 @@ static void remove_unused_name_vhosts(server_rec *main_s, ipaddr_chain **pic)
                          ic->sar->virthost, ic->sar->host_port);
             *pic = ic->next;
         }
-        else if (ic->names == NULL) {
-            /* if server != NULL and names == NULL then we're done
-             * looking at NameVirtualHosts
-             */
-            break;
-        }
         else {
             pic = &ic->next;
         }
