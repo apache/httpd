05b665109bdb602b93c19d040ad439ba2f64565c
Andre Malo <nd@apache.org>
2004-05-25 19:39:50 +0000

    this should save one or two cycles
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@103767 13f79535-47bb-0310-9956-ffa450edef68

--- modules/loggers/mod_log_config.c
+++ modules/loggers/mod_log_config.c
@@ -393,7 +393,7 @@ static const char *log_bytes_sent(request_rec *r, char *a)
         return "0";
     }
     else {
-        return apr_psprintf(r->pool, "%" APR_OFF_T_FMT, r->bytes_sent);
+        return apr_off_t_toa(r->pool, r->bytes_sent);
     }
 }
 
