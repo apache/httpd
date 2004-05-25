1d3ce6644afbd64dbc1635f1cc29e5944fc2c5fd
Graham Leggett <minfrin@apache.org>
2004-05-25 16:18:22 +0000

    Throw an error message if an attempt is made to use the LDAPTrustedCA or LDAPTrustedCAType directives in a VirtualHost. PR:	26390 Obtained from: Submitted by: Reviewed by:	minfrin, bnicholes, jim
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/APACHE_2_0_BRANCH@103757 13f79535-47bb-0310-9956-ffa450edef68

--- modules/experimental/util_ldap.c
+++ modules/experimental/util_ldap.c
@@ -1070,6 +1070,10 @@ static const char *util_ldap_set_cert_auth(cmd_parms *cmd, void *dummy, const ch
     util_ldap_state_t *st = 
         (util_ldap_state_t *)ap_get_module_config(cmd->server->module_config, 
 						  &ldap_module);
+    const char *err = ap_check_cmd_context(cmd, GLOBAL_ONLY);
+    if (err != NULL) {
+        return err;
+    }
 
     ap_log_error(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, 0, cmd->server, 
                       "LDAP: SSL trusted certificate authority file - %s", 
@@ -1086,6 +1090,10 @@ const char *util_ldap_set_cert_type(cmd_parms *cmd, void *dummy, const char *Typ
     util_ldap_state_t *st = 
     (util_ldap_state_t *)ap_get_module_config(cmd->server->module_config, 
                                               &ldap_module);
+    const char *err = ap_check_cmd_context(cmd, GLOBAL_ONLY);
+    if (err != NULL) {
+        return err;
+    }
 
     ap_log_error(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, 0, cmd->server, 
                       "LDAP: SSL trusted certificate authority file type - %s", 
