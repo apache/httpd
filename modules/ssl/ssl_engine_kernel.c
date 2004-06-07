2767d52979fa8e7b3a31615e4a8366527b542060
Joe Orton <jorton@apache.org>
2004-06-07 10:18:37 +0000

    Backport from HEAD:
      * modules/ssl/ssl_engine_kernel.c (ssl_hook_UserCheck): Fix buffer
      overflow in FakeBasicAuth code if client's subject DN exceeds 6K in
      length (CVE CAN-2004-0488); switch to using apr-util base64 encoder
      functions.
    
      * modules/ssl/ssl_engine_init.c (ssl_init_Engine): Log the OpenSSL
      error stack contents if engine load/init fails.
    
      * modules/ssl/ssl_engine_log.c (ssl_log_ssl_error): Use %lu to print
      an unsigned long.
    
      * modules/ssl/ssl_engine_log.c (ssl_log_annotate, ssl_log_annotation,
      ssl_log_ssl_error): const-ify annotation strings and simplify
      ssl_log_annotation.
    
    Reviewed by: Andr�� Malo, Jeff Trawick
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/APACHE_2_0_BRANCH@103867 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_kernel.c
+++ modules/ssl/ssl_engine_kernel.c
@@ -793,7 +793,6 @@ int ssl_hook_UserCheck(request_rec *r)
     SSLConnRec *sslconn = myConnConfig(r->connection);
     SSLSrvConfigRec *sc = mySrvConfig(r->server);
     SSLDirConfigRec *dc = myDirConfig(r);
-    char buf1[MAX_STRING_LEN], buf2[MAX_STRING_LEN];
     char *clientdn;
     const char *auth_line, *username, *password;
 
@@ -872,14 +871,16 @@ int ssl_hook_UserCheck(request_rec *r)
      * adding the string "xxj31ZMTZzkVA" as the password in the user file.
      * This is just the crypted variant of the word "password" ;-)
      */
-    apr_snprintf(buf1, sizeof(buf1), "%s:password", clientdn);
-    ssl_util_uuencode(buf2, buf1, FALSE);
-
-    apr_snprintf(buf1, sizeof(buf1), "Basic %s", buf2);
-    apr_table_set(r->headers_in, "Authorization", buf1);
+    auth_line = apr_pstrcat(r->pool, "Basic ", 
+                            ap_pbase64encode(r->pool, 
+                                             apr_pstrcat(r->pool, clientdn, 
+                                                         ":password", NULL)),
+                            NULL);
+    apr_table_set(r->headers_in, "Authorization", auth_line);
 
     ap_log_error(APLOG_MARK, APLOG_INFO, 0, r->server,
-                 "Faking HTTP Basic Auth header: \"Authorization: %s\"", buf1);
+                 "Faking HTTP Basic Auth header: \"Authorization: %s\"",
+                 auth_line);
 
     return DECLINED;
 }
