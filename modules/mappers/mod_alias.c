c9199510512f64b76c26f7db44829d4227d37dc8
André Malo <nd@apache.org>
2004-06-14 22:09:42 +0000

    Emit a warning if an alias overlaps another.
    Reviewed by: Brad Nicholes, Jean-Jacques Clar
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/APACHE_2_0_BRANCH@103950 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_alias.c
+++ modules/mappers/mod_alias.c
@@ -94,6 +94,9 @@ static void *merge_alias_dir_config(apr_pool_t *p, void *basev, void *overridesv
     return a;
 }
 
+/* need prototype for overlap check */
+static int alias_matches(const char *uri, const char *alias_fakename);
+
 static const char *add_alias_internal(cmd_parms *cmd, void *dummy,
                                       const char *f, const char *r,
                                       int use_regex)
@@ -102,6 +105,8 @@ static const char *add_alias_internal(cmd_parms *cmd, void *dummy,
     alias_server_conf *conf = ap_get_module_config(s->module_config,
                                                    &alias_module);
     alias_entry *new = apr_array_push(conf->aliases);
+    alias_entry *entries = (alias_entry *)conf->aliases->elts;
+    int i;
 
     /* XX r can NOT be relative to DocumentRoot here... compat bug. */
 
@@ -121,6 +126,29 @@ static const char *add_alias_internal(cmd_parms *cmd, void *dummy,
     }
     new->fake = f;
     new->handler = cmd->info;
+
+    /* check for overlapping (Script)Alias directives
+     * and throw a warning if found one
+     */
+    if (!use_regex) {
+        for (i = 0; i < conf->aliases->nelts - 1; ++i) {
+            alias_entry *p = &entries[i];
+
+            if (  (!p->regexp &&  alias_matches(f, p->fake) > 0)
+                || (p->regexp && !ap_regexec(p->regexp, f, 0, NULL, 0))) {
+                ap_log_error(APLOG_MARK, APLOG_WARNING, 0, cmd->server,
+                             "The %s directive in %s at line %d will probably "
+                             "never match because it overlaps an earlier "
+                             "%sAlias%s.",
+                             cmd->cmd->name, cmd->directive->filename,
+                             cmd->directive->line_num,
+                             p->handler ? "Script" : "",
+                             p->regexp ? "Match" : "");
+
+                break; /* one warning per alias should be sufficient */
+            }
+        }
+    }
 
     return NULL;
 }
