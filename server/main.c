50df82ec3503b8ff1a5ed6f2dfd682e24d4586e5
Geoffrey Young <geoff@apache.org>
2004-06-17 15:49:40 +0000

    initialize server arrays prior to calling ap_setup_prelinked_modules so that static modules can push Defines values when registering hooks just like DSO modules can Submitted by:	philippe chiasson Reviewed by:	geoff
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@103976 13f79535-47bb-0310-9956-ffa450edef68

--- server/main.c
+++ server/main.c
@@ -428,18 +428,18 @@ int main(int argc, const char * const argv[])
     }
 #endif
 
+    apr_pool_create(&pcommands, pglobal);
+    apr_pool_tag(pcommands, "pcommands");
+    ap_server_pre_read_config  = apr_array_make(pcommands, 1, sizeof(char *));
+    ap_server_post_read_config = apr_array_make(pcommands, 1, sizeof(char *));
+    ap_server_config_defines   = apr_array_make(pcommands, 1, sizeof(char *));
+
     error = ap_setup_prelinked_modules(process);
     if (error) {
         ap_log_error(APLOG_MARK, APLOG_STARTUP|APLOG_EMERG, 0, NULL, "%s: %s",
                      ap_server_argv0, error);
         destroy_and_exit_process(process, 1);
     }
-
-    apr_pool_create(&pcommands, pglobal);
-    apr_pool_tag(pcommands, "pcommands");
-    ap_server_pre_read_config  = apr_array_make(pcommands, 1, sizeof(char *));
-    ap_server_post_read_config = apr_array_make(pcommands, 1, sizeof(char *));
-    ap_server_config_defines   = apr_array_make(pcommands, 1, sizeof(char *));
 
     ap_run_rewrite_args(process);
 
