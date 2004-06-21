a66f2fe93b803f38c1d04ec586b7bb1fc3057d7f
Bradley Nicholes <bnicholes@apache.org>
2004-06-21 19:49:42 +0000

    Added display code for the URL cache
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@104010 13f79535-47bb-0310-9956-ffa450edef68

--- modules/experimental/util_ldap_cache.c
+++ modules/experimental/util_ldap_cache.c
@@ -82,6 +82,53 @@ void util_ldap_url_node_free(util_ald_cache_t *cache, void *n)
 void util_ldap_url_node_display(request_rec *r, util_ald_cache_t *cache, void *n)
 {
     util_url_node_t *node = (util_url_node_t *)n;
+    char date_str[APR_CTIME_LEN+1];
+    char *buf;
+    const char *type_str;
+    util_ald_cache_t *cache_node;
+    int x;
+
+    for (x=0;x<3;x++) {
+        switch (x) {
+            case 0:
+                cache_node = node->search_cache;
+                type_str = "Searches";
+                break;
+            case 1:
+                cache_node = node->compare_cache;
+                type_str = "Compares";
+                break;
+            case 2:
+                cache_node = node->dn_compare_cache;
+                type_str = "DN Compares";
+                break;
+        }
+        
+        if (cache_node->marktime) {
+            apr_ctime(date_str, cache_node->marktime);
+        }
+        else 
+            date_str[0] = 0;
+
+        buf = apr_psprintf(r->pool, 
+                 "<tr valign='top'>"
+                 "<td nowrap>%s (%s)</td>"
+                 "<td nowrap>%ld</td>"
+                 "<td nowrap>%ld</td>"
+                 "<td nowrap>%ld</td>"
+                 "<td nowrap>%ld</td>"
+                 "<td nowrap>%s</td>"
+                 "<tr>",
+             node->url,
+             type_str,
+             cache_node->size,
+             cache_node->maxentries,
+             cache_node->numentries,
+             cache_node->fullmark,
+             date_str);
+    
+        ap_rputs(buf, r);
+    }
 
 }
 
