1d2047b9d5e8ca61765fd4a41ba8ba6a0b5d8f12
William A. Rowe Jr <wrowe@apache.org>
2004-08-11 21:14:49 +0000

    Enable backwards compatibility with 2.0-HEAD, define ajp module base, and note CHANGES so far to proxy
    Submitted by:	mturk
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@104565 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy.c
+++ modules/proxy/mod_proxy.c
@@ -20,7 +20,13 @@
 #include "mod_core.h"
 
 #include "apr_optional.h"
+
+#if (MODULE_MAGIC_NUMBER_MAJOR > 20020903)
 #include "mod_ssl.h"
+#else
+APR_DECLARE_OPTIONAL_FN(int, ssl_proxy_enable, (conn_rec *));
+APR_DECLARE_OPTIONAL_FN(int, ssl_engine_disable, (conn_rec *));
+#endif
 
 #ifndef MAX
 #define MAX(x,y) ((x) >= (y) ? (x) : (y))
