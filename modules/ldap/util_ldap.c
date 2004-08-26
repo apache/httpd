ca11a13fc30f63a3915508a6c328edd75b5eca72
Bradley Nicholes <bnicholes@apache.org>
2004-08-26 15:24:51 +0000

    Preserve the return value from the apr_stat() call so that it can be displayed in the error log message.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@104817 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ldap/util_ldap.c
+++ modules/ldap/util_ldap.c
@@ -1045,6 +1045,7 @@ static const char *util_ldap_set_cert_auth(cmd_parms *cmd, void *dummy, const ch
 						  &ldap_module);
     const char *err = ap_check_cmd_context(cmd, GLOBAL_ONLY);
     apr_finfo_t finfo;
+    apr_status_t rv;
 
     if (err != NULL) {
         return err;
@@ -1057,11 +1058,11 @@ static const char *util_ldap_set_cert_auth(cmd_parms *cmd, void *dummy, const ch
     st->cert_auth_file = ap_server_root_relative(cmd->pool, file);
 
     if (st->cert_auth_file && 
-        (apr_stat (&finfo, st->cert_auth_file, APR_FINFO_MIN, cmd->pool) != APR_SUCCESS))
+        ((rv = apr_stat (&finfo, st->cert_auth_file, APR_FINFO_MIN, cmd->pool)) != APR_SUCCESS))
     {
         ap_log_error(APLOG_MARK, APLOG_ERR, 0, cmd->server, 
-                     "LDAP: Could not open SSL trusted certificate authority file - %s", 
-                     st->cert_auth_file == NULL ? file : st->cert_auth_file);
+                     "LDAP: Could not open SSL trusted certificate authority file - %s error: %d", 
+                     st->cert_auth_file == NULL ? file : st->cert_auth_file, rv);
         return "Invalid file path";
     }
 
