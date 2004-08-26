ff290c2c17a16ddd93fcb7ddb686c7ca568240f7
Bradley Nicholes <bnicholes@apache.org>
2004-08-26 22:21:33 +0000

    Validate the file path of the trusted certificate file at the time the LDAPTrushedCA directive is handled. PR#30782
    Reviewed by: bnicholes, trawick, clar
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/APACHE_2_0_BRANCH@104842 13f79535-47bb-0310-9956-ffa450edef68

--- modules/experimental/util_ldap.c
+++ modules/experimental/util_ldap.c
@@ -1084,6 +1084,9 @@ static const char *util_ldap_set_cert_auth(cmd_parms *cmd, void *dummy, const ch
         (util_ldap_state_t *)ap_get_module_config(cmd->server->module_config, 
 						  &ldap_module);
     const char *err = ap_check_cmd_context(cmd, GLOBAL_ONLY);
+    apr_finfo_t finfo;
+    apr_status_t rv;
+
     if (err != NULL) {
         return err;
     }
@@ -1093,6 +1096,15 @@ static const char *util_ldap_set_cert_auth(cmd_parms *cmd, void *dummy, const ch
                        file);
 
     st->cert_auth_file = ap_server_root_relative(cmd->pool, file);
+
+    if (st->cert_auth_file && 
+        ((rv = apr_stat (&finfo, st->cert_auth_file, APR_FINFO_MIN, cmd->pool)) != APR_SUCCESS))
+    {
+        ap_log_error(APLOG_MARK, APLOG_ERR, rv, cmd->server, 
+                     "LDAP: Could not open SSL trusted certificate authority file - %s", 
+                     st->cert_auth_file == NULL ? file : st->cert_auth_file);
+        return "Invalid file path";
+    }
 
     return(NULL);
 }
