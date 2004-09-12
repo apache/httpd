e190dce6b27eb3b10386eb479ad217cc92230097
Joe Orton <jorton@apache.org>
2004-09-12 22:24:05 +0000

    * modules/dav/fs/lock.c (dav_fs_refresh_locks): Fix SEGV on refresh of indirect lock.
    PR: 31183
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@105100 13f79535-47bb-0310-9956-ffa450edef68

--- modules/dav/fs/lock.c
+++ modules/dav/fs/lock.c
@@ -1469,13 +1469,13 @@ static dav_error * dav_fs_refresh_locks(dav_lockdb *lockdb,
 	}
 	if (dav_fs_do_refresh(dp_scan, ltl, new_time)) {
 	    /* the lock was refreshed. return the lock. */
-	    newlock = dav_fs_alloc_lock(lockdb, ip->key, dp->locktoken);
+	    newlock = dav_fs_alloc_lock(lockdb, ip->key, dp_scan->locktoken);
 	    newlock->is_locknull = !resource->exists;
-	    newlock->scope = dp->f.scope;
-	    newlock->type = dp->f.type;
-	    newlock->depth = dp->f.depth;
-	    newlock->timeout = dp->f.timeout;
-	    newlock->owner = dp->owner;
+	    newlock->scope = dp_scan->f.scope;
+	    newlock->type = dp_scan->f.type;
+	    newlock->depth = dp_scan->f.depth;
+	    newlock->timeout = dp_scan->f.timeout;
+	    newlock->owner = dp_scan->owner;
             newlock->auth_user = dp_scan->auth_user;
 
 	    newlock->next = *locks;
