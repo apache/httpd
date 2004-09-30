67841c5715af3fab35a235560d4e3314f9ee5aea
Mladen Turk <mturk@apache.org>
2004-09-30 15:35:45 +0000

    Use plain apr/apr-util for ajp.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@105353 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/ajp.h
+++ modules/proxy/ajp.h
@@ -355,11 +355,12 @@ apr_status_t ajp_msg_serialize_cping(ajp_msg_t *msg);
 /**
  * Dump up to the first 1024 bytes on an AJP Message
  *
+ * @param pool      pool to allocate from
  * @param msg       AJP Message to dump
  * @param err       error string to display
- * @return          APR_SUCCESS or error
+ * @return          dump message
  */
-apr_status_t ajp_msg_dump(ajp_msg_t *msg, char *err);
+char * ajp_msg_dump(apr_pool_t *pool, ajp_msg_t *msg, char *err);
 
 /** 
  * Send an AJP message to backend
