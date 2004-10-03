0e9a37b863d859b63e4794940fb5f7c05a78356c
Graham Leggett <minfrin@apache.org>
2004-10-03 16:44:06 +0000

    mod_ldap: fix a bogus error message to tell the user which file is causing a potential problem with the LDAP shared memory cache. PR:	31431 Obtained from: Submitted by:	Graham Leggett Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@105375 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ldap/util_ldap.c
+++ modules/ldap/util_ldap.c
@@ -1276,12 +1276,15 @@ static void util_ldap_child_init(apr_pool_t *p, server_rec *s)
 
     sts = apr_global_mutex_child_init(&st->util_ldap_cache_lock, st->lock_file, p);
     if (sts != APR_SUCCESS) {
-        ap_log_error(APLOG_MARK, APLOG_CRIT, sts, s, "failed to init caching lock in child process");
+        ap_log_error(APLOG_MARK, APLOG_CRIT, sts, s,
+                     "Failed to initialise global mutex %s in child process %d.",
+                     st->lock_file, getpid());
         return;
     }
     else {
         ap_log_error(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, 0, s, 
-                     "INIT global mutex %s in child %d ", st->lock_file, getpid());
+                     "Initialisation of global mutex %s in child process %d successful.",
+                     st->lock_file, getpid());
     }
 }
 
