1de5a227311f4b4a498722bf2ff02d5a5c672d9e
Bradley Nicholes <bnicholes@apache.org>
2004-10-13 15:26:45 +0000

    Add a check for SSL_ENABLED_OPTIONAL to the http_method and default_port hook so that they return the correct values for an upgradeable connection.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@105426 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/mod_ssl.c
+++ modules/ssl/mod_ssl.c
@@ -400,7 +400,7 @@ static const char *ssl_hook_http_method(const request_rec *r)
 {
     SSLSrvConfigRec *sc = mySrvConfig(r->server);
 
-    if (sc->enabled == SSL_ENABLED_FALSE) {
+    if (sc->enabled == SSL_ENABLED_FALSE || sc->enabled == SSL_ENABLED_OPTIONAL) {
         return NULL;
     }
 
@@ -411,7 +411,7 @@ static apr_port_t ssl_hook_default_port(const request_rec *r)
 {
     SSLSrvConfigRec *sc = mySrvConfig(r->server);
 
-    if (sc->enabled == SSL_ENABLED_FALSE) {
+    if (sc->enabled == SSL_ENABLED_FALSE || sc->enabled == SSL_ENABLED_OPTIONAL) {
         return 0;
     }
 
