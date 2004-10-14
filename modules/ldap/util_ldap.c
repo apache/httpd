83217b8dcb90694f48bdb26f9fad32870f1aaf00
Graham Leggett <minfrin@apache.org>
2004-10-14 11:31:48 +0000

    mod_ldap: Fix format strings to use %APR_PID_T_FMT instead of %d. PR: Obtained from: Submitted by:	Jeff Trawick Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@105453 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ldap/util_ldap.c
+++ modules/ldap/util_ldap.c
@@ -1291,13 +1291,17 @@ static void util_ldap_child_init(apr_pool_t *p, server_rec *s)
     sts = apr_global_mutex_child_init(&st->util_ldap_cache_lock, st->lock_file, p);
     if (sts != APR_SUCCESS) {
         ap_log_error(APLOG_MARK, APLOG_CRIT, sts, s,
-                     "Failed to initialise global mutex %s in child process %d.",
+                     "Failed to initialise global mutex %s in child process %"
+                     APR_PID_T_FMT
+                     ".",
                      st->lock_file, getpid());
         return;
     }
     else {
         ap_log_error(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, 0, s, 
-                     "Initialisation of global mutex %s in child process %d successful.",
+                     "Initialisation of global mutex %s in child process %"
+                     APR_PID_T_FMT
+                     " successful.",
                      st->lock_file, getpid());
     }
 }
