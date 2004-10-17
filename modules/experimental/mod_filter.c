826e4db529ccef08c3ce90ed4c455c58d896431f
Andre Malo <nd@apache.org>
2004-10-17 16:00:02 +0000

    style
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@105502 13f79535-47bb-0310-9956-ffa450edef68

--- modules/experimental/mod_filter.c
+++ modules/experimental/mod_filter.c
@@ -82,21 +82,21 @@ static void filter_trace(apr_pool_t *pool, int debug, const char *fname,
 static int filter_init(ap_filter_t *f)
 {
     ap_filter_provider_t *p;
-    int err = OK;
+    int err;
     ap_filter_rec_t *filter = f->frec;
 
     f->ctx = apr_pcalloc(f->r->pool, sizeof(harness_ctx));
     for (p = filter->providers; p; p = p->next) {
         if (p->frec->filter_init_func) {
-            if (err = p->frec->filter_init_func(f), err != OK) {
+            if ((err = p->frec->filter_init_func(f)) != OK) {
                 ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, f->r,
                               "filter_init for %s failed", p->frec->name);
-                break;        /* if anyone errors out here, so do we */
+                return err;   /* if anyone errors out here, so do we */
             }
         }
     }
 
-    return err;
+    return OK;
 }
 
 static ap_out_filter_func filter_lookup(request_rec *r, ap_filter_rec_t *filter)
