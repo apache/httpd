923e847ade0fae4d0b1567264dd94fbe14a41380
Paul Querna <pquerna@apache.org>
2004-10-18 00:49:31 +0000

    use the correct struct for CacheDisable. (cache_disable instead of cache_enable)
    PR: 31128
    Submitted by: Edward Rudd <eddie omegaware.com>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@105510 13f79535-47bb-0310-9956-ffa450edef68

--- modules/experimental/mod_cache.c
+++ modules/experimental/mod_cache.c
@@ -795,7 +795,7 @@ static const char *add_cache_disable(cmd_parms *parms, void *dummy,
                                      const char *url)
 {
     cache_server_conf *conf;
-    struct cache_enable *new;
+    struct cache_disable *new;
 
     conf =
         (cache_server_conf *)ap_get_module_config(parms->server->module_config,
