367c30406d215ae9a7294b9b5e972be94eb2f23f
Joe Orton <jorton@apache.org>
2004-11-10 15:21:44 +0000

    Add -t -DDUMP_CERTS option to mod_ssl which dumps the filenames of all configured SSL certificates to stdout, useful for cron-ing through a "do I need to renew any of my certificates this week" tool:
    * modules/ssl/ssl_engine_config.c (ssl_hook_ConfigTest): New function.
    
    * modules/ssl/mod_ssl.c (ssl_register_hooks): ...register it as a
    test_config hook.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@105741 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_config.c
+++ modules/ssl/ssl_engine_config.c
@@ -1380,3 +1380,28 @@ const char *ssl_cmd_SSLUserName(cmd_parms *cmd, void *dcfg,
     dc->szUserName = arg;
     return NULL;
 }
+
+void ssl_hook_ConfigTest(apr_pool_t *pconf, server_rec *s)
+{
+    if (!ap_exists_config_define("DUMP_CERTS")) {
+        return;
+    }
+
+    /* Dump the filenames of all configured server certificates to
+     * stdout. */
+    while (s) {
+        SSLSrvConfigRec *sc = mySrvConfig(s);
+
+        if (sc && sc->server && sc->server->pks) {
+            modssl_pk_server_t *const pks = sc->server->pks;
+            int i;
+
+            for (i = 0; (i < SSL_AIDX_MAX) && pks->cert_files[i]; i++) {
+                printf("%s\n", pks->cert_files[i]);
+            }
+        }
+
+        s = s->next;
+    }
+
+}
