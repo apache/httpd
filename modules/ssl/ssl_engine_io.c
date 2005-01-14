b1391e12be3407e42bb9f493b4c79a1db6aa94f6
Joe Orton <jorton@apache.org>
2005-01-14 14:16:58 +0000

    * modules/ssl/ssl_engine_io.c (bio_filter_out_flush): Fail if the connection was reset.
    PR: 32699
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@125166 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_io.c
+++ modules/ssl/ssl_engine_io.c
@@ -153,6 +153,10 @@ static int bio_filter_out_flush(BIO *bio)
 
     outctx->rc = ap_pass_brigade(outctx->filter_ctx->pOutputFilter->next,
                                  outctx->bb);
+    /* Fail if the connection was reset: */
+    if (outctx->rc == APR_SUCCESS && outctx->c->aborted) {
+        outctx->rc = APR_ECONNRESET;
+    }
     return (outctx->rc == APR_SUCCESS) ? 1 : -1;
 }
 
