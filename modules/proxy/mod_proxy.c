632ec345e98020711b9b5838789200326564fc3b
Justin Erenkrantz <jerenkrantz@apache.org>
2005-02-03 23:18:48 +0000

    mod_proxy: Fix ProxyRemoteMatch directive.
    PR: 33170
    Submitted by: Rici Lake <rici ricilake.net>
    Reviewed by:  Justin Erenkrantz
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@151248 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy.c
+++ modules/proxy/mod_proxy.c
@@ -618,8 +618,8 @@ static int proxy_handler(request_rec *r)
             for (i = 0; i < proxies->nelts; i++) {
                 p2 = ap_strchr_c(ents[i].scheme, ':');  /* is it a partial URL? */
                 if (strcmp(ents[i].scheme, "*") == 0 ||
-                    (ents[i].use_regex && ap_regexec(ents[i].regexp, url,
-                                                     0,NULL, 0)) ||
+                    (ents[i].use_regex &&
+                     ap_regexec(ents[i].regexp, url, 0, NULL, 0) == 0) ||
                     (p2 == NULL && strcasecmp(scheme, ents[i].scheme) == 0) ||
                     (p2 != NULL &&
                     strncasecmp(url, ents[i].scheme,
