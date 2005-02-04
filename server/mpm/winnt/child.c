aabf1f5375a2e89b116d850c03a39df72974e5c8
Justin Erenkrantz <jerenkrantz@apache.org>
2005-02-04 00:12:15 +0000

    If thread_num is long, this better be an %ld
    MFC: 106653
    Reviewed by: wrowe, nd, trawick
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.0.x@151265 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/winnt/child.c
+++ server/mpm/winnt/child.c
@@ -691,7 +691,7 @@ static void worker_main(long thread_num)
     ap_sb_handle_t *sbh;
 
     ap_log_error(APLOG_MARK, APLOG_DEBUG, APR_SUCCESS, ap_server_conf,
-                 "Child %d: Worker thread %d starting.", my_pid, thread_num);
+                 "Child %d: Worker thread %ld starting.", my_pid, thread_num);
     while (1) {
         conn_rec *c;
         apr_int32_t disconnected;
@@ -752,7 +752,7 @@ static void worker_main(long thread_num)
                                         (request_rec *) NULL);
 
     ap_log_error(APLOG_MARK, APLOG_DEBUG, APR_SUCCESS, ap_server_conf,
-                 "Child %d: Worker thread %d exiting.", my_pid, thread_num);
+                 "Child %d: Worker thread %ld exiting.", my_pid, thread_num);
 }
 
 
