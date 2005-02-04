fd9dcfccf83aaa660528f5ee729b4aca416c40b7
Justin Erenkrantz <jerenkrantz@apache.org>
2005-02-04 20:28:49 +0000

    Update copyright year to 2005 and standardize on current copyright owner line.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@151408 13f79535-47bb-0310-9956-ffa450edef68

--- modules/dav/fs/mod_dav_fs.c
+++ modules/dav/fs/mod_dav_fs.c
@@ -1,4 +1,5 @@
-/* Copyright 2000-2004 The Apache Software Foundation
+/* Copyright 2000-2005 The Apache Software Foundation or its licensors, as
+ * applicable.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- support/ab.c
+++ support/ab.c
@@ -1,5 +1,5 @@
-/* Copyright 1996-2005 The Apache Software Foundation or its licensors,
- *                     as applicable.
+/* Copyright 1996-2005 The Apache Software Foundation or its licensors, as
+ * applicable.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
