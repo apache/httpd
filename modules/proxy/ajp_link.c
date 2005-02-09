4e15690d022965880b012f2c4a0e56e43daa2598
Jeff Trawick <trawick@apache.org>
2005-02-09 19:01:43 +0000

    Remove formatting characters from ap_log_error() calls.  These were escaped as fallout from CAN-2003-0020.
    Submitted by: Eric Covener <ecovener gmail.com>
    Reviewed by: Jeff Trawick
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@153105 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/ajp_link.c
+++ modules/proxy/ajp_link.c
@@ -118,7 +118,7 @@ apr_status_t ajp_ilink_receive(apr_socket_t *sock, ajp_msg_t *msg)
 
     ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, NULL,
                  "ajp_ilink_receive() received packet len=%" APR_SIZE_T_FMT
-                 "type=%d\n",
+                 "type=%d",
                   blen, (int)msg->buf[hlen]);
 
     return APR_SUCCESS;
