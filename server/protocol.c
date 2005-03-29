2b3ad91040e3e83c40a14cccd9d7ddce885e3f25
Greg Ames <gregames@apache.org>
2005-03-29 18:24:46 +0000

    clone_headers_no_body: remove all entity headers in addition to the Transfer-Encoding header when creating a GET subrequest
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@159410 13f79535-47bb-0310-9956-ffa450edef68

--- server/protocol.c
+++ server/protocol.c
@@ -996,10 +996,16 @@ static void clone_headers_no_body(request_rec *rnew,
                                   const request_rec *r)
 {
     rnew->headers_in = apr_table_copy(rnew->pool, r->headers_in);
-    apr_table_unset(rnew->headers_in, "Content-Length");
-    apr_table_unset(rnew->headers_in, "Transfer-Encoding");
-    apr_table_unset(rnew->headers_in, "Content-Type");
     apr_table_unset(rnew->headers_in, "Content-Encoding");
+    apr_table_unset(rnew->headers_in, "Content-Language");
+    apr_table_unset(rnew->headers_in, "Content-Length");
+    apr_table_unset(rnew->headers_in, "Content-Location");
+    apr_table_unset(rnew->headers_in, "Content-MD5");
+    apr_table_unset(rnew->headers_in, "Content-Range");
+    apr_table_unset(rnew->headers_in, "Content-Type");
+    apr_table_unset(rnew->headers_in, "Expires");
+    apr_table_unset(rnew->headers_in, "Last-Modified");
+    apr_table_unset(rnew->headers_in, "Transfer-Encoding");
 }        
 
 /*
