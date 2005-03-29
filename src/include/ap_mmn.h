d5f2d017d52be6d570ed0d21977afa76aa74d38a
Jim Jagielski <jim@apache.org>
2005-03-29 23:01:34 +0000

    Uh Oh. We forgot to bump MINOR when we added ap_auth_nonce.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@159444 13f79535-47bb-0310-9956-ffa450edef68

--- src/include/ap_mmn.h
+++ src/include/ap_mmn.h
@@ -210,7 +210,7 @@
 #ifndef MODULE_MAGIC_NUMBER_MAJOR
 #define MODULE_MAGIC_NUMBER_MAJOR 19990320
 #endif
-#define MODULE_MAGIC_NUMBER_MINOR 16                    /* 0...n */
+#define MODULE_MAGIC_NUMBER_MINOR 17                    /* 0...n */
 
 /* Useful for testing for features. */
 #define AP_MODULE_MAGIC_AT_LEAST(major,minor)		\
