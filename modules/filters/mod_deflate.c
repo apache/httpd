b25736dfb06230da5f470aaa1a64f205d10c2c89
Paul Querna <pquerna@apache.org>
2005-04-17 20:25:57 +0000

    mod_deflate should be merging the Vary header, not Setting it, and ignoring what other modules put there.
    Spotted By: Rici Lake
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@161691 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/mod_deflate.c
+++ modules/filters/mod_deflate.c
@@ -315,7 +315,7 @@ static apr_status_t deflate_out_filter(ap_filter_t *f,
          * the Accept-Encoding, we need to note that we were looking
          * for this header and downstream proxies should be aware of that.
          */
-        apr_table_setn(r->headers_out, "Vary", "Accept-Encoding");
+        apr_table_mergen(r->headers_out, "Vary", "Accept-Encoding");
 
         /* force-gzip will just force it out regardless if the browser
          * can actually do anything with it.
