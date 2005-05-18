e696c7a4cfd7dd8d3b965c05647113f17a896652
Nick Kew <niq@apache.org>
2005-05-18 09:23:20 +0000

    Fix up header in mod_dbd.h (I remembered .c but not .h before previous commit)
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@170730 13f79535-47bb-0310-9956-ffa450edef68

--- modules/experimental/mod_dbd.h
+++ modules/experimental/mod_dbd.h
@@ -1,11 +1,25 @@
-/*        Copyright (c) 2004, Nick Kew.  All rights reserved.
+/* Copyright 2003-5 WebThing Ltd
+ * Copyright 2005 The Apache Software Foundation or its licensors, as
+ * applicable.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-        If this is accepted by ASF for inclusion in APR-UTIL,
-        I will assign copyright to ASF and license under ASF terms.
-
-        Otherwise I will retain copyright and license under
-        terms of my choice.
-*/
+/* Overview of what this is and does:
+ * http://www.apache.org/~niq/dbd.html
+ * or
+ * http://apache.webthing.com/database/
+ */
 #ifndef DBD_H
 #define DBD_H
 
@@ -21,16 +35,18 @@ typedef struct {
 
 /* Export functions to access the database */
 
-/* acquire a connection that MUST be explicitly closed.  Returns NULL on error */
+/* acquire a connection that MUST be explicitly closed.
+ * Returns NULL on error
+ */
 AP_DECLARE(ap_dbd_t*) ap_dbd_open(apr_pool_t*, server_rec*);
 
 /* release a connection acquired with ap_dbd_open */
 AP_DECLARE(void) ap_dbd_close(server_rec*, ap_dbd_t*);
 
 /* acquire a connection that will have the lifetime of a request
-   and MUST NOT be explicitly closed.  Return NULL on error.
-   This is the preferred function for most applications.
-*/
+ * and MUST NOT be explicitly closed.  Return NULL on error.
+ * This is the preferred function for most applications.
+ */
 AP_DECLARE(ap_dbd_t*) ap_dbd_acquire(request_rec*);
 
 /* Also export them as optional functions for modules that prefer it */
