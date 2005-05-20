80f33191f2fdfb3bcecf32fada751726ff4865af
Paul Querna <pquerna@apache.org>
2005-05-20 02:16:09 +0000

    * server/listen.c: Fix building on FreeBSD.
    Tested on FreeBSD 5.4.  Seems to enable the accept filters correctly, for both HTTP and HTTPS.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/listen-protocol@171035 13f79535-47bb-0310-9956-ffa450edef68

--- server/listen.c
+++ server/listen.c
@@ -217,11 +217,12 @@ static void ap_apply_accept_filter(apr_pool_t *p, ap_listen_rec *lis,
 
     if (accf) {
 #if APR_HAS_SO_ACCEPTFILTER
-        rv = apr_socket_accept_filter(s, ACCEPT_FILTER_NAME, "");
+        rv = apr_socket_accept_filter(s, apr_pstrdup(p, accf), 
+                                      apr_pstrdup(p,""));
         if (rv != APR_SUCCESS && !APR_STATUS_IS_ENOTIMPL(rv)) {
             ap_log_perror(APLOG_MARK, APLOG_WARNING, rv, p,
                           "Failed to enable the '%s' Accept Filter",
-                          ACCEPT_FILTER_NAME);
+                          accf);
         }
 #else
 #ifdef APR_TCP_DEFER_ACCEPT
