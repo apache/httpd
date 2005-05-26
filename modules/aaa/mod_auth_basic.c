df86e987f785a760bfd777dac31be125a1d3fc59
Paul Querna <pquerna@apache.org>
2005-05-26 20:34:21 +0000

    Fix segmentation fault in mod_auth_{basic,digest} when no provider is explicitly listed. Reported by: Ben Collins-Sussman <sussman collab.net>
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@178674 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_auth_basic.c
+++ modules/aaa/mod_auth_basic.c
@@ -232,12 +232,13 @@ static int authenticate_basic_user(request_rec *r)
                 auth_result = AUTH_GENERAL_ERROR;
                 break;
             }
+            apr_table_setn(r->notes, AUTHN_PROVIDER_NAME_NOTE, AUTHN_DEFAULT_PROVIDER);
         }
         else {
             provider = current_provider->provider;
+            apr_table_setn(r->notes, AUTHN_PROVIDER_NAME_NOTE, current_provider->provider_name);
         }
 
-        apr_table_setn(r->notes, AUTHN_PROVIDER_NAME_NOTE, current_provider->provider_name);
 
         auth_result = provider->check_password(r, sent_user, sent_pw);
 
