85015ced149243370dd2cf97be077a129c643faf
Paul Querna <pquerna@apache.org>
2005-06-03 20:19:08 +0000

    'generate_name' is too generic to be in the public symbols. Rename it to ap_cache_generate_name. No functional changes.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@179877 13f79535-47bb-0310-9956-ffa450edef68

--- modules/cache/mod_disk_cache.c
+++ modules/cache/mod_disk_cache.c
@@ -112,8 +112,8 @@ static char *header_file(apr_pool_t *p, disk_cache_conf *conf,
                          disk_cache_object_t *dobj, const char *name)
 {
     if (!dobj->hashfile) {
-        dobj->hashfile = generate_name(p, conf->dirlevels, conf->dirlength,
-                                       name);
+        dobj->hashfile = ap_cache_generate_name(p, conf->dirlevels, 
+                                                conf->dirlength, name);
     }
     return apr_pstrcat(p, conf->cache_root, "/", dobj->hashfile,
                        CACHE_HEADER_SUFFIX, NULL);
@@ -123,8 +123,8 @@ static char *data_file(apr_pool_t *p, disk_cache_conf *conf,
                        disk_cache_object_t *dobj, const char *name)
 {
     if (!dobj->hashfile) {
-        dobj->hashfile = generate_name(p, conf->dirlevels, conf->dirlength,
-                                       name);
+        dobj->hashfile = ap_cache_generate_name(p, conf->dirlevels, 
+                                                conf->dirlength, name);
     }
     return apr_pstrcat(p, conf->cache_root, "/", dobj->hashfile,
                        CACHE_DATA_SUFFIX, NULL);
