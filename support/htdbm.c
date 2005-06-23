de0e5773ea5285015ac315daebf18102b3320ed2
Jeff Trawick <trawick@apache.org>
2005-06-23 20:45:17 +0000

    Fix htdbm password validation for records which included comments.
    Submitted by: Eric Covener <covener gmail.com>
    Reviewed by:  trawick
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@201455 13f79535-47bb-0310-9956-ffa450edef68

--- support/htdbm.c
+++ support/htdbm.c
@@ -226,7 +226,7 @@ static apr_status_t htdbm_verify(htdbm_t *htdbm)
     if (apr_dbm_fetch(htdbm->dbm, key, &val) != APR_SUCCESS)
         return APR_ENOENT;
     rec = apr_pstrndup(htdbm->pool, val.dptr, val.dsize);
-    cmnt = strchr(rec, ';');
+    cmnt = strchr(rec, ':');
     if (cmnt)
         strncpy(pwd, rec, cmnt - rec);
     else
