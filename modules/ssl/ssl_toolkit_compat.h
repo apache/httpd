ec0c63c34d6abd1655badf8823d4d4797f10bb90
William A. Rowe Jr <wrowe@apache.org>
2005-07-06 15:10:36 +0000

    Accomodate API changes to OpenSSL 0.9.8 (now in beta).   For all currently building platforms, this is a noop.
    Submitted by: Georg v. Zezschwitz <gzv 2scale.de>
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.0.x@209468 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_toolkit_compat.h
+++ modules/ssl/ssl_toolkit_compat.h
@@ -99,6 +99,13 @@ typedef int (modssl_read_bio_cb_fn)(char*,int,int,void*);
 #define HAVE_SSL_X509V3_EXT_d2i
 #endif
 
+#ifndef PEM_F_DEF_CALLBACK
+#ifdef PEM_F_PEM_DEF_CALLBACK
+/* In OpenSSL 0.9.8 PEM_F_DEF_CALLBACK was renamed */
+#define PEM_F_DEF_CALLBACK PEM_F_PEM_DEF_CALLBACK 
+#endif
+#endif
+
 #elif defined (SSLC_VERSION_NUMBER) /* RSA */
 
 /* sslc does not support this function, OpenSSL has since 9.5.1 */
