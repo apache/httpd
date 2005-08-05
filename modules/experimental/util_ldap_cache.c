99f67d424f2f11045d029d5aa1e8371f3e9e456a
Joe Orton <jorton@apache.org>
2005-08-05 14:06:46 +0000

    Merge r225753 from trunk:
    * modules/ldap/util_ldap_cache.c (util_ldap_cache_init): Use the
    actual available size of the shm segment not the requested size.
    Ensure the requested size is aligned.  Check errors from apr_rmm_init.
    
    Reviewed by: jorton, minfrin, bnicholes
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.0.x@230474 13f79535-47bb-0310-9956-ffa450edef68

--- modules/experimental/util_ldap_cache.c
+++ modules/experimental/util_ldap_cache.c
@@ -397,8 +397,11 @@ apr_status_t util_ldap_cache_init(apr_pool_t *pool, util_ldap_state_t *st)
 {
 #if APR_HAS_SHARED_MEMORY
     apr_status_t result;
+    apr_size_t size;
 
-    result = apr_shm_create(&st->cache_shm, st->cache_bytes, st->cache_file, st->pool);
+    size = APR_ALIGN_DEFAULT(st->cache_bytes);
+
+    result = apr_shm_create(&st->cache_shm, size, st->cache_file, st->pool);
     if (result == APR_EEXIST) {
         /*
          * The cache could have already been created (i.e. we may be a child process).  See
@@ -410,8 +413,17 @@ apr_status_t util_ldap_cache_init(apr_pool_t *pool, util_ldap_state_t *st)
         return result;
     }
 
+    /* Determine the usable size of the shm segment. */
+    size = apr_shm_size_get(st->cache_shm);
+
     /* This will create a rmm "handler" to get into the shared memory area */
-    apr_rmm_init(&st->cache_rmm, NULL, (void *)apr_shm_baseaddr_get(st->cache_shm), st->cache_bytes, st->pool);
+    result = apr_rmm_init(&st->cache_rmm, NULL, 
+                          apr_shm_baseaddr_get(st->cache_shm), size, 
+                          st->pool);
+    if (result != APR_SUCCESS) {
+        return result;
+    }
+
 #endif
 
     apr_pool_cleanup_register(st->pool, st , util_ldap_cache_module_kill, apr_pool_cleanup_null);
