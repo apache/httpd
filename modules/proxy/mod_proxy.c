9d7b5928dbbbd5b92a4eb1415546ee44d74ccc10
Nick Kew <niq@apache.org>
2005-08-11 18:52:16 +0000

    Fix build error from cut&paste
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@231501 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy.c
+++ modules/proxy/mod_proxy.c
@@ -871,17 +871,18 @@ static void *merge_proxy_dir_config(apr_pool_t *p, void *basev, void *addv)
 {
     proxy_dir_conf *new = (proxy_dir_conf *) apr_pcalloc(p, sizeof(proxy_dir_conf));
     proxy_dir_conf *add = (proxy_dir_conf *) addv;
+    proxy_dir_conf *base = (proxy_dir_conf *) basev;
 
     new->p = add->p;
     new->p_is_fnmatch = add->p_is_fnmatch;
     new->r = add->r;
 
     /* Put these in the dir config so they work inside <Location> */
-    new->raliases = apr_array_append(p, base->raliases, overrides->raliases);
+    new->raliases = apr_array_append(p, base->raliases, add->raliases);
     new->cookie_paths
-        = apr_array_append(p, base->cookie_paths, overrides->cookie_paths);
+        = apr_array_append(p, base->cookie_paths, add->cookie_paths);
     new->cookie_domains
-        = apr_array_append(p, base->cookie_domains, overrides->cookie_domains);
+        = apr_array_append(p, base->cookie_domains, add->cookie_domains);
     new->cookie_path_str = base->cookie_path_str;
     new->cookie_domain_str = base->cookie_domain_str;
     return new;
