877ae7552f8f31947dd4586504e4aadaccaeb342
Jim Jagielski <jim@apache.org>
2005-08-24 12:50:00 +0000

    Fold in other's patch. Had enough votes and closes PR 34512 for 2.0 tree.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.0.x@239646 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/proxy_ftp.c
+++ modules/proxy/proxy_ftp.c
@@ -92,7 +92,7 @@ static int ftp_check_globbingchars(const char *path)
     for ( ; *path; ++path) {
         if (*path == '\\')
 	    ++path;
-        if (path != '\0' && strchr(FTP_GLOBBING_CHARS, *path) != NULL)
+        if (*path != '\0' && strchr(FTP_GLOBBING_CHARS, *path) != NULL)
             return TRUE;
     }
     return FALSE;
