6644fb1d5ba15cd2dcda5d079cce9eaf25273af6
Jeff Trawick <trawick@apache.org>
2005-09-11 13:04:03 +0000

    backport from trunk:
       Correct mod_cgid's argv[0] so that the full path can be delved by the
       invoked cgi application, to conform to the behavior of mod_cgi.
       [Pradeep Kumar S <pradeep.smani gmail.com>]
    
    Reviewed by: wrowe, colm, trawick
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.0.x@280123 13f79535-47bb-0310-9956-ffa450edef68

--- modules/generators/mod_cgid.c
+++ modules/generators/mod_cgid.c
@@ -1278,10 +1278,7 @@ static int cgid_handler(request_rec *r)
  
     nph = !(strncmp(argv0, "nph-", 4)); 
 
-    if ((argv0 = strrchr(r->filename, '/')) != NULL) 
-        argv0++; 
-    else 
-        argv0 = r->filename; 
+    argv0 = r->filename; 
 
     if (!(ap_allow_options(r) & OPT_EXECCGI) && !is_scriptaliased(r)) 
         return log_scripterror(r, conf, HTTP_FORBIDDEN, 0, 
