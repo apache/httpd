a77b67b48f14f864478f6a5b4e241da68529effd
Ruediger Pluem <rpluem@apache.org>
2005-10-25 20:12:48 +0000

    Merge r327185 from trunk:
    * Fix PR37100 (SEGV in mod_proxy_ajp), by sending the data up the filter
      chain immediately instead of spooling it completely before passing it
      to the filter chain. It contains a bandaid to handle intentional
      flushes from Tomcat side. Further explanation in code and report.
    
      ajp.h:           Add ajp_msg_reuse prototype
    
      mod_proxy_ajp.c: Adjust logic of ap_proxy_ajp_request
    
      ajp_msg.c:       Add ajp_msg_reuse
    
      ajp_header.c:    Adjusting logic of ajp_read_header
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@328467 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/ajp_msg.c
+++ modules/proxy/ajp_msg.c
@@ -139,6 +139,24 @@ apr_status_t ajp_msg_reset(ajp_msg_t *msg)
 }
 
 /**
+ * Reuse an AJP Message
+ *
+ * @param msg       AJP Message to reuse
+ * @return          APR_SUCCESS or error
+ */
+apr_status_t ajp_msg_reuse(ajp_msg_t *msg)
+{
+    apr_byte_t *buf;
+
+    buf = msg->buf;
+    memset(msg, 0, sizeof(ajp_msg_t));
+    msg->buf = buf;
+    msg->header_len = AJP_HEADER_LEN;
+    ajp_msg_reset(msg);
+    return APR_SUCCESS;
+}
+
+/**
  * Mark the end of an AJP Message
  *
  * @param msg       AJP Message to end
