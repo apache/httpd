1afa1638350ee7a9f02376f94da67fe2485fe75e
Nick Kew <niq@apache.org>
2005-11-14 15:07:00 +0000

    ver-dependent potential memory-use bug in preparing statements
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@344142 13f79535-47bb-0310-9956-ffa450edef68

--- modules/database/mod_dbd.c
+++ modules/database/mod_dbd.c
@@ -198,10 +198,11 @@ static apr_status_t dbd_prepared_init(apr_pool_t *pool, svr_cfg *svr,
 {
     dbd_prepared *p;
     apr_status_t ret = APR_SUCCESS;
-    apr_dbd_prepared_t *stmt = NULL;
+    apr_dbd_prepared_t *stmt;
     dbd->prepared = apr_hash_make(pool);
 
     for (p = svr->prepared; p; p = p->next) {
+        stmt = NULL;
         if (apr_dbd_prepare(dbd->driver, pool, dbd->handle, p->query,
                             p->label, &stmt) == 0) {
             apr_hash_set(dbd->prepared, p->label, APR_HASH_KEY_STRING, stmt);
