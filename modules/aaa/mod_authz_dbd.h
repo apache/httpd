5dbbe6d499157a6bad9eb805e2a9b0d26559da1e
Nick Kew <niq@apache.org>
2005-11-18 00:42:08 +0000

    mod_authz_dbd: SQL authz with Login/Session support
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@345389 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/aaa/mod_authz_dbd.h
@@ -0,0 +1,23 @@
+/* Copyright 2000-2005 The Apache Software Foundation or its licensors, as
+ * applicable.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MOD_AUTHZ_DBD_H
+#define MOD_AUTHZ_DBD_H
+#include "httpd.h"
+
+APR_DECLARE_EXTERNAL_HOOK(authz_dbd, AP, int, client_login,
+                          (request_rec *r, int code, const char *action))
+#endif
