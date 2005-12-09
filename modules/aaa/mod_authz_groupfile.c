162f35006270816bbcb172ca2bb24d97b6b92ad0
Bradley Nicholes <bnicholes@apache.org>
2005-12-09 19:40:37 +0000

    clean up a few loose ends
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/authz-dev@355577 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_authz_groupfile.c
+++ modules/aaa/mod_authz_groupfile.c
@@ -280,6 +280,15 @@ static authz_status group_check_authorization(request_rec *r,
     apr_table_t *grpstatus = NULL;
     apr_status_t status;
 
+    /* If there is no group file - then we are not
+     * configured. So decline.
+     */
+    if (!(conf->groupfile)) {
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+                        "No group file was specified in the configuration");
+        return AUTHZ_DENIED;
+    }
+
     status = groups_for_user(r->pool, user, conf->groupfile,
                                 &grpstatus);
 
@@ -319,18 +328,24 @@ APR_OPTIONAL_FN_TYPE(authz_owner_get_file_group) *authz_owner_get_file_group;
 static authz_status filegroup_check_authorization(request_rec *r,
                                               const char *require_args)
 {
-    const char *filegroup = NULL;
-
-
     authz_groupfile_config_rec *conf = ap_get_module_config(r->per_dir_config,
             &authz_groupfile_module);
     char *user = r->user;
     apr_table_t *grpstatus = NULL;
     apr_status_t status;
+    const char *filegroup = NULL;
+
+    /* If there is no group file - then we are not
+     * configured. So decline.
+     */
+    if (!(conf->groupfile)) {
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+                        "No group file was specified in the configuration");
+        return AUTHZ_DENIED;
+    }
 
     status = groups_for_user(r->pool, user, conf->groupfile,
                              &grpstatus);
-
     if (status != APR_SUCCESS) {
         ap_log_rerror(APLOG_MARK, APLOG_ERR, status, r,
                       "Could not open group file: %s",
