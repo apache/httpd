02d30be72413d14943704b8c8879c1a30361ccd0
Nick Kew <niq@apache.org>
2005-12-14 10:05:48 +0000

    Trival backports having 3 * +1
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@356764 13f79535-47bb-0310-9956-ffa450edef68

--- server/protocol.c
+++ server/protocol.c
@@ -990,6 +990,8 @@ request_rec *ap_read_request(conn_rec *conn)
     }
 
     if ((access_status = ap_run_post_read_request(r))) {
+        ap_add_input_filter_handle(ap_http_input_filter_handle,
+                                   NULL, r, r->connection);
         ap_die(access_status, r);
         ap_update_child_status(conn->sbh, SERVER_BUSY_LOG, r);
         ap_run_log_transaction(r);
