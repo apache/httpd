c9f6052126a125b05e3836197e119bd176544bef
Ruediger Pluem <rpluem@apache.org>
2005-12-18 11:17:32 +0000

    Backport of r357328 from trunk:
    * Fix ap_http_chunk_filter to create correct chunks in the case that a
      flush bucket is pre- and postfixed by data buckets in the brigade.
    
    Submitted by: Ruediger Pluem
    Reviewed by: Justin Erenkrantz, Joe Orton, Jim Jagielski
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.0.x@357454 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http/http_core.c
+++ modules/http/http_core.c
@@ -124,6 +124,8 @@ static apr_status_t chunk_filter(ap_filter_t *f, apr_bucket_brigade *b)
             }
             if (APR_BUCKET_IS_FLUSH(e)) {
                 flush = e;
+                more = apr_brigade_split(b, APR_BUCKET_NEXT(e));
+                break;
             }
             else if (e->length == (apr_size_t)-1) {
                 /* unknown amount of data (e.g. a pipe) */
