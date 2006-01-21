543dda577feec4b4d4fbf6f3e8224eca252b5cba
Ruediger Pluem <rpluem@apache.org>
2006-01-21 11:30:41 +0000

    * Fix PR38340 by actual getting the word and thus advancing the message buffer   pointer to the next data.
    Submitted by: Aleksey Pesternikov <apesternikov yahoo.com>
    Reviewed by: Ruediger Pluem
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@371013 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/ajp_header.c
+++ modules/proxy/ajp_header.c
@@ -507,7 +507,7 @@ static apr_status_t ajp_unmarshal_response(ajp_msg_t *msg,
         }
 
         if ((name & 0XFF00) == 0XA000) {
-            ajp_msg_peek_uint16(msg, &name);
+            ajp_msg_get_uint16(msg, &name);
             stringname = long_res_header_for_sc(name);
             if (stringname == NULL) {
                 ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server,
