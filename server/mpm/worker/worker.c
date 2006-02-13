b458b88d158acc1bdce71e6cc58287bb530775da
Garrett Rooney <rooneg@apache.org>
2006-02-13 02:25:53 +0000

    Make the worker and event MPMs support GracefulShutdownTimeout just like the prefork MPM does.
    Submitted by: Chris Darroch <chrisd pearsoncmg.com>
    Ported to event MPM by: rooneg
    Issue number: 38621
    
    * server/mpm/worker/worker.c
      (ap_mpm_run): Make the graceful shutdown log message refer to the right
       signal, and respect the graceful shutdown timeout if there is one.
    
    * server/mpm/experimental/event/event.c
      (ap_mpm_run): Ditto.
    
    * CHANGES: Note change.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@377291 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/worker/worker.c
+++ server/mpm/worker/worker.c
@@ -1768,8 +1768,14 @@ int ap_mpm_run(apr_pool_t *_pconf, apr_pool_t *plog, server_rec *s)
                              "removed PID file %s (pid=%" APR_PID_T_FMT ")",
                              pidfile, getpid());
 
-            ap_log_error(APLOG_MARK, APLOG_NOTICE, 0,
-                         ap_server_conf, "caught SIGTERM, shutting down");
+            ap_log_error(APLOG_MARK, APLOG_NOTICE, 0, ap_server_conf,
+                         "caught " AP_SIG_GRACEFUL_STOP_STRING
+                         ", shutting down gracefully");
+        }
+
+        if (ap_graceful_shutdown_timeout) {
+            cutoff = apr_time_now() +
+                     apr_time_from_sec(ap_graceful_shutdown_timeout);
         }
 
         /* Don't really exit until each child has finished */
