b5ca8f8f3febc6d6b3412e621bd7357fc9062d83
Ruediger Pluem <rpluem@apache.org>
2006-03-05 15:22:18 +0000

    * Crosscheck the length of the body chunk with the length of the ajp message   to prevent readings beyond the buffer boundaries which possibly could reveal   sensitive memory contents to the client.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@383339 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/ajp_header.c
+++ modules/proxy/ajp_header.c
@@ -683,6 +683,7 @@ apr_status_t  ajp_parse_data(request_rec  *r, ajp_msg_t *msg,
 {
     apr_byte_t result;
     apr_status_t rc;
+    apr_uint16_t expected_len;
 
     rc = ajp_msg_get_uint8(msg, &result);
     if (rc != APR_SUCCESS) {
@@ -698,6 +699,23 @@ apr_status_t  ajp_parse_data(request_rec  *r, ajp_msg_t *msg,
     rc = ajp_msg_get_uint16(msg, len);
     if (rc != APR_SUCCESS) {
         return rc;
+    }
+    /*
+     * msg->len contains the complete length of the message including all
+     * headers. So the expected length for a CMD_AJP13_SEND_BODY_CHUNK is
+     * msg->len minus the sum of
+     * AJP_HEADER_LEN    : The length of the header to every AJP message.
+     * AJP_HEADER_SZ_LEN : The header giving the size of the chunk.
+     * 1                 : The CMD_AJP13_SEND_BODY_CHUNK indicator byte (0x03).
+     * 1                 : The last byte of this message always seems to be
+     *                     0x00 and is not part of the chunk.
+     */
+    expected_len = msg->len - (AJP_HEADER_LEN + AJP_HEADER_SZ_LEN + 1 + 1);
+    if (*len != expected_len) {
+        ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server,
+               "ajp_parse_data: Wrong chunk length. Length of chunk is %i,"
+               " expected length is %i.", *len, expected_len);
+        return AJP_EBAD_HEADER;
     }
     *ptr = (char *)&(msg->buf[msg->pos]);
     return APR_SUCCESS;
