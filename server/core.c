d99724e71d13a3417827be64da16165d1018c3f6
Jeff Trawick <trawick@apache.org>
2006-04-02 23:36:27 +0000

    Default handler: Don't return output filter apr_status_t values.
    PR: 31759
    Helped by: Ruediger Pluem, Joe Orton
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@390922 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -3596,6 +3596,20 @@ static int default_handler(request_rec *r)
         APR_BRIGADE_INSERT_TAIL(bb, e);
 
         return ap_pass_brigade(r->output_filters, bb);
+        status = ap_pass_brigade(r->output_filters, bb);
+        if (status == APR_SUCCESS
+            || r->status != HTTP_OK
+            || c->aborted) {
+            return OK;
+        }
+        else {
+            /* no way to know what type of error occurred */
+            ap_log_rerror(APLOG_MARK, APLOG_DEBUG, status, r,
+                          "default_handler: ap_pass_brigade returned %i",
+                          status);
+            return HTTP_INTERNAL_SERVER_ERROR;
+        }
+
     }
     else {              /* unusual method (not GET or POST) */
         if (r->method_number == M_INVALID) {
