e1fbc27d1af1d04d7f2941417d5d444dac0d4517
Greg Ames <gregames@apache.org>
2006-04-10 19:24:33 +0000

    expand on a comment for a rather subtle line of code so I have a chance of understanding how it helps in the future
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@393028 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ldap/util_ldap.c
+++ modules/ldap/util_ldap.c
@@ -1757,7 +1757,9 @@ static void *util_ldap_create_config(apr_pool_t *p, server_rec *s)
     util_ldap_state_t *st =
         (util_ldap_state_t *)apr_pcalloc(p, sizeof(util_ldap_state_t));
 
-    /* Create a pool for mod_ldap to use */
+    /* Create a per vhost pool for mod_ldap to use, serialized with 
+     * st->mutex (also one per vhost) 
+     */
     apr_pool_create(&st->pool, p);
 #if APR_HAS_THREADS
     apr_thread_mutex_create(&st->mutex, APR_THREAD_MUTEX_DEFAULT, st->pool);
