57bc7a8aaddf881bbf2401d2f72bf0c2bc1fc8bc
Colm MacCarthaigh <colm@apache.org>
2006-04-16 17:34:39 +0000

    Update one of the more important copyright notices prior to any 2006 releases.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.0.x@394522 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_release.h
+++ include/ap_release.h
@@ -19,7 +19,7 @@
 #include "apr_general.h" /* stringify */
 
 #define AP_SERVER_COPYRIGHT \
-    "Copyright 2001-2005 The Apache Software Foundation" \
+    "Copyright 2001-2006 The Apache Software Foundation" \
     " or its licensors, as applicable."
 
 /*
