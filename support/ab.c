b910664e7e2f278bed4586b6cdd5e10c9651e67a
Colm MacCarthaigh <colm@apache.org>
2006-04-17 20:09:50 +0000

    Update the copyright notice to reflect that changes have gone in this year
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@394765 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -1760,14 +1760,14 @@ static void copyright(void)
     if (!use_html) {
         printf("This is ApacheBench, Version %s\n", AP_AB_BASEREVISION " <$Revision$>");
         printf("Copyright 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/\n");
-        printf("Copyright 1997-2005 The Apache Software Foundation, http://www.apache.org/\n");
+        printf("Copyright 1997-2006 The Apache Software Foundation, http://www.apache.org/\n");
         printf("\n");
     }
     else {
         printf("<p>\n");
         printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i><br>\n", AP_AB_BASEREVISION, "$Revision$");
         printf(" Copyright 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/<br>\n");
-        printf(" Copyright 1997-2005 The Apache Software Foundation, http://www.apache.org/<br>\n");
+        printf(" Copyright 1997-2006 The Apache Software Foundation, http://www.apache.org/<br>\n");
         printf("</p>\n<p>\n");
     }
 }
