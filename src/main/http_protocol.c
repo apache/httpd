a0515c2dd98b0be8363997c0e0b4b9db690c7611
Mark J. Cox <mjc@apache.org>
2006-04-19 08:28:04 +0000

    Fix escaping of Expect error message http://svn.apache.org/viewcvs?rev=394965&view=rev +1: mjc, trawick, wrowe
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@395171 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/http_protocol.c
+++ src/main/http_protocol.c
@@ -3137,7 +3137,7 @@ API_EXPORT(void) ap_send_error_response(request_rec *r, int recursive_error)
 	    ap_rvputs(r, "The expectation given in the Expect request-header"
 	              "\nfield could not be met by this server.<P>\n"
 	              "The client sent<PRE>\n    Expect: ",
-	              ap_table_get(r->headers_in, "Expect"), "\n</PRE>\n"
+	              ap_escape_html(r->pool, ap_table_get(r->headers_in, "Expect")), "\n</PRE>\n"
 	              "but we only allow the 100-continue expectation.\n",
 	              NULL);
 	    break;
