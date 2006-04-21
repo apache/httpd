5bb3ec71c55eaf8490ffdc08e5032a191718fb0a
Jim Jagielski <jim@apache.org>
2006-04-21 18:40:11 +0000

    Revert copyright date change patch. Wait until we have a universal policy and procedure... we cannot willy nilly change the dates unless significant or material changes are made.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@395984 13f79535-47bb-0310-9956-ffa450edef68

--- src/ap/ap_md5c.c
+++ src/ap/ap_md5c.c
@@ -31,7 +31,7 @@
    documentation and/or software.
  */
 
-/* Copyright 1999-2006 The Apache Software Foundation
+/* Copyright 1999-2004 The Apache Software Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
