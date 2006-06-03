c180fcb78d1f42f33ad8e1b973344cd4b62cecee
Ruediger Pluem <rpluem@apache.org>
2006-06-03 20:52:58 +0000

    * If possible, check if the size of an object to cache is within the   configured boundaries before actually saving data.
    Submitted by: Niklas Edmundsson <nikke acc.umu.se>
    Reviewed by: rpluem
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@411469 13f79535-47bb-0310-9956-ffa450edef68

--- modules/cache/mod_disk_cache.c
+++ modules/cache/mod_disk_cache.c
@@ -330,6 +330,22 @@ static int create_entity(cache_handle_t *h, request_rec *r, const char *key, apr
         return DECLINED;
     }
 
+    /* Note, len is -1 if unknown so don't trust it too hard */
+    if (len > conf->maxfs) {
+        ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, r->server,
+                     "disk_cache: URL %s failed the size check "
+                     "(%" APR_OFF_T_FMT " > %" APR_SIZE_T_FMT ")",
+                     key, len, conf->maxfs);
+        return DECLINED;
+    }
+    if (len >= 0 && len < conf->minfs) {
+        ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, r->server,
+                     "disk_cache: URL %s failed the size check "
+                     "(%" APR_OFF_T_FMT " < %" APR_SIZE_T_FMT ")",
+                     key, len, conf->minfs);
+        return DECLINED;
+    }
+
     /* Allocate and initialize cache_object_t and disk_cache_object_t */
     h->cache_obj = obj = apr_pcalloc(r->pool, sizeof(*obj));
     obj->vobj = dobj = apr_pcalloc(r->pool, sizeof(*dobj));
