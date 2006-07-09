7f81282233a3c86a876cabce938fcd616d1131e5
Ruediger Pluem <rpluem@apache.org>
2006-07-09 16:19:41 +0000

    * Add the filename of the configuration file to the warning message   about the useless use of AllowOverride.
    PR: 39992
    Submitted by: Darryl Miles <darryl darrylmiles.org>
    Reviewed by: rpluem
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@420307 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -1315,8 +1315,8 @@ static const char *set_override(cmd_parms *cmd, void *d_, const char *l)
     /* Throw a warning if we're in <Location> or <Files> */
     if (ap_check_cmd_context(cmd, NOT_IN_LOCATION | NOT_IN_FILES)) {
         ap_log_error(APLOG_MARK, APLOG_WARNING, 0, cmd->server,
-                     "Useless use of AllowOverride in line %d.",
-                     cmd->directive->line_num);
+                     "Useless use of AllowOverride in line %d of %s.",
+                     cmd->directive->line_num, cmd->directive->filename);
     }
 
     d->override = OR_NONE;
