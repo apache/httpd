c645716c52e986dac6f89801b70ef18fa39422d7
Roy T. Fielding <fielding@apache.org>
2006-07-12 03:55:27 +0000

    update collective copyright notice in manual
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@421105 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -1732,14 +1732,14 @@ static void copyright(void)
     if (!use_html) {
         printf("This is ApacheBench, Version %s\n", AP_AB_BASEREVISION " <$Revision: 1.146 $> apache-2.0");
         printf("Copyright 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/\n");
-        printf("Copyright 1997-2005 The Apache Software Foundation, http://www.apache.org/\n");
+        printf("Copyright 2006 The Apache Software Foundation, http://www.apache.org/\n");
         printf("\n");
     }
     else {
         printf("<p>\n");
         printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AP_AB_BASEREVISION, "$Revision: 1.146 $");
         printf(" Copyright 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/<br>\n");
-        printf(" Copyright 1997-2005 The Apache Software Foundation, http://www.apache.org/<br>\n");
+        printf(" Copyright 2006 The Apache Software Foundation, http://www.apache.org/<br>\n");
         printf("</p>\n<p>\n");
     }
 }
