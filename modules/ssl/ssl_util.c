892785ce3942d828d724810ed4f72465da44cc6b
Ben Laurie <ben@apache.org>
2006-07-23 10:55:07 +0000

    Add PKCS#7 support.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@424707 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_util.c
+++ modules/ssl/ssl_util.c
@@ -265,6 +265,50 @@ const char *ssl_asn1_table_keyfmt(apr_pool_t *p,
     return apr_pstrcat(p, id, ":", keystr, NULL);
 }
 
+STACK_OF(X509) *ssl_read_pkcs7(server_rec *s,const char *pkcs7)
+{
+    PKCS7 *p7;
+    STACK_OF(X509) *certs;
+    FILE *f;
+
+    f = fopen(pkcs7, "r");
+    if (!f) {
+        ap_log_error(APLOG_MARK, APLOG_ERR, 0, s, "Can't open %s", pkcs7);
+	ssl_die();
+    }
+
+    p7 = PEM_read_PKCS7(f,NULL,NULL,NULL);
+    if (!p7) {
+	ap_log_error(APLOG_MARK, APLOG_ERR|APLOG_NOERRNO, 0, s,
+		     "Can't read PKCS7 object %s", pkcs7);
+	ssl_log_ssl_error(APLOG_MARK, APLOG_CRIT, s);
+	exit(1);
+    }
+
+    switch (OBJ_obj2nid(p7->type)) {
+    case NID_pkcs7_signed:
+	certs = p7->d.sign->cert;
+	break;
+
+    case NID_pkcs7_signedAndEnveloped:
+	certs = p7->d.signed_and_enveloped->cert;
+	break;
+
+    default:
+	ap_log_error(APLOG_MARK, APLOG_CRIT|APLOG_NOERRNO, 0, s,
+		     "Don't understand PKCS7 file %s", pkcs7);
+	ssl_die();
+    }
+
+    if (!certs) {
+	ap_log_error(APLOG_MARK, APLOG_CRIT|APLOG_NOERRNO, 0, s,
+		     "No certificates in %s", pkcs7);
+	ssl_die();
+    }
+
+    return certs;
+}
+
 
 #if APR_HAS_THREADS
 /*
