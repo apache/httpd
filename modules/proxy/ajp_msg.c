02c70cf77e81561fecb461a4bd2ea5f28fd659a4
Mladen Turk <mturk@apache.org>
2006-08-02 10:42:07 +0000

    Make sure the error signature is logged as double hex byte.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@427951 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/ajp_msg.c
+++ modules/proxy/ajp_msg.c
@@ -100,7 +100,7 @@ apr_status_t ajp_msg_check_header(ajp_msg_t *msg, apr_size_t *len)
           (head[0] == 0x12 && head[1] == 0x34))) {
 
         ap_log_error(APLOG_MARK, APLOG_ERR, 0, NULL,
-                      "ajp_check_msg_header() got bad signature %x%x",
+                      "ajp_check_msg_header() got bad signature %02x%02x",
                       head[0], head[1]);
 
         return AJP_EBAD_SIGNATURE;
