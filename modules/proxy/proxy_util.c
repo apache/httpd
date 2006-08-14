bc3cfcb75aa85f53b58ad2ce054d3ce1d683bc04
Nick Kew <niq@apache.org>
2006-08-14 13:26:07 +0000

    PR#37770: Don't try to use dead backend connection in proxy (Patch from Olivier BOEL)
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@431339 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/proxy_util.c
+++ modules/proxy/proxy_util.c
@@ -2089,7 +2089,8 @@ static int is_socket_connected(apr_socket_t *sock)
     socket_status = apr_socket_recv(sock, test_buffer, &buffer_len);
     /* put back old timeout */
     apr_socket_timeout_set(sock, current_timeout);
-    if (APR_STATUS_IS_EOF(socket_status)) {
+    if (APR_STATUS_IS_EOF(socket_status)
+        || APR_STATUS_IS_ECONNRESET(socket_status)) {
         return 0;
     }
     else {
