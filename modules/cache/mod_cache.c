bc84099c0a75e17148f00a45166c4f706f0cb06c
Graham Leggett <minfrin@apache.org>
2006-09-27 14:21:26 +0000

    mod_cache: Eliminate a bogus error in the log when a filter returns AP_FILTER_ERROR.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@450453 13f79535-47bb-0310-9956-ffa450edef68

--- modules/cache/mod_cache.c
+++ modules/cache/mod_cache.c
@@ -244,10 +244,12 @@ static int cache_url_handler(request_rec *r, int lookup)
     out = apr_brigade_create(r->pool, r->connection->bucket_alloc);
     rv = ap_pass_brigade(r->output_filters, out);
     if (rv != APR_SUCCESS) {
-        ap_log_error(APLOG_MARK, APLOG_ERR, rv, r->server,
-                     "cache: error returned while trying to return %s "
-                     "cached data",
-                     cache->provider_name);
+        if(rv != AP_FILTER_ERROR) {
+            ap_log_error(APLOG_MARK, APLOG_ERR, rv, r->server,
+                         "cache: error returned while trying to return %s "
+                         "cached data",
+                         cache->provider_name);
+        }
         return rv;
     }
 
