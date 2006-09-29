1dd0180635da486c161f7f826bbc848673142002
Joe Orton <jorton@apache.org>
2006-09-29 14:03:13 +0000

    * server/mpm/prefork/prefork.c (prefork_check_config): Remove unused variable.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@451297 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/prefork/prefork.c
+++ server/mpm/prefork/prefork.c
@@ -1329,7 +1329,6 @@ static int prefork_check_config(apr_pool_t *p, apr_pool_t *plog,
 {
     static int restart_num = 0;
     int startup = 0;
-    apr_status_t rv;
 
     /* the reverse of pre_config, we want this only the first time around */
     if (restart_num++ == 0) {
