9afe6d7dbb269316eda2e18ab0958df2f28efa70
Ruediger Pluem <rpluem@apache.org>
2006-10-02 21:12:34 +0000

    * Fix precedence problem.
    PR: 40656
    Submitted by: Larry Cipriani <lvc lucent.com>
    Reviewed by: rpluem
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@452212 13f79535-47bb-0310-9956-ffa450edef68

--- modules/metadata/mod_mime_magic.c
+++ modules/metadata/mod_mime_magic.c
@@ -935,7 +935,7 @@ static int apprentice(server_rec *s, apr_pool_t *p)
         return -1;
     }
     if ((result = apr_file_open(&f, fname, APR_READ | APR_BUFFERED,
-                                APR_OS_DEFAULT, p) != APR_SUCCESS)) {
+                                APR_OS_DEFAULT, p)) != APR_SUCCESS) {
         ap_log_error(APLOG_MARK, APLOG_ERR, result, s,
                      MODNAME ": can't read magic file %s", fname);
         return -1;
