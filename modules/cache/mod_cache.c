66cb25a80d251c86ece768100fd975460a3cae37
Ruediger Pluem <rpluem@apache.org>
2006-11-18 22:17:33 +0000

    * Use the query string stored in r->parsed_uri.query instead of r->args   as r->args could have been changed (e.g. via mod_rewrite) after the quick   handler hook. This causes resources to be stored under a key, where they   cannot be fetched again in the quick handler.
    PR: 40805
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@476625 13f79535-47bb-0310-9956-ffa450edef68

--- modules/cache/mod_cache.c
+++ modules/cache/mod_cache.c
@@ -446,7 +446,7 @@ static int cache_save_filter(ap_filter_t *f, apr_bucket_brigade *in)
         /* if a Expires header is in the past, don't cache it */
         reason = "Expires header already expired, not cacheable";
     }
-    else if (r->args && exps == NULL) {
+    else if (r->parsed_uri.query && exps == NULL) {
         /* if query string present but no expiration time, don't cache it
          * (RFC 2616/13.9)
          */
