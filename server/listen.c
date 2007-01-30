6e9dfd0b387f9011fcacd710d76a9949b875e98d
Joe Orton <jorton@apache.org>
2007-01-30 10:12:45 +0000

    * server/listen.c (ap_apply_accept_filter): Bump TCP_DEFER_ACCEPT argument to 30 units-of-unspecified-nature.
    Submitted by: Dean Gaudet <dean arctic.org>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@501364 13f79535-47bb-0310-9956-ffa450edef68

--- server/listen.c
+++ server/listen.c
@@ -225,7 +225,7 @@ static void ap_apply_accept_filter(apr_pool_t *p, ap_listen_rec *lis,
         }
 #else
 #ifdef APR_TCP_DEFER_ACCEPT
-        rv = apr_socket_opt_set(s, APR_TCP_DEFER_ACCEPT, 1);
+        rv = apr_socket_opt_set(s, APR_TCP_DEFER_ACCEPT, 30);
         if (rv != APR_SUCCESS && !APR_STATUS_IS_ENOTIMPL(rv)) {
             ap_log_perror(APLOG_MARK, APLOG_WARNING, rv, p,
                               "Failed to enable APR_TCP_DEFER_ACCEPT");
