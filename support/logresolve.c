9c70c93e8ac16b2b42bc3a94cfa05b58ad581c11
Colm MacCarthaigh <colm@apache.org>
2007-03-06 01:56:17 +0000

    Prevent format specifier busting.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@514953 13f79535-47bb-0310-9956-ffa450edef68

--- support/logresolve.c
+++ support/logresolve.c
@@ -227,7 +227,7 @@ int main(int argc, const char * const argv[])
         /* See if we have it in our cache */
         hostname = (char *) apr_hash_get(cache, line, APR_HASH_KEY_STRING);
         if (hostname) {
-            apr_file_printf(outfile, hostname);
+            apr_file_printf(outfile, "%s", hostname);
             if (space) 
                 apr_file_printf(outfile, " %s", space + 1);
             cachehits++;
