38a53977c43adb3f7c08c2faf4219d8b6eec8f01
Sander Temme <sctemme@apache.org>
2007-04-05 05:43:25 +0000

    Move the thread locking upcall initialization before the hardware library init, so the hardware library can pick up the locking callbacks. Fixes PR 20951. Tested on Linux with trunk and an nCipher nShield card, and on Solaris 10/Sparc on 2.0.55 with an nCipher NetHSM.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@525709 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_init.c
+++ modules/ssl/ssl_engine_init.c
@@ -210,15 +210,15 @@ int ssl_init_Module(apr_pool_t *p, apr_pool_t *plog,
 
     }
 
+#if APR_HAS_THREADS
+    ssl_util_thread_setup(p);
+#endif
+
     /*
      * SSL external crypto device ("engine") support
      */
 #if defined(HAVE_OPENSSL_ENGINE_H) && defined(HAVE_ENGINE_INIT)
     ssl_init_Engine(base_server, p);
-#endif
-
-#if APR_HAS_THREADS
-    ssl_util_thread_setup(p);
 #endif
 
     ap_log_error(APLOG_MARK, APLOG_INFO, 0, s,
