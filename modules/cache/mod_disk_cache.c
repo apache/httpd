c795f75fea2166379ea41c961e56939e18b51901
Justin Erenkrantz <jerenkrantz@apache.org>
2007-05-17 18:17:23 +0000

    mod_disk_cache: If the Vary'd content is stale, we can handle it via our freshness tests which will attempt to revalidate the content.
    * modules/cache/mod_disk_cache.c
      (open_entity): If the root Vary index is stale, we don't need to throw away
      potentially stale content - we can try to refresh it.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@539054 13f79535-47bb-0310-9956-ffa450edef68

--- modules/cache/mod_disk_cache.c
+++ modules/cache/mod_disk_cache.c
@@ -422,10 +422,6 @@ static int open_entity(cache_handle_t *h, request_rec *r, const char *key)
         len = sizeof(expire);
         apr_file_read_full(dobj->hfd, &expire, len, &len);
 
-        if (expire < r->request_time) {
-            return DECLINED;
-        }
-
         varray = apr_array_make(r->pool, 5, sizeof(char*));
         rc = read_array(r, varray, dobj->hfd);
         if (rc != APR_SUCCESS) {
