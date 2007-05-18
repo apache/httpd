111f019f093183100c9bf67ea62586fd92909c0f
Justin Erenkrantz <jerenkrantz@apache.org>
2007-05-18 06:09:53 +0000

    mod_disk_cache: Allow Vary'd content to be refreshed properly.
    (Backport of r538992, r538997, r539054)
    
    Reviewed by: jerenkrantz, fielding, rpluem
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@539279 13f79535-47bb-0310-9956-ffa450edef68

--- modules/cache/mod_disk_cache.c
+++ modules/cache/mod_disk_cache.c
@@ -403,10 +403,6 @@ static int open_entity(cache_handle_t *h, request_rec *r, const char *key)
         len = sizeof(expire);
         apr_file_read_full(dobj->hfd, &expire, len, &len);
 
-        if (expire < r->request_time) {
-            return DECLINED;
-        }
-
         varray = apr_array_make(r->pool, 5, sizeof(char*));
         rc = read_array(r, varray, dobj->hfd);
         if (rc != APR_SUCCESS) {
@@ -823,6 +819,15 @@ static apr_status_t store_headers(cache_handle_t *h, request_rec *r, cache_info 
             apr_array_header_t* varray;
             apr_uint32_t format = VARY_FORMAT_VERSION;
 
+            /* If we were initially opened as a vary format, rollback
+             * that internal state for the moment so we can recreate the
+             * vary format hints in the appropriate directory.
+             */
+            if (dobj->prefix) {
+                dobj->hdrsfile = dobj->prefix;
+                dobj->prefix = NULL;
+            }
+
             mkdir_structure(conf, dobj->hdrsfile, r->pool);
 
             rv = apr_file_mktemp(&dobj->tfd, dobj->tempfile,
@@ -874,8 +879,6 @@ static apr_status_t store_headers(cache_handle_t *h, request_rec *r, cache_info 
     if (rv != APR_SUCCESS) {
         return rv;
     }
-
-    dobj->name = h->cache_obj->key;
 
     disk_info.format = DISK_FORMAT_VERSION;
     disk_info.date = info->date;
