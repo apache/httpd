bf017abda60f38ad8db287bc206b025ea6e40a92
Sander Temme <sctemme@apache.org>
2007-05-24 02:19:04 +0000

    Explain that POST data should be sent as the correct MIME type. Submitted by Vincent Bray noodlet at gmail dot com, edited and reviewed by sctemme
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@541138 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -1823,13 +1823,17 @@ static void usage(const char *progname)
         "[s]"
 #endif
         "://]hostname[:port]/path\n", progname);
+/* 80 column ruler:  ********************************************************************************
+ */
     fprintf(stderr, "Options are:\n");
     fprintf(stderr, "    -n requests     Number of requests to perform\n");
     fprintf(stderr, "    -c concurrency  Number of multiple requests to make\n");
     fprintf(stderr, "    -t timelimit    Seconds to max. wait for responses\n");
     fprintf(stderr, "    -b windowsize   Size of TCP send/receive buffer, in bytes\n");
-    fprintf(stderr, "    -p postfile     File containing data to POST\n");
-    fprintf(stderr, "    -T content-type Content-type header for POSTing\n");
+    fprintf(stderr, "    -p postfile     File containing data to POST. Remember also to set -T\n");
+    fprintf(stderr, "    -T content-type Content-type header for POSTing, eg.\n");
+    fprintf(stderr, "                    'application/x-www-form-urlencoded'\n");
+    fprintf(stderr, "                    Default is 'text/plain'\n");
     fprintf(stderr, "    -v verbosity    How much troubleshooting info to print\n");
     fprintf(stderr, "    -w              Print out results in HTML tables\n");
     fprintf(stderr, "    -i              Use HEAD instead of GET\n");
