4e268e14f52d9ad897fdbcf6f091dd5445401819
Jean-Frederic Clere <jfclere@apache.org>
2007-06-27 11:39:03 +0000

    Don't display "PHPSESSIONID | PHPSESSIONID" but only "PHPSESSIONID".
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@551126 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_balancer.c
+++ modules/proxy/mod_proxy_balancer.c
@@ -736,7 +736,12 @@ static int balancer_handler(request_rec *r)
             ap_rputs("\n\n<table border=\"0\" style=\"text-align: left;\"><tr>"
                 "<th>StickySession</th><th>Timeout</th><th>FailoverAttempts</th><th>Method</th>"
                 "</tr>\n<tr>", r);
-            ap_rvputs(r, "<td>", balancer->sticky, " | ", balancer->sticky_path, NULL);
+            if (strcmp(balancer->sticky, balancer->sticky_path)) {
+                ap_rvputs(r, "<td>", balancer->sticky, " | ", balancer->sticky_path, NULL);
+            }
+            else {
+                ap_rvputs(r, "<td>", balancer->sticky, NULL);
+            }
             ap_rprintf(r, "</td><td>%" APR_TIME_T_FMT "</td>",
                 apr_time_sec(balancer->timeout));
             ap_rprintf(r, "<td>%d</td>\n", balancer->max_attempts);
