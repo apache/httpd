e6e890f3f438a262ceac4600a32e481085131eb9
Sander Temme <sctemme@apache.org>
2007-07-14 17:03:18 +0000

    Backport of 2.0.x PID table problem fix
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.0.x@556298 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm_common.c
+++ server/mpm_common.c
@@ -110,6 +110,11 @@ static int reclaim_one_pid(pid_t pid, action_t action)
     apr_proc_t proc;
     apr_status_t waitret;
 
+    /* Ensure pid sanity. */
+    if (pid < 1) {
+        return 1;
+    }        
+
     proc.pid = pid;
     waitret = apr_proc_wait(&proc, NULL, NULL, APR_NOWAIT);
     if (waitret != APR_CHILD_NOTDONE) {
