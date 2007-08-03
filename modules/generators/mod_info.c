5ad2d637e8f4f670a4dc2c2fe50d002c0ac6ab3f
Jim Jagielski <jim@apache.org>
2007-08-03 15:26:34 +0000

    PR 42847.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@562503 13f79535-47bb-0310-9956-ffa450edef68

--- modules/generators/mod_info.c
+++ modules/generators/mod_info.c
@@ -722,7 +722,7 @@ static int display_info(request_rec * r)
                                            ap_escape_html(r->pool, cmd->name),
                                            cmd->name[0] == '<' ? "&gt;" : "");
                                 if (cmd->errmsg) {
-                                    ap_rputs(cmd->errmsg, r);
+                                    ap_rputs(ap_escape_html(r->pool, cmd->errmsg), r);
                                 }
                                 ap_rputs("</i></tt></dd>\n", r);
                             }
