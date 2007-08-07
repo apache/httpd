1b8b5c9f3e2dd5899b765d6adb8ffdb115cdb50c
Nick Kew <niq@apache.org>
2007-08-07 13:03:03 +0000

    PR 42572: crash in Windows MPM Davi Arnauts simple patch confirmed as working. (typo in my last commit)
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@563489 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/winnt/child.c
+++ server/mpm/winnt/child.c
@@ -1184,10 +1184,10 @@ void child_main(apr_pool_t *pconf)
         TerminateThread(child_handles[i], 1);
         CloseHandle(child_handles[i]);
         /* Reset the scoreboard entry for the thread we just whacked */
+        score_idx = apr_hash_get(ht, &child_handles[i], sizeof(HANDLE));
         if (score_idx) {
-            score_idx = apr_hash_get(ht, &child_handles[i], sizeof(HANDLE));
+            ap_update_child_status_from_indexes(0, *score_idx, SERVER_DEAD, NULL);
         }
-        ap_update_child_status_from_indexes(0, *score_idx, SERVER_DEAD, NULL);
     }
     ap_log_error(APLOG_MARK, APLOG_NOTICE, APR_SUCCESS, ap_server_conf,
                  "Child %d: All worker threads have exited.", my_pid);
