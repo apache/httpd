27be62c072b6e2bf0224df0ce9491ec09cbfd864
Jim Jagielski <jim@apache.org>
2007-08-10 11:15:40 +0000

    Add support of ProxyIOBufferSize for AJP
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@564553 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/ajp.h
+++ modules/proxy/ajp.h
@@ -131,6 +131,8 @@ struct ajp_msg
     apr_size_t  len;
     /** The current read position */ 
     apr_size_t  pos;
+    /** The size of the buffer */
+    apr_size_t max_size;
     /** Flag indicating the origing of the message */ 
     int         server_side;
 };
@@ -141,8 +143,10 @@ struct ajp_msg
 #define AJP13_WS_HEADER             0x1234
 #define AJP_HEADER_LEN              4
 #define AJP_HEADER_SZ_LEN           2
-#define AJP_MSG_BUFFER_SZ           (8*1024)
-#define AJP13_MAX_SEND_BODY_SZ      (AJP_MSG_BUFFER_SZ - 6)
+#define AJP_HEADER_SZ               6
+#define AJP_MSG_BUFFER_SZ           8192
+#define AJP_MAX_BUFFER_SZ           16384
+#define AJP13_MAX_SEND_BODY_SZ      (AJP_MAX_BUFFER_SZ - AJP_HEADER_SZ)
 
 /** Send a request from web server to container*/
 #define CMD_AJP13_FORWARD_REQUEST   (unsigned char)2
@@ -335,10 +339,11 @@ apr_status_t ajp_msg_get_bytes(ajp_msg_t *msg, apr_byte_t **rvalue,
  * Create an AJP Message from pool
  *
  * @param pool      memory pool to allocate AJP message from
+ * @param size      size of the buffer to create
  * @param rmsg      Pointer to newly created AJP message
  * @return          APR_SUCCESS or error
  */
-apr_status_t ajp_msg_create(apr_pool_t *pool, ajp_msg_t **rmsg);
+apr_status_t ajp_msg_create(apr_pool_t *pool, apr_size_t size, ajp_msg_t **rmsg);
 
 /**
  * Recopy an AJP Message to another
@@ -405,21 +410,25 @@ apr_status_t ajp_ilink_receive(apr_socket_t *sock, ajp_msg_t *msg);
  * Build the ajp header message and send it
  * @param sock      backend socket
  * @param r         current request
+ * @param buffsize  max size of the AJP packet.
  * @uri uri         requested uri
  * @return          APR_SUCCESS or error
  */
 apr_status_t ajp_send_header(apr_socket_t *sock, request_rec *r,
+                             apr_size_t buffsize,
                              apr_uri_t *uri);
 
 /**
  * Read the ajp message and return the type of the message.
  * @param sock      backend socket
  * @param r         current request
+ * @param buffsize  size of the buffer.
  * @param msg       returned AJP message
  * @return          APR_SUCCESS or error
  */
 apr_status_t ajp_read_header(apr_socket_t *sock,
                              request_rec  *r,
+                             apr_size_t buffsize,
                              ajp_msg_t **msg);
 
 /**
