f3843a4698de7fc13ca3a14561fb6190f1cbf028
Martin Kraemer <martin@apache.org>
2007-08-31 08:23:57 +0000

    mod_dumpio: Fix for correct dumping of traffic on EBCDIC hosts Data had been incorrectly converted twice, resulting in garbled log output. Based on trunk commit http://svn.apache.org/viewvc?rev=570753&view=rev
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@571398 13f79535-47bb-0310-9956-ffa450edef68

--- modules/debug/mod_dumpio.c
+++ modules/debug/mod_dumpio.c
@@ -67,6 +67,9 @@ static void dumpit(ap_filter_t *f, apr_bucket *b)
             if (nbytes) {
                 obuf = malloc(nbytes+1);    /* use pool? */
                 memcpy(obuf, buf, nbytes);
+#if APR_CHARSET_EBCDIC
+                ap_xlate_proto_from_ascii(obuf, nbytes);
+#endif
                 obuf[nbytes] = '\0';
                 ap_log_error(APLOG_MARK, ptr->loglevel, 0, c->base_server,
                      "mod_dumpio:  %s (%s-%s): %s",
