47a25d908c25a570ebce1bef4b512d9a6a8c5c66
Jim Jagielski <jim@apache.org>
2007-09-04 11:35:35 +0000

    Merge r481886 from trunk:
    * Remove expired content from cache that cannot be revalidated.
    
    PR: 30370
    
    Submitted by: rpluem
    Reviewed by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@572626 13f79535-47bb-0310-9956-ffa450edef68

--- modules/cache/cache_storage.c
+++ modules/cache/cache_storage.c
@@ -304,6 +304,20 @@ int cache_select(request_rec *r)
                     }
                     cache->stale_handle = h;
                 }
+                else {
+                    int irv;
+
+                    /*
+                     * The copy isn't fresh enough, but we cannot revalidate.
+                     * So it is the same case as if there had not been a cached
+                     * entry at all. Thus delete the entry from cache.
+                     */
+                    irv = cache->provider->remove_url(h, r->pool);
+                    if (irv != OK) {
+                        ap_log_error(APLOG_MARK, APLOG_DEBUG, irv, r->server,
+                                     "cache: attempt to remove url from cache unsuccessful.");
+                    }
+                }
 
                 return DECLINED;
             }
