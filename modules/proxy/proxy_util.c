ed62ecf93f258310fdc91a036dc73ca679492bfb
Ruediger Pluem <rpluem@apache.org>
2007-09-11 06:40:11 +0000

    * This check is now part of the PROXY_WORKER_IS_INITIALIZED macro.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@574486 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/proxy_util.c
+++ modules/proxy/proxy_util.c
@@ -1672,7 +1672,7 @@ PROXY_DECLARE(void) ap_proxy_initialize_worker_share(proxy_server_conf *conf,
 {
     lb_score *score = NULL;
 
-    if (worker->s && PROXY_WORKER_IS_INITIALIZED(worker)) {
+    if (PROXY_WORKER_IS_INITIALIZED(worker)) {
         /* The worker share is already initialized */
         ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, s,
               "proxy: worker %s already initialized",
