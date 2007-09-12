8004a1996aeb496d03516947ef289da0e0c1cacb
Jim Jagielski <jim@apache.org>
2007-09-12 13:19:47 +0000

    Merge r574100 from trunk:
    mod_proxy_ftp: Don't segfault on bad line in FTP listing
    PR 40733 [Ulf Harnhammar]
    
    
    Submitted by: niq
    Reviewed by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@574946 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_ftp.c
+++ modules/proxy/mod_proxy_ftp.c
@@ -517,6 +517,14 @@ static apr_status_t proxy_send_dir_filter(ap_filter_t *f,
             }
 
             filename = strrchr(ctx->buffer, ' ');
+            if (filename == NULL) {
+                /* Line is broken.  Ignore it. */
+                ap_log_error(APLOG_MARK, APLOG_WARNING, 0, r->server,
+                             "proxy_ftp: could not parse line %s", ctx->buffer);
+                /* erase buffer for next time around */
+                ctx->buffer[0] = 0;
+                continue;  /* while state is BODY */
+            }
             *(filename++) = '\0';
 
             /* handle filenames with spaces in 'em */
