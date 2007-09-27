dbe599b6dae1b976a5c383529d87e212857b3e36
Nick Kew <niq@apache.org>
2007-09-27 14:20:21 +0000

    Formatting.  No functional change.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@580019 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_http.c
+++ modules/proxy/mod_proxy_http.c
@@ -1035,39 +1035,39 @@ skip_body:
     return APR_SUCCESS;
 }
 
-static void process_proxy_header(request_rec* r, proxy_dir_conf* c,
-                      const char* key, const char* value)
+static void process_proxy_header(request_rec *r, proxy_dir_conf *c,
+                                 const char *key, const char *value)
 {
-    static const char* date_hdrs[]
-        = { "Date", "Expires", "Last-Modified", NULL } ;
+    static const char *date_hdrs[]
+        = { "Date", "Expires", "Last-Modified", NULL };
     static const struct {
-        const char* name;
+        const char *name;
         ap_proxy_header_reverse_map_fn func;
     } transform_hdrs[] = {
-        { "Location", ap_proxy_location_reverse_map } ,
-        { "Content-Location", ap_proxy_location_reverse_map } ,
-        { "URI", ap_proxy_location_reverse_map } ,
-        { "Destination", ap_proxy_location_reverse_map } ,
-        { "Set-Cookie", ap_proxy_cookie_reverse_map } ,
+        { "Location", ap_proxy_location_reverse_map },
+        { "Content-Location", ap_proxy_location_reverse_map },
+        { "URI", ap_proxy_location_reverse_map },
+        { "Destination", ap_proxy_location_reverse_map },
+        { "Set-Cookie", ap_proxy_cookie_reverse_map },
         { NULL, NULL }
-    } ;
-    int i ;
-    for ( i = 0 ; date_hdrs[i] ; ++i ) {
-        if ( !strcasecmp(date_hdrs[i], key) ) {
+    };
+    int i;
+    for (i = 0; date_hdrs[i]; ++i) {
+        if (!strcasecmp(date_hdrs[i], key)) {
             apr_table_add(r->headers_out, key,
-                ap_proxy_date_canon(r->pool, value)) ;
-            return ;
+                          ap_proxy_date_canon(r->pool, value));
+            return;
         }
     }
-    for ( i = 0 ; transform_hdrs[i].name ; ++i ) {
-        if ( !strcasecmp(transform_hdrs[i].name, key) ) {
+    for (i = 0; transform_hdrs[i].name; ++i) {
+        if (!strcasecmp(transform_hdrs[i].name, key)) {
             apr_table_add(r->headers_out, key,
-                (*transform_hdrs[i].func)(r, c, value)) ;
-            return ;
+                          (*transform_hdrs[i].func)(r, c, value));
+            return;
        }
     }
-    apr_table_add(r->headers_out, key, value) ;
-    return ;
+    apr_table_add(r->headers_out, key, value);
+    return;
 }
 
 /*
