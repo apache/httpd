29847ed1bef2990bf170639012fb51cdef1d5431
Jim Jagielski <jim@apache.org>
2007-10-02 21:21:04 +0000

    Ensure the URI is "*" and not something like "*foo"
    Oh, last commit was supposed to say we need to return
    DONE so we don't invoke any handlers.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@581389 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http/http_core.c
+++ modules/http/http_core.c
@@ -236,7 +236,8 @@ static int http_create_request(request_rec *r)
 
 static int http_send_options(request_rec *r)
 {
-    if ((r->method_number == M_OPTIONS) && r->uri && (r->uri[0] == '*')) {
+    if ((r->method_number == M_OPTIONS) && r->uri && (r->uri[0] == '*') &&
+         (r->uri[1] == '\0')) {
         return DONE;           /* Send HTTP pong, without Allow header */
     }
     return DECLINED;
