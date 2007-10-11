0f27de34f9bbafd09233475ac36b2e1158850622
Joe Orton <jorton@apache.org>
2007-10-11 13:02:35 +0000

    * modules/proxy/mod_proxy_http.c (ap_proxy_http_register_hook): Fix   apr_pool_cleanup_register() invocation added in r583202, which was   causing every apr_proc_create() call to segfault.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@583813 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_http.c
+++ modules/proxy/mod_proxy_http.c
@@ -1881,7 +1881,7 @@ static void ap_proxy_http_register_hook(apr_pool_t *p)
     proxy_hook_scheme_handler(proxy_http_handler, NULL, NULL, APR_HOOK_FIRST);
     proxy_hook_canon_handler(proxy_http_canon, NULL, NULL, APR_HOOK_FIRST);
     warn_rx = ap_pregcomp(p, "[0-9]{3}[ \t]+[^ \t]+[ \t]+\"[^\"]*\"([ \t]+\"([^\"]+)\")?", 0);
-    apr_pool_cleanup_register(p, p, warn_rx_free, NULL);
+    apr_pool_cleanup_register(p, p, warn_rx_free, apr_pool_cleanup_null);
 }
 
 module AP_MODULE_DECLARE_DATA proxy_http_module = {
