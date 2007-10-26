f9b0ac8843381f80f22c682eac36575c5f74b641
Nick Kew <niq@apache.org>
2007-10-26 23:22:55 +0000

    r588791 needed an MMN bump
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@588796 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_mmn.h
+++ include/ap_mmn.h
@@ -135,6 +135,7 @@
  * 20070823.1 (2.3.0-dev)  add ap_send_interim_response()
  * 20071023.0 (2.3.0-dev)  add ap_get_scoreboard(sbh) split from the less
  *                         conventional ap_get_scoreboard(proc, thread)
+ * 20071023.1 (2.3.0-dev)  Add flags field to struct proxy_alias
  */
 
 #define MODULE_MAGIC_COOKIE 0x41503234UL /* "AP24" */
@@ -142,7 +143,7 @@
 #ifndef MODULE_MAGIC_NUMBER_MAJOR
 #define MODULE_MAGIC_NUMBER_MAJOR 20071023
 #endif
-#define MODULE_MAGIC_NUMBER_MINOR 0                    /* 0...n */
+#define MODULE_MAGIC_NUMBER_MINOR 1                    /* 0...n */
 
 /**
  * Determine if the server's current MODULE_MAGIC_NUMBER is at least a
