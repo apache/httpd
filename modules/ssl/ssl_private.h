748681d5582c86fd019ce35463d6928cf480abc7
Joe Orton <jorton@apache.org>
2007-11-02 16:12:42 +0000

    * modules/ssl/ssl_engine_vars.c (ssl_var_register): Take a pool argument; determine library version strings once at startup. (ssl_var_lookup_ssl_version): Drop 'pp' argument; use new global variables rather than modifying process-global state in a function which must be thread-safe.  (all callers changed)
    * modules/ssl/mod_ssl.c (ssl_register_hooks): Pass pool to
      ssl_var_register.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@591384 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_private.h
+++ modules/ssl/ssl_private.h
@@ -680,7 +680,9 @@ void         ssl_die(void);
 void         ssl_log_ssl_error(const char *, int, int, server_rec *);
 
 /**  Variables  */
-void         ssl_var_register(void);
+
+/* Register variables for the lifetime of the process pool 'p'. */
+void         ssl_var_register(apr_pool_t *p);
 char        *ssl_var_lookup(apr_pool_t *, server_rec *, conn_rec *, request_rec *, char *);
 apr_array_header_t *ssl_ext_list(apr_pool_t *p, conn_rec *c, int peer, const char *extension);
 
