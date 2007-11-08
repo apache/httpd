7efb84f750f138f72cca5d2bdeec3812ca07ba6e
Eric Covener <covener@apache.org>
2007-11-08 20:01:04 +0000

    while technically uldap_connection_cleanup() does leave an entry in the connection list, it is fully disconnected before it's put back.  My previous commentary did more harm then good.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@593283 13f79535-47bb-0310-9956-ffa450edef68

--- include/util_ldap.h
+++ include/util_ldap.h
@@ -190,7 +190,7 @@ APR_DECLARE_OPTIONAL_FN(apr_status_t,uldap_connection_unbind,(void *param));
  * Cleanup a connection to an LDAP server
  * @param ldc A structure containing the expanded details of the server
  *            that was connected.
- * @tip The connection is unlocked and returned to the list of free connections
+ * @tip This functions unbinds and closes the connection to the LDAP server
  * @fn apr_status_t util_ldap_connection_cleanup(util_ldap_connection_t *ldc)
  */
 APR_DECLARE_OPTIONAL_FN(apr_status_t,uldap_connection_cleanup,(void *param));
