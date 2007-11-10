15d1cc4ad87bd48710dfba9c2c7d302ffee2248e
Ruediger Pluem <rpluem@apache.org>
2007-11-10 22:15:34 +0000

    * Generate valid XHTML output by adding the xhtml namespace.
    PR: 43649
    Submitted by: Jose Kahan <jose w3.org>
    Reviewed by: rpluem
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@593816 13f79535-47bb-0310-9956-ffa450edef68

--- modules/generators/mod_autoindex.c
+++ modules/generators/mod_autoindex.c
@@ -160,9 +160,18 @@ static void emit_preamble(request_rec *r, int xhtml, const char *title)
     d = (autoindex_config_rec *) ap_get_module_config(r->per_dir_config,
                                                       &autoindex_module);
 
-    ap_rvputs(r, xhtml ? DOCTYPE_XHTML_1_0T : DOCTYPE_HTML_3_2,
-              "<html>\n <head>\n  <title>Index of ", title,
-              "</title>\n", NULL);
+    if (xhtml) {
+        ap_rvputs(r, DOCTYPE_XHTML_1_0T,
+		  "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n"
+		  "<head>\n  <title>Index of ", title,
+		  "</title>\n", NULL);
+    } else {
+        ap_rvputs(r, DOCTYPE_HTML_3_2,
+		  "<html>\n <head>\n"
+		  "<title>Index of ", title,
+		  "</title>\n", NULL);
+    }
+
     if (d->style_sheet != NULL) {
         ap_rvputs(r, "  <link rel=\"stylesheet\" href=\"", d->style_sheet,
                 "\" type=\"text/css\"", xhtml ? " />\n" : ">\n", NULL);
