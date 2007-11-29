dfce1044eb8c56a55ace45d72c1653494defbdca
Ruediger Pluem <rpluem@apache.org>
2007-11-29 21:48:18 +0000

    * Detabify and make spacing in HTML code the same as before r593816. This   avoids that the autoindex test (t/modules/autoindex) fails. Adjusting   the autoindex test would cause it to fail with older versions and would   make it harder to recognize regressions.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@599589 13f79535-47bb-0310-9956-ffa450edef68

--- modules/generators/mod_autoindex.c
+++ modules/generators/mod_autoindex.c
@@ -162,14 +162,14 @@ static void emit_preamble(request_rec *r, int xhtml, const char *title)
 
     if (xhtml) {
         ap_rvputs(r, DOCTYPE_XHTML_1_0T,
-		  "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n"
-		  "<head>\n  <title>Index of ", title,
-		  "</title>\n", NULL);
+                  "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n"
+                  " <head>\n  <title>Index of ", title,
+                  "</title>\n", NULL);
     } else {
         ap_rvputs(r, DOCTYPE_HTML_3_2,
-		  "<html>\n <head>\n"
-		  "<title>Index of ", title,
-		  "</title>\n", NULL);
+                  "<html>\n <head>\n"
+                  "  <title>Index of ", title,
+                  "</title>\n", NULL);
     }
 
     if (d->style_sheet != NULL) {
