a549cfb8949325ccf1bbbd77e5415aeb824585c2
Joe Orton <jorton@apache.org>
2007-11-29 16:12:59 +0000

    * modules/ssl/ssl_util_ocsp.c (serialize_request): Fix query string   handling.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@599496 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_util_ocsp.c
+++ modules/ssl/ssl_util_ocsp.c
@@ -36,11 +36,11 @@ static BIO *serialize_request(OCSP_REQUEST *req, const apr_uri_t *uri)
 
     bio = BIO_new(BIO_s_mem());
 
-    BIO_printf(bio, "POST %s%s HTTP/1.0\r\n"
+    BIO_printf(bio, "POST %s%s%s HTTP/1.0\r\n"
                "Host: %s:%d\r\n"
                "Content-Length: %d\r\n"
                "\r\n", 
-               uri->path, uri->query ? uri->query : "",
+               uri->path, uri->query ? "?" : "", uri->query ? uri->query : "",
                uri->hostname, uri->port, len);
 
     if (i2d_OCSP_REQUEST_bio(bio, req) != 1) {
