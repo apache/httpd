a4974b2524107b98bbe1767aa4a8bc7c754ceecd
Ruediger Pluem <rpluem@apache.org>
2007-12-08 20:10:29 +0000

    * Enable the proxy to keep connections persistent in the HTTPS case.
      Basicly the persistence is created by keeping the conn_rec structure
      created for our backend connection (whether http or https) in the connection
      pool. This required to adjust scoreboard.c in a way that its functions can
      properly deal with a NULL scoreboard handle by ignoring the call or returning
      an error code.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@602542 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy.h
+++ modules/proxy/mod_proxy.h
@@ -483,6 +483,8 @@ PROXY_DECLARE(apr_status_t) ap_proxy_string_read(conn_rec *c, apr_bucket_brigade
 PROXY_DECLARE(void) ap_proxy_table_unmerge(apr_pool_t *p, apr_table_t *t, char *key);
 /* DEPRECATED (will be replaced with ap_proxy_connect_backend */
 PROXY_DECLARE(int) ap_proxy_connect_to_backend(apr_socket_t **, const char *, apr_sockaddr_t *, const char *, proxy_server_conf *, server_rec *, apr_pool_t *);
+PROXY_DECLARE(apr_status_t) ap_proxy_ssl_connection_cleanup(proxy_conn_rec *conn,
+                                                            request_rec *r);
 PROXY_DECLARE(int) ap_proxy_ssl_enable(conn_rec *c);
 PROXY_DECLARE(int) ap_proxy_ssl_disable(conn_rec *c);
 PROXY_DECLARE(int) ap_proxy_conn_is_https(conn_rec *c);
