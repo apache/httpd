3635ec52e65dc464c9405a781c9d6dd8a807da5b
Mladen Turk <mturk@apache.org>
2007-12-10 07:35:52 +0000

    Use 64K as maximum allowable AJP packet size
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@602795 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/ajp.h
+++ modules/proxy/ajp.h
@@ -132,7 +132,7 @@ struct ajp_msg
 #define AJP_HEADER_SZ_LEN           2
 #define AJP_HEADER_SZ               6
 #define AJP_MSG_BUFFER_SZ           8192
-#define AJP_MAX_BUFFER_SZ           16384
+#define AJP_MAX_BUFFER_SZ           65536
 #define AJP13_MAX_SEND_BODY_SZ      (AJP_MAX_BUFFER_SZ - AJP_HEADER_SZ)
 #define AJP_PING_PONG_SZ            128
 
