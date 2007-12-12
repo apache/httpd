9a16262b10e3deb770f778700d1159a9f6937f3d
Joe Orton <jorton@apache.org>
2007-12-12 14:11:30 +0000

    Merge r603282 from trunk:
    Fix CVE-2007-5000:
    
    * modules/mappers/mod_imap.c (menu_header): Fix cross-site-scripting
      issue by escaping the URI, and ensure that a charset parameter is 
      sent in the content-type to prevent autodetection by broken browsers.
    
    Reported by: JPCERT
    Reviewed by: jorton, rpluem, trawick
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.0.x@603619 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_imap.c
+++ modules/mappers/mod_imap.c
@@ -479,13 +479,16 @@ static int imap_reply(request_rec *r, char *redirect)
 
 static void menu_header(request_rec *r, char *menu)
 {
-    ap_set_content_type(r, "text/html");
+    ap_set_content_type(r, "text/html; charset=ISO-8859-1");
 
-    ap_rvputs(r, DOCTYPE_HTML_3_2, "<html><head>\n<title>Menu for ", r->uri,
-           "</title>\n</head><body>\n", NULL);
+    ap_rvputs(r, DOCTYPE_HTML_3_2, "<html><head>\n<title>Menu for ", 
+              ap_escape_html(r->pool, r->uri),
+              "</title>\n</head><body>\n", NULL);
 
     if (!strcasecmp(menu, "formatted")) {
-        ap_rvputs(r, "<h1>Menu for ", r->uri, "</h1>\n<hr />\n\n", NULL);
+        ap_rvputs(r, "<h1>Menu for ", 
+                  ap_escape_html(r->pool, r->uri),
+                  "</h1>\n<hr />\n\n", NULL);
     }
 
     return;
