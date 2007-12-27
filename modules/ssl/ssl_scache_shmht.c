3dec7b2626e53fb47eaa5b9e4a9223250039f80a
Guenter Knauf <fuankg@apache.org>
2007-12-27 22:30:46 +0000

    fixed type mismatch between OpenSSL 0.9.7 <-> 0.9.8 and later.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.0.x@607132 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_scache_shmht.c
+++ modules/ssl/ssl_scache_shmht.c
@@ -198,7 +198,7 @@ SSL_SESSION *ssl_scache_shmht_retrieve(server_rec *s, UCHAR *id, int idlen)
     SSLModConfigRec *mc = myModConfig(s);
     void *vp;
     SSL_SESSION *sess = NULL;
-    UCHAR *ucpData;
+    MODSSL_D2I_SSL_SESSION_CONST UCHAR *ucpData;
     int nData;
     time_t expiry;
     time_t now;
@@ -223,7 +223,7 @@ SSL_SESSION *ssl_scache_shmht_retrieve(server_rec *s, UCHAR *id, int idlen)
         return NULL;
     }
     memcpy(&expiry, vp, sizeof(time_t));
-    memcpy(ucpData, (char *)vp+sizeof(time_t), nData);
+    memcpy((void *)ucpData, (char *)vp+sizeof(time_t), nData);
     ssl_mutex_off(s);
 
     /* make sure the stuff is still not expired */
