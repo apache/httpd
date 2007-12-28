245a79001d3b8f7a2aed75a9702a79a3c61f250f
Nick Kew <niq@apache.org>
2007-12-28 00:07:39 +0000

    Fix typo in error message (noticed by gryzor on IRC)
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@607145 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -3039,7 +3039,7 @@ AP_DECLARE(int) ap_is_recursion_limit_exceeded(const request_rec *r)
                 /* uuh, too much. */
                 ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                               "Request exceeded the limit of %d subrequest "
-                              "nesting levels due to probable confguration "
+                              "nesting levels due to probable configuration "
                               "error. Use 'LimitInternalRecursion' to increase "
                               "the limit if necessary. Use 'LogLevel debug' to "
                               "get a backtrace.", slimit);
