7b0cc01751cac122ef52cca49ab2c0b09b4a55c5
Ruediger Pluem <rpluem@apache.org>
2007-12-29 20:28:57 +0000

    * Style police. No functional changes.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@607472 13f79535-47bb-0310-9956-ffa450edef68

--- modules/dav/main/util.c
+++ modules/dav/main/util.c
@@ -1413,7 +1413,7 @@ static int dav_meets_conditions(request_rec *r, int resource_state)
     /* If-Match '*' fix. Resource existence not checked by ap_meets_conditions.
      * If-Match '*' request should succeed only if the resource exists. */
     if ((if_match = apr_table_get(r->headers_in, "If-Match")) != NULL) {
-        if(if_match[0] == '*' && resource_state != DAV_RESOURCE_EXISTS)
+        if (if_match[0] == '*' && resource_state != DAV_RESOURCE_EXISTS)
             return HTTP_PRECONDITION_FAILED;
     }
 
@@ -1421,14 +1421,16 @@ static int dav_meets_conditions(request_rec *r, int resource_state)
 
     /* If-None-Match '*' fix. If-None-Match '*' request should succeed 
      * if the resource does not exist. */
-    if(retVal == HTTP_PRECONDITION_FAILED) {
+    if (retVal == HTTP_PRECONDITION_FAILED) {
         /* Note. If if_none_match != NULL, if_none_match is the culprit.
          * Since, in presence of If-None-Match, 
          * other If-* headers are undefined. */
-        if((if_none_match = 
-                apr_table_get(r->headers_in, "If-None-Match")) != NULL) {
-            if(if_none_match[0] == '*' && resource_state != DAV_RESOURCE_EXISTS)
+        if ((if_none_match =
+            apr_table_get(r->headers_in, "If-None-Match")) != NULL) {
+            if (if_none_match[0] == '*'
+                && resource_state != DAV_RESOURCE_EXISTS) {
                 return OK;
+            }
         }
     }
 
