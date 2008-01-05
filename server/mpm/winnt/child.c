67d94b37bffc27f9b2dad00a4dc235078d6cceb8
William A. Rowe Jr <wrowe@apache.org>
2008-01-05 18:20:42 +0000

    winnt_mpm: Restore Win32DisableAcceptEx On directive and Win9x platform by recreating the bucket allocator each time the trans pool is cleared.
    PR: 11427 #16 (follow-on)
    Submitted by: Tom Donovan <Tom.Donovan acm.org>
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@609181 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/winnt/child.c
+++ server/mpm/winnt/child.c
@@ -450,12 +450,12 @@ static PCOMP_CONTEXT win9x_get_connection(PCOMP_CONTEXT context)
         apr_pool_create_ex(&context->ptrans, pchild, NULL, allocator);
         apr_allocator_owner_set(allocator, context->ptrans);
         apr_pool_tag(context->ptrans, "transaction");
-        context->ba = apr_bucket_alloc_create(context->ptrans);
         apr_thread_mutex_unlock(child_lock);
     }
 
     while (1) {
         apr_pool_clear(context->ptrans);
+        context->ba = apr_bucket_alloc_create(context->ptrans);
         context->accept_socket = remove_job();
         if (context->accept_socket == INVALID_SOCKET) {
             return NULL;
