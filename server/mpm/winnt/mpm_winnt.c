724fb0b8e23e9a3d3cd335fb8336e58cae53db58
William A. Rowe Jr <wrowe@apache.org>
2008-01-07 17:36:25 +0000

    winnt_mpm: Resolve modperl issues by redirecting console mode stdout to /Device/Nul as the server is starting up, mirroring unix MPM's.
    PR: 43534
    
    Submitted by: Tom Donovan <Tom.Donovan acm.org> and William Rowe
    
    Backports: r609354, r609366
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.0.x@609715 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/winnt/mpm_winnt.c
+++ server/mpm/winnt/mpm_winnt.c
@@ -1405,6 +1405,26 @@ static int winnt_pre_config(apr_pool_t *pconf_, apr_pool_t *plog, apr_pool_t *pt
                      service_name);
         exit(APEXIT_INIT);
     }
+    else if (!one_process && !ap_my_generation) {
+        /* Open a null handle to soak stdout in this process.
+         * We need to emulate apr_proc_detach, unix performs this
+         * same check in the pre_config hook (although it is
+         * arguably premature).  Services already fixed this.
+         */
+        apr_file_t *nullfile;
+        apr_status_t rv;
+        apr_pool_t *pproc = apr_pool_parent_get(pconf);
+
+        if ((rv = apr_file_open(&nullfile, "NUL",
+                                APR_READ | APR_WRITE, APR_OS_DEFAULT,
+                                pproc)) == APR_SUCCESS) {
+            apr_file_t *nullstdout;
+            if (apr_file_open_stdout(&nullstdout, pproc)
+                    == APR_SUCCESS)
+                apr_file_dup2(nullstdout, nullfile, pproc);
+            apr_file_close(nullfile);
+        }
+    }
 
     /* Win9x: disable AcceptEx */
     if (osver.dwPlatformId == VER_PLATFORM_WIN32_WINDOWS) {
