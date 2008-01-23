965e09b442d8ad9f36d367b8a072c34775a9123d
Ruediger Pluem <rpluem@apache.org>
2008-01-23 20:57:51 +0000

    Merge r529626 from trunk:
    log the uri for some access-denied paths when r->filename is
    unset
    
    Submitted by: trawick
    Reviewed by: trawick, rpluem, covener
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@614663 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_authz_host.c
+++ modules/aaa/mod_authz_host.c
@@ -297,8 +297,9 @@ static int check_dir_access(request_rec *r)
     if (ret == HTTP_FORBIDDEN
         && (ap_satisfies(r) != SATISFY_ANY || !ap_some_auth_required(r))) {
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
-            "client denied by server configuration: %s",
-            r->filename);
+            "client denied by server configuration: %s%s",
+            r->filename ? "" : "uri ",
+            r->filename ? r->filename : r->uri);
     }
 
     return ret;
