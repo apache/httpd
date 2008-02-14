b8f5af8cbe0f5d83937f036daa9a0c79444e4643
Jim Jagielski <jim@apache.org>
2008-02-14 13:37:18 +0000

    Note why we aren't bothering to check that we have a valid octet here.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@627740 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/proxy_util.c
+++ modules/proxy/proxy_util.c
@@ -677,6 +677,9 @@ static int proxy_match_ipaddr(struct dirconn_entry *This, request_rec *r)
 
     if (4 == sscanf(host, "%d.%d.%d.%d", &ip_addr[0], &ip_addr[1], &ip_addr[2], &ip_addr[3])) {
         for (addr.s_addr = 0, i = 0; i < 4; ++i) {
+            /* ap_proxy_is_ipaddr() already confirmed that we have
+             * a valid octet in ip_addr[i]
+             */
             addr.s_addr |= htonl(ip_addr[i] << (24 - 8 * i));
         }
 
