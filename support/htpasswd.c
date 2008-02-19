e195a93a548648f9d838118c64bca9a88203240b
Paul Querna <pquerna@apache.org>
2008-02-19 20:39:32 +0000

    Fix printing of error message.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@629218 13f79535-47bb-0310-9956-ffa450edef68

--- support/htpasswd.c
+++ support/htpasswd.c
@@ -132,7 +132,7 @@ static apr_status_t seed_rand()
     apr_status_t rv;
     rv = apr_generate_random_bytes((unsigned char*) &seed, sizeof(seed));
     if (rv) {
-        apr_file_printf(errfile, "Unable to generate random bytes: %pm" NL, rv);
+        apr_file_printf(errfile, "Unable to generate random bytes: %pm" NL, &rv);
         return rv;
     }
     srand(seed);
