7b44f2dd1885a0db60c99811e06e5331a782f2b1
Ruediger Pluem <rpluem@apache.org>
2008-02-22 22:58:42 +0000

    * Second part of fix for PR 44402:
      - Fix the same race condition in event MPM.
      - Slightly optimize code in worker MPM by removing the need for an additional
        dereference operation.
      - Do some word smithing on the CHANGES entry.
    
    PR: 44402
    Submitted by: Basant Kumar Kukreja <basant.kukreja sun.com>
    Reviewed by: rpluem
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@630348 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/worker/fdqueue.c
+++ server/mpm/worker/fdqueue.c
@@ -101,7 +101,7 @@ apr_status_t ap_queue_info_set_idle(fd_queue_info_t *queue_info,
             struct recycled_pool *next = queue_info->recycled_pools;
             new_recycle->next = next;
             if (apr_atomic_casptr((volatile void**)&(queue_info->recycled_pools),
-                                  new_recycle, new_recycle->next) == next) {
+                                  new_recycle, next) == next) {
                 break;
             }
         }
