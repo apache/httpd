e5143bc75a5540ccbe590caad4e7318ee7c011c2
Eric Covener <covener@apache.org>
2008-03-04 20:20:00 +0000

    return AUTHZ_GRANTED instead of OK from the LDAP nested group checking.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@633620 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_authnz_ldap.c
+++ modules/aaa/mod_authnz_ldap.c
@@ -775,7 +775,7 @@ static authz_status ldapgroup_check_authorization(request_rec *r,
                                    "[%" APR_PID_T_FMT "] auth_ldap authorise: require group (sub-group): "
                                    "authorisation successful (attribute %s) [%s][%d - %s]",
                                    getpid(), ent[i].name, ldc->reason, result, ldap_err2string(result));
-                     return OK;
+                     return AUTHZ_GRANTED;
                 }
                 else {
                     ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
