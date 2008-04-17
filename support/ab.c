d87316a2569ca8bdd40d0940c4d405dd683e7394
Jim Jagielski <jim@apache.org>
2008-04-17 14:06:27 +0000

    Merge r612954 from trunk:
    * Do not try to read non existing response bodies of HEAD requests.
    
    PR: 34275
    Submitted by: Takashi Sato <serai lans-tv.com>
    Reviewed by: rpluem
    
    Submitted by: rpluem
    Reviewed by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@649115 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -1424,7 +1424,8 @@ static void read_connection(struct connection * c)
                     cl = strstr(c->cbuff, "Content-length:");
                 if (cl) {
                     c->keepalive = 1;
-                    c->length = atoi(cl + 16);
+                    /* response to HEAD doesn't have entity body */
+                    c->length = posting >= 0 ? atoi(cl + 16) : 0;
                 }
             }
             c->bread += c->cbx - (s + l - c->cbuff) + r - tocopy;
