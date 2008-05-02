1f2f29bd131197ea4acb1c6df1246bc9b9eb7383
Jim Jagielski <jim@apache.org>
2008-05-02 17:31:15 +0000

    Merge r652832 from trunk:
    Document cvt from V7, not GNU libc, and copyright
    Caldera International Inc.
    
    Reviewed by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/1.3.x@652837 13f79535-47bb-0310-9956-ffa450edef68

--- src/ap/ap_snprintf.c
+++ src/ap/ap_snprintf.c
@@ -77,8 +77,8 @@ typedef int bool_int;
 #define NUM_BUF_SIZE		512
 
 /*
- * cvt.c - IEEE floating point formatting routines for FreeBSD
- * from GNU libc-4.6.27.  Modified to be thread safe.
+ * cvt - IEEE floating point formatting routines.
+ *       Derived from UNIX V7, Copyright(C) Caldera International Inc.
  */
 
 /*
