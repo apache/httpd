2b34d647a2db7340ca2b144a9ec5b4bab9fec127
Ruediger Pluem <rpluem@apache.org>
2008-05-14 20:08:45 +0000

    * Check for failure of apr_file_info_get call
    Reported By: BOYA SUN <boya.sun case.edu>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@656400 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -1945,7 +1945,12 @@ static int open_postfile(const char *pfile)
         return rv;
     }
 
-    apr_file_info_get(&finfo, APR_FINFO_NORM, postfd);
+    rv = apr_file_info_get(&finfo, APR_FINFO_NORM, postfd);
+    if (rv != APR_SUCCESS) {
+        fprintf(stderr, "ab: Could not stat POST data file (%s): %s\n", pfile,
+                apr_strerror(rv, errmsg, sizeof errmsg));
+        return rv;
+    }
     postlen = (apr_size_t)finfo.size;
     postdata = malloc(postlen);
     if (!postdata) {
