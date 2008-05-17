a2d857d1b80b6620f4b1dd52842854b4e7fc506a
Ruediger Pluem <rpluem@apache.org>
2008-05-17 19:50:14 +0000

    Merge r654119 from trunk:
    * modules/ssl/mod_ssl.c (ssl_cleanup_pre_config): Remove the call to
      CRYPTO_cleanup_all_ex_data here, fixing a per-connection memory leak
      which occurs if the client indicates support for a compression
      algorithm in the initial handshake, and mod_ssl is linked against
      OpenSSL >= 0.9.8f.
    
    Thanks to Amund Elstad and Dr Stephen Henson for analysis of this
    issue.
    
    Submitted by: jorton
    Reviewed by: rpluem, jim, fielding
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@657445 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/mod_ssl.c
+++ modules/ssl/mod_ssl.c
@@ -227,16 +227,17 @@ static apr_status_t ssl_cleanup_pre_config(void *data)
 #if HAVE_ENGINE_LOAD_BUILTIN_ENGINES
     ENGINE_cleanup();
 #endif
-#ifdef HAVE_OPENSSL
-#if OPENSSL_VERSION_NUMBER >= 0x00907001
-    CRYPTO_cleanup_all_ex_data();
-#endif
-#endif
     ERR_remove_state(0);
 
     /* Don't call ERR_free_strings here; ERR_load_*_strings only
      * actually load the error strings once per process due to static
      * variable abuse in OpenSSL. */
+
+    /* Also don't call CRYPTO_cleanup_all_ex_data here; any registered
+     * ex_data indices may have been cached in static variables in
+     * OpenSSL; removing them may cause havoc.  Notably, with OpenSSL
+     * versions >= 0.9.8f, COMP_CTX cleanups would not be run, which
+     * could result in a per-connection memory leak (!). */
 
     /*
      * TODO: determine somewhere we can safely shove out diagnostics
