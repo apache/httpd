e6ce7d79ba1e338252132cc5c82fa5c01818e693
Jim Jagielski <jim@apache.org>
2008-05-27 16:23:12 +0000

    Merge r636386, r636653 from trunk:
    * Restore the original request headers if the cached resource was stale, as they
      may be needed by further output filters like the byterange filter to make the
      correct decisions.
    
    PR: 44579
    
    
    * Do not do Range requests if we use our own conditionals for validating
      a cache entity: If we get 304 the Range does not matter and otherwise the
      entity changed and we want to have the complete entity.
    
    PR: 44579
    
    Submitted by: rpluem
    Reviewed by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@660584 13f79535-47bb-0310-9956-ffa450edef68

--- modules/cache/cache_storage.c
+++ modules/cache/cache_storage.c
@@ -286,6 +286,13 @@ int cache_select(request_rec *r)
                 apr_table_unset(r->headers_in, "If-Range");
                 apr_table_unset(r->headers_in, "If-Unmodified-Since");
 
+                /*
+                 * Do not do Range requests with our own conditionals: If
+                 * we get 304 the Range does not matter and otherwise the
+                 * entity changed and we want to have the complete entity
+                 */
+                apr_table_unset(r->headers_in, "Range");
+
                 etag = apr_table_get(h->resp_hdrs, "ETag");
                 lastmod = apr_table_get(h->resp_hdrs, "Last-Modified");
 
