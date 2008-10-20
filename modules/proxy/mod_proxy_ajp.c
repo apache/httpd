8dbfbbe2f017358df9d0e180de410e4485f3ffc6
Jean-Frederic Clere <jfclere@apache.org>
2008-10-20 15:11:05 +0000

    ap_proxy_determine_connection modifies the url if we are retrying the result we should retry using the orginal url.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@706318 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_ajp.c
+++ modules/proxy/mod_proxy_ajp.c
@@ -645,9 +645,10 @@ static int proxy_ajp_handler(request_rec *r, proxy_worker *worker,
 
     retry = 0;
     while (retry < 2) {
+        char *locurl = url;
         /* Step One: Determine Who To Connect To */
         status = ap_proxy_determine_connection(p, r, conf, worker, backend,
-                                               uri, &url, proxyname, proxyport,
+                                               uri, &locurl, proxyname, proxyport,
                                                server_portstr,
                                                sizeof(server_portstr));
 
@@ -685,7 +686,7 @@ static int proxy_ajp_handler(request_rec *r, proxy_worker *worker,
             }
         }
         /* Step Three: Process the Request */
-        status = ap_proxy_ajp_request(p, r, backend, origin, dconf, uri, url,
+        status = ap_proxy_ajp_request(p, r, backend, origin, dconf, uri, locurl,
                                       server_portstr);
         break;
     }
