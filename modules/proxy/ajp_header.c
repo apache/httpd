0c674a71e6f7b980cfecc217769749cec0d0cf57
Ruediger Pluem <rpluem@apache.org>
2008-10-24 16:00:10 +0000

    * save_table needs to be declared first before it can be used.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@707665 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/ajp_header.c
+++ modules/proxy/ajp_header.c
@@ -498,12 +498,14 @@ static apr_status_t ajp_unmarshal_response(ajp_msg_t *msg,
 
     rc = ajp_msg_get_uint16(msg, &num_headers);
     if (rc == APR_SUCCESS) {
+        apr_table_t *save_table;
+
         /* First, tuck away all already existing cookies */
         /*
          * Could optimize here, but just in case we want to
          * also save other headers, keep this logic.
          */
-        save_table = apr_table_make(r->pool, num_headers+2);
+        save_table = apr_table_make(r->pool, num_headers + 2);
         apr_table_do(addit_dammit, save_table, r->headers_out,
                      "Set-Cookie", NULL);
         r->headers_out = save_table;
