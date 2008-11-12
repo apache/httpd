9a8da7ff54f21dc29ca55dfd76fd840cc02223a2
Ruediger Pluem <rpluem@apache.org>
2008-11-12 20:44:53 +0000

    * The nitpicker of the space police. No functional change.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@713504 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_dir.c
+++ modules/mappers/mod_dir.c
@@ -49,18 +49,18 @@ static const char *add_index(cmd_parms *cmd, void *dummy, const char *arg)
     dir_config_rec *d = dummy;
     const char *t, *w;
     int count = 0;
-   
+
     if (!d->index_names) {
         d->index_names = apr_array_make(cmd->pool, 2, sizeof(char *));
     }
 
     t = arg;
-    while ((w = ap_getword_conf(cmd->pool, &t)) && w[0]) { 
-        if (count == 0 && !strcasecmp(w, "disabled")) { 
+    while ((w = ap_getword_conf(cmd->pool, &t)) && w[0]) {
+        if (count == 0 && !strcasecmp(w, "disabled")) {
             /* peek to see if "disabled" is first in a series of arguments */
             const char *tt = t;
             const char *ww = ap_getword_conf(cmd->pool, &tt);
-            if (ww == NULL || !ww[0]) { 
+            if (ww == NULL || !ww[0]) {
                /* "disabled" is first, and alone */
                break;
             }
