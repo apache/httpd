745228b021f991b4b009007765dacf4e54fc61a4
Jim Jagielski <jim@apache.org>
2008-11-24 18:33:09 +0000

    Make %k work as it should. No regression noted in perl test framework.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@720250 13f79535-47bb-0310-9956-ffa450edef68

--- modules/loggers/mod_log_config.c
+++ modules/loggers/mod_log_config.c
@@ -697,7 +697,8 @@ static const char *log_connection_status(request_rec *r, char *a)
 
 static const char *log_requests_on_connection(request_rec *r, char *a)
 {
-    return apr_itoa(r->pool, r->connection->keepalives);
+    int num = r->connection->keepalives ? r->connection->keepalives - 1 : 0;
+    return apr_itoa(r->pool, num);
 }
 
 /*****************************************************************
