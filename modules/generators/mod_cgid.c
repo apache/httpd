apache/httpd

commit 74651b261bc36b07da00b71a02e8250455f314bd

Simplyfy codepath. Don't treat "" as special, just plug along. This matches the codepath in mod_cgi.c
git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@682475 13f79535-47bb-0310-9956-ffa450edef68

modules/generators/mod_cgid.c
@@ -233,13 +233,8 @@ static char **create_argv(apr_pool_t *p, char *path, char *user, char *group,
 
     for (x = 1; x <= numwords; x++) {
         w = ap_getword_nulls(p, &args, '+');
-        if (strcmp(w, "")) {
-            ap_unescape_url(w);
-            av[idx++] = ap_escape_shell_cmd(p, w);
-        }
-        else { 
-            av[idx++] = w;
-        }
+        ap_unescape_url(w);
+        av[idx++] = ap_escape_shell_cmd(p, w);
     }
     av[idx] = NULL;
     return av;

commit 25a74cc4b3edae4889b728cbbbacc30981552440

* Do not add an empty argument if we do not have any args for the script.   Fixes a regression from r682475.
PR: 46380


git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@726636 13f79535-47bb-0310-9956-ffa450edef68

modules/generators/mod_cgid.c
@@ -201,7 +201,7 @@ static char **create_argv(apr_pool_t *p, char *path, char *user, char *group,
     char *w;
     int idx = 0;
 
-    if (ap_strchr_c(args, '=')) {
+    if (!(*args) || ap_strchr_c(args, '=')) {
         numwords = 0;
     }
     else {