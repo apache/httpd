2a4df9b2b9f3cdef1e4f2d38262575e9f05d76bf
Jim Jagielski <jim@apache.org>
2008-12-29 16:25:04 +0000

    No need to sep out all the shared mem stuff... clean up
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@729929 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mem/modules.mk
+++ modules/mem/modules.mk
@@ -1,5 +1,5 @@
-libmod_sharedmem.la: mod_sharedmem.lo sharedmem_util.lo
-	$(MOD_LINK) mod_sharedmem.lo sharedmem_util.lo $(MOD_SHAREDMEM_LDADD)
+libmod_sharedmem.la: mod_sharedmem.lo
+	$(MOD_LINK) mod_sharedmem.lo $(MOD_SHAREDMEM_LDADD)
 DISTCLEAN_TARGETS = modules.mk
 static =  libmod_sharedmem.la
 shared = 
