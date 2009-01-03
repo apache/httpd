83a786f6ad5ea2137cfa9eae71810549419188df
Ruediger Pluem <rpluem@apache.org>
2009-01-03 09:54:07 +0000

    * APR_TCP_DEFER_ACCEPT is always defined. So this is pointless.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@730951 13f79535-47bb-0310-9956-ffa450edef68

--- server/listen.c
+++ server/listen.c
@@ -223,13 +223,11 @@ static void ap_apply_accept_filter(apr_pool_t *p, ap_listen_rec *lis,
                           accf);
         }
 #else
-#ifdef APR_TCP_DEFER_ACCEPT
         rv = apr_socket_opt_set(s, APR_TCP_DEFER_ACCEPT, 30);
         if (rv != APR_SUCCESS && !APR_STATUS_IS_ENOTIMPL(rv)) {
             ap_log_perror(APLOG_MARK, APLOG_WARNING, rv, p,
                               "Failed to enable APR_TCP_DEFER_ACCEPT");
         }
-#endif
 #endif
     }
 }
