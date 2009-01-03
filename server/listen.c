127f2215bb2de86ce787ce3651614a8997d4a2d5
Ruediger Pluem <rpluem@apache.org>
2009-01-03 09:52:12 +0000

    * Handle the case where conf->accf_map is NULL gracefully instead of segfaulting
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@730949 13f79535-47bb-0310-9956-ffa450edef68

--- server/listen.c
+++ server/listen.c
@@ -183,7 +183,7 @@ static const char* find_accf_name(server_rec *s, const char *proto)
     const char* accf;
     core_server_config *conf = ap_get_module_config(s->module_config,
                                                     &core_module);
-    if (!proto) {
+    if (!proto || !conf->accf_map) {
         return NULL;
     }
 
