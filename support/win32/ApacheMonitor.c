9b609e2b866c2c7f3efb22cf31c1ec8bee336c9a
William A. Rowe Jr <wrowe@apache.org>
2009-01-05 20:41:10 +0000

    more fallout from removing win9x-model services, this is no longer used
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@731697 13f79535-47bb-0310-9956-ffa450edef68

--- support/win32/ApacheMonitor.c
+++ support/win32/ApacheMonitor.c
@@ -537,32 +537,6 @@ static void addListBoxString(HWND hListBox, LPTSTR lpStr)
 }
 
 
-#ifndef UNICODE
-#define addListBoxStringA addListBoxString
-#else
-static void addListBoxStringA(HWND hListBox, LPSTR lpStr)
-{
-    static int nItems = 0;
-    TCHAR WStr[16384];
-
-    if (!g_bDlgServiceOn) {
-        return;
-    }
-    if (!MultiByteToWideChar(CP_ACP, 0, lpStr, (int)strlen(lpStr) + 1,
-                             WStr, (int) (sizeof(WStr) / sizeof(TCHAR))))
-        return;
-    ++nItems;
-    if (nItems > MAX_LOADSTRING)
-    {
-        SendMessage(hListBox, LB_RESETCONTENT, 0, 0);
-        nItems = 1;
-    }
-    ListBox_SetCurSel(hListBox,
-                      ListBox_AddString(hListBox, WStr));
-}
-#endif
-
-
 BOOL ApacheManageService(LPCTSTR szServiceName, LPCTSTR szImagePath,
                          LPTSTR szComputerName, DWORD dwCommand)
 {
