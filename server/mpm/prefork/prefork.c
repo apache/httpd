df6fa487203d9179898480e9c44d42cc8ece7e67
Jeff Trawick <trawick@apache.org>
2009-01-07 18:22:07 +0000

    As jorton discovered, recent Linux kernels have a new tunable setting for epoll that will trip up many prefork users.  Write a log message instead of crashing.
    PR: 46467
    
    As rpluem notes, some Apache doc is in order.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@732414 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/prefork/prefork.c
+++ server/mpm/prefork/prefork.c
@@ -484,8 +484,12 @@ static void child_main(int child_num_arg)
     (void) ap_update_child_status(sbh, SERVER_READY, (request_rec *) NULL);
 
     /* Set up the pollfd array */
-    /* ### check the status */
-    (void) apr_pollset_create(&pollset, num_listensocks, pchild, 0);
+    status = apr_pollset_create(&pollset, num_listensocks, pchild, 0);
+    if (status != APR_SUCCESS) {
+        ap_log_error(APLOG_MARK, APLOG_EMERG, status, ap_server_conf,
+                     "Couldn't create pollset in child; check system or user limits");
+        clean_child_exit(APEXIT_CHILDSICK); /* assume temporary resource issue */
+    }
 
     for (lr = ap_listeners, i = num_listensocks; i--; lr = lr->next) {
         apr_pollfd_t pfd = { 0 };
