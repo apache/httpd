a6e008c9e2a89d4ade1c42a3a9558fa06acba1ea
Jeff Trawick <trawick@apache.org>
2009-01-25 21:35:44 +0000

    fix spelling in debug log message
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@737582 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ldap/util_ldap.c
+++ modules/ldap/util_ldap.c
@@ -2353,7 +2353,7 @@ static const char *util_ldap_set_chase_referrals(cmd_parms *cmd,
     util_ldap_config_t *dc =  config;
 
     ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, cmd->server,
-                      "LDAP: Setting refferal chasing %s",
+                      "LDAP: Setting referral chasing %s",
                       (mode == AP_LDAP_CHASEREFERRALS_ON) ? "ON" : "OFF");
 
     dc->ChaseReferrals = mode;
