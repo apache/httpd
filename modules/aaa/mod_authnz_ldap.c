0320cc13d1c55d663eb2c733c2d6e2ab4f6fcfdc
Jeff Trawick <trawick@apache.org>
2009-02-17 13:59:30 +0000

    fix the name of this module in an error log message
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@745073 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_authnz_ldap.c
+++ modules/aaa/mod_authnz_ldap.c
@@ -1505,7 +1505,7 @@ static int authnz_ldap_post_config(apr_pool_t *p, apr_pool_t *plog, apr_pool_t *
     if (ap_find_linked_module("util_ldap.c") == NULL) {
         ap_log_error(APLOG_MARK, APLOG_ERR, 0, s,
                      "Module mod_ldap missing. Mod_ldap (aka. util_ldap) "
-                     "must be loaded in order for mod_auth_ldap to function properly");
+                     "must be loaded in order for mod_authnz_ldap to function properly");
         return HTTP_INTERNAL_SERVER_ERROR;
 
     }
