323483ccf2d6f106d58d3a419808a7901e8a91cd
Ruediger Pluem <rpluem@apache.org>
2009-03-04 20:05:09 +0000

    * remove uneeded remains of r750126.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@750132 13f79535-47bb-0310-9956-ffa450edef68

--- modules/debugging/mod_dumpio.c
+++ modules/debugging/mod_dumpio.c
@@ -130,8 +130,6 @@ static int dumpio_input_filter (ap_filter_t *f, apr_bucket_brigade *bb,
     apr_status_t ret;
     conn_rec *c = f->c;
     dumpio_conf_t *ptr = f->ctx;
-    (dumpio_conf_t *) ap_get_module_config(c->base_server->module_config,
-                                           &dumpio_module);
 
     ap_log_error(APLOG_MARK, ptr->loglevel, 0, c->base_server,
         "mod_dumpio: %s [%s-%s] %" APR_OFF_T_FMT " readbytes",
@@ -159,8 +157,6 @@ static int dumpio_output_filter (ap_filter_t *f, apr_bucket_brigade *bb)
     apr_bucket *b;
     conn_rec *c = f->c;
     dumpio_conf_t *ptr = f->ctx;
-    (dumpio_conf_t *) ap_get_module_config(c->base_server->module_config,
-                                           &dumpio_module);
 
     ap_log_error(APLOG_MARK, ptr->loglevel, 0, c->base_server, "mod_dumpio: %s", f->frec->name) ;
 
