3af5ffb3ebddd11ef971fe83a2c0349fb75a4da6
Mladen Turk <mturk@apache.org>
2009-03-30 06:38:14 +0000

    Use correct check logic
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@759849 13f79535-47bb-0310-9956-ffa450edef68

--- modules/cluster/mod_heartbeat.c
+++ modules/cluster/mod_heartbeat.c
@@ -136,7 +136,7 @@ static int hb_watchdog_step(server_rec *s, const char *name, apr_pool_t *pool)
     apr_status_t rv;
     hb_ctx_t *ctx = ap_get_module_config(s->module_config, &heartbeat_module);
 
-    if (!ctx->active && strcmp(name, AP_WATCHDOG_SINGLETON)) {
+    if (!ctx->active || strcmp(name, AP_WATCHDOG_SINGLETON)) {
         return OK;
     }
     return hb_monitor(ctx, pool);
