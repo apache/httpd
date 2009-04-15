37c3a15d0d9e15a4dbb4cc16c5048edcfdf66765
Greg Ames <gregames@apache.org>
2009-04-15 18:52:39 +0000

    no longer amusing.
    I'll still pay off if somebody does it legitimately, but I haven't been
    to Apachecon in ages so we'll have to work out a different delivery
    mechanism.  With the stability of the 2.0.x and 2.2.x releases and
    Moore's Law in effect for the last decade, it might be possible...
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@765305 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/event/event.c
+++ server/mpm/event/event.c
@@ -772,17 +772,6 @@ static void check_infinite_requests(void)
         signal_threads(ST_GRACEFUL);
     }
     else {
-        /* wow! if you're executing this code, you may have set a record.
-         * either this child process has served over 2 billion requests, or
-         * you're running a threaded 2.0 on a 16 bit machine.
-         *
-         * I'll buy pizza and beers at Apachecon for the first person to do
-         * the former without cheating (dorking with INT_MAX, or running with
-         * uncommitted performance patches, for example).
-         *
-         * for the latter case, you probably deserve a beer too.   Greg Ames
-         */
-
         requests_this_child = INT_MAX;  /* keep going */
     }
 }
