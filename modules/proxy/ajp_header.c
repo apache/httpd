bbc16c8d6d1cf46df80682383729c49ec71160cb
Jim Jagielski <jim@apache.org>
2009-04-17 13:40:08 +0000

    Merge r761329 from trunk:
    The remote port information was forgotten from the builtin
    data of the AJP 13 protocol.  Since the servlet spec allows
    to retrieve it via getRemotePort(), we provide the port to
    the Tomcat connector as an additional request attribute.
    Modern/future Tomcat versions know how to retrieve
    the remote port from the attribute AJP_REMOTE_PORT.
    
    See:
    https://issues.apache.org/bugzilla/show_bug.cgi?id=41263
    http://svn.eu.apache.org/viewvc?view=rev&revision=756926
    http://svn.eu.apache.org/viewvc?view=rev&revision=757223
    
    Submitted by: rjung
    Reviewed/backported by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@765998 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/ajp_header.c
+++ modules/proxy/ajp_header.c
@@ -403,6 +403,26 @@ static apr_status_t ajp_marshal_into_msgb(ajp_msg_t *msg,
             }
         }
     }
+    /* Forward the remote port information, which was forgotten
+     * from the builtin data of the AJP 13 protocol.
+     * Since the servlet spec allows to retrieve it via getRemotePort(),
+     * we provide the port to the Tomcat connector as a request
+     * attribute. Modern Tomcat versions know how to retrieve
+     * the remote port from this attribute.
+     */
+    {
+        const char *key = SC_A_REQ_REMOTE_PORT;
+        char *val = apr_itoa(r->pool, r->connection->remote_addr->port);
+        if (ajp_msg_append_uint8(msg, SC_A_REQ_ATTRIBUTE) ||
+            ajp_msg_append_string(msg, key)   ||
+            ajp_msg_append_string(msg, val)) {
+            ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server,
+                    "ajp_marshal_into_msgb: "
+                    "Error appending attribute %s=%s",
+                    key, val);
+            return AJP_EOVERFLOW;
+        }
+    }
     /* Use the environment vars prefixed with AJP_
      * and pass it to the header striping that prefix.
      */
