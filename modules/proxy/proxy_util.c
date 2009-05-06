124dd1c76fbbf5edead1c520a80aacb0147c48af
Ruediger Pluem <rpluem@apache.org>
2009-05-06 10:06:06 +0000

    * Fix syntax error
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@772126 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/proxy_util.c
+++ modules/proxy/proxy_util.c
@@ -1476,7 +1476,7 @@ ap_proxy_add_worker_to_balancer(apr_pool_t *pool, proxy_balancer *balancer,
     proxy_worker **runtime;
 
     runtime = apr_array_push(balancer->workers);
-    *runtime = worker
+    *runtime = worker;
     (*runtime)->id = proxy_lb_workers;
     /* Increase the total runtime count */
     proxy_lb_workers++;
