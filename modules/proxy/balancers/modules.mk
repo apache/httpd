0829152d5d486d7d5847844a9d720f12691a856b
Jim Jagielski <jim@apache.org>
2009-05-06 15:56:31 +0000

    Autogen'ed
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/httpd-2.2-proxy@772331 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/balancers/modules.mk
+++
@@ -1,9 +0,0 @@
-libmod_lbmethod_byrequests.la: mod_lbmethod_byrequests.lo
-	$(MOD_LINK) mod_lbmethod_byrequests.lo $(MOD_LBMETHOD_BYREQUESTS_LDADD)
-libmod_lbmethod_bytraffic.la: mod_lbmethod_bytraffic.lo
-	$(MOD_LINK) mod_lbmethod_bytraffic.lo $(MOD_LBMETHOD_BYTRAFFIC_LDADD)
-libmod_lbmethod_bybusyness.la: mod_lbmethod_bybusyness.lo
-	$(MOD_LINK) mod_lbmethod_bybusyness.lo $(MOD_LBMETHOD_BYBUSYNESS_LDADD)
-DISTCLEAN_TARGETS = modules.mk
-static =  libmod_lbmethod_byrequests.la libmod_lbmethod_bytraffic.la libmod_lbmethod_bybusyness.la
-shared = 
