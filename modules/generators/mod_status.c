c2cd448eeecda152e9d6832dcd72e54b32bc33ff
William A. Rowe Jr <wrowe@apache.org>
2009-05-15 21:09:21 +0000

    Quit hiding the previous request information while the subsequent request is partially read
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@775341 13f79535-47bb-0310-9956-ffa450edef68

--- modules/generators/mod_status.c
+++ modules/generators/mod_status.c
@@ -750,19 +750,15 @@ static int status_handler(request_rec *r)
                                (float)conn_bytes / KBYTE, (float) my_bytes / MBYTE,
                                (float)bytes / MBYTE);
 
-                    if (ws_record->status == SERVER_BUSY_READ)
-                        ap_rprintf(r,
-                                   "</td><td>?</td><td nowrap>?</td><td nowrap>..reading.. </td></tr>\n\n");
-                    else
-                        ap_rprintf(r,
-                                   "</td><td>%s</td><td nowrap>%s</td><td nowrap>%s</td></tr>\n\n",
-                                   ap_escape_html(r->pool,
-                                                  ws_record->client),
-                                   ap_escape_html(r->pool,
-                                                  ws_record->vhost),
-                                   ap_escape_html(r->pool,
-                                                  ap_escape_logitem(r->pool, 
-                                                                    ws_record->request)));
+                    ap_rprintf(r, "</td><td>%s</td><td nowrap>%s</td>"
+                                  "<td nowrap>%s</td></tr>\n\n",
+                               ap_escape_html(r->pool,
+                                              ws_record->client),
+                               ap_escape_html(r->pool,
+                                              ws_record->vhost),
+                               ap_escape_html(r->pool,
+                                              ap_escape_logitem(r->pool, 
+                                                      ws_record->request)));
                 } /* no_table_report */
             } /* for (j...) */
         } /* for (i...) */
