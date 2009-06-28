0e5ae01eeb2ca2e9148b04a59a1cfe9a01f0acf0
Paul Querna <pquerna@apache.org>
2009-06-28 08:13:43 +0000

    server/mpm/simple/simple_run.c     (simple_setup_pollcb): Only pollcbs created using KQueue, EPoll or Event Ports operate correctly with removing sockets across threads.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@789064 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/simple/simple_run.c
+++ server/mpm/simple/simple_run.c
@@ -254,14 +254,18 @@ static int simple_setup_privs(simple_core_t * sc)
 
 static int simple_setup_pollcb(simple_core_t * sc)
 {
+    int i;
     apr_status_t rv;
+    int good_methods[] = {APR_POLLSET_KQUEUE, APR_POLLSET_PORT, APR_POLLSET_EPOLL};
 
-    rv = apr_pollcb_create(&sc->pollcb, 512 /* pqXXXXX: make configrable */ ,
-                           sc->pool, 0);
-    
+    for (i = 0; i < sizeof(good_methods) / sizeof(void*); i++) {
+        /* pqXXXXX: make size of pollcb configrable or dynamic */
+        rv = apr_pollcb_create_ex(&sc->pollcb, 512,
+                                  sc->pool, APR_POLLSET_NODEFAULT, good_methods[i]);
+    }
     if (rv) {
         ap_log_error(APLOG_MARK, APLOG_CRIT, rv, NULL,
-                     "simple_setup_pollcb: apr_pollcb_create failed.");
+                   "simple_setup_pollcb: apr_pollcb_create failed for all possible backends!");
         return rv;
     }
     return rv;
