0fcc94dd0136b951fd5fc7a874ceb3e85850eaa7
William A. Rowe Jr <wrowe@apache.org>
2009-07-06 20:22:49 +0000

    Use uninterruptable write; noted borrowing this code for lowportd (mod_ftp)
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@791604 13f79535-47bb-0310-9956-ffa450edef68

--- modules/generators/mod_cgid.c
+++ modules/generators/mod_cgid.c
@@ -730,7 +730,7 @@ static int cgid_server(void *data)
             pid_t pid;
 
             pid = (pid_t)((long)apr_hash_get(script_hash, &cgid_req.conn_id, sizeof(cgid_req.conn_id)));
-            if (write(sd2, &pid, sizeof(pid)) != sizeof(pid)) {
+            if (sock_write(sd2, &pid, sizeof(pid)) != sizeof(pid)) {
                 ap_log_error(APLOG_MARK, APLOG_ERR, 0,
                              main_server,
                              "Error writing pid %" APR_PID_T_FMT " to handler", pid);
