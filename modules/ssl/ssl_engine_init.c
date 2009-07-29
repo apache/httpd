186874c25d9fc2ac5192909c53db50228c7fb25a
Guenter Knauf <fuankg@apache.org>
2009-07-29 17:42:49 +0000

    removed another obsolete cast. Mentioned by Peter Sylvester.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@798989 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_init.c
+++ modules/ssl/ssl_engine_init.c
@@ -584,7 +584,7 @@ static void ssl_init_ctx_verify(server_rec *s,
      * should take place. This cannot work.
      */
     if (mctx->auth.verify_mode == SSL_CVERIFY_REQUIRE) {
-        ca_list = (STACK_OF(X509_NAME) *)SSL_CTX_get_client_CA_list(ctx);
+        ca_list = SSL_CTX_get_client_CA_list(ctx);
 
         if (sk_X509_NAME_num(ca_list) == 0) {
             ap_log_error(APLOG_MARK, APLOG_WARNING, 0, s,
