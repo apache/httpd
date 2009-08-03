apache/httpd

commit 74651b261bc36b07da00b71a02e8250455f314bd

Simplyfy codepath. Don't treat "" as special, just plug along. This matches the codepath in mod_cgi.c
git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@682475 13f79535-47bb-0310-9956-ffa450edef68

modules/generators/mod_cgid.c
@@ -233,13 +233,8 @@ static char **create_argv(apr_pool_t *p, char *path, char *user, char *group,
 
     for (x = 1; x <= numwords; x++) {
         w = ap_getword_nulls(p, &args, '+');
-        if (strcmp(w, "")) {
-            ap_unescape_url(w);
-            av[idx++] = ap_escape_shell_cmd(p, w);
-        }
-        else { 
-            av[idx++] = w;
-        }
+        ap_unescape_url(w);
+        av[idx++] = ap_escape_shell_cmd(p, w);
     }
     av[idx] = NULL;
     return av;

commit 7308d5e889b5f921b7cd6d167433470fd4b69ba6

Merge r726636 from trunk:
* Do not add an empty argument if we do not have any args for the script.
  Fixes a regression from r682475.

PR: 46380

Reviewed by: rpluem, covener, minfrin


git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@800410 13f79535-47bb-0310-9956-ffa450edef68

STATUS
@@ -84,14 +84,6 @@ RELEASE SHOWSTOPPERS:
 PATCHES ACCEPTED TO BACKPORT FROM TRUNK:
   [ start all new proposals below, under PATCHES PROPOSED. ]
 
- * mod_cgid: Do not add an empty argument when calling the CGI script.
-   PR 46380 (fixes a regression of r682475 (trunk) / r686392 (2.2.x).
-   Trunk version of patch:
-      http://svn.apache.org/viewvc?rev=726636&view=rev
-   Backport version for 2.2.x of patch:
-      Trunk version of patch works
-   +1: rpluem, covener, minfrin
-
 PATCHES PROPOSED TO BACKPORT FROM TRUNK:
   [ New proposals should be added at the end of the list ]
 

modules/generators/mod_cgid.c
@@ -203,7 +203,7 @@ static char **create_argv(apr_pool_t *p, char *path, char *user, char *group,
     char *w;
     int idx = 0;
 
-    if (ap_strchr_c(args, '=')) {
+    if (!(*args) || ap_strchr_c(args, '=')) {
         numwords = 0;
     }
     else {