256f3e913b4b80d3d45e6b472ca13594fa8f5cbd
Jeff Trawick <trawick@apache.org>
2009-08-20 19:25:49 +0000

    fix wrong name in function prototype which led to unresolved reference at link time
    hint at adding some doc as well
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@806315 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_listen.h
+++ include/ap_listen.h
@@ -95,7 +95,11 @@ AP_DECLARE(int) ap_setup_listeners(server_rec *s);
  * Loop through the global ap_listen_rec list and close each of the sockets.
  */
 AP_DECLARE_NONSTD(void) ap_close_listeners(void);
-AP_DECLARE_NONSTD(int) ap_close_listeners_selected(ap_slave_t*);
+
+/**
+ * FIXMEDOC
+ */
+AP_DECLARE_NONSTD(int) ap_close_selected_listeners(ap_slave_t *);
 
 /* Although these functions are exported from libmain, they are not really
  * public functions.  These functions are actually called while parsing the
