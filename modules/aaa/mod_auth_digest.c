2d76c3ec6961b1d1d37d2c52ffd8bf972389b2d4
Daniel Earl Poirier <poirier@apache.org>
2009-08-28 14:42:25 +0000

    Make code match warning message: disable nonce-checking if shared memory isn't available.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@808903 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_auth_digest.c
+++ modules/aaa/mod_auth_digest.c
@@ -592,11 +592,13 @@ static const char *set_nonce_format(cmd_parms *cmd, void *config,
 
 static const char *set_nc_check(cmd_parms *cmd, void *config, int flag)
 {
-    if (flag && !client_shm)
+    if (flag && !client_shm) {
         ap_log_error(APLOG_MARK, APLOG_WARNING, 0,
                      cmd->server, "Digest: WARNING: nonce-count checking "
                      "is not supported on platforms without shared-memory "
                      "support - disabling check");
+        flag = 0;
+    }
 
     ((digest_config_rec *) config)->check_nc = flag;
     return NULL;
