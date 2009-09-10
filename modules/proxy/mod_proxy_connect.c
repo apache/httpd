6a047020ca1f73b120eb21933aadf39dc0de6d13
Ruediger Pluem <rpluem@apache.org>
2009-09-10 07:00:27 +0000

    * More style fixes. No functional changes.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@813265 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_connect.c
+++ modules/proxy/mod_proxy_connect.c
@@ -73,7 +73,7 @@ static int proxy_connect_canon(request_rec *r, char *url)
 
 /* read available data (in blocks of CONN_BLKSZ) from c_i and copy to c_o */
 static int proxy_connect_transfer(request_rec *r, conn_rec *c_i, conn_rec *c_o,
-				  apr_bucket_brigade *bb, char *name)
+                                  apr_bucket_brigade *bb, char *name)
 {
     int rv;
 #ifdef DEBUGGING
@@ -81,36 +81,37 @@ static int proxy_connect_transfer(request_rec *r, conn_rec *c_i, conn_rec *c_o,
 #endif
 
     do {
-	apr_brigade_cleanup(bb);
-	rv = ap_get_brigade(c_i->input_filters, bb, AP_MODE_READBYTES,
-			    APR_NONBLOCK_READ, CONN_BLKSZ);
-	if (rv == APR_SUCCESS) {
-	    if (APR_BRIGADE_EMPTY(bb))
-		break;
+        apr_brigade_cleanup(bb);
+        rv = ap_get_brigade(c_i->input_filters, bb, AP_MODE_READBYTES,
+                            APR_NONBLOCK_READ, CONN_BLKSZ);
+        if (rv == APR_SUCCESS) {
+            if (APR_BRIGADE_EMPTY(bb))
+                break;
 #ifdef DEBUGGING
-	    len = -1;
-	    apr_brigade_length(bb, 0, &len);
-	    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
-			  "proxy: CONNECT: read %" APR_OFF_T_FMT
-			  " bytes from %s", len, name);
+            len = -1;
+            apr_brigade_length(bb, 0, &len);
+            ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
+                          "proxy: CONNECT: read %" APR_OFF_T_FMT
+                          " bytes from %s", len, name);
 #endif
-	    rv = ap_pass_brigade(c_o->output_filters, bb);
-	    if (rv == APR_SUCCESS) {
-		ap_fflush(c_o->output_filters, bb);
-	    } else {
-		ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r,
-			      "proxy: CONNECT: error on %s - ap_pass_brigade",
-			      name);
-	    }
-	} else if (!APR_STATUS_IS_EAGAIN(rv)) {
-	    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, rv, r,
-			  "proxy: CONNECT: error on %s - ap_get_brigade",
-			  name);
-	}
+            rv = ap_pass_brigade(c_o->output_filters, bb);
+            if (rv == APR_SUCCESS) {
+                ap_fflush(c_o->output_filters, bb);
+            }
+            else {
+                ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r,
+                              "proxy: CONNECT: error on %s - ap_pass_brigade",
+                              name);
+            }
+        } else if (!APR_STATUS_IS_EAGAIN(rv)) {
+            ap_log_rerror(APLOG_MARK, APLOG_DEBUG, rv, r,
+                          "proxy: CONNECT: error on %s - ap_get_brigade",
+                          name);
+        }
     } while (rv == APR_SUCCESS);
 
     if (APR_STATUS_IS_EAGAIN(rv)) {
-	rv = APR_SUCCESS;
+        rv = APR_SUCCESS;
     }
     return rv;
 }
