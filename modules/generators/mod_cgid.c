c5e09c5987f3a34b19dc89253626338b19340ac0
Takashi Sato <takashi@apache.org>
2009-10-10 05:52:17 +0000

    Stop unexpected error logs in each requests. sock_write returns not size but status.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@823797 13f79535-47bb-0310-9956-ffa450edef68

--- modules/generators/mod_cgid.c
+++ modules/generators/mod_cgid.c
@@ -728,10 +728,12 @@ static int cgid_server(void *data)
 
         if (cgid_req.req_type == GETPID_REQ) {
             pid_t pid;
+            apr_status_t rv;
 
             pid = (pid_t)((long)apr_hash_get(script_hash, &cgid_req.conn_id, sizeof(cgid_req.conn_id)));
-            if (sock_write(sd2, &pid, sizeof(pid)) != sizeof(pid)) {
-                ap_log_error(APLOG_MARK, APLOG_ERR, 0,
+            rv = sock_write(sd2, &pid, sizeof(pid));
+            if (rv != APR_SUCCESS) {
+                ap_log_error(APLOG_MARK, APLOG_ERR, rv,
                              main_server,
                              "Error writing pid %" APR_PID_T_FMT " to handler", pid);
             }
