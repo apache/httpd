d55490d01fa3ff356174b66b53c572359071b750
Stefan Fritsch <sf@apache.org>
2009-10-18 19:02:20 +0000

    ab: Fix segfault in case the argument for -n is a very large number.
    PR: 47178
    Submitted by: Philipp Hagemeister <oss phihag.de>
    Reviewed by: Stefan Fritsch
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@826497 13f79535-47bb-0310-9956-ffa450edef68

--- support/ab.c
+++ support/ab.c
@@ -1593,7 +1593,14 @@ static void test(void)
 
     con = calloc(concurrency, sizeof(struct connection));
 
+    /*
+     * XXX: a way to calculate the stats without requiring O(requests) memory
+     * XXX: would be nice.
+     */
     stats = calloc(requests, sizeof(struct data));
+    if (stats == NULL) {
+    	err("Cannot allocate memory for result statistics");
+    }
 
     if ((status = apr_pollset_create(&readbits, concurrency, cntxt,
                                      APR_POLLSET_NOCOPY)) != APR_SUCCESS) {
