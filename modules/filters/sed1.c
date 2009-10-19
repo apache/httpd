9c175a59e54d2fcfa24874fa18c85a674275cc42
Ruediger Pluem <rpluem@apache.org>
2009-10-19 19:22:55 +0000

    * Reduce memory consumption when processing very long lines by at least   doubleing the size of the new buffer each time.
    PR: 48024
    Submitted by: Basant Kumar Kukreja <basant.kukreja sun.com>
    Reviewed by: rpluem
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@826772 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/sed1.c
+++ modules/filters/sed1.c
@@ -99,6 +99,12 @@ static void grow_buffer(apr_pool_t *pool, char **buffer,
     int spendsize = 0;
     if (*cursize >= newsize)
         return;
+    /* Avoid number of times realloc is called. It could cause huge memory
+     * requirement if line size is huge e.g 2 MB */
+    if (newsize < *cursize * 2) {
+        newsize = *cursize * 2;
+    }
+
     /* Align it to 4 KB boundary */
     newsize = (newsize  + ((1 << 12) - 1)) & ~((1 << 12) -1);
     newbuffer = apr_pcalloc(pool, newsize);
