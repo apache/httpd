2c9a9d53cf2c843210bfdc26be9dec8ab5dfb0e1
Stefan Fritsch <sf@apache.org>
2009-10-19 21:41:31 +0000

    htpasswd: Improve out of disk space handling
    PR: 30877
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@826822 13f79535-47bb-0310-9956-ffa450edef68

--- support/htpasswd.c
+++ support/htpasswd.c
@@ -141,7 +141,15 @@ static apr_status_t seed_rand(void)
 
 static void putline(apr_file_t *f, const char *l)
 {
-    apr_file_puts(l, f);
+    apr_status_t rc;
+    rc = apr_file_puts(l, f);
+    if (rc != APR_SUCCESS) {
+        char errstr[MAX_STRING_LEN];
+        apr_strerror(rc, errstr, MAX_STRING_LEN);
+        apr_file_printf(errfile, "Error writing temp file: %s" NL, errstr);
+        apr_file_close(f);
+        exit(ERR_FILEPERM);
+    }
 }
 
 /*
