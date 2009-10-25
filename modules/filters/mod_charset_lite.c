5162a4383848bae3a650dcdb2e892bd453045187
Eric Covener <covener@apache.org>
2009-10-25 22:06:18 +0000

    CharsetOptions NoImplicitAdd wasn't referenced during insert_filter callback.
    Doc: Correct defaults, charset_lite inserts itself by as soon as you tell it about
    input and output character sets.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@829664 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/mod_charset_lite.c
+++ modules/filters/mod_charset_lite.c
@@ -337,6 +337,15 @@ static void xlate_insert_filter(request_rec *r)
     charset_dir_t *dc = ap_get_module_config(r->per_dir_config,
                                              &charset_lite_module);
 
+    if (dc && (dc->implicit_add == IA_NOIMPADD)) { 
+        if (dc->debug >= DBGLVL_GORY) {
+            ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
+                          "xlate output filter not added implicitly because "
+                          "CharsetOptions included 'NoImplicitAdd'");
+        }
+        return;
+    }
+
     if (reqinfo) {
         if (reqinfo->output_ctx && !configured_on_output(r, XLATEOUT_FILTER_NAME)) {
             ap_add_output_filter(XLATEOUT_FILTER_NAME, reqinfo->output_ctx, r,
