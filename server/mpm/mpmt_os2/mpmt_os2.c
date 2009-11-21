01026cbc46f5d14bb146df9ed87e813955dab609
Jeff Trawick <trawick@apache.org>
2009-11-21 17:18:39 +0000

    remove dead code related to the accept mutex
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@882943 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/mpmt_os2/mpmt_os2.c
+++ server/mpm/mpmt_os2/mpmt_os2.c
@@ -272,12 +272,6 @@ static char master_main()
                 ap_get_server_description());
     ap_log_error(APLOG_MARK, APLOG_INFO, 0, ap_server_conf,
                 "Server built: %s", ap_get_server_built());
-#ifdef AP_MPM_WANT_SET_ACCEPT_LOCK_MECH
-    ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, ap_server_conf,
-                "AcceptMutex: %s (default: %s)",
-                apr_proc_mutex_name(accept_mutex),
-                apr_proc_mutex_defname());
-#endif
     if (one_process) {
         ap_scoreboard_image->parent[0].pid = getpid();
         ap_mpm_child_main(pconf);
