4d0ee48cfba160c4d36bf80181a8d341ad27cdd1
Jeff Trawick <trawick@apache.org>
2009-11-24 20:32:42 +0000

    non-static directive parsers should be AP_DECLARE_NONSTD()
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@883860 13f79535-47bb-0310-9956-ffa450edef68

--- server/vhost.c
+++ server/vhost.c
@@ -249,8 +249,9 @@ const char *ap_parse_vhost_addrs(apr_pool_t *p,
 }
 
 
-const char *ap_set_name_virtual_host(cmd_parms *cmd, void *dummy,
-                                     const char *arg)
+AP_DECLARE_NONSTD(const char *)ap_set_name_virtual_host(cmd_parms *cmd,
+                                                        void *dummy,
+                                                        const char *arg)
 {
     /* use whatever port the main server has at this point */
     return get_addresses(cmd->pool, arg, &name_vhost_list_tail,
