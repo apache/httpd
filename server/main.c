16c7c534582ac4fcf2564a0b799870320de6d59e
Ruediger Pluem <rpluem@apache.org>
2009-12-23 15:09:21 +0000

    * With APR >= 2.0 there is no longer an APR-UTIL only an APR.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@893542 13f79535-47bb-0310-9956-ffa450edef68

--- server/main.c
+++ server/main.c
@@ -97,10 +97,15 @@ static void show_compile_settings(void)
     printf("Server built:   %s\n", ap_get_server_built());
     printf("Server's Module Magic Number: %u:%u\n",
            MODULE_MAGIC_NUMBER_MAJOR, MODULE_MAGIC_NUMBER_MINOR);
+#if APR_MAJOR_VERSION >= 2
+    printf("Server loaded:  APR %s\n", apr_version_string());
+    printf("Compiled using: APR %s\n", APR_VERSION_STRING);
+#else
     printf("Server loaded:  APR %s, APR-UTIL %s\n",
            apr_version_string(), apu_version_string());
     printf("Compiled using: APR %s, APR-UTIL %s\n",
            APR_VERSION_STRING, APU_VERSION_STRING);
+#endif
     /* sizeof(foo) is long on some platforms so we might as well
      * make it long everywhere to keep the printf format
      * consistent
