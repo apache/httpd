09f43b8aa6840a0526adc06d9cc7ffe5278c29c1
Ruediger Pluem <rpluem@apache.org>
2010-01-13 19:28:00 +0000

    * Fix compiler warning about wrong format
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@898910 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ldap/util_ldap.c
+++ modules/ldap/util_ldap.c
@@ -2541,7 +2541,7 @@ static const char *util_ldap_set_op_timeout(cmd_parms *cmd,
 
     ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, cmd->server,
                  "[%" APR_PID_T_FMT "] ldap connection: Setting op timeout "
-                 "to %d seconds.", getpid(), timeout);
+                 "to %ld seconds.", getpid(), timeout);
 
 #ifndef LDAP_OPT_TIMEOUT
 
