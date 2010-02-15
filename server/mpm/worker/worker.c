d03c932b1a7d61c280c968124b21f3acae434c13
Jim Jagielski <jim@apache.org>
2010-02-15 19:52:00 +0000

    *) worker: Don't report server has reached MaxClients until it has.        Add message when server gets within MinSpareThreads of MaxClients.             PR 46996.  [Dan Poirier]
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@910320 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/worker/worker.c
+++ server/mpm/worker/worker.c
@@ -1513,15 +1513,32 @@ static void perform_idle_server_maintenance(void)
     else if (idle_thread_count < min_spare_threads) {
         /* terminate the free list */
         if (free_length == 0) {
-            /* only report this condition once */
-            static int reported = 0;
+            /* No room for more children, might warn about configuration */
+            if (active_thread_count >= ap_daemons_limit * ap_threads_per_child) {
+                /* no threads are "inactive" - starting, stopping, etc. - which would confuse matters */
+                /* Are all threads in use?  Then we're really at MaxClients */
+                if (0 == idle_thread_count) {
+                    /* only report this condition once */
+                    static int reported = 0;
 
-            if (!reported) {
-                ap_log_error(APLOG_MARK, APLOG_ERR, 0,
-                             ap_server_conf,
-                             "server reached MaxClients setting, consider"
-                             " raising the MaxClients setting");
-                reported = 1;
+                    if (!reported) {
+                        ap_log_error(APLOG_MARK, APLOG_ERR, 0,
+                                     ap_server_conf,
+                                     "server reached MaxClients setting, consider"
+                                     " raising the MaxClients setting");
+                        reported = 1;
+                    }
+                } else {
+                    static int reported = 0;
+
+                    if (!reported) {
+                        ap_log_error(APLOG_MARK, APLOG_ERR, 0,
+                                     ap_server_conf,
+                                     "server is within MinSpareThreads of MaxClients, consider"
+                                     " raising the MaxClients setting");
+                        reported = 1;
+                    }
+                }
             }
             idle_spawn_rate = 1;
         }
