0c96068dc42769570088980c7436ba8b9dcf0123
Daniel Earl Poirier <poirier@apache.org>
2010-02-16 20:50:10 +0000

    Log command line on startup, so there's a record of command line arguments like -f.  Suggested by Shaya Potter.  [Dan Poirier] PR: 48752
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@910684 13f79535-47bb-0310-9956-ffa450edef68

--- server/log.c
+++ server/log.c
@@ -784,6 +784,33 @@ AP_DECLARE(void) ap_log_cerror(const char *file, int line, int level,
     va_end(args);
 }
 
+AP_DECLARE(void) ap_log_command_line(apr_pool_t *plog, server_rec *s)
+{
+    int i;
+    process_rec *process = s->process;
+    char *result;
+    int len_needed = 0;
+    
+    /* Piece together the command line from the pieces
+     * in process->argv, with spaces in between.
+     */
+    for (i = 0; i < process->argc; i++) {
+        len_needed += strlen(process->argv[i]) + 1;
+    }
+
+    result = (char *) apr_palloc(plog, len_needed);
+    *result = '\0';
+
+    for (i = 0; i < process->argc; i++) {
+        strcat(result, process->argv[i]);
+        if ((i+1)< process->argc) {
+            strcat(result, " ");
+        }
+    }
+    ap_log_error(APLOG_MARK, APLOG_NOTICE, 0, s,
+                 "Command line: '%s'", result);
+}
+
 AP_DECLARE(void) ap_log_pid(apr_pool_t *p, const char *filename)
 {
     apr_file_t *pid_file = NULL;
