55b6450ecb93bc5394eeadc8bcffff40cf55d0ff
Joe Orton <jorton@apache.org>
2010-02-26 16:05:11 +0000

    * modules/ssl/ssl_engine_kernel.c (ssl_hook_Access): Dump the SSL   error stack if reneg fails; this will give a big hint for failures   due to secure reneg.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@916733 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_kernel.c
+++ modules/ssl/ssl_engine_kernel.c
@@ -790,6 +790,7 @@ int ssl_hook_Access(request_rec *r)
             if (SSL_get_state(ssl) != SSL_ST_OK) {
                 ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                               "Re-negotiation request failed");
+                ssl_log_ssl_error(APLOG_MARK, APLOG_ERR, r->server);
 
                 r->connection->keepalive = AP_CONN_CLOSE;
                 return HTTP_FORBIDDEN;
