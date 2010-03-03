6ecc05b13e69a9124466883d499d0a466ae55042
Jeff Trawick <trawick@apache.org>
2010-03-03 19:28:28 +0000

    fix this error:
    mod_sed.c: In function ‘log_sed_errf’:
    mod_sed.c:67: warning: format not a string literal and no format arguments
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@918653 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/mod_sed.c
+++ modules/filters/mod_sed.c
@@ -64,7 +64,7 @@ module AP_MODULE_DECLARE_DATA sed_module;
 static apr_status_t log_sed_errf(void *data, const char *error)
 {
     request_rec *r = (request_rec *) data;
-    ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, error);
+    ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "%s", error);
     return APR_SUCCESS;
 }
 
