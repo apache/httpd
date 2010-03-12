c81cbf5592886f9a88fcce30800a527c1a369dda
Eric Covener <covener@apache.org>
2010-03-12 18:49:26 +0000

    reduce the warning level in the bindprocessor() call used to make sure child processes aren't bound to the CPU the parent is running on.
    In recent levels of AIX, the parent isn't implicitly bound to a CPU 
    (so the children don't inherit it), and when the AIX WPAR feature is enabled 
    this call can return EPERM, even though the child process is already unbound.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@922392 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/prefork/prefork.c
+++ server/mpm/prefork/prefork.c
@@ -743,7 +743,7 @@ static int make_child(server_rec *s, int slot)
         int status = bindprocessor(BINDPROCESS, (int)getpid(),
                                    PROCESSOR_CLASS_ANY);
         if (status != OK) {
-            ap_log_error(APLOG_MARK, APLOG_WARNING, errno,
+            ap_log_error(APLOG_MARK, APLOG_DEBUG, errno,
                          ap_server_conf, "processor unbind failed %d", status);
         }
 #endif
