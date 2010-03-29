aa79f88985c72a6dc22cc6b650473dab9b5176b9
Stefan Fritsch <sf@apache.org>
2010-03-29 19:33:49 +0000

    Disable body timeout for CONNECT requests
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@928881 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/mod_reqtimeout.c
+++ modules/filters/mod_reqtimeout.c
@@ -305,11 +305,13 @@ static int reqtimeout_after_headers(request_rec *r)
 
     ccfg->timeout_at = 0;
     ccfg->max_timeout_at = 0;
-    ccfg->new_timeout = cfg->body_timeout;
-    ccfg->new_max_timeout = cfg->body_max_timeout;
-    ccfg->min_rate = cfg->body_min_rate;
-    ccfg->rate_factor = cfg->body_rate_factor;
-    ccfg->type = "body";
+    if (r->method_number != M_CONNECT) {
+        ccfg->new_timeout = cfg->body_timeout;
+        ccfg->new_max_timeout = cfg->body_max_timeout;
+        ccfg->min_rate = cfg->body_min_rate;
+        ccfg->rate_factor = cfg->body_rate_factor;
+        ccfg->type = "body";
+    }
 
     return OK;
 }
