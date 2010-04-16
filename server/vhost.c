623a5d15c9925430e6a93ea643acf7d6901bad5f
Richard Bowen <rbowen@apache.org>
2010-04-16 16:33:20 +0000

    Expand the "NameVirtualHost has no VirtualHosts" error message, since it seems that we have to explain a dozen times a day what it really means.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@934984 13f79535-47bb-0310-9956-ffa450edef68

--- server/vhost.c
+++ server/vhost.c
@@ -507,7 +507,9 @@ static void remove_unused_name_vhosts(server_rec *main_s, ipaddr_chain **pic)
 
         if (ic->server == NULL) {
             ap_log_error(APLOG_MARK, APLOG_WARNING, 0, main_s,
-                         "NameVirtualHost %s:%u has no VirtualHosts",
+                         "Either NameVirtualHost %s:%u has no VirtualHosts,"
+                         " or there is more than one identical NameVirtualHost line,"
+                         " or your VirtualHost declarations do not match the NameVirtualHost line",
                          ic->sar->virthost, ic->sar->host_port);
             *pic = ic->next;
         }
