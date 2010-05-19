de81a01914ceae778b2355f5bb29a267318fab37
Joe Orton <jorton@apache.org>
2010-05-19 21:14:35 +0000

    Merge r417988 from trunk:
    * modules/ssl/ssl_engine_init.c (ssl_init_ctx_cleanup_proxy): Unset
    pkp->certs pointer after freeing the array; fixes segfault at startup
    if the certs are shared across >1 server_rec.
    
    PR: 39915
    Submitted by: jorton
    Reviewed by: minfrin, rjung, jorton
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@946401 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_init.c
+++ modules/ssl/ssl_engine_init.c
@@ -1250,6 +1250,7 @@ static void ssl_init_ctx_cleanup_proxy(modssl_ctx_t *mctx)
 
     if (mctx->pkp->certs) {
         sk_X509_INFO_pop_free(mctx->pkp->certs, X509_INFO_free);
+        mctx->pkp->certs = NULL;
     }
 }
 
