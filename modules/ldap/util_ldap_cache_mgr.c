56e25c48e80ac8296134cc89327bc514d2272906
Eric Covener <covener@apache.org>
2010-06-04 02:03:57 +0000

    The rest of this cache display method just writes its response, and the caller doesn't care about the returned string.  Issue a message to complete the /ldap-status cache handler page which otherwise looks truncated.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@951237 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ldap/util_ldap_cache_mgr.c
+++ modules/ldap/util_ldap_cache_mgr.c
@@ -662,7 +662,8 @@ char *util_ald_cache_display(request_rec *r, util_ldap_state_t *st)
 
 
     if (!util_ldap_cache) {
-        return "<tr valign='top'><td nowrap colspan=7>Cache has not been enabled/initialised.</td></tr>";
+        ap_rputs("<tr valign='top'><td nowrap colspan=7>Cache has not been enabled/initialised.</td></tr>", r);
+        return NULL;
     }
 
     if (r->args && strlen(r->args)) {
