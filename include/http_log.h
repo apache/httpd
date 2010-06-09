9658bc326dbd1ed858e29f18f70a197c926121c4
Stefan Fritsch <sf@apache.org>
2010-06-09 21:23:48 +0000

    Fix build failure by reverting part of r953125
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@953166 13f79535-47bb-0310-9956-ffa450edef68

--- include/http_log.h
+++ include/http_log.h
@@ -277,9 +277,10 @@ void ap_logs_child_init(apr_pool_t *p, server_rec *s);
 #if __STDC_VERSION__ >= 199901L
 /* need additional step to expand APLOG_MARK first */
 #define ap_log_error(...) ap_log_error__(__VA_ARGS__)
+/* need server_rec *sr = ... for the case if s is verbatim NULL */
 #define ap_log_error__(file, line, mi, level, status, s, ...)           \
-    do { if (APLOG_MODULE_IS_LEVEL(s, mi, level))                          \
-             ap_log_error_(file, line, mi, level, status, s, __VA_ARGS__); \
+    do { server_rec *sr = s; if (APLOG_MODULE_IS_LEVEL(sr, mi, level))      \
+             ap_log_error_(file, line, mi, level, status, sr, __VA_ARGS__); \
     } while(0)
 #else
 #define ap_log_error ap_log_error_
