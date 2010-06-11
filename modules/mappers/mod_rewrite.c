5c266f21872f0163b33bcc22a1dacd24a4d81b6f
Eric Covener <covener@apache.org>
2010-06-11 17:39:17 +0000

    mod_rewrite.c:447: warning: format not a string literal and no format arguments
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@953785 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_rewrite.c
+++ modules/mappers/mod_rewrite.c
@@ -444,7 +444,7 @@ static void do_rewritelog(request_rec *r, int level, char *perdir,
 
     AP_REWRITE_LOG((uintptr_t)r, level, r->main ? 0 : 1, (char *)ap_get_server_name(r), logline);
 
-    ap_log_rerror(APLOG_MARK, APLOG_DEBUG + level, 0, r, logline);
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG + level, 0, r, "%s", logline);
 
     return;
 }
