3efec0f2bdbd5908d14dd8d65ee9952a6826e803
Rainer Jung <rjung@apache.org>
2010-06-11 09:10:29 +0000

    Merge r953377,r953385,r953418 from trunk:
    mod_proxy_http, mod_proxy_ajp, mod_reqtimeout:
    Use APR_STATUS_IS_TIMEUP instead of direct compare
    to APR_TIMEUP to be more safe on different platforms.
    
    Submitted by: rjung, rpluem
    Reviewed by: rjung, rpluem, wrowe
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@953616 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_ajp.c
+++ modules/proxy/mod_proxy_ajp.c
@@ -450,15 +450,18 @@ static int ap_proxy_ajp_request(apr_pool_t *p, request_rec *r,
                         }
                     }
                     else {
+                        apr_status_t rv;
+
                         e = apr_bucket_transient_create(send_body_chunk_buff, size,
                                                     r->connection->bucket_alloc);
                         APR_BRIGADE_INSERT_TAIL(output_brigade, e);
 
                         if ((conn->worker->flush_packets == flush_on) ||
                             ((conn->worker->flush_packets == flush_auto) &&
-                            (apr_poll(conn_poll, 1, &conn_poll_fd,
-                                      conn->worker->flush_wait)
-                                        == APR_TIMEUP) ) ) {
+                            ((rv = apr_poll(conn_poll, 1, &conn_poll_fd,
+                                             conn->worker->flush_wait))
+                                             != APR_SUCCESS) &&
+                              APR_STATUS_IS_TIMEUP(rv))) {
                             e = apr_bucket_flush_create(r->connection->bucket_alloc);
                             APR_BRIGADE_INSERT_TAIL(output_brigade, e);
                         }
