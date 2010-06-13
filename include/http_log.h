8aeb2e0e4e9ee71a7373f2344d19c42c2406c5a0
Stefan Fritsch <sf@apache.org>
2010-06-13 09:18:20 +0000

    Use the correct loglevel checks in the wrappers for ap_log_cerror/ap_log_rerror. This fixes per-dir loglevel configuration for loglevels > notice.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@954188 13f79535-47bb-0310-9956-ffa450edef68

--- include/http_log.h
+++ include/http_log.h
@@ -349,7 +349,7 @@ AP_DECLARE(void) ap_log_perror_(const char *file, int line, int module_index,
 /* need additional step to expand APLOG_MARK first */
 #define ap_log_rerror(...) ap_log_rerror__(__VA_ARGS__)
 #define ap_log_rerror__(file, line, mi, level, status, r, ...)              \
-    do { if (APLOG_MODULE_IS_LEVEL(r->server, mi, level))                   \
+    do { if (APLOG_R_MODULE_IS_LEVEL(r, mi, level))                         \
              ap_log_rerror_(file, line, mi, level, status, r, __VA_ARGS__); \
     } while(0)
 #else
@@ -386,7 +386,7 @@ AP_DECLARE(void) ap_log_rerror_(const char *file, int line, int module_index,
 /* need additional step to expand APLOG_MARK first */
 #define ap_log_cerror(...) ap_log_cerror__(__VA_ARGS__)
 #define ap_log_cerror__(file, line, mi, level, status, c, ...)              \
-    do { if (APLOG_MODULE_IS_LEVEL(c->base_server, mi, level))              \
+    do { if (APLOG_C_MODULE_IS_LEVEL(c, mi, level))                         \
              ap_log_cerror_(file, line, mi, level, status, c, __VA_ARGS__); \
     } while(0)
 #else
