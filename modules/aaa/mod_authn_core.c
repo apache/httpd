14649dcb0b7f88bcf0a9a6d02a91ab7a6398e16c
Stefan Fritsch <sf@apache.org>
2010-06-20 20:18:55 +0000

    Don't apr_pstrdup in ap_auth_type(). It returns const char *, anyway.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@956405 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_authn_core.c
+++ modules/aaa/mod_authn_core.c
@@ -315,7 +315,7 @@ static const char *authn_ap_auth_type(request_rec *r)
     conf = (authn_core_dir_conf *)ap_get_module_config(r->per_dir_config,
         &authn_core_module);
 
-    return apr_pstrdup(r->pool, conf->ap_auth_type);
+    return conf->ap_auth_type;
 }
 
 static const char *authn_ap_auth_name(request_rec *r)
