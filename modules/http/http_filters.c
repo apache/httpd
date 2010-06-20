b7936c98ba0b45be57719a34e3fbd9dddd148040
Stefan Fritsch <sf@apache.org>
2010-06-20 16:17:23 +0000

    More trace logging
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@956374 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http/http_filters.c
+++ modules/http/http_filters.c
@@ -809,7 +809,7 @@ static apr_status_t send_all_header_fields(header_struct *h,
                       "Headers sent to client:");
         t_elt = (const apr_table_entry_t *)(elts->elts);
         do {
-            ap_log_rerror(APLOG_MARK, APLOG_TRACE4, 0, r, "%s: %s",
+            ap_log_rerror(APLOG_MARK, APLOG_TRACE4, 0, r, "  %s: %s",
                           ap_escape_logitem(r->pool, t_elt->key),
                           ap_escape_logitem(r->pool, t_elt->val));
             t_elt++;
