22d2c7297e04f753d9150f5dc5c133678ffd755d
Brian Havard <bjh@apache.org>
2010-06-22 12:49:59 +0000

    OS/2: Add support for ThreadStackSize.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@956863 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/mpmt_os2/mpmt_os2_child.c
+++ server/mpm/mpmt_os2/mpmt_os2_child.c
@@ -291,13 +291,14 @@ void ap_mpm_child_main(apr_pool_t *pconf)
 void add_worker()
 {
     int thread_slot;
+    int stacksize = ap_thread_stacksize == 0 ? 128*1024 : ap_thread_stacksize;
 
     /* Find a free thread slot */
     for (thread_slot=0; thread_slot < HARD_THREAD_LIMIT; thread_slot++) {
         if (ap_scoreboard_image->servers[child_slot][thread_slot].status == SERVER_DEAD) {
             ap_scoreboard_image->servers[child_slot][thread_slot].status = SERVER_STARTING;
             ap_scoreboard_image->servers[child_slot][thread_slot].tid =
-                _beginthread(worker_main, NULL, 128*1024, (void *)thread_slot);
+                _beginthread(worker_main, NULL, stacksize, (void *)thread_slot);
             break;
         }
     }
