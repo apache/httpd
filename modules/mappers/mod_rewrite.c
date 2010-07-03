60e8f3a91326e07fea014d315e0ada31627136b5
Rainer Jung <rjung@apache.org>
2010-07-03 16:21:58 +0000

    Allow to set environment variables using mod_rewrite without explicitely giving a value.
    Most modules only check presence of a variable, not the
    value, so it makes sense to make the VAL argument in
    the mod_rewrite ENV flag optional.
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@960233 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_rewrite.c
+++ modules/mappers/mod_rewrite.c
@@ -2389,11 +2389,13 @@ static void do_expand_env(data_item *env, rewrite_ctx *ctx)
         name = do_expand(env->data, ctx, NULL);
         if ((val = ap_strchr(name, ':')) != NULL) {
             *val++ = '\0';
-
-            apr_table_set(ctx->r->subprocess_env, name, val);
-            rewritelog((ctx->r, 5, NULL, "setting env variable '%s' to '%s'",
-                        name, val));
+        } else {
+            val = "";
         }
+
+        apr_table_set(ctx->r->subprocess_env, name, val);
+        rewritelog((ctx->r, 5, NULL, "setting env variable '%s' to '%s'",
+                    name, val));
 
         env = env->next;
     }
