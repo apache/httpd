47b4c31ed0ddf6301a537b367254530626193c93
Nick Kew <niq@apache.org>
2010-07-11 06:08:34 +0000

    Improve misleading error message. PR 38322
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@962991 13f79535-47bb-0310-9956-ffa450edef68

--- server/request.c
+++ server/request.c
@@ -190,7 +190,7 @@ AP_DECLARE(int) ap_process_request_internal(request_rec *r)
                 if (((access_status = ap_run_check_user_id(r)) != 0)
                     || !ap_auth_type(r)) {
                     return decl_die(access_status, ap_auth_type(r)
-                                  ? "check user.  No user file?"
+                                  ? "check user.  Check your authn provider!"
                                   : "perform authentication. AuthType not set!",
                                   r);
                 }
@@ -198,7 +198,7 @@ AP_DECLARE(int) ap_process_request_internal(request_rec *r)
                 if (((access_status = ap_run_auth_checker(r)) != 0)
                     || !ap_auth_type(r)) {
                     return decl_die(access_status, ap_auth_type(r)
-                                  ? "check access.  No groups file?"
+                                  ? "check access.  "Check your 'Require' directive"
                                   : "perform authentication. AuthType not set!",
                                    r);
                 }
@@ -214,7 +214,7 @@ AP_DECLARE(int) ap_process_request_internal(request_rec *r)
                 if (((access_status = ap_run_check_user_id(r)) != 0)
                     || !ap_auth_type(r)) {
                     return decl_die(access_status, ap_auth_type(r)
-                                  ? "check user.  No user file?"
+                                  ? "check user.  Check your authn provider!"
                                   : "perform authentication. AuthType not set!",
                                   r);
                 }
@@ -222,7 +222,7 @@ AP_DECLARE(int) ap_process_request_internal(request_rec *r)
                 if (((access_status = ap_run_auth_checker(r)) != 0)
                     || !ap_auth_type(r)) {
                     return decl_die(access_status, ap_auth_type(r)
-                                  ? "check access.  No groups file?"
+                                  ? "check access.  "Check your 'Require' directive"
                                   : "perform authentication. AuthType not set!",
                                   r);
                 }
