e6fcd04001c512143801e56adba054564c65f2d6
Jeff Trawick <trawick@apache.org>
2010-07-11 02:54:30 +0000

    follow up to r930125
    change log message when forcing worker into error state:
    * APLOG_ERR instead of unsuppressible APLOG_NOTICE
    * follow formatting of existing proxy messages more closely
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@962972 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_balancer.c
+++ modules/proxy/mod_proxy_balancer.c
@@ -617,8 +617,9 @@ static int proxy_balancer_post_request(proxy_worker *worker,
         for (i = 0; i < balancer->errstatuses->nelts; i++) {
             int val = ((int *)balancer->errstatuses->elts)[i];
             if (r->status == val) {
-                ap_log_error(APLOG_MARK, APLOG_NOTICE, rv, r->server,
-                    "Detected ErrorOnState (%d) for member (%s). Forcing worker into error state.", val, worker->name);
+                ap_log_error(APLOG_MARK, APLOG_ERR, rv, r->server,
+                             "proxy: BALANCER: (%s).  Forcing recovery for worker (%s), erroronstatus %d",
+                             balancer->name, worker->name, val);
                 worker->s->status |= PROXY_WORKER_IN_ERROR;
                 worker->s->error_time = apr_time_now();
                 break;
