b24bd5dcbee4b6694c86915c8df716e5e3117709
Stefan Fritsch <sf@apache.org>
2010-07-14 20:03:42 +0000

    fix log messages
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@964161 13f79535-47bb-0310-9956-ffa450edef68

--- server/request.c
+++ server/request.c
@@ -216,7 +216,7 @@ AP_DECLARE(int) ap_process_request_internal(request_rec *r)
             if (access_status == OK) {
                 ap_log_rerror(APLOG_MARK, APLOG_TRACE3, 0, r,
                               "request authorized without authentication by "
-                              "access_checker_autoritative hook: %s", r->uri);
+                              "access_checker_ex hook: %s", r->uri);
             }
             else if (access_status != DECLINED) {
                 return decl_die(access_status, "check access", r);
@@ -243,7 +243,7 @@ AP_DECLARE(int) ap_process_request_internal(request_rec *r)
             if (access_status == OK) {
                 ap_log_rerror(APLOG_MARK, APLOG_TRACE3, 0, r,
                               "request authorized without authentication by "
-                              "access_checker_autoritative hook: %s", r->uri);
+                              "access_checker_ex hook: %s", r->uri);
             }
             else if (access_status != DECLINED) {
                 return decl_die(access_status, "check access", r);
