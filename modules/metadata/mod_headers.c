793a6041d4dfb4134bbbbc07b3c811da9f72e4a6
Nick Kew <niq@apache.org>
2010-07-21 00:20:43 +0000

    Fix omission in r894036 noted by rpluem.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@966059 13f79535-47bb-0310-9956-ffa450edef68

--- modules/metadata/mod_headers.c
+++ modules/metadata/mod_headers.c
@@ -592,7 +592,7 @@ static const char *process_regexp(header_entry *hdr, const char *value,
     ret = apr_palloc(pool, strlen(value) + 1 + diffsz);
     memcpy(ret, value, pmatch[0].rm_so);
     strcpy(ret + pmatch[0].rm_so, subs);
-    strcat(ret, value + pmatch[0].rm_eo);
+    strcat(ret, remainder);
     return ret;
 }
 
