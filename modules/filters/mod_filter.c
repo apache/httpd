465ea019f573f091e945b51efe8ed6797af7c85b
Nick Kew <niq@apache.org>
2010-07-22 23:02:12 +0000

    Use temp_pool not pool for a string that's a temp
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@966892 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/mod_filter.c
+++ modules/filters/mod_filter.c
@@ -558,7 +558,7 @@ static const char *filter_bytype(cmd_parms *cmd, void *CFG,
     /* back compatibility, need to parse multiple components in pname */
     char *pname;
     char *strtok_state = NULL;
-    char *name = apr_pstrdup(cmd->pool, names);
+    char *name = apr_pstrdup(cmd->temp_pool, names);
     const char *rv = NULL;
     for (pname = apr_strtok(name, ";", &strtok_state);
          pname != NULL && rv == NULL;
