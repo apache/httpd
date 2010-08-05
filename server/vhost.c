09e3207660e284bfc5600129ecd7e51e16e9748c
Daniel Earl Poirier <poirier@apache.org>
2010-08-05 14:12:25 +0000

    With -S, show ServerAlias values that go with the virtual hosts
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@982629 13f79535-47bb-0310-9956-ffa450edef68

--- server/vhost.c
+++ server/vhost.c
@@ -447,6 +447,26 @@ static void dump_a_vhost(apr_file_t *f, ipaddr_chain *ic)
         apr_file_printf(f, "namevhost %s (%s:%u)\n",
                         nc->server->server_hostname,
                         nc->server->defn_name, nc->server->defn_line_number);
+        if (nc->server->names) {
+            apr_array_header_t *names = nc->server->names;
+            char **name = (char **)names->elts;
+            int i;
+            for (i = 0; i < names->nelts; ++i) {
+                if (name[i]) {
+                    apr_file_printf(f, "%16s alias %s\n", "", name[i]);
+                }
+            }
+        }
+        if (nc->server->wild_names) {
+            apr_array_header_t *names = nc->server->wild_names;
+            char **name = (char **)names->elts;
+            int i;
+            for (i = 0; i < names->nelts; ++i) {
+                if (name[i]) {
+                    apr_file_printf(f, "%16s wild alias %s\n", "", name[i]);
+                }
+            }
+        }
     }
 }
 
