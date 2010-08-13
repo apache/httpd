633efb33146f9fcb0c41edf3e3bb065e180a024c
Ruediger Pluem <rpluem@apache.org>
2010-08-13 07:24:36 +0000

    * The forward and reverse worker do not keep connections alive, so   ensure that mod_proxy_http does the correct thing   regarding the Connection header in the request.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@985112 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/proxy_util.c
+++ modules/proxy/proxy_util.c
@@ -1573,6 +1573,12 @@ PROXY_DECLARE(int) ap_proxy_pre_request(proxy_worker **worker,
                 *balancer = NULL;
                 *worker = conf->forward;
                 access_status = OK;
+                /*
+                 * The forward worker does not keep connections alive, so
+                 * ensure that mod_proxy_http does the correct thing
+                 * regarding the Connection header in the request.
+                 */
+                apr_table_set(r->subprocess_env, "proxy-nokeepalive", "1");
             }
         }
         else if (r->proxyreq == PROXYREQ_REVERSE) {
@@ -1583,6 +1589,12 @@ PROXY_DECLARE(int) ap_proxy_pre_request(proxy_worker **worker,
                 *balancer = NULL;
                 *worker = conf->reverse;
                 access_status = OK;
+                /*
+                 * The reverse worker does not keep connections alive, so
+                 * ensure that mod_proxy_http does the correct thing
+                 * regarding the Connection header in the request.
+                 */
+                apr_table_set(r->subprocess_env, "proxy-nokeepalive", "1");
             }
         }
     }
