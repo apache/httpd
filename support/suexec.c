86554cdfb4252fb01e63ec3eb0bd927083912fcc
Stefan Fritsch <sf@apache.org>
2010-08-22 10:34:20 +0000

    Support large log files in suexec
    PR: 45856
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@987861 13f79535-47bb-0310-9956-ffa450edef68

--- support/suexec.c
+++ support/suexec.c
@@ -131,7 +131,11 @@ static void err_output(int is_error, const char *fmt, va_list ap)
     struct tm *lt;
 
     if (!log) {
+#if defined(_LARGEFILE64_SOURCE) && HAVE_FOPEN64
+        if ((log = fopen64(AP_LOG_EXEC, "a")) == NULL) {
+#else
         if ((log = fopen(AP_LOG_EXEC, "a")) == NULL) {
+#endif
             fprintf(stderr, "suexec failure: could not open log file\n");
             perror("fopen");
             exit(1);
