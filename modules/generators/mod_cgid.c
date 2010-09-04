fcba85cb20ee7a40039cd3b94eda7129cf3cc98f
Stefan Fritsch <sf@apache.org>
2010-09-04 11:24:49 +0000

    mod_cgid: Log a warning if the ScriptSock path is truncated because it is too long.
    PR 49388
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@992583 13f79535-47bb-0310-9956-ffa450edef68

--- modules/generators/mod_cgid.c
+++ modules/generators/mod_cgid.c
@@ -909,12 +909,20 @@ static int cgid_init(apr_pool_t *p, apr_pool_t *plog, apr_pool_t *ptemp,
     }
 
     if (!first_time) {
+        char *tmp_sockname;
         total_modules = 0;
         for (m = ap_preloaded_modules; *m != NULL; m++)
             total_modules++;
 
         parent_pid = getpid();
-        sockname = ap_server_root_relative(p, sockname);
+        tmp_sockname = ap_server_root_relative(p, sockname);
+        if (strlen(tmp_sockname) > sizeof(server_addr->sun_path) - 1) {
+            tmp_sockname[sizeof(server_addr->sun_path)] = '\0';
+            ap_log_error(APLOG_MARK, APLOG_ERR, 0, main_server,
+                        "The length of the ScriptSock path exceeds maximum, "
+                        "truncating to %s", tmp_sockname);
+        }
+        sockname = tmp_sockname;
 
         server_addr_len = APR_OFFSETOF(struct sockaddr_un, sun_path) + strlen(sockname);
         server_addr = (struct sockaddr_un *)apr_palloc(p, server_addr_len + 1);
