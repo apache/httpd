f0c835568817fc83da69f0e0a676664c4ac77562
Guenter Knauf <fuankg@apache.org>
2010-10-06 10:40:52 +0000

    Rename macro to a better name and sync with trunk. REviewed by: wrowe, rjung.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1004980 13f79535-47bb-0310-9956-ffa450edef68

--- server/gen_test_char.c
+++ server/gen_test_char.c
@@ -30,7 +30,7 @@
 #include "apr_lib.h"
 
 #if defined(WIN32) || defined(OS2)
-#define WANT_WIN32_OS2
+#define NEED_ENHANCED_ESCAPES
 #endif
 
 #endif
@@ -80,7 +80,7 @@ int main(int argc, char *argv[])
             printf("\n    ");
 
         /* escape_shell_cmd */
-#if defined(WANT_WIN32_OS2)
+#ifdef NEED_ENHANCED_ESCAPES
         /* Win32/OS2 have many of the same vulnerable characters
          * as Unix sh, plus the carriage return and percent char.
          * The proper escaping of these characters varies from unix
