2a125fe5d40d1a43d9f07defac9ded1ac863708e
Graham Leggett <minfrin@apache.org>
2010-10-12 20:43:51 +0000

    Restore the apr_brigade_insert_file() call to within recall_body(), as the function does more than add a single file bucket. Pass an empty brigade to recall_body(), and prepend the result to the existing output brigade, the next bucket being eos.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1021917 13f79535-47bb-0310-9956-ffa450edef68

--- modules/cache/mod_disk_cache.c
+++ modules/cache/mod_disk_cache.c
@@ -863,9 +863,7 @@ static apr_status_t recall_body(cache_handle_t *h, apr_pool_t *p, apr_bucket_bri
     disk_cache_object_t *dobj = (disk_cache_object_t*) h->cache_obj->vobj;
 
     if (dobj->data.fd) {
-        apr_bucket *e = apr_bucket_file_create(dobj->data.fd, 0,
-                dobj->file_size, p, bb->bucket_alloc);
-        APR_BRIGADE_INSERT_HEAD(bb, e);
+        apr_brigade_insert_file(bb, dobj->data.fd, 0, dobj->file_size, p);
     }
 
     return APR_SUCCESS;
