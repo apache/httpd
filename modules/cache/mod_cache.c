d1da525e055237b2a0d9fa4a11a85d22e4a2ed23
Graham Leggett <minfrin@apache.org>
2010-10-12 20:43:51 +0000

    Restore the apr_brigade_insert_file() call to within recall_body(), as the function does more than add a single file bucket. Pass an empty brigade to recall_body(), and prepend the result to the existing output brigade, the next bucket being eos.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1021917 13f79535-47bb-0310-9956-ffa450edef68

--- modules/cache/mod_cache.c
+++ modules/cache/mod_cache.c
@@ -568,7 +568,7 @@ static int cache_handler(request_rec *r)
  *
  * Deliver cached content (headers and body) up the stack.
  */
-static int cache_out_filter(ap_filter_t *f, apr_bucket_brigade *bb)
+static int cache_out_filter(ap_filter_t *f, apr_bucket_brigade *in)
 {
     request_rec *r = f->r;
     apr_bucket *e;
@@ -580,31 +580,34 @@ static int cache_out_filter(ap_filter_t *f, apr_bucket_brigade *bb)
         ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server,
                      "CACHE/CACHE_OUT filter enabled while caching is disabled, ignoring");
         ap_remove_output_filter(f);
-        return ap_pass_brigade(f->next, bb);
+        return ap_pass_brigade(f->next, in);
     }
 
     ap_log_error(APLOG_MARK, APLOG_DEBUG, APR_SUCCESS, r->server,
                  "cache: running CACHE_OUT filter");
 
     /* clean out any previous response up to EOS, if any */
-    for (e = APR_BRIGADE_FIRST(bb);
-         e != APR_BRIGADE_SENTINEL(bb);
+    for (e = APR_BRIGADE_FIRST(in);
+         e != APR_BRIGADE_SENTINEL(in);
          e = APR_BUCKET_NEXT(e))
     {
         if (APR_BUCKET_IS_EOS(e)) {
+            apr_bucket_brigade *bb = apr_brigade_create(r->pool,
+                    r->connection->bucket_alloc);
 
             /* restore status of cached response */
             r->status = cache->handle->cache_obj->info.status;
 
             /* recall_headers() was called in cache_select() */
             cache->provider->recall_body(cache->handle, r->pool, bb);
+            APR_BRIGADE_PREPEND(in, bb);
 
             /* This filter is done once it has served up its content */
             ap_remove_output_filter(f);
 
             ap_log_error(APLOG_MARK, APLOG_DEBUG, APR_SUCCESS, r->server,
                          "cache: serving %s", r->uri);
-            return ap_pass_brigade(f->next, bb);
+            return ap_pass_brigade(f->next, in);
 
         }
         apr_bucket_delete(e);
