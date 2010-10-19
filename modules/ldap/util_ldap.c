ce77b90180c1c625a3b6296f114eb345fd98ee7d
Graham Leggett <minfrin@apache.org>
2010-10-19 22:53:41 +0000

    Avoid unnecessariy initialisation before we test ldap-status handler name.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1024453 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ldap/util_ldap.c
+++ modules/ldap/util_ldap.c
@@ -104,17 +104,19 @@ static void util_ldap_strdup (char **str, const char *newstr)
  */
 static int util_ldap_handler(request_rec *r)
 {
-    util_ldap_state_t *st = (util_ldap_state_t *)
-                            ap_get_module_config(r->server->module_config,
-                                                 &ldap_module);
+    util_ldap_state_t *st;
 
     r->allowed |= (1 << M_GET);
-    if (r->method_number != M_GET)
+    if (r->method_number != M_GET) {
         return DECLINED;
+    }
 
     if (strcmp(r->handler, "ldap-status")) {
         return DECLINED;
     }
+
+    st = (util_ldap_state_t *) ap_get_module_config(r->server->module_config,
+            &ldap_module);
 
     ap_set_content_type(r, "text/html; charset=ISO-8859-1");
 
