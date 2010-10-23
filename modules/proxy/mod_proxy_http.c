341be582feee4038acf95146d8b2a324b76850e4
Stefan Fritsch <sf@apache.org>
2010-10-23 16:20:10 +0000

    Remove needless pool cleanup. ap_pregcomp already registers a cleanup for the regex.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1026636 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_http.c
+++ modules/proxy/mod_proxy_http.c
@@ -2127,17 +2127,11 @@ cleanup:
     }
     return status;
 }
-static apr_status_t warn_rx_free(void *p)
-{
-    ap_pregfree((apr_pool_t*)p, warn_rx);
-    return APR_SUCCESS;
-}
 static void ap_proxy_http_register_hook(apr_pool_t *p)
 {
     proxy_hook_scheme_handler(proxy_http_handler, NULL, NULL, APR_HOOK_FIRST);
     proxy_hook_canon_handler(proxy_http_canon, NULL, NULL, APR_HOOK_FIRST);
     warn_rx = ap_pregcomp(p, "[0-9]{3}[ \t]+[^ \t]+[ \t]+\"[^\"]*\"([ \t]+\"([^\"]+)\")?", 0);
-    apr_pool_cleanup_register(p, p, warn_rx_free, apr_pool_cleanup_null);
 }
 
 AP_DECLARE_MODULE(proxy_http) = {
