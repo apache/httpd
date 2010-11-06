0d6be9186de772e58b9163990dd032473439f10d
Stefan Fritsch <sf@apache.org>
2010-11-06 22:14:12 +0000

    Ooops. Forgot the minor bump for r1032167
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1032168 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_mmn.h
+++ include/ap_mmn.h
@@ -281,6 +281,7 @@
  *                         mod_include's parser with one derived from
  *                         mod_ssl's parser. Clean up ap_expr's public
  *                         interface.
+ * 20101106.1 (2.3.9-dev)  Add ap_pool_cleanup_set_null() generic cleanup
  */
 
 #define MODULE_MAGIC_COOKIE 0x41503234UL /* "AP24" */
@@ -288,7 +289,7 @@
 #ifndef MODULE_MAGIC_NUMBER_MAJOR
 #define MODULE_MAGIC_NUMBER_MAJOR 20101106
 #endif
-#define MODULE_MAGIC_NUMBER_MINOR 0                     /* 0...n */
+#define MODULE_MAGIC_NUMBER_MINOR 1                     /* 0...n */
 
 /**
  * Determine if the server's current MODULE_MAGIC_NUMBER is at least a
