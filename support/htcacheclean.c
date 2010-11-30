c163534ebc480c4d0bd69459b049e8f32fad9aa7
Guenter Knauf <fuankg@apache.org>
2010-11-30 19:48:06 +0000

    Fixed return type to silent compiler warning.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1040733 13f79535-47bb-0310-9956-ffa450edef68

--- support/htcacheclean.c
+++ support/htcacheclean.c
@@ -255,7 +255,7 @@ static void printstats(char *path, struct stats *s)
  */
 static apr_size_t round_up(apr_size_t val, apr_off_t round) {
     if (round > 1) {
-        return ((val + round - 1) / round) * round;
+        return (apr_size_t)(((val + round - 1) / round) * round);
     }
     return val;
 }
