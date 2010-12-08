83fd51b4fb256a675fedc61b6dbfdc66e23d0900
Daniel Earl Poirier <poirier@apache.org>
2010-12-08 19:31:34 +0000

    Backport r1031551 proxy_connect: Don't give up in the middle of a CONNECT tunnel when the child process is starting to exit.  PR50220. [Eric Covener]
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1043628 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_connect.c
+++ modules/proxy/mod_proxy_connect.c
@@ -288,6 +288,9 @@ static int proxy_connect_handler(request_rec *r, proxy_worker *worker,
 
     while (1) { /* Infinite loop until error (one side closes the connection) */
         if ((rv = apr_pollset_poll(pollset, -1, &pollcnt, &signalled)) != APR_SUCCESS) {
+            if (APR_STATUS_IS_EINTR(rv)) { 
+                continue;
+            }
             apr_socket_close(sock);
             ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r, "proxy: CONNECT: error apr_poll()");
             return HTTP_INTERNAL_SERVER_ERROR;
