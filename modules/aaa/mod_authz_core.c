b196fbc49193a3f442b8357d6647700911e7956a
Stefan Fritsch <sf@apache.org>
2010-12-30 22:38:30 +0000

    Remove obsolete and redundant log message in env auth provider
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1054014 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_authz_core.c
+++ modules/aaa/mod_authz_core.c
@@ -909,11 +909,6 @@ static authz_status env_check_authorization(request_rec *r,
         }
     }
 
-    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
-                  "access to %s failed, reason: env variable list does not meet "
-                  "'require'ments for user '%s' to be allowed access",
-                  r->uri, r->user);
-
     return AUTHZ_DENIED;
 }
 
