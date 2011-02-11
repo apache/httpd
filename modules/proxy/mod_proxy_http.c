5baa5e9ab38a3949deafa92d6b31032f47706b4b
Ruediger Pluem <rpluem@apache.org>
2011-02-11 12:30:21 +0000

    Merge r1039304, r1053584 from trunk:
    * Put a note in the connection notes that the SSL handshake to the backend
      failed such that mod_proxy can put the worker in error state.
    
    PR: 50332
    Submitted by: Daniel Ruggeri <DRuggeri primary.net>
    Reviewed by: rpluem
    
    
    * Fix r1039304 and make the patch similar to the one proposed for
      2.2.x: If the SSL handshake to the backend fails we cannot even
      sent an HTTP request. So the check needs to happen already when
      we sent data not when we receive data.
    
    Reviewed by: rpluem, jim, wrowe
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1069773 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_http.c
+++ modules/proxy/mod_proxy_http.c
@@ -271,10 +271,16 @@ static int pass_brigade(apr_bucket_alloc_t *bucket_alloc,
         ap_log_error(APLOG_MARK, APLOG_ERR, status, r->server,
                      "proxy: pass request body failed to %pI (%s)",
                      conn->addr, conn->hostname);
-        if (origin->aborted) { 
+        if (origin->aborted) {
+            if (strcmp(apr_table_get(origin->notes,
+                                     "SSL_connect_rv"), "err") == 0) {
+                return ap_proxyerror(r, HTTP_INTERNAL_SERVER_ERROR,
+                                     "Error during SSL Handshake with"
+                                     " remote server");
+            }
             return APR_STATUS_IS_TIMEUP(status) ? HTTP_GATEWAY_TIME_OUT : HTTP_BAD_GATEWAY;
         }
-        else { 
+        else {
             return HTTP_BAD_REQUEST; 
         }
     }
