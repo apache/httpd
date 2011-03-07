6f2dd694496e338d6306ece6c11652f0119fb4cd
Rainer Jung <rjung@apache.org>
2011-03-07 20:39:54 +0000

    mod_rewrite: Allow to unset environment variables.
    PR: 50746
    Submitted by: rjung
    Reviewed  by: covener, rpluem
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1078933 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_rewrite.c
+++ modules/mappers/mod_rewrite.c
@@ -2347,15 +2347,22 @@ static void do_expand_env(data_item *env, rewrite_ctx *ctx)
 
     while (env) {
         name = do_expand(env->data, ctx, NULL);
-        if ((val = ap_strchr(name, ':')) != NULL) {
-            *val++ = '\0';
-        } else {
-            val = "";
+        if (*name == '!') {
+            name++;
+            apr_table_unset(ctx->r->subprocess_env, name);
+            rewritelog((ctx->r, 5, NULL, "unsetting env variable '%s'", name));
         }
+        else {
+            if ((val = ap_strchr(name, ':')) != NULL) {
+                *val++ = '\0';
+            } else {
+                val = "";
+            }
 
-        apr_table_set(ctx->r->subprocess_env, name, val);
-        rewritelog((ctx->r, 5, NULL, "setting env variable '%s' to '%s'",
-                    name, val));
+            apr_table_set(ctx->r->subprocess_env, name, val);
+            rewritelog((ctx->r, 5, NULL, "setting env variable '%s' to '%s'",
+                        name, val));
+        }
 
         env = env->next;
     }
