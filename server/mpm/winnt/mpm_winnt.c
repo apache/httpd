35aa4599ac81012fe1a228e1f37e2319becd1a3c
Jeff Trawick <trawick@apache.org>
2011-04-06 20:14:35 +0000

    Log the OS socket (int) instead of the apr_socket_t *.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1089614 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/winnt/mpm_winnt.c
+++ server/mpm/winnt/mpm_winnt.c
@@ -471,14 +471,14 @@ static int send_listeners_to_child(apr_pool_t *p, DWORD dwProcessId,
     for (lr = ap_listeners; lr; lr = lr->next, ++lcnt) {
         apr_os_sock_t nsd;
         lpWSAProtocolInfo = apr_pcalloc(p, sizeof(WSAPROTOCOL_INFO));
-        apr_os_sock_get(&nsd,lr->sd);
+        apr_os_sock_get(&nsd, lr->sd);
         ap_log_error(APLOG_MARK, APLOG_INFO, APR_SUCCESS, ap_server_conf,
                      "Parent: Duplicating socket %d and sending it to child process %d",
                      nsd, dwProcessId);
         if (WSADuplicateSocket(nsd, dwProcessId,
                                lpWSAProtocolInfo) == SOCKET_ERROR) {
             ap_log_error(APLOG_MARK, APLOG_CRIT, apr_get_netos_error(), ap_server_conf,
-                         "Parent: WSADuplicateSocket failed for socket %d. Check the FAQ.", lr->sd );
+                         "Parent: WSADuplicateSocket failed for socket %d. Check the FAQ.", nsd);
             return -1;
         }
 
@@ -486,7 +486,7 @@ static int send_listeners_to_child(apr_pool_t *p, DWORD dwProcessId,
                                       sizeof(WSAPROTOCOL_INFO), &BytesWritten))
                 != APR_SUCCESS) {
             ap_log_error(APLOG_MARK, APLOG_CRIT, rv, ap_server_conf,
-                         "Parent: Unable to write duplicated socket %d to the child.", lr->sd );
+                         "Parent: Unable to write duplicated socket %d to the child.", nsd);
             return -1;
         }
     }
