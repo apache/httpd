8965a30e10efd83c9d321486ec2aadaa40d1d9b0
Joe Orton <jorton@apache.org>
2011-04-08 13:13:36 +0000

    * server/util_expr_eval.c (req_table_func): Try r->err_headers_out for   response headers if given header is not found in r->headers_out   (e.g. as with non-standard headers from CGI scripts).
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1090234 13f79535-47bb-0310-9956-ffa450edef68

--- server/util_expr_eval.c
+++ server/util_expr_eval.c
@@ -761,8 +761,14 @@ static const char *req_table_func(ap_expr_eval_ctx_t *ctx, const void *data,
     if (!ctx->r)
         return "";
 
-    if (name[2] == 's')             /* resp */
-        t = ctx->r->headers_out;
+    if (name[2] == 's') {           /* resp */
+        /* Try r->headers_out first, fall back on err_headers_out. */
+        const char *v = apr_table_get(ctx->r->headers_out, arg);
+        if (v) {
+            return v;
+        }
+        t = ctx->r->err_headers_out;
+    }        
     else if (name[0] == 'n')        /* notes */
         t = ctx->r->notes;
     else if (name[3] == 'e')        /* reqenv */
