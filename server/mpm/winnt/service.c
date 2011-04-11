bd456ceef9af599d2525cc75518fcc27136eb1c2
Jeff Trawick <trawick@apache.org>
2011-04-11 13:56:17 +0000

    match expected type of arg to StartService()
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1091076 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/winnt/service.c
+++ server/mpm/winnt/service.c
@@ -750,7 +750,7 @@ apr_status_t mpm_service_start(apr_pool_t *ptemp, int argc,
                                const char * const * argv)
 {
     apr_status_t rv;
-    char **start_argv;
+    const CHAR **start_argv;
     SC_HANDLE   schService;
     SC_HANDLE   schSCManager;
 
