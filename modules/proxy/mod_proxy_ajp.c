e35a1a09d2c01eba1c30da8ce10ac604e47c7cd4
Guenter Knauf <fuankg@apache.org>
2011-04-14 13:05:48 +0000

    Fixed missing struct member error introduced with r1092076.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1092212 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_ajp.c
+++ modules/proxy/mod_proxy_ajp.c
@@ -341,7 +341,7 @@ static int ap_proxy_ajp_request(apr_pool_t *p, request_rec *r,
          * we assume it is a request that cause a back-end timeout,
          * but doesn't affect the whole worker.
          */
-        if (status == APR_TIMEUP && conn->worker->ping_timeout_set) {
+        if (status == APR_TIMEUP && conn->worker->s->ping_timeout_set) {
             return HTTP_GATEWAY_TIME_OUT;
         }
 
