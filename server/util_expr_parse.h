ef10673c8eb81a6dd00022a214212d74b93e5683
Stefan Fritsch <sf@apache.org>
2011-07-02 07:48:12 +0000

    Update auto-generated code
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1142165 13f79535-47bb-0310-9956-ffa450edef68

--- server/util_expr_parse.h
+++ server/util_expr_parse.h
@@ -41,38 +41,40 @@
    enum yytokentype {
      T_TRUE = 258,
      T_FALSE = 259,
-     T_ERROR = 260,
-     T_DIGIT = 261,
-     T_ID = 262,
-     T_STRING = 263,
-     T_REGEX = 264,
-     T_REGEX_I = 265,
-     T_REGEX_BACKREF = 266,
-     T_OP_UNARY = 267,
-     T_OP_BINARY = 268,
-     T_STR_BEGIN = 269,
-     T_STR_END = 270,
-     T_VAR_BEGIN = 271,
-     T_VAR_END = 272,
-     T_OP_EQ = 273,
-     T_OP_NE = 274,
-     T_OP_LT = 275,
-     T_OP_LE = 276,
-     T_OP_GT = 277,
-     T_OP_GE = 278,
-     T_OP_REG = 279,
-     T_OP_NRE = 280,
-     T_OP_IN = 281,
-     T_OP_STR_EQ = 282,
-     T_OP_STR_NE = 283,
-     T_OP_STR_LT = 284,
-     T_OP_STR_LE = 285,
-     T_OP_STR_GT = 286,
-     T_OP_STR_GE = 287,
-     T_OP_CONCAT = 288,
-     T_OP_OR = 289,
-     T_OP_AND = 290,
-     T_OP_NOT = 291
+     T_EXPR_BOOL = 260,
+     T_EXPR_STRING = 261,
+     T_ERROR = 262,
+     T_DIGIT = 263,
+     T_ID = 264,
+     T_STRING = 265,
+     T_REGEX = 266,
+     T_REGEX_I = 267,
+     T_REGEX_BACKREF = 268,
+     T_OP_UNARY = 269,
+     T_OP_BINARY = 270,
+     T_STR_BEGIN = 271,
+     T_STR_END = 272,
+     T_VAR_BEGIN = 273,
+     T_VAR_END = 274,
+     T_OP_EQ = 275,
+     T_OP_NE = 276,
+     T_OP_LT = 277,
+     T_OP_LE = 278,
+     T_OP_GT = 279,
+     T_OP_GE = 280,
+     T_OP_REG = 281,
+     T_OP_NRE = 282,
+     T_OP_IN = 283,
+     T_OP_STR_EQ = 284,
+     T_OP_STR_NE = 285,
+     T_OP_STR_LT = 286,
+     T_OP_STR_LE = 287,
+     T_OP_STR_GT = 288,
+     T_OP_STR_GE = 289,
+     T_OP_CONCAT = 290,
+     T_OP_OR = 291,
+     T_OP_AND = 292,
+     T_OP_NOT = 293
    };
 #endif
 
@@ -92,7 +94,7 @@ typedef union YYSTYPE
 
 
 /* Line 1676 of yacc.c  */
-#line 96 "util_expr_parse.h"
+#line 98 "util_expr_parse.h"
 } YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
