1317bee50b41ea9e53ccd4a528f858b69a714b21
Guenter Knauf <fuankg@apache.org>
2011-07-12 16:13:28 +0000

    Fixed some more env vars which make problems.
    This fix is based on BZ 13029 / 34985, and includes
    now the SSL_ and GEOIP_ vars; otherwise its impossible
    to run CGIs when mod_ssl and/or mod_geoip are loaded
    and those mods return UTF-8 chars in any var during a
    request.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1145647 13f79535-47bb-0310-9956-ffa450edef68

--- modules/arch/win32/mod_win32.c
+++ modules/arch/win32/mod_win32.c
@@ -528,9 +528,10 @@ static apr_status_t ap_cgi_build_command(const char **cmd, const char ***argv,
                 && (strncmp(elts[i].key, "HTTP_", 5) == 0
                  || strncmp(elts[i].key, "SERVER_", 7) == 0
                  || strncmp(elts[i].key, "REQUEST_", 8) == 0
-                 || strcmp(elts[i].key, "QUERY_STRING") == 0
-                 || strcmp(elts[i].key, "PATH_INFO") == 0
-                 || strcmp(elts[i].key, "PATH_TRANSLATED") == 0)) {
+                 || strncmp(elts[i].key, "PATH_", 5) == 0
+                 || strncmp(elts[i].key, "SSL_", 4) == 0
+                 || strncmp(elts[i].key, "GEOIP_", 6) == 0
+                 || strcmp(elts[i].key, "QUERY_STRING") == 0)) {
             prep_string((const char**) &elts[i].val, r->pool);
         }
     }
