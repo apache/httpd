52a548856f1c2dd330e22d07701b6d0c09c0c147
Stefan Fritsch <sf@apache.org>
2011-08-20 18:29:02 +0000

    Simplify some overly complicated code using apr_brigade_putstrs and apr_pstrcat
    Submitted by: Christophe Jaillet <christophe jaillet wanadoo fr>,
                  Stefan Fritsch
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1159896 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/mod_include.c
+++ modules/filters/mod_include.c
@@ -2680,36 +2680,18 @@ static apr_status_t handle_printenv(include_ctx_t *ctx, ap_filter_t *f,
 
     for (i = 0; i < arr->nelts; ++i) {
         const char *key_text, *val_text;
-        char *key_val, *next;
-        apr_size_t k_len, v_len, kv_length;
 
         /* get key */
         key_text = ap_escape_html(ctx->dpool, elts[i].key);
-        k_len = strlen(key_text);
 
         /* get value */
         val_text = elts[i].val;
-        if (val_text == LAZY_VALUE) {
+        if (val_text == LAZY_VALUE)
             val_text = add_include_vars_lazy(r, elts[i].key, ctx->time_str);
-        }
         val_text = ap_escape_html(ctx->dpool, val_text);
-        v_len = strlen(val_text);
 
-        /* assemble result */
-        kv_length = k_len + v_len + sizeof("=\n");
-        key_val = apr_palloc(ctx->pool, kv_length);
-        next = key_val;
-
-        memcpy(next, key_text, k_len);
-        next += k_len;
-        *next++ = '=';
-        memcpy(next, val_text, v_len);
-        next += v_len;
-        *next++ = '\n';
-        *next = 0;
-
-        APR_BRIGADE_INSERT_TAIL(bb, apr_bucket_pool_create(key_val, kv_length-1,
-                                ctx->pool, f->c->bucket_alloc));
+        apr_brigade_putstrs(bb, NULL, NULL, key_text, "=", val_text, "\n",
+                            NULL);
     }
 
     ctx->flush_now = 1;
