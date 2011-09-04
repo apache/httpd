b5dc0f0ca94faaca105b5d26c9e18d48418a7147
Jim Jagielski <jim@apache.org>
2011-09-04 19:37:31 +0000

    PR 44020
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1165101 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_alias.c
+++ modules/mappers/mod_alias.c
@@ -429,7 +429,7 @@ static int translate_alias_redir(request_rec *r)
             if (ret[0] == '/') {
 
                 ret = ap_construct_url(r->pool, ret, r);
-                ap_log_rerror(APLOG_MARK, APLOG_WARNING, 0, r,
+                ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
                               "incomplete redirection target of '%s' for "
                               "URI '%s' modified to '%s'",
                               orig_target, r->uri, ret);
