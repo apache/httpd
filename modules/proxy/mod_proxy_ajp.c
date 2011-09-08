17d779bff4f706b16449c7042c1ee7b0b59a29eb
Jean-Frederic Clere <jfclere@apache.org>
2011-09-08 12:41:30 +0000

    Use HTTP_NOT_IMPLEMENTED instead of HTTP_BAD_REQUEST.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1166657 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_ajp.c
+++ modules/proxy/mod_proxy_ajp.c
@@ -214,9 +214,11 @@ static int ap_proxy_ajp_request(apr_pool_t *p, request_rec *r,
                      "proxy: AJP: request failed to %pI (%s)",
                      conn->worker->cp->addr,
                      conn->worker->s->hostname);
-        if (status == AJP_EOVERFLOW || status == AJP_EBAD_METHOD)
+        if (status == AJP_EOVERFLOW)
             return HTTP_BAD_REQUEST;
-        else {
+        else if  (status == AJP_EBAD_METHOD) {
+            return HTTP_NOT_IMPLEMENTED;
+        } else {
             /*
              * This is only non fatal when the method is idempotent. In this
              * case we can dare to retry it with a different worker if we are
