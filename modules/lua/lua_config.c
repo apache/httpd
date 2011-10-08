45cbf99b002655c29126abc2cbfd853066ed8a49
Stefan Fritsch <sf@apache.org>
2011-10-08 07:50:06 +0000

    Fix format string bugs in mod_lua. Found by gcc and pointed out by Igor Galic.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1180332 13f79535-47bb-0310-9956-ffa450edef68

--- modules/lua/lua_config.c
+++ modules/lua/lua_config.c
@@ -174,7 +174,8 @@ static int cmd_log_at(lua_State *L, int level)
     lua_getinfo(L, "Sl", &dbg);
 
     msg = luaL_checkstring(L, 2);
-    ap_log_error(dbg.source, dbg.currentline, APLOG_MODULE_INDEX, level, 0, cmd->server, msg);
+    ap_log_error(dbg.source, dbg.currentline, APLOG_MODULE_INDEX, level, 0,
+                 cmd->server, "%s", msg);
     return 0;
 }
 
