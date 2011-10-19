899ad6feac1a93c7eebc4bbcd8e44648f534e979
Stefan Fritsch <sf@apache.org>
2011-10-19 22:40:06 +0000

    Comment out some incomplete code that does nothing but produce a compiler warning
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1186545 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/simple/simple_io.c
+++ server/mpm/simple/simple_io.c
@@ -31,12 +31,14 @@ APLOG_USE_MODULE(mpm_simple);
 
 static void simple_io_timeout_cb(simple_core_t * sc, void *baton)
 {
+#if 0
     simple_conn_t *scon = (simple_conn_t *) baton;
     /* pqXXXXX: handle timeouts. */
     conn_rec *c = scon->c;
     conn_state_t *cs = c->cs;
 
     cs = NULL;
+#endif
 
     ap_log_error(APLOG_MARK, APLOG_WARNING, 0, ap_server_conf,
                  "io timeout hit (?)");
