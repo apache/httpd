dfd7da92cb64b46d49bad3b0400c984268249a85
Stefan Fritsch <sf@apache.org>
2011-11-08 23:27:52 +0000

    fix typo
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1199528 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -4622,7 +4622,7 @@ AP_CORE_DECLARE(void) ap_random_parent_after_fork(void)
     /*
      * To ensure that the RNG state in the parent changes after the fork, we
      * pull some data from the RNG and discard it. This ensures that the RNG
-     * states in the children ar different even after the pid wraps around.
+     * states in the children are different even after the pid wraps around.
      * As we only use apr_random for insecure random bytes, pulling 2 bytes
      * should be enough.
      * XXX: APR should probably have some dedicated API to do this, but it
