e4c7cb718e4d657c9aba7d2631479d676cc867c6
Jeff Trawick <trawick@apache.org>
2011-11-09 04:23:13 +0000

    use non-'const char *' while building string to avoid warnings
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1199611 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_authn_socache.c
+++ modules/aaa/mod_authn_socache.c
@@ -231,11 +231,13 @@ static const char *construct_key(request_rec *r, const char *context,
     /* handle "special" context values */
     if (!strcmp(context, "directory")) {
         /* FIXME: are we at risk of this blowing up? */
+        char *new_context;
         char *slash = strrchr(r->uri, '/');
-        context = apr_palloc(r->pool, slash - r->uri +
-                                      strlen(r->server->server_hostname) + 1);
-        strcpy(context, r->server->server_hostname);
-        strncat(context, r->uri, slash - r->uri);
+        new_context = apr_palloc(r->pool, slash - r->uri +
+                                 strlen(r->server->server_hostname) + 1);
+        strcpy(new_context, r->server->server_hostname);
+        strncat(new_context, r->uri, slash - r->uri);
+        context = new_context;
     }
     else if (!strcmp(context, "server")) {
         context = r->server->server_hostname;
