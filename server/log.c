apache/httpd

commit aeb675b6c169303c6562e23efb5ebc20ae716ddc

Fix warning about comparing signed/unsigned
git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1041140 13f79535-47bb-0310-9956-ffa450edef68

server/log.c
@@ -1017,7 +1017,7 @@ static int do_errorlog_format(apr_array_header_t *fmt, ap_errorlog_info *info,
         else if (skipping) {
             continue;
         }
-        else if (item->min_loglevel > info->level) {
+        else if ((int)item->min_loglevel > info->level) {
             len = field_start;
             skipping = 1;
         }

commit 4a69a843de602db563fdec067327c98768321ad4

Fix per-request / per-conn logging, probably broken by r1041140
git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1199591 13f79535-47bb-0310-9956-ffa450edef68

server/log.c
@@ -1033,7 +1033,7 @@ static int do_errorlog_format(apr_array_header_t *fmt, ap_errorlog_info *info,
         else if (skipping) {
             continue;
         }
-        else if ((int)item->min_loglevel > info->level) {
+        else if (info->level != -1 && (int)item->min_loglevel > info->level) {
             len = field_start;
             skipping = 1;
         }