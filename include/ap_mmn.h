27a2fafa39cc2e90083b1f50d05cba868bfb7abb
Ruediger Pluem <rpluem@apache.org>
2011-11-18 19:12:31 +0000

    * Actually change the defines and not only the comment.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1203792 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_mmn.h
+++ include/ap_mmn.h
@@ -371,9 +371,9 @@
 #define MODULE_MAGIC_COOKIE 0x41503234UL /* "AP24" */
 
 #ifndef MODULE_MAGIC_NUMBER_MAJOR
-#define MODULE_MAGIC_NUMBER_MAJOR 20111025
+#define MODULE_MAGIC_NUMBER_MAJOR 20111118
 #endif
-#define MODULE_MAGIC_NUMBER_MINOR 2                   /* 0...n */
+#define MODULE_MAGIC_NUMBER_MINOR 0                   /* 0...n */
 
 /**
  * Determine if the server's current MODULE_MAGIC_NUMBER is at least a
