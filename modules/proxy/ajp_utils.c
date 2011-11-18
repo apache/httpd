b56ba5a823513545e91b07bef989283c003e0146
Stefan Fritsch <sf@apache.org>
2011-11-18 22:02:27 +0000

    More ap_log_rerror() usage and axe some more AJP: prefixes
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1203878 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/ajp_utils.c
+++ modules/proxy/ajp_utils.c
@@ -30,19 +30,19 @@ apr_status_t ajp_handle_cping_cpong(apr_socket_t *sock,
     apr_interval_time_t org;
     apr_byte_t result;
 
-    ap_log_error(APLOG_MARK, APLOG_TRACE8, 0, r->server,
+    ap_log_rerror(APLOG_MARK, APLOG_TRACE8, 0, r,
                          "Into ajp_handle_cping_cpong");
 
     rc = ajp_msg_create(r->pool, AJP_PING_PONG_SZ, &msg);
     if (rc != APR_SUCCESS) {
-        ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server,
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                "ajp_handle_cping_cpong: ajp_msg_create failed");
         return rc;
     }
 
     rc = ajp_msg_serialize_cping(msg);
     if (rc != APR_SUCCESS) {
-        ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server,
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                "ajp_handle_cping_cpong: ajp_marshal_into_msgb failed");
         return rc;
     }
@@ -50,14 +50,14 @@ apr_status_t ajp_handle_cping_cpong(apr_socket_t *sock,
     rc = ajp_ilink_send(sock, msg);
     ajp_msg_log(r, msg, "ajp_handle_cping_cpong: ajp_ilink_send packet dump");
     if (rc != APR_SUCCESS) {
-        ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server,
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                "ajp_handle_cping_cpong: ajp_ilink_send failed");
         return rc;
     }
 
     rc = apr_socket_timeout_get(sock, &org);
     if (rc != APR_SUCCESS) {
-        ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server,
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                "ajp_handle_cping_cpong: apr_socket_timeout_get failed");
         return rc;
     }
@@ -65,7 +65,7 @@ apr_status_t ajp_handle_cping_cpong(apr_socket_t *sock,
     /* Set CPING/CPONG response timeout */
     rc = apr_socket_timeout_set(sock, timeout);
     if (rc != APR_SUCCESS) {
-        ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server,
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                "ajp_handle_cping_cpong: apr_socket_timeout_set failed");
         return rc;
     }
@@ -74,7 +74,7 @@ apr_status_t ajp_handle_cping_cpong(apr_socket_t *sock,
     /* Read CPONG reply */
     rv = ajp_ilink_receive(sock, msg);
     if (rv != APR_SUCCESS) {
-        ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server,
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                "ajp_handle_cping_cpong: ajp_ilink_receive failed");
         goto cleanup;
     }
@@ -82,12 +82,12 @@ apr_status_t ajp_handle_cping_cpong(apr_socket_t *sock,
     ajp_msg_log(r, msg, "ajp_handle_cping_cpong: ajp_ilink_receive packet dump");
     rv = ajp_msg_get_uint8(msg, &result);
     if (rv != APR_SUCCESS) {
-        ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server,
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                "ajp_handle_cping_cpong: invalid CPONG message");
         goto cleanup;
     }
     if (result != CMD_AJP13_CPONG) {
-        ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server,
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                "ajp_handle_cping_cpong: awaited CPONG, received %d ",
                result);
         rv = APR_EGENERAL;
@@ -98,12 +98,12 @@ cleanup:
     /* Restore original socket timeout */
     rc = apr_socket_timeout_set(sock, org);
     if (rc != APR_SUCCESS) {
-        ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server,
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                "ajp_handle_cping_cpong: apr_socket_timeout_set failed");
         return rc;
     }
 
-    ap_log_error(APLOG_MARK, APLOG_TRACE8, 0, r->server,
+    ap_log_rerror(APLOG_MARK, APLOG_TRACE8, 0, r,
                          "ajp_handle_cping_cpong: Done");
     return rv;
 }
--- modules/proxy/mod_proxy_express.c
+++ modules/proxy/mod_proxy_express.c
@@ -122,16 +122,15 @@ static int xlate_name(request_rec *r)
         return DECLINED;
     }
 
-    ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, r->server,
-                 "proxy_express: Enabled");
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r, "proxy_express: Enabled");
     if (!sconf->dbmfile || (r->filename && strncmp(r->filename, "proxy:", 6) == 0)) {
         /* it should be go on as an internal proxy request */
         return DECLINED;
     }
 
-    ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, r->server,
-                 "proxy_express: Opening DBM file: %s (%s)",
-                 sconf->dbmfile, sconf->dbmtype);
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
+                  "proxy_express: Opening DBM file: %s (%s)",
+                  sconf->dbmfile, sconf->dbmtype);
     rv = apr_dbm_open_ex(&db, sconf->dbmtype, sconf->dbmfile, APR_DBM_READONLY,
                          APR_OS_DEFAULT, r->pool);
     if (rv != APR_SUCCESS) {
@@ -139,8 +138,8 @@ static int xlate_name(request_rec *r)
     }
 
     name = ap_get_server_name(r);
-    ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, r->server,
-                 "proxy_express: looking for %s", name);
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
+                  "proxy_express: looking for %s", name);
     key.dptr = (char *)name;
     key.dsize = strlen(key.dptr);
 
@@ -155,14 +154,14 @@ static int xlate_name(request_rec *r)
         return DECLINED;
     }
 
-    ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, r->server,
-                 "proxy_express: found %s -> %s", name, backend);
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
+                  "proxy_express: found %s -> %s", name, backend);
     r->filename = apr_pstrcat(r->pool, "proxy:", backend, r->uri, NULL);
     r->handler = "proxy-server";
     r->proxyreq = PROXYREQ_REVERSE;
 
-    ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, r->server,
-                 "proxy_express: rewritten as: %s", r->filename);
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
+                  "proxy_express: rewritten as: %s", r->filename);
 
     ralias = (struct proxy_alias *)dconf->raliases->elts;
     /*
@@ -183,8 +182,8 @@ static int xlate_name(request_rec *r)
 
     /* Didn't find one... add it */
     if (!ralias) {
-        ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, r->server,
-                    "proxy_express: adding PPR entry");
+        ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
+                      "proxy_express: adding PPR entry");
         ralias = apr_array_push(dconf->raliases);
         ralias->fake = "/";
         ralias->real = apr_pstrdup(dconf->raliases->pool, backend);
