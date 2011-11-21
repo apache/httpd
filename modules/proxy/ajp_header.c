99977abb7164fb3834be8fa7af7f29145733342d
Jim Jagielski <jim@apache.org>
2011-11-21 15:51:46 +0000

    check return status
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1204556 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/ajp_header.c
+++ modules/proxy/ajp_header.c
@@ -683,6 +683,11 @@ apr_status_t ajp_read_header(apr_socket_t *sock,
     }
     ajp_msg_log(r, *msg, "ajp_read_header: ajp_ilink_receive packet dump");
     rc = ajp_msg_peek_uint8(*msg, &result);
+    if (rc != APR_SUCCESS) {
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+                      "ajp_read_header: ajp_msg_peek_uint8 failed");
+        return rc;
+    }
     ap_log_rerror(APLOG_MARK, APLOG_TRACE1, 0, r,
                "ajp_read_header: ajp_ilink_received %s (0x%02x)",
                ajp_type_str(result), result);
