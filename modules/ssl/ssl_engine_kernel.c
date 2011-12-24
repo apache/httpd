4f69e8c4d737a738d462d8456f98d83a7433949f
Kaspar Brand <kbrand@apache.org>
2011-12-24 06:27:02 +0000

    merge r1222915 from trunk:
    REMOTE_ADDR is now r->useragent_ip, which is not/never available at
    SSL_CB_HANDSHAKE_DONE time. ap_log_cerror() already logs c->client_ip
    for us, however, so we can simply drop REMOTE_ADDR from the message.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1222916 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_kernel.c
+++ modules/ssl/ssl_engine_kernel.c
@@ -1879,9 +1879,7 @@ static void log_tracing_state(const SSL *ssl, conn_rec *c,
      */
     if (where & SSL_CB_HANDSHAKE_DONE) {
         ap_log_cerror(APLOG_MARK, APLOG_DEBUG, 0, c, APLOGNO(02041)
-                      "Connection: Client IP: %s, Protocol: %s, "
-                      "Cipher: %s (%s/%s bits)",
-                      ssl_var_lookup(NULL, s, c, NULL, "REMOTE_ADDR"),
+                      "Protocol: %s, Cipher: %s (%s/%s bits)",
                       ssl_var_lookup(NULL, s, c, NULL, "SSL_PROTOCOL"),
                       ssl_var_lookup(NULL, s, c, NULL, "SSL_CIPHER"),
                       ssl_var_lookup(NULL, s, c, NULL, "SSL_CIPHER_USEKEYSIZE"),
