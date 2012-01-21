935ce4278b44436f658bdf499cd5a2c1671eeb1f
Jim Jagielski <jim@apache.org>
2012-01-21 14:38:25 +0000

    Update copyrights for externally visible and changed code
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1234336 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_release.h
+++ include/ap_release.h
@@ -25,7 +25,7 @@
 #include "apr_general.h" /* stringify */
 
 #define AP_SERVER_COPYRIGHT \
-  "Copyright 2011 The Apache Software Foundation."
+  "Copyright 2012 The Apache Software Foundation."
 
 /*
  * The below defines the base string of the Server: header. Additional
