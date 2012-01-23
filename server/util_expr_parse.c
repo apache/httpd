875b3310179a3503d995442bb72bba3d52a97ecf
Jim Jagielski <jim@apache.org>
2012-01-23 11:58:30 +0000

    remove mistakenly commited patches...
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1234755 13f79535-47bb-0310-9956-ffa450edef68

--- server/util_expr_parse.c
+++ server/util_expr_parse.c
@@ -2,7 +2,7 @@
 
 /* Bison implementation for Yacc-like parsers in C
    
-      Copyright (C) 1984, 1989-1990, 2000-2012 Free Software Foundation, Inc.
+      Copyright (C) 1984, 1989-1990, 2000-2011 Free Software Foundation, Inc.
    
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
