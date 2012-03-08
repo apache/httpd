deab2b57866e883d7da0504e25ad2cc9f213587d
Jim Jagielski <jim@apache.org>
2012-03-08 15:40:40 +0000

    Bump
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1298432 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_mmn.h
+++ include/ap_mmn.h
@@ -402,7 +402,7 @@
 #ifndef MODULE_MAGIC_NUMBER_MAJOR
 #define MODULE_MAGIC_NUMBER_MAJOR 20120211
 #endif
-#define MODULE_MAGIC_NUMBER_MINOR 1                   /* 0...n */
+#define MODULE_MAGIC_NUMBER_MINOR 2                   /* 0...n */
 
 /**
  * Determine if the server's current MODULE_MAGIC_NUMBER is at least a
