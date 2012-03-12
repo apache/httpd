c8b741d22fda94ce5c34aa353ae6acbccc968c1b
Eric Covener <covener@apache.org>
2012-03-12 17:03:39 +0000

    Stray/confusing "recovery" in this failonstatus=XXX error message seems to be a copy/paste error.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1299738 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_balancer.c
+++ modules/proxy/mod_proxy_balancer.c
@@ -624,8 +624,9 @@ static int proxy_balancer_post_request(proxy_worker *worker,
             int val = ((int *)balancer->errstatuses->elts)[i];
             if (r->status == val) {
                 ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r, APLOGNO(01174)
-                              "%s:  Forcing recovery for worker (%s), "
-                              "failonstatus %d",
+                              "%s: Forcing worker (%s) into error state " 
+                              "due to status code %d matching 'failonstatus' "
+                              "balancer parameter",
                               balancer->s->name, worker->s->name, val);
                 worker->s->status |= PROXY_WORKER_IN_ERROR;
                 worker->s->error_time = apr_time_now();
