bc4364641edcee397dec664604c3ea3addc380ed
Eric Covener <covener@apache.org>
2012-03-23 22:43:28 +0000

    mod_request: fix check for bad KeptBodySize argument
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1304641 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/mod_request.c
+++ modules/filters/mod_request.c
@@ -358,7 +358,7 @@ static const char *set_kept_body_size(cmd_parms *cmd, void *dconf,
     char *end = NULL;
 
     if (APR_SUCCESS != apr_strtoff(&(conf->keep_body), arg, &end, 10)
-            || conf->keep_body < 0 || end) {
+            || conf->keep_body < 0 || *end) {
         return "KeptBodySize must be a valid size in bytes, or zero.";
     }
     conf->keep_body_set = 1;
