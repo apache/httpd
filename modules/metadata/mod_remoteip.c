7e993eabe1b340e64a59885aecee25fed78313c9
Eric Covener <covener@apache.org>
2012-03-25 12:05:39 +0000

    Submitted by: sf Reviewed by sf, trawick, druggeri
    Merge r1304855 from trunk:
    
    Downgrade log message about adjusted IP address to trace1
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1305016 13f79535-47bb-0310-9956-ffa450edef68

--- modules/metadata/mod_remoteip.c
+++ modules/metadata/mod_remoteip.c
@@ -397,7 +397,7 @@ static int remoteip_modify_request(request_rec *r)
     r->useragent_addr = req->useragent_addr;
     r->useragent_ip = req->useragent_ip;
 
-    ap_log_rerror(APLOG_MARK, APLOG_INFO, 0, r,
+    ap_log_rerror(APLOG_MARK, APLOG_TRACE1, 0, r,
                   req->proxy_ips
                       ? "Using %s as client's IP by proxies %s"
                       : "Using %s as client's IP by internal proxies",
