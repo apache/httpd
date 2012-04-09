60ec3c7796bfb2c65d82a13ee5ddb47dd3b2093d
Stefan Fritsch <sf@apache.org>
2012-04-09 09:28:14 +0000

    If there is an error reading the headers from the backend, send an error to the client
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1311172 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_fcgi.c
+++ modules/proxy/mod_proxy_fcgi.c
@@ -748,10 +748,10 @@ recv_again:
                                 apr_brigade_cleanup(ob);
                                 tmp_b = apr_bucket_eos_create(c->bucket_alloc);
                                 APR_BRIGADE_INSERT_TAIL(ob, tmp_b);
+                                r->status = status;
                                 ap_pass_brigade(r->output_filters, ob);
                                 ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(01070)
                                               "Error parsing script headers");
-                                r->status = status;
                                 rv = APR_EINVAL;
                                 break;
                             }
