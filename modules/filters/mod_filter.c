e32fd2b7757b911990adb37949c4bc941f465245
Jim Jagielski <jim@apache.org>
2012-04-27 13:06:38 +0000

    Merge r1299783 from trunk:
    some more trace logging for AddOutputFilterByType
    
    Submitted by: sf
    Reviewed/backported by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1331412 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/mod_filter.c
+++ modules/filters/mod_filter.c
@@ -165,12 +165,20 @@ static int filter_lookup(ap_filter_t *f, ap_filter_rec_t *filter)
             const char **type = provider->types;
             size_t len = strcspn(r->content_type, "; \t");
             AP_DEBUG_ASSERT(type != NULL);
+            ap_log_rerror(APLOG_MARK, APLOG_TRACE4, 0, r,
+                          "Content-Type '%s' ...", r->content_type);
             while (*type) {
                 /* Handle 'content-type;charset=...' correctly */
                 if (strncmp(*type, r->content_type, len) == 0
                     && (*type)[len] == '\0') {
+                    ap_log_rerror(APLOG_MARK, APLOG_TRACE4, 0, r,
+                                  "... matched '%s'", *type);
                     match = 1;
                     break;
+                }
+                else {
+                    ap_log_rerror(APLOG_MARK, APLOG_TRACE4, 0, r,
+                                  "... did not match '%s'", *type);
                 }
                 type++;
             }
@@ -178,6 +186,11 @@ static int filter_lookup(ap_filter_t *f, ap_filter_rec_t *filter)
                           "Content-Type condition for '%s' %s",
                           provider->frec->name,
                           match ? "matched" : "did not match");
+        }
+        else {
+            ap_log_rerror(APLOG_MARK, APLOG_TRACE2, 0, r,
+                          "Content-Type condition for '%s' did not match: "
+                          "no Content-Type", provider->frec->name);
         }
 
         if (match) {
