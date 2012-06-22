6ca2bca4a2497653fe1cbb967e5f82f7cbb5b38d
Stefan Fritsch <sf@apache.org>
2012-06-22 14:56:44 +0000

    note more prominently that SHA and crypt are insecure
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1352910 13f79535-47bb-0310-9956-ffa450edef68

--- support/htpasswd.c
+++ support/htpasswd.c
@@ -283,9 +283,10 @@ static void usage(void)
         " (default)"
         "." NL);
     apr_file_printf(errfile, " -d  Force CRYPT encryption of the password"
-            "." NL);
+            " (8 chars max, insecure)." NL);
     apr_file_printf(errfile, " -p  Do not encrypt the password (plaintext)." NL);
-    apr_file_printf(errfile, " -s  Force SHA encryption of the password." NL);
+    apr_file_printf(errfile, " -s  Force SHA encryption of the password"
+            " (insecure)." NL);
     apr_file_printf(errfile, " -b  Use the password from the command line "
             "rather than prompting for it." NL);
     apr_file_printf(errfile, " -D  Delete the specified user." NL);
