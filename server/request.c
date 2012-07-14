917d8369f2daa044140f715877812edf054fc320
Stefan Fritsch <sf@apache.org>
2012-07-14 18:43:31 +0000

    Merge 1351015:
        Adjust log message: Condition can be caused by configuration error
        not only by bugs
    
    Only text change, therefore CTR.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1361571 13f79535-47bb-0310-9956-ffa450edef68

--- server/request.c
+++ server/request.c
@@ -236,7 +236,9 @@ AP_DECLARE(int) ap_process_request_internal(request_rec *r)
                 if (r->user == NULL) {
                     /* don't let buggy authn module crash us in authz */
                     ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(00027)
-                                  "Buggy authn provider failed to set user for %s",
+                                  "No authentication done but request not "
+                                  "allowed without authentication for %s. "
+                                  "Authentication not configured?",
                                   r->uri);
                     access_status = HTTP_INTERNAL_SERVER_ERROR;
                     return decl_die(access_status, "check user", r);
@@ -271,7 +273,9 @@ AP_DECLARE(int) ap_process_request_internal(request_rec *r)
                 if (r->user == NULL) {
                     /* don't let buggy authn module crash us in authz */
                     ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(00028)
-                                  "Buggy authn provider failed to set user for %s",
+                                  "No authentication done but request not "
+                                  "allowed without authentication for %s. "
+                                  "Authentication not configured?",
                                   r->uri);
                     access_status = HTTP_INTERNAL_SERVER_ERROR;
                     return decl_die(access_status, "check user", r);
