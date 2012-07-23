754956a9879f22c395cec560d62ad7bc2ee14098
Joe Orton <jorton@apache.org>
2012-07-23 15:34:59 +0000

    Merge r1327689 from trunk:
    For a local variable in a macro, use a name that is less likely to clash
    with other variable names.
    
    Submitted by: sf
    Reviewed by: rjung, humbedooh, jorton
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1364682 13f79535-47bb-0310-9956-ffa450edef68

--- include/http_log.h
+++ include/http_log.h
@@ -363,8 +363,8 @@ AP_DECLARE(void) ap_log_error(const char *file, int line, int module_index,
 #define ap_log_error(...) ap_log_error__(__VA_ARGS__)
 /* need server_rec *sr = ... for the case if s is verbatim NULL */
 #define ap_log_error__(file, line, mi, level, status, s, ...)           \
-    do { const server_rec *sr = s; if (APLOG_MODULE_IS_LEVEL(sr, mi, level))      \
-             ap_log_error_(file, line, mi, level, status, sr, __VA_ARGS__); \
+    do { const server_rec *sr__ = s; if (APLOG_MODULE_IS_LEVEL(sr__, mi, level)) \
+             ap_log_error_(file, line, mi, level, status, sr__, __VA_ARGS__);    \
     } while(0)
 #else
 #define ap_log_error ap_log_error_
