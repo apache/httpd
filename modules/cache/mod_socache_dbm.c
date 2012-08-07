ae51bf65d4fe61251e8cc590d0dca037270826b8
Jeff Trawick <trawick@apache.org>
2012-08-07 12:44:30 +0000

    mod_socache_shmcb and mod_socache_dbm: finish support of DefaultRuntimeDir
    a partial conversion was made in r1299718, affecting cases
    where no filename was specified
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1370225 13f79535-47bb-0310-9956-ffa450edef68

--- modules/cache/mod_socache_dbm.c
+++ modules/cache/mod_socache_dbm.c
@@ -84,7 +84,7 @@ static const char *socache_dbm_create(ap_socache_instance_t **context,
     *context = ctx = apr_pcalloc(p, sizeof *ctx);
 
     if (arg && *arg) {
-        ctx->data_file = ap_server_root_relative(p, arg);
+        ctx->data_file = ap_runtime_dir_relative(p, arg);
         if (!ctx->data_file) {
             return apr_psprintf(tmp, "Invalid cache file path %s", arg);
         }
