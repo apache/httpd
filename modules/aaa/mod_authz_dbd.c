4b1d9f5c008ee2e37cb58e7c87d85a15381e585d
Jim Jagielski <jim@apache.org>
2012-08-14 20:54:00 +0000

    Merge r1364229 from trunk:
    Correctly log the error returned by apr_dbd_pvselect()
    
    Submitted by: sf
    Reviewed/backported by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1373087 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_authz_dbd.c
+++ modules/aaa/mod_authz_dbd.c
@@ -162,8 +162,8 @@ static int authz_dbd_login(request_rec *r, authz_dbd_cfg *cfg,
                           "authz_dbd: no redirect query!");
             /* OK, this is non-critical; we can just not-redirect */
         }
-        else if (apr_dbd_pvselect(dbd->driver, r->pool, dbd->handle, &res,
-                                  query, 0, r->user, NULL) == 0) {
+        else if ((rv = apr_dbd_pvselect(dbd->driver, r->pool, dbd->handle,
+                                        &res, query, 0, r->user, NULL) == 0)) {
             for (rv = apr_dbd_get_row(dbd->driver, r->pool, res, &row, -1);
                  rv != -1;
                  rv = apr_dbd_get_row(dbd->driver, r->pool, res, &row, -1)) {
