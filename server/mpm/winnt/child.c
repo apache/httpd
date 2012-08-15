e836ae48a1738a53713dee44cff9ea5d8c225afa
Jeff Trawick <trawick@apache.org>
2012-08-15 11:41:56 +0000

    WinNT MPM: Fix INVALID_SOCKET bogosity
    The related code and a lot more was removed from trunk in r730828.
    
    This doesn't address any reported problem symptom.
    
    Submitted by: trawick
    Reviewed by: druggeri, rpluem
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1373349 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/winnt/child.c
+++ server/mpm/winnt/child.c
@@ -401,9 +401,9 @@ static unsigned int __stdcall win9x_accept(void * dummy)
         do {
             clen = sizeof(sa_client);
             csd = accept(nsd, (struct sockaddr *) &sa_client, &clen);
-        } while (csd < 0 && APR_STATUS_IS_EINTR(apr_get_netos_error()));
+        } while (csd == INVALID_SOCKET && APR_STATUS_IS_EINTR(apr_get_netos_error()));
 
-        if (csd < 0) {
+        if (csd == INVALID_SOCKET) {
             if (APR_STATUS_IS_ECONNABORTED(apr_get_netos_error())) {
                 ap_log_error(APLOG_MARK, APLOG_ERR, apr_get_netos_error(), ap_server_conf,
                             "accept: (client socket)");
