5a9f66d9e61a6e06ea4f871e12ebddc436ebfd6a
Jim Jagielski <jim@apache.org>
2012-09-19 14:17:03 +0000

    wtf are we doing merging in these from the parent?? These are server specific!
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1387603 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy.c
+++ modules/proxy/mod_proxy.c
@@ -1173,13 +1173,13 @@ static void * merge_proxy_config(apr_pool_t *p, void *basev, void *overridesv)
     proxy_server_conf *base = (proxy_server_conf *) basev;
     proxy_server_conf *overrides = (proxy_server_conf *) overridesv;
 
-    ps->proxies = apr_array_append(p, base->proxies, overrides->proxies);
-    ps->sec_proxy = apr_array_append(p, base->sec_proxy, overrides->sec_proxy);
-    ps->aliases = apr_array_append(p, base->aliases, overrides->aliases);
-    ps->noproxies = apr_array_append(p, base->noproxies, overrides->noproxies);
-    ps->dirconn = apr_array_append(p, base->dirconn, overrides->dirconn);
-    ps->workers = apr_array_append(p, base->workers, overrides->workers);
-    ps->balancers = apr_array_append(p, base->balancers, overrides->balancers);
+    ps->proxies = overrides->proxies;
+    ps->sec_proxy = overrides->sec_proxy;
+    ps->aliases = overrides->aliases;
+    ps->noproxies = overrides->noproxies;
+    ps->dirconn = overrides->dirconn;
+    ps->workers = overrides->workers;
+    ps->balancers = overrides->balancers;
     ps->forward = overrides->forward ? overrides->forward : base->forward;
     ps->reverse = overrides->reverse ? overrides->reverse : base->reverse;
 
