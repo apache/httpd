ca557acd34d4ebc52b387a2c21f1f7a280663cd7
Joe Orton <jorton@apache.org>
2012-09-19 15:09:35 +0000

    * server/mpm/prefork/prefork.c (child_main): Don't log errors for an   apr_pollset_add() failure if a graceful-stop has been signalled.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1387633 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/prefork/prefork.c
+++ server/mpm/prefork/prefork.c
@@ -564,8 +564,14 @@ static void child_main(int child_num_arg)
 
         status = apr_pollset_add(pollset, &pfd);
         if (status != APR_SUCCESS) {
-            ap_log_error(APLOG_MARK, APLOG_EMERG, status, ap_server_conf, APLOGNO(00157)
-                         "Couldn't add listener to pollset; check system or user limits");
+            /* If the child processed a SIGWINCH before setting up the
+             * pollset, this error path is expected and harmless,
+             * since the listener fd was already closed; so don't
+             * pollute the logs in that case. */
+            if (!die_now) {
+                ap_log_error(APLOG_MARK, APLOG_EMERG, status, ap_server_conf, APLOGNO(00157)
+                             "Couldn't add listener to pollset; check system or user limits");
+            }
             clean_child_exit(APEXIT_CHILDSICK);
         }
 
