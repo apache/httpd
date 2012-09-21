04954afd043b3db62b439e5834fe9d7ee76c5c87
Jim Jagielski <jim@apache.org>
2012-09-21 18:27:50 +0000

    force these
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1388603 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy.c
+++ modules/proxy/mod_proxy.c
@@ -1147,6 +1147,7 @@ static void * create_proxy_config(apr_pool_t *p, server_rec *s)
     ps->req = 0;
     ps->max_balancers = 0;
     ps->bal_persist = 0;
+    ps->inherit = 0;
     ps->bgrowth = 5;
     ps->bgrowth_set = 0;
     ps->req_set = 0;
@@ -1204,6 +1205,7 @@ static void * merge_proxy_config(apr_pool_t *p, void *basev, void *overridesv)
     ps->bgrowth_set = overrides->bgrowth_set || base->bgrowth_set;
     ps->max_balancers = overrides->max_balancers || base->max_balancers;
     ps->bal_persist = overrides->bal_persist;
+    ps->inherit = overrides->inherit;
     ps->recv_buffer_size = (overrides->recv_buffer_size_set == 0) ? base->recv_buffer_size : overrides->recv_buffer_size;
     ps->recv_buffer_size_set = overrides->recv_buffer_size_set || base->recv_buffer_size_set;
     ps->io_buffer_size = (overrides->io_buffer_size_set == 0) ? base->io_buffer_size : overrides->io_buffer_size;
