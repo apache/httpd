769f9a490b85e4e99707ed73191beab4e10c181b
Jim Jagielski <jim@apache.org>
2012-09-24 20:51:03 +0000

    Might as well show 'em all
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1389566 13f79535-47bb-0310-9956-ffa450edef68

--- modules/metadata/mod_headers.c
+++ modules/metadata/mod_headers.c
@@ -224,7 +224,8 @@ static const char *header_request_loadavg(request_rec *r, char *a)
 {
     ap_loadavg_t t;
     ap_get_loadavg(&t);
-    return apr_psprintf(r->pool, "l=%.2f", t.loadavg);
+    return apr_psprintf(r->pool, "l=%.2f/%.2f/%.2f", t.loadavg,
+                        t.loadavg5, t.loadavg15);
 }
 
 static const char *header_request_idle(request_rec *r, char *a)
