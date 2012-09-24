524bdd994c75d4227b9960d78a4f301942c1c48a
Jim Jagielski <jim@apache.org>
2012-09-24 09:40:29 +0000

    Merge r1376695, r1376700 from trunk:
    Apply the same length limit when logging Status header values
    as used when logging invalid header lines.
    
    Application of a limit on logged header data suggested by Jeff Trawick.
    
    
    Catch up change log with r1376695.
    
    Submitted by: chrisd
    Reviewed/backported by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1389260 13f79535-47bb-0310-9956-ffa450edef68

--- server/util_script.c
+++ server/util_script.c
@@ -591,11 +591,11 @@ AP_DECLARE(int) ap_scan_script_header_err_core_ex(request_rec *r, char *buffer,
             r->status = cgi_status = atoi(l);
             if (!ap_is_HTTP_VALID_RESPONSE(cgi_status))
                 ap_log_rerror(SCRIPT_LOG_MARK, APLOG_ERR|APLOG_TOCLIENT, 0, r,
-                              "Invalid status line from script '%s': %s",
+                              "Invalid status line from script '%s': %.30s",
                               apr_filepath_name_get(r->filename), l);
             else
                 ap_log_rerror(SCRIPT_LOG_MARK, APLOG_TRACE1, 0, r,
-                              "Status line from script '%s': %s",
+                              "Status line from script '%s': %.30s",
                               apr_filepath_name_get(r->filename), l);
             r->status_line = apr_pstrdup(r->pool, l);
         }
