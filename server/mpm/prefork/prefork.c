3d8fc7bb647d882c24c957fe004534109cd2c0cd
Joe Orton <jorton@apache.org>
2012-10-03 10:03:32 +0000

    Merge 1387633,1392850 from trunk:
    * server/mpm/prefork/prefork.c (child_main): Don't log errors for an
      apr_pollset_add() failure if a graceful-stop has been signalled.
    
    follow up to r1387633: use the right exit code depending on the error scenario
    
    Submitted by: jorton, trawick
    Reviewed by: trawick, minfrin, jorton
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1393382 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/prefork/prefork.c
+++ server/mpm/prefork/prefork.c
@@ -564,9 +564,16 @@ static void child_main(int child_num_arg)
 
         status = apr_pollset_add(pollset, &pfd);
         if (status != APR_SUCCESS) {
-            ap_log_error(APLOG_MARK, APLOG_EMERG, status, ap_server_conf, APLOGNO(00157)
-                         "Couldn't add listener to pollset; check system or user limits");
-            clean_child_exit(APEXIT_CHILDSICK);
+            /* If the child processed a SIGWINCH before setting up the
+             * pollset, this error path is expected and harmless,
+             * since the listener fd was already closed; so don't
+             * pollute the logs in that case. */
+            if (!die_now) {
+                ap_log_error(APLOG_MARK, APLOG_EMERG, status, ap_server_conf, APLOGNO(00157)
+                             "Couldn't add listener to pollset; check system or user limits");
+                clean_child_exit(APEXIT_CHILDSICK);
+            }
+            clean_child_exit(0);
         }
 
         lr->accept_func = ap_unixd_accept;
