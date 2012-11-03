766eabd7d9e08235fdb1d290c596e19524990e72
Jeff Trawick <trawick@apache.org>
2012-11-03 19:20:47 +0000

    revert r1401448 and add a comment on why there's a bewildering copy of args passed to apr_socket_accept_filter()
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1405407 13f79535-47bb-0310-9956-ffa450edef68

--- server/listen.c
+++ server/listen.c
@@ -213,7 +213,11 @@ static void ap_apply_accept_filter(apr_pool_t *p, ap_listen_rec *lis,
 
     if (accf) {
 #if APR_HAS_SO_ACCEPTFILTER
-        rv = apr_socket_accept_filter(s, accf, "");
+        /* In APR 1.x, the 2nd and 3rd parameters are char * instead of 
+         * const char *, so make a copy of those args here.
+         */
+        rv = apr_socket_accept_filter(s, apr_pstrdup(p, accf),
+                                      apr_pstrdup(p, ""));
         if (rv != APR_SUCCESS && !APR_STATUS_IS_ENOTIMPL(rv)) {
             ap_log_perror(APLOG_MARK, APLOG_WARNING, rv, p, APLOGNO(00075)
                           "Failed to enable the '%s' Accept Filter",
