bdee3abaa14d336720f20370aee7cdcd09d7b183
Eric Covener <covener@apache.org>
2012-12-04 02:20:17 +0000

    Merge r1415008, r1416150 from https://svn.apache.org/repos/asf/httpd/httpd/trunk/:
    PR54222: catch invalid ServerTokens args
    
    Submitted by: Jackie Zhang  <jackie.qq.zhang gmail.com>
    Reviewed/modified by: covener
    
    
    
    Sort error message for ServerTokens by increasing verbosity. Add missing 'OS'
    variant.
    
    Remove some trailing whitespace.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1416746 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -3222,8 +3222,11 @@ static const char *set_serv_tokens(cmd_parms *cmd, void *dummy,
     else if (!strcasecmp(arg1, "Prod") || !strcasecmp(arg1, "ProductOnly")) {
         ap_server_tokens = SrvTk_PRODUCT_ONLY;
     }
-    else {
+    else if (!strcasecmp(arg1, "Full")) {
         ap_server_tokens = SrvTk_FULL;
+    }
+    else {
+        return "ServerTokens takes 1 argument, 'Prod', 'Major', 'Minor', 'Min', 'OS', or 'Full'";
     }
 
     return NULL;
