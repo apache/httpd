baa9b5b21ccdbf0da8d1487d0ebdd1b7aa02fdc8
Daniel Gruno <humbedooh@apache.org>
2012-12-11 20:08:24 +0000

    mod_lua: Add a lot of core httpd/apr functionality to mod_lua (such as regex matching, expr evaluation, changing/fetching server configuration/info - see docs for a complete list). This also includes a bunch of automatically scraped functions, which may or may not be super useful. Comments appreciated as always, especially on the more hacky bits.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1420377 13f79535-47bb-0310-9956-ffa450edef68

--- modules/lua/lua_apr.h
+++ modules/lua/lua_apr.h
@@ -18,8 +18,20 @@
 #ifndef _LUA_APR_H_
 #define _LUA_APR_H_
 
+#include "scoreboard.h"
+#include "http_main.h"
+#include "ap_mpm.h"
+#include "apr_md5.h"
+#include "apr_sha1.h"
+#include "apr_poll.h"
+#include "apr.h"
+#include "apr_tables.h"
+#include "apr_base64.h"
+
+
 AP_LUA_DECLARE(int) ap_lua_init(lua_State *L, apr_pool_t * p);
 AP_LUA_DECLARE(apr_table_t*) ap_lua_check_apr_table(lua_State *L, int index);
 AP_LUA_DECLARE(void) ap_lua_push_apr_table(lua_State *L, apr_table_t *t);
+AP_LUA_DECLARE(int) ap_lua_load_httpd_functions(lua_State *L);
 
 #endif /* !_LUA_APR_H_ */
