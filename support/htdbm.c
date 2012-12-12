93bcb9c25f8d612b93c8a267bbf8f2bd3a38dd80
Rainer Jung <rjung@apache.org>
2012-12-12 12:34:55 +0000

    htdbm:  - Add vxl to getopt  - Remove "-C" from usage for -x and -l  - Add space between -C and "cost"  - Usage reorder and sync with htpasswd htpasswd:  - Usage reorder and sync with htdbm
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1420644 13f79535-47bb-0310-9956-ffa450edef68

--- support/htdbm.c
+++ support/htdbm.c
@@ -276,31 +276,31 @@ static void htdbm_usage(void)
 {
     fprintf(stderr,
         "htdbm -- program for manipulating DBM password databases.\n\n"
-        "Usage: htdbm   [-cimBdpstvx] [-Ccost] [-TDBTYPE] database username\n"
-        "                -b[cmBdptsv] [-Ccost] [-TDBTYPE] database username password\n"
-        "                -n[imBdpst]  [-Ccost] username\n"
-        "                -nb[mBdpst]  [-Ccost] username password\n"
-        "                -v[imBdps]   [-Ccost] [-TDBTYPE] database username\n"
-        "                -vb[mBdps]   [-Ccost] [-TDBTYPE] database username password\n"
-        "                -x           [-Ccost] [-TDBTYPE] database username\n"
-        "                -l           [-Ccost] [-TDBTYPE] database\n"
+        "Usage: htdbm   [-cimBdpstvx] [-C cost] [-TDBTYPE] database username\n"
+        "                -b[cmBdptsv] [-C cost] [-TDBTYPE] database username password\n"
+        "                -n[imBdpst]  [-C cost] username\n"
+        "                -nb[mBdpst]  [-C cost] username password\n"
+        "                -v[imBdps]   [-C cost] [-TDBTYPE] database username\n"
+        "                -vb[mBdps]   [-C cost] [-TDBTYPE] database username password\n"
+        "                -x                     [-TDBTYPE] database username\n"
+        "                -l                     [-TDBTYPE] database\n"
         "Options:\n"
-        "   -b   Use the password from the command line rather than prompting for it.\n"
         "   -c   Create a new database.\n"
         "   -n   Don't update database; display results on stdout.\n"
-        "   -i   Read password from stdin without verification (for script usage)\n"
+        "   -b   Use the password from the command line rather than prompting for it.\n"
+        "   -i   Read password from stdin without verification (for script usage).\n"
         "   -m   Force MD5 encryption of the password (default).\n"
         "   -B   Force BCRYPT encryption of the password (very secure).\n"
+        "   -C   Set the computing time used for the bcrypt algorithm\n"
+        "        (higher is more secure but slower, default: %d, valid: 4 to 31).\n"
         "   -d   Force CRYPT encryption of the password (8 chars max, insecure).\n"
-        "   -p   Do not encrypt the password (plaintext).\n"
         "   -s   Force SHA encryption of the password (insecure).\n"
-        "   -C   Set the computing time used for the bcrypt algorithm.\n"
-        "        (higher is more secure but slower, default: %d, valid: 4 to 31)\n"
+        "   -p   Do not encrypt the password (plaintext, insecure).\n"
         "   -T   DBM Type (SDBM|GDBM|DB|default).\n"
         "   -l   Display usernames from database on stdout.\n"
-        "   -t   The last param is username comment.\n"
         "   -v   Verify the username/password.\n"
         "   -x   Remove the username record from database.\n"
+        "   -t   The last param is username comment.\n"
         "The SHA algorithm does not use a salt and is less secure than the "
         "MD5 algorithm.\n",
         BCRYPT_DEFAULT_COST);
@@ -336,7 +336,7 @@ int main(int argc, const char * const argv[])
     if (rv != APR_SUCCESS)
         exit(ERR_SYNTAX);
 
-    while ((rv = apr_getopt(state, "cnmspdBbDiC:T:", &opt, &opt_arg)) == APR_SUCCESS) {
+    while ((rv = apr_getopt(state, "cnmspdBbDivxlC:T:", &opt, &opt_arg)) == APR_SUCCESS) {
         switch (opt) {
         case 'c':
             h->create = 1;
