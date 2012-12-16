5005ade63f3c2e997ea63fcf2bdbadad48712c37
Stefan Fritsch <sf@apache.org>
2012-12-16 11:48:19 +0000

    remove unused variable
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1422548 13f79535-47bb-0310-9956-ffa450edef68

--- modules/lua/lua_apr.c
+++ modules/lua/lua_apr.c
@@ -709,7 +709,6 @@ static int lua_ap_module_info(lua_State *L)
     moduleName = lua_tostring(L, 1);
     mod = ap_find_linked_module(moduleName);
     if (mod) {
-        int i = 0;
         const command_rec *cmd;
         lua_newtable(L);
         lua_pushstring(L, "commands");
