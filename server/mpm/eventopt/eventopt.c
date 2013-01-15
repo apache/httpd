08b6d16ffced0fd52ddb099d9289c98deafb23ab
Christophe Jaillet <jailletc36@apache.org>
2013-01-15 21:53:07 +0000

    Log a warning if ThreadStackSize has an inappropriate value. PR : 54311 Submitted by: Tianyin Xu <tixu cs ucsd edu>
    Only event.c has been compiled on my system. The 2 others are just cut and paste.
    I have left the same value for APLOGNO in event and eventopt has for the other logged messages. I don't know if it is the right way to do.
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1433682 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/eventopt/eventopt.c
+++ server/mpm/eventopt/eventopt.c
@@ -2222,7 +2222,13 @@ static void child_main(int child_num_arg)
     apr_threadattr_detach_set(thread_attr, 0);
 
     if (ap_thread_stacksize != 0) {
-        apr_threadattr_stacksize_set(thread_attr, ap_thread_stacksize);
+        rv = apr_threadattr_stacksize_set(thread_attr, ap_thread_stacksize);
+        if (rv != APR_SUCCESS && rv != APR_ENOTIMPL) {
+            ap_log_error(APLOG_MARK, APLOG_WARNING, rv, ap_server_conf, APLOGNO(02436)
+                         "WARNING: ThreadStackSize of %" APR_SIZE_T_FMT " is "
+                         "inappropriate, using default", 
+                         ap_thread_stacksize);
+        }
     }
 
     ts->threads = threads;
