4d81fec84a66b1dedcb7ff3928884ff82ed3d1cc
Ruediger Pluem <rpluem@apache.org>
2013-01-21 16:09:52 +0000

    Merge r1433988 from trunk:
    * A limit of zero means unlimited for LimitRequestBody.
    
    PR: 54435
    Submitted by: Pavel Mateja <pavel netsafe.cz>
    Reviewed by: rpluem
    
    Submitted by: rpluem
    Reviewed by: rpluem, jim, jorton
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1436443 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_http.c
+++ modules/proxy/mod_proxy_http.c
@@ -587,7 +587,7 @@ static int spool_reqbody_cl(apr_pool_t *p,
              * Let it take effect if we decide to store the body in a
              * temporary file on disk.
              */
-            if (bytes_spooled + bytes > limit) {
+            if (limit && (bytes_spooled + bytes > limit)) {
                 ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(01088)
                               "Request body is larger than the configured "
                               "limit of %" APR_OFF_T_FMT, limit);
