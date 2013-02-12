bd117d535917f782a89dfbbe2762a0dbdafa9501
Rainer Jung <rjung@apache.org>
2013-02-12 12:07:32 +0000

    mod_ldap: Fix regression in handling "server unavailable" errors on Windows. Followup to r1375696.
    PR 54140.
    
    Backport of r1408402 from trunk resp. r1409773 from 2.4.x.
    
    Submitted by: covener
    Reviewed by: wrowe, rjung
    Backported by: covener
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1445119 13f79535-47bb-0310-9956-ffa450edef68

--- include/util_ldap.h
+++ include/util_ldap.h
@@ -30,7 +30,7 @@
 #include "apr_time.h"
 #include "apr_ldap.h"
 
-#ifdef LDAP_UNAVAILABLE
+#if defined(LDAP_UNAVAILABLE) || APR_HAS_MICROSOFT_LDAPSDK
 #define AP_LDAP_IS_SERVER_DOWN(s)                ((s) == LDAP_SERVER_DOWN \
                 ||(s) == LDAP_UNAVAILABLE)
 #else
