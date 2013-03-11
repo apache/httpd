dd5210831e2f6680b3bd33913132a8bf64e7b3b8
Jim Jagielski <jim@apache.org>
2013-03-11 16:32:55 +0000

    Merge r1429561 from trunk:
    According top my testing 'socache_mc_id2key' is 6x faster with the use 'ap_bin2hex' instead of
    apr_snprintf(..., "%02X" for each character.
    Output is *not* exactly the same. It was uppercase, now it is lowercase.
    
    According to my understanding, this is not an issue.
    Should it be, a call to ap_str_toupper should be added.
    
    The speedup would be less, but still significant.
    Submitted by: jailletc36
    Reviewed/backported by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1455220 13f79535-47bb-0310-9956-ffa450edef68

--- modules/cache/mod_socache_memcache.c
+++ modules/cache/mod_socache_memcache.c
@@ -182,19 +182,13 @@ static int socache_mc_id2key(ap_socache_instance_t *ctx,
                              char *key, apr_size_t keylen)
 {
     char *cp;
-    unsigned int n;
 
     if (idlen * 2 + ctx->taglen >= keylen)
         return 1;
 
     cp = apr_cpystrn(key, ctx->tag, ctx->taglen);
+    ap_bin2hex(id, idlen, cp);
 
-    for (n = 0; n < idlen; n++) {
-        apr_snprintf(cp, 3, "%02X", (unsigned) id[n]);
-        cp += 2;
-    }
-
-    *cp = '\0';
     return 0;
 }
 
