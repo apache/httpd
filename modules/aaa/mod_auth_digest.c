979cd4725798a5e68879a62d540da48469c77903
Stefan Fritsch <sf@apache.org>
2013-03-31 20:13:48 +0000

    ap_log_error already logs the error string, no need to log it twice
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1463045 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_auth_digest.c
+++ modules/aaa/mod_auth_digest.c
@@ -240,10 +240,8 @@ static apr_status_t initialize_secret(server_rec *s)
 #endif
 
     if (status != APR_SUCCESS) {
-        char buf[120];
         ap_log_error(APLOG_MARK, APLOG_CRIT, status, s, APLOGNO(01758)
-                     "error generating secret: %s",
-                     apr_strerror(status, buf, sizeof(buf)));
+                     "error generating secret");
         return status;
     }
 
