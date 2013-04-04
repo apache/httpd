48c6af9348c0e48ec274befb46cb147d713c10b9
Christophe Jaillet <jailletc36@apache.org>
2013-04-04 21:03:03 +0000

    make the "ProxyHTMLFixups" options case insensitive in mod_proxy_html PR 54420 reported by Tianyin Xu [tixu cs ucsd edu]
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1464762 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/mod_proxy_html.c
+++ modules/filters/mod_proxy_html.c
@@ -1148,11 +1148,11 @@ static const char *set_flags(cmd_parms *cmd, void *CFG, const char *arg)
 {
     proxy_html_conf *cfg = CFG;
     if (arg && *arg) {
-        if (!strcmp(arg, "lowercase"))
+        if (!strcasecmp(arg, "lowercase"))
             cfg->flags |= NORM_LC;
-        else if (!strcmp(arg, "dospath"))
+        else if (!strcasecmp(arg, "dospath"))
             cfg->flags |= NORM_MSSLASH;
-        else if (!strcmp(arg, "reset"))
+        else if (!strcasecmp(arg, "reset"))
             cfg->flags |= NORM_RESET;
     }
     return NULL;
