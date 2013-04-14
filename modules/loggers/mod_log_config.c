d5c34b1b2309a049ee1f8d399e3c206436fcd366
Rainer Jung <rjung@apache.org>
2013-04-14 11:16:40 +0000

    Fix crash in mod_log_config when logging request end time for a failed request (LogFormat contains %{end}t).
    The request_config for mod_log_config might
    not be initialized.
    
    PR 54828.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1467765 13f79535-47bb-0310-9956-ffa450edef68

--- modules/loggers/mod_log_config.c
+++ modules/loggers/mod_log_config.c
@@ -597,6 +597,10 @@ static apr_time_t get_request_end_time(request_rec *r)
 {
     log_request_state *state = (log_request_state *)ap_get_module_config(r->request_config,
                                                                          &log_config_module);
+    if (!state) {
+        state = apr_pcalloc(r->pool, sizeof(log_request_state));
+        ap_set_module_config(r->request_config, &log_config_module, state);
+    }
     if (state->request_end_time == 0) {
         state->request_end_time = apr_time_now();
     }
