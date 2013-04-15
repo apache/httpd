6fe3e0fde57d3395966291f58d4e7a23493faa0e
Jim Jagielski <jim@apache.org>
2013-04-15 12:39:55 +0000

    Merge r1463750, r1463754, r1464675 from trunk:
    Use apr_file_printf(... "%pm"...) instead of explicit call to apr_strerror
    
    Use apr_psprintf(... "%pm"...) instead of explicit call to apr_strerror
    
    Use apr_file_printf(... "%pm"...) instead of explicit call to apr_strerror
    + add some spaces to improve formatting
    Submitted by: jailletc36
    Reviewed/backported by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1467978 13f79535-47bb-0310-9956-ffa450edef68

--- support/htdigest.c
+++ support/htdigest.c
@@ -202,8 +202,8 @@ int main(int argc, const char * const argv[])
 #if APR_CHARSET_EBCDIC
     rv = apr_xlate_open(&to_ascii, "ISO-8859-1", APR_DEFAULT_CHARSET, cntxt);
     if (rv) {
-        apr_file_printf(errfile, "apr_xlate_open(): %s (%d)\n",
-                apr_strerror(rv, line, sizeof(line)), rv);
+        apr_file_printf(errfile, "apr_xlate_open(): %pm (%d)\n",
+                &rv, rv);
         exit(1);
     }
 #endif
@@ -215,11 +215,8 @@ int main(int argc, const char * const argv[])
         rv = apr_file_open(&f, argv[2], APR_WRITE | APR_CREATE,
                            APR_OS_DEFAULT, cntxt);
         if (rv != APR_SUCCESS) {
-            char errmsg[120];
-
-            apr_file_printf(errfile, "Could not open passwd file %s for writing: %s\n",
-                    argv[2],
-                    apr_strerror(rv, errmsg, sizeof errmsg));
+            apr_file_printf(errfile, "Could not open passwd file %s for writing: %pm\n",
+                    argv[2], &rv);
             exit(1);
         }
         apr_cpystrn(user, argv[4], sizeof(user));
