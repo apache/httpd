5d652f66ec804a74aba81be0483dc386bcb5afe9
Daniel Gruno <humbedooh@apache.org>
2013-04-19 14:28:25 +0000

    exists_config_define should return a boolean, not a number, or it will always evaluate to true.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1469844 13f79535-47bb-0310-9956-ffa450edef68

--- modules/lua/lua_request.c
+++ modules/lua/lua_request.c
@@ -1499,7 +1499,7 @@ static int lua_ap_exists_config_define(lua_State *L)
     luaL_checktype(L, 1, LUA_TSTRING);
     name = lua_tostring(L, 1);
     returnValue = ap_exists_config_define(name);
-    lua_pushinteger(L, returnValue);
+    lua_pushboolean(L, returnValue);
     return 1;
 }
 
