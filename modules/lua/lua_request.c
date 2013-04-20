8b509f121255af8527c8032b4de2da0c787a072f
Guenter Knauf <fuankg@apache.org>
2013-04-20 12:32:08 +0000

    Fixed r.started value to return seconds.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1470157 13f79535-47bb-0310-9956-ffa450edef68

--- modules/lua/lua_request.c
+++ modules/lua/lua_request.c
@@ -644,7 +644,7 @@ static const char* lua_ap_allowoverrides(request_rec* r)
 
 static int lua_ap_started(request_rec* r) 
 {
-    return ap_scoreboard_image->global->restart_time;
+    return (int)(ap_scoreboard_image->global->restart_time / 1000000);
 }
 
 static const char* lua_ap_basic_auth_pw(request_rec* r) 
