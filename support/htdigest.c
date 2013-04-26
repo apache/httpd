8f42c3c41a009556fce1918b9a6bac88f74b229f
Rainer Jung <rjung@apache.org>
2013-04-26 07:42:01 +0000

    htdigest: Fix buffer overflow when reading digest password file with very long lines.
    PR 54893.
    
    Backport of r1475878 from trunk.
    
    Proposed/Backported by: rjung
    Reviewed by: humbedooh, covener
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1476089 13f79535-47bb-0310-9956-ffa450edef68

--- support/htdigest.c
+++ support/htdigest.c
@@ -96,12 +96,15 @@ static int get_line(char *s, int n, apr_file_t *f)
     char ch;
     apr_status_t rv = APR_EINVAL;
 
-    while (i < (n - 1) &&
+    /* we need 2 remaining bytes in buffer */
+    while (i < (n - 2) &&
            ((rv = apr_file_getc(&ch, f)) == APR_SUCCESS) && (ch != '\n')) {
         s[i++] = ch;
     }
+    /* First remaining byte potentially used here */
     if (ch == '\n')
         s[i++] = ch;
+    /* Second remaining byte used here */
     s[i] = '\0';
 
     if (rv != APR_SUCCESS)
