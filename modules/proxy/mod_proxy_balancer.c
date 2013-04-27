6984d837f49b32a5d3e19e950298df07fa589976
Graham Leggett <minfrin@apache.org>
2013-04-27 22:32:42 +0000

    mod_proxy_balancer: Add failontimeout parameter. Timeout will put worker in error state if an IO timeout is detected.
    trunk patch: http://svn.apache.org/viewvc?view=revision&revision=1465839
    2.4.x patch: http://people.apache.org/~druggeri/patches/httpd-2.4.x-failontimeout.patch
    
    Submitted by: druggeri
    Reviewed by: jim, minfrin
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1476689 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_balancer.c
+++ modules/proxy/mod_proxy_balancer.c
@@ -649,6 +649,17 @@ static int proxy_balancer_post_request(proxy_worker *worker,
         }
     }
 
+    if (balancer->failontimeout
+        && (apr_table_get(r->notes, "proxy_timedout")) != NULL) {
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(02460)
+                      "%s: Forcing worker (%s) into error state "
+                      "due to timeout and 'failonstatus' parameter being set",
+                       balancer->s->name, worker->s->name);
+        worker->s->status |= PROXY_WORKER_IN_ERROR;
+        worker->s->error_time = apr_time_now();
+
+    }
+
     if ((rv = PROXY_THREAD_UNLOCK(balancer)) != APR_SUCCESS) {
         ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r, APLOGNO(01175)
                       "%s: Unlock failed for post_request", balancer->s->name);
