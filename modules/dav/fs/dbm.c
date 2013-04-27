5345db8652fa82e18380dfb44291927850b8767b
Graham Leggett <minfrin@apache.org>
2013-04-27 17:38:45 +0000

    mod_dav: When a PROPPATCH attempts to remove a non-existent dead property on a resource for which there is no dead property in the same namespace httpd segfaults. PR 52559 Submitted by Diego Santa Cruz <diego.santaCruz spinetix.com> Tested by William Lewis <wiml omnigroup com>
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1476642 13f79535-47bb-0310-9956-ffa450edef68

--- modules/dav/fs/dbm.c
+++ modules/dav/fs/dbm.c
@@ -731,6 +731,10 @@ static dav_error * dav_propdb_get_rollback(dav_db *db,
 static dav_error * dav_propdb_apply_rollback(dav_db *db,
                                              dav_deadprop_rollback *rollback)
 {
+    if (!rollback) {
+        return NULL; /* no rollback, nothing to do */
+    }
+
     if (rollback->value.dptr == NULL) {
         /* don't fail if the thing isn't really there. */
         (void) dav_dbm_delete(db, rollback->key);
