b953a0a42499d8523d4ec97bd80e4f92da472440
Graham Leggett <minfrin@apache.org>
2013-04-27 15:40:18 +0000

    mod_dav: Sending an If or If-Match header with an invalid ETag doesn't result in a 412 Precondition Failed for a COPY operation.
    PR: 54610
    Submitted by: Timothy Wood <tjw omnigroup.com>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1476604 13f79535-47bb-0310-9956-ffa450edef68

--- modules/dav/main/mod_dav.c
+++ modules/dav/main/mod_dav.c
@@ -2748,10 +2748,10 @@ static int dav_method_copymove(request_rec *r, int is_move)
     }
 
     /*
-     * Check If-Headers and existing locks for each resource in the source
-     * if we are performing a MOVE. We will return a 424 response with a
-     * DAV:multistatus body. The multistatus responses will contain the
-     * information about any resource that fails the validation.
+     * Check If-Headers and existing locks for each resource in the source.
+     * We will return a 424 response with a DAV:multistatus body.
+     * The multistatus responses will contain the information about any
+     * resource that fails the validation.
      *
      * We check the parent resource, too, since this is a MOVE. Moving the
      * resource effectively removes it from the parent collection, so we
@@ -2760,17 +2760,17 @@ static int dav_method_copymove(request_rec *r, int is_move)
      * If a problem occurs with the Request-URI itself, then a plain error
      * (rather than a multistatus) will be returned.
      */
-    if (is_move
-        && (err = dav_validate_request(r, resource, depth, NULL,
-                                       &multi_response,
-                                       DAV_VALIDATE_PARENT
-                                       | DAV_VALIDATE_USE_424,
-                                       NULL)) != NULL) {
+    if ((err = dav_validate_request(r, resource, depth, NULL,
+                                    &multi_response,
+                                    DAV_VALIDATE_PARENT
+                                    | DAV_VALIDATE_USE_424,
+                                    NULL)) != NULL) {
         err = dav_push_error(r->pool, err->status, 0,
                              apr_psprintf(r->pool,
-                                          "Could not MOVE %s due to a failed "
+                                          "Could not %s %s due to a failed "
                                           "precondition on the source "
                                           "(e.g. locks).",
+                                          is_move ? "MOVE" : "COPY",
                                           ap_escape_html(r->pool, r->uri)),
                              err);
         return dav_handle_err(r, err, multi_response);
