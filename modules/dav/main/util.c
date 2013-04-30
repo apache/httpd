42f0d9dd77eddab791c636e990f2ec45fe874f25
Graham Leggett <minfrin@apache.org>
2013-04-30 10:15:23 +0000

    mod_dav: Make sure the URI length is calculated correctly.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1477530 13f79535-47bb-0310-9956-ffa450edef68

--- modules/dav/main/util.c
+++ modules/dav/main/util.c
@@ -659,15 +659,17 @@ static dav_error * dav_process_if_header(request_rec *r, dav_if_header **p_ih)
 
             /* clean up the URI a bit */
             ap_getparents(parsed_uri.path);
-            uri_len = strlen(parsed_uri.path);
-            if (uri_len > 1 && parsed_uri.path[uri_len - 1] == '/')
-                parsed_uri.path[--uri_len] = '\0';
 
             /* the resources we will compare to have unencoded paths */
             if (ap_unescape_url(parsed_uri.path) != OK) {
                 return dav_new_error(r->pool, HTTP_BAD_REQUEST,
                         DAV_ERR_IF_TAGGED, rv,
                         "Invalid percent encoded URI in tagged If-header.");
+            }
+
+            uri_len = strlen(parsed_uri.path);
+            if (uri_len > 1 && parsed_uri.path[uri_len - 1] == '/') {
+                parsed_uri.path[--uri_len] = '\0';
             }
 
             uri = parsed_uri.path;
