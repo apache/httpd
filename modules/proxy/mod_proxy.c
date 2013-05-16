a1c0d50b72920f3a886dae779e45b603cd9e7d78
Jim Jagielski <jim@apache.org>
2013-05-16 04:28:14 +0000

    add in child_init which is needed
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1483190 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy.c
+++ modules/proxy/mod_proxy.c
@@ -2514,6 +2514,15 @@ static void child_init(apr_pool_t *p, server_rec *s)
 {
     proxy_worker *reverse = NULL;
 
+    apr_status_t rv = apr_global_mutex_child_init(&proxy_mutex,
+                                      apr_global_mutex_lockfile(proxy_mutex),
+                                      p);
+    if (rv != APR_SUCCESS) {
+        ap_log_error(APLOG_MARK, APLOG_CRIT, rv, s, APLOGNO()
+                     "could not init proxy_mutex in child");
+        exit(1); /* Ugly, but what else? */
+    }
+
     /* TODO */
     while (s) {
         void *sconf = s->module_config;
