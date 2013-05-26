d279048503e8634e188d6735fedcda9e9cbd2010
Graham Leggett <minfrin@apache.org>
2013-05-26 19:58:20 +0000

    mod_dav: Do not fail PROPPATCH when prop namespace is not known. PR 52559
    trunk patch: http://svn.apache.org/r1476644
    Submitted by: Diego Santa Cruz <diego.santaCruz spinetix.com>
    Reviewed by: minfrin, jim, jorton
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1486459 13f79535-47bb-0310-9956-ffa450edef68

--- modules/dav/fs/dbm.c
+++ modules/dav/fs/dbm.c
@@ -191,7 +191,15 @@ void dav_dbm_close(dav_db *db)
 
 dav_error * dav_dbm_fetch(dav_db *db, apr_datum_t key, apr_datum_t *pvalue)
 {
-    apr_status_t status = apr_dbm_fetch(db->file, key, pvalue);
+    apr_status_t status;
+
+    if (!key.dptr) {
+        /* no key could be created (namespace not known) => no value */
+        memset(pvalue, 0, sizeof(*pvalue));
+        status = APR_SUCCESS;
+    } else {
+        status = apr_dbm_fetch(db->file, key, pvalue);
+    }
 
     return dav_fs_dbm_error(db, NULL, status);
 }
