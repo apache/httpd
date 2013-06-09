fe32420e2cbc43393560d84db2c8f99685a56539
Stefan Fritsch <sf@apache.org>
2013-06-09 08:19:29 +0000

    Allow use of strtoul()
    It conforms to C89 and we don't support older compilers than that.
    
    PR: 55077
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1491155 13f79535-47bb-0310-9956-ffa450edef68

--- include/httpd.h
+++ include/httpd.h
@@ -2162,12 +2162,6 @@ extern int raise_sigstop_flags;
  */
 AP_DECLARE(const char *) ap_psignature(const char *prefix, request_rec *r);
 
-/** strtoul does not exist on sunos4. */
-#ifdef strtoul
-#undef strtoul
-#endif
-#define strtoul strtoul_is_not_a_portable_function_use_strtol_instead
-
   /* The C library has functions that allow const to be silently dropped ...
      these macros detect the drop in maintainer mode, but use the native
      methods for normal builds
