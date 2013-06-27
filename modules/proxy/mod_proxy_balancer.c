52b42cf0871042d214c972a73456c431c12a1483
Rainer Jung <rjung@apache.org>
2013-06-27 22:39:21 +0000

    Fix indentation. No functional change.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1497597 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_balancer.c
+++ modules/proxy/mod_proxy_balancer.c
@@ -631,10 +631,10 @@ static int proxy_balancer_post_request(proxy_worker *worker,
 
     if (balancer->failontimeout
         && (apr_table_get(r->notes, "proxy_timedout")) != NULL) {
-          ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
-                        "%s: Forcing worker (%s) into error state "
-                        "due to timeout and 'failonstatus' parameter being set",
-                        balancer->name, worker->name);
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+                      "%s: Forcing worker (%s) into error state "
+                      "due to timeout and 'failonstatus' parameter being set",
+                       balancer->name, worker->name);
         worker->s->status |= PROXY_WORKER_IN_ERROR;
         worker->s->error_time = apr_time_now();
 
