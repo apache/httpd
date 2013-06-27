97041d5eaa281df50b2abf309c6d16c177eebd53
William A. Rowe Jr <wrowe@apache.org>
2013-06-27 16:22:31 +0000

    mod_proxy_balancer: Add failontimeout parameter. Timeout will put worker in error state if an IO timeout is detected.
    Backports: r1465839
    Submitted by: druggeri
    Reviewed by: druggeri, wrowe, rjung
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1497423 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_balancer.c
+++ modules/proxy/mod_proxy_balancer.c
@@ -629,6 +629,17 @@ static int proxy_balancer_post_request(proxy_worker *worker,
         }
     }
 
+    if (balancer->failontimeout
+        && (apr_table_get(r->notes, "proxy_timedout")) != NULL) {
+          ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+                        "%s: Forcing worker (%s) into error state "
+                        "due to timeout and 'failonstatus' parameter being set",
+                        balancer->name, worker->name);
+        worker->s->status |= PROXY_WORKER_IN_ERROR;
+        worker->s->error_time = apr_time_now();
+
+    }
+
     if ((rv = PROXY_THREAD_UNLOCK(balancer)) != APR_SUCCESS) {
         ap_log_error(APLOG_MARK, APLOG_ERR, rv, r->server,
             "proxy: BALANCER: (%s). Unlock failed for post_request",
--- modules/proxy/mod_proxy_http.c
+++ modules/proxy/mod_proxy_http.c
@@ -1410,6 +1410,7 @@ apr_status_t ap_proxy_http_process_response(apr_pool_t * p, request_rec *r,
                           "proxy: error reading status line from remote "
                           "server %s:%d", backend->hostname, backend->port);
             if (APR_STATUS_IS_TIMEUP(rc)) {
+                apr_table_set(r->notes, "proxy_timedout", "1");
                 ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
                               "proxy: read timeout");
             }
