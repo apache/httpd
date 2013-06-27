apache/httpd

commit 116211e090ed0a7d1ca4ecd514b3f5c28541f939

mod_dav: Sending a MERGE request against a URI handled by mod_dav_svn with the source href (sent as part of the request body as XML) pointing to a URI that is not configured for DAV will trigger a segfault.
Backports: r1485668
Reviewed by: minfrin, trawick, wrowe
Submitted by: Ben Reser <ben reser.org>



git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1497101 13f79535-47bb-0310-9956-ffa450edef68

modules/dav/main/mod_dav.c
@@ -719,6 +719,12 @@ static dav_error *dav_get_resource(request_rec *r, int label_allowed,
 
     conf = ap_get_module_config(r->per_dir_config, &dav_module);
     /* assert: conf->provider != NULL */
+    if (conf->provider == NULL) {
+        return dav_new_error(r->pool, HTTP_METHOD_NOT_ALLOWED, 0, 0,
+                             apr_psprintf(r->pool,
+				          "DAV not enabled for %s",
+					  ap_escape_html(r->pool, r->uri)));
+    }
 
     /* resolve the resource */
     err = (*conf->provider->repos->get_resource)(r, conf->dir,
@@ -2648,11 +2654,6 @@ static int dav_method_copymove(request_rec *r, int is_move)
         /* ### how best to report this... */
         return dav_error_response(r, lookup.rnew->status,
                                   "Destination URI had an error.");
-    }
-
-    if (dav_get_provider(lookup.rnew) == NULL) {
-        return dav_error_response(r, HTTP_METHOD_NOT_ALLOWED,
-                                  "DAV not enabled for Destination URI.");
     }
 
     /* Resolve destination resource */

STATUS
@@ -114,13 +114,6 @@ RELEASE SHOWSTOPPERS:
 PATCHES ACCEPTED TO BACKPORT FROM TRUNK:
   [ start all new proposals below, under PATCHES PROPOSED. ]
   
-  * mod_dav: Sending a MERGE request against a URI handled by mod_dav_svn with
-    the source href (sent as part of the request body as XML) pointing to a
-    URI that is not configured for DAV will trigger a segfault.
-    trunk patch: http://svn.apache.org/r1485668
-    2.2.x patch: trunk works, modulo CHANGES
-    +1: minfrin, trawick, wrowe
-
 
 PATCHES PROPOSED TO BACKPORT FROM TRUNK:
   [ New proposals should be added at the end of the list ]

commit aad0b4ce0ce51056a79b8cac2f3ff77ce8454b32

Fix build breakage introduced by the backport in r1497101.
git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1497212 13f79535-47bb-0310-9956-ffa450edef68

modules/dav/main/mod_dav.c
@@ -722,7 +722,7 @@ static dav_error *dav_get_resource(request_rec *r, int label_allowed,
     conf = ap_get_module_config(r->per_dir_config, &dav_module);
     /* assert: conf->provider != NULL */
     if (conf->provider == NULL) {
-        return dav_new_error(r->pool, HTTP_METHOD_NOT_ALLOWED, 0, 0,
+        return dav_new_error(r->pool, HTTP_METHOD_NOT_ALLOWED, 0,
                              apr_psprintf(r->pool,
 				          "DAV not enabled for %s",
 					  ap_escape_html(r->pool, r->uri)));