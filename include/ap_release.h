94c6364b196e427df8c69f76e8ec1d8e4ef70f24
Guenter Knauf <fuankg@apache.org>
2013-07-06 00:48:47 +0000

    Avoid including apr_general.h. The MSVC Resource Compiler truncates macro names at char 31. Since apr_general.h includes apr.h which in turn includes system headers we get warnings or errors with the Resource Compiler with newer system headers (VC10 and later) which have macro names defined with more than 31 chars. (backport from trunk r1499742 / httpd-2.4.x r1499855) Reviewed by: gsmith, sf
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1500183 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_release.h
+++ include/ap_release.h
@@ -22,8 +22,6 @@
 #ifndef AP_RELEASE_H
 #define AP_RELEASE_H
 
-#include "apr_general.h" /* stringify */
-
 #define AP_SERVER_COPYRIGHT \
     "Copyright 2013 The Apache Software Foundation."
 
@@ -54,6 +52,14 @@
 #define AP_SERVER_ADD_STRING          "-dev"
 #else
 #define AP_SERVER_ADD_STRING          ""
+#endif
+
+/* APR_STRINGIFY is defined here, and also in apr_general.h, so wrap it */
+#ifndef APR_STRINGIFY
+/** Properly quote a value as a string in the C preprocessor */
+#define APR_STRINGIFY(n) APR_STRINGIFY_HELPER(n)
+/** Helper macro for APR_STRINGIFY */
+#define APR_STRINGIFY_HELPER(n) #n
 #endif
 
 /* keep old macros as well */
