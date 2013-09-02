d7aa057282f5c1b6f5a66dfb3d8dcf603b3c1756
Eric Covener <covener@apache.org>
2013-09-02 11:43:53 +0000

    Return a 500 error instead of DECLINED when LuaHook* script does not return a numeric value.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1519398 13f79535-47bb-0310-9956-ffa450edef68

--- modules/lua/mod_lua.c
+++ modules/lua/mod_lua.c
@@ -664,6 +664,11 @@ static int lua_request_rec_hook_harness(request_rec *r, const char *name, int ap
                 ap_log_rerror(APLOG_MARK, APLOG_TRACE4, 0, r, "Lua hook %s:%s for phase %s returned %d", 
                               hook_spec->file_name, hook_spec->function_name, name, rc);
             }
+            else { 
+                ap_log_rerror(APLOG_MARK, APLOG_CRIT, 0, r, "Lua hook %s:%s for phase %s did not return a numeric value", 
+                              hook_spec->file_name, hook_spec->function_name, name);
+                return HTTP_INTERNAL_SERVER_ERROR;
+            }
             if (rc != DECLINED) {
                 ap_lua_release_state(L, spec, r);
                 return rc;
