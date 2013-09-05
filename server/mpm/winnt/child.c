dedba685f4789918c29833391efcdf316217d629
Jeff Trawick <trawick@apache.org>
2013-09-05 16:33:11 +0000

    Mistakenly try to use a line comment to the end of your Listen directive on Windows => crash-o-matic
    Or,
    
    WinNT MPM: Don't crash during child process initialization if the
    Listen protocol is unrecognized. 
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1520368 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/winnt/child.c
+++ server/mpm/winnt/child.c
@@ -315,7 +315,15 @@ static unsigned int __stdcall winnt_accept(void *lr_)
     core_sconf = ap_get_core_module_config(ap_server_conf->module_config);
     accf_name = apr_table_get(core_sconf->accf_map, lr->protocol);
 
-    if (strcmp(accf_name, "data") == 0)
+    if (!accf_name) {
+        accf = 0;
+        accf_name = "none";
+        ap_log_error(APLOG_MARK, APLOG_WARNING, 0, ap_server_conf,
+                     APLOGNO(02531) "winnt_accept: Listen protocol '%s' has "
+                     "no known accept filter. Using 'none' instead",
+                     lr->protocol);
+    }
+    else if (strcmp(accf_name, "data") == 0)
         accf = 2;
     else if (strcmp(accf_name, "connect") == 0)
         accf = 1;
