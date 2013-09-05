a28492c59bff869c85ee97d0c534a8b6493780cd
Daniel Gruno <humbedooh@apache.org>
2013-09-05 08:59:10 +0000

    mod_lua: init cookie as NULL.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1520256 13f79535-47bb-0310-9956-ffa450edef68

--- modules/lua/lua_request.c
+++ modules/lua/lua_request.c
@@ -1895,6 +1895,7 @@ static int lua_get_cookie(lua_State *L)
     const char *key, *cookie;
     request_rec *r = ap_lua_check_request_rec(L, 1);
     key = luaL_checkstring(L, 2);
+    cookie = NULL;
     ap_cookie_read(r, key, &cookie, 0);
     if (cookie != NULL) {
         lua_pushstring(L, cookie);
