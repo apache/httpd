d3a3acccb61899c13a21dc7d3df0f65923b1311a
Graham Leggett <minfrin@apache.org>
2013-09-14 15:15:38 +0000

    authz_groupfile/authz_user: don't log at ERROR for AUTHZ_DENIED, since authz_core is collecting providers results. Subsequently reported in PR55523?
    trunk patch: http://svn.apache.org/r1517366
    
    Submitted by: covener
    Reviewed by: jim, humbedooh
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1523267 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_authz_groupfile.c
+++ modules/aaa/mod_authz_groupfile.c
@@ -249,7 +249,7 @@ static authz_status filegroup_check_authorization(request_rec *r,
         return AUTHZ_DENIED;
     }
 
-    ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(01671)
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r, APLOGNO(01671)
                   "Authorization of user %s to access %s failed, reason: "
                   "user is not part of the 'require'ed file group.",
                   r->user, r->uri);
