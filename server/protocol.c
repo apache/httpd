cd894c018c718f6a872b436f6a521790adef4b82
Jim Jagielski <jim@apache.org>
2013-09-17 18:37:18 +0000

    RFC2616 issue
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1524161 13f79535-47bb-0310-9956-ffa450edef68

--- server/protocol.c
+++ server/protocol.c
@@ -1091,6 +1091,8 @@ request_rec *ap_read_request(conn_rec *conn)
     }
 
     if (!r->assbackwards) {
+        const char *tenc, *clen;
+
         ap_get_mime_headers_core(r, tmp_bb);
         if (r->status != HTTP_OK) {
             ap_log_rerror(APLOG_MARK, APLOG_INFO, 0, r, APLOGNO(00567)
@@ -1102,13 +1104,37 @@ request_rec *ap_read_request(conn_rec *conn)
             goto traceout;
         }
 
-        if (apr_table_get(r->headers_in, "Transfer-Encoding")
-            && apr_table_get(r->headers_in, "Content-Length")) {
+        if ((tenc = apr_table_get(r->headers_in, "Transfer-Encoding"))) {
             /* 2616 section 4.4, point 3: "if both Transfer-Encoding
              * and Content-Length are received, the latter MUST be
-             * ignored"; so unset it here to prevent any confusion
-             * later. */
-            apr_table_unset(r->headers_in, "Content-Length");
+             * ignored"; unless the former is "identity". So unset
+             * the one concerned here to prevent any confusion later.
+             */
+            if ((clen = apr_table_get(r->headers_in, "Content-Length"))) {
+                if (strcasecmp(tenc, "chunked") == 0) {
+                    ap_log_rerror(APLOG_MARK, APLOG_INFO, 0, r, APLOGNO(2537)
+                                  "client sent both Transfer-Encoding (chunked)"
+                                  " and Content-Length; using TE");
+                    apr_table_unset(r->headers_in, "Content-Length");
+                }
+                else {
+                    ap_log_rerror(APLOG_MARK, APLOG_INFO, 0, r, APLOGNO(2538)
+                                  "client sent identity or unknown Transfer-Encoding (%s);"
+                                  " using Content-Length", tenc);
+                    apr_table_unset(r->headers_in, "Transfer-Encoding");
+                }
+            }
+            else if (strcasecmp(tenc, "chunked") != 0) {
+                ap_log_rerror(APLOG_MARK, APLOG_INFO, 0, r, APLOGNO(2539)
+                              "client sent unknown Transfer-Encoding;"
+                              " not implemented: %s", tenc);
+                r->status = HTTP_NOT_IMPLEMENTED;
+                ap_send_error_response(r, 0);
+                ap_update_child_status(conn->sbh, SERVER_BUSY_LOG, r);
+                ap_run_log_transaction(r);
+                apr_brigade_destroy(tmp_bb);
+                goto traceout;
+            }
         }
     }
     else {
