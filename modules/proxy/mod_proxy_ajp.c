619866c1982df4c04d41e269ecb62e5e137d893c
Jeff Trawick <trawick@apache.org>
2013-09-17 12:51:08 +0000

    fill in missing message numbers in APLOGNO() invocations
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1524018 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_ajp.c
+++ modules/proxy/mod_proxy_ajp.c
@@ -762,7 +762,7 @@ static int proxy_ajp_handler(request_rec *r, proxy_worker *worker,
             if (worker->s->ping_timeout < 0) {
                 if (!ap_proxy_is_socket_connected(backend->sock)) {
                     backend->close = 1;
-                    ap_log_rerror(APLOG_MARK, APLOG_ERR, status, r, APLOGNO()
+                    ap_log_rerror(APLOG_MARK, APLOG_ERR, status, r, APLOGNO(02534)
                                   "socket check failed to %pI (%s)",
                                   worker->cp->addr, worker->s->hostname);
                     status = HTTP_SERVICE_UNAVAILABLE;
