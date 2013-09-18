e9ed2c0311a7032ba29071ce1c2a063323f5e39b
Daniel Gruno <humbedooh@apache.org>
2013-09-18 07:48:22 +0000

    mod_lua: ap_ntoh64 endianess test
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1524321 13f79535-47bb-0310-9956-ffa450edef68

--- modules/lua/lua_request.c
+++ modules/lua/lua_request.c
@@ -1933,7 +1933,10 @@ static apr_uint64_t ap_ntoh64(const apr_uint64_t *input)
 {
     apr_uint64_t rval;
     unsigned char *data = (unsigned char *)&rval;
-
+    if (APR_IS_BIGENDIAN) {
+        return *input;
+    }
+    
     data[0] = *input >> 56;
     data[1] = *input >> 48;
     data[2] = *input >> 40;
