430144f14050244d572f82d739809d80376aa5eb
Kaspar Brand <kbrand@apache.org>
2013-09-29 09:36:31 +0000

    Follow-up fixes for r1526168:
    - drop SSL_TMP_KEY_* constants from ssl_private.h, too
    
    - make sure we also disable aNULL, eNULL and EXP ciphers
      for per-directory SSLCipherSuite directives
    
    - apply the same treatment to SSLProxyCipherSuite
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1527291 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_init.c
+++ modules/ssl/ssl_engine_init.c
@@ -668,11 +668,12 @@ static void ssl_init_ctx_cipher_suite(server_rec *s,
 
     /*
      *  Configure SSL Cipher Suite. Always disable NULL and export ciphers,
-     *  no matter what SSLCipherSuite directive is appearing in the config.
+     *  see also ssl_engine_config.c:ssl_cmd_SSLCipherSuite().
+     *  OpenSSL's SSL_DEFAULT_CIPHER_LIST already includes !aNULL:!eNULL,
+     *  so only prepend !EXP in this case.
      */
-    suite = apr_pstrcat(ptemp, "!aNULL:!eNULL:!EXP:", mctx->auth.cipher_suite ?
-                        mctx->auth.cipher_suite : SSL_DEFAULT_CIPHER_LIST,
-                        NULL);
+    suite = mctx->auth.cipher_suite ? mctx->auth.cipher_suite :
+            apr_pstrcat(ptemp, "!EXP:", SSL_DEFAULT_CIPHER_LIST, NULL);
 
     ap_log_error(APLOG_MARK, APLOG_TRACE1, 0, s,
                  "Configuring permitted SSL ciphers [%s]",
