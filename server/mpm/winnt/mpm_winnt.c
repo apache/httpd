c95ea675e62b14483f683a7b7a7858e950333842
Jim Jagielski <jim@apache.org>
2013-10-10 14:34:19 +0000

    Merge r1526666, r1527220 from trunk:
    WinNT MPM: Exit the child if the parent process crashes or is terminated.
    
    Submitted by: Oracle, via trawick
    
    The original modification was made some years ago for Oracle HTTP Server
    by an Oracle employee.  trawick made additional changes for style and
    for trunk/2.4.x changes.
    
    
    Follow up to r1526666:
    
    Use SYNCHRONIZE instead of PROCESS_ALL_ACCESS because
    
    a. it is sufficient
    b. it avoids an issue where PROCESS_ALL_ACCESS is larger on
       newer SDKs, resulting in a run-time error when running on
       older Windows
    
    Close the handle.
    
    Submitted by: Ivan Zhakov <ivan visualsvn.com>
    
    Submitted by: trawick
    Reviewed/backported by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1531000 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/winnt/mpm_winnt.c
+++ server/mpm/winnt/mpm_winnt.c
@@ -1708,7 +1708,7 @@ static int winnt_run(apr_pool_t *_pconf, apr_pool_t *plog, server_rec *s )
         ap_log_error(APLOG_MARK, APLOG_DEBUG, APR_SUCCESS, ap_server_conf, APLOGNO(00453)
                      "Child process is running");
 
-        child_main(pconf);
+        child_main(pconf, parent_pid);
 
         ap_log_error(APLOG_MARK, APLOG_DEBUG, APR_SUCCESS, ap_server_conf, APLOGNO(00454)
                      "Child process is exiting");
