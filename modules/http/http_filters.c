bffa83f941aaa1e2b1a3ef7e63482966cc3068da
Jim Jagielski <jim@apache.org>
2013-11-12 22:48:33 +0000

    80 rule
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1541290 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http/http_filters.c
+++ modules/http/http_filters.c
@@ -263,7 +263,8 @@ apr_status_t ap_http_filter(ap_filter_t *f, apr_bucket_brigade *b,
 
                 ctx->remaining = 0;
                 ap_log_rerror(
-                        APLOG_MARK, APLOG_INFO, 0, f->r, APLOGNO(01587) "Invalid Content-Length");
+                        APLOG_MARK, APLOG_INFO, 0, f->r, APLOGNO(01587)
+                        "Invalid Content-Length");
 
                 return APR_ENOSPC;
             }
@@ -273,7 +274,9 @@ apr_status_t ap_http_filter(ap_filter_t *f, apr_bucket_brigade *b,
              */
             if (ctx->limit && ctx->limit < ctx->remaining) {
                 ap_log_rerror(
-                        APLOG_MARK, APLOG_INFO, 0, f->r, APLOGNO(01588) "Requested content-length of %" APR_OFF_T_FMT " is larger than the configured limit"
+                        APLOG_MARK, APLOG_INFO, 0, f->r, APLOGNO(01588)
+                        "Requested content-length of %" APR_OFF_T_FMT
+                        " is larger than the configured limit"
                         " of %" APR_OFF_T_FMT, ctx->remaining, ctx->limit);
                 return APR_ENOSPC;
             }
