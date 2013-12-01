859d1e5caee8b8cd3f40cb8ef5c29fa394b9749e
Kaspar Brand <kbrand@apache.org>
2013-12-01 11:57:42 +0000

    SGC became dead in January 2000, effectively (http://www.gpo.gov/fdsys/pkg/FR-2000-01-14/pdf/00-983.pdf) Almost 14 years later, there's certainly no longer any need to spit out some fancy log message.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1546805 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_util_ssl.c
+++ modules/ssl/ssl_util_ssl.c
@@ -188,29 +188,6 @@ int SSL_smart_shutdown(SSL *ssl)
 **  _________________________________________________________________
 */
 
-/* check whether cert contains extended key usage with a SGC tag */
-BOOL SSL_X509_isSGC(X509 *cert)
-{
-    int ext_nid;
-    EXTENDED_KEY_USAGE *sk;
-    BOOL is_sgc;
-    int i;
-
-    is_sgc = FALSE;
-    sk = X509_get_ext_d2i(cert, NID_ext_key_usage, NULL, NULL);
-    if (sk) {
-        for (i = 0; i < sk_ASN1_OBJECT_num(sk); i++) {
-            ext_nid = OBJ_obj2nid(sk_ASN1_OBJECT_value(sk, i));
-            if (ext_nid == NID_ms_sgc || ext_nid == NID_ns_sgc) {
-                is_sgc = TRUE;
-                break;
-            }
-        }
-    EXTENDED_KEY_USAGE_free(sk);
-    }
-    return is_sgc;
-}
-
 /* retrieve basic constraints ingredients */
 BOOL SSL_X509_getBC(X509 *cert, int *ca, int *pathlen)
 {
