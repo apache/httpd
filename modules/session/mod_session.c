5bbecb10be3d7b2a280028c6cf70a23e1a86163f
Jeff Trawick <trawick@apache.org>
2014-01-20 21:01:15 +0000

    mod_session: Fix problems interpreting the SessionInclude and SessionExclude configuration.
    PR: 56038
    Submitted by: Erik Pearson <erik adaptations.com>
    Reviewed by: trawick
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1559828 13f79535-47bb-0310-9956-ffa450edef68

--- modules/session/mod_session.c
+++ modules/session/mod_session.c
@@ -62,16 +62,16 @@ static int session_included(request_rec * r, session_dir_conf * conf)
         included = 0;
         for (i = 0; !included && i < conf->includes->nelts; i++) {
             const char *include = includes[i];
-            if (strncmp(r->uri, include, strlen(include))) {
+            if (strncmp(r->uri, include, strlen(include)) == 0) {
                 included = 1;
             }
         }
     }
 
     if (conf->excludes->nelts) {
-        for (i = 0; included && i < conf->includes->nelts; i++) {
+        for (i = 0; included && i < conf->excludes->nelts; i++) {
             const char *exclude = excludes[i];
-            if (strncmp(r->uri, exclude, strlen(exclude))) {
+            if (strncmp(r->uri, exclude, strlen(exclude)) == 0) {
                 included = 0;
             }
         }
