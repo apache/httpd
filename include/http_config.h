26f5f933c07e0cb2eef2210082a09526959795e8
Jeff Trawick <trawick@apache.org>
2014-01-22 19:15:14 +0000

    doxygen improvements
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1560482 13f79535-47bb-0310-9956-ffa450edef68

--- include/http_config.h
+++ include/http_config.h
@@ -1229,6 +1229,7 @@ AP_CORE_DECLARE(void *) ap_set_config_vectors(server_rec *server,
  * Run the header parser functions for each module
  * @param r The current request
  * @return OK or DECLINED
+ * @ingroup hooks
  */
 AP_DECLARE_HOOK(int,header_parser,(request_rec *r))
 
@@ -1238,6 +1239,7 @@ AP_DECLARE_HOOK(int,header_parser,(request_rec *r))
  * @param plog The logging streams pool
  * @param ptemp The temporary pool
  * @return OK or DECLINED on success anything else is a error
+ * @ingroup hooks
  */
 AP_DECLARE_HOOK(int,pre_config,(apr_pool_t *pconf,apr_pool_t *plog,
                                 apr_pool_t *ptemp))
@@ -1249,6 +1251,7 @@ AP_DECLARE_HOOK(int,pre_config,(apr_pool_t *pconf,apr_pool_t *plog,
  * @param ptemp The temporary pool
  * @param s the server to operate upon
  * @return OK or DECLINED on success anything else is a error
+ * @ingroup hooks
  */
 AP_DECLARE_HOOK(int,check_config,(apr_pool_t *pconf, apr_pool_t *plog,
                                   apr_pool_t *ptemp, server_rec *s))
@@ -1261,7 +1264,7 @@ AP_DECLARE_HOOK(int,check_config,(apr_pool_t *pconf, apr_pool_t *plog,
  * @note To avoid reordering problems due to different buffering, hook
  *       functions should only apr_file_*() to print to stdout/stderr and
  *       not simple printf()/fprintf().
- *     
+ * @ingroup hooks
  */
 AP_DECLARE_HOOK(void,test_config,(apr_pool_t *pconf, server_rec *s))
 
@@ -1272,6 +1275,7 @@ AP_DECLARE_HOOK(void,test_config,(apr_pool_t *pconf, server_rec *s))
  * @param ptemp The temporary pool
  * @param s The list of server_recs
  * @return OK or DECLINED on success anything else is a error
+ * @ingroup hooks
  */
 AP_DECLARE_HOOK(int,post_config,(apr_pool_t *pconf,apr_pool_t *plog,
                                  apr_pool_t *ptemp,server_rec *s))
@@ -1283,6 +1287,7 @@ AP_DECLARE_HOOK(int,post_config,(apr_pool_t *pconf,apr_pool_t *plog,
  * @param ptemp The temporary pool
  * @param s The list of server_recs
  * @return OK or DECLINED on success anything else is a error
+ * @ingroup hooks
  */
 AP_DECLARE_HOOK(int,open_logs,(apr_pool_t *pconf,apr_pool_t *plog,
                                apr_pool_t *ptemp,server_rec *s))
@@ -1291,6 +1296,7 @@ AP_DECLARE_HOOK(int,open_logs,(apr_pool_t *pconf,apr_pool_t *plog,
  * Run the child_init functions for each module
  * @param pchild The child pool
  * @param s The list of server_recs in this server
+ * @ingroup hooks
  */
 AP_DECLARE_HOOK(void,child_init,(apr_pool_t *pchild, server_rec *s))
 
@@ -1298,6 +1304,7 @@ AP_DECLARE_HOOK(void,child_init,(apr_pool_t *pchild, server_rec *s))
  * Run the handler functions for each module
  * @param r The request_rec
  * @remark non-wildcard handlers should HOOK_MIDDLE, wildcard HOOK_LAST
+ * @ingroup hooks
  */
 AP_DECLARE_HOOK(int,handler,(request_rec *r))
 
@@ -1311,6 +1318,7 @@ AP_DECLARE_HOOK(int,handler,(request_rec *r))
  * @param lookup_uri Controls whether the caller actually wants content or not.
  * lookup is set when the quick_handler is called out of
  * ap_sub_req_lookup_uri()
+ * @ingroup hooks
  */
 AP_DECLARE_HOOK(int,quick_handler,(request_rec *r, int lookup_uri))
 
@@ -1318,6 +1326,7 @@ AP_DECLARE_HOOK(int,quick_handler,(request_rec *r, int lookup_uri))
  * Retrieve the optional functions for each module.
  * This is run immediately before the server starts. Optional functions should
  * be registered during the hook registration phase.
+ * @ingroup hooks
  */
 AP_DECLARE_HOOK(void,optional_fn_retrieve,(void))
 
@@ -1334,6 +1343,7 @@ AP_DECLARE_HOOK(void,optional_fn_retrieve,(void))
  *         APR_ENOENT or APR_ENOTDIR if no htaccess file exists,
  *         AP_DECLINED to let later modules do the opening,
  *         any other error code on error.
+ * @ingroup hooks
  */
 AP_DECLARE_HOOK(apr_status_t,open_htaccess,
                 (request_rec *r, const char *dir_name, const char *access_name,
