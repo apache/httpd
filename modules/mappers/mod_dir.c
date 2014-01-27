f39014c128824377cb88f28c9b92e167dade1b3f
Tjerk Meesters <tjerk.meesters@gmail.com>
2014-01-27 14:54:22 +0800

    attempt fallback resource if directory index fails
    Attempt fallback if fixup_dir() returns anything but OK instead of returning DECLINED.

--- modules/mappers/mod_dir.c
+++ modules/mappers/mod_dir.c
@@ -388,7 +388,14 @@ static int dir_fixups(request_rec *r)
 {
     if (r->finfo.filetype == APR_DIR) {
         /* serve up a directory */
-        return fixup_dir(r);
+        int res = fixup_dir(r);
+
+        if (res != OK) {
+           /* use fallback */
+           return fixup_dflt(r);
+        }
+
+        return res;
     }
     else if ((r->finfo.filetype == APR_NOFILE) && (r->handler == NULL)) {
         /* No handler and nothing in the filesystem - use fallback */
