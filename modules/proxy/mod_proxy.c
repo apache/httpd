bf09134f801ea740a2e4eea8f1c300c25d5f369a
Jim Jagielski <jim@apache.org>
2014-01-28 18:11:59 +0000

    Merge r1557317, r1556911, r1556914, r1555259, r1556912, r1556937, r1559351, r1463046 from trunk:
    Style, indentation. No functional change.
    
    As in 'dav_generic_do_refresh', add missing break in 'dav_fs_do_refresh' to avoid useless computation.
    
    Add missing break in 'dav_generic_do_refresh' to avoid useless computation.
    
    Allocate correct size for the array to avoid useless memory allocation and copy
    
    Add missing break.
    
    Oops (fix r1556912)
    
    No need to test for NULL, apr_pstrndup already handles it.
    
    Remove some useless declarations that were shadowing other local
    variables of the same name.
    
    Submitted by: jailletc36, sf
    Reviewed/backported by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1562134 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy.c
+++ modules/proxy/mod_proxy.c
@@ -1726,6 +1726,7 @@ static const char *
     for (i = 0; i < conf->noproxies->nelts; i++) {
         if (strcasecmp(arg, list[i].name) == 0) { /* ignore case for host names */
             found = 1;
+            break;
         }
     }
 
@@ -1759,8 +1760,10 @@ static const char *
 
     /* Don't duplicate entries */
     for (i = 0; i < conf->dirconn->nelts; i++) {
-        if (strcasecmp(arg, list[i].name) == 0)
+        if (strcasecmp(arg, list[i].name) == 0) {
             found = 1;
+            break;
+        }
     }
 
     if (!found) {
--- server/util_regex.c
+++ server/util_regex.c
@@ -162,7 +162,6 @@ AP_DECLARE(int) ap_rxplus_exec(apr_pool_t *pool, ap_rxplus_t *rx,
         remainder = pattern + startl + oldl;
         if (rx->flags & AP_REG_MULTI) {
             /* recurse to do any further matches */
-            char *subs;
             ret += ap_rxplus_exec(pool, rx, remainder, &subs);
             if (ret > 1) {
                 /* a further substitution happened */
@@ -206,5 +205,5 @@ AP_DECLARE(char*) ap_rxplus_pmatch(apr_pool_t *pool, ap_rxplus_t *rx, int n)
     int len;
     const char *match;
     ap_rxplus_match(rx, n, &len, &match);
-    return (match != NULL) ? apr_pstrndup(pool, match, len) : NULL;
+    return apr_pstrndup(pool, match, len);
 }
