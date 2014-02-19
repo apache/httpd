33f7dbafce1a7cf76da1aa83d7cfcb7bb599844b
Eric Covener <covener@apache.org>
2014-02-19 02:42:29 +0000

    handle POLLERR/POLLHUP during poll() to avoid high CPU busy loop.
    Submitted By: Joffroy Christen <joffroy.christen solvaxis com>, Eric Covener]
    Committed By: covener
     
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1569615 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_wstunnel.c
+++ modules/proxy/mod_proxy_wstunnel.c
@@ -64,7 +64,7 @@ static int proxy_wstunnel_pump(ws_baton_t *baton, apr_time_t timeout) {
             }
         }
 
-        ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r, APLOGNO(02445)
+        ap_log_rerror(APLOG_MARK, APLOG_TRACE1, 0, r, APLOGNO(02445)
                 "woke from poll(), i=%d", pollcnt);
 
         for (pi = 0; pi < pollcnt; pi++) {
@@ -73,7 +73,7 @@ static int proxy_wstunnel_pump(ws_baton_t *baton, apr_time_t timeout) {
             if (cur->desc.s == sock) {
                 pollevent = cur->rtnevents;
                 if (pollevent & APR_POLLIN) {
-                    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r, APLOGNO(02446)
+                    ap_log_rerror(APLOG_MARK, APLOG_TRACE1, 0, r, APLOGNO(02446)
                             "sock was readable");
                     rv = proxy_wstunnel_transfer(r, backconn, c, bb, "sock");
                 }
@@ -83,15 +83,32 @@ static int proxy_wstunnel_pump(ws_baton_t *baton, apr_time_t timeout) {
                     ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, APLOGNO(02447)
                             "err/hup on backconn");
                 }
+                else { 
+                    rv = APR_EGENERAL;
+                    ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, APLOGNO(02605)
+                            "unknown event on backconn %d", pollevent);
+                }
                 if (rv != APR_SUCCESS)
                     client_error = 1;
             }
             else if (cur->desc.s == client_socket) {
                 pollevent = cur->rtnevents;
                 if (pollevent & APR_POLLIN) {
-                    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r, APLOGNO(02448)
+                    ap_log_rerror(APLOG_MARK, APLOG_TRACE1, 0, r, APLOGNO(02448)
                             "client was readable");
                     rv = proxy_wstunnel_transfer(r, c, backconn, bb, "client");
+                }
+                else if ((pollevent & APR_POLLERR)
+                        || (pollevent & APR_POLLHUP)) {
+                    rv = APR_EPIPE;
+                    c->aborted = 1;
+                    ap_log_rerror(APLOG_MARK, APLOG_TRACE1, 0, r, APLOGNO(02607)
+                            "err/hup on client conn");
+                }
+                else { 
+                    rv = APR_EGENERAL;
+                    ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, APLOGNO(02606)
+                            "unknown event on client conn %d", pollevent);
                 }
             }
             else {
