a3533194fa088292718cfb2f384221b58792d7ff
Ruediger Pluem <rpluem@apache.org>
2014-03-12 12:28:03 +0000

    Merge r1493330, r1504276, r1552227, r1551714, r1490994, r1172732 from trunk:
    Correct typo in error message
    
    Unlikely potential memory leak.
    Point 12 of PR 54936
    
    Remove <Proxy ~ wildcard-url> syntax which:
       - is equivalent to <ProxyMatch wildcard-url>
       - has never been documented
       - incorrectly checks parameters (!cmd->path should be !cmd->path[0])
       - is buggy (! is missing in front of strncasecmp)
    
    Avoid sending back uninitialized memory, use 'ftpmessage' in order to have something meaningful instead.
    
    Save 8k of stack by the same time.
    
    NULL terminate the right buffer
    
    Fix issues found by PVS-Studio static analyzer:
    
    mod_mime_magic:
    cut'n'paste error rsl_encoding vs. rsl_separator, also simplify code a bit
    
    mod_header:
    wrong string initialization (leading to segfault if format argument is missing)
    
    util_win32: duplicate sizeof (untested; Windows people, please review)
    
    PR: 51542
    Submitted by: Andrey Karpov <karpov viva64 com>, Stefan Fritsch
    
    Reviewed by: jailletc36, rpluem, wrowe
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1576709 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/proxy_util.c
+++ modules/proxy/proxy_util.c
@@ -2329,7 +2329,7 @@ static apr_status_t send_http_connect(proxy_conn_rec *backend,
         nbytes = sizeof(drain_buffer) - 1;
         while (status == APR_SUCCESS && nbytes) {
             status = apr_socket_recv(backend->sock, drain_buffer, &nbytes);
-            buffer[nbytes] = '\0';
+            drain_buffer[nbytes] = '\0';
             nbytes = sizeof(drain_buffer) - 1;
             if (strstr(drain_buffer, "\r\n\r\n") != NULL) {
                 complete = 1;
