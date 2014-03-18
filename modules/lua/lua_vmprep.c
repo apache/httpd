6c534899d7f8dba5bcc6410934ec6cae43be90c8
Daniel Gruno <humbedooh@apache.org>
2014-03-18 17:02:47 +0000

    mod_lua: backport r1578870+r1578882 and add to change log.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1578964 13f79535-47bb-0310-9956-ffa450edef68

--- modules/lua/lua_vmprep.c
+++ modules/lua/lua_vmprep.c
@@ -354,7 +354,12 @@ static apr_status_t vm_construct(lua_State **vm, void *params, apr_pool_t *lifec
                                            : lua_tostring(L, 0));
             return APR_EBADF;
         }
-        lua_pcall(L, 0, LUA_MULTRET, 0);
+        if ( lua_pcall(L, 0, LUA_MULTRET, 0) == LUA_ERRRUN ) {
+            ap_log_perror(APLOG_MARK, APLOG_ERR, 0, lifecycle_pool, APLOGNO(02613)
+                          "Error loading %s: %s", spec->file,
+                            lua_tostring(L, -1));
+            return APR_EBADF;
+        }
     }
 
 #ifdef AP_ENABLE_LUAJIT
