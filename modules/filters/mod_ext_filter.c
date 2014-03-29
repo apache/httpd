73cfa6e2de2ab0a1ce1dde5f60733bb7bfe64b70
Jeff Trawick <trawick@apache.org>
2014-03-29 14:58:24 +0000

    s/comment/self-documenting/
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1583008 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/mod_ext_filter.c
+++ modules/filters/mod_ext_filter.c
@@ -715,7 +715,7 @@ static apr_status_t pass_data_to_filter(ap_filter_t *f, const char *data,
                 /* Yuck... I'd really like to wait until I can read
                  * or write, but instead I have to sleep and try again
                  */
-                apr_sleep(100000); /* 100 milliseconds */
+                apr_sleep(apr_time_from_msec(100));
                 ap_log_rerror(APLOG_MARK, APLOG_TRACE6, 0, f->r, "apr_sleep()");
 #endif /* APR_FILES_AS_SOCKETS */
             }
