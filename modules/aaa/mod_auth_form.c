08346fc97286230dfaf5e36d27065b52d2695d2c
Yann Ylavic <ylavic@apache.org>
2014-04-07 09:12:09 +0000

    mod_auth_form: update empty log tags.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1585435 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_auth_form.c
+++ modules/aaa/mod_auth_form.c
@@ -1282,7 +1282,7 @@ static int authenticate_form_post_config(apr_pool_t *pconf, apr_pool_t *plog,
         ap_session_get_fn = APR_RETRIEVE_OPTIONAL_FN(ap_session_get);
         ap_session_set_fn = APR_RETRIEVE_OPTIONAL_FN(ap_session_set);
         if (!ap_session_load_fn || !ap_session_get_fn || !ap_session_set_fn) {
-            ap_log_error(APLOG_MARK, APLOG_CRIT, 0, NULL, APLOGNO()
+            ap_log_error(APLOG_MARK, APLOG_CRIT, 0, NULL, APLOGNO(02617)
                     "You must load mod_session to enable the mod_auth_form "
                                        "functions");
             return !OK;
@@ -1293,7 +1293,7 @@ static int authenticate_form_post_config(apr_pool_t *pconf, apr_pool_t *plog,
         ap_request_insert_filter_fn = APR_RETRIEVE_OPTIONAL_FN(ap_request_insert_filter);
         ap_request_remove_filter_fn = APR_RETRIEVE_OPTIONAL_FN(ap_request_remove_filter);
         if (!ap_request_insert_filter_fn || !ap_request_remove_filter_fn) {
-            ap_log_error(APLOG_MARK, APLOG_CRIT, 0, NULL, APLOGNO()
+            ap_log_error(APLOG_MARK, APLOG_CRIT, 0, NULL, APLOGNO(02618)
                     "You must load mod_request to enable the mod_auth_form "
                                        "functions");
             return !OK;
