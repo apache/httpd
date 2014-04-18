47d10d6f50bb122e02a8b2c47de6c4246957ad86
Jim Jagielski <jim@apache.org>
2014-04-18 15:27:42 +0000

    Merge r1543174, r1560367, r1560546, r1560689, r1560979, r1561137, r1561660 from trunk:
    Correct logic... if this is a UDS socket, then bypass all
    this.
    
    make mod_rewrite and mod_proxy UDS work together...
    
    Adjust url as required, following what we did to r->filename.
    Save some cycles when searching...
    
    Tuck away UDS path in request-rec, since worker isn't
    thread-safe. Protect from NULL refs.
    
    Reflow logic... pull UDS stuff out
    
    handle leak. thx to Y^2
    
    r->filename should maintain the proxy: prefix for PHP-FPM
    Reviewed/backported by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1588494 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy.c
+++ modules/proxy/mod_proxy.c
@@ -2631,6 +2631,8 @@ static void child_init(apr_pool_t *p, server_rec *s)
                 ap_proxy_hashfunc(conf->forward->s->name, PROXY_HASHFUNC_FNV);
             /* Do not disable worker in case of errors */
             conf->forward->s->status |= PROXY_WORKER_IGNORE_ERRORS;
+            /* Mark as the "generic" worker */
+            conf->forward->s->status |= PROXY_WORKER_GENERIC;
             ap_proxy_initialize_worker(conf->forward, s, conf->pool);
             /* Disable address cache for generic forward worker */
             conf->forward->s->is_address_reusable = 0;
@@ -2646,6 +2648,8 @@ static void child_init(apr_pool_t *p, server_rec *s)
                 ap_proxy_hashfunc(reverse->s->name, PROXY_HASHFUNC_FNV);
             /* Do not disable worker in case of errors */
             reverse->s->status |= PROXY_WORKER_IGNORE_ERRORS;
+            /* Mark as the "generic" worker */
+            reverse->s->status |= PROXY_WORKER_GENERIC;
             conf->reverse = reverse;
             ap_proxy_initialize_worker(conf->reverse, s, conf->pool);
             /* Disable address cache for generic reverse worker */
