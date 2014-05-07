apache/httpd

commit cc4c936c2ebcb99e92afb8fedcee74b2e51ac7cb

Support optional initialization arguments for socache providers in mod_authn_socache.
git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1531961 13f79535-47bb-0310-9956-ffa450edef68

modules/aaa/mod_authn_socache.c
@@ -39,7 +39,7 @@ typedef struct authn_cache_dircfg {
     const char *context;
 } authn_cache_dircfg;
 
-/* FIXME: figure out usage of socache create vs init
+/* FIXME:
  * I think the cache and mutex should be global
  */
 static apr_global_mutex_t *authn_cache_mutex = NULL;
@@ -85,7 +85,6 @@ static int authn_cache_post_config(apr_pool_t *pconf, apr_pool_t *plog,
                                    apr_pool_t *ptmp, server_rec *s)
 {
     apr_status_t rv;
-    const char *errmsg;
     static struct ap_socache_hints authn_cache_hints = {64, 32, 60000000};
 
     if (!configured) {
@@ -107,12 +106,6 @@ static int authn_cache_post_config(apr_pool_t *pconf, apr_pool_t *plog,
         return 500; /* An HTTP status would be a misnomer! */
     }
     apr_pool_cleanup_register(pconf, NULL, remove_lock, apr_pool_cleanup_null);
-
-    errmsg = socache_provider->create(&socache_instance, NULL, ptmp, pconf);
-    if (errmsg) {
-        ap_log_perror(APLOG_MARK, APLOG_CRIT, rv, plog, APLOGNO(01676) "%s", errmsg);
-        return 500; /* An HTTP status would be a misnomer! */
-    }
 
     rv = socache_provider->init(socache_instance, authn_cache_id,
                                 &authn_cache_hints, s, pconf);
@@ -143,15 +136,36 @@ static const char *authn_cache_socache(cmd_parms *cmd, void *CFG,
                                        const char *arg)
 {
     const char *errmsg = ap_check_cmd_context(cmd, GLOBAL_ONLY);
+    const char *sep, *name;
+
     if (errmsg)
         return errmsg;
-    socache_provider = ap_lookup_provider(AP_SOCACHE_PROVIDER_GROUP, arg,
+
+    /* Argument is of form 'name:args' or just 'name'. */
+    sep = ap_strchr_c(arg, ':');
+    if (sep) {
+        name = apr_pstrmemdup(cmd->pool, arg, sep - arg);
+        sep++;
+    }
+    else {
+        name = arg;
+    }
+
+    socache_provider = ap_lookup_provider(AP_SOCACHE_PROVIDER_GROUP, name,
                                           AP_SOCACHE_PROVIDER_VERSION);
     if (socache_provider == NULL) {
         errmsg = apr_psprintf(cmd->pool,
                               "Unknown socache provider '%s'. Maybe you need "
                               "to load the appropriate socache module "
                               "(mod_socache_%s?)", arg, arg);
+    }
+    else {
+        errmsg = socache_provider->create(&socache_instance, sep,
+                                          cmd->temp_pool, cmd->pool);
+    }
+
+    if (errmsg) {
+        errmsg = apr_psprintf(cmd->pool, "AuthnCacheSOCache: %s", errmsg);
     }
     return errmsg;
 }

docs/manual/mod/mod_authn_socache.xml
@@ -115,16 +115,17 @@ AuthnCacheSOCache dbm
 <directivesynopsis>
 <name>AuthnCacheSOCache</name>
 <description>Select socache backend provider to use</description>
-<syntax>AuthnCacheSOCache <var>provider-name</var></syntax>
+<syntax>AuthnCacheSOCache <var>provider-name[:provider-args]</var></syntax>
 <contextlist><context>server config</context></contextlist>
 <override>None</override>
 
 <usage>
     <p>This is a server-wide setting to select a provider for the
-    <a href="../socache.html">shared object cache</a>.
-    Values are "dbm", "dc", "memcache", or "shmcb", each subject to the
-    appropriate module being loaded.  If not set, your platform's
-       default will be used.</p>
+    <a href="../socache.html">shared object cache</a>, followed by
+    optional arguments for that provider.
+    Some possible values for <var>provider-name</var> are "dbm", "dc",
+    "memcache", or "shmcb", each subject to the appropriate module
+    being loaded.  If not set, your platform's default will be used.</p>
 </usage>
 </directivesynopsis>
 

commit 32b06a6e6b985ade5ca94be28b27a5d858250b25

mod_authn_socache: Fix creation of default socache_instance. Fixes crash                    on startup if default socache_provider is used and                    AuthnCacheEnable or AuthnCacheProvideFor is used.                    This problem has been introduced in r1531961.                    PR 56371.
Submitted by: jkaluza
Reviewed by: ylavic, jim, trawick


git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1592991 13f79535-47bb-0310-9956-ffa450edef68

STATUS
@@ -100,16 +100,6 @@ RELEASE SHOWSTOPPERS:
 PATCHES ACCEPTED TO BACKPORT FROM TRUNK:
   [ start all new proposals below, under PATCHES PROPOSED. ]
 
-   * mod_authn_socache: Fix creation of default socache_instance. Fixes crash
-                        on startup if default socache_provider is used and
-                        AuthnCacheEnable or AuthnCacheProvideFor is used.
-                        This problem has been introduced in r1531961.
-                        PR 56371.
-     Submitted/Committed by: jkaluza
-     trunk patch: http://svn.apache.org/r1576233
-     2.4.x patch: trunk works
-     +1: ylavic, jim, trawick
-
    * mod_proxy_fcgi: Don't crash when connect to the backend fails.
      trunk patch: http://svn.apache.org/r1590437 (and additional CHANGES tweak in r1592500)
      2.4.x patch: trunk patch works other than CHANGES

modules/aaa/mod_authn_socache.c
@@ -87,6 +87,7 @@ static int authn_cache_post_config(apr_pool_t *pconf, apr_pool_t *plog,
 {
     apr_status_t rv;
     static struct ap_socache_hints authn_cache_hints = {64, 32, 60000000};
+    const char *errmsg;
 
     if (!configured) {
         return OK;    /* don't waste the overhead of creating mutex & cache */
@@ -97,6 +98,20 @@ static int authn_cache_post_config(apr_pool_t *pconf, apr_pool_t *plog,
                       "(no default found on this platform). Maybe you need to "
                       "load mod_socache_shmcb or another socache module first");
         return 500; /* An HTTP status would be a misnomer! */
+    }
+
+    /* We have socache_provider, but do not have socache_instance. This should
+     * happen only when using "default" socache_provider, so create default
+     * socache_instance in this case. */
+    if (socache_instance == NULL) {
+        errmsg = socache_provider->create(&socache_instance, NULL,
+                                          ptmp, pconf);
+        if (errmsg) {
+            ap_log_perror(APLOG_MARK, APLOG_CRIT, rv, plog, APLOGNO(02612)
+                        "failed to create mod_socache_shmcb socache "
+                        "instance: %s", errmsg);
+            return 500;
+        }
     }
 
     rv = ap_global_mutex_create(&authn_cache_mutex, NULL,