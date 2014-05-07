52f7cf9c456b7cc2bfc2902c13186f971d5b740c
Jeff Trawick <trawick@apache.org>
2014-05-07 12:43:56 +0000

    mod_authn_socache: Fix creation of default socache_instance. Fixes crash                    on startup if default socache_provider is used and                    AuthnCacheEnable or AuthnCacheProvideFor is used.                    This problem has been introduced in r1531961.                    PR 56371.
    Submitted by: jkaluza
    Reviewed by: ylavic, jim, trawick
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1592991 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_authn_socache.c
+++ modules/aaa/mod_authn_socache.c
@@ -87,6 +87,7 @@ static int authn_cache_post_config(apr_pool_t *pconf, apr_pool_t *plog,
 {
     apr_status_t rv;
     static struct ap_socache_hints authn_cache_hints = {64, 32, 60000000};
+    const char *errmsg;
 
     if (!configured) {
         return OK;    /* don't waste the overhead of creating mutex & cache */
@@ -97,6 +98,20 @@ static int authn_cache_post_config(apr_pool_t *pconf, apr_pool_t *plog,
                       "(no default found on this platform). Maybe you need to "
                       "load mod_socache_shmcb or another socache module first");
         return 500; /* An HTTP status would be a misnomer! */
+    }
+
+    /* We have socache_provider, but do not have socache_instance. This should
+     * happen only when using "default" socache_provider, so create default
+     * socache_instance in this case. */
+    if (socache_instance == NULL) {
+        errmsg = socache_provider->create(&socache_instance, NULL,
+                                          ptmp, pconf);
+        if (errmsg) {
+            ap_log_perror(APLOG_MARK, APLOG_CRIT, rv, plog, APLOGNO(02612)
+                        "failed to create mod_socache_shmcb socache "
+                        "instance: %s", errmsg);
+            return 500;
+        }
     }
 
     rv = ap_global_mutex_create(&authn_cache_mutex, NULL,
