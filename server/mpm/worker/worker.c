652fbb9b5b0d6652e7aebb15916ae8e4e8639f92
Mike Rumph <mrumph@apache.org>
2014-05-13 17:56:33 +0000

    Fix some comment typos
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1594306 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/worker/worker.c
+++ server/mpm/worker/worker.c
@@ -457,7 +457,7 @@ static int volatile restart_pending;
  * Previously this was initiated in sig_term() and restart() signal handlers,
  * but we want to be able to start a shutdown/restart from other sources --
  * e.g. on Win32, from the service manager. Now the service manager can
- * call ap_start_shutdown() or ap_start_restart() as appropiate.  Note that
+ * call ap_start_shutdown() or ap_start_restart() as appropriate.  Note that
  * these functions can also be called by the child processes, since global
  * variables are no longer used to pass on the required action to the parent.
  *
