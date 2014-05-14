1eaa191595cd1b020ba326376f08d410945eb6fe
Jeff Trawick <trawick@apache.org>
2014-05-14 10:50:08 +0000

    Merge r1583175 and r1593745 from trunk:
    mod_alias: Stop setting CONTEXT_PREFIX and CONTEXT_DOCUMENT environment
    variables as a result of AliasMatch.
    
    Submitted by: covener
    Reviewed by: jim, ylavic
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1594539 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_alias.c
+++ modules/mappers/mod_alias.c
@@ -370,17 +370,6 @@ static char *try_alias_list(request_rec *r, apr_array_header_t *aliases,
                                                     uri.fragment, NULL);
                             }
                        }
-                       else {
-                           int pathlen = strlen(found) -
-                                         (strlen(r->uri + regm[0].rm_eo));
-                           AP_DEBUG_ASSERT(pathlen >= 0);
-                           AP_DEBUG_ASSERT(pathlen <= strlen(found));
-                           ap_set_context_info(r,
-                                               apr_pstrmemdup(r->pool, r->uri,
-                                                              regm[0].rm_eo),
-                                               apr_pstrmemdup(r->pool, found,
-                                                              pathlen));
-                       }
                     }
                     else {
                         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(00672)
