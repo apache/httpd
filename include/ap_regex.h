3b2463748a6271965a56884a06bf59f0368912a5
Christophe Jaillet <jailletc36@apache.org>
2014-05-29 06:20:36 +0000

    Doxygen fix + alignment + typo
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1598207 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_regex.h
+++ include/ap_regex.h
@@ -65,19 +65,19 @@ extern "C" {
 
 /* Options for ap_regcomp, ap_regexec, and ap_rxplus versions: */
 
-#define AP_REG_ICASE    0x01 /** use a case-insensitive match */
-#define AP_REG_NEWLINE  0x02 /** don't match newlines against '.' etc */
-#define AP_REG_NOTBOL   0x04 /** ^ will not match against start-of-string */
-#define AP_REG_NOTEOL   0x08 /** $ will not match against end-of-string */
+#define AP_REG_ICASE    0x01 /**< use a case-insensitive match */
+#define AP_REG_NEWLINE  0x02 /**< don't match newlines against '.' etc */
+#define AP_REG_NOTBOL   0x04 /**< ^ will not match against start-of-string */
+#define AP_REG_NOTEOL   0x08 /**< $ will not match against end-of-string */
 
-#define AP_REG_EXTENDED (0)  /** unused */
-#define AP_REG_NOSUB    (0)  /** unused */
+#define AP_REG_EXTENDED (0)  /**< unused */
+#define AP_REG_NOSUB    (0)  /**< unused */
 
-#define AP_REG_MULTI 0x10    /* perl's /g (needs fixing) */
-#define AP_REG_NOMEM 0x20    /* nomem in our code */
-#define AP_REG_DOTALL 0x40   /* perl's /s flag */
+#define AP_REG_MULTI    0x10 /**< perl's /g (needs fixing) */
+#define AP_REG_NOMEM    0x20 /**< nomem in our code */
+#define AP_REG_DOTALL   0x40 /**< perl's /s flag */
 
-#define AP_REG_MATCH "MATCH_" /** suggested prefix for ap_regname */
+#define AP_REG_MATCH "MATCH_" /**< suggested prefix for ap_regname */
 
 /* Error values: */
 enum {
@@ -246,5 +246,4 @@ AP_DECLARE(char*) ap_rxplus_pmatch(apr_pool_t *pool, ap_rxplus_t *rx, int n);
 }   /* extern "C" */
 #endif
 
-#endif /* AP_REGEX_T */
-
+#endif /* AP_REGEX_H */
