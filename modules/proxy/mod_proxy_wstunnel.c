0c2094251fef3fb8ddcc733a5baff1ae24423a61
Christophe Jaillet <jailletc36@apache.org>
2014-06-24 21:47:21 +0000

    Use ap_remove_input_filter_byhandle instead of duplicating the code.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1605207 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_wstunnel.c
+++ modules/proxy/mod_proxy_wstunnel.c
@@ -307,29 +307,6 @@ static apr_status_t proxy_wstunnel_transfer(request_rec *r, conn_rec *c_i, conn_
     return rv;
 }
 
-/* Search thru the input filters and remove the reqtimeout one */
-static void remove_reqtimeout(ap_filter_t *next)
-{
-    ap_filter_t *reqto = NULL;
-    ap_filter_rec_t *filter;
-
-    filter = ap_get_input_filter_handle("reqtimeout");
-    if (!filter) {
-        return;
-    }
-
-    while (next) {
-        if (next->frec == filter) {
-            reqto = next;
-            break;
-        }
-        next = next->next;
-    }
-    if (reqto) {
-        ap_remove_input_filter(reqto);
-    }
-}
-
 /*
  * process the request and write the response.
  */
@@ -403,7 +380,7 @@ static int proxy_wstunnel_request(apr_pool_t *p, request_rec *r,
     pollfd.desc.s = client_socket;
     apr_pollset_add(pollset, &pollfd);
 
-    remove_reqtimeout(c->input_filters);
+    ap_remove_input_filter_byhandle(c->input_filters, "reqtimeout");
 
     r->output_filters = c->output_filters;
     r->proto_output_filters = c->output_filters;
