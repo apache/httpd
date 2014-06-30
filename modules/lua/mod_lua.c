1cbfd4a30e7a60d4d2f3cf2b0c3d3ba98f8e0de7
Ben Reser <breser@apache.org>
2014-06-30 16:54:27 +0000

    mod_lua: Remove dead code left over from the old code cache.
    The code that used this was commented out in r721594, then removed entirely in
    r728497, and finally a commit was made intending to remove the last traces of
    the code cache in r1200513, but this initialization lived on anyway.
    
    * modules/lua/mod_lua.c
      (create_server_config): Remove unused empty hash and rwlock for hash.
    
    * modules/lua/mod_lua.h
      (ap_lua_server_cfg): Remove unneeded hash and rwlock entries.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1606836 13f79535-47bb-0310-9956-ffa450edef68

--- modules/lua/mod_lua.c
+++ modules/lua/mod_lua.c
@@ -1960,8 +1960,6 @@ static void *create_server_config(apr_pool_t *p, server_rec *s)
 {
 
     ap_lua_server_cfg *cfg = apr_pcalloc(p, sizeof(ap_lua_server_cfg));
-    cfg->vm_reslists = apr_hash_make(p);
-    apr_thread_rwlock_create(&cfg->vm_reslists_lock, p);
     cfg->root_path = NULL;
 
     return cfg;
