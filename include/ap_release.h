64daaa40ab4bacb5f1adce4ad99a9c97b19c64b6
Jim Jagielski <jim@apache.org>
2014-07-15 17:14:08 +0000

    And we are at 2.4.11-dev
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1610760 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_release.h
+++ include/ap_release.h
@@ -43,8 +43,8 @@
 
 #define AP_SERVER_MAJORVERSION_NUMBER 2
 #define AP_SERVER_MINORVERSION_NUMBER 4
-#define AP_SERVER_PATCHLEVEL_NUMBER   10
-#define AP_SERVER_DEVBUILD_BOOLEAN    0
+#define AP_SERVER_PATCHLEVEL_NUMBER   11
+#define AP_SERVER_DEVBUILD_BOOLEAN    1
 
 /* Synchronize the above with docs/manual/style/version.ent */
 
