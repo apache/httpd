48327558320d596b28212a8a6746f780ce6d82ae
Christophe Jaillet <jailletc36@apache.org>
2014-07-16 22:09:39 +0000

    Improve doxygen comment.
    Improve layout, add trailing '.' in function description, remove unneeded @fn.
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1611202 13f79535-47bb-0310-9956-ffa450edef68

--- include/util_md5.h
+++ include/util_md5.h
@@ -33,38 +33,34 @@ extern "C" {
 #include "apr_md5.h"
 
 /**
- * Create an MD5 checksum of a given string
- * @param a Pool to allocate out of
- * @param string String to get the checksum of
+ * Create an MD5 checksum of a given string.
+ * @param   a       Pool to allocate out of
+ * @param   string  String to get the checksum of
  * @return The checksum
- * @fn char *ap_md5(apr_pool_t *a, const unsigned char *string)
  */
 AP_DECLARE(char *) ap_md5(apr_pool_t *a, const unsigned char *string);
 
 /**
- * Create an MD5 checksum of a string of binary data
- * @param a Pool to allocate out of
- * @param buf Buffer to generate checksum for
- * @param len The length of the buffer
+ * Create an MD5 checksum of a string of binary data.
+ * @param   a       Pool to allocate out of
+ * @param   buf     Buffer to generate checksum for
+ * @param   len     The length of the buffer
  * @return The checksum
- * @fn char *ap_md5_binary(apr_pool_t *a, const unsigned char *buf, int len)
  */
 AP_DECLARE(char *) ap_md5_binary(apr_pool_t *a, const unsigned char *buf, int len);
 
 /**
- * Convert an MD5 checksum into a base64 encoding
- * @param p The pool to allocate out of
- * @param context The context to convert
+ * Convert an MD5 checksum into a base64 encoding.
+ * @param   p       The pool to allocate out of
+ * @param   context The context to convert
  * @return The converted encoding
- * @fn char *ap_md5contextTo64(apr_pool_t *p, apr_md5_ctx_t *context)
  */
 AP_DECLARE(char *) ap_md5contextTo64(apr_pool_t *p, apr_md5_ctx_t *context);
 
 /**
- * Create an MD5 Digest for a given file
- * @param p The pool to allocate out of
- * @param infile The file to create the digest for
- * @fn char *ap_md5digest(apr_pool_t *p, apr_file_t *infile)
+ * Create an MD5 Digest for a given file.
+ * @param   p       The pool to allocate out of
+ * @param   infile  The file to create the digest for
  */
 AP_DECLARE(char *) ap_md5digest(apr_pool_t *p, apr_file_t *infile);
 
