f2b5cb6c3f8ec9553179ef3c5fe5a86ab6c5885f
Rainer Jung <rjung@apache.org>
2014-07-16 06:04:38 +0000

    Extend the scope of SSLSessionCacheTimeout to sessions resumed by TLS session resumption (RFC 5077).
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1610914 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_init.c
+++ modules/ssl/ssl_engine_init.c
@@ -1264,6 +1264,10 @@ static void ssl_init_server_ctx(server_rec *s,
     ssl_init_ctx(s, p, ptemp, sc->server);
 
     ssl_init_server_certs(s, p, ptemp, sc->server);
+
+    SSL_CTX_set_timeout(sc->server->ssl_ctx,
+                        sc->session_cache_timeout == UNSET ?
+                        SSL_SESSION_CACHE_TIMEOUT : sc->session_cache_timeout);
 }
 
 /*
