f582f97579eb6811969212d46d21319ad49e5c5e
Jim Jagielski <jim@apache.org>
2014-07-17 18:21:59 +0000

    Merge r1572896, r1572911, r1603156 from trunk:
    mod_deflate:
    Don't fail when asked to flush inflated data to the user-agent and that
    coincides with the end of stream ("Zlib error flushing inflate buffer").
    PR 56196.
    
    Submitted By: [Christoph Fausak <christoph.fausak glueckkanja com>]
    Committed By: ylavic
    
    
    mod_deflate: follows up r1572896.
    Be safe from successive or post end-of-stream flush buckets.
    
    
    Add missing CHANGES entries for r1572655,1572663,1572668-1572671,1573224,1586745,1587594,1587639,1590509, r1572092, and r1572896,1572911.
    Submitted by: ylavic
    Reviewed/backported by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1611428 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/mod_deflate.c
+++ modules/filters/mod_deflate.c
@@ -1286,7 +1286,13 @@ static apr_status_t inflate_out_filter(ap_filter_t *f,
             /* flush the remaining data from the zlib buffers */
             zRC = flush_libz_buffer(ctx, c, f->c->bucket_alloc, inflate,
                                     Z_SYNC_FLUSH, UPDATE_CRC);
-            if (zRC != Z_OK) {
+            if (zRC == Z_STREAM_END) {
+                if (ctx->validation_buffer == NULL) {
+                    ctx->validation_buffer = apr_pcalloc(f->r->pool,
+                                                         VALIDATION_SIZE);
+                }
+            }
+            else if (zRC != Z_OK) {
                 return APR_EGENERAL;
             }
 
