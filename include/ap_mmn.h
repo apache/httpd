13978f5c4e47283cac3e396c946137e8a4564f64
Yann Ylavic <ylavic@apache.org>
2014-07-18 21:38:38 +0000

    Merge r1572630, r1572611, r1572967, r1573229 from trunk:
    Redo what was reverted in r1572627.
    Don't reuse a SSL backend connection whose SNI differs. PR 55782.
    This may happen when ProxyPreserveHost is on and the proxy-worker
    handles connections to different Hosts.
    
    
    Follows up r1572606.
    MMN minor bump required by proxy_conn_rec change.
    
    
    mod_proxy: follows up r1572630.
    Don't reuse a SSL backend connection with no SNI for a request requiring SNI.
    
    
    mod_proxy: Add comment and avoid ternary operator as condition (no functional change).
    
    
    Submitted by: ylavic
    Reviewed by: ylavic, rpluem, wrowe
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1611813 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_mmn.h
+++ include/ap_mmn.h
@@ -152,6 +152,7 @@
  * 20051115.32 (2.2.24) Add ap_get_exec_line
  * 20051115.33 (2.2.24) Add ap_pregsub_ex()
  * 20051115.34 (2.2.28) Add ap_copy_scoreboard_worker()
+ * 20051115.35 (2.2.27) Add SSL reusable SNI to mod_proxy.h's proxy_conn_rec
  */
 
 #define MODULE_MAGIC_COOKIE 0x41503232UL /* "AP22" */
@@ -159,7 +160,7 @@
 #ifndef MODULE_MAGIC_NUMBER_MAJOR
 #define MODULE_MAGIC_NUMBER_MAJOR 20051115
 #endif
-#define MODULE_MAGIC_NUMBER_MINOR 34                    /* 0...n */
+#define MODULE_MAGIC_NUMBER_MINOR 35                    /* 0...n */
 
 /**
  * Determine if the server's current MODULE_MAGIC_NUMBER is at least a
