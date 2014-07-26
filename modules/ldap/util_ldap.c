a29cb3fa4d49dc73d3c1ed6c3390e780795583f2
Eric Covener <covener@apache.org>
2014-07-26 16:59:50 +0000

    In 2.4.10, AuthLDAPBindDN might not be used for some LDAP searches, causing LDAP authz failures if AuthLDAPBindDN was able to search through more of LDAP than web users.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1613682 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ldap/util_ldap.c
+++ modules/ldap/util_ldap.c
@@ -1825,7 +1825,7 @@ start_over:
          * combination, which might be reused unintentionally next time this
          * connection is used from the connection pool.
          */
-        ldc->must_rebind = 0;
+        ldc->must_rebind = 1;
         ap_log_rerror(APLOG_MARK, APLOG_TRACE5, 0, r, "LDC %pp used for authn, must be rebound", ldc);
     }
 
