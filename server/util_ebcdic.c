5b8c1587992e5c8b0197f7ea5e91b9152b7f82bc
Christophe Jaillet <jailletc36@apache.org>
2014-08-21 06:56:40 +0000

    Missing va_end spotted by cppcheck
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1619297 13f79535-47bb-0310-9956-ffa450edef68

--- server/util_ebcdic.c
+++ server/util_ebcdic.c
@@ -104,8 +104,10 @@ int ap_rvputs_proto_in_ascii(request_rec *r, ...)
         len = strlen(s);
         ascii_s = apr_pstrmemdup(r->pool, s, len);
         ap_xlate_proto_to_ascii(ascii_s, len);
-        if (ap_rputs(ascii_s, r) < 0)
+        if (ap_rputs(ascii_s, r) < 0) {
+            va_end(va);
             return -1;
+        }
         written += len;
     }
     va_end(va);
