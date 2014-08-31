6d0a719a5dd681349f078b25b6235998e8f0f1e2
Jim Jagielski <jim@apache.org>
2014-08-31 16:07:45 +0000

    Merge r1615289, r1620324 from trunk:
    PR53420: Proxy responses with error status and
    "ProxyErrorOverride On" hang until proxy timeout.
    
    Regression from 2.2. It was introduced by r912063
    in order to fix PR41646.
    
    
    Switch preference for headers, Transfer-Encoding
    first, Content-Length second.
    
    Addition to r1615289.
    
    Submitted by: rjung
    Reviewed/backported by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1621601 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_http.c
+++ modules/proxy/mod_proxy_http.c
@@ -1652,6 +1652,18 @@ apr_status_t ap_proxy_http_process_response(apr_pool_t * p, request_rec *r,
             if (!r->header_only && /* not HEAD request */
                 (proxy_status != HTTP_NO_CONTENT) && /* not 204 */
                 (proxy_status != HTTP_NOT_MODIFIED)) { /* not 304 */
+                const char *tmp;
+                /* Add minimal headers needed to allow http_in filter
+                 * detecting end of body without waiting for a timeout. */
+                if ((tmp = apr_table_get(r->headers_out, "Transfer-Encoding"))) {
+                    apr_table_set(backend->r->headers_in, "Transfer-Encoding", tmp);
+                }
+                else if ((tmp = apr_table_get(r->headers_out, "Content-Length"))) {
+                    apr_table_set(backend->r->headers_in, "Content-Length", tmp);
+                }
+                else if (te) {
+                    apr_table_set(backend->r->headers_in, "Transfer-Encoding", te);
+                }
                 ap_discard_request_body(backend->r);
             }
             return proxy_status;
