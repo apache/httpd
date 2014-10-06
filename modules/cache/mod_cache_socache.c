49fd895b9e95c453bbbd5a87ddf7658827684b56
Rainer Jung <rjung@apache.org>
2014-10-06 13:39:30 +0000

    Silence build warning about missing prototype. Followup to r1629507.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1629652 13f79535-47bb-0310-9956-ffa450edef68

--- modules/cache/mod_cache_socache.c
+++ modules/cache/mod_cache_socache.c
@@ -1422,7 +1422,7 @@ static int socache_status_hook(request_rec *r, int flags)
     return OK;
 }
 
-void socache_status_register(apr_pool_t *p)
+static void socache_status_register(apr_pool_t *p)
 {
     APR_OPTIONAL_HOOK(ap, status_hook, socache_status_hook, NULL, NULL, APR_HOOK_MIDDLE);
 }
