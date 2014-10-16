3f9687a4559b52615fafc65b146c5cdc88cc0afb
Christophe Jaillet <jailletc36@apache.org>
2014-10-16 21:33:54 +0000

    Merge 1621373 from trunk:
    mod_slotmem: Increase log level for some originally debug messages.
    
    Submitted by: jim
    Reviewed by: jim, ylavic, rjung
    Backported by: jailletc36
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1632437 13f79535-47bb-0310-9956-ffa450edef68

--- modules/slotmem/mod_slotmem_shm.c
+++ modules/slotmem/mod_slotmem_shm.c
@@ -213,19 +213,19 @@ static apr_status_t restore_slotmem(void *ptr, const char *name, apr_size_t size
                         rv = APR_SUCCESS;
                         apr_md5(digest2, ptr, nbytes);
                         if (memcmp(digest, digest2, APR_MD5_DIGESTSIZE)) {
-                            ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, ap_server_conf,
+                            ap_log_error(APLOG_MARK, APLOG_ERR, 0, ap_server_conf,
                                          APLOGNO(02551) "bad md5 match");
                             rv = APR_EGENERAL;
                         }
                     }
                 }
                 else {
-                    ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, ap_server_conf,
+                    ap_log_error(APLOG_MARK, APLOG_NOTICE, 0, ap_server_conf,
                                  APLOGNO(02552) "at EOF... bypassing md5 match check (old persist file?)");
                 }
             }
             else if (nbytes != size) {
-                ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, ap_server_conf,
+                ap_log_error(APLOG_MARK, APLOG_ERR, 0, ap_server_conf,
                              APLOGNO(02553) "Expected %" APR_SIZE_T_FMT ": Read %" APR_SIZE_T_FMT,
                              size, nbytes);
                 rv = APR_EGENERAL;
