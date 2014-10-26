6c624d794e5b88546f47e0fd19bd85080d1b4fe5
braoult <braoult@gmail.com>
2014-10-26 10:25:28 +0100

    mod_autoindex: add CSS class name when Description is empty.

--- modules/generators/mod_autoindex.c
+++ modules/generators/mod_autoindex.c
@@ -1782,7 +1782,7 @@ static void output_directories(struct ent **ar, int n,
                     }
                 }
                 else {
-                    ap_rputs("</td><td>&nbsp;", r);
+                    ap_rvputs(r, "</td><td", (d->style_sheet != NULL) ? " class=\"indexcoldesc\">" : ">", "&nbsp;", NULL);
                 }
             }
             ap_rputs("</td></tr>\n", r);
