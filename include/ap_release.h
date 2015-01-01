7857db7aae2ab9c6db4c8046f63f968a65037e33
Rainer Jung <rjung@apache.org>
2015-01-01 13:55:29 +0000

    Happy New Year 2015
    Backport of r1648840from trunk.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1648845 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_release.h
+++ include/ap_release.h
@@ -23,7 +23,7 @@
 #define AP_RELEASE_H
 
 #define AP_SERVER_COPYRIGHT \
-  "Copyright 2014 The Apache Software Foundation."
+  "Copyright 2015 The Apache Software Foundation."
 
 /*
  * The below defines the base string of the Server: header. Additional
