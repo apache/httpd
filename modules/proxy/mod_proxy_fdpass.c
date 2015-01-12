3657f4dcd9ff048f24428caa76b5c62e4c2d48c5
Jim Jagielski <jim@apache.org>
2015-01-12 13:36:17 +0000

    Merge r1598946, r1602989 from trunk:
    Fix computation of the size of 'struct sockaddr_un' when passed to 'connect()'.
    Use the same logic as the one in ' in 'proxy_util.c'.
    
    
    mod_proxy: Don't limit the size of the connectable Unix Domain Socket paths.
    Since connect() to UDS path is used at several places, introduce
    ap_proxy_connect_uds() in proxy_util.
    
    Submitted by: jailletc36, ylavic
    Reviewed/backported by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1651081 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_fdpass.c
+++ modules/proxy/mod_proxy_fdpass.c
@@ -24,12 +24,6 @@
 #error This module only works on unix platforms with the correct OS support
 #endif
 
-#include "apr_version.h"
-#if APR_MAJOR_VERSION < 2
-/* for apr_wait_for_io_or_timeout */
-#include "apr_support.h"
-#endif
-
 #include "mod_proxy_fdpass.h"
 
 module AP_MODULE_DECLARE_DATA proxy_fdpass_module;
@@ -54,54 +48,10 @@ static int proxy_fdpass_canon(request_rec *r, char *url)
     return OK;
 }
 
-/* TODO: In APR 2.x: Extend apr_sockaddr_t to possibly be a path !!! */
-static apr_status_t socket_connect_un(apr_socket_t *sock,
-                                      struct sockaddr_un *sa)
-{
-    apr_status_t rv;
-    apr_os_sock_t rawsock;
-    apr_interval_time_t t;
-
-    rv = apr_os_sock_get(&rawsock, sock);
-    if (rv != APR_SUCCESS) {
-        return rv;
-    }
-
-    rv = apr_socket_timeout_get(sock, &t);
-    if (rv != APR_SUCCESS) {
-        return rv;
-    }
-
-    do {
-        rv = connect(rawsock, (struct sockaddr*)sa,
-                               sizeof(*sa) + strlen(sa->sun_path));
-    } while (rv == -1 && errno == EINTR);
-
-    if ((rv == -1) && (errno == EINPROGRESS || errno == EALREADY)
-        && (t > 0)) {
-#if APR_MAJOR_VERSION < 2
-        rv = apr_wait_for_io_or_timeout(NULL, sock, 0);
-#else
-        rv = apr_socket_wait(sock, APR_WAIT_WRITE);
-#endif
-
-        if (rv != APR_SUCCESS) {
-            return rv;
-        }
-    }
-
-    if (rv == -1 && errno != EISCONN) {
-        return errno;
-    }
-
-    return APR_SUCCESS;
-}
-
 static apr_status_t get_socket_from_path(apr_pool_t *p,
                                          const char* path,
                                          apr_socket_t **out_sock)
 {
-    struct sockaddr_un sa;
     apr_socket_t *s;
     apr_status_t rv;
     *out_sock = NULL;
@@ -112,10 +62,7 @@ static apr_status_t get_socket_from_path(apr_pool_t *p,
         return rv;
     }
 
-    sa.sun_family = AF_UNIX;
-    apr_cpystrn(sa.sun_path, path, sizeof(sa.sun_path));
-
-    rv = socket_connect_un(s, &sa);
+    rv = ap_proxy_connect_uds(s, path, p);
     if (rv != APR_SUCCESS) {
         return rv;
     }
