0158d4158dd2a53b83bf0c32e71c0c396c9b6a08
Rainer Jung <rjung@apache.org>
2015-02-24 18:34:01 +0000

    Merge r1661067 from trunk:
    mod_proxy_ajp: Forward SSL protocol name (SSLv3, TLSv1.1 etc.) as a
    request attribute to the backend. The name is taken from
    the SSL_PROTOCOL env var. Recent Tomcat versions will extract
    it and provide it as a servlet request attribute named
    "org.apache.tomcat.util.net.secure_protocol_version". 
    
    The change is compatible with existing AJP13, because we forward
    the protocol name as a normal named attribute, not with a new
    byte abbreviated attribute name.
    
    Submitted by: rjung
    Reviewed by: rjung, ylavic, covener
    Backported by: rjung
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1662076 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/ajp_header.c
+++ modules/proxy/ajp_header.c
@@ -415,6 +415,26 @@ static apr_status_t ajp_marshal_into_msgb(ajp_msg_t *msg,
             return AJP_EOVERFLOW;
         }
     }
+    /* Forward the SSL protocol name.
+     * Modern Tomcat versions know how to retrieve
+     * the protocol name from this attribute.
+     */
+    if (is_ssl) {
+        if ((envvar = ap_proxy_ssl_val(r->pool, r->server, r->connection, r,
+                                       AJP13_SSL_PROTOCOL_INDICATOR))
+            && envvar[0]) {
+            const char *key = SC_A_SSL_PROTOCOL;
+            if (ajp_msg_append_uint8(msg, SC_A_REQ_ATTRIBUTE) ||
+                ajp_msg_append_string(msg, key)   ||
+                ajp_msg_append_string(msg, envvar)) {
+                ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(02830)
+                        "ajp_marshal_into_msgb: "
+                        "Error appending attribute %s=%s",
+                        key, envvar);
+                return AJP_EOVERFLOW;
+            }
+        }
+    }
     /* Forward the remote port information, which was forgotten
      * from the builtin data of the AJP 13 protocol.
      * Since the servlet spec allows to retrieve it via getRemotePort(),
