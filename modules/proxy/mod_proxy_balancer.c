70201cd1499192b8bd75568f5be9bcd5faaee4cb
Ruediger Pluem <rpluem@apache.org>
2015-03-04 20:16:34 +0000

    * Fix typo in error message
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1664133 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_balancer.c
+++ modules/proxy/mod_proxy_balancer.c
@@ -657,7 +657,7 @@ static int proxy_balancer_post_request(proxy_worker *worker,
         && (apr_table_get(r->notes, "proxy_timedout")) != NULL) {
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(02460)
                       "%s: Forcing worker (%s) into error state "
-                      "due to timeout and 'failonstatus' parameter being set",
+                      "due to timeout and 'failontimeout' parameter being set",
                        balancer->s->name, ap_proxy_worker_name(r->pool, worker));
         worker->s->status |= PROXY_WORKER_IN_ERROR;
         worker->s->error_time = apr_time_now();
