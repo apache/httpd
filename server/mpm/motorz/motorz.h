ef79b293a6b89eacec912af9b0cc996de8eb12bb
Yann Ylavic <ylavic@apache.org>
2015-03-09 23:11:45 +0000

    motorz:
    Fix access to timer's pool from different threads (timer_event_process).
    Set current_thread before calling timer callback.
    
    Reuse timers by using spare_timers from motorz core (acquire/release_timer).
    
    Use KeepAliveTimeout for CONN_STATE_CHECK_REQUEST_LINE_READABLE.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1665381 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/motorz/motorz.h
+++ server/mpm/motorz/motorz.h
@@ -109,6 +109,9 @@
 /**
  * typedefs
  */
+struct motorz_timer_t;
+typedef struct motorz_timer_t motorz_timer_t;
+
 /* data retained by prefork across load/unload of the module
  * allocated on first call to pre-config hook; located on
  * subsequent calls to pre-config hook
@@ -131,6 +134,7 @@ struct motorz_core_t {
     apr_pollset_t *pollset;
     apr_skiplist *timer_ring;
     apr_thread_pool_t *workers;
+    motorz_timer_t *spare_timers;
 };
 
 typedef struct motorz_child_bucket motorz_child_bucket;
@@ -161,7 +165,6 @@ typedef void (*motorz_io_file_cb) (motorz_core_t *mz, apr_socket_t *sock,
                                    int flags, void *baton);
 
 
-typedef struct motorz_timer_t motorz_timer_t;
 struct motorz_timer_t
 {
     apr_time_t expires;
@@ -169,6 +172,7 @@ struct motorz_timer_t
     void *baton;
     apr_pool_t *pool;
     motorz_core_t *mz;
+    motorz_timer_t *next;
 };
 
 typedef struct motorz_conn_t motorz_conn_t;
