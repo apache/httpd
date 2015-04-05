c556b481ffb2e42babd0c1ca02af3d94a60f71cb
Haw Loeung <h.loeung@unixque.com>
2015-04-05 17:07:12 +1000

    modules/http/http_etag.c: Fixed missing semi-colon.

--- modules/http/http_etag.c
+++ modules/http/http_etag.c
@@ -158,7 +158,7 @@ AP_DECLARE(char *) ap_make_etag(request_rec *r, int force_weak)
                               "file permissions deny server access: %s", r->filename);
             } else {
                 apr_cpystrn(next, ap_md5digest(r->pool, fd), APR_MD5_DIGESTSIZE);
-                next += APR_MD5_DIGESTSIZE
+                next += APR_MD5_DIGESTSIZE;
             }
         }
         *next++ = '"';
