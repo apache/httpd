d2a0562f12b7c6c532fd1af9fda833d6e0ff56f4
Jan Kaluža <jkaluza@apache.org>
2015-04-17 07:28:50 +0000

    * mod_dav_fs: set default value of DavLockDB using installation layout
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1674222 13f79535-47bb-0310-9956-ffa450edef68

--- modules/dav/fs/mod_dav_fs.c
+++ modules/dav/fs/mod_dav_fs.c
@@ -17,6 +17,7 @@
 #include "httpd.h"
 #include "http_config.h"
 #include "apr_strings.h"
+#include "ap_config_auto.h"
 
 #include "mod_dav.h"
 #include "repos.h"
@@ -39,7 +40,15 @@ const char *dav_get_lockdb_path(const request_rec *r)
 
 static void *dav_fs_create_server_config(apr_pool_t *p, server_rec *s)
 {
-    return apr_pcalloc(p, sizeof(dav_fs_server_conf));
+    dav_fs_server_conf *conf = apr_pcalloc(p, sizeof(dav_fs_server_conf));
+#ifdef DEFAULT_EXP_DAVLOCKDB
+    conf->lockdb_path = DEFAULT_EXP_DAVLOCKDB;
+    if (*conf->lockdb_path == '\0') {
+        conf->lockdb_path = NULL;
+    }
+#endif
+
+    return conf;
 }
 
 static void *dav_fs_merge_server_config(apr_pool_t *p,
