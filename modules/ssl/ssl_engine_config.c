da368a8fe9effef1cdf212b33693a2ea750e94e9
Jim Jagielski <jim@apache.org>
2015-04-19 18:00:08 +0000

    Merge r1520445, r1672985, r1672989, r1673769 from trunk:
    Add a warning if protocol given in SSLProtocol  or SSLProxyProtocol will override other parameters given in the same directive.
    This could be a missing + or - prefix.
    
    PR 52820
    
    Tweak log message
    
    Add CHANGES entry before backport proposal
    
    Follow-up to r1520445:
    
    Tweak error message for clarity
    
    Submitted by: jailletc36, trawick
    Reviewed/backported by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1674655 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_config.c
+++ modules/ssl/ssl_engine_config.c
@@ -1360,8 +1360,7 @@ static const char *ssl_cmd_protocol_parse(cmd_parms *parms,
         else {
             return apr_pstrcat(parms->temp_pool,
                                parms->cmd->name,
-                               ": Illegal protocol '",
-                               w, "'", NULL);
+                               ": Illegal protocol '", w, "'", NULL);
         }
 
         if (action == '-') {
@@ -1371,6 +1370,12 @@ static const char *ssl_cmd_protocol_parse(cmd_parms *parms,
             *options |= thisopt;
         }
         else {
+            if (*options != SSL_PROTOCOL_NONE) {
+                ap_log_error(APLOG_MARK, APLOG_WARNING, 0, parms->server, APLOGNO(02532)
+                             "%s: Protocol '%s' overrides already set parameter(s). "
+                             "Check if a +/- prefix is missing.",
+                             parms->cmd->name, w);
+            }
             *options = thisopt;
         }
     }
