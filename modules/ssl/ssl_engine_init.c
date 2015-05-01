a2a2bb17eedc48e7097a7dfc996a6bb80f1de2e4
Stefan Sperling <stsp@apache.org>
2015-05-01 14:15:22 +0000

    mod_ssl namespacing: SSL_X509_getBC -> modssl_X509_getBC
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1677146 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_init.c
+++ modules/ssl/ssl_engine_init.c
@@ -940,7 +940,7 @@ static void ssl_check_public_cert(server_rec *s,
      * Some information about the certificate(s)
      */
 
-    if (SSL_X509_getBC(cert, &is_ca, &pathlen)) {
+    if (modssl_X509_getBC(cert, &is_ca, &pathlen)) {
         if (is_ca) {
             ap_log_error(APLOG_MARK, APLOG_WARNING, 0, s, APLOGNO(01906)
                          "%s server certificate is a CA certificate "
