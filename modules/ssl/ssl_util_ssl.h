783a2cce2e5655509fe5d530774f3e4a5052b21d
Stefan Sperling <stsp@apache.org>
2015-05-01 14:40:01 +0000

    mod_ssl namespacing: Make SSL_X509_getIDs a static function inside the file ssl_util_ssl.c (no outside callers). Rename to just getIDs().
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1677155 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_util_ssl.h
+++ modules/ssl/ssl_util_ssl.h
@@ -66,7 +66,6 @@ BOOL        modssl_X509_getBC(X509 *, int *, int *);
 char       *modssl_X509_NAME_ENTRY_to_string(apr_pool_t *p, X509_NAME_ENTRY *xsne);
 char       *modssl_X509_NAME_to_string(apr_pool_t *, X509_NAME *, int);
 BOOL        modssl_X509_getSAN(apr_pool_t *, X509 *, int, int, apr_array_header_t **);
-BOOL        SSL_X509_getIDs(apr_pool_t *, X509 *, apr_array_header_t **);
 BOOL        SSL_X509_match_name(apr_pool_t *, X509 *, const char *, BOOL, server_rec *);
 BOOL        SSL_X509_INFO_load_file(apr_pool_t *, STACK_OF(X509_INFO) *, const char *);
 BOOL        SSL_X509_INFO_load_path(apr_pool_t *, STACK_OF(X509_INFO) *, const char *);
