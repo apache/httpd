1f3722a25eb03ba9c5958db3e97d8f293908e9bf
Yann Ylavic <ylavic@apache.org>
2015-05-21 16:47:38 +0000

    Merge r1664205 from trunk.
    r1664205 | covener | 2015-03-05 03:33:16 +0100 (Thu, 05 Mar 2015) | 12 lines
    
      *) SECURITY: CVE-2015-0253 (cve.mitre.org)
         core: Fix a crash introduced in with ErrorDocument 400 pointing
         to a local URL-path with the INCLUDES filter active, introduced
         in 2.4.11. PR 57531. [Yann Ylavic]
    
    
    Submitted By: ylavic
    Committed By: covener
    
    
    Reviewed by: ylavic, wrowe, rjung
    Backported by: ylavic
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1680927 13f79535-47bb-0310-9956-ffa450edef68

--- server/protocol.c
+++ server/protocol.c
@@ -605,12 +605,12 @@ static int read_request_line(request_rec *r, apr_bucket_brigade *bb)
              */
             if (rv == APR_ENOSPC) {
                 r->status    = HTTP_REQUEST_URI_TOO_LARGE;
-                r->proto_num = HTTP_VERSION(1,0);
-                r->protocol  = apr_pstrdup(r->pool, "HTTP/1.0");
             }
             else if (APR_STATUS_IS_TIMEUP(rv)) {
                 r->status = HTTP_REQUEST_TIME_OUT;
             }
+            r->proto_num = HTTP_VERSION(1,0);
+            r->protocol  = apr_pstrdup(r->pool, "HTTP/1.0");
             return 0;
         }
     } while ((len <= 0) && (++num_blank_lines < max_blank_lines));
