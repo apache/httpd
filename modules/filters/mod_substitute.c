a691e368a365afae80dc0e22390d0e0f1603bae4
Nick Kew <niq@apache.org>
2015-06-11 14:22:21 +0000

    mod_substitute: Fix configuraton merge order. PR 57641 [Marc.Stern]
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1684900 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/mod_substitute.c
+++ modules/filters/mod_substitute.c
@@ -84,8 +84,8 @@ static void *merge_substitute_dcfg(apr_pool_t *p, void *basev, void *overv)
     subst_dir_conf *base = (subst_dir_conf *) basev;
     subst_dir_conf *over = (subst_dir_conf *) overv;
 
-    a->patterns = apr_array_append(p, over->patterns,
-                                                  base->patterns);
+    a->patterns = apr_array_append(p, base->patterns,
+                                                  over->patterns);
     a->max_line_length = over->max_line_length_set ?
                          over->max_line_length : base->max_line_length;
     a->max_line_length_set = over->max_line_length_set ?
