8f27aa0c92fc44a8a07237a45ce753ff2831bacb
Jim Jagielski <jim@apache.org>
2015-06-16 17:20:26 +0000

    Merge r1685659 from trunk:
    Fix directive name in error message + fix some style issue
    Submitted by: jailletc36
    Reviewed/backported by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1685869 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ldap/util_ldap.c
+++ modules/ldap/util_ldap.c
@@ -2717,8 +2717,8 @@ static const char *util_ldap_set_op_timeout(cmd_parms *cmd,
 }
 
 static const char *util_ldap_set_conn_ttl(cmd_parms *cmd,
-                                            void *dummy,
-                                            const char *val)
+                                          void *dummy,
+                                          const char *val)
 {
     apr_interval_time_t timeout;
     util_ldap_state_t *st =
@@ -2726,19 +2726,20 @@ static const char *util_ldap_set_conn_ttl(cmd_parms *cmd,
                                                   &ldap_module);
 
     if (ap_timeout_parameter_parse(val, &timeout, "s") != APR_SUCCESS) {
-        return "LDAPConnPoolTTL has wrong format";
+        return "LDAPConnectionPoolTTL has wrong format";
     }
 
     if (timeout < 0) {
         /* reserve -1 for default value */
-        timeout =  AP_LDAP_CONNPOOL_INFINITE;
+        timeout = AP_LDAP_CONNPOOL_INFINITE;
     }
     st->connection_pool_ttl = timeout;
     return NULL;
 }
+
 static const char *util_ldap_set_retry_delay(cmd_parms *cmd,
-                                            void *dummy,
-                                            const char *val)
+                                             void *dummy,
+                                             const char *val)
 {
     apr_interval_time_t timeout;
     util_ldap_state_t *st =
@@ -2763,8 +2764,8 @@ static const char *util_ldap_set_retry_delay(cmd_parms *cmd,
 }
 
 static const char *util_ldap_set_retries(cmd_parms *cmd,
-                                            void *dummy,
-                                            const char *val)
+                                         void *dummy,
+                                         const char *val)
 {
     util_ldap_state_t *st =
         (util_ldap_state_t *)ap_get_module_config(cmd->server->module_config,
