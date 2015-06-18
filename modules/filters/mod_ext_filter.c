0d946f386b98e16f61430df237606cdf8015e7ae
Jim Jagielski <jim@apache.org>
2015-06-18 17:05:28 +0000

    Merge r1686085 from trunk:
    *) mod_ext_filter, mod_charset_lite: Avoid inadvertent filtering of protocol
       data during read of chunked request bodies. PR 58049. 
       [Edward Lu <Chaosed0 gmail.com>]
    
    Submitted By: Edward Lu <Chaosed0 gmail.com>
    
    
    Submitted by: covener
    Reviewed/backported by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1686272 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/mod_ext_filter.c
+++ modules/filters/mod_ext_filter.c
@@ -891,6 +891,11 @@ static apr_status_t ef_input_filter(ap_filter_t *f, apr_bucket_brigade *bb,
     ef_ctx_t *ctx = f->ctx;
     apr_status_t rv;
 
+    /* just get out of the way of things we don't want. */
+    if (mode != AP_MODE_READBYTES) {
+        return ap_get_brigade(f->next, bb, mode, block, readbytes);
+    }
+
     if (!ctx) {
         if ((rv = init_filter_instance(f)) != APR_SUCCESS) {
             ctx = f->ctx;
