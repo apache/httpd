8514ff11619a8be1b4979fc3bfc77a041e3b2b92
William A. Rowe Jr <wrowe@apache.org>
2015-06-30 01:29:43 +0000

    Optimize in all cases that the members are all explicitly initialized.
    Useful for 2.2 and 2.4, but trunk will require the subsequent patch.
    
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1688340 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/mod_substitute.c
+++ modules/filters/mod_substitute.c
@@ -71,7 +71,7 @@ typedef struct {
 static void *create_substitute_dcfg(apr_pool_t *p, char *d)
 {
     subst_dir_conf *dcfg =
-    (subst_dir_conf *) apr_pcalloc(p, sizeof(subst_dir_conf));
+        (subst_dir_conf *) apr_palloc(p, sizeof(subst_dir_conf));
 
     dcfg->patterns = apr_array_make(p, 10, sizeof(subst_pattern_t));
     dcfg->max_line_length = AP_SUBST_MAX_LINE_LENGTH;
@@ -82,7 +82,7 @@ static void *create_substitute_dcfg(apr_pool_t *p, char *d)
 static void *merge_substitute_dcfg(apr_pool_t *p, void *basev, void *overv)
 {
     subst_dir_conf *a =
-    (subst_dir_conf *) apr_pcalloc(p, sizeof(subst_dir_conf));
+        (subst_dir_conf *) apr_palloc(p, sizeof(subst_dir_conf));
     subst_dir_conf *base = (subst_dir_conf *) basev;
     subst_dir_conf *over = (subst_dir_conf *) overv;
 
