54bf3a248e3d0fba5fce2e6afcf1f4dfda6774a4
Yann Ylavic <ylavic@apache.org>
2015-07-01 14:37:00 +0000

    mod_authz_dbd: Avoid a crash when lacking correct DB access permissions. PR 57868.
    Submitted by: Jose Kahan <jose w3.org>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1688660 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_authz_dbd.c
+++ modules/aaa/mod_authz_dbd.c
@@ -126,6 +126,13 @@ static int authz_dbd_login(request_rec *r, authz_dbd_cfg *cfg,
                       "No query configured for %s!", action);
         return HTTP_INTERNAL_SERVER_ERROR;
     }
+    if (dbd == NULL) {
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(02902)
+                      "No db handle available for %s! "
+                      "Check your database access",
+                      action);
+        return HTTP_INTERNAL_SERVER_ERROR;
+    }
     query = apr_hash_get(dbd->prepared, cfg->query, APR_HASH_KEY_STRING);
     if (query == NULL) {
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(01643)
@@ -210,6 +217,12 @@ static int authz_dbd_group_query(request_rec *r, authz_dbd_cfg *cfg,
     if (cfg->query == NULL) {
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(01649)
                       "No query configured for dbd-group!");
+        return HTTP_INTERNAL_SERVER_ERROR;
+    }
+    if (dbd == NULL) {
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(02903)
+                      "No db handle available for dbd-query! "
+                      "Check your database access");
         return HTTP_INTERNAL_SERVER_ERROR;
     }
     query = apr_hash_get(dbd->prepared, cfg->query, APR_HASH_KEY_STRING);
