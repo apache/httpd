2e211ad4a87462bfbdbb15c72606c444284f29ed
Eric Covener <covener@apache.org>
2015-08-03 15:29:42 +0000

    c89
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1693918 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -3808,6 +3808,7 @@ static const char *set_protocols(cmd_parms *cmd, void *dummy,
 {
     core_server_config *conf =
     ap_get_core_module_config(cmd->server->module_config);
+    const char **np;
     const char *err = ap_check_cmd_context(cmd, NOT_IN_DIR_LOC_FILE);
 
     if (err) {
@@ -3815,7 +3816,7 @@ static const char *set_protocols(cmd_parms *cmd, void *dummy,
     }
     
     /* Should we check for some ALPN valid char sequence here? */
-    const char **np = (const char **)apr_array_push(conf->protocols);
+    np = (const char **)apr_array_push(conf->protocols);
     *np = arg;
 
     return NULL;
