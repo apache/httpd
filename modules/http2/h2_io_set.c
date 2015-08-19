515d9ca6a8107a37f8d02ad504494d6a90681986
Jean-Frederic Clere <jfclere@apache.org>
2015-08-19 11:46:23 +0000

    keep the C89-ers happy. Submitted by Norm (normw@gknw.net).
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1696565 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http2/h2_io_set.c
+++ modules/http2/h2_io_set.c
@@ -67,12 +67,13 @@ h2_io *h2_io_set_get(h2_io_set *sp, int stream_id)
     /* we keep the array sorted by id, so lookup can be done
      * by bsearch.
      */
+    h2_io **ps;
     h2_io key;
     h2_io *pkey = &key;
 
     memset(&key, 0, sizeof(key));
     key.id = stream_id;
-    h2_io **ps = bsearch(&pkey, sp->list->elts, sp->list->nelts, 
+    ps = bsearch(&pkey, sp->list->elts, sp->list->nelts, 
                          sp->list->elt_size, h2_stream_id_cmp);
     return ps? *ps : NULL;
 }
