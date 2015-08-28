add85e5ce62eda69551bf1d4704bd42ad0c8e592
Eric Covener <covener@apache.org>
2015-08-28 15:44:29 +0000

    On ebcdic systems, make sure E2A conversion happens last, so other resource filters looking at or adding ebcdic strings see ebcdic bytes. e.g. SSI in an EBCDIC file.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1698357 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/mod_charset_lite.c
+++ modules/filters/mod_charset_lite.c
@@ -1123,10 +1123,17 @@ static void charset_register_hooks(apr_pool_t *p)
 {
     ap_hook_fixups(find_code_page, NULL, NULL, APR_HOOK_MIDDLE);
     ap_hook_insert_filter(xlate_insert_filter, NULL, NULL, APR_HOOK_REALLY_LAST);
+#if APR_CHARSET_EBCDIC
+    ap_register_output_filter(XLATEOUT_FILTER_NAME, xlate_out_filter, NULL,
+                              AP_FTYPE_RESOURCE+1);
+    ap_register_input_filter(XLATEIN_FILTER_NAME, xlate_in_filter, NULL,
+                             AP_FTYPE_RESOURCE+1);
+#else
     ap_register_output_filter(XLATEOUT_FILTER_NAME, xlate_out_filter, NULL,
                               AP_FTYPE_RESOURCE);
     ap_register_input_filter(XLATEIN_FILTER_NAME, xlate_in_filter, NULL,
                              AP_FTYPE_RESOURCE);
+#endif
 }
 
 AP_DECLARE_MODULE(charset_lite) =
