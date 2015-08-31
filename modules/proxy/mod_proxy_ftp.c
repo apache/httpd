85082b028ded204e1582facb031adb280b9e23ef
Christophe Jaillet <jailletc36@apache.org>
2015-08-31 20:37:44 +0000

    Fix some spurious ident spotted by sparse
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1700334 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_ftp.c
+++ modules/proxy/mod_proxy_ftp.c
@@ -172,7 +172,7 @@ static int ftp_check_globbingchars(const char *path)
 {
     for ( ; *path; ++path) {
         if (*path == '\\')
-        ++path;
+            ++path;
         if (*path != '\0' && strchr(FTP_GLOBBING_CHARS, *path) != NULL)
             return TRUE;
     }
