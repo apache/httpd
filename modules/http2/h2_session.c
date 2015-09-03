fe16191d6dfb808cd979b4b66df08d252922c7de
Gregg L. Smith <gsmith@apache.org>
2015-09-03 00:30:53 +0000

    C89
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1700917 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http2/h2_session.c
+++ modules/http2/h2_session.c
@@ -259,8 +259,9 @@ static int on_begin_headers_cb(nghttp2_session *ngh2,
                                const nghttp2_frame *frame, void *userp)
 {
     /* This starts a new stream. */
+    int rv;
     (void)ngh2;
-    int rv = stream_open((h2_session *)userp, frame->hd.stream_id);
+    rv = stream_open((h2_session *)userp, frame->hd.stream_id);
     if (rv != NGHTTP2_ERR_CALLBACK_FAILURE) {
       /* on_header_cb or on_frame_recv_cb will dectect that stream
          does not exist and submit RST_STREAM. */
