a490bc24c19802387b76cb6a13f9524b46b0eef4
Christophe Jaillet <jailletc36@apache.org>
2015-09-07 05:15:48 +0000

    Revert r1697735.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1701543 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_rewrite.c
+++ modules/mappers/mod_rewrite.c
@@ -3439,7 +3439,7 @@ static const char *cmd_rewritecond(cmd_parms *cmd, void *in_dconf,
                      "RewriteCond: NoCase option for non-regex pattern '%s' "
                      "is not supported and will be ignored. (%s:%d)", a2,
                      cmd->directive->filename, cmd->directive->line_num);
-        newcond->flags &= ~(CONDFLAG_NOCASE);
+        newcond->flags &= ~CONDFLAG_NOCASE;
     }
 
     newcond->pskip = a2 - newcond->pattern;
