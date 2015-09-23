e8a93c493dd2baf8f6538c4eb3090dd3fc84ff1d
Stefan Eissing <icing@apache.org>
2015-09-23 09:42:03 +0000

    avoiding race where h2_stream is closed/reset by client while being unprocessed in ready_ios set
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1704797 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http2/h2_io.c
+++ modules/http2/h2_io.c
@@ -33,7 +33,6 @@ h2_io *h2_io_create(int id, apr_pool_t *pool, apr_bucket_alloc_t *bucket_alloc)
         io->pool = pool;
         io->bbin = NULL;
         io->bbout = apr_brigade_create(pool, bucket_alloc);
-        io->response = apr_pcalloc(pool, sizeof(h2_response));
     }
     return io;
 }
