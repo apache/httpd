2ab16b02b91f4456d176755a6623790813c09aad
Yann Ylavic <ylavic@apache.org>
2015-09-24 21:26:22 +0000

    Follow up to r1705134: don't insert the filter (twice) for sub-requests.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1705157 13f79535-47bb-0310-9956-ffa450edef68

--- modules/loggers/mod_logio.c
+++ modules/loggers/mod_logio.c
@@ -225,7 +225,7 @@ static void logio_insert_filter(request_rec * r)
 {
     logio_dirconf_t *conf = ap_get_module_config(r->per_dir_config,
                                                  &logio_module);
-    if (conf->track_ttfb) { 
+    if (conf->track_ttfb && !r->main) { 
         ap_add_output_filter(logio_ttfb_filter_name, NULL, r, r->connection);
     }
     return;
