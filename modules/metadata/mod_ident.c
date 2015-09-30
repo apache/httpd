273ecf6d0d087547be8bf71850623e9297ea5415
Christophe Jaillet <jailletc36@apache.org>
2015-09-30 08:35:35 +0000

    Fix some style issues on files that can easily be synch'ed with 2.4.x
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1705983 13f79535-47bb-0310-9956-ffa450edef68

--- modules/metadata/mod_ident.c
+++ modules/metadata/mod_ident.c
@@ -202,7 +202,7 @@ static apr_status_t rfc1413_query(apr_socket_t *sock, conn_rec *conn,
      * Note that the strchr function below checks for \012 instead of '\n'
      * this allows it to work on both ASCII and EBCDIC machines.
      */
-    while((cp = strchr(buffer, '\012')) == NULL && i < sizeof(buffer) - 1) {
+    while ((cp = strchr(buffer, '\012')) == NULL && i < sizeof(buffer) - 1) {
         apr_size_t j = sizeof(buffer) - 1 - i;
         apr_status_t status;
         status = apr_socket_recv(sock, buffer+i, &j);
