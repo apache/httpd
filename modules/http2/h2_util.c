a141e0d803f869a3cb9d099b3c0e2ec799ac25b0
Stefan Eissing <icing@apache.org>
2015-10-08 11:24:11 +0000

    improved fix for EOS checking on zero length read, bump to 1.0.0
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1707497 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http2/h2_util.c
+++ modules/http2/h2_util.c
@@ -539,14 +539,36 @@ apr_status_t h2_util_bb_avail(apr_bucket_brigade *bb,
                               apr_size_t *plen, int *peos)
 {
     apr_status_t status;
-    /* test read to determine available length */
     apr_off_t blen = 0;
-    status = apr_brigade_length(bb, 0, &blen);
-    if (blen < (apr_off_t)*plen) {
-        *plen = blen;
+
+    /* test read to determine available length */
+    status = apr_brigade_length(bb, 1, &blen);
+    if (status != APR_SUCCESS) {
+        return status;
     }
-    *peos = h2_util_has_eos(bb, *plen);
-    return status;
+    else if (blen == 0) {
+        /* empty brigade, does it have an EOS bucket somwhere? */
+        *plen = 0;
+        *peos = h2_util_has_eos(bb, 0);
+    }
+    else if (blen > 0) {
+        /* data in the brigade, limit the length returned. Check for EOS
+         * bucket only if we indicate data. This is required since plen == 0
+         * means "the whole brigade" for h2_util_hash_eos()
+         */
+        if (blen < (apr_off_t)*plen) {
+            *plen = blen;
+        }
+        *peos = (*plen > 0)? h2_util_has_eos(bb, *plen) : 0;
+    }
+    else if (blen < 0) {
+        /* famous SHOULD NOT HAPPEN, sinc we told apr_brigade_length to readall
+         */
+        *plen = 0;
+        *peos = h2_util_has_eos(bb, 0);
+        return APR_EINVAL;
+    }
+    return APR_SUCCESS;
 }
 
 apr_status_t h2_util_bb_readx(apr_bucket_brigade *bb, 
