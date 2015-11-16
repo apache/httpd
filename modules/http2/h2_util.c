b11e0a5285c18daf04db0731d3db44310d831235
Stefan Eissing <icing@apache.org>
2015-11-16 11:23:38 +0000

    fixing h2c scheme/authority initialization
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1714557 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http2/h2_util.c
+++ modules/http2/h2_util.c
@@ -867,6 +867,10 @@ h2_ngheader *h2_util_ngheader_make_req(apr_pool_t *p,
     size_t n;
     
     AP_DEBUG_ASSERT(req);
+    AP_DEBUG_ASSERT(req->scheme);
+    AP_DEBUG_ASSERT(req->authority);
+    AP_DEBUG_ASSERT(req->path);
+    AP_DEBUG_ASSERT(req->method);
 
     n = 4;
     apr_table_do(count_header, &n, req->headers, NULL);
