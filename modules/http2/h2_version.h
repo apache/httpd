0d56968e7de99ade6303d09b3952ba034605ce3a
Stefan Eissing <icing@apache.org>
2015-12-07 11:51:17 +0000

    mod_http2: version bump, merge of authority default port handling for upgraded requests
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1718318 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http2/h2_version.h
+++ modules/http2/h2_version.h
@@ -20,7 +20,7 @@
  * @macro
  * Version number of the h2 module as c string
  */
-#define MOD_HTTP2_VERSION "1.0.10"
+#define MOD_HTTP2_VERSION "1.0.11-DEV"
 
 /**
  * @macro
@@ -28,7 +28,7 @@
  * release. This is a 24 bit number with 8 bits for major number, 8 bits
  * for minor and 8 bits for patch. Version 1.2.3 becomes 0x010203.
  */
-#define MOD_HTTP2_VERSION_NUM 0x01000a
+#define MOD_HTTP2_VERSION_NUM 0x01000b
 
 
 #endif /* mod_h2_h2_version_h */
