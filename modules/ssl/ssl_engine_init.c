51da86c0bea9c9fb5c4b73c580f1c6905d121a9c
Jan Kaluža <jkaluza@apache.org>
2015-12-15 11:50:48 +0000

    * mod_ssl: Free dhparams when getting DH params. This fixes issue when   SSLCryptoDevice does not get unregistered because of non-zero refcount   during the mod_ssl unload happening on httpd startup.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1720129 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_init.c
+++ modules/ssl/ssl_engine_init.c
@@ -1215,6 +1215,7 @@ static apr_status_t ssl_init_server_certs(server_rec *s,
         ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, s, APLOGNO(02540)
                      "Custom DH parameters (%d bits) for %s loaded from %s",
                      BN_num_bits(dhparams->p), vhost_id, certfile);
+        DH_free(dhparams);
     }
 
 #ifdef HAVE_ECC
