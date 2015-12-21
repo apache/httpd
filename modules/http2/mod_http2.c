dd2fc36267f7e6f6702cf3105549b74ae42ac06c
Stefan Eissing <icing@apache.org>
2015-12-21 15:20:20 +0000

    gack, c99 mode stroke again
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1721189 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http2/mod_http2.c
+++ modules/http2/mod_http2.c
@@ -225,7 +225,9 @@ static int h2_h2_fixups(request_rec *r)
 {
     if (r->connection->master) {
         h2_ctx *ctx = h2_ctx_rget(r);
-        for (int i = 0; ctx && i < H2_ALEN(H2_VARS); ++i) {
+        int i;
+        
+        for (i = 0; ctx && i < H2_ALEN(H2_VARS); ++i) {
             h2_var_def *vdef = &H2_VARS[i];
             if (vdef->subprocess) {
                 apr_table_setn(r->subprocess_env, vdef->name, 
