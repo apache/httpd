243adfb263ee905bc99f6b2e688cbd63434ac069
Christophe Jaillet <jailletc36@apache.org>
2015-12-22 07:42:21 +0000

    Save a few bytes in conf pool when processing 'NWSSLTrustedCerts'.
    *Untested* as I don't have the corresponding system.
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1721313 13f79535-47bb-0310-9956-ffa450edef68

--- modules/arch/netware/mod_nw_ssl.c
+++ modules/arch/netware/mod_nw_ssl.c
@@ -404,7 +404,7 @@ static int SSLize_Socket(SOCKET socketHnd, char *key, request_rec *r)
     ulFlag = SO_TLS_ENABLE;
     rcode = WSAIoctl(socketHnd, SO_TLS_SET_FLAGS, &ulFlag,
                      sizeof(unsigned long), NULL, 0, NULL, NULL, NULL);
-    if(rcode) {
+    if (rcode) {
         ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server, APLOGNO(02126)
                      "Error: %d with WSAIoctl(SO_TLS_SET_FLAGS, SO_TLS_ENABLE)",
                      WSAGetLastError());
@@ -635,7 +635,7 @@ static const char *set_trusted_certs(cmd_parms *cmd, void *dummy, char *arg)
 {
     char **ptr = (char **)apr_array_push(certlist);
 
-    *ptr = apr_pstrdup(cmd->pool, arg);
+    *ptr = arg;
     return NULL;
 }
 
