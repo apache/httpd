d743d3cc2817064b83964e5f7939c956c9f130e8
Stefan Eissing <icing@apache.org>
2016-01-04 16:26:18 +0000

    joining zombie workers thread, as suggested by yann
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1722914 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http2/h2_worker.c
+++ modules/http2/h2_worker.c
@@ -160,6 +160,11 @@ apr_status_t h2_worker_destroy(h2_worker *worker)
         apr_thread_cond_destroy(worker->io);
         worker->io = NULL;
     }
+    if (worker->thread) {
+        apr_status_t status;
+        apr_thread_join(&status, worker->thread);
+        worker->thread = NULL;
+    }
     if (worker->pool) {
         apr_pool_destroy(worker->pool);
         /* worker is gone */
