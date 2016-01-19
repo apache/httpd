41a8762166db76560adad03dda4dd7bfeebf9841
Rainer Jung <rjung@apache.org>
2016-01-19 00:27:36 +0000

    Added many log numbers to log statements that had none.
    Handled all files server/*.c.
    
    Excluded startup error messages in server/config.c
    and server/main.c.
    
    I used the coccinelle script provided by Stefan.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1725395 13f79535-47bb-0310-9956-ffa450edef68

--- server/protocol.c
+++ server/protocol.c
@@ -2039,7 +2039,7 @@ AP_DECLARE(const char *) ap_select_protocol(conn_rec *c, request_rec *r,
     
     if (APLOGcdebug(c)) {
         const char *p = apr_array_pstrcat(pool, conf->protocols, ',');
-        ap_log_cerror(APLOG_MARK, APLOG_DEBUG, 0, c, 
+        ap_log_cerror(APLOG_MARK, APLOG_DEBUG, 0, c, APLOGNO(03155) 
                       "select protocol from %s, choices=%s for server %s", 
                       p, apr_array_pstrcat(pool, choices, ','),
                       s->server_hostname);
@@ -2083,7 +2083,7 @@ AP_DECLARE(const char *) ap_select_protocol(conn_rec *c, request_rec *r,
 
         /* Select the most preferred protocol */
         if (APLOGcdebug(c)) {
-            ap_log_cerror(APLOG_MARK, APLOG_DEBUG, 0, c, 
+            ap_log_cerror(APLOG_MARK, APLOG_DEBUG, 0, c, APLOGNO(03156) 
                           "select protocol, proposals=%s preferences=%s configured=%s", 
                           apr_array_pstrcat(pool, proposals, ','),
                           apr_array_pstrcat(pool, prefs, ','),
@@ -2103,7 +2103,8 @@ AP_DECLARE(const char *) ap_select_protocol(conn_rec *c, request_rec *r,
         }
     }
     if (APLOGcdebug(c)) {
-        ap_log_cerror(APLOG_MARK, APLOG_DEBUG, 0, c, "selected protocol=%s", 
+        ap_log_cerror(APLOG_MARK, APLOG_DEBUG, 0, c, APLOGNO(03157)
+                      "selected protocol=%s", 
                       protocol? protocol : "(none)");
     }
 
