a1ca0c1f484305719fc9eba69f629be1468f79c1
Rainer Jung <rjung@apache.org>
2016-01-19 10:57:24 +0000

    Remove new APLOGNO form mod_log_debug.
    Here we log the custom log messages
    provided by the user.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1725468 13f79535-47bb-0310-9956-ffa450edef68

--- modules/loggers/mod_log_debug.c
+++ modules/loggers/mod_log_debug.c
@@ -79,12 +79,14 @@ static void do_debug_log(request_rec *r, const char *hookname)
             ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(00641)
                           "Can't evaluate message expression: %s", err);
         if (APLOGrdebug(r))
-            ap_log_rerror(APLOG_MARK, APLOG_INFO, 0, r, APLOGNO(02999)
+            /* Intentional no APLOGNO */
+            ap_log_rerror(APLOG_MARK, APLOG_INFO, 0, r,
                            "%s (%s hook, %s:%d)",
                            msg, hookname, entry->msg_expr->filename,
                            entry->msg_expr->line_number);
         else
-            ap_log_rerror(APLOG_MARK, APLOG_INFO, 0, r, APLOGNO(03000)
+            /* Intentional no APLOGNO */
+            ap_log_rerror(APLOG_MARK, APLOG_INFO, 0, r,
                           "%s", msg);
     }
 }
