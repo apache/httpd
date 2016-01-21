16e5bfb06d549dd932a98902bf9bf57c0b58bed9
Jim Jagielski <jim@apache.org>
2016-01-21 16:52:20 +0000

    Merge r1724857 from trunk:
      *) mod_proxy_fcgi: Suppress HTTP error 503 and message 01075, 
         "Error dispatching request", when the cause appears to be 
         the client closing the connection. 
    
    PR58118.  
    
    Submitted By: Tobias Adolph <adolph lrz.de>
    Committed By: covener
    
    
    Submitted by: covener
    Reviewed/backported by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1726019 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_fcgi.c
+++ modules/proxy/mod_proxy_fcgi.c
@@ -833,6 +833,16 @@ static int fcgi_do_request(apr_pool_t *p, request_rec *r,
     rv = dispatch(conn, conf, r, temp_pool, request_id,
                   &err, &bad_request, &has_responded);
     if (rv != APR_SUCCESS) {
+        /* If the client aborted the connection during retrieval or (partially)
+         * sending the response, dont't return a HTTP_SERVICE_UNAVAILABLE, since
+         * this is not a backend problem. */
+        if (r->connection->aborted) {
+            ap_log_rerror(APLOG_MARK, APLOG_TRACE1, rv, r, 
+                          "The client aborted the connection.");
+            conn->close = 1;
+            return OK;
+        }
+
         ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r, APLOGNO(01075)
                       "Error dispatching request to %s: %s%s%s",
                       server_portstr,
