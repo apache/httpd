4f48d0bb34bac8be7ecf1c5c2aa4edc65006f176
Ruediger Pluem <rpluem@apache.org>
2016-02-17 15:47:03 +0000

    * c is set to cs->c. So use it.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1730865 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/event/event.c
+++ server/mpm/event/event.c
@@ -940,7 +940,7 @@ static int start_lingering_close_nonblocking(event_conn_state_t *cs)
     conn_rec *c = cs->c;
     apr_socket_t *csd = cs->pfd.desc.s;
 
-    if (ap_prep_lingering_close(cs->c)
+    if (ap_prep_lingering_close(c)
         || c->aborted
         || ap_shutdown_conn(c, 0) != APR_SUCCESS || c->aborted
         || apr_socket_shutdown(csd, APR_SHUTDOWN_WRITE) != APR_SUCCESS) {
