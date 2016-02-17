5e04c2abb465731f4b0daafbbe8834e528ff4d02
Ruediger Pluem <rpluem@apache.org>
2016-02-17 12:37:46 +0000

    * Use correct bucket allocator for sending data to the backend
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1730823 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_wstunnel.c
+++ modules/proxy/mod_proxy_wstunnel.c
@@ -318,7 +318,7 @@ static int proxy_wstunnel_request(apr_pool_t *p, request_rec *r,
     e = apr_bucket_pool_create(buf, strlen(buf), p, c->bucket_alloc);
     APR_BRIGADE_INSERT_TAIL(header_brigade, e);
 
-    if ((rv = ap_proxy_pass_brigade(c->bucket_alloc, r, conn, backconn,
+    if ((rv = ap_proxy_pass_brigade(backconn->bucket_alloc, r, conn, backconn,
                                     header_brigade, 1)) != OK)
         return rv;
 
