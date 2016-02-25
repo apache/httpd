c70285d0c7782aeb2a5949c1a1bba099aba8f9f2
Christophe Jaillet <jailletc36@apache.org>
2016-02-25 06:51:13 +0000

    Save a few bytes in conf pool when parsing 'DefaultRuntimeDir' directive.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1732252 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -3056,7 +3056,7 @@ static const char *set_runtime_dir(cmd_parms *cmd, void *dummy, const char *arg)
     }
 
     if ((apr_filepath_merge((char**)&ap_runtime_dir, NULL,
-                            ap_server_root_relative(cmd->pool, arg),
+                            ap_server_root_relative(cmd->temp_pool, arg),
                             APR_FILEPATH_TRUENAME, cmd->pool) != APR_SUCCESS)
         || !ap_is_directory(cmd->temp_pool, ap_runtime_dir)) {
         return "DefaultRuntimeDir must be a valid directory, absolute or relative to ServerRoot";
