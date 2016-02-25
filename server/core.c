824640a6f30b1446f8857e4c8518217a49c93a18
Christophe Jaillet <jailletc36@apache.org>
2016-02-25 19:49:21 +0000

    Save a few bytes in conf pool when parsing 'DocumentRoot' directive on some OS.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1732353 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -1612,7 +1612,7 @@ static const char *set_document_root(cmd_parms *cmd, void *dummy,
     /* TODO: ap_configtestonly */
     if (apr_filepath_merge((char**)&conf->ap_document_root, NULL, arg,
                            APR_FILEPATH_TRUENAME, cmd->pool) != APR_SUCCESS
-        || !ap_is_directory(cmd->pool, arg)) {
+        || !ap_is_directory(cmd->temp_pool, arg)) {
         if (cmd->server->is_virtual) {
             ap_log_perror(APLOG_MARK, APLOG_STARTUP, 0,
                           cmd->pool, APLOGNO(00112)
