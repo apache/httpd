4151e6d215cb4e92a05055c79f74826ab754251f
Jim Jagielski <jim@apache.org>
2016-03-03 15:09:32 +0000

    Merge r1729826, r1729847, r1732986, r1733056 from trunk:
    mod_proxy: Play/restore the TLS-SNI on new backend connections which
    had to be issued because the remote closed the previous/reusable one
    during idle (keep-alive) time.
    
    
    mod_proxy: follow up to r1729826: really copy conn->ssl_hostname.
    
    mod_proxy: follow up to r1729826 + r1729847.
    Adjust stacked ssl_hostname maximum size.
    
    mod_proxy: follow up to r1729826 + r1729847 + r1732986.
    Don't use magic constants.
    Submitted by: ylavic
    Reviewed/backported by: jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1733474 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/proxy_util.c
+++ modules/proxy/proxy_util.c
@@ -2658,10 +2658,24 @@ PROXY_DECLARE(int) ap_proxy_connect_backend(const char *proxy_function,
 
     if (conn->sock) {
         if (!(connected = is_socket_connected(conn->sock))) {
+            /* This clears conn->scpool (and associated data), so backup and
+             * restore any ssl_hostname for this connection set earlier by
+             * ap_proxy_determine_connection().
+             */
+            char ssl_hostname[PROXY_WORKER_RFC1035_NAME_SIZE];
+            if (!conn->ssl_hostname || PROXY_STRNCPY(ssl_hostname,
+                                                     conn->ssl_hostname)) {
+                ssl_hostname[0] = '\0';
+            }
+
             socket_cleanup(conn);
             ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, s, APLOGNO(00951)
                          "%s: backend socket is disconnected.",
                          proxy_function);
+
+            if (ssl_hostname[0]) {
+                conn->ssl_hostname = apr_pstrdup(conn->scpool, ssl_hostname);
+            }
         }
     }
     while ((backend_addr || conn->uds_path) && !connected) {
