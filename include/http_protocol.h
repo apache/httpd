fbc383fef18248c371fced28eb5ebccc22f8b609
Stefan Eissing <icing@apache.org>
2016-03-09 12:39:04 +0000

    added AP_DECLARE for new ap_create_request
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1734231 13f79535-47bb-0310-9956-ffa450edef68

--- include/http_protocol.h
+++ include/http_protocol.h
@@ -58,7 +58,7 @@ AP_DECLARE_DATA extern ap_filter_rec_t *ap_old_write_func;
  * @param c The current connection
  * @return The new request_rec
  */
-request_rec *ap_create_request(conn_rec *c);
+AP_DECLARE(request_rec *) ap_create_request(conn_rec *c);
 
 /**
  * Read a request and fill in the fields.
