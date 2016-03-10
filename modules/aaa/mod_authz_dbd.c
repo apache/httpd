7a416b430e62af9bba0ac357c6cdd78f05c684d0
Yann Ylavic <ylavic@apache.org>
2016-03-10 12:27:49 +0000

    Merge r1733523, r1733691 from trunk:
    Use ap_array_str_contains to simplify code
    
    Use ap_array_str_contains to simplify code
    Remove a useless test (groups is always NULL)
    Improve some style
    
    Submitted by: jailletc36
    Reviewed by: jailletc36, jim, ylavic
    Backported by: ylavic
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1734395 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_authz_dbd.c
+++ modules/aaa/mod_authz_dbd.c
@@ -61,6 +61,7 @@ static void *authz_dbd_cr_cfg(apr_pool_t *pool, char *dummy)
     ret->redirect = -1;
     return ret;
 }
+
 static void *authz_dbd_merge_cfg(apr_pool_t *pool, void *BASE, void *ADD)
 {
     authz_dbd_cfg *base = BASE;
@@ -73,6 +74,7 @@ static void *authz_dbd_merge_cfg(apr_pool_t *pool, void *BASE, void *ADD)
     ret->redirect = (add->redirect == -1) ? base->redirect : add->redirect;
     return ret;
 }
+
 static const char *authz_dbd_prepare(cmd_parms *cmd, void *cfg,
                                      const char *query)
 {
@@ -96,6 +98,7 @@ static const char *authz_dbd_prepare(cmd_parms *cmd, void *cfg,
     /* save the label here for our own use */
     return ap_set_string_slot(cmd, cfg, label);
 }
+
 static const command_rec authz_dbd_cmds[] = {
     AP_INIT_FLAG("AuthzDBDLoginToReferer", ap_set_flag_slot,
                  (void*)APR_OFFSETOF(authz_dbd_cfg, redirect), ACCESS_CONF,
@@ -265,9 +268,9 @@ static authz_status dbdgroup_check_authorization(request_rec *r,
                                                  const char *require_args,
                                                  const void *parsed_require_args)
 {
-    int i, rv;
+    int rv;
     const char *w;
-    apr_array_header_t *groups = NULL;
+    apr_array_header_t *groups;
 
     const char *err = NULL;
     const ap_expr_info_t *expr = parsed_require_args;
@@ -281,12 +284,10 @@ static authz_status dbdgroup_check_authorization(request_rec *r,
         return AUTHZ_DENIED_NO_USER;
     }
 
-    if (groups == NULL) {
-        groups = apr_array_make(r->pool, 4, sizeof(const char*));
-        rv = authz_dbd_group_query(r, cfg, groups);
-        if (rv != OK) {
-            return AUTHZ_GENERAL_ERROR;
-        }
+    groups = apr_array_make(r->pool, 4, sizeof(const char*));
+    rv = authz_dbd_group_query(r, cfg, groups);
+    if (rv != OK) {
+        return AUTHZ_GENERAL_ERROR;
     }
 
     require = ap_expr_str_exec(r, expr, &err);
@@ -300,10 +301,8 @@ static authz_status dbdgroup_check_authorization(request_rec *r,
     t = require;
     while (t[0]) {
         w = ap_getword_white(r->pool, &t);
-        for (i=0; i < groups->nelts; ++i) {
-            if (!strcmp(w, ((const char**)groups->elts)[i])) {
-                return AUTHZ_GRANTED;
-            }
+        if (ap_array_str_contains(groups, w)) {
+            return AUTHZ_GRANTED;
         }
     }
 
@@ -339,18 +338,19 @@ static authz_status dbdlogout_check_authorization(request_rec *r,
 }
 
 static const char *dbd_parse_config(cmd_parms *cmd, const char *require_line,
-                                     const void **parsed_require_line)
+                                    const void **parsed_require_line)
 {
     const char *expr_err = NULL;
     ap_expr_info_t *expr;
 
     expr = ap_expr_parse_cmd(cmd, require_line, AP_EXPR_FLAG_STRING_RESULT,
-            &expr_err, NULL);
+                             &expr_err, NULL);
 
-    if (expr_err)
+    if (expr_err) {
         return apr_pstrcat(cmd->temp_pool,
                            "Cannot parse expression in require line: ",
                            expr_err, NULL);
+    }
 
     *parsed_require_line = expr;
 
@@ -368,7 +368,6 @@ static const authz_provider authz_dbdlogin_provider =
     &dbdlogin_check_authorization,
     NULL,
 };
-
 
 static const authz_provider authz_dbdlogout_provider =
 {
@@ -402,4 +401,3 @@ AP_DECLARE_MODULE(authz_dbd) =
     authz_dbd_cmds,
     authz_dbd_hooks
 };
-
