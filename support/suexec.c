9bc9b3a69c3c5b09a9a20629f19a7cc4e7ef7c45
Jeff Trawick <trawick@apache.org>
2016-03-14 16:31:36 +0000

    Follow-up to r1734817: suexec passes through mod_include's DOCUMENT_ARGS
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1734955 13f79535-47bb-0310-9956-ffa450edef68

--- support/suexec.c
+++ support/suexec.c
@@ -102,6 +102,7 @@ static const char *const safe_env_lst[] =
     "CONTEXT_PREFIX=",
     "DATE_GMT=",
     "DATE_LOCAL=",
+    "DOCUMENT_ARGS=",
     "DOCUMENT_NAME=",
     "DOCUMENT_PATH_INFO=",
     "DOCUMENT_ROOT=",
