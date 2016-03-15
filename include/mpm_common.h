99c0d03451f22170b19bdb3daf64049c432361b3
Graham Leggett <minfrin@apache.org>
2016-03-15 22:52:51 +0000

    mpm: Generalise the ap_mpm_register_socket functions to accept pipes or sockets.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1735174 13f79535-47bb-0310-9956-ffa450edef68

--- include/mpm_common.h
+++ include/mpm_common.h
@@ -426,25 +426,26 @@ AP_DECLARE_HOOK(apr_status_t, mpm_register_timed_callback,
  * register the specified callback
  * @ingroup hooks
  */
-AP_DECLARE_HOOK(apr_status_t, mpm_register_socket_callback,
-                (apr_socket_t **s, apr_pool_t *p, int for_read, ap_mpm_callback_fn_t *cbfn, void *baton))
+AP_DECLARE_HOOK(apr_status_t, mpm_register_poll_callback,
+                (apr_array_header_t *pds, ap_mpm_callback_fn_t *cbfn, void *baton))
 
 /* register the specified callback, with timeout 
  * @ingroup hooks
  *
  */
-AP_DECLARE_HOOK(apr_status_t, mpm_register_socket_callback_timeout,
-                (apr_socket_t **s, apr_pool_t *p, int for_read, 
-                 ap_mpm_callback_fn_t *cbfn,  
-                 ap_mpm_callback_fn_t *tofn, 
-                 void *baton, 
-                 apr_time_t timeout))
+AP_DECLARE_HOOK(apr_status_t, mpm_register_poll_callback_timeout,
+        (apr_array_header_t *pds,
+                ap_mpm_callback_fn_t *cbfn,
+                ap_mpm_callback_fn_t *tofn,
+                void *baton,
+                apr_time_t timeout))
+
 /**
  * Unregister the specified callback
  * @ingroup hooks
  */
-AP_DECLARE_HOOK(apr_status_t, mpm_unregister_socket_callback,
-                (apr_socket_t **s, apr_pool_t *p))
+AP_DECLARE_HOOK(apr_status_t, mpm_unregister_poll_callback,
+                (apr_array_header_t *pds))
 
 /** Resume the suspended connection 
  * @ingroup hooks
