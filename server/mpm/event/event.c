239ac0c076a48a8315ddd012f7f8055cba8cf073
Yann Ylavic <ylavic@apache.org>
2016-03-22 17:08:29 +0000

    Follow up to r1734656: restore c->data_in_input_filters usage to see if it helps unblocking test framework.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1736216 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/event/event.c
+++ server/mpm/event/event.c
@@ -1179,7 +1179,7 @@ read_request:
             listener_may_exit) {
             cs->pub.state = CONN_STATE_LINGER;
         }
-        else if (ap_run_input_pending(c) == OK) {
+        else if (c->data_in_input_filters || ap_run_input_pending(c) == OK) {
             cs->pub.state = CONN_STATE_READ_REQUEST_LINE;
             goto read_request;
         }
