c020edfebd51f2c6cbe7dc3c39a0654fea6620e4
Jim Jagielski <jim@apache.org>
2016-03-25 12:14:30 +0000

    protect logic in case backend == NULL
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1736563 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_hcheck.c
+++ modules/proxy/mod_proxy_hcheck.c
@@ -503,12 +503,12 @@ static apr_status_t backend_cleanup(const char *proxy_function, proxy_conn_rec *
     if (backend) {
         backend->close = 1;
         ap_proxy_release_connection(proxy_function, backend, s);
+        ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, s, APLOGNO(03251)
+                         "Health check %s Status (%d) for %s.",
+                         ap_proxy_show_hcmethod(backend->worker->s->method),
+                         status,
+                         backend->worker->s->name);
     }
-    ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, s, APLOGNO(03251)
-                     "Health check %s Status (%d) for %s.",
-                     ap_proxy_show_hcmethod(backend->worker->s->method),
-                     status,
-                     backend->worker->s->name);
     if (status != OK) {
         return APR_EGENERAL;
     }
