95444c5cdeb310ecf52aa0a24d887ab2fc42153e
Yann Ylavic <ylavic@apache.org>
2016-04-12 22:43:00 +0000

    mod_proxy: really return an error message on invalid "flusher" value.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1738875 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy.c
+++ modules/proxy/mod_proxy.c
@@ -303,10 +303,10 @@ static const char *set_worker_param(apr_pool_t *p,
         worker->s->conn_timeout_set = 1;
     }
     else if (!strcasecmp(key, "flusher")) {
-        if (strlen(val) >= sizeof(worker->s->flusher))
-            apr_psprintf(p, "flusher name length must be < %d characters",
-                    (int)sizeof(worker->s->flusher));
-        PROXY_STRNCPY(worker->s->flusher, val);
+        if (PROXY_STRNCPY(worker->s->flusher, val) != APR_SUCCESS) {
+            return apr_psprintf(p, "flusher name length must be < %d characters",
+                                (int)sizeof(worker->s->flusher));
+        }
     }
     else {
         if (set_worker_hc_param_f) {
