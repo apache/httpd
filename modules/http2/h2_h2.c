7f5b88b558d9d808700f5bc7abc864435060f9c9
Christophe Jaillet <jailletc36@apache.org>
2016-04-20 21:34:48 +0000

    Typo
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1740199 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http2/h2_h2.c
+++ modules/http2/h2_h2.c
@@ -86,7 +86,7 @@ const char *h2_h2_err_description(unsigned int h2_error)
     if (h2_error < (sizeof(h2_err_descr)/sizeof(h2_err_descr[0]))) {
         return h2_err_descr[h2_error];
     }
-    return "unknown http/2 errotr code";
+    return "unknown http/2 error code";
 }
 
 /*******************************************************************************
