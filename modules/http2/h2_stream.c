b87719bebc698f0c7da44cc57b0ecd101c5f0578
Stefan Eissing <icing@apache.org>
2016-05-18 14:39:33 +0000

    mod_http2: fixes timeout vs. keepalive timeout use when closing streams on flush, adding pseudo header length check against limit_req_line
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1744415 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http2/h2_stream.c
+++ modules/http2/h2_stream.c
@@ -290,39 +290,38 @@ apr_status_t h2_stream_add_header(h2_stream *stream,
                                   const char *value, size_t vlen)
 {
     AP_DEBUG_ASSERT(stream);
-    if (stream->response) {
-        /* already have the response, ignore any more request headers
-           (hint: response might already be a failure due to previous errs */
-        return APR_SUCCESS;
-    }
-    
-    if ((nlen > 0) && name[0] == ':') {
-        if ((vlen) > stream->session->s->limit_req_line) {
-            /* pseudo header: approximation of request line size check */
+    if (!stream->response) {
+        if (name[0] == ':') {
+            if ((vlen) > stream->session->s->limit_req_line) {
+                /* pseudo header: approximation of request line size check */
+                ap_log_cerror(APLOG_MARK, APLOG_TRACE1, 0, stream->session->c,
+                              "h2_stream(%ld-%d): pseudo header %s too long", 
+                              stream->session->id, stream->id, name);
+                return h2_stream_set_error(stream, 
+                                           HTTP_REQUEST_URI_TOO_LARGE);
+            }
+        }
+        else if ((nlen + 2 + vlen) > stream->session->s->limit_req_fieldsize) {
+            /* header too long */
             ap_log_cerror(APLOG_MARK, APLOG_TRACE1, 0, stream->session->c,
-                          "h2_stream(%ld-%d): pseudo header %s too long", 
+                          "h2_stream(%ld-%d): header %s too long", 
                           stream->session->id, stream->id, name);
             return h2_stream_set_error(stream, 
                                        HTTP_REQUEST_HEADER_FIELDS_TOO_LARGE);
         }
-    }
-    else if ((nlen + 2 + vlen) > stream->session->s->limit_req_fieldsize) {
-        /* header too long */
-        ap_log_cerror(APLOG_MARK, APLOG_TRACE1, 0, stream->session->c,
-                      "h2_stream(%ld-%d): header %s too long", 
-                      stream->session->id, stream->id, name);
-        return h2_stream_set_error(stream, 
-                                   HTTP_REQUEST_HEADER_FIELDS_TOO_LARGE);
-    }
-    
-    ++stream->request_headers_added;
-    if (stream->request_headers_added > stream->session->s->limit_req_fields) {
-        /* too many header lines */
-        ap_log_cerror(APLOG_MARK, APLOG_TRACE1, 0, stream->session->c,
-                      "h2_stream(%ld-%d): too many header lines", 
-                      stream->session->id, stream->id);
-        return h2_stream_set_error(stream, 
-                                   HTTP_REQUEST_HEADER_FIELDS_TOO_LARGE);
+        
+        if (name[0] != ':') {
+            ++stream->request_headers_added;
+            if (stream->request_headers_added 
+                > stream->session->s->limit_req_fields) {
+                /* too many header lines */
+                ap_log_cerror(APLOG_MARK, APLOG_TRACE1, 0, stream->session->c,
+                              "h2_stream(%ld-%d): too many header lines", 
+                              stream->session->id, stream->id);
+                return h2_stream_set_error(stream, 
+                                           HTTP_REQUEST_HEADER_FIELDS_TOO_LARGE);
+            }
+        }
     }
     
     if (h2_stream_is_scheduled(stream)) {
