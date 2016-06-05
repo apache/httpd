dbe61c6eab12ec6ada51d3c316e5162864a41394
Christophe Jaillet <jailletc36@apache.org>
2016-06-05 08:40:20 +0000

    No need to scan the first 5 bytes, they are already correct (i.e. HTTP_) Fix indentation.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1746878 13f79535-47bb-0310-9956-ffa450edef68

--- server/apreq_module_cgi.c
+++ server/apreq_module_cgi.c
@@ -261,7 +261,8 @@ static const char *cgi_header_in(apreq_handle_t *handle,
     apr_pool_t *p = handle->pool;
     char *key = apr_pstrcat(p, "HTTP_", name, NULL);
     char *k, *value = NULL;
-    for (k = key; *k; ++k) {
+
+    for (k = key+5; *k; ++k) {
         if (*k == '-')
             *k = '_';
         else
@@ -269,17 +270,15 @@ static const char *cgi_header_in(apreq_handle_t *handle,
     }
 
     if (!strcmp(key, "HTTP_CONTENT_TYPE")
-        || !strcmp(key, "HTTP_CONTENT_LENGTH"))
-        {
-            key += 5; /* strlen("HTTP_") */
-        }
+        || !strcmp(key, "HTTP_CONTENT_LENGTH")) {
+
+        key += 5; /* strlen("HTTP_") */
+    }
 
     apr_env_get(&value, key, p);
 
     return value;
 }
-
-
 
 
 static void cgi_log_error(const char *file, int line, int level,
