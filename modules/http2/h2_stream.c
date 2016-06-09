19b19a5a9660c95e05c96034f108f40b4c6cdee0
Stefan Eissing <icing@apache.org>
2016-06-09 10:28:51 +0000

    mod_http2: more orderly destruction of stream/task pairs
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1747531 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http2/h2_stream.c
+++ modules/http2/h2_stream.c
@@ -213,12 +213,12 @@ void h2_stream_cleanup(h2_stream *stream)
     }
     if (stream->input) {
         apr_status_t status;
-        status = h2_beam_shutdown(stream->input, APR_NONBLOCK_READ);
+        status = h2_beam_shutdown(stream->input, APR_NONBLOCK_READ, 1);
         if (status == APR_EAGAIN) {
             ap_log_cerror(APLOG_MARK, APLOG_TRACE2, 0, stream->session->c, 
                           "h2_stream(%ld-%d): wait on input shutdown", 
                           stream->session->id, stream->id);
-            status = h2_beam_shutdown(stream->input, APR_BLOCK_READ);
+            status = h2_beam_shutdown(stream->input, APR_BLOCK_READ, 1);
             ap_log_cerror(APLOG_MARK, APLOG_TRACE2, status, stream->session->c, 
                           "h2_stream(%ld-%d): input shutdown returned", 
                           stream->session->id, stream->id);
