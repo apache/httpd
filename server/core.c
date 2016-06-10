2d0b6b55981853b713854c08a16857a450d08687
Eric Covener <covener@apache.org>
2016-06-10 22:44:39 +0000

    Add -DDUMP_INCLUDES configtest option to show the tree of Included configuration files.
    Example:
    
        Included configuration files:
          (*) .../conf/httpd.conf
            (517) .../conf/extra/proxy-html.conf
              (91) /dev/null
    
    
    
    Submitted By: Jacob Champion <champion.pxi gmail.com>
    Committed By: covener
    
    
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1747808 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -3252,6 +3252,10 @@ static const char *include_config (cmd_parms *cmd, void *dummy,
     int optional = cmd->cmd->cmd_data ? 1 : 0;
     void *data;
 
+    /* NOTE: ap_include_sentinel is also used by ap_process_resource_config()
+     * during DUMP_INCLUDES; don't change its type or remove it without updating
+     * the other.
+     */
     apr_pool_userdata_get(&data, "ap_include_sentinel", cmd->pool);
     if (data) {
         recursion = data;
@@ -3274,6 +3278,24 @@ static const char *include_config (cmd_parms *cmd, void *dummy,
         *recursion = 0;
         return apr_pstrcat(cmd->pool, "Invalid Include path ",
                            name, NULL);
+    }
+
+    if (ap_exists_config_define("DUMP_INCLUDES")) {
+        unsigned *line_number;
+
+        /* NOTE: ap_include_lineno is used by ap_process_resource_config()
+         * during DUMP_INCLUDES; don't change its type or remove it without
+         * updating the other.
+         */
+        apr_pool_userdata_get(&data, "ap_include_lineno", cmd->pool);
+        if (data) {
+            line_number = data;
+        } else {
+            data = line_number = apr_palloc(cmd->pool, sizeof(*line_number));
+            apr_pool_userdata_setn(data, "ap_include_lineno", NULL, cmd->pool);
+        }
+
+        *line_number = cmd->config_file->line_number;
     }
 
     error = ap_process_fnmatch_configs(cmd->server, conffile, &conftree,
