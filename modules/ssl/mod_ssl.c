7871d235f1b9bb9bc210ebe43bf9db3dfb4aa4db
Rainer Jung <rjung@apache.org>
2016-08-10 11:20:00 +0000

    Remove calling ERR_remove_thread_state when using OpenSSL 1.1.0.
    This API is now a no-op in OpenSSL 1.1.0 and
    deprecated.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1755725 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/mod_ssl.c
+++ modules/ssl/mod_ssl.c
@@ -323,10 +323,12 @@ static apr_status_t ssl_cleanup_pre_config(void *data)
 #if HAVE_ENGINE_LOAD_BUILTIN_ENGINES
     ENGINE_cleanup();
 #endif
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
 #if OPENSSL_VERSION_NUMBER >= 0x1000000fL
     ERR_remove_thread_state(NULL);
 #else
     ERR_remove_state(0);
+#endif
 #endif
 
     /* Don't call ERR_free_strings in earlier versions, ERR_load_*_strings only
