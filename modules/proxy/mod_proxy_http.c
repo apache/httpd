b7767cc7295b67016b700c2d64ddd945b327132f
Yann Ylavic <ylavic@apache.org>
2016-08-11 23:37:45 +0000

    Revert r1756064 and r1756060 until fixed (tests framework passes).
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1756065 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_http.c
+++ modules/proxy/mod_proxy_http.c
@@ -2068,10 +2068,9 @@ static int proxy_http_handler(request_rec *r, proxy_worker *worker,
         }
 
         /* Step Two: Make the Connection */
-        if (ap_proxy_check_connection(proxy_function, backend, r->server, 1,
-                                      PROXY_CHECK_CONN_EMPTY)
-                && ap_proxy_connect_backend(proxy_function, backend, worker,
-                                            r->server)) {
+        if (ap_proxy_check_backend(proxy_function, backend, r->server, 1) &&
+                ap_proxy_connect_backend(proxy_function, backend, worker,
+                                         r->server)) {
             ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(01114)
                           "HTTP: failed to make connection to backend: %s",
                           backend->hostname);
