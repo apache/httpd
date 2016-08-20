d7550f7958f5395c7d9018695dc6c9ea773cddb0
Christophe Jaillet <jailletc36@apache.org>
2016-08-20 15:36:12 +0000

    Fix the number of column for 'Async connections'. There are only 3 columns (writing, keep-alive, closing), not 4.
    Try to improve the code layout for it to be more readable.
    Each <th> is on its own line so keep the corresponding "colspan" <td> fields grouped together.
    
    r1738628 introduced a new column, 'Slot'.
    Add an empty cell for it in the last line of the table, in order to fix the layout of the Totals.
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1757009 13f79535-47bb-0310-9956-ffa450edef68

--- modules/generators/mod_status.c
+++ modules/generators/mod_status.c
@@ -541,10 +541,10 @@ static int status_handler(request_rec *r)
                          "<th rowspan=\"2\">PID</th>"
                          "<th colspan=\"2\">Connections</th>\n"
                          "<th colspan=\"2\">Threads</th>"
-                         "<th colspan=\"4\">Async connections</th></tr>\n"
+                         "<th colspan=\"3\">Async connections</th></tr>\n"
                      "<tr><th>total</th><th>accepting</th>"
-                         "<th>busy</th><th>idle</th><th>writing</th>"
-                         "<th>keep-alive</th><th>closing</th></tr>\n", r);
+                         "<th>busy</th><th>idle</th>"
+                         "<th>writing</th><th>keep-alive</th><th>closing</th></tr>\n", r);
         for (i = 0; i < server_limit; ++i) {
             ps_record = ap_get_scoreboard_process(i);
             if (ps_record->pid) {
@@ -556,25 +556,29 @@ static int status_handler(request_rec *r)
                 idle_workers     += thread_idle_buffer[i];
                 if (!short_report)
                     ap_rprintf(r, "<tr><td>%u</td><td>%" APR_PID_T_FMT "</td>"
-                                      "<td>%u</td><td>%s</td><td>%u</td>"
+                                      "<td>%u</td><td>%s</td>"
+                                      "<td>%u</td><td>%u</td>"
                                       "<td>%u</td><td>%u</td><td>%u</td>"
-                                      "<td>%u</td>"
                                       "</tr>\n",
-                               i, ps_record->pid, ps_record->connections,
+                               i, ps_record->pid,
+                               ps_record->connections,
                                ps_record->not_accepting ? "no" : "yes",
-                               thread_busy_buffer[i], thread_idle_buffer[i],
+                               thread_busy_buffer[i],
+                               thread_idle_buffer[i],
                                ps_record->write_completion,
                                ps_record->keep_alive,
                                ps_record->lingering_close);
             }
         }
         if (!short_report) {
-            ap_rprintf(r, "<tr><td>Sum</td><td>%d</td><td>&nbsp;</td><td>%d</td>"
-                          "<td>%d</td><td>%d</td><td>%d</td><td>%d</td>"
+            ap_rprintf(r, "<tr><td>&nbsp;</td><td>Sum</td>"
+                          "<td>%d</td><td>&nbsp;</td>"
+                          "<td>%d</td><td>%d</td>"
+                          "<td>%d</td><td>%d</td><td>%d</td>"
                           "</tr>\n</table>\n",
-                          connections, busy_workers, idle_workers,
+                          connections,
+                          busy_workers, idle_workers,
                           write_completion, keep_alive, lingering_close);
-
         }
         else {
             ap_rprintf(r, "ConnsTotal: %d\n"
