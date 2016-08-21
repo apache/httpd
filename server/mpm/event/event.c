366bdb1c40d7408d4d0a5c941ce367b7b08465a0
Stefan Fritsch <sf@apache.org>
2016-08-21 13:06:45 +0000

    Revert "On exit, don't write to other generations' scoreboard slots"
    As we have removed the possibility to re-use used scoreboard slots in r1757031,
    we don't need this check anymore.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1757056 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm/event/event.c
+++ server/mpm/event/event.c
@@ -1258,18 +1258,12 @@ static void close_listeners(int process_slot, int *closed)
 {
     if (!*closed) {
         int i;
-        worker_score *ws;
         disable_listensocks(process_slot);
         ap_close_listeners_ex(my_bucket->listeners);
         *closed = 1;
         dying = 1;
         ap_scoreboard_image->parent[process_slot].quiescing = 1;
         for (i = 0; i < threads_per_child; ++i) {
-            ws = ap_get_scoreboard_worker_from_indexes(process_slot, i);
-            if (ws->pid != ap_my_pid) {
-                /* scoreboard slot still in use by previous generation */
-                continue;
-            }
             ap_update_child_status_from_indexes(process_slot, i,
                                                 SERVER_GRACEFUL, NULL);
         }
