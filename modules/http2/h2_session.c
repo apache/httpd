1878ca4acb1f1fa8bc674c04a28d1d655ed18848
Stefan Eissing <icing@apache.org>
2016-08-24 15:13:12 +0000

    mod_http2: give timeout goaway reason when applicable
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1757540 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http2/h2_session.c
+++ modules/http2/h2_session.c
@@ -2291,6 +2291,7 @@ apr_status_t h2_session_pre_close(h2_session *session, int async)
 {
     ap_log_cerror(APLOG_MARK, APLOG_TRACE1, 0, session->c, 
                   "h2_session(%ld): pre_close", session->id);
-    dispatch_event(session, H2_SESSION_EV_PRE_CLOSE, 0, NULL);
+    dispatch_event(session, H2_SESSION_EV_PRE_CLOSE, 0, 
+        (session->state == H2_SESSION_ST_IDLE)? "timeout" : NULL);
     return APR_SUCCESS;
 }
