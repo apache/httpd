apache/httpd

commit 5a53fac7267665609948b54bcec2f034039e4fcc

mod_proxy_fcgi: handle the HTTP 412 use case
This is a follow up of http://svn.apache.org/r1752347;
ap_meet_conditions could return a 412 status that if not
handled causes subsequent bogus reads and wrong messages
logged (like AH01070). After a chat on dev@ the feedback
was to couple HTTP_NOT_MODIFIED with HTTP_PRECONDITION_FAILED,
but any other feedback is welcome.



git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1759984 13f79535-47bb-0310-9956-ffa450edef68

modules/proxy/mod_proxy_fcgi.c
@@ -660,13 +660,17 @@ recv_again:
                                     *err = "passing headers brigade to output filters";
                                     break;
                                 }
-                                else if (status == HTTP_NOT_MODIFIED) {
-                                    /* The 304 response MUST NOT contain
-                                     * a message-body, ignore it.
+                                else if (status == HTTP_NOT_MODIFIED
+                                         || HTTP_PRECONDITION_FAILED) {
+                                    /* Special 'status' cases handled:
+                                     * 1) HTTP 304 response MUST NOT contain
+                                     *    a message-body, ignore it.
+                                     * 2) HTTP 412 response.
                                      * The break is not added since there might
                                      * be more bytes to read from the FCGI
                                      * connection. Even if the message-body is
-                                     * ignored we want to avoid subsequent
+                                     * ignored (and the EOS bucket has already
+                                     * been sent) we want to avoid subsequent
                                      * bogus reads. */
                                     ignore_body = 1;
                                 }

commit 2154c0e24430f036d7278d68aaa9d100eab34d33

Fix stupid mistake introduced in r1759984
git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1760018 13f79535-47bb-0310-9956-ffa450edef68

modules/proxy/mod_proxy_fcgi.c
@@ -661,7 +661,7 @@ recv_again:
                                     break;
                                 }
                                 else if (status == HTTP_NOT_MODIFIED
-                                         || HTTP_PRECONDITION_FAILED) {
+                                         || status == HTTP_PRECONDITION_FAILED) {
                                     /* Special 'status' cases handled:
                                      * 1) HTTP 304 response MUST NOT contain
                                      *    a message-body, ignore it.