56b999ff5391bd6082edce9bdd2e2d882fde98d5
Yann Ylavic <ylavic@apache.org>
2016-10-16 09:06:58 +0000

    Follow up to r1765115: MMN minor is now 3.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1765126 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_mmn.h
+++ include/ap_mmn.h
@@ -556,7 +556,7 @@
 #ifndef MODULE_MAGIC_NUMBER_MAJOR
 #define MODULE_MAGIC_NUMBER_MAJOR 20160629
 #endif
-#define MODULE_MAGIC_NUMBER_MINOR 2                 /* 0...n */
+#define MODULE_MAGIC_NUMBER_MINOR 3                 /* 0...n */
 
 /**
  * Determine if the server's current MODULE_MAGIC_NUMBER is at least a
