fb209dbba2dfc74ee3c4a800b574e2f67d08915e
Christophe Jaillet <jailletc36@apache.org>
2017-01-06 11:58:23 +0000

    Fix some tiny style issues (missing space)
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1777594 13f79535-47bb-0310-9956-ffa450edef68

--- server/vhost.c
+++ server/vhost.c
@@ -907,7 +907,7 @@ static int matches_aliases(server_rec *s, const char *host)
     if (names) {
         char **name = (char **) names->elts;
         for (i = 0; i < names->nelts; ++i) {
-            if(!name[i]) continue;
+            if (!name[i]) continue;
             if (!strcasecmp(host, name[i]))
                 return 1;
         }
@@ -916,7 +916,7 @@ static int matches_aliases(server_rec *s, const char *host)
     if (names) {
         char **name = (char **) names->elts;
         for (i = 0; i < names->nelts; ++i) {
-            if(!name[i]) continue;
+            if (!name[i]) continue;
             if (!ap_strcasecmp_match(host, name[i]))
                 return 1;
         }
