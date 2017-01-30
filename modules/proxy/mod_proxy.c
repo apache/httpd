apache/httpd

commit c64e69c3106aef47dfcd30b1f3dc35832034f966

ProxyPass ! doesn't block per-directory ProxyPass
 *) mod_proxy: Honor a server scoped ProxyPass exception when ProxyPass is
     configured in <Location>, like in 2.2. PR 60458.
     [Eric Covener]



git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1773397 13f79535-47bb-0310-9956-ffa450edef68

modules/proxy/mod_proxy.c
@@ -788,17 +788,28 @@ static int proxy_trans(request_rec *r)
      */
 
     dconf = ap_get_module_config(r->per_dir_config, &proxy_module);
-
+    conf = (proxy_server_conf *) ap_get_module_config(r->server->module_config,
+                                                      &proxy_module);
     /* short way - this location is reverse proxied? */
     if (dconf->alias) {
         int rv = ap_proxy_trans_match(r, dconf->alias, dconf);
+        if (OK == rv) { 
+            /* Got a hit. Need to make sure it's not explicitly declined */
+            if (conf->aliases->nelts) {
+                ent = (struct proxy_alias *) conf->aliases->elts;
+                for (i = 0; i < conf->aliases->nelts; i++) {
+                    int rv = ap_proxy_trans_match(r, &ent[i], dconf);
+                    if (DECLINED == rv) { 
+                        return DECLINED;
+                    }
+                }
+            }
+            return OK; 
+        }
         if (DONE != rv) {
             return rv;
         }
     }
-
-    conf = (proxy_server_conf *) ap_get_module_config(r->server->module_config,
-                                                      &proxy_module);
 
     /* long way - walk the list of aliases, find a match */
     if (conf->aliases->nelts) {

commit 7a595279efd5bbbd3a72129bb973f53b363ee893

"fix" regression in r1773397.
committing for discussion purposes to trunk.  I have reopened PR60458 because
I am tempted to revert the entire thing, it was not really a release-to-release
regression because the reporter arbitrarily used a new 2.4 syntax and the semantics
were unclear.

It is useful to have global exceptions apply to per-location proxypass, but it totally
breaks the "fast" lookup part of it.



git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1780909 13f79535-47bb-0310-9956-ffa450edef68

modules/proxy/mod_proxy.c
@@ -794,15 +794,22 @@ static int proxy_trans(request_rec *r)
     if (dconf->alias) {
         int rv = ap_proxy_trans_match(r, dconf->alias, dconf);
         if (OK == rv) { 
+            int matches = 0;
             /* Got a hit. Need to make sure it's not explicitly declined */
             if (conf->aliases->nelts) {
                 ent = (struct proxy_alias *) conf->aliases->elts;
                 for (i = 0; i < conf->aliases->nelts; i++) {
                     int rv = ap_proxy_trans_match(r, &ent[i], dconf);
+                    if (OK == rv) matches++;
                     if (DECLINED == rv) { 
                         return DECLINED;
                     }
                 }
+            }
+
+            /* a non !-rule matches in server scope, restore */
+            if (matches > 0) {
+                ap_proxy_trans_match(r, dconf->alias, dconf);
             }
             return OK; 
         }