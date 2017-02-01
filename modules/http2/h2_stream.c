832cbdb76ae5ff8709a910c25684c0d8cf3ddaef
Stefan Eissing <icing@apache.org>
2017-02-01 20:40:38 +0000

    On the trunk:
    mod_http2: fix for crash when running out of memory. Initial patch by Robert Swiecki <robert@swiecki.net>
    CVEID: CVE-2017-7659
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1781304 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http2/h2_stream.c
+++ modules/http2/h2_stream.c
@@ -262,11 +262,13 @@ apr_status_t h2_stream_set_request_rec(h2_stream *stream, request_rec *r)
         return APR_ECONNRESET;
     }
     status = h2_request_rcreate(&req, stream->pool, r);
-    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, status, r, APLOGNO(03058)
-                  "h2_request(%d): set_request_rec %s host=%s://%s%s",
-                  stream->id, req->method, req->scheme, req->authority, 
-                  req->path);
-    stream->rtmp = req;
+    if (status == APR_SUCCESS) {
+        ap_log_rerror(APLOG_MARK, APLOG_DEBUG, status, r, APLOGNO(03058)
+                      "h2_request(%d): set_request_rec %s host=%s://%s%s",
+                      stream->id, req->method, req->scheme, req->authority, 
+                      req->path);
+        stream->rtmp = req;
+    }
     return status;
 }
 
