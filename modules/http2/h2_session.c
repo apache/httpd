4df8e313bfcf69612f6c65a228f0737e7c94d5df
Stefan Eissing <icing@apache.org>
2017-03-15 14:19:51 +0000

    On the trunk:
    mod_http2: documentation and formatting
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1787051 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http2/h2_session.c
+++ modules/http2/h2_session.c
@@ -1772,7 +1772,7 @@ static void ev_stream_open(h2_session *session, h2_stream *stream)
     switch (session->state) {
         case H2_SESSION_ST_IDLE:
             if (session->open_streams == 1) {
-                /* enter tiomeout, since we have a stream again */
+                /* enter timeout, since we have a stream again */
                 session->idle_until = (session->s->timeout + apr_time_now());
             }
             break;
