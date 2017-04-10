825b04d0c2e5780042019ee42b7a15f3ef8c1106
Evgeny Kotkov <kotkov@apache.org>
2017-04-10 15:27:58 +0000

    mod_brotli: Comment on the default choice (0) for BROTLI_PARAM_LGBLOCK.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1790860 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/mod_brotli.c
+++ modules/filters/mod_brotli.c
@@ -53,6 +53,10 @@ static void *create_server_config(apr_pool_t *p, server_rec *s)
      */
     conf->quality = 5;
     conf->lgwin = 18;
+    /* Zero is a special value for BROTLI_PARAM_LGBLOCK that allows
+     * Brotli to automatically select the optimal input block size based
+     * on other encoder parameters.  See enc/quality.h: ComputeLgBlock().
+     */
     conf->lgblock = 0;
     conf->etag_mode = ETAG_MODE_ADDSUFFIX;
 
