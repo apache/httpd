c8598a912636278b446fcf0f5ffe8d822bda1315
Stefan Eissing <icing@apache.org>
2017-04-14 16:02:22 +0000

    On the trunk:
    mod_http2: client streams that lack the EOF flag get now forcefully
         closed with a RST_STREAM (NO_ERROR) when the request has been answered.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1791388 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http2/h2_mplx.c
+++ modules/http2/h2_mplx.c
@@ -258,7 +258,17 @@ static int input_consumed_signal(h2_mplx *m, h2_stream *stream)
 
 static int report_consumption_iter(void *ctx, void *val)
 {
-    input_consumed_signal(ctx, val);
+    h2_stream *stream = val;
+    h2_mplx *m = ctx;
+    
+    input_consumed_signal(m, stream);
+    if (stream->state == H2_SS_CLOSED_L
+        && (!stream->task || stream->task->worker_done)) {
+        ap_log_cerror(APLOG_MARK, APLOG_DEBUG, 0, m->c, 
+                      H2_STRM_LOG(APLOGNO(10026), stream, "remote close missing")); 
+        nghttp2_submit_rst_stream(stream->session->ngh2, NGHTTP2_FLAG_NONE, 
+                                  stream->id, NGHTTP2_NO_ERROR);
+    }
     return 1;
 }
 
