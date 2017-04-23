200137e8e9f3327abfca1cf24507239ba4be02c6
Eric Covener <covener@apache.org>
2017-04-23 21:27:15 +0000

    despite trace, assign a APLOGNO, as done elsewhere in this mod
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1792395 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_wstunnel.c
+++ modules/proxy/mod_proxy_wstunnel.c
@@ -67,7 +67,7 @@ static int proxy_wstunnel_pump(ws_baton_t *baton, apr_time_t timeout, int try_po
                     return SUSPENDED;
                 }
                 else { 
-                    ap_log_rerror(APLOG_MARK, APLOG_TRACE1, rv, r, APLOGNO() "Closing idle tunnel");
+                    ap_log_rerror(APLOG_MARK, APLOG_TRACE1, rv, r, APLOGNO(10031) "Closing idle tunnel");
                     return HTTP_REQUEST_TIME_OUT;
                 }
             }
