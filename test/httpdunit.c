d3289c73ee5f053dc99ccb8546624017e83cca8b
Jacob Champion <jchampion@apache.org>
2017-05-25 21:18:32 +0000

    httpdunit: a Check-based unit test suite
    Add a unit test suite based on Check:
    
        https://libcheck.github.io/check/
    
    The suite depends on the build system to automatically generate the code
    stubs that call every test case.
    
    httpdunit is automatically enabled in the build if configure is able to
    find Check via pkg-config. At the moment pkg-config is the only official
    (non-deprecated) way to build and link against Check with an autoconf
    system, since platforms may distribute Check as a static library.
    
    Note that Check is an LGPL'd library, so we can't distribute test
    objects and binaries. Building and running the suite remains optional
    and is not required to run the server.
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/httpdunit@1796202 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/httpdunit.c
@@ -0,0 +1,53 @@
+/* Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "check.h"
+
+#include "apr_general.h"
+
+static Suite *main_test_suite(void)
+{
+    Suite *suite = suite_create("main");
+
+    /* The list of test cases is automatically generated from the test/unit
+     * directory by the build system. */
+#   include "test/httpdunit.cases"
+
+    return suite;
+}
+
+int main(int argc, const char * const argv[])
+{
+    SRunner *runner;
+    int failed;
+
+    /* Initialize APR and create our test runner. */
+    apr_app_initialize(&argc, &argv, NULL);
+    runner = srunner_create(main_test_suite());
+
+    /* Log TAP to stdout. */
+    srunner_set_tap(runner, "-");
+
+    /* Run the tests and collect failures. */
+    srunner_run_all(runner, CK_SILENT /* output only TAP */);
+    failed = srunner_ntests_failed(runner);
+
+    /* Clean up. */
+    srunner_free(runner);
+    apr_terminate();
+
+    return failed ? 1 : 0;
+}
