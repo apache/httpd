cd8065c27b88f8347b6f0b92e5e4a9cc65c2d3de
Jim Jagielski <jim@apache.org>
2017-05-30 12:25:13 +0000

    Merge r1795635 from trunk:
    When processing a 'SetEnv' directive, warn if the environment variable name includes a '='. It is likely a configuration error.   PR 60249
    Submitted by: jailletc36
    Reviewed by: jailletc36, covener, ylavic
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1796853 13f79535-47bb-0310-9956-ffa450edef68

--- modules/metadata/mod_env.c
+++ modules/metadata/mod_env.c
@@ -113,6 +113,17 @@ static const char *add_env_module_vars_set(cmd_parms *cmd, void *sconf_,
 {
     env_dir_config_rec *sconf = sconf_;
 
+    if (ap_strchr_c(name, '=')) {
+        char *env, *plast;
+
+        env = apr_strtok(apr_pstrdup(cmd->temp_pool, name), "=", &plast);
+
+        ap_log_error(APLOG_MARK, APLOG_WARNING, 0, cmd->server, APLOGNO(10032)
+                     "Spurious usage of '=' in an environment variable name. "
+                     "'%s %s %s' expected instead?", cmd->cmd->name, env, plast);
+    
+    }
+
     /* name is mandatory, value is optional.  no value means
      * set the variable to an empty string
      */
