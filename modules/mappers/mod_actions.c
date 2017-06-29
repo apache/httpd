4a406ae8a880319cc64d6b4667eaa8f0239bb43f
Jacob Champion <jchampion@apache.org>
2017-06-29 17:43:48 +0000

    proxy_fcgi: remove FPM-specific logic
    Reverts r1780328, r1780329, and their associated followups, which
    incorrectly manipulated SCRIPT_NAME by default. All proxy_fcgi.t
    regression tests now pass.
    
    PR: 61202
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1800306 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_actions.c
+++ modules/mappers/mod_actions.c
@@ -186,8 +186,7 @@ static int action_handler(request_rec *r)
         ap_field_noparam(r->pool, r->content_type);
 
     if (action && (t = apr_table_get(conf->action_types, action))) {
-        int virtual = (*t++ == '0' ? 0 : 1);
-        if (!virtual && r->finfo.filetype == APR_NOFILE) {
+        if (*t++ == '0' && r->finfo.filetype == APR_NOFILE) {
             ap_log_rerror(APLOG_MARK, APLOG_INFO, 0, r, APLOGNO(00652)
                           "File does not exist: %s", r->filename);
             return HTTP_NOT_FOUND;
@@ -198,9 +197,6 @@ static int action_handler(request_rec *r)
          * (will be REDIRECT_HANDLER there)
          */
         apr_table_setn(r->subprocess_env, "HANDLER", action);
-        if (virtual) {
-            apr_table_setn(r->notes, "virtual_script", "1");
-        }
     }
 
     if (script == NULL)
