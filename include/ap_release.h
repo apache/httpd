0c4428887ac044f9427275b85aecc0464e62e943
William A. Rowe Jr <wrowe@apache.org>
2017-07-06 18:46:42 +0000

    2.2.35-dev is most likely in our attic, but bump for disambiguation.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.2.x@1801092 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_release.h
+++ include/ap_release.h
@@ -43,8 +43,8 @@
 
 #define AP_SERVER_MAJORVERSION_NUMBER 2
 #define AP_SERVER_MINORVERSION_NUMBER 2
-#define AP_SERVER_PATCHLEVEL_NUMBER   34
-#define AP_SERVER_DEVBUILD_BOOLEAN    0
+#define AP_SERVER_PATCHLEVEL_NUMBER   35
+#define AP_SERVER_DEVBUILD_BOOLEAN    1
 
 /* Synchronize the above with docs/manual/style/version.ent */ 
 
