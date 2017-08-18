868be43279480e5e2c511f1084a2e54b5b0658a2
Joe Orton <jorton@apache.org>
2017-08-18 08:49:42 +0000

    Merge 1805099 from trunk:
    Fix ProxyAddHeaders merging.
    
    * modules/proxy/mod_proxy.h:
      Add add_forwarded_headers_set field to proxy_dir_conf.
    
    * modules/proxy/mod_proxy.c (create_proxy_dir_config):
      Initialize add_forwarded_headers_set.
      (add_proxy_http_headers): Set it.
      (merge_proxy_dir_config): Merge add_forwarded_headers correctly.
    
    Reviewed by: jorton, rpluem, jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1805390 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy.c
+++ modules/proxy/mod_proxy.c
@@ -1451,6 +1451,7 @@ static void *create_proxy_dir_config(apr_pool_t *p, char *dummy)
     new->error_override = 0;
     new->error_override_set = 0;
     new->add_forwarded_headers = 1;
+    new->add_forwarded_headers_set = 0;
 
     return (void *) new;
 }
@@ -1482,7 +1483,12 @@ static void *merge_proxy_dir_config(apr_pool_t *p, void *basev, void *addv)
     new->error_override_set = add->error_override_set || base->error_override_set;
     new->alias = (add->alias_set == 0) ? base->alias : add->alias;
     new->alias_set = add->alias_set || base->alias_set;
-    new->add_forwarded_headers = add->add_forwarded_headers;
+    new->add_forwarded_headers =
+        (add->add_forwarded_headers_set == 0) ? base->add_forwarded_headers
+        : add->add_forwarded_headers;
+    new->add_forwarded_headers_set = add->add_forwarded_headers_set
+        || base->add_forwarded_headers_set;
+    
     return new;
 }
 
@@ -1970,6 +1976,7 @@ static const char *
 {
    proxy_dir_conf *conf = dconf;
    conf->add_forwarded_headers = flag;
+   conf->add_forwarded_headers_set = 1;
    return NULL;
 }
 static const char *
