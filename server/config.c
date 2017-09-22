91566715427817ae70ec386ae0d283015d5a73ee
Yann Ylavic <ylavic@apache.org>
2017-09-22 13:13:44 +0000

    config: follow up to r1809302.
    Provide a convenient function to get module flags, and remove useless
    AP_MODULE_HAS_FLAGS checks in the core, core's version is at current MMN.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1809311 13f79535-47bb-0310-9956-ffa450edef68

--- server/config.c
+++ server/config.c
@@ -338,20 +338,12 @@ static void merge_server_configs(apr_pool_t *p, ap_conf_vector_t *base,
         int i = modp->module_index;
 
         if (!virt_vector[i]) {
-#if AP_MODULE_HAS_FLAGS
-            if (df
-                    && modp->create_server_config
-                    && (modp->version > AP_MODULE_FLAGS_MMN_MAJOR
-                        || (modp->version == AP_MODULE_FLAGS_MMN_MAJOR
-                            && (modp->minor_version >=
-                                AP_MODULE_FLAGS_MMN_MINOR)))
-                    /* keep this after version checks (flags out-of-bound) */
-                    && (modp->flags & AP_MODULE_FLAG_ALWAYS_MERGE)) {
+            if (df && modp->create_server_config
+                   && (ap_get_module_flags(modp) &
+                       AP_MODULE_FLAG_ALWAYS_MERGE)) {
                 virt_vector[i] = (*modp->create_server_config)(p, virt);
             }
-            else
-#endif
-            {
+            else {
                 virt_vector[i] = base_vector[i];
                 df = NULL;
             }
