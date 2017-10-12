d8afd98367ffcb4f1f761be58a775893c6debe85
Joe Orton <jorton@apache.org>
2017-10-12 15:11:50 +0000

    Add optional _RAW suffix to SSL_*_DN_xx attribute names, allowing users to convert an attribute value without conversion to UTF-8.  (A public CA has issued certs with attributes tagged as the wrong ASN.1 string types.)
    * modules/ssl/ssl_util_ssl.c (asn1_string_convert): Rename from
      asn1_string_to_utf8; add raw argument. Reimplement _to_utf8 as
      macro.
      (modssl_X509_NAME_ENTRY_to_string): Add raw argument.
    
    * modules/ssl/ssl_engine_vars.c (ssl_var_lookup_ssl_cert_dn): Use raw
      string conversion if _RAW suffix is present in DN component.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1811976 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_util_ssl.h
+++ modules/ssl/ssl_util_ssl.h
@@ -68,7 +68,8 @@ EVP_PKEY   *modssl_read_privatekey(const char *, EVP_PKEY **, pem_password_cb *,
 EVP_PKEY   *modssl_read_encrypted_pkey(const char *, EVP_PKEY **, const char *, apr_size_t);
 int         modssl_smart_shutdown(SSL *ssl);
 BOOL        modssl_X509_getBC(X509 *, int *, int *);
-char       *modssl_X509_NAME_ENTRY_to_string(apr_pool_t *p, X509_NAME_ENTRY *xsne);
+char       *modssl_X509_NAME_ENTRY_to_string(apr_pool_t *p, X509_NAME_ENTRY *xsne,
+                                             int raw);
 char       *modssl_X509_NAME_to_string(apr_pool_t *, X509_NAME *, int);
 BOOL        modssl_X509_getSAN(apr_pool_t *, X509 *, int, const char *, int, apr_array_header_t **);
 BOOL        modssl_X509_match_name(apr_pool_t *, X509 *, const char *, BOOL, server_rec *);
