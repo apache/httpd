33c6df915a4e59d375e0a2b56da94c84f021004c
Joe Orton <jorton@apache.org>
2017-10-16 16:40:52 +0000

    * server/util_expr_eval.c (ap_expr_eval_re_backref): Fix gcc 7.x warning.
    util_expr_eval.c: In function ‘ap_expr_eval_re_backref’:
    util_expr_eval.c:265:63: error: comparison between pointer and zero character constant [-Werror=pointer-compare]
         if (!ctx->re_pmatch || !ctx->re_source || *ctx->re_source == '\0' ||
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1812307 13f79535-47bb-0310-9956-ffa450edef68

--- server/util_expr_eval.c
+++ server/util_expr_eval.c
@@ -262,8 +262,8 @@ static const char *ap_expr_eval_re_backref(ap_expr_eval_ctx_t *ctx, unsigned int
 {
     int len;
 
-    if (!ctx->re_pmatch || !ctx->re_source || *ctx->re_source == '\0' ||
-        ctx->re_nmatch < n + 1)
+    if (!ctx->re_pmatch || !ctx->re_source || !*ctx->re_source
+        || **ctx->re_source == '\0' || ctx->re_nmatch < n + 1)
         return "";
 
     len = ctx->re_pmatch[n].rm_eo - ctx->re_pmatch[n].rm_so;
