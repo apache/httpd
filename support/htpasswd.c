a4099762df4f72c71f30dee739d4734aa2bd552d
Joe Orton <jorton@apache.org>
2017-10-20 15:40:37 +0000

    * support/htpasswd.c (main): Only check for readability if running in   verify (-v) mode.
    PR: 61631
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1812756 13f79535-47bb-0310-9956-ffa450edef68

--- support/htpasswd.c
+++ support/htpasswd.c
@@ -329,13 +329,23 @@ int main(int argc, const char * const argv[])
      */
     if (!(mask & APHTP_NOFILE)) {
         existing_file = exists(pwfilename, pool);
-        if (existing_file) {
+        if (existing_file && (mask & APHTP_VERIFY) == 0) {
             /*
              * Check that this existing file is readable and writable.
              */
             if (!accessible(pool, pwfilename, APR_FOPEN_READ|APR_FOPEN_WRITE)) {
                 apr_file_printf(errfile, "%s: cannot open file %s for "
                                 "read/write access" NL, argv[0], pwfilename);
+                exit(ERR_FILEPERM);
+            }
+        }
+        else if (existing_file && (mask & APHTP_VERIFY) != 0) {
+            /*
+             * Check that this existing file is readable.
+             */
+            if (!accessible(pool, pwfilename, APR_FOPEN_READ)) {
+                apr_file_printf(errfile, "%s: cannot open file %s for "
+                                "read access" NL, argv[0], pwfilename);
                 exit(ERR_FILEPERM);
             }
         }
