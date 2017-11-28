821fff7cf60ac53087ccacf873d8893168657f10
Stefan Eissing <icing@apache.org>
2017-11-28 15:14:56 +0000

    On the 2.4.x-mod_md branch:
    Merged r1816552 from trunk.
    
    mod_md: v1.0.5, restricting post_config dry run to be more silent and performing
         only necessary work for mod_ssl to be also happy with the configuration.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x-mod_md@1816553 13f79535-47bb-0310-9956-ffa450edef68

--- modules/md/md_version.h
+++ modules/md/md_version.h
@@ -26,7 +26,7 @@
  * @macro
  * Version number of the md module as c string
  */
-#define MOD_MD_VERSION "1.0.4"
+#define MOD_MD_VERSION "1.0.5"
 
 /**
  * @macro
@@ -34,7 +34,7 @@
  * release. This is a 24 bit number with 8 bits for major number, 8 bits
  * for minor and 8 bits for patch. Version 1.2.3 becomes 0x010203.
  */
-#define MOD_MD_VERSION_NUM 0x010004
+#define MOD_MD_VERSION_NUM 0x010005
 
 #define MD_EXPERIMENTAL 0
 #define MD_ACME_DEF_URL    "https://acme-v01.api.letsencrypt.org/directory"
