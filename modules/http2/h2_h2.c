c49841f7191f55034198f52ffd430c868020dcd7
Yann Ylavic <ylavic@apache.org>
2017-12-24 11:39:30 +0000

    mpm_event: follow up to r1818804 and r1818960.
    Align mod_http2 with expected returned state from process_connection hooks in
    async MPMs.
    When the master connection is handled, enter CONN_STATE_LINGER in any case.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1819214 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http2/h2_h2.c
+++ modules/http2/h2_h2.c
@@ -669,10 +669,11 @@ int h2_h2_process_conn(conn_rec* c)
             ap_log_cerror(APLOG_MARK, APLOG_TRACE1, status, c, "conn_setup");
             if (status != APR_SUCCESS) {
                 h2_ctx_clear(c);
-                return status;
+                return !OK;
             }
         }
-        return h2_conn_run(ctx, c);
+        h2_conn_run(ctx, c);
+        return OK;
     }
     
     ap_log_cerror(APLOG_MARK, APLOG_TRACE1, 0, c, "h2_h2, declined");
