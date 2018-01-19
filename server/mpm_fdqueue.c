95fbbd8d278b59e04f3733d3a3fc239f42c938fd
Yann Ylavic <ylavic@apache.org>
2018-01-19 12:23:05 +0000

    mpm_fdqueue: follow up to r1821624.
    Clear recycled_pools_count in ap_free_idle_pools().
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1821632 13f79535-47bb-0310-9956-ffa450edef68

--- server/mpm_fdqueue.c
+++ server/mpm_fdqueue.c
@@ -286,11 +286,13 @@ void ap_free_idle_pools(fd_queue_info_t *queue_info)
     apr_pool_t *p;
 
     queue_info->max_recycled_pools = 0;
-    do {
+    for (;;) {
         ap_pop_pool(&p, queue_info);
-        if (p != NULL)
-            apr_pool_destroy(p);
-    } while (p != NULL);
+        if (p == NULL)
+            break;
+        apr_pool_destroy(p);
+    }
+    apr_atomic_set32(&queue_info->recycled_pools_count, 0);
 }
 
 
