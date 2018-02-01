72c6243f5e08c3fbed0ed94f6a818baa6cfab87f
Stefan Eissing <icing@apache.org>
2018-02-01 12:37:51 +0000

    On the trunk:
    adding AP_DECLARE for ap_parse_vhost_addrs() and minor bumb mmn. Resolves
         building mod_ssl on Windows. 
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1822872 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_mmn.h
+++ include/ap_mmn.h
@@ -561,6 +561,7 @@
  *                         semantics
  * 20171014.2 (2.5.1-dev)  Add "use_specific_errors" to listen_rec and 
  *                         ap_accept_error_is_nonfatal()
+ * 20171014.3 (2.5.1-dev)  AP_DECLARE ap_parse_vhost_addrs() as public 
  */
 
 #define MODULE_MAGIC_COOKIE 0x41503235UL /* "AP25" */
@@ -568,7 +569,7 @@
 #ifndef MODULE_MAGIC_NUMBER_MAJOR
 #define MODULE_MAGIC_NUMBER_MAJOR 20171014
 #endif
-#define MODULE_MAGIC_NUMBER_MINOR 2                 /* 0...n */
+#define MODULE_MAGIC_NUMBER_MINOR 3                 /* 0...n */
 
 /**
  * Determine if the server's current MODULE_MAGIC_NUMBER is at least a
