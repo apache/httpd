acf49a7ea6e5da86d8c088615c1e03526a0d99bb
Yann Ylavic <ylavic@apache.org>
2018-03-12 15:19:07 +0000

    Revert r1826543, was meant for trunk...
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1826555 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http/http_request.c
+++ modules/http/http_request.c
@@ -480,9 +480,13 @@ AP_DECLARE(void) ap_process_request(request_rec *r)
              * Notice a timeout as an error message. This might be
              * valuable for detecting clients with broken network
              * connections or possible DoS attacks.
+             *
+             * It is still safe to use r / r->pool here as the eor bucket
+             * could not have been destroyed in the event of a timeout.
              */
-            ap_log_cerror(APLOG_MARK, APLOG_INFO, rv, c, APLOGNO(01581)
-                          "flushing data to the client");
+            ap_log_rerror(APLOG_MARK, APLOG_INFO, rv, r, APLOGNO(01581)
+                          "Timeout while writing data for URI %s to the"
+                          " client", r->unparsed_uri);
         }
     }
     if (ap_extended_status) {
