4b2af278e657ab6190e887d70e90e4c086776fd7
Stefan Eissing <icing@apache.org>
2018-03-19 09:16:00 +0000

    mod_md: /.well-known/acme-challenge requests that cannot be answered for hostnames      outside the configured MDs are free to be answered by other handlers. This allows      co-existance between mod_md and other ACME clients on the same server (implements PR62189).      [Stefan Eissing, Arkadiusz Miskiewicz <arekm@maven.pl>]
    Fixes PR62189.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1827175 13f79535-47bb-0310-9956-ffa450edef68

--- modules/md/mod_md.c
+++ modules/md/mod_md.c
@@ -1302,48 +1302,61 @@ static int md_http_challenge_pr(request_rec *r)
     const md_srv_conf_t *sc;
     const char *name, *data;
     md_reg_t *reg;
+    int configured;
     apr_status_t rv;
     
     if (!strncmp(ACME_CHALLENGE_PREFIX, r->parsed_uri.path, sizeof(ACME_CHALLENGE_PREFIX)-1)) {
-        if (r->method_number == M_GET) {
-        
-            sc = ap_get_module_config(r->server->module_config, &md_module);
-            reg = sc && sc->mc? sc->mc->reg : NULL;
-            name = r->parsed_uri.path + sizeof(ACME_CHALLENGE_PREFIX)-1;
-
-            r->status = HTTP_NOT_FOUND;
-            if (!ap_strchr_c(name, '/') && reg) {
-                md_store_t *store = md_reg_store_get(reg);
-                ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r, 
-                              "Challenge for %s (%s)", r->hostname, r->uri);
-
-                rv = md_store_load(store, MD_SG_CHALLENGES, r->hostname, 
-                                   MD_FN_HTTP01, MD_SV_TEXT, (void**)&data, r->pool);
-                if (APR_SUCCESS == rv) {
-                    apr_size_t len = strlen(data);
+        sc = ap_get_module_config(r->server->module_config, &md_module);
+        if (sc && sc->mc) {
+            configured = (NULL != md_get_by_domain(sc->mc->mds, r->hostname));
+            if (r->method_number == M_GET) {
+                name = r->parsed_uri.path + sizeof(ACME_CHALLENGE_PREFIX)-1;
+                reg = sc && sc->mc? sc->mc->reg : NULL;
+                
+                r->status = HTTP_NOT_FOUND;
+                if (!ap_strchr_c(name, '/') && reg) {
+                    md_store_t *store = md_reg_store_get(reg);
+                    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r, 
+                                  "Challenge for %s (%s)", r->hostname, r->uri);
                     
-                    r->status = HTTP_OK;
-                    apr_table_setn(r->headers_out, "Content-Length", apr_ltoa(r->pool, (long)len));
-                    
-                    bb = apr_brigade_create(r->pool, r->connection->bucket_alloc);
-                    apr_brigade_write(bb, NULL, NULL, data, len);
-                    ap_pass_brigade(r->output_filters, bb);
-                    apr_brigade_cleanup(bb);
+                    rv = md_store_load(store, MD_SG_CHALLENGES, r->hostname, 
+                                       MD_FN_HTTP01, MD_SV_TEXT, (void**)&data, r->pool);
+                    if (APR_SUCCESS == rv) {
+                        apr_size_t len = strlen(data);
+                        
+                        r->status = HTTP_OK;
+                        apr_table_setn(r->headers_out, "Content-Length", apr_ltoa(r->pool, (long)len));
+                        
+                        bb = apr_brigade_create(r->pool, r->connection->bucket_alloc);
+                        apr_brigade_write(bb, NULL, NULL, data, len);
+                        ap_pass_brigade(r->output_filters, bb);
+                        apr_brigade_cleanup(bb);
+                    }
+                    else if (!configured) {
+                        /* The request hostname is not for a configured domain. We are not
+                         * the sole authority here for /.well-known/acme-challenge (see PR62189).
+                         * So, we decline to handle this and let others step in.
+                         */
+                        return DECLINED;
+                    }
+                    else if (APR_STATUS_IS_ENOENT(rv)) {
+                        return HTTP_NOT_FOUND;
+                    }
+                    else if (APR_ENOENT != rv) {
+                        ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r, APLOGNO(10081)
+                                      "loading challenge %s from store", name);
+                        return HTTP_INTERNAL_SERVER_ERROR;
+                    }
                 }
-                else if (APR_STATUS_IS_ENOENT(rv)) {
-                    return HTTP_NOT_FOUND;
-                }
-                else if (APR_ENOENT != rv) {
-                    ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r, APLOGNO(10081)
-                                  "loading challenge %s from store", name);
-                    return HTTP_INTERNAL_SERVER_ERROR;
-                }
+                return r->status;
             }
-            return r->status;
+            else if (configured) {
+                /* See comment above, we prevent any other access only for domains
+                 * the have been configured for mod_md. */ 
+                return HTTP_NOT_IMPLEMENTED;
+            }
         }
-        else {
-            return HTTP_NOT_IMPLEMENTED;
-        }
+        
     }
     return DECLINED;
 }
