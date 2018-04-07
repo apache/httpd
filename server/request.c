92a2afcfc08d56775215e31b6bf8f2365d8ecdbf
Christophe Jaillet <jailletc36@apache.org>
2018-04-07 21:04:16 +0000

    Add some spaces that were skipped when r1684524 from trunk has been backported in 2.4.x in r1684525.
    (in order to synch 2.4.x and trunk)
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1828619 13f79535-47bb-0310-9956-ffa450edef68

--- server/request.c
+++ server/request.c
@@ -99,7 +99,7 @@ AP_IMPLEMENT_HOOK_RUN_FIRST(apr_status_t,dirwalk_stat,
                             (apr_finfo_t *finfo, request_rec *r, apr_int32_t wanted),
                             (finfo, r, wanted), AP_DECLINED)
 AP_IMPLEMENT_HOOK_RUN_FIRST(int,force_authn,
-                          (request_rec *r), (r), DECLINED)
+                            (request_rec *r), (r), DECLINED)
 
 static int auth_internal_per_conf = 0;
 static int auth_internal_per_conf_hooks = 0;
