90bfc436ee18e366f531caceab8942c579a0bc49
Joe Orton <jorton@apache.org>
2018-04-11 15:58:04 +0000

    * modules/ssl/ssl_util.c (ssl_util_vhostid): Simplify code,   no functional change.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1828912 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_util.c
+++ modules/ssl/ssl_util.c
@@ -41,23 +41,17 @@
 
 char *ssl_util_vhostid(apr_pool_t *p, server_rec *s)
 {
-    char *id;
     SSLSrvConfigRec *sc;
-    char *host;
     apr_port_t port;
 
-    host = s->server_hostname;
     if (s->port != 0)
         port = s->port;
     else {
         sc = mySrvConfig(s);
-        if (sc->enabled == TRUE)
-            port = DEFAULT_HTTPS_PORT;
-        else
-            port = DEFAULT_HTTP_PORT;
+        port = sc->enabled == TRUE ? DEFAULT_HTTPS_PORT : DEFAULT_HTTP_PORT;
     }
-    id = apr_psprintf(p, "%s:%lu", host, (unsigned long)port);
-    return id;
+
+    return apr_psprintf(p, "%s:%lu", s->server_hostname, (unsigned long)port);
 }
 
 /*
