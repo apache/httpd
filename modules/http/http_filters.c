07e795c9bf4068c786b49634bc5faf292932d86f
Yann Ylavic <ylavic@apache.org>
2018-04-20 10:38:05 +0000

    Restore comment about LimitRequestBody vs proxied responses.
    ResponseFieldSize actually applies to headers (not body), so the comment was
    valid but did match the code (addressed by r1829642 still).
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1829643 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http/http_filters.c
+++ modules/http/http_filters.c
@@ -317,9 +317,10 @@ apr_status_t ap_http_filter(ap_filter_t *f, apr_bucket_brigade *b,
         f->ctx = ctx = apr_pcalloc(f->r->pool, sizeof(*ctx));
         ctx->state = BODY_NONE;
 
-        /* LimitRequestBody does not apply to proxied responses, which have
-         * their own ResponseFieldSize parameter.
+        /* LimitRequestBody does not apply to proxied responses.
          * Consider implementing this check in its own filter.
+         * Would adding a directive to limit the size of proxied
+         * responses be useful?
          */
         if (f->r->proxyreq != PROXYREQ_RESPONSE) {
             ctx->limit = ap_get_limit_req_body(f->r);
