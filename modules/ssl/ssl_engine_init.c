9f49836d98a21949fad73b980401aa9b9b1510e6
Yann Ylavic <ylavic@apache.org>
2018-04-29 22:07:26 +0000

    Follow up to r1828222: fix "defined but not used 'prot'" warning with libressl.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1830522 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_engine_init.c
+++ modules/ssl/ssl_engine_init.c
@@ -582,7 +582,8 @@ static apr_status_t ssl_init_ctx_protocol(server_rec *s,
     char *cp;
     int protocol = mctx->protocol;
     SSLSrvConfigRec *sc = mySrvConfig(s);
-#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L  && \
+	(!defined(LIBRESSL_VERSION_NUMBER) || LIBRESSL_VERSION_NUMBER >= 0x20800000L)
     int prot;
 #endif
 
