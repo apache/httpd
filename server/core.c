ea3f9470c13b990e618091549345c796be98d513
Yann Ylavic <ylavic@apache.org>
2018-06-12 12:48:42 +0000

    Follow up to r1833368: fix "mixed declarations and code" warning (buildbot).
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1833400 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -5464,11 +5464,13 @@ static void core_child_init(apr_pool_t *pchild, server_rec *s)
     apr_crypto_prng_after_fork(&proc);
 #else
 #if APR_HAS_THREADS
-    int threaded_mpm;
-    if (ap_mpm_query(AP_MPMQ_IS_THREADED, &threaded_mpm) == APR_SUCCESS
-        && threaded_mpm)
     {
-        apr_thread_mutex_create(&rng_mutex, APR_THREAD_MUTEX_DEFAULT, pchild);
+        int threaded_mpm;
+        if (ap_mpm_query(AP_MPMQ_IS_THREADED, &threaded_mpm) == APR_SUCCESS
+            && threaded_mpm)
+        {
+            apr_thread_mutex_create(&rng_mutex, APR_THREAD_MUTEX_DEFAULT, pchild);
+        }
     }
 #endif
     apr_random_after_fork(&proc);
