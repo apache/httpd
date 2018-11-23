5fe15b4d1fb0db716dba590b5fa138b864dce52f
Graham Leggett <minfrin@apache.org>
2018-11-23 15:34:40 +0000

    *) mod_session_cookie: avoid duplicate Set-Cookie headers in the response.    trunk patch: http://svn.apache.org/r1843244 +1: elukey, jim, minfrin
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1847288 13f79535-47bb-0310-9956-ffa450edef68

--- modules/session/mod_session_cookie.c
+++ modules/session/mod_session_cookie.c
@@ -67,7 +67,7 @@ static apr_status_t session_cookie_save(request_rec * r, session_rec * z)
     if (conf->name_set) {
         if (z->encoded && z->encoded[0]) {
             ap_cookie_write(r, conf->name, z->encoded, conf->name_attrs,
-                            z->maxage, r->headers_out, r->err_headers_out,
+                            z->maxage, r->err_headers_out,
                             NULL);
         }
         else {
@@ -80,7 +80,7 @@ static apr_status_t session_cookie_save(request_rec * r, session_rec * z)
     if (conf->name2_set) {
         if (z->encoded && z->encoded[0]) {
             ap_cookie_write2(r, conf->name2, z->encoded, conf->name2_attrs,
-                             z->maxage, r->headers_out, r->err_headers_out,
+                             z->maxage, r->err_headers_out,
                              NULL);
         }
         else {
