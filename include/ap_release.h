c25bd86f258da6998385e68131aa1675c5480cbd
Rainer Jung <rjung@apache.org>
2019-01-01 12:13:21 +0000

    Happy New Year 2019
    Backport of r1850098 from trunk.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1850100 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_release.h
+++ include/ap_release.h
@@ -23,7 +23,7 @@
 #define AP_RELEASE_H
 
 #define AP_SERVER_COPYRIGHT \
-  "Copyright 2018 The Apache Software Foundation."
+  "Copyright 2019 The Apache Software Foundation."
 
 /*
  * The below defines the base string of the Server: header. Additional
