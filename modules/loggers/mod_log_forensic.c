e08d91896979bd0337cda517bf7b6bd7095dfb9f
Yann Ylavic <ylavic@apache.org>
2019-03-28 16:27:26 +0000

    Follow up to r1856490: missing one mod_log_forensic test_char_table case.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1856491 13f79535-47bb-0310-9956-ffa450edef68

--- modules/loggers/mod_log_forensic.c
+++ modules/loggers/mod_log_forensic.c
@@ -151,7 +151,7 @@ static int count_string(const char *p)
     int n;
 
     for (n = 0 ; *p ; ++p, ++n)
-        if (test_char_table[*(unsigned char *)p]&T_ESCAPE_FORENSIC)
+        if (TEST_CHAR(*p, T_ESCAPE_FORENSIC))
             n += 2;
     return n;
 }
