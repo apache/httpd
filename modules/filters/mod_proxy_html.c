66f6adf2ea80cada147afe8ee6251fb6a1997105
Rainer Jung <rjung@apache.org>
2019-08-05 18:31:35 +0000

    Unfortunately -Wcomment can only be influenced using a diagnostics pragma starting with GCC 4.6.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1864451 13f79535-47bb-0310-9956-ffa450edef68

--- modules/filters/mod_proxy_html.c
+++ modules/filters/mod_proxy_html.c
@@ -33,8 +33,6 @@
 #if defined(__GNUC__)
 #if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
 #pragma GCC diagnostic push
-#endif
-#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 2)
 #pragma GCC diagnostic warning "-Wcomment"
 #endif
 #elif defined(__clang__)
