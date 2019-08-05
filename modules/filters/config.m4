apache/httpd

commit 87f16dd355942b3135e68589e0e270ba0332b61d

Make the work-around for C++ style comments causing fatal errors in maintainer-mode for later versions of clang module compile specific (we will still SEE the errors though when buidlin)
git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1855446 13f79535-47bb-0310-9956-ffa450edef68

modules/filters/config.m4
@@ -116,7 +116,7 @@ AC_DEFUN([FIND_LIBXML2], [
       XML2_INCLUDES="${xml2_path}"
       dnl libxml2 includes unicode/*.h files which uses C++ comments
       if test "$GCC" = "yes"; then
-        APACHE_ADD_GCC_CFLAG([-Wno-error=comment])
+        APR_ADDTO(MOD_CPPFLAGS, ["-Wno-error=comment"])
       fi
     else
       ac_cv_libxml2=no

commit 2c3b51a0b0fec746f2cd9d3fde25d86cb1d70639

The GCC flag "-Wno-error=comment" introduced by r1855446 and r1850745 are only known since GCC 4.2. Since it gets set unconditionally, this breaks compilation with old GCC even when not using maintainer mode.
Make the fix for maintainer mode more specific by using
a version dependent pragma in the relevant two C files
only switching off error status for comment warnings.


git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1864435 13f79535-47bb-0310-9956-ffa450edef68

modules/filters/config.m4
@@ -114,10 +114,6 @@ AC_DEFUN([FIND_LIBXML2], [
     if test -n "${xml2_path}" ; then
       ac_cv_libxml2=yes
       XML2_INCLUDES="${xml2_path}"
-      dnl libxml2 includes unicode/*.h files which uses C++ comments
-      if test "$GCC" = "yes"; then
-        APR_ADDTO(MOD_CPPFLAGS, ["-Wno-error=comment"])
-      fi
     else
       ac_cv_libxml2=no
     fi

modules/filters/mod_proxy_html.c
@@ -29,8 +29,27 @@
 #define VERBOSEB(x) if (verbose) {x}
 #endif
 
+/* libxml2 includes unicode/*.h files which uses C++ comments */
+#if defined(__GNUC__)
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
+#pragma GCC diagnostic push
+#endif
+#pragma GCC diagnostic ignored "-Werror=comment"
+#elif defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Werror=comment"
+#endif
+
 /* libxml2 */
 #include <libxml/HTMLparser.h>
+
+#if defined(__GNUC__)
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
+#pragma GCC diagnostic pop
+#endif
+#elif defined(__clang__)
+#pragma clang diagnostic pop
+#endif
 
 #include "http_protocol.h"
 #include "http_config.h"

modules/filters/mod_xml2enc.c
@@ -23,8 +23,29 @@
 
 #include <ctype.h>
 
+/* libxml2 includes unicode/*.h files which uses C++ comments */
+#if defined(__GNUC__)
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
+#pragma GCC diagnostic push
+#endif
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 2)
+#pragma GCC diagnostic ignored "-Werror=comment"
+#endif
+#elif defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Werror=comment"
+#endif
+
 /* libxml2 */
 #include <libxml/encoding.h>
+
+#if defined(__GNUC__)
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
+#pragma GCC diagnostic pop
+#endif
+#elif defined(__clang__)
+#pragma clang diagnostic pop
+#endif
 
 #include "http_protocol.h"
 #include "http_config.h"