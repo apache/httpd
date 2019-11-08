4b47e7697720d423286c101275afe323e0a92fee
Joe Orton <jorton@apache.org>
2019-11-08 12:50:16 +0000

    Merge r1869387, r1869441, r1869456, r1869459, r1869527, r1869533, r1869539, r1869540, r1869548, r1869550 from trunk:
    Add a basic Travis config.
    
    
    Pull in more -dev packages to get more modules building (including mod_md).
    Test gcc 7 on bionic, xenial is the Travis default anyway.
    Only add the toolchain PPA where really needed.
    
    
    Update travis config
    
    * Add 'if TRAVIS_OS_NAME' to before_install/before_script/script to allow multi-os testing.
    * Add separation lines in matrix config (better visual impact).
    * Add Linux to all the current job's name.
    * Add tests for Worker mpm.
    
    
    
    Move before/run scripts out of .travis.yml.
    
    Reviewed by: elukey
    
    
    Support 2.4.x builds, which lacks --with-test-suite support.
    Build with --enable-load-all-modules only when the test suite is run.
    
    
    Add Travis support for multiple APR/APR-util versions.
    Add an APR build with --enable-pool-debug.
    
    
    Build all modules when testing APR variations.
    
    Build APR-util 1.6.x --with-crypto otherwise the "mod_crypto" build fails,
    though this is a bug: https://travis-ci.org/apache/httpd/jobs/609166867
    
    
    Add APR 1.4/APR-util 1.4 build, but only for 2.4.x.
    
    
    Start sketching out some travis docs and check we can -> [skip ci]
    
    Submitted by: jorton, elukey
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1869554 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/travis_run_linux.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -ex
+./buildconf --with-apr=/usr/bin/apr-1-config
+# For trunk, "make check" is sufficient to run the test suite.
+# For 2.4.x, the test suite must be run manually
+if test ! -v SKIP_TESTING; then
+    CONFIG="$CONFIG --enable-load-all-modules"
+    if grep -q ^check: Makefile.in; then
+        CONFIG="--with-test-suite=test/perl-framework $CONFIG"
+        WITH_TEST_SUITE=1
+    else
+        CONFIG="--prefix=$HOME/build/httpd-root $CONFIG"
+    fi
+fi
+if test -v APR_VERSION; then
+    CONFIG="$CONFIG --with-apr=$HOME/root/apr-${APR_VERSION}"
+else
+    CONFIG="$CONFIG --with-apr=/usr"
+fi
+if test -v APU_VERSION; then
+    CONFIG="$CONFIG --with-apr-util=$HOME/root/apu-${APU_VERSION}"
+else
+    CONFIG="$CONFIG --with-apr-util=/usr"
+fi
+./configure $CONFIG
+make $MAKEFLAGS -j2
+if ! test -v SKIP_TESTING; then
+    if test -v WITH_TEST_SUITE; then
+        make check
+    else
+        make install
+        cd test/perl-framework
+        perl Makefile.PL -apxs $HOME/build/httpd-root/bin/apxs
+        make test
+    fi
+fi
