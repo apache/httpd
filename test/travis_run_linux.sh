cc470ff55912783b6d5e04ddbe3cdcc3d68c38a1
Joe Orton <jorton@apache.org>
2019-11-08 11:02:16 +0000

    Run buildconf using the specific APR/APR-util versions if configured.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1869541 13f79535-47bb-0310-9956-ffa450edef68

--- test/travis_run_linux.sh
+++ test/travis_run_linux.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -ex
-./buildconf --with-apr=/usr/bin/apr-1-config
+if test -v APR_VERSION -a -v APU_VERSION; then
+    ./buildconf --with-apr=$HOME/root/apr-${APR_VERSION}/bin/apr-1-config \
+                --with-apr-util=$HOME/root/apu-${APU_VERSION}/bin/apu-1-config
+else
+    ./buildconf --with-apr=/usr/bin/apr-1-config
+fi
 # For trunk, "make check" is sufficient to run the test suite.
 # For 2.4.x, the test suite must be run manually
 if test ! -v SKIP_TESTING; then
