5602cda60959490d82dcec0f7a03fb7170b53bd9
Joe Orton <jorton@apache.org>
2019-11-08 09:46:50 +0000

    Add Travis support for multiple APR/APR-util versions. Add an APR build with --enable-pool-debug.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1869533 13f79535-47bb-0310-9956-ffa450edef68

--- test/travis_before_linux.sh
+++ test/travis_before_linux.sh
@@ -1,5 +1,39 @@
 #!/bin/bash -ex
-svn export -q https://svn.apache.org/repos/asf/apr/apr/trunk srclib/apr
 if ! test -v SKIP_TESTING; then
    svn export -q https://svn.apache.org/repos/asf/httpd/test/framework/trunk test/perl-framework
+fi
+if test -v APR_VERSION; then
+    if ! test -d $HOME/root/apr-${APR_VERSION}; then
+        case $APR_VERSION in
+            trunk) url=https://svn.apache.org/repos/asf/apr/apr/trunk ;;
+            *) url=https://svn.apache.org/repos/asf/apr/apr/tags/${APR_VERSION} ;;
+        esac
+        svn export -q ${url} $HOME/build/apr-${APR_VERSION}
+        pushd $HOME/build/apr-${APR_VERSION}
+        if [ $APR_VERSION = 1.4.1 ]; then
+            # 1.4.1 doesn't build with current libtool
+            svn cat https://svn.apache.org/repos/asf/apr/apr/tags/1.5.1/buildconf > buildconf
+        fi
+        ./buildconf
+        ./configure ${APR_CONFIG} --prefix=$HOME/root/apr-${APR_VERSION}
+        make -j2
+        make install
+        popd
+        APU_CONFIG="$APU_CONFIG --with-apr=$HOME/root/apr-${APR_VERSION}"
+    fi
+fi
+if test -v APU_VERSION; then
+    if ! test -d $HOME/root/apu-${APU_VERSION}; then
+        case $APU_VERSION in
+            trunk) url=https://svn.apache.org/repos/asf/apr/apr-util/trunk ;;
+            *) url=https://svn.apache.org/repos/asf/apr/apr-util/tags/${APU_VERSION} ;;
+        esac
+        svn export -q ${url} $HOME/build/apu-${APU_VERSION}
+        pushd $HOME/build/apu-${APU_VERSION}
+        ./buildconf --with-apr=$HOME/build/apr-${APR_VERSION}
+        ./configure ${APU_CONFIG} --prefix=$HOME/root/apu-${APU_VERSION}
+        make -j2
+        make install
+        popd
+    fi
 fi
--- test/travis_run_linux.sh
+++ test/travis_run_linux.sh
@@ -11,7 +11,17 @@ if test ! -v SKIP_TESTING; then
         CONFIG="--prefix=$HOME/build/httpd-root $CONFIG"
     fi
 fi
-./configure $CONFIG --with-apr=/usr --with-apr-util=/usr
+if test -v APR_VERSION; then
+    CONFIG="$CONFIG --with-apr=$HOME/root/apr-${APR_VERSION}"
+else
+    CONFIG="$CONFIG --with-apr=/usr"
+fi
+if test -v APU_VERSION; then
+    CONFIG="$CONFIG --with-apr-util=$HOME/root/apu-${APU_VERSION}"
+else
+    CONFIG="$CONFIG --with-apr-util=/usr"
+fi
+./configure $CONFIG
 make $MAKEFLAGS -j2
 if ! test -v SKIP_TESTING; then
     if test -v WITH_TEST_SUITE; then
