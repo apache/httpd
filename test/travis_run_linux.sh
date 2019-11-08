c096af4bcc808ac025cb1f9d58eacc5701b3bd68
Joe Orton <jorton@apache.org>
2019-11-08 07:45:55 +0000

    Support 2.4.x builds, which lacks --with-test-suite support. Build with --enable-load-all-modules only when the test suite is run.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1869527 13f79535-47bb-0310-9956-ffa450edef68

--- test/travis_run_linux.sh
+++ test/travis_run_linux.sh
@@ -1,6 +1,25 @@
 #!/bin/bash -ex
-./buildconf
-test -v SKIP_TESTING || CONFIG="--with-test-suite=test/perl-framework $CONFIG"
+./buildconf --with-apr=/usr/bin/apr-1-config
+# For trunk, "make check" is sufficient to run the test suite.
+# For 2.4.x, the test suite must be run manually
+if test ! -v SKIP_TESTING; then
+    CONFIG="$CONFIG --enable-load-all-modules"
+    if grep -q ^check: Makefile.in; then
+        CONFIG="--with-test-suite=test/perl-framework $CONFIG"
+        WITH_TEST_SUITE=1
+    else
+        CONFIG="--prefix=$HOME/build/httpd-root $CONFIG"
+    fi
+fi
 ./configure $CONFIG --with-apr=/usr --with-apr-util=/usr
 make $MAKEFLAGS -j2
-test -v SKIP_TESTING || make check
+if ! test -v SKIP_TESTING; then
+    if test -v WITH_TEST_SUITE; then
+        make check
+    else
+        make install
+        cd test/perl-framework
+        perl Makefile.PL -apxs $HOME/build/httpd-root/bin/apxs
+        make test
+    fi
+fi
