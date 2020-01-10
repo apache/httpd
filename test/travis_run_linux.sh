debdedc681b5c98084be16c08c376e9c69dc7d84
Joe Orton <jorton@apache.org>
2020-01-10 10:20:07 +0000

    Add Travis job running the litmus WebDAV test suite.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1872588 13f79535-47bb-0310-9956-ffa450edef68

--- test/travis_run_linux.sh
+++ test/travis_run_linux.sh
@@ -30,8 +30,17 @@ if ! test -v SKIP_TESTING; then
         make check TESTS="${TEST_ARGS}"
     else
         make install
-        cd test/perl-framework
-        perl Makefile.PL -apxs $HOME/build/httpd-root/bin/apxs
-        make test APACHE_TEST_EXTRA_ARGS="${TEST_ARGS}"
+        pushd test/perl-framework
+            perl Makefile.PL -apxs $HOME/build/httpd-root/bin/apxs
+            make test APACHE_TEST_EXTRA_ARGS="${TEST_ARGS}"
+        popd
+    fi
+    if test -v LITMUS; then
+        pushd test/perl-framework
+           mkdir -p t/htdocs/modules/dav
+           ./t/TEST -start
+           litmus http://localhost:8529/modules/dav/
+           ./t/TEST -stop
+        popd
     fi
 fi
