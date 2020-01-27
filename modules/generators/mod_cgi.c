9f706003f26465bb7670b66c157d329424d4c751
Joe Orton <jorton@redhat.com>
2020-01-27 15:54:03 +0100

    Backport r1867970

--- modules/generators/mod_cgi.c
+++ modules/generators/mod_cgi.c
@@ -541,19 +541,15 @@ static void discard_script_output(apr_bucket_brigade *bb)
     apr_bucket *e;
     const char *buf;
     apr_size_t len;
-    apr_status_t rv;
 
     for (e = APR_BRIGADE_FIRST(bb);
-         e != APR_BRIGADE_SENTINEL(bb);
-         e = APR_BUCKET_NEXT(e))
+         e != APR_BRIGADE_SENTINEL(bb) && !APR_BUCKET_IS_EOS(e);
+         e = APR_BRIGADE_FIRST(bb))
     {
-        if (APR_BUCKET_IS_EOS(e)) {
+        if (apr_bucket_read(e, &buf, &len, APR_BLOCK_READ)) {
             break;
         }
-        rv = apr_bucket_read(e, &buf, &len, APR_BLOCK_READ);
-        if (rv != APR_SUCCESS) {
-            break;
-        }
+        apr_bucket_delete(e);
     }
 }
 
