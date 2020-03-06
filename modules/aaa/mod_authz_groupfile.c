8cf60e50043b9548308b0d6b18dcd650217b6906
Jim Jagielski <jim@apache.org>
2020-03-06 16:13:09 +0000

    Merge r1874424 from trunk:
    PR64172: drop severity of AH01666
    
    
    Submitted by: covener
    Reviewed by: covener, ylavic, jim
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1874907 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_authz_groupfile.c
+++ modules/aaa/mod_authz_groupfile.c
@@ -172,7 +172,7 @@ static authz_status group_check_authorization(request_rec *r,
 
     if (apr_is_empty_table(grpstatus)) {
         /* no groups available, so exit immediately */
-        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(01666)
+        ap_log_rerror(APLOG_MARK, APLOG_INFO, 0, r, APLOGNO(01666)
                       "Authorization of user %s to access %s failed, reason: "
                       "user doesn't appear in group file (%s).",
                       r->user, r->uri, conf->groupfile);
