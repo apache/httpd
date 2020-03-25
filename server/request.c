8e7b3d49024b6c7d35d9608f3faf49f921cacd02
Eric Covener <covener@apache.org>
2020-03-25 02:01:04 +0000

    revert for now, surprising the scheme is present.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1875605 13f79535-47bb-0310-9956-ffa450edef68

--- server/request.c
+++ server/request.c
@@ -194,9 +194,7 @@ AP_DECLARE(int) ap_process_request_internal(request_rec *r)
 
     ap_getparents(r->uri);     /* OK --- shrinking transformations... */
     if (sconf->merge_slashes != AP_CORE_CONFIG_OFF) { 
-        int offset = r->parsed_uri.scheme ? strlen(r->parsed_uri.scheme) + 3 /* '://' */
-                                          : 0;
-        ap_no2slash(&r->uri[offset]);
+        ap_no2slash(r->uri);
         if (r->parsed_uri.path) {
             ap_no2slash(r->parsed_uri.path);
         }
