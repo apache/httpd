11d565c3ce89c4a1b0fc9b7c09c9f6b71b8e4fd4
Joe Orton <jorton@apache.org>
2020-03-26 11:03:54 +0000

    * modules/ssl/ssl_private.h: Define X509_up_ref and EVP_PKEY_up_ref   for OpenSSL < 1.1.
    * modules/ssl/ssl_engine_kernel.c (modssl_set_cert_info):
    * modules/ssl/ssl_util_stapling.c (stapling_get_issuer): 
      Use the above macros for all OpenSSL versions.
    
    Github: closes #104
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1875702 13f79535-47bb-0310-9956-ffa450edef68

--- modules/ssl/ssl_util_stapling.c
+++ modules/ssl/ssl_util_stapling.c
@@ -107,11 +107,7 @@ static X509 *stapling_get_issuer(modssl_ctx_t *mctx, X509 *x)
     for (i = 0; i < sk_X509_num(extra_certs); i++) {
         issuer = sk_X509_value(extra_certs, i);
         if (X509_check_issued(issuer, x) == X509_V_OK) {
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
-            CRYPTO_add(&issuer->references, 1, CRYPTO_LOCK_X509);
-#else
             X509_up_ref(issuer);
-#endif
             return issuer;
         }
     }
