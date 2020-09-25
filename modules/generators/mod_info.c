b428ab04feb1fb44af17640e09fe4fe95e1d7137
Christophe Jaillet <jailletc36@apache.org>
2020-09-25 21:06:48 +0000

    Merge r1880500, r1880501, r1880509 from trunk
       * Slightly speed-up the execution of the test framework.
    
    Submitted by: jailletc36
    Reviewed by: jailletc36, rpluem, jorton
    Backported by: jailletc36
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1882028 13f79535-47bb-0310-9956-ffa450edef68

--- modules/generators/mod_info.c
+++ modules/generators/mod_info.c
@@ -230,7 +230,7 @@ static int mod_info_has_cmd(const command_rec * cmds, ap_directive_t * dir)
     if (cmds == NULL)
         return 1;
     for (cmd = cmds; cmd->name; ++cmd) {
-        if (strcasecmp(cmd->name, dir->directive) == 0)
+        if (ap_cstr_casecmp(cmd->name, dir->directive) == 0)
             return 1;
     }
     return 0;
