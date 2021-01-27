3e4c918fba9fe8306eed1fcbdc19699af30e23b8
Ruediger Pluem <rpluem@apache.org>
2021-01-27 08:01:06 +0000

    Do not allow to set empty bind passwords to be set via AuthLDAPBindPassword
    Binds with empty passwords always succeed, but in case the password of the
    user was not empty subsequent LDAP operations fail.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1885939 13f79535-47bb-0310-9956-ffa450edef68

--- modules/aaa/mod_authnz_ldap.c
+++ modules/aaa/mod_authnz_ldap.c
@@ -1719,6 +1719,10 @@ static const char *set_bind_password(cmd_parms *cmd, void *_cfg, const char *arg
         sec->bindpw = (char *)arg;
     }
 
+    if (!(*sec->bindpw)) {
+        return "Empty passwords are invalid for AuthLDAPBindPassword";
+    }
+
     return NULL;
 }
 
