599310caf39a1eaf62ab0fe54fd212ed27fdd4c3
Ruediger Pluem <rpluem@apache.org>
2021-03-02 20:02:32 +0000

    Merge r1398307, r1465839, r1530603, r1578759, r1729507 from trunk:
    * If we face a timeout during receiving the response from the backend and if
      we pinged it successfully before don't assume the whole backend has failed.
      Assume that only the request has failed and return a gateway timeout then.
    
    
    
    Add failontimeout to allow server admin to mark balancer member in err if IO timeout occurs.
    
    ping tuning via Yann Ylavic <ylavic.dev@gmail.com>
    
    
    Use 'apr_table_setn' instead of 'apr_table_set' when possible in order to save memory.
    
    mod_proxy: axe negative "ping" parameter setting and handling.
    This used to check for the backend connection readability only (instead of
    the full ping/100-continue round-trip), but the case is already handled by
    ap_proxy_connect_backend() which is always called.
    
    
    Submitted by: rpluem, druggeri, jim, jailletc36, ylavic
    Reviewed by: rpluem, ylavic, icing
    
    Github: closes #170
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1887116 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_ajp.c
+++ modules/proxy/mod_proxy_ajp.c
@@ -349,7 +349,8 @@ static int ap_proxy_ajp_request(apr_pool_t *p, request_rec *r,
          * we assume it is a request that cause a back-end timeout,
          * but doesn't affect the whole worker.
          */
-        if (APR_STATUS_IS_TIMEUP(status) && conn->worker->s->ping_timeout_set) {
+        if (APR_STATUS_IS_TIMEUP(status) &&
+                conn->worker->s->ping_timeout_set) {
             return HTTP_GATEWAY_TIME_OUT;
         }
 
@@ -680,7 +681,18 @@ static int ap_proxy_ajp_request(apr_pool_t *p, request_rec *r,
              */
             rv = HTTP_SERVICE_UNAVAILABLE;
         } else {
-            rv = HTTP_INTERNAL_SERVER_ERROR;
+            /* If we had a successful cping/cpong and then a timeout
+             * we assume it is a request that cause a back-end timeout,
+             * but doesn't affect the whole worker.
+             */
+            if (APR_STATUS_IS_TIMEUP(status) &&
+                    conn->worker->s->ping_timeout_set) {
+                apr_table_setn(r->notes, "proxy_timedout", "1");
+                rv = HTTP_GATEWAY_TIME_OUT;
+            }
+            else {
+                rv = HTTP_INTERNAL_SERVER_ERROR;
+            }
         }
     }
     else if (client_failed) {
