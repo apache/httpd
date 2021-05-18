69ef5ca58b7613c554e91254503ca7817631479e
Stefan Eissing <icing@apache.org>
2021-05-18 12:11:32 +0000

    Merge: /httpd/httpd/trunk:r1889995
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1889997 13f79535-47bb-0310-9956-ffa450edef68

--- modules/md/md_crypt.c
+++ modules/md/md_crypt.c
@@ -71,6 +71,11 @@
 #include <openssl/ct.h>
 #endif
 
+#if defined(LIBRESSL_VERSION_NUMBER)
+#define EVP_PKEY_X25519 NID_X25519
+#define EVP_PKEY_X448 NID_X448
+#endif
+
 static int initialized;
 
 struct md_pkey_t {
