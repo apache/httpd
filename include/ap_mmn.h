a962ba73047b5478d702c8ad09fd1a167e1d3736
Yann Ylavic <ylavic@apache.org>
2021-12-14 15:35:56 +0000

    Merge r1895914, r1895921 from trunk:
      *) http: Enforce that fully qualified uri-paths not to be forward-proxied
         have an http(s) scheme, and that the ones to be forward proxied have a
         hostname, per HTTP specifications.
         trunk patch: http://svn.apache.org/r1895914
                      http://svn.apache.org/r1895921
         2.4.x patch: https://patch-diff.githubusercontent.com/raw/apache/httpd/pull/286.patch
         backport PR: https://github.com/apache/httpd/pull/286
         +1: ylavic, minfrin, gbechis
    
    
    mod_proxy: Detect unix: scheme syntax errors at load time.
    
    * modules/proxy/mod_proxy.c(add_pass, add_member, set_proxy_param,
                                proxysection):
      Check return value of ap_proxy_de_socketfy().
    
    * modules/proxy/proxy_util.c(ap_proxy_get_worker_ex):
      Check return value of ap_proxy_de_socketfy().
    
    
    
    http: Enforce that fully qualified uri-paths not to be forward-proxied
          have an http(s) scheme, and that the ones to be forward proxied have a
          hostname, per HTTP specifications.
    
    The early checks avoid failing the request later on and thus save cycles
    for those invalid cases.
    
    
    Submitted by: ylavic
    Reviewed by: ylavic, minfrin, gbechis
    Closes #286
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1895955 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_mmn.h
+++ include/ap_mmn.h
@@ -586,6 +586,7 @@
  *                           dav_find_attr().
  * 20120211.120 (2.4.51-dev) Add dav_liveprop_elem structure and
  *                           dav_get_liveprop_element().
+ * 20120211.121 (2.4.51-dev) Add ap_post_read_request()
  * 
  */
 
@@ -594,7 +595,7 @@
 #ifndef MODULE_MAGIC_NUMBER_MAJOR
 #define MODULE_MAGIC_NUMBER_MAJOR 20120211
 #endif
-#define MODULE_MAGIC_NUMBER_MINOR 120                 /* 0...n */
+#define MODULE_MAGIC_NUMBER_MINOR 121                 /* 0...n */
 
 /**
  * Determine if the server's current MODULE_MAGIC_NUMBER is at least a
