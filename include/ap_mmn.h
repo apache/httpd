9d74016b80d2cf537811499df6bb46b079c8312e
Stefan Eissing <icing@apache.org>
2022-04-19 10:16:02 +0000

    *) core: add ap_sb_get_child_thread() to retrieve child_num      and thread_num from a scoreboard handle.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1900029 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_mmn.h
+++ include/ap_mmn.h
@@ -710,6 +710,7 @@
  *                         Add new http/1.x formatting helpers
  *                         Add ap_assign_request_line()
  * 20211221.7 (2.5.1-dev)  Add ap_h1_append_header()
+ * 20211221.8 (2.5.1-dev)  Add ap_sb_get_child_thread()
  */
 
 #define MODULE_MAGIC_COOKIE 0x41503235UL /* "AP25" */
@@ -717,7 +718,7 @@
 #ifndef MODULE_MAGIC_NUMBER_MAJOR
 #define MODULE_MAGIC_NUMBER_MAJOR 20211221
 #endif
-#define MODULE_MAGIC_NUMBER_MINOR 7             /* 0...n */
+#define MODULE_MAGIC_NUMBER_MINOR 8             /* 0...n */
 
 /**
  * Determine if the server's current MODULE_MAGIC_NUMBER is at least a
