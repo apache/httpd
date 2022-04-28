cc894406d8e2c2afb79269dcb6190c28837fc142
Stefan Eissing <icing@apache.org>
2022-04-28 10:41:48 +0000

    *) mod_http2: remove unused and insecure code. Fixes PR66037.      Thanks to Ronald Crane (Zippenhop LLC) for reporting this.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1900356 13f79535-47bb-0310-9956-ffa450edef68

--- modules/http2/h2_util.c
+++ modules/http2/h2_util.c
@@ -76,26 +76,6 @@ size_t h2_util_hex_dump(char *buffer, size_t maxlen,
     return strlen(buffer);
 }
 
-size_t h2_util_header_print(char *buffer, size_t maxlen,
-                            const char *name, size_t namelen,
-                            const char *value, size_t valuelen)
-{
-    size_t offset = 0;
-    size_t i;
-    for (i = 0; i < namelen && offset < maxlen; ++i, ++offset) {
-        buffer[offset] = name[i];
-    }
-    for (i = 0; i < 2 && offset < maxlen; ++i, ++offset) {
-        buffer[offset] = ": "[i];
-    }
-    for (i = 0; i < valuelen && offset < maxlen; ++i, ++offset) {
-        buffer[offset] = value[i];
-    }
-    buffer[offset] = '\0';
-    return offset;
-}
-
-
 void h2_util_camel_case_header(char *s, size_t len)
 {
     size_t start = 1;
