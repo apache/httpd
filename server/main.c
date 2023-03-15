11eb5a312dea1fdc15640dce16ea19de99daa843
Yann Ylavic <ylavic@apache.org>
2023-03-15 10:42:57 +0000

    core: Follow up to r1908393: comment about where ap_server_conf is set.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1908406 13f79535-47bb-0310-9956-ffa450edef68

--- server/main.c
+++ server/main.c
@@ -717,7 +717,7 @@ static void usage(process_rec *process)
     if (temp_error_log) {
         ap_replace_stderr_log(process->pool, temp_error_log);
     }
-    ap_server_conf = NULL;
+    ap_server_conf = NULL; /* set early by ap_read_config() for logging */
     if (!ap_read_config(process, ptemp, confname, &ap_conftree)) {
         if (showcompile) {
             /* Well, we tried. Show as much as we can, but exit nonzero to
@@ -825,7 +825,7 @@ static void usage(process_rec *process)
         apr_pool_create(&ptemp, pconf);
         apr_pool_tag(ptemp, "ptemp");
         ap_server_root = def_server_root;
-        ap_server_conf = NULL;
+        ap_server_conf = NULL; /* set early by ap_read_config() for logging */
         if (!ap_read_config(process, ptemp, confname, &ap_conftree)) {
             destroy_and_exit_process(process, 1);
         }
