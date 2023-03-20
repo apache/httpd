62c5aeadd0099b9f5ff11f304eab81ecd6d6b04f
Yann Ylavic <ylavic@apache.org>
2023-03-20 15:02:13 +0000

    pytests: Use python3-multipart lib.
    Use the API of https://github.com/andrew-d/python-multipart, available as
    package python3-multipart on Debian/Ubuntu.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1908574 13f79535-47bb-0310-9956-ffa450edef68

--- test/modules/tls/htdocs/a.mod-tls.test/vars.py
+++ test/modules/tls/htdocs/a.mod-tls.test/vars.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python3
 import json
 import os, sys
-import multipart
 from urllib import parse
+import multipart # https://github.com/andrew-d/python-multipart (`apt install python3-multipart`)
 
 
 def get_request_params():
     oforms = {}
+    ofiles = {}
     if "REQUEST_URI" in os.environ:
         qforms = parse.parse_qs(parse.urlsplit(os.environ["REQUEST_URI"]).query)
         for name, values in qforms.items():
             oforms[name] = values[0]
-    myenv = os.environ.copy()
-    myenv['wsgi.input'] = sys.stdin.buffer
-    mforms, ofiles = multipart.parse_form_data(environ=myenv)
-    for name, item in mforms.items():
-        oforms[name] = item
+    if "HTTP_CONTENT_TYPE" in os.environ:
+        ctype = os.environ["HTTP_CONTENT_TYPE"]
+        if ctype == "application/x-www-form-urlencoded":
+            qforms = parse.parse_qs(parse.urlsplit(sys.stdin.read()).query)
+            for name, values in qforms.items():
+                oforms[name] = values[0]
+        elif ctype.startswith("multipart/"):
+            def on_field(field):
+                oforms[field.field_name] = field.value
+            def on_file(file):
+                ofiles[field.field_name] = field.value
+            multipart.parse_form(headers={"Content-Type": ctype}, input_stream=sys.stdin.buffer, on_field=on_field, on_file=on_file)
     return oforms, ofiles
 
 
