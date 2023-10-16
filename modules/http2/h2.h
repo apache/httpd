apache/httpd

commit 931c39c03b9fb181540871c78b5fd3bc71b387e7

Further h2 compile fixes:
* modules/http2/h2_session.c (h2_session_start):
  Restrict WebSockets options handling to with-WS builds.

* modules/http2/h2_ws.c: Don't include apr_encode.h (not used).


git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1913019 13f79535-47bb-0310-9956-ffa450edef68

modules/http2/h2_ws.c
@@ -19,7 +19,6 @@
 #include "apr.h"
 #include "apr_strings.h"
 #include "apr_lib.h"
-#include "apr_encode.h"
 #include "apr_sha1.h"
 #include "apr_strmatch.h"
 

modules/http2/h2_session.c
@@ -1085,11 +1085,13 @@ static apr_status_t h2_session_start(h2_session *session, int *rv)
         settings[slen].value = win_size;
         ++slen;
     }
+#if H2_USE_WEBSOCKETS
     if (h2_config_sgeti(session->s, H2_CONF_WEBSOCKETS)) {
       settings[slen].settings_id = NGHTTP2_SETTINGS_ENABLE_CONNECT_PROTOCOL;
       settings[slen].value = 1;
       ++slen;
     }
+#endif
 
     ap_log_cerror(APLOG_MARK, APLOG_DEBUG, status, session->c1,
                   H2_SSSN_LOG(APLOGNO(03201), session, 

commit b2ef1b58868191a779c86b0e8956af7752f21c18

Fix h2 build broken in r1913019:
* modules/http2: Make WebSockets support conditional also
  on APR 1.7.x since apr_encode.h is required. Restore
  #include "apr_encode.h" removed in previous commit.


git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1913023 13f79535-47bb-0310-9956-ffa450edef68

modules/http2/h2.h
@@ -41,7 +41,9 @@ struct h2_stream;
 #define H2_USE_POLLFD_FROM_CONN 0
 #endif
 
-#if H2_USE_PIPES && defined(NGHTTP2_VERSION_NUM) && NGHTTP2_VERSION_NUM >= 0x012200
+/* WebSockets support requires apr 1.7.0 for apr_encode.h, plus the
+ * WebSockets features of nghttp2 1.34.0 and later. */
+#if H2_USE_PIPES && defined(NGHTTP2_VERSION_NUM) && NGHTTP2_VERSION_NUM >= 0x012200 && APR_VERSION_AT_LEAST(1,7,0)
 #define H2_USE_WEBSOCKETS       1
 #else
 #define H2_USE_WEBSOCKETS       0

modules/http2/h2_ws.c
@@ -44,6 +44,8 @@
 
 #if H2_USE_WEBSOCKETS
 
+#include "apr_encode.h" /* H2_USE_WEBSOCKETS is conditional on APR 1.6+ */
+
 static ap_filter_rec_t *c2_ws_out_filter_handle;
 
 struct ws_filter_ctx {