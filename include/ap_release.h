9d8a3a209073108454281c44617c83e4ef60bc92
Stefan Eissing <icing@apache.org>
2023-10-19 09:27:09 +0000

    publishing release httpd-2.4.58
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1913112 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_release.h
+++ include/ap_release.h
@@ -43,7 +43,7 @@
 
 #define AP_SERVER_MAJORVERSION_NUMBER 2
 #define AP_SERVER_MINORVERSION_NUMBER 4
-#define AP_SERVER_PATCHLEVEL_NUMBER   58
+#define AP_SERVER_PATCHLEVEL_NUMBER   59
 #define AP_SERVER_DEVBUILD_BOOLEAN    1
 
 /* Synchronize the above with docs/manual/style/version.ent */
