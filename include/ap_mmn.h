c09f4ffdbb15d709beec20fe9b07f0a582fbe5c6
Yann Ylavic <ylavic@apache.org>
2024-06-01 16:03:58 +0000

    Follow up to r1918098 (and r1918022): Actually bump the MMN.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1918100 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_mmn.h
+++ include/ap_mmn.h
@@ -732,7 +732,7 @@
 #ifndef MODULE_MAGIC_NUMBER_MAJOR
 #define MODULE_MAGIC_NUMBER_MAJOR 20211221
 #endif
-#define MODULE_MAGIC_NUMBER_MINOR 20             /* 0...n */
+#define MODULE_MAGIC_NUMBER_MINOR 21             /* 0...n */
 
 /**
  * Determine if the server's current MODULE_MAGIC_NUMBER is at least a
