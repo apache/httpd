f4a5fbe55d14d5fbd8871dbd3ab9bfbe04fed667
Yann Ylavic <ylavic@apache.org>
2024-08-01 12:48:02 +0000

    Fix 2.4 versions of latest MMN entries.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1919618 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_mmn.h
+++ include/ap_mmn.h
@@ -603,10 +603,10 @@
  *                           and AP_REQUEST_TRUSTED_CT BNOTE.
  * 20120211.133 (2.4.60-dev) Add ap_proxy_fixup_uds_filename()
  * 20120211.134 (2.4.60-dev) AP_SLASHES and AP_IS_SLASH
- * 20120211.135 (2.4.59-dev) Add CONN_STATE_ASYNC_WAITIO, CONN_STATE_KEEPALIVE
+ * 20120211.135 (2.4.63-dev) Add CONN_STATE_ASYNC_WAITIO, CONN_STATE_KEEPALIVE
  *                           and CONN_STATE_PROCESSING
- * 20120211.136 (2.4.59-dev) Add wait_io field to struct process_score
- * 20120211.137 (2.4.59-dev) Add AP_MPMQ_CAN_WAITIO
+ * 20120211.136 (2.4.63-dev) Add wait_io field to struct process_score
+ * 20120211.137 (2.4.63-dev) Add AP_MPMQ_CAN_WAITIO
  */
 
 #define MODULE_MAGIC_COOKIE 0x41503234UL /* "AP24" */
