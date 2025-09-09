8217af3f75342a54cb2bfaf0aea4a2279559f331
Leo <i@hardrain980.com>
2025-09-09 10:31:09 +0800

    modules/dav/fs/repos.c: return 500 instead of 400 upon `apr_file_mtime_set` fails, emit error-level log message also docs/log-message-tags: bumped with `docs/log-message-tags/update-log-msg-tags`
    Signed-off-by: Leo <i@hardrain980.com>

--- modules/dav/fs/repos.c
+++ modules/dav/fs/repos.c
@@ -1168,12 +1168,17 @@ static dav_error * dav_fs_set_mtime(dav_resource *resource, apr_time_t mtime)
 {
     apr_pool_t *pool;
     apr_status_t status;
+    char errbuf[1024];
 
     pool = resource->pool;
     status = apr_file_mtime_set(resource->info->pathname, mtime, pool);
 
     if (status != APR_SUCCESS) {
-        return dav_new_error(pool, HTTP_BAD_REQUEST, 0, status, "Could not set mtime.");
+        apr_strerror(status, errbuf, sizeof(errbuf));
+        ap_log_perror(APLOG_MARK, APLOG_ERR, status, pool, APLOGNO(10521)
+                      "Failed setting mtime for file %s: apr_file_mtime_set: %s",
+                      resource->info->pathname, errbuf);
+        return dav_new_error(pool, HTTP_INTERNAL_SERVER_ERROR, 0, status, "Could not set mtime");
     }
 
     return NULL;
