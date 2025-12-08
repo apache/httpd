d2c5828a7390602f3a780e83eb39039e4a83fc3a
Stefan Eissing <icing@apache.org>
2025-12-08 13:01:14 +0000

    Merge /httpd/httpd/trunk:r1930363
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x@1930364 13f79535-47bb-0310-9956-ffa450edef68

--- modules/md/mod_md_ocsp.c
+++ modules/md/mod_md_ocsp.c
@@ -61,8 +61,18 @@ int md_ocsp_prime_status(server_rec *s, apr_pool_t *p,
     apr_array_header_t *chain;
     apr_status_t rv = APR_ENOENT;
 
+    ap_log_error(APLOG_MARK, APLOG_TRACE1, 0, s, "ocsp prime status call for: %s",
+                 s->server_hostname);
     sc = md_config_get(s);
-    if (!staple_here(sc)) goto cleanup;
+    if (!staple_here(sc)) {
+        ap_log_error(APLOG_MARK, APLOG_TRACE1, 0, s,
+                     "ocsp prime does not apply here: server=%s, sc=%d"
+                     "ocsp=%d, conf-ocsp=%d conf-others=%d",
+                     s->server_hostname, !!sc, sc? !!sc->mc->ocsp : 0,
+                     md_config_geti(sc, MD_CONFIG_STAPLING),
+                     md_config_geti(sc, MD_CONFIG_STAPLE_OTHERS));
+        goto cleanup;
+    }
 
     md = ((sc->assigned && sc->assigned->nelts == 1)?
           APR_ARRAY_IDX(sc->assigned, 0, const md_t*) : NULL);
