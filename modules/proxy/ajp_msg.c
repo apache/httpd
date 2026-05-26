d94db8fd8d42894f3862381e3fb0060f450dc7c9
Christophe Jaillet <jailletc36@apache.org>
2026-05-26 20:35:23 +0000

    Follow-up to r1932498.
    The error message also has to be updated.
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1934658 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/ajp_msg.c
+++ modules/proxy/ajp_msg.c
@@ -591,7 +591,7 @@ apr_status_t ajp_msg_copy(ajp_msg_t *smsg, ajp_msg_t *dmsg)
         ap_log_error(APLOG_MARK, APLOG_ERR, 0, NULL, APLOGNO(01082)
                      "ajp_msg_copy(): destination buffer too "
                      "small %" APR_SIZE_T_FMT ", max size is %" APR_SIZE_T_FMT,
-                     smsg->len, smsg->max_size);
+                     smsg->len, dmsg->max_size);
         return  AJP_ETOSMALL;
     }
 
