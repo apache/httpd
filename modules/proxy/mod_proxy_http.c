0237005ebe405f4ff5ad0b949a0fe89fb025f65f
Michael Osipov <michaelo@apache.org>
2026-06-03 09:08:46 +0000

    Use ap_get_status_line() instead of string literals
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1934914 13f79535-47bb-0310-9956-ffa450edef68

--- modules/proxy/mod_proxy_http.c
+++ modules/proxy/mod_proxy_http.c
@@ -1288,7 +1288,7 @@ int ap_proxy_http_process_response(proxy_http_req_t *req)
                  */
                 r->headers_out = apr_table_make(r->pool,1);
                 r->status = HTTP_BAD_GATEWAY;
-                r->status_line = "bad gateway";
+                r->status_line = ap_get_status_line(r->status);
                 return r->status;
             }
 
@@ -1414,7 +1414,7 @@ int ap_proxy_http_process_response(proxy_http_req_t *req)
             /* an http/0.9 response */
             backasswards = 1;
             r->status = proxy_status = HTTP_OK;
-            r->status_line = "200 OK";
+            r->status_line = ap_get_status_line(r->status);
             backend->close = 1;
         }
 
