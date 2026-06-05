52cb79b19e110bdd42bffc696844f801fb3c4a2d
Eric Covener <covener@apache.org>
2026-06-05 10:17:58 +0000

    ap_regname: restrict to reasonable captures
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1935014 13f79535-47bb-0310-9956-ffa450edef68

--- server/core.c
+++ server/core.c
@@ -2600,7 +2600,9 @@ static const char *dirsection(cmd_parms *cmd, void *mconfig, const char *arg)
 
     if (cmd->regex) {
         conf->refs = apr_array_make(cmd->pool, 8, sizeof(char *));
-        ap_regname(cmd->regex, conf->refs, AP_REG_MATCH, 1);
+        if (ap_regname(cmd->regex, conf->refs, AP_REG_MATCH, 1) < 0) {
+            return "Error processing regex captures";
+        }
     }
 
     /* Make this explicit - the "/" root has 0 elements, that is, we
@@ -2685,7 +2687,9 @@ static const char *urlsection(cmd_parms *cmd, void *mconfig, const char *arg)
 
     if (cmd->regex) {
         conf->refs = apr_array_make(cmd->pool, 8, sizeof(char *));
-        ap_regname(cmd->regex, conf->refs, AP_REG_MATCH, 1);
+        if (ap_regname(cmd->regex, conf->refs, AP_REG_MATCH, 1) < 0) {
+            return "Error processing regex captures";
+        }
     }
 
     ap_add_per_url_conf(cmd->server, new_url_conf);
@@ -2776,7 +2780,9 @@ static const char *filesection(cmd_parms *cmd, void *mconfig, const char *arg)
 
     if (cmd->regex) {
         conf->refs = apr_array_make(cmd->pool, 8, sizeof(char *));
-        ap_regname(cmd->regex, conf->refs, AP_REG_MATCH, 1);
+        if (ap_regname(cmd->regex, conf->refs, AP_REG_MATCH, 1) < 0) {
+            return "Error processing regex captures";
+        }
     }
 
     ap_add_file_conf(cmd->pool, (core_dir_config *)mconfig, new_file_conf);
