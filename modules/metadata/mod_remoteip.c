apache/httpd

commit 7c6129b51935d9165241279637915eaf905c58f1

mod_remoteip: Validate v2 PROXY protocol address length
* modules/metadata/mod_remoteip.c
  (remoteip_get_v2_len): Move definition before first use.
  (remoteip_parse_v2_header): Add length validation for TCPv4
  and TCPv6 address families before parsing, returning HDR_ERROR
  if the header length is too short.

Submitted by: arshiya tabasum <arshi bugqore.com>
GitHub: closes #683


git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1936357 13f79535-47bb-0310-9956-ffa450edef68

modules/metadata/mod_remoteip.c
@@ -931,6 +931,12 @@ static int remoteip_hook_pre_connection(conn_rec *c, void *csd)
     return OK;
 }
 
+/** Return length for a v2 protocol header. */
+static apr_size_t remoteip_get_v2_len(proxy_header *hdr)
+{
+    return ntohs(hdr->v2.len);
+}
+
 /* Binary format:
  * <sig><cmd><proto><addr-len><addr>
  * sig = \x0D \x0A \x0D \x0A \x00 \x0D \x0A \x51 \x55 \x49 \x54 \x0A
@@ -956,6 +962,13 @@ static remoteip_parse_status_t remoteip_process_v2_header(conn_rec *c,
         case 0x01: /* PROXY command */
             switch (hdr->v2.fam) {
                 case 0x11:  /* TCPv4 */
+                    if (remoteip_get_v2_len(hdr) < sizeof(hdr->v2.addr.ip4)) {
+                        ap_log_cerror(APLOG_MARK, APLOG_ERR, 0, c, APLOGNO()
+                                      "RemoteIPProxyProtocol: address length "
+                                      "%" APR_SIZE_T_FMT " too short for TCPv4",
+                                      remoteip_get_v2_len(hdr));
+                        return HDR_ERROR;
+                    }
                     ret = apr_sockaddr_info_get(&conn_conf->client_addr, NULL,
                                                 APR_INET,
                                                 ntohs(hdr->v2.addr.ip4.src_port),
@@ -973,6 +986,13 @@ static remoteip_parse_status_t remoteip_process_v2_header(conn_rec *c,
 
                 case 0x21:  /* TCPv6 */
 #if APR_HAVE_IPV6
+                    if (remoteip_get_v2_len(hdr) < sizeof(hdr->v2.addr.ip6)) {
+                        ap_log_cerror(APLOG_MARK, APLOG_ERR, 0, c, APLOGNO()
+                                      "RemoteIPProxyProtocol: address length "
+                                      "%" APR_SIZE_T_FMT " too short for TCPv6",
+                                      remoteip_get_v2_len(hdr));
+                        return HDR_ERROR;
+                    }
                     ret = apr_sockaddr_info_get(&conn_conf->client_addr, NULL,
                                                 APR_INET6,
                                                 ntohs(hdr->v2.addr.ip6.src_port),
@@ -1017,12 +1037,6 @@ static remoteip_parse_status_t remoteip_process_v2_header(conn_rec *c,
     }
 
     return HDR_DONE;
-}
-
-/** Return length for a v2 protocol header. */
-static apr_size_t remoteip_get_v2_len(proxy_header *hdr)
-{
-    return ntohs(hdr->v2.len);
 }
 
 /** Determine if this is a v1 or v2 PROXY header.

commit f6c2694cc25eee683f01f13e47e065fe577a06c8

Fill in APLOGNO() missed in r1936357.
Fixes: 7c6129b51935d9165241279637915eaf905c58f1


git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1936366 13f79535-47bb-0310-9956-ffa450edef68

docs/log-message-tags/next-number
@@ -1,1 +1,1 @@
-10597
+10599

modules/metadata/mod_remoteip.c
@@ -963,7 +963,7 @@ static remoteip_parse_status_t remoteip_process_v2_header(conn_rec *c,
             switch (hdr->v2.fam) {
                 case 0x11:  /* TCPv4 */
                     if (remoteip_get_v2_len(hdr) < sizeof(hdr->v2.addr.ip4)) {
-                        ap_log_cerror(APLOG_MARK, APLOG_ERR, 0, c, APLOGNO()
+                        ap_log_cerror(APLOG_MARK, APLOG_ERR, 0, c, APLOGNO(10597)
                                       "RemoteIPProxyProtocol: address length "
                                       "%" APR_SIZE_T_FMT " too short for TCPv4",
                                       remoteip_get_v2_len(hdr));
@@ -987,7 +987,7 @@ static remoteip_parse_status_t remoteip_process_v2_header(conn_rec *c,
                 case 0x21:  /* TCPv6 */
 #if APR_HAVE_IPV6
                     if (remoteip_get_v2_len(hdr) < sizeof(hdr->v2.addr.ip6)) {
-                        ap_log_cerror(APLOG_MARK, APLOG_ERR, 0, c, APLOGNO()
+                        ap_log_cerror(APLOG_MARK, APLOG_ERR, 0, c, APLOGNO(10598)
                                       "RemoteIPProxyProtocol: address length "
                                       "%" APR_SIZE_T_FMT " too short for TCPv6",
                                       remoteip_get_v2_len(hdr));