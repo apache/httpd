e8922f0be9b068b12d70c19e234fa541f2d141d9
Eric Covener <covener@apache.org>
2026-07-20 19:55:13 +0000

    protect as in r1936401
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1936403 13f79535-47bb-0310-9956-ffa450edef68

--- modules/mappers/mod_dir.c
+++ modules/mappers/mod_dir.c
@@ -312,7 +312,7 @@ static int fixup_dir(request_rec *r)
     /* we're running between mod_rewrites fixup and its internal redirect handler, step aside */
     if (!strcmp(r->handler, REWRITE_REDIRECT_HANDLER_NAME)) { 
         /* Prevent DIR_MAGIC_TYPE from leaking out when someone has taken over */
-        if (!strcmp(r->content_type, DIR_MAGIC_TYPE)) { 
+        if (r->content_type && !strcmp(r->content_type, DIR_MAGIC_TYPE)) {
             r->content_type = NULL;
         }
         return DECLINED;
