65e4e9fc1e3897bc5ca3ba2e315a0f0a482142e9
Joe Orton <jorton@redhat.com>
2026-07-31 10:57:19 +0100

    * modules/generators/cgi_common.h (cgi_read_stdout): Log an error   if the read fails.
    PR: 70021

--- modules/generators/cgi_common.h
+++ modules/generators/cgi_common.h
@@ -312,20 +312,22 @@ static apr_status_t cgi_read_stdout(apr_bucket *a, apr_file_t *out,
 {
     char *buf;
     apr_status_t rv;
+    struct cgi_bucket_data *data = a->data;
 
     *str = NULL;
     *len = APR_BUCKET_BUFF_SIZE;
     buf = apr_bucket_alloc(*len, a->list); /* XXX: check for failure? */
 
     rv = apr_file_read(out, buf, len);
-
     if (rv != APR_SUCCESS && rv != APR_EOF) {
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, data->r, APLOGNO(10599)
+                      "failed reading stdout from script %s",
+                      data->r->filename);
         apr_bucket_free(buf);
         return rv;
     }
 
     if (*len > 0) {
-        struct cgi_bucket_data *data = a->data;
         apr_bucket_heap *h;
 
         /* Change the current bucket to refer to what we read */
