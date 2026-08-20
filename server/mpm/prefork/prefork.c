28bfb67e3e1f5ff357dc209459abca8b20414ed8
Tarek Ibrahim <t1br4h1m@gmail.com>
2026-08-20 15:47:26 -0400

    mpm_prefork: give clean_child_exit() a from_signal variant

--- server/mpm/prefork/prefork.c
+++ server/mpm/prefork/prefork.c
@@ -215,19 +215,18 @@ static void prefork_note_child_started(int slot, pid_t pid)
 }
 
 /* a clean exit from a child with proper cleanup */
-static void clean_child_exit(int code) __attribute__ ((noreturn));
-static void clean_child_exit(int code)
+static void clean_child_exit_ex(int code, int from_signal) __attribute__ ((noreturn));
+static void clean_child_exit_ex(int code, int from_signal)
 {
     retained->mpm->mpm_state = AP_MPMQ_STOPPING;
 
     apr_signal(SIGHUP, SIG_IGN);
     apr_signal(SIGTERM, SIG_IGN);
 
-    if (code == 0) {
-        ap_run_child_stopping(pchild, 0);
-    }
-
     if (pchild) {
+        if (!code && !from_signal) {
+            ap_run_child_stopping(pchild, !retained->mpm->is_ungraceful);
+        }
         apr_pool_destroy(pchild);
     }
 
@@ -238,6 +237,12 @@ static void clean_child_exit(int code)
     ap_mpm_pod_close(my_bucket->pod);
     chdir_for_gprof();
     exit(code);
+}
+
+static void clean_child_exit(int code) __attribute__ ((noreturn));
+static void clean_child_exit(int code)
+{
+    clean_child_exit_ex(code, 0);
 }
 
 static apr_status_t accept_mutex_on(void)
@@ -356,7 +361,7 @@ static const char *prefork_get_name(void)
 
 static void just_die(int sig)
 {
-    clean_child_exit(0);
+    clean_child_exit_ex(0, 1);
 }
 
 /* volatile because it's updated from a signal handler */
