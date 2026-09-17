bc3a3ec40b83e9bc7863894b574d4e5d988082f7
Joe Orton <jorton@apache.org>
2026-09-17 14:42:29 +0000

    * modules/md/md_acme_acct.c (acct_select): Guard the ctx->md   dereference in the debug log message, using "(none)" when no md   is present.
    Signed-off-by: p.nekrasov@fobos-nt.ru
    Signed-off-by: sovtouch@altlinux.org
    Submitted by: Pavel Nekrasov <p.nekrasov fobos-nt.ru>
    GitHub: closes #764
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/httpd/trunk@1938313 13f79535-47bb-0310-9956-ffa450edef68

--- modules/md/md_acme_acct.c
+++ modules/md/md_acme_acct.c
@@ -284,7 +284,8 @@ static int find_acct(void *baton, const char *name, const char *aspect,
             && (!ctx->md || md_acme_acct_matches_md(acct, ctx->md))) {
             md_log_perror(MD_LOG_MARK, MD_LOG_DEBUG, 0, ctx->p, 
                           "found account %s for %s: %s, status=%d",
-                          acct->id, ctx->md->ca_effective, aspect, acct->status);
+                          acct->id, ctx->md? ctx->md->ca_effective : "(none)",
+                          aspect, acct->status);
             ctx->id = apr_pstrdup(ctx->p, name);
             return 0;
         }
